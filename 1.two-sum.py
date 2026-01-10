a1bc7e8dba9a0cb66a1662f43828c5b13928493d
1271421361@qq.com <1271421361@qq.com@users.noreply.github.com>
2026-01-10 20:17:56 +0800

    Add solution: 1.two-sum.py

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -1,1 +1,16 @@
-#\n# @lc app=leetcode id=1 lang=python3\n#\n# [1] Two Sum\n#\n\n# @lc code=start\nclass Solution:\n    def twoSum(self, nums: List[int], target: int) -> List[int]:\n        hashmap = {}\n        for i, num in enumerate(nums):\n            complement = target - num\n            if complement in hashmap:\n                return [hashmap[complement], i]\n            hashmap[num] = i\n        return []\n# @lc code=end
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[num] = i
+        return []
+# @lc code=end
