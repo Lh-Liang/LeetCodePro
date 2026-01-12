7de29a1ce93dc8cd9f218e13dd223fd65c315d14
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-12 13:57:24 +0800

    Add solution: 3413.maximum-coins-from-k-consecutive-bags.py

--- 3413.maximum-coins-from-k-consecutive-bags.py
+++ 3413.maximum-coins-from-k-consecutive-bags.py
@@ -5,43 +5,47 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def maximumCoins(self, coins: List[List[int]], k: int) -> int:
         coins.sort()
         n = len(coins)
         
         def solve(intervals):
+            m = len(intervals)
             res = 0
-            current_sum = 0
-            j = 0
-            for i in range(n):
-                # Window starts at intervals[i][0], ends at intervals[i][0] + k - 1
-                window_end = intervals[i][0] + k - 1
-                while j < n and intervals[j][1] <= window_end:
-                    current_sum += (intervals[j][1] - intervals[j][0] + 1) * intervals[j][2]
-                    j += 1
+            curr_sum = 0
+            right = 0
+            for left in range(m):
+                # Window starts at intervals[left][0], ends at intervals[left][0] + k - 1
+                window_end = intervals[left][0] + k - 1
+                while right < m and intervals[right][1] <= window_end:
+                    curr_sum += (intervals[right][1] - intervals[right][0] + 1) * intervals[right][2]
+                    right += 1
                 
-                # Partial segment at the end
+                # Partial segment at the end of the window
                 extra = 0
-                if j < n and intervals[j][0] <= window_end:
-                    extra = (window_end - intervals[j][0] + 1) * intervals[j][2]
+                if right < m and intervals[right][0] <= window_end:
+                    extra = (window_end - intervals[right][0] + 1) * intervals[right][2]
                 
-                res = max(res, current_sum + extra)
+                res = max(res, curr_sum + extra)
                 
-                # Remove the current starting segment from sum before moving i
-                current_sum -= (intervals[i][1] - intervals[i][0] + 1) * intervals[i][2]
+                # Remove the current left segment from the sum for the next iteration
+                curr_sum -= (intervals[left][1] - intervals[left][0] + 1) * intervals[left][2]
             return res
 
-        # Case 1: Window starts at the beginning of a segment
-        ans1 = solve(coins)
+        # Case 1: Window starts at the beginning of some segment
+        ans = solve(coins)
         
-        # Case 2: Window ends at the end of a segment
-        # We can flip the segments and reuse the same logic
-        flipped_coins = []
+        # Case 2: Window ends at the end of some segment
+        # We transform the segments to represent them from right to left
+        reversed_coins = []
         for l, r, c in coins:
-            flipped_coins.append([-r, -l, c])
-        flipped_coins.sort()
-        ans2 = solve(flipped_coins)
+            reversed_coins.append([-r, -l, c])
+        reversed_coins.sort()
         
-        return max(ans1, ans2)
+        ans = max(ans, solve(reversed_coins))
+        
+        return ans
 # @lc code=end
