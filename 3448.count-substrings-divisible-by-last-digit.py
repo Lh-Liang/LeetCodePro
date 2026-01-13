42d324972c0735822b41540cf08fe04c57dfc470
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 00:32:11 +0800

    Add solution: 3448.count-substrings-divisible-by-last-digit.py

--- 3448.count-substrings-divisible-by-last-digit.py
+++ 3448.count-substrings-divisible-by-last-digit.py
@@ -7,35 +7,28 @@
 # @lc code=start
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        # dp[d][r] stores the number of substrings ending at the current index
-        # that have a remainder 'r' when divided by 'd'.
-        dp = [[0] * 10 for _ in range(10)]
-        total_count = 0
-        
-        for char in s:
-            v = ord(char) - 48
-            # next_dp will store the counts for substrings ending at the current character
-            next_dp = [[0] * 10 for _ in range(10)]
-            
+        # dp[d] is a list of length d: counts of substrings ending at current position with remainder r mod d
+        dp = {d: [0] * d for d in range(1, 10)}
+        ans = 0
+
+        for ch in s:
+            x = ord(ch) - 48
             for d in range(1, 10):
-                # For each divisor d, update the remainders for existing substrings
-                # extended by the current digit v.
-                d_row = dp[d]
-                nd_row = next_dp[d]
-                for r in range(d):
-                    if d_row[r] > 0:
-                        new_rem = (r * 10 + v) % d
-                        nd_row[new_rem] += d_row[r]
-                
-                # Also add the substring consisting of only the current digit itself.
-                nd_row[v % d] += 1
-            
-            # If the current digit is non-zero, it can be a divisor.
-            # Add the count of substrings ending here that are divisible by v.
-            if v > 0:
-                total_count += next_dp[v][0]
-            
-            dp = next_dp
-            
-        return total_count
+                prev = dp[d]
+                new = [0] * d
+
+                # extend all previous substrings by digit x
+                for r, cnt in enumerate(prev):
+                    if cnt:
+                        new[(r * 10 + x) % d] += cnt
+
+                # start a new substring at this position
+                new[x % d] += 1
+
+                dp[d] = new
+
+            if x != 0:
+                ans += dp[x][0]
+
+        return ans
 # @lc code=end
