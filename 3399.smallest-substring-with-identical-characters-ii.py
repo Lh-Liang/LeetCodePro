c9e4bbf00348b5f46b3cf0ddfbaadab8c614e3de
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 00:04:14 +0800

    Add solution: 3399.smallest-substring-with-identical-characters-ii.py

--- 3399.smallest-substring-with-identical-characters-ii.py
+++ 3399.smallest-substring-with-identical-characters-ii.py
@@ -5,46 +5,59 @@
 #
 
 # @lc code=start
+from collections import deque
+
 class Solution:
     def minLength(self, s: str, numOps: int) -> int:
         n = len(s)
-        
-        def check(k):
-            if k == 1:
-                # Check alternating patterns 0101... and 1010...
-                ops0 = 0
-                ops1 = 0
-                for i in range(n):
-                    expected0 = '0' if i % 2 == 0 else '1'
-                    if s[i] != expected0:
-                        ops0 += 1
-                    else:
-                        ops1 += 1
-                return min(ops0, ops1) <= numOps
-            
-            # For k >= 2, we break identical segments
-            total_ops = 0
-            count = 1
-            for i in range(1, n):
-                if s[i] == s[i-1]:
-                    count += 1
-                else:
-                    total_ops += count // (k + 1)
-                    count = 1
-            total_ops += count // (k + 1)
-            return total_ops <= numOps
+        pref1 = [0] * (n + 1)
+        for i, ch in enumerate(s, 1):
+            pref1[i] = pref1[i - 1] + (ch == '1')
 
-        low = 1
-        high = n
-        ans = n
-        
-        while low <= high:
-            mid = (low + high) // 2
-            if check(mid):
-                ans = mid
-                high = mid - 1
+        INF = 10**18
+
+        def feasible(L: int) -> bool:
+            dp0 = [INF] * (n + 1)  # end with 0-block
+            dp1 = [INF] * (n + 1)  # end with 1-block
+            dp0[0] = dp1[0] = 0
+
+            # dq1 keeps candidates for min(dp1[j] - pref1[j]) in current window
+            # dq0 keeps candidates for min(dp0[j] - pref0[j]) in current window
+            dq1 = deque([(0, 0)])  # (idx, value)
+            dq0 = deque([(0, 0)])
+
+            for i in range(1, n + 1):
+                left = i - L
+
+                while dq1 and dq1[0][0] < left:
+                    dq1.popleft()
+                while dq0 and dq0[0][0] < left:
+                    dq0.popleft()
+
+                ones_i = pref1[i]
+                zeros_i = i - ones_i
+
+                dp0[i] = ones_i + dq1[0][1]
+                dp1[i] = zeros_i + dq0[0][1]
+
+                v1 = dp1[i] - ones_i
+                while dq1 and dq1[-1][1] >= v1:
+                    dq1.pop()
+                dq1.append((i, v1))
+
+                v0 = dp0[i] - zeros_i
+                while dq0 and dq0[-1][1] >= v0:
+                    dq0.pop()
+                dq0.append((i, v0))
+
+            return min(dp0[n], dp1[n]) <= numOps
+
+        lo, hi = 1, n
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if feasible(mid):
+                hi = mid
             else:
-                low = mid + 1
-        
-        return ans
+                lo = mid + 1
+        return lo
 # @lc code=end
