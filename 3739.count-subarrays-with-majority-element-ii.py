1f4c9e73d96bca654a6c2feb658820e076dfc7c8
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 03:11:39 +0800

    Add solution: 3739.count-subarrays-with-majority-element-ii.py

--- 3739.count-subarrays-with-majority-element-ii.py
+++ 3739.count-subarrays-with-majority-element-ii.py
@@ -5,37 +5,46 @@
 #
 
 # @lc code=start
+from typing import List
+
+class Fenwick:
+    def __init__(self, n: int):
+        self.n = n
+        self.bit = [0] * (n + 1)
+
+    def add(self, i: int, delta: int) -> None:
+        while i <= self.n:
+            self.bit[i] += delta
+            i += i & -i
+
+    def sum(self, i: int) -> int:
+        res = 0
+        while i > 0:
+            res += self.bit[i]
+            i -= i & -i
+        return res
+
 class Solution:
     def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        # The prefix sums range from -n to n. 
-        # We use an offset of n to map this range to 0 to 2n.
-        offset = n
-        freq = [0] * (2 * n + 1)
-        
+
+        # Build prefix sums of mapped array: +1 if == target else -1
+        pref = [0] * (n + 1)
+        for i, x in enumerate(nums, 1):
+            pref[i] = pref[i - 1] + (1 if x == target else -1)
+
+        # Coordinate compression of prefix sums
+        vals = sorted(set(pref))
+        rank = {v: i + 1 for i, v in enumerate(vals)}  # 1-indexed for Fenwick
+
+        bit = Fenwick(len(vals))
         ans = 0
-        cur_P = 0 # Represents the current prefix sum P_k
-        freq[cur_P + offset] = 1 # Initial prefix sum P_0 = 0
-        smaller_counts = 0 # Number of previous prefix sums P_i < cur_P
-        
-        for x in nums:
-            if x == target:
-                # Prefix sum increases: P_{k+1} = P_k + 1
-                # The number of i <= k such that P_i < P_{k+1} is:
-                # count(P_i < P_k) + count(P_i == P_k)
-                smaller_counts += freq[cur_P + offset]
-                cur_P += 1
-            else:
-                # Prefix sum decreases: P_{k+1} = P_k - 1
-                # The number of i <= k such that P_i < P_{k+1} is:
-                # count(P_i < P_k) - count(P_i == P_k - 1)
-                cur_P -= 1
-                smaller_counts -= freq[cur_P + offset]
-            
-            # Add the number of valid starting indices i for the current end index k
-            ans += smaller_counts
-            # Update the frequency of the newly reached prefix sum
-            freq[cur_P + offset] += 1
-            
+
+        for s in pref:
+            r = rank[s]
+            # count of previous prefix sums strictly smaller than s
+            ans += bit.sum(r - 1)
+            bit.add(r, 1)
+
         return ans
 # @lc code=end
