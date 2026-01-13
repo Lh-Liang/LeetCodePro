7eb81c9d0f1210cefd20045b7aa796ef67ffaca2
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 02:53:07 +0800

    Add solution: 3704.count-no-zero-pairs-that-sum-to-n.py

--- 3704.count-no-zero-pairs-that-sum-to-n.py
+++ 3704.count-no-zero-pairs-that-sum-to-n.py
@@ -5,51 +5,45 @@
 #
 
 # @lc code=start
+from functools import lru_cache
+
 class Solution:
     def countNoZeroPairs(self, n: int) -> int:
-        s_n = str(n)[::-1]
-        L = len(s_n)
-        
-        # Precompute the number of pairs (x, y) with x, y in {1..9} such that x + y = S
-        # S ranges from 2 (1+1) to 18 (9+9)
-        pair_counts = [0] * 20
-        for x in range(1, 10):
-            for y in range(1, 10):
-                pair_counts[x + y] += 1
-        
-        memo = {}
+        digits = list(map(int, str(n)))[::-1]  # least-significant first
+        L = len(digits)
 
-        def solve(idx, carry):
-            if idx == L:
+        @lru_cache(None)
+        def dp(pos: int, carry: int, finA: int, finB: int) -> int:
+            if pos == L:
                 return 1 if carry == 0 else 0
-            
-            state = (idx, carry)
-            if state in memo:
-                return memo[state]
-            
-            target_digit = int(s_n[idx])
-            res = 0
-            
-            # Possible values for current_sum = x + y + prev_carry
-            # current_sum % 10 must be target_digit
-            # current_sum = 10 * new_carry + target_digit
-            # Since x, y in [1, 9], 2 <= x+y <= 18
-            # So 2 + carry <= current_sum <= 18 + carry
-            
-            # Case 1: new_carry = 0
-            s0 = target_digit
-            val0 = s0 - carry
-            if 2 <= val0 <= 18:
-                res += pair_counts[val0] * solve(idx + 1, 0)
-                
-            # Case 2: new_carry = 1
-            s1 = 10 + target_digit
-            val1 = s1 - carry
-            if 2 <= val1 <= 18:
-                res += pair_counts[val1] * solve(idx + 1, 1)
-                
-            memo[state] = res
-            return res
 
-        return solve(0, 0)
+            target = digits[pos]
+            total = 0
+
+            # Build allowed digits for a at this position
+            if finA:
+                a_choices = [(0, 1)]  # (digit, next_finA)
+            else:
+                a_choices = [(d, 0) for d in range(1, 10)]
+                if pos > 0:
+                    a_choices.append((0, 1))  # finish a here
+
+            # Build allowed digits for b at this position
+            if finB:
+                b_choices = [(0, 1)]
+            else:
+                b_choices = [(d, 0) for d in range(1, 10)]
+                if pos > 0:
+                    b_choices.append((0, 1))
+
+            for ad, nfinA in a_choices:
+                for bd, nfinB in b_choices:
+                    s = ad + bd + carry
+                    if s % 10 == target:
+                        total += dp(pos + 1, s // 10, nfinA, nfinB)
+
+            return total
+
+        # LSD cannot be 0 for positive no-zero integers, so both start as not finished.
+        return dp(0, 0, 0, 0)
 # @lc code=end
