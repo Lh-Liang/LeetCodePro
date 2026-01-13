3ec3d9199b0ec1f5e9922b6f3071a28f295b2bfa
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 07:42:07 +0800

    Add solution: 3503.longest-palindrome-after-substring-concatenation-i.py

--- 3503.longest-palindrome-after-substring-concatenation-i.py
+++ 3503.longest-palindrome-after-substring-concatenation-i.py
@@ -7,43 +7,25 @@
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str, t: str) -> int:
-        def all_substrings(x: str):
-            n = len(x)
-            subs = [""]
-            for i in range(n):
-                for j in range(i + 1, n + 1):
-                    subs.append(x[i:j])
-            return subs
-
-        def is_pal(x: str) -> bool:
-            i, j = 0, len(x) - 1
-            while i < j:
-                if x[i] != x[j]:
-                    return False
-                i += 1
-                j -= 1
-            return True
-
-        subs_s = all_substrings(s)
-        subs_t = all_substrings(t)
-
-        subs_s.sort(key=len, reverse=True)
-        subs_t.sort(key=len, reverse=True)
-
-        max_t_len = len(subs_t[0])
-        best = 0
-
-        for a in subs_s:
-            la = len(a)
-            if la + max_t_len <= best:
-                break
-            for b in subs_t:
-                total = la + len(b)
-                if total <= best:
-                    break
-                if is_pal(a + b):
-                    best = total
-                    break
-
-        return best
+        def is_palindrome(string):
+            return string == string[::-1]
+        
+        max_len = 0
+        
+        # Try all substrings of s
+        for i in range(len(s) + 1):
+            for j in range(i, len(s) + 1):
+                substr_s = s[i:j]
+                
+                # Try all substrings of t
+                for k in range(len(t) + 1):
+                    for l in range(k, len(t) + 1):
+                        substr_t = t[k:l]
+                        
+                        # Concatenate and check
+                        concat = substr_s + substr_t
+                        if is_palindrome(concat):
+                            max_len = max(max_len, len(concat))
+        
+        return max_len
 # @lc code=end
