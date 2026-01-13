900a657450a7b74fa47dcd140b76ddf2b80b39ae
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 01:36:50 +0800

    Add solution: 3525.find-x-value-of-array-ii.py

--- 3525.find-x-value-of-array-ii.py
+++ 3525.find-x-value-of-array-ii.py
@@ -5,83 +5,111 @@
 #
 
 # @lc code=start
+from typing import List
+from array import array
+
 class Solution:
     def resultArray(self, nums: List[int], k: int, queries: List[List[int]]) -> List[int]:
         n = len(nums)
-        size = 1 << (n - 1).bit_length()
-        tree_prod = [1 % k] * (2 * size)
-        tree_cnt = [0] * (2 * size * k)
+        if k <= 0:
+            return [0] * len(queries)
 
-        def update(i, val):
-            idx = i + size
-            val %= k
-            tree_prod[idx] = val
-            base = idx * k
-            for j in range(k):
-                tree_cnt[base + j] = 0
-            tree_cnt[base + val] = 1
-            while idx > 1:
-                idx >>= 1
-                left = 2 * idx
-                right = 2 * idx + 1
-                l_p = tree_prod[left]
-                r_p = tree_prod[right]
-                tree_prod[idx] = (l_p * r_p) % k
-                
-                curr_base = idx * k
-                l_base = left * k
-                r_base = right * k
-                for j in range(k):
-                    tree_cnt[curr_base + j] = tree_cnt[l_base + j]
-                for j in range(k):
-                    if tree_cnt[r_base + j]:
-                        tree_cnt[curr_base + (l_p * j) % k] += tree_cnt[r_base + j]
+        # Build iterative segment tree
+        size = 1
+        while size < n:
+            size <<= 1
 
+        prods = [0] * (2 * size)  # product modulo k
+        pref = array('I', [0]) * (2 * size * k)  # flat: pref[node*k + r]
+
+        def _set_leaf(pos: int, val: int) -> None:
+            i = pos + size
+            p = val % k
+            prods[i] = p
+            base = i * k
+            for r in range(k):
+                pref[base + r] = 0
+            pref[base + p] = 1
+
+        def _pull(i: int) -> None:
+            # merge children (2*i) then (2*i+1) into i
+            left = 2 * i
+            right = left + 1
+            prodA = prods[left]
+            prodB = prods[right]
+            prods[i] = (prodA * prodB) % k
+
+            baseA = left * k
+            baseB = right * k
+            tmp = [0] * k
+            # prefixes entirely in A
+            for r in range(k):
+                tmp[r] = pref[baseA + r]
+            # prefixes that include all A and some prefix of B
+            for r in range(k):
+                cnt = pref[baseB + r]
+                if cnt:
+                    tmp[(prodA * r) % k] += cnt
+
+            baseC = i * k
+            for r in range(k):
+                pref[baseC + r] = tmp[r]
+
+        # initialize leaves
         for i, v in enumerate(nums):
-            update(i, v)
+            _set_leaf(i, v)
+        # build
+        for i in range(size - 1, 0, -1):
+            _pull(i)
 
-        results = []
-        for idx, val, start, x_val in queries:
-            update(idx, val)
-            
-            # Query range [start, n-1]
-            l, r = start + size, n + size
-            res_l_p, res_l_c = 1 % k, [0] * k
-            res_r_p, res_r_c = 1 % k, [0] * k
-            
+        def update(pos: int, val: int) -> None:
+            _set_leaf(pos, val)
+            i = (pos + size) // 2
+            while i:
+                _pull(i)
+                i //= 2
+
+        def merge_node(A_prod: int, A_pref: List[int], B_prod: int, B_pref: List[int]):
+            prodC = (A_prod * B_prod) % k
+            tmp = A_pref[:]  # k is tiny
+            for r in range(k):
+                cnt = B_pref[r]
+                if cnt:
+                    tmp[(A_prod * r) % k] += cnt
+            return prodC, tmp
+
+        def get_node(i: int):
+            base = i * k
+            return prods[i], [pref[base + r] for r in range(k)]
+
+        def query(l: int, r: int):
+            # returns node info for nums[l:r] (contiguous)
+            # identity: empty segment
+            id_prod = 1 % k
+            left_prod, left_pref = id_prod, [0] * k
+            right_prod, right_pref = id_prod, [0] * k
+
+            l += size
+            r += size
             while l < r:
                 if l & 1:
-                    # Merge res_l and tree[l]
-                    new_l_c = list(res_l_c)
-                    l_base = l * k
-                    for j in range(k):
-                        if tree_cnt[l_base + j]:
-                            new_l_c[(res_l_p * j) % k] += tree_cnt[l_base + j]
-                    res_l_p = (res_l_p * tree_prod[l]) % k
-                    res_l_c = new_l_c
+                    p, pc = get_node(l)
+                    left_prod, left_pref = merge_node(left_prod, left_pref, p, pc)
                     l += 1
                 if r & 1:
                     r -= 1
-                    # Merge tree[r] and res_r
-                    new_r_c = [0] * k
-                    r_base = r * k
-                    for j in range(k):
-                        new_r_c[j] = tree_cnt[r_base + j]
-                    r_p = tree_prod[r]
-                    for j in range(k):
-                        if res_r_c[j]:
-                            new_r_c[(r_p * j) % k] += res_r_c[j]
-                    res_r_p = (r_p * res_r_p) % k
-                    res_r_c = new_r_c
-                l >>= 1
-                r >>= 1
-            
-            # Final merge of res_l and res_r
-            final_ans = res_l_c[x_val]
-            for j in range(k):
-                if (res_l_p * j) % k == x_val:
-                    final_ans += res_r_c[j]
-            results.append(final_ans)
-            
-        return results
+                    p, pc = get_node(r)
+                    right_prod, right_pref = merge_node(p, pc, right_prod, right_pref)
+                l //= 2
+                r //= 2
+
+            return merge_node(left_prod, left_pref, right_prod, right_pref)
+
+        ans = []
+        for idx, val, start, x in queries:
+            update(idx, val)
+            _, pc = query(start, n)
+            ans.append(pc[x])
+        return ans
+
 # @lc code=end
