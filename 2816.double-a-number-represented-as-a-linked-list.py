15dae344494d5070e53c0e7cb44cc4214190a612
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-13 23:43:12 +0800

    Add solution: 2816.double-a-number-represented-as-a-linked-list.py

--- 2816.double-a-number-represented-as-a-linked-list.py
+++ 2816.double-a-number-represented-as-a-linked-list.py
@@ -3,7 +3,6 @@
 #
 # [2816] Double a Number Represented as a Linked List
 #
-
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
@@ -12,21 +11,31 @@
 #         self.next = next
 class Solution:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # If the head value is 5 or more, doubling it will result in a carry
-        # that requires an additional digit at the front.
-        if head.val >= 5:
-            head = ListNode(0, head)
-        
-        curr = head
-        while curr:
-            # The new value of the current node is (original value * 2) % 10
-            # We also add 1 if the NEXT node's original value is >= 5 (the carry).
-            new_val = (curr.val * 2) % 10
-            if curr.next and curr.next.val >= 5:
-                new_val += 1
-            
-            curr.val = new_val
-            curr = curr.next
-        
+        def reverse(node: Optional[ListNode]) -> Optional[ListNode]:
+            prev = None
+            cur = node
+            while cur:
+                nxt = cur.next
+                cur.next = prev
+                prev = cur
+                cur = nxt
+            return prev
+
+        head = reverse(head)
+
+        carry = 0
+        cur = head
+        prev = None
+        while cur:
+            total = cur.val * 2 + carry
+            cur.val = total % 10
+            carry = total // 10
+            prev = cur
+            cur = cur.next
+
+        if carry:
+            prev.next = ListNode(carry)
+
+        head = reverse(head)
         return head
 # @lc code=end
