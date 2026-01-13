022b0b90027d4a05c4d98688427573e736615a1b
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 01:55:37 +0800

    Add solution: 3562.maximum-profit-from-trading-stocks-with-discounts.py

--- 3562.maximum-profit-from-trading-stocks-with-discounts.py
+++ 3562.maximum-profit-from-trading-stocks-with-discounts.py
@@ -4,57 +4,67 @@
 # [3562] Maximum Profit from Trading Stocks with Discounts
 #
 
-# @lc code=start
 from typing import List
-from collections import defaultdict
+import sys
 
+# @lc code=start
 class Solution:
     def maxProfit(self, n: int, present: List[int], future: List[int], hierarchy: List[List[int]], budget: int) -> int:
-        adj = defaultdict(list)
+        sys.setrecursionlimit(10**7)
+        B = budget
+        INF = 10**18
+
+        children = [[] for _ in range(n)]
         for u, v in hierarchy:
-            adj[u].append(v)
-        
-        # Adjust arrays to 0-indexed
-        present = [0] + present
-        future = [0] + future
+            children[u - 1].append(v - 1)
 
-        memo = {}
-
-        def solve(u, b, boss_bought):
-            state = (u, b, boss_bought)
-            if state in memo:
-                return memo[state]
-            
-            # Option 1: u does not buy
-            # dp[w] stores max profit for children using budget w
-            dp_no_buy = [0] * (b + 1)
-            for v in adj[u]:
-                v_res = solve(v, b, False)
-                for w in range(b, -1, -1):
-                    for cost_v in range(w + 1):
-                        dp_no_buy[w] = max(dp_no_buy[w], dp_no_buy[w - cost_v] + v_res[cost_v])
-            
-            # Option 2: u buys
-            cost_u = (present[u] // 2) if boss_bought else present[u]
-            profit_u = future[u] - cost_u
-            
-            dp_buy = [-float('inf')] * (b + 1)
-            if cost_u <= b:
-                temp_dp = [0] * (b - cost_u + 1)
-                for v in adj[u]:
-                    v_res = solve(v, b - cost_u, True)
-                    for w in range(b - cost_u, -1, -1):
-                        for cost_v in range(w + 1):
-                            temp_dp[w] = max(temp_dp[w], temp_dp[w - cost_v] + v_res[cost_v])
-                
-                for w in range(cost_u, b + 1):
-                    dp_buy[w] = profit_u + temp_dp[w - cost_u]
-            
-            res = [max(dp_no_buy[w], dp_buy[w]) for w in range(b + 1)]
-            memo[state] = res
+        def merge(a: List[int], b: List[int]) -> List[int]:
+            """Knapsack convolution: both arrays are length B+1, -INF means unreachable."""
+            res = [-INF] * (B + 1)
+            for i in range(B + 1):
+                if a[i] <= -INF // 2:
+                    continue
+                maxj = B - i
+                for j in range(maxj + 1):
+                    if b[j] <= -INF // 2:
+                        continue
+                    val = a[i] + b[j]
+                    if val > res[i + j]:
+                        res[i + j] = val
             return res
 
-        final_dp = solve(1, budget, False)
-        return max(0, max(final_dp))
+        def dfs(u: int):
+            # compute dp for children first
+            child_dps = [dfs(v) for v in children[u]]  # list of (dp0, dp1)
 
+            # Option A: do not buy u (independent of parent state)
+            notBuy = [-INF] * (B + 1)
+            notBuy[0] = 0
+            for dp0c, dp1c in child_dps:
+                notBuy = merge(notBuy, dp0c)  # children see parent not bought
+
+            # Option B: buy u (depends on parent state)
+            cost0 = present[u]
+            cost1 = present[u] // 2
+            prof0 = future[u] - cost0
+            prof1 = future[u] - cost1
+
+            buy0 = [-INF] * (B + 1)
+            if cost0 <= B:
+                buy0[cost0] = prof0
+            for dp0c, dp1c in child_dps:
+                buy0 = merge(buy0, dp1c)  # children see parent bought
+
+            buy1 = [-INF] * (B + 1)
+            if cost1 <= B:
+                buy1[cost1] = prof1
+            for dp0c, dp1c in child_dps:
+                buy1 = merge(buy1, dp1c)
+
+            dp0 = [max(notBuy[c], buy0[c]) for c in range(B + 1)]
+            dp1 = [max(notBuy[c], buy1[c]) for c in range(B + 1)]
+            return dp0, dp1
+
+        dp0_root, _ = dfs(0)
+        return max(0, max(dp0_root))
 # @lc code=end
