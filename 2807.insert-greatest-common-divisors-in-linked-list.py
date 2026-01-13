dbee2d6827cd613cce5d456c49ebe112baee38e8
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-13 23:42:25 +0800

    Add solution: 2807.insert-greatest-common-divisors-in-linked-list.py

--- 2807.insert-greatest-common-divisors-in-linked-list.py
+++ 2807.insert-greatest-common-divisors-in-linked-list.py
@@ -5,36 +5,22 @@
 #
 
 # @lc code=start
+from math import gcd
+from typing import Optional
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import math
 
 class Solution:
-    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # If the list is empty or has only one node, no pairs exist.
-        if not head or not head.next:
-            return head
-        
-        curr = head
-        # Iterate through the list as long as there is an adjacent pair
-        while curr and curr.next:
-            # Calculate GCD of current node and the next node
-            v1 = curr.val
-            v2 = curr.next.val
-            gcd_val = math.gcd(v1, v2)
-            
-            # Create a new node with the GCD value
-            new_node = ListNode(gcd_val)
-            
-            # Insert new_node between curr and curr.next
-            new_node.next = curr.next
-            curr.next = new_node
-            
-            # Move curr to the original next node (now two steps away)
-            curr = new_node.next
-            
+    def insertGreatestCommonDivisors(self, head: Optional['ListNode']) -> Optional['ListNode']:
+        cur = head
+        while cur and cur.next:
+            nxt = cur.next
+            g = gcd(cur.val, nxt.val)
+            cur.next = ListNode(g, nxt)
+            cur = nxt
         return head
 # @lc code=end
