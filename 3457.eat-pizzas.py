a552b759b180f1d15b004898c2a74bb4ceb57be2
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 00:40:34 +0800

    Add solution: 3457.eat-pizzas.py

--- 3457.eat-pizzas.py
+++ 3457.eat-pizzas.py
@@ -5,32 +5,31 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def maxWeight(self, pizzas: List[int]) -> int:
-        # Sort pizzas in descending order to pick the heaviest ones first
-        pizzas.sort(reverse=True)
-        
+        pizzas.sort()
         n = len(pizzas)
-        k = n // 4
-        # Number of odd-numbered days (1-indexed: 1, 3, 5...)
-        k_odd = (k + 1) // 2
-        # Number of even-numbered days (1-indexed: 2, 4, 6...)
-        k_even = k // 2
-        
-        # On odd days, we gain the weight of the largest pizza (Z).
-        # We take the top k_odd pizzas for these days.
-        total_weight = sum(pizzas[:k_odd])
-        
-        # On even days, we gain the weight of the second largest pizza (Y).
-        # To maximize Y, we pair the largest remaining pizzas: (Z1, Y1), (Z2, Y2)...
-        # This means we skip one pizza (the Z) and take the next (the Y).
-        # We start picking from index k_odd.
-        # Indices to pick: k_odd + 1, k_odd + 3, ..., k_odd + 2*k_even - 1
-        start_idx = k_odd + 1
-        end_idx = k_odd + 2 * k_even
-        even_day_weights = pizzas[start_idx : end_idx : 2]
-        
-        total_weight += sum(even_day_weights)
-        
-        return total_weight
+        m = n // 4
+        odd_days = (m + 1) // 2
+        even_days = m // 2
+
+        low, high = 0, n - 1
+        ans = 0
+
+        # Allocate pizzas for odd days: gain the largest Z, waste 3 smallest as fillers
+        for _ in range(odd_days):
+            ans += pizzas[high]
+            high -= 1
+            low += 3
+
+        # Allocate pizzas for even days: take two largest (Y,Z), gain Y (= second largest),
+        # and waste 2 smallest as fillers
+        for _ in range(even_days):
+            ans += pizzas[high - 1]
+            high -= 2
+            low += 2
+
+        return ans
 # @lc code=end
