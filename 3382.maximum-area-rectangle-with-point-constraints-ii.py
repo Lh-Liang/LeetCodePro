0a05ed16c8d4fcee0e523b283f7b3e37ddcc5bf1
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 06:32:48 +0800

    Add solution: 3382.maximum-area-rectangle-with-point-constraints-ii.py

--- 3382.maximum-area-rectangle-with-point-constraints-ii.py
+++ 3382.maximum-area-rectangle-with-point-constraints-ii.py
@@ -5,103 +5,51 @@
 #
 
 # @lc code=start
-from typing import List
 from collections import defaultdict
-from array import array
-import sys
 
 class Solution:
     def maxRectangleArea(self, xCoord: List[int], yCoord: List[int]) -> int:
-        sys.setrecursionlimit(1_000_000)
-        n = len(xCoord)
-        pts = list(zip(xCoord, yCoord))
-
-        # Group y's by x (columns)
-        cols = defaultdict(list)
-        for x, y in pts:
-            cols[x].append(y)
-        for x in cols:
-            cols[x].sort()
-        xs_cols = sorted(cols.keys())
-
-        # Coordinate compress y
-        yvals = sorted(set(yCoord))
-        m = len(yvals)
-        y_to_idx = {y: i for i, y in enumerate(yvals)}
-
-        # Sort points by x for persistent structure
-        pts_sorted = sorted(((x, y_to_idx[y]) for x, y in pts), key=lambda t: t[0])
-
-        # Persistent segment tree arrays (node 0 is null)
-        L = array('I', [0])
-        R = array('I', [0])
-        S = array('I', [0])
-
-        def update(prev: int, l: int, r: int, pos: int) -> int:
-            new = len(S)
-            L.append(L[prev])
-            R.append(R[prev])
-            S.append(S[prev] + 1)
-            if l != r:
-                mid = (l + r) >> 1
-                if pos <= mid:
-                    left = update(L[prev], l, mid, pos)
-                    L[new] = left
-                else:
-                    right = update(R[prev], mid + 1, r, pos)
-                    R[new] = right
-            return new
-
-        def query(node: int, l: int, r: int, ql: int, qr: int) -> int:
-            if node == 0 or ql > r or qr < l:
-                return 0
-            if ql <= l and r <= qr:
-                return S[node]
-            mid = (l + r) >> 1
-            return query(L[node], l, mid, ql, qr) + query(R[node], mid + 1, r, ql, qr)
-
-        # Build roots and x boundary indices:
-        # x_to_start[x] = number of points with x < this x
-        # x_to_end[x]   = number of points with x <= this x
-        roots = [0] * (n + 1)
-        x_to_start = {}
-        x_to_end = {}
-
-        i = 0
-        while i < n:
-            x = pts_sorted[i][0]
-            start = i
-            while i < n and pts_sorted[i][0] == x:
-                roots[i + 1] = update(roots[i], 0, m - 1, pts_sorted[i][1])
-                i += 1
-            end = i
-            x_to_start[x] = start
-            x_to_end[x] = end
-
-        ans = -1
-        last_x_for_pair = {}
-
-        for x2 in xs_cols:
-            ys = cols[x2]
-            for j in range(len(ys) - 1):
-                y1 = ys[j]
-                y2 = ys[j + 1]
-                key = (y1 << 32) | y2
-
-                if key in last_x_for_pair:
-                    x1 = last_x_for_pair[key]
-                    area = (x2 - x1) * (y2 - y1)
-                    if area > ans:
-                        # Count points in closed rectangle [x1,x2] x [y1,y2]
-                        idxL = x_to_start[x1]   # x < x1
-                        idxR = x_to_end[x2]     # x <= x2
-                        yl = y_to_idx[y1]
-                        yr = y_to_idx[y2]
-                        cnt = query(roots[idxR], 0, m - 1, yl, yr) - query(roots[idxL], 0, m - 1, yl, yr)
-                        if cnt == 4:
-                            ans = area
-
-                last_x_for_pair[key] = x2
-
-        return ans
+        points = set(zip(xCoord, yCoord))
+        
+        # Group points by x-coordinate
+        by_x = defaultdict(set)
+        for x, y in points:
+            by_x[x].add(y)
+        
+        xs = sorted(by_x.keys())
+        max_area = -1
+        
+        # Try all pairs of x-coordinates
+        for i in range(len(xs)):
+            for j in range(i + 1, len(xs)):
+                x1, x2 = xs[i], xs[j]
+                
+                # Find common y-coordinates (potential rectangle corners)
+                common_ys = sorted(by_x[x1] & by_x[x2])
+                
+                # Try all pairs of common y-coordinates
+                for k in range(len(common_ys)):
+                    for l in range(k + 1, len(common_ys)):
+                        y1, y2 = common_ys[k], common_ys[l]
+                        
+                        # Check if any other point is inside or on the border
+                        valid = True
+                        for x in xs:
+                            if x < x1 or x > x2:
+                                continue
+                            for y in by_x[x]:
+                                if y < y1 or y > y2:
+                                    continue
+                                # This point is inside or on border
+                                if (x, y) not in [(x1, y1), (x1, y2), (x2, y1), (x2, y2)]:
+                                    valid = False
+                                    break
+                            if not valid:
+                                break
+                        
+                        if valid:
+                            area = (x2 - x1) * (y2 - y1)
+                            max_area = max(max_area, area)
+        
+        return max_area
 # @lc code=end
