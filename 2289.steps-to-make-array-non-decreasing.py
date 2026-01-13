231b9de8749c7569d8273ea344d323f34fb090ce
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 06:14:40 +0800

    Add solution: 2289.steps-to-make-array-non-decreasing.py

--- 2289.steps-to-make-array-non-decreasing.py
+++ 2289.steps-to-make-array-non-decreasing.py
@@ -4,30 +4,25 @@
 # [2289] Steps to Make Array Non-decreasing
 #
 
-from typing import List
-
 # @lc code=start
 class Solution:
     def totalSteps(self, nums: List[int]) -> int:
-        # stack holds (value, steps_to_be_removed)
-        stack = []
-        ans = 0
-
-        for x in nums:
-            cur = 0
-            # Pop all elements <= x; they cannot be the "killer" of x.
-            # While popping, collect the max removal time among them.
-            while stack and x >= stack[-1][0]:
-                cur = max(cur, stack[-1][1])
+        stack = []  # stack of (value, steps_to_remove)
+        max_steps = 0
+        
+        for num in nums:
+            steps = 0
+            # Pop all elements that are <= current element
+            while stack and stack[-1][0] <= num:
+                steps = max(steps, stack[-1][1])
                 stack.pop()
-
-            if not stack:
-                cur = 0
-            else:
-                cur += 1
-
-            ans = max(ans, cur)
-            stack.append((x, cur))
-
-        return ans
+            
+            # If stack is not empty, current element will be removed
+            if stack:
+                steps += 1
+            
+            stack.append((num, steps))
+            max_steps = max(max_steps, steps)
+        
+        return max_steps
 # @lc code=end
