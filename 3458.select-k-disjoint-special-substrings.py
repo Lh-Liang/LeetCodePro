e73f08ab011ae09e472f23dd7040a5656532ccc0
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 07:20:24 +0800

    Add solution: 3458.select-k-disjoint-special-substrings.py

--- 3458.select-k-disjoint-special-substrings.py
+++ 3458.select-k-disjoint-special-substrings.py
@@ -3,55 +3,57 @@
 #
 # [3458] Select K Disjoint Special Substrings
 #
-
 # @lc code=start
 class Solution:
     def maxSubstringLength(self, s: str, k: int) -> bool:
+        n = len(s)
+        
         if k == 0:
             return True
-
-        n = len(s)
-        first = [n] * 26
-        last = [-1] * 26
-
-        for i, ch in enumerate(s):
-            idx = ord(ch) - 97
-            if first[idx] == n:
-                first[idx] = i
-            last[idx] = i
-
-        intervals = []
-
-        # Build minimal valid intervals starting at first occurrence of each character
-        for c in range(26):
-            l = first[c]
-            if l == n:
-                continue
-            r = last[c]
-            i = l
-            valid = True
-            while i <= r:
-                x = ord(s[i]) - 97
-                if first[x] < l:
-                    valid = False
+        
+        # Precompute first and last occurrence of each character
+        first = {}
+        last = {}
+        for i, c in enumerate(s):
+            if c not in first:
+                first[c] = i
+            last[c] = i
+        
+        # Find all special substrings (as intervals [i, j])
+        special_substrings = []
+        
+        for i in range(n):
+            max_last = i
+            
+            for j in range(i, n):
+                c = s[j]
+                
+                # If this character appeared before position i, break
+                if first[c] < i:
                     break
-                r = max(r, last[x])
-                i += 1
-
-            if valid and not (l == 0 and r == n - 1):
-                intervals.append((l, r))
-
-        # Maximum number of non-overlapping intervals via greedy scheduling
-        intervals.sort(key=lambda p: (p[1], -p[0]))
-
-        count = 0
-        end = -1
-        for l, r in intervals:
-            if l > end:
+                
+                max_last = max(max_last, last[c])
+                
+                if max_last == j:
+                    # Exclude the entire string
+                    if not (i == 0 and j == n - 1):
+                        special_substrings.append((i, j))
+        
+        # Find maximum number of disjoint intervals using greedy algorithm
+        if not special_substrings:
+            return False
+        
+        # Sort by ending position
+        special_substrings.sort(key=lambda x: x[1])
+        
+        # Greedy selection
+        count = 1
+        last_end = special_substrings[0][1]
+        
+        for i in range(1, len(special_substrings)):
+            if special_substrings[i][0] > last_end:
                 count += 1
-                end = r
-                if count >= k:
-                    return True
-
-        return False
+                last_end = special_substrings[i][1]
+        
+        return count >= k
 # @lc code=end
