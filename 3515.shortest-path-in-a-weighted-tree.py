badfc00e55b54f68a39583d9e42f1a6462c71534
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 01:33:59 +0800

    Add solution: 3515.shortest-path-in-a-weighted-tree.py

--- 3515.shortest-path-in-a-weighted-tree.py
+++ 3515.shortest-path-in-a-weighted-tree.py
@@ -4,8 +4,28 @@
 # [3515] Shortest Path in a Weighted Tree
 #
 
-# @lc code=start
 from typing import List
+
+# @lc code=start
+class Fenwick:
+    def __init__(self, n: int):
+        self.n = n
+        self.bit = [0] * (n + 1)
+
+    def add(self, i: int, delta: int) -> None:
+        # i is 1-indexed
+        while i <= self.n:
+            self.bit[i] += delta
+            i += i & -i
+
+    def sum(self, i: int) -> int:
+        # prefix sum up to i (1-indexed)
+        s = 0
+        while i > 0:
+            s += self.bit[i]
+            i -= i & -i
+        return s
+
 
 class Solution:
     def treeQueries(self, n: int, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
@@ -13,75 +33,66 @@ class Solution:
         for u, v, w in edges:
             adj[u].append((v, w))
             adj[v].append((u, w))
-        
+
+        parent = [0] * (n + 1)
         tin = [0] * (n + 1)
         tout = [0] * (n + 1)
-        dist = [0] * (n + 1)
-        parent = [0] * (n + 1)
-        edge_weight_to_parent = [0] * (n + 1)
-        visited_count = [0] * (n + 1)
-        
-        # Iterative DFS to compute tin, tout, and initial distances
-        timer = 0
-        stack = [(1, 0, 0)] # node, parent, current_distance
+        dist0 = [0] * (n + 1)
+        wToPar = [0] * (n + 1)  # weight of edge (parent[node], node)
+
+        # Iterative DFS to compute parent, tin/tout, dist0
+        time = 0
+        stack = [(1, 0, 0, 0)]  # (node, par, w_par, state) state 0=enter,1=exit
+        parent[1] = 0
+        dist0[1] = 0
+        wToPar[1] = 0
+
         while stack:
-            u, p, d = stack[-1]
-            if tin[u] == 0:
-                timer += 1
-                tin[u] = timer
-                dist[u] = d
-            
-            u_adj = adj[u]
-            found_child = False
-            curr_idx = visited_count[u]
-            while curr_idx < len(u_adj):
-                v, w = u_adj[curr_idx]
-                curr_idx += 1
-                if v != p:
-                    visited_count[u] = curr_idx
-                    parent[v] = u
-                    edge_weight_to_parent[v] = w
-                    stack.append((v, u, d + w))
-                    found_child = True
-                    break
-            
-            if not found_child:
-                visited_count[u] = curr_idx
-                tout[u] = timer
-                stack.pop()
-        
-        # Binary Indexed Tree for range updates and point queries
-        bit = [0] * (n + 2)
-        bit_len = n + 2
+            u, p, wp, state = stack.pop()
+            if state == 0:
+                parent[u] = p
+                wToPar[u] = wp
+                tin[u] = time
+                time += 1
+                stack.append((u, p, wp, 1))
+                # push children
+                for v, w in adj[u][::-1]:
+                    if v == p:
+                        continue
+                    dist0[v] = dist0[u] + w
+                    stack.append((v, u, w, 0))
+            else:
+                tout[u] = time - 1
+
+        # BIT over Euler indices [0..n-1] using range-add / point-query
+        bit = Fenwick(n + 2)
+
+        def range_add(l0: int, r0: int, delta: int) -> None:
+            # l0,r0 are 0-indexed inclusive
+            l = l0 + 1
+            r_plus_1 = r0 + 2
+            bit.add(l, delta)
+            bit.add(r_plus_1, -delta)
+
+        def point(pos0: int) -> int:
+            return bit.sum(pos0 + 1)
+
         ans = []
-        
         for q in queries:
-            if q[0] == 1: # Update Query
-                u, v, w_new = q[1], q[2], q[3]
-                # Identify the child node in the edge (u, v)
-                child = v if parent[v] == u else u
-                delta = w_new - edge_weight_to_parent[child]
-                edge_weight_to_parent[child] = w_new
-                
-                # Range update [tin[child], tout[child]] in BIT
-                idx = tin[child]
-                while idx < bit_len:
-                    bit[idx] += delta
-                    idx += idx & (-idx)
-                
-                idx = tout[child] + 1
-                while idx < bit_len:
-                    bit[idx] -= delta
-                    idx += idx & (-idx)
-            else: # Distance Query
-                x = q[1]
-                # Point query at tin[x] (prefix sum of deltas)
-                res = 0
-                idx = tin[x]
-                while idx > 0:
-                    res += bit[idx]
-                    idx -= idx & (-idx)
-                ans.append(dist[x] + res)
-                
+            if q[0] == 1:
+                _, u, v, wnew = q
+                # determine child endpoint in rooted tree
+                if parent[u] == v:
+                    child = u
+                else:
+                    child = v
+                delta = wnew - wToPar[child]
+                if delta != 0:
+                    wToPar[child] = wnew
+                    range_add(tin[child], tout[child], delta)
+            else:
+                _, x = q
+                ans.append(dist0[x] + point(tin[x]))
+
         return ans
 # @lc code=end
