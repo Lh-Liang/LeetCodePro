7ad6622099e59c3df36a5a404473fd3dd587d6b1
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 09:20:40 +0800

    Add solution: 3748.count-stable-subarrays.py

--- 3748.count-stable-subarrays.py
+++ 3748.count-stable-subarrays.py
@@ -4,64 +4,32 @@
 # [3748] Count Stable Subarrays
 #
 
-from typing import List
-
 # @lc code=start
 class Solution:
     def countStableSubarrays(self, nums: List[int], queries: List[List[int]]) -> List[int]:
         n = len(nums)
-
-        def tri(x: int) -> int:
-            return x * (x + 1) // 2
-
-        # Build maximal non-decreasing runs
-        starts: List[int] = []
-        ends: List[int] = []
-        contrib: List[int] = []
-        runId = [-1] * n
-
-        s = 0
-        rid = 0
-        for i in range(n - 1):
-            if nums[i] <= nums[i + 1]:
-                continue
-            # close run [s, i]
-            starts.append(s)
-            ends.append(i)
-            L = i - s + 1
-            contrib.append(tri(L))
-            for k in range(s, i + 1):
-                runId[k] = rid
-            rid += 1
-            s = i + 1
-
-        # close last run [s, n-1]
-        starts.append(s)
-        ends.append(n - 1)
-        L = (n - 1) - s + 1
-        contrib.append(tri(L))
-        for k in range(s, n):
-            runId[k] = rid
-        rid += 1
-
-        m = rid  # number of runs
-
-        # Prefix sums of full run contributions
-        pref = [0] * (m + 1)
-        for i in range(m):
-            pref[i + 1] = pref[i] + contrib[i]
-
-        ans: List[int] = []
-        for l, r in queries:
-            i = runId[l]
-            j = runId[r]
-            if i == j:
-                ans.append(tri(r - l + 1))
+        # max_reach[i] = the maximum index j such that nums[i:j+1] is non-decreasing
+        max_reach = [0] * n
+        
+        # Process from right to left
+        for i in range(n - 1, -1, -1):
+            if i == n - 1:
+                max_reach[i] = i
+            elif nums[i + 1] < nums[i]:
+                max_reach[i] = i
             else:
-                left_len = ends[i] - l + 1
-                right_len = r - starts[j] + 1
-                mid = pref[j] - pref[i + 1]  # full runs between i and j
-                ans.append(tri(left_len) + tri(right_len) + mid)
-
-        return ans
+                max_reach[i] = max_reach[i + 1]
+        
+        result = []
+        for l, r in queries:
+            count = 0
+            for i in range(l, r + 1):
+                # The maximum ending position within [l, r]
+                end = min(max_reach[i], r)
+                # Number of stable subarrays starting at i
+                count += (end - i + 1)
+            
+            result.append(count)
+        
+        return result
 # @lc code=end
