db055396c72cc63e6d726308e73ff750bc3e5b93
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 10:29:17 +0800

    Add solution: 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py

--- 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
+++ 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
@@ -3,54 +3,39 @@
 #
 # [3373] Maximize the Number of Target Nodes After Connecting Trees II
 #
-
 # @lc code=start
 from collections import deque
-from typing import List
 
 class Solution:
     def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
-        # Build adjacency list
-        def build_graph(edges):
-            n = len(edges) + 1
+        def get_parity_info(edges, n):
             graph = [[] for _ in range(n)]
             for u, v in edges:
                 graph[u].append(v)
                 graph[v].append(u)
-            return graph, n
-        
-        # Color the tree using BFS (bipartite coloring)
-        def color_tree(graph, n):
-            colors = [-1] * n
-            colors[0] = 0
+            
+            parity = [-1] * n
+            parity[0] = 0
+            count = [0, 0]
             queue = deque([0])
+            
             while queue:
-                u = queue.popleft()
-                for v in graph[u]:
-                    if colors[v] == -1:
-                        colors[v] = 1 - colors[u]
-                        queue.append(v)
-            return colors
+                node = queue.popleft()
+                count[parity[node]] += 1
+                for neighbor in graph[node]:
+                    if parity[neighbor] == -1:
+                        parity[neighbor] = 1 - parity[node]
+                        queue.append(neighbor)
+            
+            return parity, count
         
-        # Tree 1
-        graph1, n = build_graph(edges1)
-        colors1 = color_tree(graph1, n)
-        count1 = [0, 0]
-        for c in colors1:
-            count1[c] += 1
+        n = len(edges1) + 1
+        m = len(edges2) + 1
         
-        # Tree 2
-        graph2, m = build_graph(edges2)
-        colors2 = color_tree(graph2, m)
-        count2 = [0, 0]
-        for c in colors2:
-            count2[c] += 1
+        parity1, count1 = get_parity_info(edges1, n)
+        _, count2 = get_parity_info(edges2, m)
         
-        # Calculate answer
-        max_count2 = max(count2[0], count2[1])
-        answer = []
-        for i in range(n):
-            answer.append(count1[colors1[i]] + max_count2)
+        max_from_tree2 = max(count2)
         
-        return answer
+        return [count1[parity1[i]] + max_from_tree2 for i in range(n)]
 # @lc code=end
