f43cc9e6612784d52a7dcd349e04bb624fe318e5
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 09:24:13 +0800

    Add solution: 3753.total-waviness-of-numbers-in-range-ii.py

--- 3753.total-waviness-of-numbers-in-range-ii.py
+++ 3753.total-waviness-of-numbers-in-range-ii.py
@@ -3,52 +3,67 @@
 #
 # [3753] Total Waviness of Numbers in Range II
 #
-
 # @lc code=start
-from functools import lru_cache
-
 class Solution:
     def totalWaviness(self, num1: int, num2: int) -> int:
-        def total_upto(N: int) -> int:
-            if N <= 0:
+        def dp_solve(max_str):
+            target_len = len(max_str)
+            if target_len < 3:
                 return 0
-            digits = list(map(int, str(N)))
-            m = len(digits)
-
-            @lru_cache(None)
-            def dfs(pos: int, tight: int, started: int, last2: int, last1: int):
-                # returns (count_numbers, sum_waviness) for all completions
-                if pos == m:
-                    return 1, 0
-
-                limit = digits[pos] if tight else 9
-                total_cnt = 0
-                total_sum = 0
-
-                for d in range(limit + 1):
-                    ntight = 1 if (tight and d == limit) else 0
-
-                    if not started:
-                        if d == 0:
-                            cnt, sm = dfs(pos + 1, ntight, 0, -1, -1)
-                            total_cnt += cnt
-                            total_sum += sm
-                        else:
-                            cnt, sm = dfs(pos + 1, ntight, 1, -1, d)
-                            total_cnt += cnt
-                            total_sum += sm
-                    else:
-                        inc = 0
-                        if last2 != -1:
-                            if (last1 > last2 and last1 > d) or (last1 < last2 and last1 < d):
-                                inc = 1
-                        cnt, sm = dfs(pos + 1, ntight, 1, last1, d)
-                        total_cnt += cnt
-                        total_sum += sm + inc * cnt
-
-                return total_cnt, total_sum
-
-            return dfs(0, 1, 0, -1, -1)[1]
-
-        return total_upto(num2) - total_upto(num1 - 1)
+            
+            memo = {}
+            
+            def dp(pos, tight, prev, prev_prev):
+                if pos == target_len:
+                    return (1, 0)
+                
+                state = (pos, tight, prev, prev_prev)
+                if state in memo:
+                    return memo[state]
+                
+                limit = int(max_str[pos]) if tight else 9
+                start = 1 if pos == 0 else 0
+                
+                total_count = 0
+                total_waviness = 0
+                
+                for digit in range(start, limit + 1):
+                    new_tight = tight and (digit == limit)
+                    
+                    wave_contrib = 0
+                    if 2 <= pos <= target_len - 1 and prev != -1 and prev_prev != -1:
+                        if prev > prev_prev and prev > digit:
+                            wave_contrib = 1
+                        elif prev < prev_prev and prev < digit:
+                            wave_contrib = 1
+                    
+                    sub_count, sub_waviness = dp(pos + 1, new_tight, digit, prev)
+                    
+                    total_count += sub_count
+                    total_waviness += sub_waviness + wave_contrib * sub_count
+                
+                memo[state] = (total_count, total_waviness)
+                return (total_count, total_waviness)
+            
+            _, waviness = dp(0, True, -1, -1)
+            return waviness
+        
+        def solve(n):
+            if n < 100:
+                return 0
+            
+            total = 0
+            n_str = str(n)
+            n_len = len(n_str)
+            
+            for length in range(3, n_len):
+                max_val = 10 ** length - 1
+                total += dp_solve(str(max_val))
+            
+            if n_len >= 3:
+                total += dp_solve(n_str)
+            
+            return total
+        
+        return solve(num2) - solve(num1 - 1)
 # @lc code=end
