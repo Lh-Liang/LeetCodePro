d27f72fbc03512e75983a95e4fae9ae856f02ca5
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 08:00:15 +0800

    Add solution: 3534.path-existence-queries-in-a-graph-ii.py

--- 3534.path-existence-queries-in-a-graph-ii.py
+++ 3534.path-existence-queries-in-a-graph-ii.py
@@ -4,68 +4,51 @@
 # [3534] Path Existence Queries in a Graph II
 #
 
-from typing import List
-
 # @lc code=start
 class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[int]:
-        # Sort nodes by value
-        order = sorted([(nums[i], i) for i in range(n)])
-        val = [0] * n
-        pos = [0] * n  # original index -> sorted position
-        for p, (v, idx) in enumerate(order):
-            val[p] = v
-            pos[idx] = p
-
-        # Connected components in sorted order split by gaps > maxDiff
-        comp = [0] * n
-        cid = 0
-        for i in range(1, n):
-            if val[i] - val[i - 1] > maxDiff:
-                cid += 1
-            comp[i] = cid
-
-        # next[i] = farthest reachable index in 1 step (val[next[i]] - val[i] <= maxDiff)
-        nxt = [0] * n
-        r = 0
+        from collections import deque, defaultdict
+        
+        # Build adjacency list using sets to avoid duplicates
+        graph = defaultdict(set)
+        
+        # Sort nodes by their values to efficiently find neighbors
+        sorted_nodes = sorted(range(n), key=lambda x: nums[x])
+        
+        # For each node, connect to nodes within maxDiff
         for i in range(n):
-            if r < i:
-                r = i
-            while r + 1 < n and val[r + 1] - val[i] <= maxDiff:
-                r += 1
-            nxt[i] = r
-
-        # Binary lifting table
-        LOG = (n).bit_length()
-        up = [nxt[:]]
-        for k in range(1, LOG):
-            prev = up[k - 1]
-            cur = [0] * n
-            for i in range(n):
-                cur[i] = prev[prev[i]]
-            up.append(cur)
-
-        ans = []
+            node = sorted_nodes[i]
+            # Look forward in sorted order
+            j = i + 1
+            while j < n and nums[sorted_nodes[j]] - nums[node] <= maxDiff:
+                neighbor = sorted_nodes[j]
+                graph[node].add(neighbor)
+                graph[neighbor].add(node)
+                j += 1
+        
+        def bfs(start, end):
+            if start == end:
+                return 0
+            
+            visited = set([start])
+            queue = deque([(start, 0)])
+            
+            while queue:
+                curr, dist = queue.popleft()
+                
+                for neighbor in graph[curr]:
+                    if neighbor == end:
+                        return dist + 1
+                    
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        queue.append((neighbor, dist + 1))
+            
+            return -1
+        
+        result = []
         for u, v in queries:
-            if u == v:
-                ans.append(0)
-                continue
-            i, j = pos[u], pos[v]
-            if i > j:
-                i, j = j, i
-            if comp[i] != comp[j]:
-                ans.append(-1)
-                continue
-
-            steps = 0
-            cur = i
-            for k in range(LOG - 1, -1, -1):
-                jump_to = up[k][cur]
-                if jump_to < j:
-                    cur = jump_to
-                    steps += 1 << k
-            # one more jump reaches >= j
-            ans.append(steps + 1)
-
-        return ans
+            result.append(bfs(u, v))
+        
+        return result
 # @lc code=end
