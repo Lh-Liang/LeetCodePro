e0b67d6de342b63d4b22f80b46bae8b944c4a750
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 23:50:41 +0800

    Add solution: 3765.complete-prime-number.py

--- 3765.complete-prime-number.py
+++ 3765.complete-prime-number.py
@@ -3,36 +3,34 @@
 #
 # [3765] Complete Prime Number
 #
-
 # @lc code=start
 class Solution:
-    def completePrime(self, num: int) -> bool:
-        def is_prime(n):
-            if n < 2:
-                return False
-            if n == 2:
-                return True
-            if n % 2 == 0:
-                return False
-            for i in range(3, int(n**0.5) + 1, 2):
-                if n % i == 0:
-                    return False
+    def is_prime(self, n: int) -> bool:
+        if n < 2:
+            return False
+        if n == 2:
             return True
-        
+        if n % 2 == 0:
+            return False
+        i = 3
+        while i * i <= n:
+            if n % i == 0:
+                return False
+            i += 2
+        return True
+
+    def completePrime(self, num: int) -> bool:
         s = str(num)
         n = len(s)
-        
-        # Check all prefixes
-        for i in range(1, n + 1):
-            prefix = int(s[:i])
-            if not is_prime(prefix):
+        # Check all prefixes: first k digits for k from 1 to n
+        for k in range(1, n + 1):
+            prefix = int(s[:k])
+            if not self.is_prime(prefix):
                 return False
-        
-        # Check all suffixes
-        for i in range(n):
-            suffix = int(s[i:])
-            if not is_prime(suffix):
+        # Check all suffixes: last k digits for k from 1 to n
+        for k in range(1, n + 1):
+            suffix = int(s[n - k:])
+            if not self.is_prime(suffix):
                 return False
-        
         return True
 # @lc code=end
