74db9af507dea1573eeceeeb0fa0734c59c25f68
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 21:59:09 +0800

    Add solution: 1367.linked-list-in-binary-tree.py

--- 1367.linked-list-in-binary-tree.py
+++ 1367.linked-list-in-binary-tree.py
@@ -3,7 +3,6 @@
 #
 # [1367] Linked List in Binary Tree
 #
-
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
@@ -18,19 +17,45 @@
 #         self.right = right
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-        def dfs(head, root):
-            # If we've matched the entire linked list
-            if not head:
+        # Cache for memoization
+        cache = {}
+        
+        def match(list_node: Optional[ListNode], tree_node: Optional[TreeNode]) -> bool:
+            # If linked list is exhausted -> success
+            if not list_node:
                 return True
-            # If tree is exhausted but list isn't
-            if not root:
+            # If binary tree node doesn't exist -> failure
+            if not tree_node:
                 return False
-            # Values must match, then continue down either left or right child
-            return head.val == root.val and (dfs(head.next, root.left) or dfs(head.next, root.right))
+            
+            # Check cache using object ids
+            key = (id(list_node), id(tree_node))
+            if key in cache:
+                return cache[key]
+            
+            # Values don't match -> failure
+            if list_node.val != tree_node.val:
+                cache[key] = False
+                return False
+            
+            # Try continuing down left or right subtree
+            res = match(list_node.next, tree_node.left) or match(list_node.next, tree_node.right)
+            cache[key] = res
+            return res
         
-        if not root:
-            return False
+        # Iterative DFS traversal of binary tree
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if not node:
+                continue
+            # Check if path starts at current node
+            if match(head, node):
+                return True
+            # Push children to stack
+            stack.append(node.left)
+            stack.append(node.right)
         
-        # Try matching from current root, or try starting from left/right subtrees
-        return dfs(head, root) or self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
+        # No matching path found
+        return False
 # @lc code=end
