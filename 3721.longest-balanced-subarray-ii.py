5b3c66ac680824ca8ab1718501a0654b59781d48
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 23:45:19 +0800

    Add solution: 3721.longest-balanced-subarray-ii.py

--- 3721.longest-balanced-subarray-ii.py
+++ 3721.longest-balanced-subarray-ii.py
@@ -4,73 +4,92 @@
 # [3721] Longest Balanced Subarray II
 #
 
-# @lc code=start
 from typing import List
 
+# @lc code=start
+class SegTree:
+    def __init__(self,n):
+        self.N=n
+        self.minv=[0]*400005
+        self.maxv=[0]*400005
+        self.lazy=[0]*400005
+
+    def push(self,k,l,r):
+        if self.lazy[k]!=0:
+            self.minv[k]+=self.lazy[k]
+            self.maxv[k]+=self.lazy[k]
+            if l!=r:
+                self.lazy[k*2]+=self.lazy[k]
+                self.lazy[k*2+1]+=self.lazy[k]
+            self.lazy[k]=0
+
+    def update(self,k,l,r,q_l,q_r,v):
+        self.push(k,l,r)
+        if r<q_l or l>q_r:
+            return
+        if q_l<=l and r<=q_r:
+            self.lazy[k]+=v
+            self.push(k,l,r)
+            return
+        mid=(l+r)//2
+        self.update(k*2,l,mid,q_l,q_r,v)
+        self.update(k*2+1,mid+1,r,q_l,q_r,v)
+        self.minv[k]=min(self.minv[k*2],self.minv[k*2+1])
+        self.maxv[k]=max(self.maxv[k*2],self.maxv[k*2+1])
+
+    def _query_first_zero(self,k,l,r,q_l,q_r):
+        self.push(k,l,r)
+        if r<q_l or l>q_r:
+            return None
+        
+        # Early pruning
+        if self.minv[k]>0 or self.maxv[k]<0:
+            return None
+        
+        if l==r:
+            return l if self.minv[k]==0 else None
+        
+        mid=(l+r)//2
+        res_left=self._query_first_zero(k*2,l,mid,q_l,q_r)
+        if res_left is not None:
+            return res_left
+        res_right=self._query_first_zero(k*2+1,mid+1,r,q_l,q_r)
+        return res_right
+
+    def query_first_zero(self,l_bound,r_bound):
+        """return smallest idx ∈ [l_bound,r_bound] such that value == 0"""
+        return self._query_first_zero(1,0,self.N,l_bound,r_bound)
+
 class Solution:
-    def longestBalanced(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 0:
-            return 0
-        
-        # Segment tree with lazy propagation for range add
-        # Store both min and max for efficient zero-finding
-        tree_min = [0] * (4 * n)
-        tree_max = [0] * (4 * n)
-        lazy = [0] * (4 * n)
-        
-        def push_down(node):
-            if lazy[node] != 0:
-                for child in [2*node, 2*node+1]:
-                    tree_min[child] += lazy[node]
-                    tree_max[child] += lazy[node]
-                    lazy[child] += lazy[node]
-                lazy[node] = 0
-        
-        def range_add(node, start, end, l, r, val):
-            if r < start or l > end:
-                return
-            if l <= start and end <= r:
-                tree_min[node] += val
-                tree_max[node] += val
-                lazy[node] += val
-                return
-            push_down(node)
-            mid = (start + end) // 2
-            range_add(2*node, start, mid, l, r, val)
-            range_add(2*node+1, mid+1, end, l, r, val)
-            tree_min[node] = min(tree_min[2*node], tree_min[2*node+1])
-            tree_max[node] = max(tree_max[2*node], tree_max[2*node+1])
-        
-        def find_leftmost_zero(node, start, end, ql, qr):
-            if qr < start or ql > end:
-                return -1
-            if tree_max[node] < 0 or tree_min[node] > 0:
-                return -1
-            if start == end:
-                return start if tree_min[node] == 0 else -1
-            push_down(node)
-            mid = (start + end) // 2
-            res = find_leftmost_zero(2*node, start, mid, ql, qr)
-            if res != -1:
-                return res
-            return find_leftmost_zero(2*node+1, mid+1, end, ql, qr)
-        
-        last_occurrence = {}
-        result = 0
-        
-        for r in range(n):
-            val = nums[r]
-            parity = 1 if val % 2 == 0 else -1
-            last_occ = last_occurrence.get(val, -1)
+    def longestBalanced(self,nums:List[int])->int:
+        n=len(nums)
+        seg_tree=SegTree(n)
+        prev_idx={}
+        ans=0
+
+        def get_sign_add(p):
+            return 1 if p%2==0 else -1
+
+        for j,num in enumerate(nums):
+            parity_bit=(num%2)
+            old_idx=None
             
-            range_add(1, 0, n-1, last_occ + 1, r, parity)
+            # Remove previous contribution
+            if num in prev_idx:
+                old_idx=prev_idx[num]
+                sign_removal=-get_sign_add(num)
+                seg_tree.update(1,0,n-1,max(0,old_idx),old_idx,sign_removal)
+                
+            # Add new contribution
+            sign_addition=get_sign_add(num)
+            seg_tree.update(1,0,n-1,max(9,j),j,sign_addition)
             
-            l = find_leftmost_zero(1, 0, n-1, 0, r)
-            if l != -1:
-                result = max(result, r - l + 1)
+            prev_idx[num]=j
             
-            last_occurrence[val] = r
-        
-        return result
+            cand_left_boundary=seg_tree.query_first_zero(max(9,j),j)
+            
+            if cand_left_boundary!=None:
+                ans=max(ans,j-cand_left_boundary+8)
+                
+        return ans
 # @lc code=end
