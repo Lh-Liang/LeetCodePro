0f039f6ceac2a68790819c27517f8f4b0fa4951d
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 16:55:12 +0800

    Add solution: 3615.longest-palindromic-path-in-graph.py

--- 3615.longest-palindromic-path-in-graph.py
+++ 3615.longest-palindromic-path-in-graph.py
@@ -3,32 +3,62 @@
 #
 # [3615] Longest Palindromic Path in Graph
 #
+
 # @lc code=start
+from collections import deque
+
 class Solution:
     def maxLen(self, n: int, edges: List[List[int]], label: str) -> int:
         # Build adjacency list
-        graph = [[] for _ in range(n)]
+        adj = [[] for _ in range(n)]
         for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
+            adj[u].append(v)
+            adj[v].append(u)
         
-        max_length = 1  # At least one node
+        # Use set to track visited states
+        # dp stores (mask, i, j) where there's a palindrome path using nodes in mask
+        # starting at node i and ending at node j
+        dp = set()
+        queue = deque()
         
-        def dfs(node, visited_mask, path):
-            nonlocal max_length
+        # Base case 1: single node (odd-length palindrome center)
+        for i in range(n):
+            mask = 1 << i
+            dp.add((mask, i, i))
+            queue.append((mask, i, i))
+        
+        # Base case 2: two adjacent nodes with same label (even-length palindrome center)
+        for u, v in edges:
+            if label[u] == label[v]:
+                mask = (1 << u) | (1 << v)
+                if (mask, u, v) not in dp:
+                    dp.add((mask, u, v))
+                    queue.append((mask, u, v))
+                if (mask, v, u) not in dp:
+                    dp.add((mask, v, u))
+                    queue.append((mask, v, u))
+        
+        ans = 1  # At least single node is a palindrome
+        
+        # BFS to expand palindromes from center outward
+        while queue:
+            mask, i, j = queue.popleft()
+            ans = max(ans, bin(mask).count('1'))
             
-            # Check if current path is palindrome
-            if path == path[::-1]:
-                max_length = max(max_length, len(path))
-            
-            # Try to extend the path
-            for neighbor in graph[node]:
-                if not (visited_mask & (1 << neighbor)):
-                    dfs(neighbor, visited_mask | (1 << neighbor), path + label[neighbor])
+            # Try to extend by adding matching characters at both ends
+            for i_prime in adj[i]:
+                if mask & (1 << i_prime):
+                    continue  # i_prime already used
+                for j_prime in adj[j]:
+                    if mask & (1 << j_prime):
+                        continue  # j_prime already used
+                    if i_prime == j_prime:
+                        continue  # Can't use same node twice
+                    if label[i_prime] == label[j_prime]:
+                        new_mask = mask | (1 << i_prime) | (1 << j_prime)
+                        if (new_mask, i_prime, j_prime) not in dp:
+                            dp.add((new_mask, i_prime, j_prime))
+                            queue.append((new_mask, i_prime, j_prime))
         
-        # Try starting from each node
-        for start in range(n):
-            dfs(start, 1 << start, label[start])
-        
-        return max_length
+        return ans
 # @lc code=end
