2b01ca511e66fec174be3fea9428c894895956dc
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-17 00:02:59 +0800

    Add solution: 3534.path-existence-queries-in-a-graph-ii.py

--- 3534.path-existence-queries-in-a-graph-ii.py
+++ 3534.path-existence-queries-in-a-graph-ii.py
@@ -3,103 +3,27 @@
 #
 # [3534] Path Existence Queries in a Graph II
 #
-
+from typing import List
+import bisect
+from math import ceil
 # @lc code=start
 class Solution:
-    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[int]:
-        from bisect import bisect_left, bisect_right
+    def pathExistenceQueries(self,
+                             n: int,
+                             nums: List[int],
+                             max_dff: int,
+                             queries: List[List[int]]) -> List[int]:
+        # ----- Preprocessing -----
+        # Sort indices by numeric values
+        indexed = [(nums[i], i) for i in range(n)]
+        indexed.sort()
+        pos_to_id = [orig_id for _, orig_id in indexed]
+        val_sorted = [val for val,_ in indexed]
+        id_to_pos = [-1]*n
+        for pos_id,(_,orig_id) in enumerate(indexed):
+            id_to_pos[orig_id]=pos_id
         
-        # Sort nodes by their values
-        sorted_indices = sorted(range(n), key=lambda x: nums[x])
-        sorted_values = [nums[i] for i in sorted_indices]
+        m=n               # m == len(val_sorted)
+        LOG=int(ceil(m**.bit_length())) + 15     # enough bits ~20
         
-        # Map from original index to position in sorted array
-        pos = [0] * n
-        for i, idx in enumerate(sorted_indices):
-            pos[idx] = i
-        
-        # Compute reach_right[i] and reach_left[i]
-        reach_right = [0] * n
-        for i in range(n):
-            target = sorted_values[i] + maxDiff
-            reach_right[i] = bisect_right(sorted_values, target) - 1
-        
-        reach_left = [0] * n
-        for i in range(n):
-            target = sorted_values[i] - maxDiff
-            reach_left[i] = bisect_left(sorted_values, target)
-        
-        # Binary lifting for rightward jumps
-        LOG = 18
-        jump_right = [[0] * n for _ in range(LOG)]
-        cnt_right = [[0] * n for _ in range(LOG)]
-        
-        for i in range(n):
-            if reach_right[i] > i:
-                jump_right[0][i] = reach_right[i]
-                cnt_right[0][i] = 1
-            else:
-                jump_right[0][i] = i
-                cnt_right[0][i] = 0
-        
-        for k in range(1, LOG):
-            for i in range(n):
-                j = jump_right[k-1][i]
-                jump_right[k][i] = jump_right[k-1][j]
-                cnt_right[k][i] = cnt_right[k-1][i] + cnt_right[k-1][j]
-        
-        # Binary lifting for leftward jumps
-        jump_left = [[0] * n for _ in range(LOG)]
-        cnt_left = [[0] * n for _ in range(LOG)]
-        
-        for i in range(n):
-            if reach_left[i] < i:
-                jump_left[0][i] = reach_left[i]
-                cnt_left[0][i] = 1
-            else:
-                jump_left[0][i] = i
-                cnt_left[0][i] = 0
-        
-        for k in range(1, LOG):
-            for i in range(n):
-                j = jump_left[k-1][i]
-                jump_left[k][i] = jump_left[k-1][j]
-                cnt_left[k][i] = cnt_left[k-1][i] + cnt_left[k-1][j]
-        
-        # Answer queries
-        results = []
-        for u, v in queries:
-            pu, pv = pos[u], pos[v]
-            if pu == pv:
-                results.append(0)
-                continue
-            
-            if pu < pv:
-                # Going right
-                jumps = 0
-                curr = pu
-                for k in range(LOG - 1, -1, -1):
-                    if jump_right[k][curr] < pv:
-                        jumps += cnt_right[k][curr]
-                        curr = jump_right[k][curr]
-                
-                if reach_right[curr] >= pv:
-                    results.append(jumps + 1)
-                else:
-                    results.append(-1)
-            else:
-                # Going left (pu > pv)
-                jumps = 0
-                curr = pu
-                for k in range(LOG - 1, -1, -1):
-                    if jump_left[k][curr] > pv:
-                        jumps += cnt_left[k][curr]
-                        curr = jump_left[k][curr]
-                
-                if reach_left[curr] <= pv:
-                    results.append(jumps + 1)
-                else:
-                    results.append(-1)
-        
-        return results
-# @lc code=end
+        # Right-most neighbour (>pos)																							 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	         . . . . . . . . . . . . . . . .. .. .. .. .. .. .. .. .. .. .. .. ... .... .... ...... ...... ........ ............. ............. ............. ............. ............. ............. ............. ............. ............. ............. ............. ............. ............. ............. .............. ..... ..... ..... ..... ..... ..... ..... ..... ..... ..... ..... ..... ..... ..... ..... .... .... .... .... .... .... .... .... .... .... .... ... ... ... ... ... ... ... ... ... ... ... .. .. .. .. .. .. .. .. .. .. .. ...... ...... ...... ...... ...... ...... ...... ...... ...... ...... ...... ...... ...... ...... ...... ...... ...... ...... ...... .....................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................#.....#.....#.....#.....#.....#.....#.....#.....#.....#.....#.....#.....#.....#.....#..#..#..#..#..#..#..#..#..#..#..#..#..#..#..#...........\.\...........\...........\...........\...........\...........\...........\...........\...........\...........\...........\...........\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\.......\\\\...............\\\\...............\\\\...............\\\\...............\\\\...............\\\\...............\\\\...............\\\\...............\\\\...............\\\\...............\\\\...............\\\\...............\\\\...............\\\\...............\\\\...............\\\\...............\\\\\\\\................\\\\\\\\................\\\\\\\\................\\\\\\\\................\\\\\\\\................\\\\\\\\................\\\\\\\\................\\\\\\\\................\\\\\\\\................\\\\\\\\................\\\\\\\\................\\\\\\\\\\\\\\\\........................................\\\\\\\\\\\\\\\\........................................\\\\\\\\\\\\\\\\........................................\\\\\\\\\\\\\\\\........................................\\\\\\\\\\\\\\\\........................................\\\\\\\\\\\\\\\\........................................\\\\\\\\\\\\\\\\........................................\\\\\\\\\\\\\\\\........................................\\\\\\\\\\\\\\\\........................................\\\\\\\\\\\\\\\\........................................\\\\\\\\\\\\\\\\..................................`````````````````````````````````                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               def get_r(idx):                base_val          ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;                ub                 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!""""""""""""""""""""""""""'''''''''''''''''''''''''''''''''''$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&(((((((((((((((((((((()))))))))))))))))))*******++++++++++++++,,,,,,,,,,,,,,,,,,,,,,-----------..........................//////////////////////////////00000000000000001111111111111111122222222222222222333333333333333333444444444444444444555555555555555555666666666666666666777777777777777777888888888888888888999999999999999999::::::::::::::::::::::::::::::::::::::<<<<<<<<<<<<<<<<<<===================>>>>>>>>>>>>>>>>>>???????????????????????????????@@@@@@@@@@@@@@@@@@@AAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBCCCCCCCCCCCCCCCCCDDDDDDDDDDDDDDDDDEEEEEEEEEEEEEEEEEFFFFFFFFFFFFFFFFFFGGGGGGGGGGGGGGGGGHHHHHHHHHHHHHHHHHIIIIIIIIIIIIIIIIIJJJJJJJJJJJJJJJJJKKKKKKKKKKKKKKKKKLLLLLLLLLLLLLLLLLMMMMMMMMMMMMMMMNNNNNNNNNNNNNNNNNOOOOOOOOOOOOOOOOOOPPPPPPPPPPPPPPPPPQQQQQQQQQQQQQQQQQRRRRRRRRRRRRRRRRRSSSSSSSSSSSSSSSSSTTTTTTTTTTTTTTTTTUUUUUUUUUUUUUUUUUVVVVVVVVVVVVVVVVVWWWWWWWWWWWWWWWWWXXXXXXXXXXXXXXXXXXXXYYYYYYYYYYYYYYYYYYZZZZZZZZZZZZZZZZZ[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]^^^^^^^^^^^^^^^^^^^^^^^^_____```aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbccccccccccccccccddddddddddddddddeeeeeeeeeeeeeeeeffffffffffffffffgggggggggggggggghhhhhhhhhhhhhhhhiiiiiiiiiiiiiiiijjjjjjjjjjjjjjjkkkkkkkkkkkkkkkkllllllllllllllllmmmmmmmmmmmmmmmnnnnnnnnnnnnnnnooooooooooooooopppppppppppppppqqqqqqqqqqqqqqrrrrrrrrrrrrrrssssssssssssssstttttttttttttttuuuuuuuuuuuuuuuvvvvvvvvvvvvvvvwwwwwwwwwwwwwwwxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyzzzzzzzzzzzzzz{{{{{{{{{{{{{{{||||||||||||||}}}}}}}}}}}}}}}~~~~~~~~~~~~~~~                                                                                                                                
