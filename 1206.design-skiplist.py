8391b09bd358abb3c6ba4cb5945d1c589ff984e7
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-16 09:43:46 +0800

    Add solution: 1206.design-skiplist.py

--- 1206.design-skiplist.py
+++ 1206.design-skiplist.py
@@ -6,123 +6,85 @@
 import random
 
 # @lc code=start
+class Node:
+    def __init__(self, val, levels):
+        self.val = val
+        # Array to hold forward pointers for each level
+        self.forward = [None] * levels
+
 class Skiplist:
-    class Node:
-        def __init__(self, val: int, level: int):
-            """
-            Create a skip list node.
-            :param val: Value stored.
-            :param level: Highest index this node participates in.
-                          Number of forward pointers = level + 1.
-            """
-            self.val = val
-            self.next = [None] * (level + 1)
-
     def __init__(self):
-        """
-        Initialize an empty skiplist.
-        Maximum number of layers is set to 16,
-        enough for up to ~65k elements.
-        """
-        self._MAX_LEVEL = 16
-        # Head sentinel with maximum possible height
-        self.head = self.__class__.Node(-1, self._MAX_LEVEL - 1)
-        # Current highest used layer index
-        self.level = 0
-
-    def _random_level(self) -> int:
-        """
-        Generate a random height for a new node.
-        Each additional layer has probability 0.5.
-        Returns an integer between 0 and _MAX_LEVEL-1 inclusive.
-        """
-        lvl = 0
-        while random.random() < 0.5 and lvl < self._MAX_LEVEL - 1:
-            lvl += 1
-        return lvl
-
+        self.max_levels = 16
+        self.probability = 0.5
+        # Create head node with value -1 (smaller than any possible input)
+        self.head = Node(-1, self.max_levels)
+    
+    def _random_level(self):
+        """Generate random level for a new node"""
+        level = 1
+        while random.random() < self.probability and level < self.max_levels:
+            level += 1
+        return level
+    
     def search(self, target: int) -> bool:
-        """
-        Returns true if target exists in skiplist.
-        """
-        cur = self.head
-        # Traverse from highest layer down to layer 0
-        for i in range(self.level, -1, -1):
-            # Move forward on current layer while next node's value < target
-            while cur.next[i] and cur.next[i].val < target:
-                cur = cur.next[i]
-        # Now cur is largest node < target on layer 0
-        cur = cur.next[0]
-        return cur is not None and cur.val == target
-
+        current = self.head
+        
+        # Start from highest level and work down
+        for level in range(self.max_levels - 1, -1, -1):
+            # Move forward while next node is smaller than target
+            while current.forward[level] and current.forward[level].val < target:
+                current = current.forward[level]
+        
+        # Move to the next node at base level
+        current = current.forward[0]
+        
+        # Check if we found the target
+        return current and current.val == target
+    
     def add(self, num: int) -> None:
-        """
-        Insert a new element into skiplist.
-        Duplicates are allowed.
-        """
-        # Predecessor nodes on each layer where insertion will happen
-        prevs = [None] * self._MAX_LEVEL
-        cur = self.head
-        # Find predecessors on each layer
-        for i in range(self.level, -1, -1):
-            while cur.next[i] and cur.next[i].val < num:
-                cur = cur.next[i]
-            prevs[i] = cur
+        # Array to store predecessors at each level
+        update = [None] * self.max_levels
+        current = self.head
         
-        # Generate random height for new node
-        new_lvl = self._random_level()
+        # Find the position to insert at each level
+        for level in range(self.max_levels - 1, -1, -1):
+            while current.forward[level] and current.forward[level].val < num:
+                current = current.forward[level]
+            update[level] = current
         
-        # If new height exceeds current highest used layer,
-        # fill predecessors above old height with head
-        if new_lvl > self.level:
-            for i in range(self.level + 1, new_lvl + 1):
-                prevs[i] = self.head
-            self.level = new_lvl
-
-        # Create new node
-        newNode = self.__class__.Node(num, new_lvl)
+        # Create new node with random level
+        new_level = self._random_level()
+        new_node = Node(num, new_level)
         
-        # Insert into all layers up to its own height
-        for i in range(new_lvl + 1):
-            newNode.next[i] = prevs[i].next[i]
-            prevs[i].next[i] = newNode
-
+        # Insert the new node at each of its levels
+        for level in range(new_level):
+            new_node.forward[level] = update[level].forward[level]
+            update[level].forward[level] = new_node
+    
     def erase(self, num: int) -> bool:
-        """
-        Remove one occurrence of `num`.
-        Returns True if removed successfully,
-        False otherwise.
-        """
-        prevs = [None] * self._MAX_LEVEL
-        cur = self.head
-        # Find predecessors on each layer
-        for i in range(self.level, -1, -1):
-            while cur.next[i] and cur.next[i].val < num:
-                cur = cur.next[i]
-            prevs[i] = cur
-
-        # Candidate to delete – first occurrence >=num on layer0
-        cand = cur.next[0]
+        # Array to store predecessors at each level
+        update = [None] * self.max_levels
+        current = self.head
         
-        # If not found or not matching exactly
-        if cand is None or cand.val != num:
+        # Find the node to delete at each level
+        for level in range(self.max_levels - 1, -1, -1):
+            while current.forward[level] and current.forward[level].val < num:
+                current = current.forward[level]
+            update[level] = current
+        
+        # Move to the node to delete (if it exists)
+        current = current.forward[0]
+        
+        # If node doesn't exist or value doesn't match, return False
+        if not current or current.val != num:
             return False
-
-        cand_lvl = len(cand.next) - 1   # Highest index where cand appears
         
-        # Unlink cand from all layers where it exists
-        for i in range(cand_lvl + 1):
-            # Normally prevs[i].next[i] equals cand,
-            # but just skip mismatch safely.
-            if prevs[i].next[i] != cand:
-                continue
-            prevs[i].next[i] = cand.next[i]
-
-        # Possibly reduce highest used layer
-        while self.level > 0 and self.head.next[self.level] is None:
-            self.level -= 1
-
+        # Remove the node from all levels where it appears
+        for level in range(len(current.forward)):
+            update[level].forward[level] = current.forward[level]
+        
         return True
+
 # Your Skiplist object will be instantiated and called as such:
 # obj = Skiplist()
 # param_1 = obj.search(target)
