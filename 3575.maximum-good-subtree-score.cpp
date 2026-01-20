38f8d2ce313ee0d1ae0c1674dd913fc2b7096c93
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-20 22:16:36 +0800

    Batch add 1 solutions: 3575.maximum-good-su...

--- 3575.maximum-good-subtree-score.cpp
+++ 3575.maximum-good-subtree-score.cpp
@@ -8,85 +8,90 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
 class Solution {
-    long long G[500][1024];
-    vector<int> adj[500];
-    
-    int get_mask(int val) {
-        int mask = 0;
-        int temp = val;
-        while (temp > 0) {
-            int digit = temp % 10;
-            if ((mask >> digit) & 1) return -1;
-            mask |= (1 << digit);
-            temp /= 10;
+public:
+    int get_mask(int n) {
+        int m = 0;
+        while (n > 0) {
+            int d = n % 10;
+            if (m & (1 << d)) return -1;
+            m |= (1 << d);
+            n /= 10;
         }
-        return mask;
+        return m;
     }
 
-    void dfs(int u, const vector<int>& vals) {
-        vector<long long> dp(1024, -1);
-        dp[0] = 0;
+    int goodSubtreeSum(vector<int>& vals, vector<int>& par) {
+        int n = vals.size();
+        vector<vector<int>> children(n);
+        for (int i = 1; i < n; ++i) {
+            children[par[i]].push_back(i);
+        }
 
-        for (int v : adj[u]) {
-            dfs(v, vals);
-            vector<long long> next_dp = dp;
-            for (int m1 = 0; m1 < 1024; ++m1) {
-                if (dp[m1] == -1) continue;
-                int rem = 1023 ^ m1;
-                // Iterate over all non-empty subsets m2 of rem
-                for (int m2 = rem; m2 > 0; m2 = (m2 - 1) & rem) {
-                    if (G[v][m2] != -1) {
-                        if (dp[m1] + G[v][m2] > next_dp[m1 | m2]) {
-                            next_dp[m1 | m2] = dp[m1] + G[v][m2];
+        vector<int> masks(n);
+        for (int i = 0; i < n; ++i) {
+            masks[i] = get_mask(vals[i]);
+        }
+
+        // max_val_for_mask[u][m] is the max value for mask m in subtree of u
+        static int max_val_for_mask[500][1024];
+        memset(max_val_for_mask, 0, sizeof(max_val_for_mask));
+
+        // Post-order traversal to fill max_val_for_mask
+        // Using iterative DFS or recursion since n is small (500)
+        auto fill_max_vals = [&](auto self, int u) -> void {
+            if (masks[u] != -1) {
+                max_val_for_mask[u][masks[u]] = vals[u];
+            }
+            for (int v : children[u]) {
+                self(self, v);
+                for (int m = 0; m < 1024; ++m) {
+                    if (max_val_for_mask[v][m] > max_val_for_mask[u][m]) {
+                        max_val_for_mask[u][m] = max_val_for_mask[v][m];
+                    }
+                }
+            }
+        };
+        fill_max_vals(fill_max_vals, 0);
+
+        long long total_max_score_sum = 0;
+        long long MOD = 1e9 + 7;
+
+        for (int u = 0; u < n; ++u) {
+            vector<pair<int, int>> items;
+            for (int m = 1; m < 1024; ++m) {
+                if (max_val_for_mask[u][m] > 0) {
+                    items.push_back({m, max_val_for_mask[u][m]});
+                }
+            }
+
+            vector<long long> dp(1024, -1);
+            dp[0] = 0;
+            long long current_max = 0;
+
+            for (auto& item : items) {
+                int m_v = item.first;
+                int v_v = item.second;
+                for (int curr_m = 1023; curr_m >= 0; --curr_m) {
+                    if (dp[curr_m] != -1 && (curr_m & m_v) == 0) {
+                        int next_m = curr_m | m_v;
+                        if (dp[curr_m] + v_v > dp[next_m]) {
+                            dp[next_m] = dp[curr_m] + v_v;
+                            if (dp[next_m] > current_max) {
+                                current_max = dp[next_m];
+                            }
                         }
                     }
                 }
             }
-            dp = next_dp;
+            total_max_score_sum = (total_max_score_sum + current_max) % MOD;
         }
 
-        // Copy current dp to G[u]
-        for (int m = 0; m < 1024; ++m) G[u][m] = dp[m];
-
-        // Try including node u itself
-        int mu = get_mask(vals[u]);
-        if (mu != -1) {
-            for (int m = 0; m < 1024; ++m) {
-                if (dp[m] != -1 && (m & mu) == 0) {
-                    if (dp[m] + vals[u] > G[u][m | mu]) {
-                        G[u][m | mu] = dp[m] + vals[u];
-                    }
-                }
-            }
-        }
-    }
-
-public:
-    int goodSubtreeSum(vector<int>& vals, vector<int>& par) {
-        int n = vals.size();
-        for (int i = 0; i < n; ++i) adj[i].clear();
-        for (int i = 1; i < n; ++i) {
-            adj[par[i]].push_back(i);
-        }
-
-        dfs(0, vals);
-
-        long long totalSum = 0;
-        long long MOD = 1e9 + 7;
-
-        for (int i = 0; i < n; ++i) {
-            long long maxScoreU = 0;
-            for (int m = 0; m < 1024; ++m) {
-                if (G[i][m] > maxScoreU) maxScoreU = G[i][m];
-            }
-            totalSum = (totalSum + maxScoreU) % MOD;
-        }
-
-        return (int)totalSum;
+        return (int)total_max_score_sum;
     }
 };
 # @lc code=end
