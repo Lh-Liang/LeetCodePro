6886d27d967b976286ba866977922e0879b6166b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-21 13:16:17 +0800

    Add solution: 1670.design-front-middle-back-queue.py

--- 1670.design-front-middle-back-queue.py
+++ 1670.design-front-middle-back-queue.py
@@ -10,68 +10,56 @@ from collections import deque
 class FrontMiddleBackQueue:
 
     def __init__(self):
-        # Two deques to manage the front and back halves of the queue.
-        # Invariant: len(left) == len(right) or len(left) == len(right) + 1
         self.left = deque()
         self.right = deque()
 
     def _balance(self):
-        # Maintain the invariant len(left) >= len(right)
-        if len(self.left) < len(self.right):
-            self.left.append(self.right.popleft())
-        # Maintain the invariant len(left) <= len(right) + 1
-        if len(self.left) > len(self.right) + 1:
+        # Invariant: 0 <= len(right) - len(left) <= 1
+        if len(self.left) > len(self.right):
             self.right.appendleft(self.left.pop())
+        elif len(self.right) > len(self.left) + 1:
+            self.left.append(self.right.popleft())
 
     def pushFront(self, val: int) -> None:
         self.left.appendleft(val)
         self._balance()
 
     def pushMiddle(self, val: int) -> None:
-        # If left has more elements, move the last of left to right 
-        # to make space for the new middle in left.
-        if len(self.left) > len(self.right):
-            self.right.appendleft(self.left.pop())
-        self.left.append(val)
-        # No explicit balance needed here as we manually adjusted, 
-        # but _balance() wouldn't hurt.
+        if len(self.left) == len(self.right):
+            self.right.appendleft(val)
+        else:
+            self.left.append(val)
 
     def pushBack(self, val: int) -> None:
         self.right.append(val)
         self._balance()
 
     def popFront(self) -> int:
-        if not self.left and not self.right:
+        if not self.right:
             return -1
-        
-        # Usually pop from left, but if left is empty (implies right is empty due to invariant),
-        # handled by check above. 
-        val = self.left.popleft()
+        if not self.left:
+            res = self.right.popleft()
+        else:
+            res = self.left.popleft()
         self._balance()
-        return val
+        return res
 
     def popMiddle(self) -> int:
-        if not self.left and not self.right:
+        if not self.right:
             return -1
-        
-        # The middle element is always at the back of self.left 
-        # because len(left) >= len(right).
-        val = self.left.pop()
+        if len(self.left) == len(self.right):
+            res = self.left.pop()
+        else:
+            res = self.right.popleft()
         self._balance()
-        return val
+        return res
 
     def popBack(self) -> int:
-        if not self.left and not self.right:
+        if not self.right:
             return -1
-        
-        if self.right:
-            val = self.right.pop()
-        else:
-            # If right is empty, left must have exactly 1 element (invariant)
-            val = self.left.pop()
-        
+        res = self.right.pop()
         self._balance()
-        return val
+        return res
 
 # Your FrontMiddleBackQueue object will be instantiated and called as such:
 # obj = FrontMiddleBackQueue()
