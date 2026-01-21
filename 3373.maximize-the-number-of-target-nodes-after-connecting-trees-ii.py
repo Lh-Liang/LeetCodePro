a18dd586a5356fb55ef1a149f1c182bb0b93a473
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-21 13:22:01 +0800

    Add solution: 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py

--- 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
+++ 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
@@ -5,54 +5,50 @@
 #
 
 # @lc code=start
+from collections import deque
+from typing import List
+
 class Solution:
     def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
-        n = len(edges1) + 1
-        m = len(edges2) + 1
-        
-        adj1 = [[] for _ in range(n)]
-        for u, v in edges1:
-            adj1[u].append(v)
-            adj1[v].append(u)
+        def get_colors_and_counts(num_nodes, edges):
+            adj = [[] for _ in range(num_nodes)]
+            for u, v in edges:
+                adj[u].append(v)
+                adj[v].append(u)
             
-        adj2 = [[] for _ in range(m)]
-        for u, v in edges2:
-            adj2[u].append(v)
-            adj2[v].append(u)
-            
-        # Helper to get bipartition counts and node colors
-        def get_colors_and_counts(adj, num_nodes):
             colors = [-1] * num_nodes
             counts = [0, 0]
+            
+            # Since it's a tree, it's connected and bipartite.
+            # We can start BFS from any node, say node 0.
+            queue = deque([0])
             colors[0] = 0
             counts[0] = 1
-            stack = [0]
             
-            while stack:
-                u = stack.pop()
-                current_color = colors[u]
-                next_color = 1 - current_color
-                
+            while queue:
+                u = queue.popleft()
                 for v in adj[u]:
                     if colors[v] == -1:
-                        colors[v] = next_color
-                        counts[next_color] += 1
-                        stack.append(v)
+                        colors[v] = 1 - colors[u]
+                        counts[colors[v]] += 1
+                        queue.append(v)
             return colors, counts
 
-        colors1, counts1 = get_colors_and_counts(adj1, n)
-        _, counts2 = get_colors_and_counts(adj2, m)
+        n = len(edges1) + 1
+        m = len(edges2) + 1
+
+        colors1, counts1 = get_colors_and_counts(n, edges1)
+        colors2, counts2 = get_colors_and_counts(m, edges2)
         
-        max_targets_from_tree2 = max(counts2[0], counts2[1])
+        # For any node i in Tree 1, we can always connect Tree 1 to Tree 2 
+        # such that we pick the color partition in Tree 2 with the maximum number of nodes.
+        max_tree2_targets = max(counts2)
         
         ans = []
         for i in range(n):
-            # Targets in Tree 1 are nodes with the same color as i
-            targets_in_tree1 = counts1[colors1[i]]
-            # We always connect to a node in Tree 2 such that we get the max possible targets from Tree 2
-            # Since we can pick any node v in Tree 2, we can always achieve max(counts2[0], counts2[1])
-            # by picking v with appropriate parity.
-            ans.append(targets_in_tree1 + max_targets_from_tree2)
+            # Target nodes in Tree 1 are those with the same parity as node i.
+            targets1 = counts1[colors1[i]]
+            ans.append(targets1 + max_tree2_targets)
             
         return ans
 # @lc code=end
