280faa743c1ca39351a1459c502f1c067e6ee92a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-21 13:25:47 +0800

    Add solution: 3425.longest-special-path.py

--- 3425.longest-special-path.py
+++ 3425.longest-special-path.py
@@ -6,9 +6,10 @@
 
 # @lc code=start
 import sys
+from typing import List
 
 # Increase recursion depth for deep trees
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(10**5)
 
 class Solution:
     def longestSpecialPath(self, edges: List[List[int]], nums: List[int]) -> List[int]:
@@ -17,67 +18,58 @@ class Solution:
         for u, v, w in edges:
             adj[u].append((v, w))
             adj[v].append((u, w))
-            
-        # pos[val] stores the depth index of the last occurrence of val in the current path
-        # using a list is faster than a dict given the constraints on nums[i]
-        # Max value in nums is 5*10^4
-        max_val = 0
-        for x in nums:
-            max_val = max(max_val, x)
-        pos = [-1] * (max_val + 1)
         
-        # dist_stack[i] stores the cumulative distance from root to the node at depth i
-        dist_stack = []
+        # result[0] is max length, result[1] is min nodes
+        result = [0, 1]
         
-        # Result: [max_len, min_nodes]
-        # Initialize with [0, 1] because a single node is a valid path of length 0
-        res = [0, 1]
+        # last_pos[val] stores the depth (index in current path) of the last occurrence of val
+        last_pos = [-1] * (max(nums) + 1 if nums else 1)
+        # path_dists[i] stores the distance from root to the i-th node in the current DFS path
+        path_dists = []
         
-        def dfs(u, p, depth, curr_dist, start_index):
-            val = nums[u]
-            prev_index = pos[val]
+        def dfs(u, p, current_dist, current_left):
+            # Ancestor depth of the last occurrence of nums[u]
+            old_pos = last_pos[nums[u]]
             
-            # The valid path must start after the previous occurrence of the current value
-            # Also must respect constraints from ancestors (captured in start_index)
-            new_start_index = start_index
-            if prev_index != -1 and prev_index >= start_index:
-                new_start_index = prev_index + 1
+            # The special path ending at u must start at or after new_left
+            # to ensure all values in the path are unique.
+            new_left = current_left
+            if old_pos + 1 > new_left:
+                new_left = old_pos + 1
             
-            # Calculate length and node count for the longest special path ending at u
-            # The path starts at new_start_index in the stack
-            # Distance at start of path: dist_stack[new_start_index]
-            # Note: dist_stack currently has entries up to depth-1.
-            # If new_start_index == depth, distance is curr_dist.
+            # Current depth in the path (0-indexed)
+            current_depth = len(path_dists)
             
-            dist_at_start = 0
-            if new_start_index == depth:
-                dist_at_start = curr_dist
+            # Distance from root to the start node of this special path
+            if new_left < current_depth:
+                d_start = path_dists[new_left]
             else:
-                dist_at_start = dist_stack[new_start_index]
-                
-            path_len = curr_dist - dist_at_start
-            node_count = depth - new_start_index + 1
+                d_start = current_dist
             
-            if path_len > res[0]:
-                res[0] = path_len
-                res[1] = node_count
-            elif path_len == res[0]:
-                if node_count < res[1]:
-                    res[1] = node_count
+            path_len = current_dist - d_start
+            num_nodes = current_depth - new_left + 1
             
-            # Update state for children
-            pos[val] = depth
-            dist_stack.append(curr_dist)
+            # Update the global best result
+            if path_len > result[0]:
+                result[0] = path_len
+                result[1] = num_nodes
+            elif path_len == result[0]:
+                if num_nodes < result[1]:
+                    result[1] = num_nodes
+            
+            # Prepare state for children
+            last_pos[nums[u]] = current_depth
+            path_dists.append(current_dist)
             
             for v, w in adj[u]:
                 if v != p:
-                    dfs(v, u, depth + 1, curr_dist + w, new_start_index)
+                    dfs(v, u, current_dist + w, new_left)
             
-            # Backtrack
-            dist_stack.pop()
-            pos[val] = prev_index
+            # Backtrack: restore last_pos and path_dists for other branches
+            path_dists.pop()
+            last_pos[nums[u]] = old_pos
 
-        dfs(0, -1, 0, 0, 0)
-        return res
-
+        # Start DFS from the root (node 0)
+        dfs(0, -1, 0, 0)
+        return result
 # @lc code=end
