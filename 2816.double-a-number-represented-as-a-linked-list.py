ab016697929ca1f8219c122d15c5b58e792d1ccb
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-21 13:19:24 +0800

    Add solution: 2816.double-a-number-represented-as-a-linked-list.py

--- 2816.double-a-number-represented-as-a-linked-list.py
+++ 2816.double-a-number-represented-as-a-linked-list.py
@@ -12,22 +12,21 @@
 #         self.next = next
 class Solution:
     def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # If the head is greater than 4, doubling it will result in a carry
-        # that requires a new node at the front.
-        if head.val > 4:
+        # If the head value is 5 or more, the result will have an extra digit at the front.
+        if head.val >= 5:
             head = ListNode(0, head)
         
-        current = head
-        while current:
-            # Double the current value and take modulo 10
-            current.val = (current.val * 2) % 10
+        curr = head
+        while curr:
+            # Update the current node's value: double it and take the last digit.
+            # The maximum value of (val * 2) % 10 is 8.
+            curr.val = (curr.val * 2) % 10
             
-            # Check if the next node will generate a carry
-            # A carry is generated if the next node's value is > 4 (since 5*2=10)
-            if current.next and current.next.val > 4:
-                current.val += 1
+            # If the next node's original value is 5 or more, it will produce a carry.
+            if curr.next and curr.next.val >= 5:
+                curr.val += 1
             
-            current = current.next
+            curr = curr.next
             
         return head
 # @lc code=end
