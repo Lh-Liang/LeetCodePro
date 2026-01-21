36d334325809d2bcd6ecdcda01336ab49ee927bf
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-21 13:42:50 +0800

    Add solution: 3585.find-weighted-median-node-in-tree.py

--- 3585.find-weighted-median-node-in-tree.py
+++ 3585.find-weighted-median-node-in-tree.py
@@ -1,4 +1,10 @@
-from collections import deque
+#
+# @lc app=leetcode id=3585 lang=python3
+#
+# [3585] Find Weighted Median Node in Tree
+#
+
+# @lc code=start
 from typing import List
 
 class Solution:
@@ -7,91 +13,74 @@ class Solution:
         for u, v, w in edges:
             adj[u].append((v, w))
             adj[v].append((u, w))
-            
-        parent = [-1] * n
-        depth = [0] * n
-        dist = [0] * n
-        LOG = 18  # Sufficient for 10^5 nodes
+        
+        LOG = 18
         up = [[-1] * LOG for _ in range(n)]
+        dist = [0] * n
+        depth = [0] * n
         
-        # BFS to build the tree structure (parent, depth, dist)
-        queue = deque([0])
-        # Standard BFS
-        while queue:
-            u = queue.popleft()
+        # Iterative DFS to compute dist, depth, and the first parent
+        stack = [(0, -1, 0, 0)]
+        while stack:
+            u, p, d, dep = stack.pop()
+            up[u][0] = p
+            dist[u] = d
+            depth[u] = dep
             for v, w in adj[u]:
-                if v != parent[u]:
-                    parent[v] = u
-                    depth[v] = depth[u] + 1
-                    dist[v] = dist[u] + w
-                    queue.append(v)
+                if v != p:
+                    stack.append((v, u, d + w, dep + 1))
         
-        # Initialize binary lifting table
-        for u in range(n):
-            up[u][0] = parent[u]
-            
-        for i in range(1, LOG):
-            for u in range(n):
-                if up[u][i-1] != -1:
-                    up[u][i] = up[up[u][i-1]][i-1]
-                else:
-                    up[u][i] = -1
-                    
+        # Fill binary lifting table
+        for k in range(1, LOG):
+            for i in range(n):
+                if up[i][k-1] != -1:
+                    up[i][k] = up[up[i][k-1]][k-1]
+        
         def get_lca(u, v):
             if depth[u] < depth[v]:
                 u, v = v, u
-            
-            diff = depth[u] - depth[v]
-            for i in range(LOG):
-                if (diff >> i) & 1:
-                    u = up[u][i]
-            
+            for k in range(LOG - 1, -1, -1):
+                if depth[u] - (1 << k) >= depth[v]:
+                    u = up[u][k]
             if u == v:
                 return u
-            
-            for i in range(LOG - 1, -1, -1):
-                if up[u][i] != up[v][i]:
-                    u = up[u][i]
-                    v = up[v][i]
-            
+            for k in range(LOG - 1, -1, -1):
+                if up[u][k] != up[v][k]:
+                    u = up[u][k]
+                    v = up[v][k]
             return up[u][0]
-
-        ans = []
-        for u, v in queries:
-            lca = get_lca(u, v)
-            total_dist = dist[u] + dist[v] - 2 * dist[lca]
-            dist_u_lca = dist[u] - dist[lca]
-            
-            # Check if the median is on the path segment u -> lca
-            # We check if dist(u, lca) >= total_dist / 2
-            if 2 * dist_u_lca >= total_dist:
-                target_2dist = 2 * dist[u] - total_dist
-                # We want the node x closest to u (deepest) such that 2*dist[x] <= target_2dist.
-                # Check if u itself is the answer
-                if 2 * dist[u] <= target_2dist:
-                    ans.append(u)
-                else:
-                    # Lift u to the highest node that is INVALID (2*dist > target)
-                    # The parent of that node will be the first VALID node (closest to u)
-                    curr = u
-                    for i in range(LOG - 1, -1, -1):
-                        if up[curr][i] != -1:
-                            if 2 * dist[up[curr][i]] > target_2dist:
-                                curr = up[curr][i]
-                    ans.append(up[curr][0])
-            else:
-                # Median is on path segment lca -> v
-                # We want the node x closest to lca (shallowest) such that dist(u, x) >= total_dist / 2
-                # Equivalent to: 2*dist[x] >= total_dist - 2*dist[u] + 4*dist[lca]
-                target_2dist = total_dist - 2 * dist[u] + 4 * dist[lca]
-                
-                # We want the highest ancestor of v that satisfies the condition (VALID)
-                # Lift v as long as the destination is VALID
-                curr = v
-                for i in range(LOG - 1, -1, -1):
-                    if up[curr][i] != -1:
-                        if 2 * dist[up[curr][i]] >= target_2dist:
-                            curr = up[curr][i]
-                ans.append(curr)
         
-        return ans
+        results = []
+        for u, v in queries:
+            if u == v:
+                results.append(u)
+                continue
+            
+            lca = get_lca(u, v)
+            total_w = dist[u] + dist[v] - 2 * dist[lca]
+            
+            # Check if median is on the path from u to lca
+            if 2 * (dist[u] - dist[lca]) >= total_w:
+                # If the first edge is already >= total_w / 2, or no edge exists
+                curr = u
+                for k in range(LOG - 1, -1, -1):
+                    anc = up[curr][k]
+                    if anc != -1 and depth[anc] >= depth[lca]:
+                        if 2 * (dist[u] - dist[anc]) < total_w:
+                            curr = anc
+                results.append(up[curr][0])
+            else:
+                # Median is on the path from lca to v
+                # Condition: D(u, lca) + D(lca, x) >= total_w / 2
+                # (dist[u] - dist[lca]) + (dist[x] - dist[lca]) >= total_w / 2
+                # 2*dist[u] - 4*dist[lca] + 2*dist[x] >= total_w
+                target2 = total_w - 2 * dist[u] + 4 * dist[lca]
+                curr = v
+                for k in range(LOG - 1, -1, -1):
+                    anc = up[curr][k]
+                    if anc != -1 and depth[anc] >= depth[lca] and 2 * dist[anc] >= target2:
+                        curr = anc
+                results.append(curr)
+                
+        return results
+# @lc code=end
