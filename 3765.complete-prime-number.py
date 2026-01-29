e28449fe20ac4f1c908a4acfcd999658390102af
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 03:50:09 +0800

    Add solution: 3765.complete-prime-number.py

--- 3765.complete-prime-number.py
+++ 3765.complete-prime-number.py
@@ -7,39 +7,32 @@
 # @lc code=start
 class Solution:
     def completePrime(self, num: int) -> bool:
-        def is_prime(x: int) -> bool:
-            if x <= 1:
+        def is_prime(n: int) -> bool:
+            if n < 2:
                 return False
-            if x == 2 or x == 3:
+            if n <= 3:
                 return True
-            if x % 2 == 0 or x % 3 == 0:
+            if n % 2 == 0 or n % 3 == 0:
                 return False
             i = 5
-            while i * i <= x:
-                if x % i == 0 or x % (i + 2) == 0:
+            while i * i <= n:
+                if n % i == 0 or n % (i + 2) == 0:
                     return False
                 i += 6
             return True
-        
+
         s = str(num)
         n = len(s)
         
         # Check all prefixes
-        prefix = 0
-        for char in s:
-            prefix = prefix * 10 + int(char)
-            if not is_prime(prefix):
+        for i in range(1, n + 1):
+            if not is_prime(int(s[:i])):
                 return False
         
-        # Check all suffixes
-        suffix = 0
-        power = 1
-        for i in range(n - 1, -1, -1):
-            suffix += int(s[i]) * power
-            if not is_prime(suffix):
+        # Check all suffixes (skipping the full number as it was checked in prefixes)
+        for i in range(1, n):
+            if not is_prime(int(s[i:])):
                 return False
-            power *= 10
         
         return True
-
 # @lc code=end
