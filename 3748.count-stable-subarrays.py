6a0eca9e7b5aaa8a1c81244d9b5882e2190110c3
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 06:20:33 +0800

    Add solution: 3748.count-stable-subarrays.py

--- 3748.count-stable-subarrays.py
+++ 3748.count-stable-subarrays.py
@@ -5,47 +5,23 @@
 #
 
 # @lc code=start
-import bisect
-from typing import List
-
 class Solution:
     def countStableSubarrays(self, nums: List[int], queries: List[List[int]]) -> List[int]:
-        n = len(nums)
-        # R[i] is the largest index such that nums[i...R[i]] is non-decreasing.
-        # This is precomputed in O(N) from right to left.
-        R = [0] * n
-        R[n-1] = n - 1
-        for i in range(n - 2, -1, -1):
-            if nums[i] <= nums[i+1]:
-                R[i] = R[i+1]
-            else:
-                R[i] = i
+        def count_stable_in_range(l, r):
+            count = 0
+            start = l
+            while start <= r:
+                end = start
+                while end < r and nums[end] <= nums[end + 1]:
+                    end += 1
+                # Calculate number of stable subarrays in this range [start, end]
+                length = end - start + 1
+                count += (length * (length + 1)) // 2 # Sum of first n natural numbers formula for counting subarrays
+                start = end + 1 # Move start to next potential starting point
+            return count
         
-        # f[i] is the total stable subarrays starting at index i in the global array.
-        f = [R[i] - i + 1 for i in range(n)]
-        
-        # P is the prefix sum of f to allow O(1) range sums.
-        P = [0] * (n + 1)
-        for i in range(n):
-            P[i+1] = P[i] + f[i]
-            
-        ans = []
-        for L, right_query in queries:
-            # R[i] is monotonic, so we find 'k' where R[i] starts being >= right_query.
-            # We search only within the query range [L, right_query].
-            k = bisect.bisect_left(R, right_query, L, right_query + 1)
-            
-            res = 0
-            # Part 1: i < k, where R[i] < right_query. Contribution is R[i] - i + 1.
-            if L < k:
-                res += P[k] - P[L]
-            
-            # Part 2: i >= k, where R[i] >= right_query. Contribution is right_query - i + 1.
-            if k <= right_query:
-                m = right_query - k + 1
-                res += m * (m + 1) // 2
-                
-            ans.append(res)
-            
-        return ans
+        results = []
+        for li, ri in queries:
+            results.append(count_stable_in_range(li, ri))
+        return results
 # @lc code=end
