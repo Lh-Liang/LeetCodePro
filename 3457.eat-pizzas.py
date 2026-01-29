0ac8b40e46cc1ae32a7c34ddb78e5427f366bbb6
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 00:24:09 +0800

    Add solution: 3457.eat-pizzas.py

--- 3457.eat-pizzas.py
+++ 3457.eat-pizzas.py
@@ -5,31 +5,32 @@
 #
 
 # @lc code=start
-import math
-
 class Solution:
     def maxWeight(self, pizzas: List[int]) -> int:
-        # Sort descending to pick the largest weights first
+        # Sort pizzas in descending order to pick the heaviest ones greedily
         pizzas.sort(reverse=True)
         
         n = len(pizzas)
-        days = n // 4
+        total_days = n // 4
         # Number of odd-numbered days (1, 3, 5...)
-        odd_days = (days + 1) // 2
+        odd_days = (total_days + 1) // 2
         # Number of even-numbered days (2, 4, 6...)
-        even_days = days // 2
+        even_days = total_days // 2
         
-        # For odd days, we gain weight Z (the heaviest of the 4).
+        # For odd days, we gain the largest pizza (Z).
         # We take the top 'odd_days' pizzas.
         total_gain = sum(pizzas[:odd_days])
         
-        # For even days, we gain weight Y (the second heaviest).
-        # To maximize Y, we must 'sacrifice' one heavier pizza to be Z for each even day.
-        # We start picking from index 'odd_days', skipping one for every one we take.
-        # Indices: odd_days + 1, odd_days + 3, ..., odd_days + 2*even_days - 1
-        start_idx = odd_days
-        even_day_gains = pizzas[start_idx + 1 : start_idx + 1 + 2 * even_days : 2]
-        total_gain += sum(even_day_gains)
-        
+        # For even days, we gain the second largest pizza (Y).
+        # For each even day, we need one pizza to be Z (larger than Y).
+        # So we skip one, take one, skip one, take one...
+        # Starting from the index after the odd_day pizzas.
+        current_idx = odd_days
+        for _ in range(even_days):
+            # Skip one (to be Z), take the next (to be Y)
+            current_idx += 1
+            total_gain += pizzas[current_idx]
+            current_idx += 1
+            
         return total_gain
 # @lc code=end
