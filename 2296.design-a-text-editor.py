c81ee483b7cb1a26da3f704d2691fc52733bba22
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 21:34:32 +0800

    Add solution: 2296.design-a-text-editor.py

--- 2296.design-a-text-editor.py
+++ 2296.design-a-text-editor.py
@@ -14,34 +14,35 @@ class TextEditor:
         self.right = []
 
     def addText(self, text: str) -> None:
-        # O(len(text)) operation
+        # Add characters to the left stack; cursor ends to the right of text
         for char in text:
             self.left.append(char)
 
     def deleteText(self, k: int) -> int:
-        # O(k) operation: remove up to k characters from the left stack
+        # Delete up to k characters to the left of the cursor
         count = 0
         while k > 0 and self.left:
             self.left.pop()
-            k -= 1
             count += 1
+            k -= 1
         return count
 
     def cursorLeft(self, k: int) -> str:
-        # O(k) operation: move up to k characters from left to right stack
+        # Move cursor left: transfer chars from left to right stack
         while k > 0 and self.left:
             self.right.append(self.left.pop())
             k -= 1
-        # Return last 10 characters to the left of the cursor
-        return "".join(self.left[-10:])
+        # Return last min(10, len) characters to the left of cursor
+        return ''.join(self.left[-10:])
 
     def cursorRight(self, k: int) -> str:
-        # O(k) operation: move up to k characters from right to left stack
+        # Move cursor right: transfer chars from right to left stack
         while k > 0 and self.right:
             self.left.append(self.right.pop())
             k -= 1
-        # Return last 10 characters to the left of the cursor
-        return "".join(self.left[-10:])
+        # Return last min(10, len) characters to the left of cursor
+        return ''.join(self.left[-10:])
+
 
 # Your TextEditor object will be instantiated and called as such:
 # obj = TextEditor()
