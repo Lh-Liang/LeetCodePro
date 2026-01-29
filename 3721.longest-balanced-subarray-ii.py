b3f0aa22b15537ce3303a2c924c2a51b0953f96d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 06:15:19 +0800

    Add solution: 3721.longest-balanced-subarray-ii.py

--- 3721.longest-balanced-subarray-ii.py
+++ 3721.longest-balanced-subarray-ii.py
@@ -5,82 +5,31 @@
 #
 
 # @lc code=start
-import sys
+from typing import List
 
 class Solution:
     def longestBalanced(self, nums: List[int]) -> int:
-        n = len(nums)
-        # Using a flat array for segment tree to reduce overhead
-        # tree_min[v] and tree_max[v] store the range of |E| - |O|
-        tree_min = [0] * (4 * n)
-        tree_max = [0] * (4 * n)
-        lazy = [0] * (4 * n)
-
-        def update(v, tl, tr, l, r, add):
-            if l > r:
-                return
-            if l == tl and r == tr:
-                tree_min[v] += add
-                tree_max[v] += add
-                lazy[v] += add
-            else:
-                # Push lazy
-                if lazy[v] != 0:
-                    lazy[2*v] += lazy[v]
-                    tree_min[2*v] += lazy[v]
-                    tree_max[2*v] += lazy[v]
-                    lazy[2*v+1] += lazy[v]
-                    tree_min[2*v+1] += lazy[v]
-                    tree_max[2*v+1] += lazy[v]
-                    lazy[v] = 0
-                
-                tm = (tl + tr) // 2
-                update(2*v, tl, tm, l, min(r, tm), add)
-                update(2*v+1, tm+1, tr, max(l, tm+1), r, add)
-                tree_min[v] = min(tree_min[2*v], tree_min[2*v+1])
-                tree_max[v] = max(tree_max[2*v], tree_max[2*v+1])
-
-        def find_first_zero(v, tl, tr, r_bound):
-            if tree_min[v] > 0 or tree_max[v] < 0:
-                return -1
-            if tl == tr:
-                return tl
-            
-            # Push lazy
-            if lazy[v] != 0:
-                lazy[2*v] += lazy[v]
-                tree_min[2*v] += lazy[v]
-                tree_max[2*v] += lazy[v]
-                lazy[2*v+1] += lazy[v]
-                tree_min[2*v+1] += lazy[v]
-                tree_max[2*v+1] += lazy[v]
-                lazy[v] = 0
-                
-            tm = (tl + tr) // 2
-            res = find_first_zero(2*v, tl, tm, r_bound)
-            if res == -1 and tm + 1 <= r_bound:
-                res = find_first_zero(2*v+1, tm+1, tr, r_bound)
-            return res
-
-        last_pos = {}
-        max_len = 0
+        start = 0
+        max_length = 0
+        even_set = set()
+        odd_set = set()
         
-        # Set recursion limit for deep trees
-        sys.setrecursionlimit(200000)
-
-        for j in range(n):
-            val = nums[j]
-            prev = last_pos.get(val, -1)
-            diff = 1 if val % 2 == 0 else -1
+        for end in range(len(nums)):
+            num = nums[end]
+            if num % 2 == 0:
+                even_set.add(num)
+            else:
+                odd_set.add(num)
             
-            # Update range [prev + 1, j]
-            update(1, 0, n - 1, prev + 1, j, diff)
-            last_pos[val] = j
+            while len(even_set) != len(odd_set):
+                start_num = nums[start]
+                if start_num % 2 == 0:
+                    even_set.discard(start_num)
+                else:
+                    odd_set.discard(start_num)
+                start += 1
             
-            # Find leftmost i in [0, j] where f(i) == 0
-            first_i = find_first_zero(1, 0, n - 1, j)
-            if first_i != -1:
-                max_len = max(max_len, j - first_i + 1)
-                
-        return max_len
+            max_length = max(max_length, end - start + 1)
+        
+        return max_length
 # @lc code=end
