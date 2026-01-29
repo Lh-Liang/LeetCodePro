36e945a9fa4eb1ff58af980f4a667655c66fec97
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 03:50:40 +0800

    Add solution: 3762.minimum-operations-to-equalize-subarrays.py

--- 3762.minimum-operations-to-equalize-subarrays.py
+++ 3762.minimum-operations-to-equalize-subarrays.py
@@ -3,126 +3,104 @@
 #
 # [3762] Minimum Operations to Equalize Subarrays
 #
+
 # @lc code=start
-from typing import List
-import math
-
-class FenwickTree:
-    def __init__(self, size: int):
-        self.size = size
-        self.tree = [0] * (size + 2)
-
-    def update(self, index: int, delta: int) -> None:
-        while index <= self.size:
-            self.tree[index] += delta
-            index += index & -index
-
-    def query(self, index: int) -> int:
-        res = 0
-        while index > 0:
-            res += self.tree[index]
-            index -= index & -index
-        return res
-
 class Solution:
     def minOperations(self, nums: List[int], k: int, queries: List[List[int]]) -> List[int]:
         n = len(nums)
-        mods = [num % k for num in nums]
-        qarr = [num // k for num in nums]
-
-        # Sparse table for range min/max on mods
-        LOG = n.bit_length()
-        st_min = [[0] * n for _ in range(LOG)]
-        st_max = [[0] * n for _ in range(LOG)]
-        for i in range(n):
-            st_min[0][i] = mods[i]
-            st_max[0][i] = mods[i]
-        for j in range(1, LOG):
-            for i in range(n - (1 << j) + 1):
-                st_min[j][i] = min(st_min[j-1][i], st_min[j-1][i + (1 << (j-1))])
-                st_max[j][i] = max(st_max[j-1][i], st_max[j-1][i + (1 << (j-1))])
-
-        def get_log(length: int) -> int:
-            j = 0
-            while (1 << (j + 1)) <= length:
-                j += 1
-            return j
-
-        def range_min(l: int, r: int) -> int:
-            length = r - l + 1
-            j = get_log(length)
-            return min(st_min[j][l], st_min[j][r - (1 << j) + 1])
-
-        def range_max(l: int, r: int) -> int:
-            length = r - l + 1
-            j = get_log(length)
-            return max(st_max[j][l], st_max[j][r - (1 << j) + 1])
-
-        # Mo's algorithm setup
-        B = int(math.sqrt(n)) + 1
-        query_list = [[l, r, i] for i, (l, r) in enumerate(queries)]
-        query_list.sort(key=lambda x: (x[0] // B, x[1]))
-
-        # Coordinate compression
-        vals = sorted(set(qarr))
-        m = len(vals)
-        ranks = {vals[i]: i + 1 for i in range(m)}
-
-        mo_ans = [0] * len(queries)
-        ft_count = FenwickTree(m)
-        ft_sumval = FenwickTree(m)
-        cur_l = 0
-        cur_r = -1
-
-        def add(pos: int):
-            rk = ranks[qarr[pos]]
-            ft_count.update(rk, 1)
-            ft_sumval.update(rk, qarr[pos])
-
-        def remove(pos: int):
-            rk = ranks[qarr[pos]]
-            ft_count.update(rk, -1)
-            ft_sumval.update(rk, -qarr[pos])
-
-        for ql, qr, qidx in query_list:
-            while cur_r < qr:
-                cur_r += 1
-                add(cur_r)
-            while cur_l > ql:
-                cur_l -= 1
-                add(cur_l)
-            while cur_r > qr:
-                remove(cur_r)
-                cur_r -= 1
-            while cur_l < ql:
-                remove(cur_l)
-                cur_l += 1
-            sz = cur_r - cur_l + 1
-            need = sz // 2 + 1
-            # Binary search for med_rk
-            lo, hi = 1, m
-            while lo < hi:
-                md = (lo + hi) // 2
-                if ft_count.query(md) >= need:
-                    hi = md
-                else:
-                    lo = md + 1
-            med_rk = lo
-            med = vals[med_rk - 1]
-            num_lt = ft_count.query(med_rk - 1) if med_rk > 1 else 0
-            cnt_ge = sz - num_lt
-            total_sumq = ft_sumval.query(m)
-            sum_lt = ft_sumval.query(med_rk - 1) if med_rk > 1 else 0
-            sum_ge = total_sumq - sum_lt
-            mo_ans[qidx] = (2 * sum_ge - total_sumq) - med * (2 * cnt_ge - sz)
-
-        # Build final ans
-        ans = []
-        for i, (l, r) in enumerate(queries):
-            if range_min(l, r) != range_max(l, r):
-                ans.append(-1)
+        y = [x // k for x in nums]
+        rems = [x % k for x in nums]
+        
+        # Prefix sum to check if all elements in nums[li...ri] have same remainder mod k
+        diff_rem = [0] * n
+        for i in range(1, n):
+            diff_rem[i] = diff_rem[i-1] + (1 if rems[i] != rems[i-1] else 0)
+            
+        # Coordinate compression for y values
+        sorted_y = sorted(list(set(y)))
+        rank_map = {val: i for i, val in enumerate(sorted_y)}
+        m = len(sorted_y)
+        
+        # Persistent Segment Tree: store count and sum of y values
+        tree_cnt = [0] * (n * 35)
+        tree_sum = [0] * (n * 35)
+        L = [0] * (n * 35)
+        R = [0] * (n * 35)
+        roots = [0] * (n + 1)
+        node_ptr = 0
+        
+        def update(prev, l, r, val_idx, val):
+            nonlocal node_ptr
+            node_ptr += 1
+            curr = node_ptr
+            tree_cnt[curr] = tree_cnt[prev] + 1
+            tree_sum[curr] = tree_sum[prev] + val
+            L[curr] = L[prev]
+            R[curr] = R[prev]
+            if l == r:
+                return curr
+            mid = (l + r) // 2
+            if val_idx <= mid:
+                L[curr] = update(L[prev], l, mid, val_idx, val)
             else:
-                ans.append(mo_ans[i])
-        return ans
+                R[curr] = update(R[prev], mid + 1, r, val_idx, val)
+            return curr
 
+        for i in range(n):
+            roots[i+1] = update(roots[i], 0, m - 1, rank_map[y[i]], y[i])
+            
+        # Single pass to find median and calculate operations
+        def query_ops(node_l, node_r, l, r, k_th, total_sum, total_cnt):
+            if l == r:
+                median = sorted_y[l]
+                # All elements in this range are equal to median
+                return median
+            
+            mid = (l + r) // 2
+            cnt_l = tree_cnt[L[node_r]] - tree_cnt[L[node_l]]
+            sum_l = tree_sum[L[node_r]] - tree_sum[L[node_l]]
+            
+            if k_th <= cnt_l:
+                return query_ops(L[node_l], L[node_r], l, mid, k_th, total_sum, total_cnt)
+            else:
+                # Median is in the right child. We know everything in left child is < median.
+                # We pass down the logic or just return median and calculate outside.
+                return query_ops(R[node_l], R[node_r], mid + 1, r, k_th - cnt_l, total_sum, total_cnt)
+
+        def get_stats(node_l, node_r, l, r, q_rank):
+            # Returns (count, sum) of elements with rank <= q_rank
+            if r <= q_rank:
+                return tree_cnt[node_r] - tree_cnt[node_l], tree_sum[node_r] - tree_sum[node_l]
+            mid = (l + r) // 2
+            c, s = get_stats(L[node_l], L[node_r], l, mid, q_rank)
+            if q_rank > mid:
+                c2, s2 = get_stats(R[node_l], R[node_r], mid + 1, r, q_rank)
+                c += c2
+                s += s2
+            return c, s
+
+        ans = []
+        for li, ri in queries:
+            # Check if all elements in nums[li...ri] have same remainder
+            if diff_rem[ri] - diff_rem[li] > 0:
+                ans.append(-1)
+                continue
+            
+            size = ri - li + 1
+            mid_rank_in_window = (size + 1) // 2
+            median = query_ops(roots[li], roots[ri+1], 0, m - 1, mid_rank_in_window, 0, 0)
+            
+            # Calculate sum of |y_i - median|
+            # ops = (cnt_le * median - sum_le) + (sum_gt - cnt_gt * median)
+            med_idx = rank_map[median]
+            cnt_le, sum_le = get_stats(roots[li], roots[ri+1], 0, m - 1, med_idx)
+            
+            total_sum = tree_sum[roots[ri+1]] - tree_sum[roots[li]]
+            sum_gt = total_sum - sum_le
+            cnt_gt = size - cnt_le
+            
+            res = (cnt_le * median - sum_le) + (sum_gt - cnt_gt * median)
+            ans.append(res)
+            
+        return ans
 # @lc code=end
