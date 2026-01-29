93501601aa0877f2032e75f2deb7c51f6e3ab976
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 16:04:34 +0800

    Add solution: 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py

--- 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
+++ 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
@@ -6,49 +6,49 @@
 
 # @lc code=start
 from collections import deque
-from typing import List
 
 class Solution:
     def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
-        def get_colors_and_counts(num_nodes, edges):
-            adj = [[] for _ in range(num_nodes)]
-            for u, v in edges:
-                adj[u].append(v)
-                adj[v].append(u)
-            
-            colors = [-1] * num_nodes
-            counts = [0, 0]
-            
-            # Since it's a tree, it's connected and bipartite.
-            # We can start BFS from any node, say node 0.
-            queue = deque([0])
-            colors[0] = 0
-            counts[0] = 1
-            
-            while queue:
-                u = queue.popleft()
-                for v in adj[u]:
-                    if colors[v] == -1:
-                        colors[v] = 1 - colors[u]
-                        counts[colors[v]] += 1
-                        queue.append(v)
-            return colors, counts
-
         n = len(edges1) + 1
         m = len(edges2) + 1
-
-        colors1, counts1 = get_colors_and_counts(n, edges1)
-        colors2, counts2 = get_colors_and_counts(m, edges2)
-        
-        # For any node i in Tree 1, we can always connect Tree 1 to Tree 2 
-        # such that we pick the color partition in Tree 2 with the maximum number of nodes.
-        max_tree2_targets = max(counts2)
-        
-        ans = []
+        adj1 = [[] for _ in range(n)]
+        for a, b in edges1:
+            adj1[a].append(b)
+            adj1[b].append(a)
+        adj2 = [[] for _ in range(m)]
+        for a, b in edges2:
+            adj2[a].append(b)
+            adj2[b].append(a)
+        # Tree2
+        color2 = [-1] * m
+        sz2 = [0, 0]
+        q = deque([0])
+        color2[0] = 0
+        sz2[0] += 1
+        while q:
+            u = q.popleft()
+            for v in adj2[u]:
+                if color2[v] == -1:
+                    color2[v] = 1 - color2[u]
+                    sz2[color2[v]] += 1
+                    q.append(v)
+        big2 = max(sz2[0], sz2[1])
+        # Tree1
+        color1 = [-1] * n
+        sz1 = [0, 0]
+        q = deque([0])
+        color1[0] = 0
+        sz1[0] += 1
+        while q:
+            u = q.popleft()
+            for v in adj1[u]:
+                if color1[v] == -1:
+                    color1[v] = 1 - color1[u]
+                    sz1[color1[v]] += 1
+                    q.append(v)
+        ans = [0] * n
         for i in range(n):
-            # Target nodes in Tree 1 are those with the same parity as node i.
-            targets1 = counts1[colors1[i]]
-            ans.append(targets1 + max_tree2_targets)
-            
+            ans[i] = sz1[color1[i]] + big2
         return ans
+
 # @lc code=end
