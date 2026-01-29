48dce3e42dbcfef86700de7be9f742fd97f65115
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 05:55:09 +0800

    Add solution: 3598.longest-common-prefix-between-adjacent-strings-after-removals.py

--- 3598.longest-common-prefix-between-adjacent-strings-after-removals.py
+++ 3598.longest-common-prefix-between-adjacent-strings-after-removals.py
@@ -5,54 +5,23 @@
 #
 
 # @lc code=start
-from typing import List
-
 class Solution:
     def longestCommonPrefix(self, words: List[str]) -> List[int]:
+        def common_prefix_length(a: str, b: str) -> int:
+            min_len = min(len(a), len(b))
+            for i in range(min_len):
+                if a[i] != b[i]:
+                    return i
+            return min_len
+        
         n = len(words)
-        if n <= 1:
-            return [0] * n
-        
-        def get_lcp_len(s1: str, s2: str) -> int:
-            length = 0
-            for c1, c2 in zip(s1, s2):
-                if c1 != c2:
-                    break
-                length += 1
-            return length
-        
-        # P[i] is the LCP of words[i] and words[i+1]
-        # There are n-1 such pairs.
-        P = [get_lcp_len(words[i], words[i+1]) for i in range(n - 1)]
-        
-        # pref_max[i] = max(P[0...i-1])
-        pref_max = [0] * n
-        curr_max = 0
-        for i in range(n - 1):
-            curr_max = max(curr_max, P[i])
-            pref_max[i + 1] = curr_max
-            
-        # suff_max[i] = max(P[i...n-2])
-        suff_max = [0] * n
-        curr_max = 0
-        for i in range(n - 2, -1, -1):
-            curr_max = max(curr_max, P[i])
-            suff_max[i] = curr_max
-            
-        ans = [0] * n
+        answer = []
         for i in range(n):
-            if i == 0:
-                # Pairs: (w1, w2), (w2, w3)... LCPs: P[1...n-2]
-                ans[i] = suff_max[1]
-            elif i == n - 1:
-                # Pairs: (w0, w1), (w1, w2)... LCPs: P[0...n-3]
-                ans[i] = pref_max[n - 2]
-            else:
-                # Pairs: (w0...wi-1) [P[0...i-2]], (wi-1, wi+1) [new], (wi+1...wn-1) [P[i+1...n-2]]
-                v_pref = pref_max[i - 1]
-                v_suff = suff_max[i + 1]
-                v_bridge = get_lcp_len(words[i - 1], words[i + 1])
-                ans[i] = max(v_pref, v_suff, v_bridge)
-                
-        return ans
+            max_prefix = 0
+            modified_words = words[:i] + words[i+1:]
+            for j in range(len(modified_words) - 1):
+                current_prefix = common_prefix_length(modified_words[j], modified_words[j+1])
+                max_prefix = max(max_prefix, current_prefix)
+            answer.append(max_prefix)
+        return answer
 # @lc code=end
