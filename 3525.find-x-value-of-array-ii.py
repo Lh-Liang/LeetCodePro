38a4fb4be5cee173648281dbdaa99de59d1d681e
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 22:44:14 +0800

    Add solution: 3525.find-x-value-of-array-ii.py

--- 3525.find-x-value-of-array-ii.py
+++ 3525.find-x-value-of-array-ii.py
@@ -5,82 +5,83 @@
 #
 
 # @lc code=start
-from typing import List
-
 class Solution:
     def resultArray(self, nums: List[int], k: int, queries: List[List[int]]) -> List[int]:
-        class Node:
-            def __init__(self, k: int):
-                self.total = 1
-                self.count = [0] * k
-        
-        class SegTree:
-            def __init__(self, n: int, k: int, nums: List[int]):
-                self.n = n
-                self.k = k
-                self.tree = [Node(k) for _ in range(4 * n + 10)]
-                self._build(1, 0, n - 1, nums)
-            
-            def _build(self, node: int, start: int, end: int, nums: List[int]):
-                if start == end:
-                    m = nums[start] % self.k
-                    self.tree[node].total = m
-                    self.tree[node].count[m] = 1
-                    return
-                mid = (start + end) // 2
-                self._build(2 * node, start, mid, nums)
-                self._build(2 * node + 1, mid + 1, end, nums)
-                self.tree[node] = self._merge(self.tree[2 * node], self.tree[2 * node + 1])
-            
-            def _merge(self, a: Node, b: Node) -> Node:
-                res = Node(self.k)
-                res.total = (a.total * b.total) % self.k
-                contrib = [0] * self.k
-                for y in range(self.k):
-                    z = (a.total * y) % self.k
-                    contrib[z] += b.count[y]
-                for x in range(self.k):
-                    res.count[x] = a.count[x] + contrib[x]
-                return res
-            
-            def update(self, pos: int, val: int):
-                self._update(1, 0, self.n - 1, pos, val)
-            
-            def _update(self, node: int, start: int, end: int, pos: int, val: int):
-                if start == end:
-                    m = val % self.k
-                    self.tree[node].total = m
-                    self.tree[node].count = [0] * self.k
-                    self.tree[node].count[m] = 1
-                    return
-                mid = (start + end) // 2
-                if pos <= mid:
-                    self._update(2 * node, start, mid, pos, val)
-                else:
-                    self._update(2 * node + 1, mid + 1, end, pos, val)
-                self.tree[node] = self._merge(self.tree[2 * node], self.tree[2 * node + 1])
-            
-            def query(self, left: int, right: int) -> Node:
-                return self._query(1, 0, self.n - 1, left, right)
-            
-            def _query(self, node: int, start: int, end: int, l: int, r: int) -> Node:
-                if l > end or r < start:
-                    return Node(self.k)
-                if l <= start and end <= r:
-                    return self.tree[node]
-                mid = (start + end) // 2
-                leftq = self._query(2 * node, start, mid, l, r)
-                rightq = self._query(2 * node + 1, mid + 1, end, l, r)
-                return self._merge(leftq, rightq)
-        
         n = len(nums)
-        st = SegTree(n, k, nums)
-        result = []
-        for q in queries:
-            idx, val, si, xi = q
-            st.update(idx, val)
-            res_node = st.query(si, n - 1)
-            result.append(res_node.count[xi])
-        return result
+        size = 1
+        while size < n: 
+            size *= 2
+        
+        tree_prod = [1] * (2 * size)
+        tree_counts = [[0] * k for _ in range(2 * size)]
 
+        def merge_into(lc, lp, rc, target):
+            # lc: left counts, lp: left total product, rc: right counts
+            for i in range(k):
+                target[i] = lc[i]
+            for i in range(k):
+                if rc[i] > 0:
+                    target[(lp * i) % k] += rc[i]
+
+        def update_node(idx):
+            left = 2 * idx
+            right = 2 * idx + 1
+            tree_prod[idx] = (tree_prod[left] * tree_prod[right]) % k
+            merge_into(tree_counts[left], tree_prod[left], tree_counts[right], tree_counts[idx])
+
+        # Initial build
+        for i in range(n):
+            val = nums[i] % k
+            tree_prod[size + i] = val
+            tree_counts[size + i][val] = 1
+        
+        for i in range(size - 1, 0, -1):
+            update_node(i)
+
+        def update(idx, val):
+            idx += size
+            v = val % k
+            tree_prod[idx] = v
+            tree_counts[idx] = [0] * k
+            tree_counts[idx][v] = 1
+            while idx > 1:
+                idx //= 2
+                update_node(idx)
+
+        def query(l, r):
+            l += size
+            r += size
+            l_res_prod = 1
+            l_res_counts = [0] * k
+            
+            # Collect indices for the right-side parts to merge in correct order
+            r_parts = []
+            while l <= r:
+                if l % 2 == 1:
+                    new_counts = [0] * k
+                    merge_into(l_res_counts, l_res_prod, tree_counts[l], new_counts)
+                    l_res_counts = new_counts
+                    l_res_prod = (l_res_prod * tree_prod[l]) % k
+                    l += 1
+                if r % 2 == 0:
+                    r_parts.append(r)
+                    r -= 1
+                l //= 2
+                r //= 2
+            
+            for node_idx in reversed(r_parts):
+                new_counts = [0] * k
+                merge_into(l_res_counts, l_res_prod, tree_counts[node_idx], new_counts)
+                l_res_counts = new_counts
+                l_res_prod = (l_res_prod * tree_prod[node_idx]) % k
+                
+            return l_res_counts
+
+        results = []
+        for indexi, valuei, starti, xi in queries:
+            update(indexi, valuei)
+            res_counts = query(starti, n - 1)
+            results.append(res_counts[xi])
+            
+        return results
 # @lc code=end
