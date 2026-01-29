9948e45ad557b020f28bec3ffbf1520241adf5cb
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 23:38:24 +0800

    Add solution: 3382.maximum-area-rectangle-with-point-constraints-ii.py

--- 3382.maximum-area-rectangle-with-point-constraints-ii.py
+++ 3382.maximum-area-rectangle-with-point-constraints-ii.py
@@ -6,78 +6,81 @@
 
 # @lc code=start
 from typing import List
-import collections
-
-class FenwickTree:
-    def __init__(self, n):
-        self.tree = [0] * (n + 1)
-
-    def update(self, i, delta):
-        i += 1
-        while i < len(self.tree):
-            self.tree[i] += delta
-            i += i & (-i)
-
-    def query(self, i):
-        i += 1
-        s = 0
-        while i > 0:
-            s += self.tree[i]
-            i -= i & (-i)
-        return s
-
-    def query_range(self, left, right):
-        if left > right: return 0
-        return self.query(right) - self.query(left - 1)
 
 class Solution:
     def maxRectangleArea(self, xCoord: List[int], yCoord: List[int]) -> int:
+        n = len(xCoord)
+        # Combine and sort points: primarily by x, secondarily by y
         points = sorted(zip(xCoord, yCoord))
-        x_map = collections.defaultdict(list)
+        
+        # Coordinate compression for y-coordinates to use in Segment Tree
+        unique_y = sorted(list(set(yCoord)))
+        y_map = {y: i for i, y in enumerate(unique_y)}
+        m = len(unique_y)
+        
+        # Iterative Segment Tree for maximum x-coordinate in y-ranges
+        # Size is 2*m to accommodate the tree structure
+        tree = [-1] * (2 * m)
+
+        def update(i, val):
+            i += m
+            tree[i] = val
+            while i > 1:
+                i >>= 1
+                # Use max of children
+                new_val = tree[i << 1] if tree[i << 1] > tree[i << 1 | 1] else tree[i << 1 | 1]
+                if tree[i] == new_val: break
+                tree[i] = new_val
+
+        def query(l, r):
+            res = -1
+            l += m
+            r += m
+            while l <= r:
+                if l % 2 == 1:
+                    if tree[l] > res: res = tree[l]
+                    l += 1
+                if r % 2 == 0:
+                    if tree[r] > res: res = tree[r]
+                    r -= 1
+                l >>= 1
+                r >>= 1
+            return res
+
+        # Group points by x-coordinate
+        x_groups = []
         for x, y in points:
-            x_map[x].append(y)
+            if not x_groups or x_groups[-1][0] != x:
+                x_groups.append([x, [y]])
+            else:
+                x_groups[-1][1].append(y)
         
-        sorted_x = sorted(x_map.keys())
-        all_y = sorted(list(set(yCoord)))
-        y_rank = {y: i for i, y in enumerate(all_y)}
-        m = len(all_y)
-        
-        ft = FenwickTree(m)
-        # last_seen stores: (y_low, y_high) -> (x_prev, point_count_after_prev_x)
-        last_seen = {}
+        last_seen_pair = {}
         max_area = -1
         
-        for x in sorted_x:
-            ys = x_map[x]
+        for x, ys in x_groups:
+            # For each x, we check vertical edges formed by adjacent y-coordinates
+            y_indices = [y_map[y] for y in ys]
             
-            # Step 1: Check candidates before updating Fenwick tree with current x
-            for i in range(len(ys) - 1):
-                y_low, y_high = ys[i], ys[i+1]
-                pair = (y_low, y_high)
+            for i in range(len(y_indices) - 1):
+                y1_idx, y2_idx = y_indices[i], y_indices[i+1]
+                pair = (y1_idx, y2_idx)
                 
-                if pair in last_seen:
-                    prev_x, prev_count = last_seen[pair]
-                    # current_count here represents points strictly left of current x
-                    # in the range [y_low, y_high]
-                    current_count = ft.query_range(y_rank[y_low], y_rank[y_high])
-                    
-                    # If current_count == prev_count, it means no points exist between 
-                    # x_prev and x_curr in the y-range [y_low, y_high].
-                    # Since we only check adjacent ys in the current x, 
-                    # we know there are no points on the right edge between corners.
-                    if current_count == prev_count:
-                        area = (x - prev_x) * (y_high - y_low)
-                        if area > max_area:
-                            max_area = area
+                if pair in last_seen_pair:
+                    prev_x = last_seen_pair[pair]
+                    # The 'no point inside or on border' constraint is satisfied if 
+                    # the maximum x-coordinate in the range [y1, y2] is exactly prev_x.
+                    # This ensures no points exist at x_prev < x' < x_curr in this y-range,
+                    # and adjacency in the y-list ensures no points on vertical edges.
+                    if query(y1_idx, y2_idx) == prev_x:
+                        area = (x - prev_x) * (unique_y[y2_idx] - unique_y[y1_idx])
+                        if area > max_area: max_area = area
+                
+                last_seen_pair[pair] = x
             
-            # Step 2: Update Fenwick tree with all points at current x
-            for y in ys:
-                ft.update(y_rank[y], 1)
-                
-            # Step 3: Update last_seen with the count including current x points
-            for i in range(len(ys) - 1):
-                y_low, y_high = ys[i], ys[i+1]
-                last_seen[(y_low, y_high)] = (x, ft.query_range(y_rank[y_low], y_rank[y_high]))
+            # Update segment tree with current x for all y-coordinates at this x
+            for y_idx in y_indices:
+                update(y_idx, x)
                 
         return max_area
 # @lc code=end
