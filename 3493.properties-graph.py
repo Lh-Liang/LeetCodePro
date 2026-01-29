29cd1cffb2d1e4cd8f75bb9834a0639d9214dfb0
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 22:04:50 +0800

    Add solution: 3493.properties-graph.py

--- 3493.properties-graph.py
+++ 3493.properties-graph.py
@@ -9,20 +9,34 @@ class Solution:
     def numberOfComponents(self, properties: List[List[int]], k: int) -> int:
         n = len(properties)
         parent = list(range(n))
-        def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-            return parent[x]
-        def union(x, y):
-            px = find(x)
-            py = find(y)
-            if px != py:
-                parent[px] = py
+        
+        def find(i):
+            if parent[i] == i:
+                return i
+            parent[i] = find(parent[i])
+            return parent[i]
+            
+        def union(i, j):
+            root_i = find(i)
+            root_j = find(j)
+            if root_i != root_j:
+                parent[root_i] = root_j
+
+        # Pre-convert each property list to a set to count distinct integers efficiently
+        property_sets = [set(p) for p in properties]
+        
+        # Compare all pairs to build the graph components
         for i in range(n):
             for j in range(i + 1, n):
-                inter = len(set(properties[i]) & set(properties[j]))
-                if inter >= k:
+                # Intersection of sets gives the distinct common integers
+                if len(property_sets[i] & property_sets[j]) >= k:
                     union(i, j)
-        roots = set(find(i) for i in range(n))
-        return len(roots)
+        
+        # Count how many distinct roots exist in the DSU
+        num_components = 0
+        for i in range(n):
+            if parent[i] == i:
+                num_components += 1
+                
+        return num_components
 # @lc code=end
