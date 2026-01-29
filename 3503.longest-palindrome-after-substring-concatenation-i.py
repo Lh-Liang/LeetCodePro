3d084a463648e61b79552c90ea371fcdbc83e7d1
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 22:07:42 +0800

    Add solution: 3503.longest-palindrome-after-substring-concatenation-i.py

--- 3503.longest-palindrome-after-substring-concatenation-i.py
+++ 3503.longest-palindrome-after-substring-concatenation-i.py
@@ -7,15 +7,36 @@
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str, t: str) -> int:
-        max_len = 0
-        ns, nt = len(s), len(t)
-        for i in range(ns + 1):
-            for j in range(i, ns + 1):
-                for k in range(nt + 1):
-                    for l in range(k, nt + 1):
-                        candidate = s[i:j] + t[k:l]
-                        if candidate == candidate[::-1]:
-                            max_len = max(max_len, len(candidate))
-        return max_len
-
+        n, m = len(s), len(t)
+        ans = 0
+        
+        # Case 1: Core is in t. 
+        # Substring of t is B = X + Y, where X is a palindrome.
+        # If Y^R is a substring in s, we can form the palindrome Y^R + X + Y.
+        for i in range(m):
+            for j in range(i, m):
+                B = t[i:j+1]
+                for k in range(len(B) + 1):
+                    X = B[:k]  # Potential palindromic core
+                    if X == X[::-1]:
+                        Y = B[k:]
+                        # Check if the reverse of the suffix Y exists in s
+                        if Y[::-1] in s:
+                            ans = max(ans, 2 * len(Y) + len(X))
+                            
+        # Case 2: Core is in s.
+        # Substring of s is A = X + Y, where Y is a palindrome.
+        # If X^R is a substring in t, we can form the palindrome X + Y + X^R.
+        for i in range(n):
+            for j in range(i, n):
+                A = s[i:j+1]
+                for k in range(len(A) + 1):
+                    Y = A[k:]  # Potential palindromic core
+                    if Y == Y[::-1]:
+                        X = A[:k]
+                        # Check if the reverse of the prefix X exists in t
+                        if X[::-1] in t:
+                            ans = max(ans, 2 * len(X) + len(Y))
+                            
+        return ans
 # @lc code=end
