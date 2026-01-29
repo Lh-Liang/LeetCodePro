53fe246e32c18533d62fdd67bfb5bf35a5ed1be6
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 17:32:20 +0800

    Add solution: 3670.maximum-product-of-two-integers-with-no-common-bits.py

--- 3670.maximum-product-of-two-integers-with-no-common-bits.py
+++ 3670.maximum-product-of-two-integers-with-no-common-bits.py
@@ -7,51 +7,44 @@
 # @lc code=start
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        # The maximum value in nums is 10^6, which is less than 2^20 (1,048,576).
-        # We need 20 bits to represent all numbers.
-        NUM_BITS = 20
-        MAX_MASK = 1 << NUM_BITS
+        class TrieNode:
+            def __init__(self):
+                self.children = [None, None]
         
-        # dp[mask] will store the maximum number in 'nums' that is a submask of 'mask'.
-        # Initialize with 0.
-        dp = [0] * MAX_MASK
+        def insert(root, num):
+            node = root
+            for i in range(19, -1, -1):
+                bit = (num >> i) & 1
+                if node.children[bit] is None:
+                    node.children[bit] = TrieNode()
+                node = node.children[bit]
         
-        # Fill dp with the numbers present in the input.
-        # If a number x is present, it is a submask of itself.
+        def query(root, forbidden):
+            node = root
+            res = 0
+            for i in range(19, -1, -1):
+                bit_f = (forbidden >> i) & 1
+                if bit_f == 1:
+                    if node.children[0] is None:
+                        return 0
+                    node = node.children[0]
+                else:
+                    if node.children[1] is not None:
+                        res |= 1 << i
+                        node = node.children[1]
+                    elif node.children[0] is not None:
+                        node = node.children[0]
+                    else:
+                        return 0
+            return res
+        
+        nums.sort(reverse=True)
+        root = TrieNode()
+        ans = 0
         for num in nums:
-            dp[num] = max(dp[num], num)
-            
-        # Propagate the maximum values using SOS DP (Sum Over Subsets).
-        # For each bit position 'i', and each 'mask', if the 'i-th' bit is set in 'mask',
-        # then any submask of 'mask ^ (1 << i)' is also a submask of 'mask'.
-        # We want dp[mask] to be the max value among all submasks.
-        for i in range(NUM_BITS):
-            bit = 1 << i
-            for mask in range(MAX_MASK):
-                if mask & bit:
-                    # If the i-th bit is set, we can either keep it or turn it off.
-                    # Turning it off gives us the submask 'mask ^ bit'.
-                    # We take the max of the current value and the value from the submask.
-                    if dp[mask ^ bit] > dp[mask]:
-                        dp[mask] = dp[mask ^ bit]
-                        
-        max_prod = 0
-        full_mask = MAX_MASK - 1
-        
-        # Iterate through each number to find its best disjoint pair.
-        for num in nums:
-            # The complement contains all bits NOT set in num.
-            # Any number that is disjoint from 'num' must be a submask of 'complement'.
-            complement = full_mask ^ num
-            
-            # dp[complement] gives the largest number from the array that fits into the complement mask.
-            partner = dp[complement]
-            
-            # If partner is 0, it means no valid disjoint number was found (or only 0, which gives product 0).
-            current_prod = num * partner
-            if current_prod > max_prod:
-                max_prod = current_prod
-                
-        return max_prod
+            partner = query(root, num)
+            ans = max(ans, num * partner)
+            insert(root, num)
+        return ans
 
 # @lc code=end
