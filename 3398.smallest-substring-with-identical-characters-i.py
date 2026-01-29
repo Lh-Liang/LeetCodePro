7b3784a252ae42e3dfc837d79be2e9fc226ff00c
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 00:13:52 +0800

    Add solution: 3398.smallest-substring-with-identical-characters-i.py

--- 3398.smallest-substring-with-identical-characters-i.py
+++ 3398.smallest-substring-with-identical-characters-i.py
@@ -11,36 +11,34 @@ class Solution:
         
         def check(k):
             if k == 1:
-                # For k=1, the string must be alternating: 0101... or 1010...
-                cnt1 = 0 # flips for 0101...
-                cnt2 = 0 # flips for 1010...
+                # For k=1, the string must be strictly alternating.
+                # Compare s with "0101..." and "1010..."
+                pattern1_flips = 0 # Target: 010101...
+                pattern2_flips = 0 # Target: 101010...
                 for i in range(n):
-                    # Check against '010101...'
-                    if i % 2 == 0:
-                        if s[i] != '0': cnt1 += 1
-                        if s[i] != '1': cnt2 += 1
-                    else:
-                        if s[i] != '1': cnt1 += 1
-                        if s[i] != '0': cnt2 += 1
-                return min(cnt1, cnt2) <= numOps
+                    expected1 = str(i % 2)
+                    expected2 = str((i + 1) % 2)
+                    if s[i] != expected1:
+                        pattern1_flips += 1
+                    if s[i] != expected2:
+                        pattern2_flips += 1
+                return min(pattern1_flips, pattern2_flips) <= numOps
             
-            # For k > 1, use the greedy formula: floor(L / (k+1)) flips per run of length L
-            total_ops = 0
-            i = 0
-            while i < n:
-                j = i
-                while j < n and s[j] == s[i]:
-                    j += 1
-                length = j - i
-                total_ops += length // (k + 1)
-                i = j
-            return total_ops <= numOps
+            # For k >= 2, we can greedily break blocks of identical characters.
+            total_flips = 0
+            current_block_len = 1
+            for i in range(1, n):
+                if s[i] == s[i-1]:
+                    current_block_len += 1
+                else:
+                    total_flips += current_block_len // (k + 1)
+                    current_block_len = 1
+            total_flips += current_block_len // (k + 1)
+            return total_flips <= numOps
 
-        # Binary search for the minimum length k
         low = 1
         high = n
         ans = n
-        
         while low <= high:
             mid = (low + high) // 2
             if check(mid):
@@ -48,6 +46,5 @@ class Solution:
                 high = mid - 1
             else:
                 low = mid + 1
-        
         return ans
 # @lc code=end
