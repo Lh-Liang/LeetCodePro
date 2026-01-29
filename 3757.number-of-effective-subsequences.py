8a474c66b5b9c2ea536751ea42587849b9ac75b5
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 06:22:41 +0800

    Add solution: 3757.number-of-effective-subsequences.py

--- 3757.number-of-effective-subsequences.py
+++ 3757.number-of-effective-subsequences.py
@@ -5,60 +5,27 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def countEffective(self, nums: List[int]) -> int:
         MOD = 10**9 + 7
         n = len(nums)
-        total_or = 0
-        for x in nums:
-            total_or |= x
+        full_or = 0
+        for num in nums:
+            full_or |= num
         
-        if total_or == 0:
-            return 0
-            
-        # Identify relevant bits (those present in total_or)
-        bits = []
-        for i in range(21):
-            if (total_or >> i) & 1:
-                bits.append(i)
+        count = {0: 1}  # Start with empty set contributing to zero OR
+        for num in nums:
+            new_count = count.copy()
+            for existing_or in count:
+                new_or = existing_or | num
+                if new_or != full_or:  # Only consider if it reduces OR value
+                    if new_or in new_count:
+                        new_count[new_or] = (new_count[new_or] + count[existing_or]) % MOD
+                    else:
+                        new_count[new_or] = count[existing_or]
+            count = new_count
         
-        m = len(bits)
-        # count[mask] will store how many nums[i] have their relevant bits as a submask of mask
-        # We map the bits of total_or to 0...m-1
-        count = [0] * (1 << m)
-        for x in nums:
-            mask = 0
-            for i in range(m):
-                if (x >> bits[i]) & 1:
-                    mask |= (1 << i)
-            count[mask] += 1
-            
-        # SOS DP (Sum Over Subsets) to find f(mask): number of elements that are submasks of mask
-        for i in range(m):
-            for mask in range(1 << m):
-                if (mask >> i) & 1:
-                    count[mask] += count[mask ^ (1 << i)]
-        
-        # Power of 2 precomputation
-        pow2 = [1] * (n + 1)
-        for i in range(1, n + 1):
-            pow2[i] = (pow2[i-1] * 2) % MOD
-            
-        # Inclusion-Exclusion to find number of subsets whose OR is exactly total_or (mask (1<<m)-1)
-        ineffective_subsets_count = 0
-        full_mask = (1 << m) - 1
-        for mask in range(1 << m):
-            # Parity of bits missing from full_mask
-            bits_diff = bin(full_mask ^ mask).count('1')
-            term = pow2[count[mask]]
-            if bits_diff % 2 == 1:
-                ineffective_subsets_count = (ineffective_subsets_count - term) % MOD
-            else:
-                ineffective_subsets_count = (ineffective_subsets_count + term) % MOD
-        
-        # Total subsequences = 2^n
-        # Effective = Total - Ineffective
-        total_subsequences = pow2[n]
-        ans = (total_subsequences - ineffective_subsets_count) % MOD
-        return ans
+        return sum(count.values()) % MOD - 1 # Subtract one to not include full set OR itself
 # @lc code=end
