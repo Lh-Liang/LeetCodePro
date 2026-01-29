ca2e9ce75b47e7d1806fcd75273ce27fad8af771
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 05:18:23 +0800

    Add solution: 3441.minimum-cost-good-caption.py

--- 3441.minimum-cost-good-caption.py
+++ 3441.minimum-cost-good-caption.py
@@ -11,62 +11,30 @@ class Solution:
         if n < 3:
             return ""
         
-        inf = float('inf')
-        dp = [inf] * (n + 1)
-        best_char = [26] * (n + 1)
-        
-        dp[n] = 0
-        ords = [ord(c) - ord('a') for c in caption]
-        
-        # DP to find min cost
-        for i in range(n - 3, -1, -1):
-            for c_idx in range(25, -1, -1):
-                cost = 0
-                for k in range(1, 6):
-                    if i + k > n:
-                        break
-                    cost += abs(ords[i + k - 1] - c_idx)
-                    if k >= 3:
-                        res = cost + dp[i + k]
-                        if res < dp[i]:
-                            dp[i] = res
-                            best_char[i] = c_idx
-                        elif res == dp[i]:
-                            best_char[i] = c_idx # Lexicographically smaller character is checked last
-        
-        if dp[0] == inf:
-            return ""
-            
-        # Reconstruction
-        res = []
+        result = []
         i = 0
         while i < n:
-            char_idx = best_char[i]
-            char = chr(ord('a') + char_idx)
-            
-            # Determine the best length k among 3, 4, 5
-            best_k = -1
-            for k in [3, 4, 5]:
-                if i + k <= n:
-                    cost_k = sum(abs(ords[j] - char_idx) for j in range(i, i + k))
-                    if cost_k + dp[i + k] == dp[i]:
-                        if best_k == -1:
-                            best_k = k
-                        else:
-                            # Compare current best_k suffix vs this k suffix
-                            # Both start with 'char' * best_k. 
-                            # Next char of shorter one is best_char[i + best_k]
-                            # We want to keep the one that leads to smaller string
-                            next_c = best_char[i + best_k]
-                            if next_c < char_idx:
-                                # Shorter is better, stay with best_k
-                                pass
-                            else:
-                                # Longer (k) is better or same
-                                best_k = k
-            
-            res.append(char * best_k)
-            i += best_k
-            
-        return "".join(res)
+            j = i
+            while j < n and caption[j] == caption[i]:
+                j += 1
+            count = j - i
+            if count >= 3:
+                result.extend([caption[i]] * count)
+            else:
+                if i > 0 and j < n:
+                    prev_char = chr(ord(caption[i-1]) + 1) if caption[i-1] != 'z' else None
+                    next_char = chr(ord(caption[j]) - 1) if caption[j] != 'a' else None
+                    if prev_char and (not next_char or prev_char <= next_char):
+                        result.extend([prev_char] * (3 - count))
+                        result.extend([caption[i]] * count)
+                    elif next_char:
+                        result.extend([caption[i]] * count)
+                        result.extend([next_char] * (3 - count))
+                    else:
+                        return ""
+                else:
+                    return ""
+            i = j + max(0, (3 - count))
+        
+        return "".join(result)
 # @lc code=end
