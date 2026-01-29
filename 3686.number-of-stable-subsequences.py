07e48d5345434822e53eb78d593a1189e2498f6e
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 03:32:52 +0800

    Add solution: 3686.number-of-stable-subsequences.py

--- 3686.number-of-stable-subsequences.py
+++ 3686.number-of-stable-subsequences.py
@@ -8,29 +8,33 @@
 class Solution:
     def countStableSubsequences(self, nums: List[int]) -> int:
         MOD = 10**9 + 7
-        # e1: ends in 1 even, e2: ends in 2 evens
-        # o1: ends in 1 odd, o2: ends in 2 odds
-        e1, e2, o1, o2 = 0, 0, 0, 0
+        # e1: Ending with exactly one even number
+        # e2: Ending with exactly two even numbers
+        # o1: Ending with exactly one odd number
+        # o2: Ending with exactly two odd numbers
+        e1 = e2 = o1 = o2 = 0
         
         for x in nums:
             if x % 2 == 0:
-                # New subsequences ending in exactly two evens
-                # come from those previously ending in exactly one even
-                added_e2 = e1
-                # New subsequences ending in exactly one even
-                # come from those ending in any number of odds, plus the singleton [x]
-                added_e1 = (o1 + o2 + 1) % MOD
+                # New subsequences ending in exactly one even number:
+                # 1 (for [x] itself) + subsequences ending in odd(s)
+                gain_e1 = (o1 + o2 + 1) % MOD
+                # New subsequences ending in exactly two even numbers:
+                # Subsequences currently ending in exactly one even number
+                gain_e2 = e1
                 
-                e1 = (e1 + added_e1) % MOD
-                e2 = (e2 + added_e2) % MOD
+                e1 = (e1 + gain_e1) % MOD
+                e2 = (e2 + gain_e2) % MOD
             else:
-                # New subsequences ending in exactly two odds
-                added_o2 = o1
-                # New subsequences ending in exactly one odd
-                added_o1 = (e1 + e2 + 1) % MOD
+                # New subsequences ending in exactly one odd number:
+                # 1 (for [x] itself) + subsequences ending in even(s)
+                gain_o1 = (e1 + e2 + 1) % MOD
+                # New subsequences ending in exactly two odd numbers:
+                # Subsequences currently ending in exactly one odd number
+                gain_o2 = o1
                 
-                o1 = (o1 + added_o1) % MOD
-                o2 = (o2 + added_o2) % MOD
+                o1 = (o1 + gain_o1) % MOD
+                o2 = (o2 + gain_o2) % MOD
                 
         return (e1 + e2 + o1 + o2) % MOD
 # @lc code=end
