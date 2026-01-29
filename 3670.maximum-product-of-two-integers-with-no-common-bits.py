d32424e215e83300d35d5f512190003e7a327bfc
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 22:56:37 +0800

    Add solution: 3670.maximum-product-of-two-integers-with-no-common-bits.py

--- 3670.maximum-product-of-two-integers-with-no-common-bits.py
+++ 3670.maximum-product-of-two-integers-with-no-common-bits.py
@@ -5,46 +5,55 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        class TrieNode:
-            def __init__(self):
-                self.children = [None, None]
+        if not nums:
+            return 0
         
-        def insert(root, num):
-            node = root
-            for i in range(19, -1, -1):
-                bit = (num >> i) & 1
-                if node.children[bit] is None:
-                    node.children[bit] = TrieNode()
-                node = node.children[bit]
+        # Determine the number of bits needed based on the maximum value in nums
+        max_val = 0
+        for x in nums:
+            if x > max_val:
+                max_val = x
         
-        def query(root, forbidden):
-            node = root
-            res = 0
-            for i in range(19, -1, -1):
-                bit_f = (forbidden >> i) & 1
-                if bit_f == 1:
-                    if node.children[0] is None:
-                        return 0
-                    node = node.children[0]
-                else:
-                    if node.children[1] is not None:
-                        res |= 1 << i
-                        node = node.children[1]
-                    elif node.children[0] is not None:
-                        node = node.children[0]
-                    else:
-                        return 0
-            return res
+        if max_val == 0:
+            return 0
+            
+        k = max_val.bit_length()
+        limit = 1 << k
+        # f[mask] will store the maximum value in nums that is a subset of mask
+        f = [0] * limit
         
-        nums.sort(reverse=True)
-        root = TrieNode()
+        unique_nums = set()
+        for x in nums:
+            unique_nums.add(x)
+            if x > f[x]:
+                f[x] = x
+        
+        # SOS DP (Sum Over Subsets) variation to find Max Over Subsets
+        for i in range(k):
+            bit = 1 << i
+            for mask in range(limit):
+                if mask & bit:
+                    # If the subset excluding this bit has a larger value, update
+                    if f[mask ^ bit] > f[mask]:
+                        f[mask] = f[mask ^ bit]
+        
         ans = 0
-        for num in nums:
-            partner = query(root, num)
-            ans = max(ans, num * partner)
-            insert(root, num)
+        all_bits_mask = limit - 1
+        
+        # For each number, find the max value in the array that is a subset of its complement
+        for x in unique_nums:
+            complement = all_bits_mask ^ x
+            # We need to ensure we only use bits within the k-bit range
+            # However, all_bits_mask ^ x already handles this.
+            y = f[complement]
+            if y > 0:
+                prod = x * y
+                if prod > ans:
+                    ans = prod
+                    
         return ans
-
 # @lc code=end
