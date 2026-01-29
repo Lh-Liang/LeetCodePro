f544e0c056241d937fa3f337700a3acf843362a0
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 02:26:25 +0800

    Add solution: 3515.shortest-path-in-a-weighted-tree.py

--- 3515.shortest-path-in-a-weighted-tree.py
+++ 3515.shortest-path-in-a-weighted-tree.py
@@ -5,74 +5,81 @@
 #
 
 # @lc code=start
+import sys
+
 class Solution:
     def treeQueries(self, n: int, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
         adj = [[] for _ in range(n + 1)]
         for u, v, w in edges:
             adj[u].append((v, w))
             adj[v].append((u, w))
-        
-        parent = [0] * (n + 1)
+
         tin = [0] * (n + 1)
         tout = [0] * (n + 1)
-        edge_weight_at_node = [0] * (n + 1)
+        parent_edge_weight = [0] * (n + 1)
+        child_node_map = {}
         
-        # Iterative DFS to compute tin, tout, and parent-child mapping
+        # Iterative DFS to linearize the tree and map edges to child nodes
         timer = 0
-        stack = [(1, iter(adj[1]))]
+        stack = [(1, -1)]
+        adj_iter = [iter(adj[i]) for i in range(n + 1)]
+        
         timer += 1
         tin[1] = timer
         
         while stack:
-            u, children = stack[-1]
+            u, p = stack[-1]
             try:
-                v, w = next(children)
-                if v != parent[u]:
-                    parent[v] = u
-                    edge_weight_at_node[v] = w
+                v, w = next(adj_iter[u])
+                if v != p:
                     timer += 1
                     tin[v] = timer
-                    stack.append((v, iter(adj[v])))
+                    parent_edge_weight[v] = w
+                    # Map edge to the child node
+                    edge_key = (u, v) if u < v else (v, u)
+                    child_node_map[edge_key] = v
+                    stack.append((v, u))
             except StopIteration:
                 tout[u] = timer
                 stack.pop()
-        
-        # Binary Indexed Tree for range updates (difference array approach)
+
+        # BIT for Range Update (tin[v] to tout[v]) and Point Query (tin[x])
+        # We use a difference array approach: update(L, delta), update(R+1, -delta)
         bit = [0] * (n + 2)
+
         def bit_update(i, delta):
-            while i < n + 2:
+            while i <= n + 1:
                 bit[i] += delta
                 i += i & (-i)
-        
+
         def bit_query(i):
             s = 0
             while i > 0:
                 s += bit[i]
                 i -= i & (-i)
             return s
-        
-        # Initial edge weights as range updates in the BIT
-        for v in range(2, n + 1):
-            w = edge_weight_at_node[v]
-            bit_update(tin[v], w)
-            bit_update(tout[v] + 1, -w)
-        
+
+        # Initialize BIT with starting weights
+        for i in range(2, n + 1):
+            w = parent_edge_weight[i]
+            bit_update(tin[i], w)
+            bit_update(tout[i] + 1, -w)
+
         results = []
         for q in queries:
             if q[0] == 1:
                 u, v, w_new = q[1], q[2], q[3]
-                # Identify which node is the child in the edge (u, v)
-                child = v if parent[v] == u else u
-                old_w = edge_weight_at_node[child]
-                delta = w_new - old_w
-                # Update the distance for all nodes in the child's subtree
-                bit_update(tin[child], delta)
-                bit_update(tout[child] + 1, -delta)
-                edge_weight_at_node[child] = w_new
+                edge_key = (u, v) if u < v else (v, u)
+                c = child_node_map[edge_key]
+                delta = w_new - parent_edge_weight[c]
+                parent_edge_weight[c] = w_new
+                # Range update on subtree of child c
+                bit_update(tin[c], delta)
+                bit_update(tout[c] + 1, -delta)
             else:
                 x = q[1]
-                # Distance from root to x is the prefix sum of the difference array
+                # Point query: prefix sum in BIT gives path distance
                 results.append(bit_query(tin[x]))
-        
+
         return results
 # @lc code=end
