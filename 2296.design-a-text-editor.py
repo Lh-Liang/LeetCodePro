c579565aae485415e6581c119ae1e8b5bc9d26cb
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 15:57:11 +0800

    Add solution: 2296.design-a-text-editor.py

--- 2296.design-a-text-editor.py
+++ 2296.design-a-text-editor.py
@@ -1,37 +1,38 @@
+#
+# @lc app=leetcode id=2296 lang=python3
+#
+# [2296] Design a Text Editor
+#
+
+from collections import deque
+
+# @lc code=start
 class TextEditor:
 
     def __init__(self):
-        # Characters to the left of the cursor
         self.left = []
-        # Characters to the right of the cursor (top of stack is closest to cursor)
-        self.right = []
+        self.right = deque()
 
     def addText(self, text: str) -> None:
-        # Add characters to the left stack
         self.left.extend(text)
 
     def deleteText(self, k: int) -> int:
-        # Delete characters to the left of the cursor
-        num_to_delete = min(k, len(self.left))
-        for _ in range(num_to_delete):
+        cnt = min(k, len(self.left))
+        for _ in range(cnt):
             self.left.pop()
-        return num_to_delete
+        return cnt
 
     def cursorLeft(self, k: int) -> str:
-        # Move cursor left: move characters from left stack to right stack
-        num_to_move = min(k, len(self.left))
-        for _ in range(num_to_move):
-            self.right.append(self.left.pop())
-        # Return the last 10 characters to the left of the cursor
-        return "".join(self.left[-10:])
+        cnt = min(k, len(self.left))
+        for _ in range(cnt):
+            self.right.appendleft(self.left.pop())
+        return ''.join(self.left[-10:])
 
     def cursorRight(self, k: int) -> str:
-        # Move cursor right: move characters from right stack to left stack
-        num_to_move = min(k, len(self.right))
-        for _ in range(num_to_move):
-            self.left.append(self.right.pop())
-        # Return the last 10 characters to the left of the cursor
-        return "".join(self.left[-10:])
+        cnt = min(k, len(self.right))
+        for _ in range(cnt):
+            self.left.append(self.right.popleft())
+        return ''.join(self.left[-10:])
 
 
 # Your TextEditor object will be instantiated and called as such:
@@ -40,3 +41,4 @@ class TextEditor:
 # param_2 = obj.deleteText(k)
 # param_3 = obj.cursorLeft(k)
 # param_4 = obj.cursorRight(k)
+# @lc code=end
