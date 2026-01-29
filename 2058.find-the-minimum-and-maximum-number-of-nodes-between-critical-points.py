aeda63c281ccc261bb3e5a0c7abf51f8f019e691
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 00:05:39 +0800

    Add solution: 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py

--- 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -15,38 +15,35 @@ class Solution:
         if not head or not head.next or not head.next.next:
             return [-1, -1]
         
-        first_cp_idx = -1
-        prev_cp_idx = -1
+        first_idx = -1
+        last_idx = -1
         min_dist = float('inf')
         
-        # Start from the second node (index 1)
-        prev_val = head.val
+        prev = head
         curr = head.next
         idx = 1
         
         while curr.next:
-            next_val = curr.next.val
+            nxt = curr.next
+            # Check for critical point: local maxima or local minima
+            is_critical = (curr.val > prev.val and curr.val > nxt.val) or \
+                          (curr.val < prev.val and curr.val < nxt.val)
             
-            # Identify if current node is a critical point
-            is_maxima = curr.val > prev_val and curr.val > next_val
-            is_minima = curr.val < prev_val and curr.val < next_val
-            
-            if is_maxima or is_minima:
-                if first_cp_idx == -1:
-                    first_cp_idx = idx
+            if is_critical:
+                if first_idx == -1:
+                    first_idx = idx
                 else:
-                    min_dist = min(min_dist, idx - prev_cp_idx)
+                    min_dist = min(min_dist, idx - last_idx)
                 
-                prev_cp_idx = idx
+                last_idx = idx
             
-            prev_val = curr.val
-            curr = curr.next
+            prev = curr
+            curr = nxt
             idx += 1
             
-        # If we found fewer than 2 critical points
         if min_dist == float('inf'):
             return [-1, -1]
         
-        max_dist = prev_cp_idx - first_cp_idx
+        max_dist = last_idx - first_idx
         return [int(min_dist), int(max_dist)]
 # @lc code=end
