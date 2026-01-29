1552a3db03478b27da9a95dbeebdebf3e2480541
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 17:32:54 +0800

    Add solution: 3686.number-of-stable-subsequences.py

--- 3686.number-of-stable-subsequences.py
+++ 3686.number-of-stable-subsequences.py
@@ -8,36 +8,29 @@
 class Solution:
     def countStableSubsequences(self, nums: List[int]) -> int:
         MOD = 10**9 + 7
-        
-        # dp[0]: count of stable subsequences ending with 1 even number
-        # dp[1]: count of stable subsequences ending with 2 even numbers
-        # dp[2]: count of stable subsequences ending with 1 odd number
-        # dp[3]: count of stable subsequences ending with 2 odd numbers
-        dp = [0] * 4
-        
-        for x in nums:
-            if x % 2 == 0:
-                # Current number is Even
-                # Can form new subsequences ending in 2 evens by appending to those ending in 1 even
-                added_to_1_even = dp[0]
-                
-                # Can form new subsequences ending in 1 even by appending to those ending in odds
-                # Plus the single element subsequence [x]
-                added_to_odds = (dp[2] + dp[3] + 1) % MOD
-                
-                dp[1] = (dp[1] + added_to_1_even) % MOD
-                dp[0] = (dp[0] + added_to_odds) % MOD
-            else:
-                # Current number is Odd
-                # Can form new subsequences ending in 2 odds by appending to those ending in 1 odd
-                added_to_1_odd = dp[2]
-                
-                # Can form new subsequences ending in 1 odd by appending to those ending in evens
-                # Plus the single element subsequence [x]
-                added_to_evens = (dp[0] + dp[1] + 1) % MOD
-                
-                dp[3] = (dp[3] + added_to_1_odd) % MOD
-                dp[2] = (dp[2] + added_to_evens) % MOD
-                
-        return sum(dp) % MOD
+        n = len(nums)
+        parity = [num % 2 for num in nums]
+        prev = [[0] * 2 for _ in range(2)]
+        for i in range(n):
+            p = parity[i]
+            curr = [[0] * 2 for _ in range(2)]
+            for s in range(2):
+                for pp in range(2):
+                    curr[s][pp] = (curr[s][pp] + prev[s][pp]) % MOD
+            curr[0][p] = (curr[0][p] + 1) % MOD
+            for ps in range(2):
+                plen = ps + 1
+                for pp in range(2):
+                    ways = prev[ps][pp]
+                    if ways == 0:
+                        continue
+                    if pp != p:
+                        curr[0][p] = (curr[0][p] + ways) % MOD
+                    else:
+                        nlen = plen + 1
+                        if nlen <= 2:
+                            ns = nlen - 1
+                            curr[ns][p] = (curr[ns][p] + ways) % MOD
+            prev = curr
+        return sum(sum(row) for row in prev) % MOD
 # @lc code=end
