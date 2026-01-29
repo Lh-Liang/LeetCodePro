258fd118f630b3a0db2532d3a1fdb5b99a591567
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 03:38:55 +0800

    Add solution: 3721.longest-balanced-subarray-ii.py

--- 3721.longest-balanced-subarray-ii.py
+++ 3721.longest-balanced-subarray-ii.py
@@ -5,53 +5,82 @@
 #
 
 # @lc code=start
+import sys
+
 class Solution:
     def longestBalanced(self, nums: List[int]) -> int:
         n = len(nums)
-        def check(L: int) -> bool:
-            even_cnt = {}
-            odd_cnt = {}
-            dist_e = dist_o = 0
-            # first window [0, L)
-            for i in range(L):
-                num = nums[i]
-                cnt = even_cnt if num & 1 == 0 else odd_cnt
-                cnt[num] = cnt.get(num, 0) + 1
-                if cnt[num] == 1:
-                    if num & 1 == 0:
-                        dist_e += 1
-                    else:
-                        dist_o += 1
-            if dist_e == dist_o:
-                return True
-            for start in range(1, n - L + 1):
-                num_out = nums[start - 1]
-                cnt_out = even_cnt if num_out & 1 == 0 else odd_cnt
-                cnt_out[num_out] -= 1
-                if cnt_out[num_out] == 0:
-                    if num_out & 1 == 0:
-                        dist_e -= 1
-                    else:
-                        dist_o -= 1
-                num_in = nums[start + L - 1]
-                cnt_in = even_cnt if num_in & 1 == 0 else odd_cnt
-                cnt_in[num_in] = cnt_in.get(num_in, 0) + 1
-                if cnt_in[num_in] == 1:
-                    if num_in & 1 == 0:
-                        dist_e += 1
-                    else:
-                        dist_o += 1
-                if dist_e == dist_o:
-                    return True
-            return False
-        lo = 0
-        hi = n
-        while lo < hi:
-            mid = (lo + hi + 1) // 2
-            if check(mid):
-                lo = mid
-            else:
-                hi = mid - 1
-        return lo
+        # Using a flat array for segment tree to reduce overhead
+        # tree_min[v] and tree_max[v] store the range of |E| - |O|
+        tree_min = [0] * (4 * n)
+        tree_max = [0] * (4 * n)
+        lazy = [0] * (4 * n)
 
+        def update(v, tl, tr, l, r, add):
+            if l > r:
+                return
+            if l == tl and r == tr:
+                tree_min[v] += add
+                tree_max[v] += add
+                lazy[v] += add
+            else:
+                # Push lazy
+                if lazy[v] != 0:
+                    lazy[2*v] += lazy[v]
+                    tree_min[2*v] += lazy[v]
+                    tree_max[2*v] += lazy[v]
+                    lazy[2*v+1] += lazy[v]
+                    tree_min[2*v+1] += lazy[v]
+                    tree_max[2*v+1] += lazy[v]
+                    lazy[v] = 0
+                
+                tm = (tl + tr) // 2
+                update(2*v, tl, tm, l, min(r, tm), add)
+                update(2*v+1, tm+1, tr, max(l, tm+1), r, add)
+                tree_min[v] = min(tree_min[2*v], tree_min[2*v+1])
+                tree_max[v] = max(tree_max[2*v], tree_max[2*v+1])
+
+        def find_first_zero(v, tl, tr, r_bound):
+            if tree_min[v] > 0 or tree_max[v] < 0:
+                return -1
+            if tl == tr:
+                return tl
+            
+            # Push lazy
+            if lazy[v] != 0:
+                lazy[2*v] += lazy[v]
+                tree_min[2*v] += lazy[v]
+                tree_max[2*v] += lazy[v]
+                lazy[2*v+1] += lazy[v]
+                tree_min[2*v+1] += lazy[v]
+                tree_max[2*v+1] += lazy[v]
+                lazy[v] = 0
+                
+            tm = (tl + tr) // 2
+            res = find_first_zero(2*v, tl, tm, r_bound)
+            if res == -1 and tm + 1 <= r_bound:
+                res = find_first_zero(2*v+1, tm+1, tr, r_bound)
+            return res
+
+        last_pos = {}
+        max_len = 0
+        
+        # Set recursion limit for deep trees
+        sys.setrecursionlimit(200000)
+
+        for j in range(n):
+            val = nums[j]
+            prev = last_pos.get(val, -1)
+            diff = 1 if val % 2 == 0 else -1
+            
+            # Update range [prev + 1, j]
+            update(1, 0, n - 1, prev + 1, j, diff)
+            last_pos[val] = j
+            
+            # Find leftmost i in [0, j] where f(i) == 0
+            first_i = find_first_zero(1, 0, n - 1, j)
+            if first_i != -1:
+                max_len = max(max_len, j - first_i + 1)
+                
+        return max_len
 # @lc code=end
