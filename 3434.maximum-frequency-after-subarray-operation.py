43ab6e340cb85faec84aba8fa806531a223779fa
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 00:17:44 +0800

    Add solution: 3434.maximum-frequency-after-subarray-operation.py

--- 3434.maximum-frequency-after-subarray-operation.py
+++ 3434.maximum-frequency-after-subarray-operation.py
@@ -7,32 +7,39 @@
 # @lc code=start
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        # Initial frequency of k
-        total_k = nums.count(k)
-        
-        # We want to maximize: total_k + (count of v in subarray - count of k in subarray)
-        # This is equivalent to Kadane's where v is +1 and k is -1.
-        # Since values are small (1-50), we can track the max gain for each value v.
-        
-        # max_gain[v] stores the maximum gain found so far using value v
-        # current_gain[v] stores the running Kadane sum for value v
-        max_gain = [0] * 51
-        current_gain = [0] * 51
-        
+        # Count original occurrences of k
+        base_k = 0
         for x in nums:
             if x == k:
-                # If we encounter k, it's a -1 for all potential values v
-                for v in range(1, 51):
-                    if v == k: continue
-                    current_gain[v] -= 1
-                    # If current_gain drops below 0, reset (standard Kadane's)
-                    if current_gain[v] < 0:
-                        current_gain[v] = 0
-            else:
-                # If we encounter x (where x != k), it's a +1 for value x
-                current_gain[x] += 1
-                if current_gain[x] > max_gain[x]:
-                    max_gain[x] = current_gain[x]
+                base_k += 1
         
-        return total_k + max(max_gain)
+        max_gain = 0
+        # Constraints say nums[i] is between 1 and 50.
+        # We iterate through all possible values that could be changed to k.
+        unique_vals = set(nums)
+        
+        for v in unique_vals:
+            if v == k:
+                continue
+            
+            current_gain = 0
+            best_v_gain = 0
+            
+            # Kadane's algorithm to maximize: (count of v) - (count of k)
+            for x in nums:
+                if x == v:
+                    current_gain += 1
+                elif x == k:
+                    current_gain -= 1
+                
+                if current_gain < 0:
+                    current_gain = 0
+                
+                if current_gain > best_v_gain:
+                    best_v_gain = current_gain
+            
+            if best_v_gain > max_gain:
+                max_gain = best_v_gain
+                
+        return base_k + max_gain
 # @lc code=end
