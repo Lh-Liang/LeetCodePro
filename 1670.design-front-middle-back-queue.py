dbf6eccf7802577dc35b507112d3212b5d1b8286
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 23:20:29 +0800

    Add solution: 1670.design-front-middle-back-queue.py

--- 1670.design-front-middle-back-queue.py
+++ 1670.design-front-middle-back-queue.py
@@ -10,33 +10,32 @@ from collections import deque
 class FrontMiddleBackQueue:
 
     def __init__(self):
-        # Invariant: len(left) == len(right) OR len(right) == len(left) + 1
+        # Invariant: len(left) == len(right) or len(left) + 1 == len(right)
         self.left = deque()
         self.right = deque()
 
-    def _rebalance(self):
-        # Case 1: Left is too long
+    def _balance(self):
         if len(self.left) > len(self.right):
             self.right.appendleft(self.left.pop())
-        # Case 2: Right is too long (more than 1 greater than left)
         elif len(self.right) > len(self.left) + 1:
             self.left.append(self.right.popleft())
 
     def pushFront(self, val: int) -> None:
         self.left.appendleft(val)
-        self._rebalance()
+        self._balance()
 
     def pushMiddle(self, val: int) -> None:
-        if len(self.left) == len(self.right):
-            # Adding to right keeps it at len(left) + 1
-            self.right.appendleft(val)
-        else:
-            # Right was len(left) + 1, adding to left makes them equal
+        if len(self.left) < len(self.right):
             self.left.append(val)
+        else:
+            self.right.appendleft(val)
+        # Balance not strictly needed here if logic is precise, 
+        # but calling it ensures invariant safety.
+        self._balance()
 
     def pushBack(self, val: int) -> None:
         self.right.append(val)
-        self._rebalance()
+        self._balance()
 
     def popFront(self) -> int:
         if not self.right:
@@ -45,35 +44,24 @@ class FrontMiddleBackQueue:
             res = self.right.popleft()
         else:
             res = self.left.popleft()
-        self._rebalance()
+        self._balance()
         return res
 
     def popMiddle(self) -> int:
         if not self.right:
             return -1
         if len(self.left) == len(self.right):
-            # Even size: [1, 2 | 3, 4] -> middle is 2 (frontmost)
             res = self.left.pop()
         else:
-            # Odd size: [1, 2 | 3, 4, 5] -> middle is 3
             res = self.right.popleft()
-        self._rebalance()
+        self._balance()
         return res
 
     def popBack(self) -> int:
         if not self.right:
             return -1
         res = self.right.pop()
-        self._rebalance()
+        self._balance()
         return res
 
-
-# Your FrontMiddleBackQueue object will be instantiated and called as such:
-# obj = FrontMiddleBackQueue()
-# obj.pushFront(val)
-# obj.pushMiddle(val)
-# obj.pushBack(val)
-# param_4 = obj.popFront()
-# param_5 = obj.popMiddle()
-# param_6 = obj.popBack()
 # @lc code=end
