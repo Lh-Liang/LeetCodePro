8c0d33d5f3018d751fadc54be2739f020e5ca82f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 16:08:01 +0800

    Add solution: 3341.find-minimum-time-to-reach-last-room-i.py

--- 3341.find-minimum-time-to-reach-last-room-i.py
+++ 3341.find-minimum-time-to-reach-last-room-i.py
@@ -5,44 +5,27 @@
 #
 
 # @lc code=start
-import heapq
-from typing import List
-
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        import heapq
         n, m = len(moveTime), len(moveTime[0])
-        
-        # dist[i][j] will store the minimum time to reach room (i, j)
-        dist = [[float('inf')] * m for _ in range(n)]
+        INF = 10**18
+        dist = [[INF] * m for _ in range(n)]
         dist[0][0] = 0
-        
-        # Priority queue stores (current_time, row, col)
-        pq = [(0, 0, 0)]
-        
+        pq = []
+        heapq.heappush(pq, (0, 0, 0))  # time, r, c
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         while pq:
-            t, r, c = heapq.heappop(pq)
-            
-            # If we already found a shorter path to this room, skip it
-            if t > dist[r][c]:
+            time, r, c = heapq.heappop(pq)
+            if time > dist[r][c]:
                 continue
-            
-            # If we reached the target room, return the time
-            if r == n - 1 and c == m - 1:
-                return t
-            
-            # Check all 4 adjacent rooms
-            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            for dr, dc in dirs:
                 nr, nc = r + dr, c + dc
-                
                 if 0 <= nr < n and 0 <= nc < m:
-                    # The room (nr, nc) opens at moveTime[nr][nc].
-                    # We can only start moving to it at max(t, moveTime[nr][nc]).
-                    # The move itself takes exactly 1 second.
-                    new_time = max(t, moveTime[nr][nc]) + 1
-                    
+                    new_time = max(time, moveTime[nr][nc]) + 1
                     if new_time < dist[nr][nc]:
                         dist[nr][nc] = new_time
                         heapq.heappush(pq, (new_time, nr, nc))
-        
-        return -1
+        return dist[n-1][m-1]
+
 # @lc code=end
