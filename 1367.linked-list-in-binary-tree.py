dcd1887b030be5247cd14f1b11e2e7c96474aa2f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 15:56:39 +0800

    Add solution: 1367.linked-list-in-binary-tree.py

--- 1367.linked-list-in-binary-tree.py
+++ 1367.linked-list-in-binary-tree.py
@@ -1,23 +1,36 @@
+#
+# @lc app=leetcode id=1367 lang=python3
+#
+# [1367] Linked List in Binary Tree
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-        if not root:
-            return False
+        def dfs(node, lnode):
+            if not lnode:
+                return True
+            if not node or node.val != lnode.val:
+                return False
+            return dfs(node.left, lnode.next) or dfs(node.right, lnode.next)
         
-        # Check if a matching path starts at the current root,
-        # or if it exists starting from any node in the left or right subtrees.
-        return self.checkPath(head, root) or \
-               self.isSubPath(head, root.left) or \
-               self.isSubPath(head, root.right)
-
-    def checkPath(self, head: Optional[ListNode], node: Optional[TreeNode]) -> bool:
-        # If we have reached the end of the linked list, we found a match.
-        if not head:
-            return True
-        # If the tree ends before the linked list is fully matched.
-        if not node:
-            return False
-        # If the values do not match, this path is not a valid match.
-        if head.val != node.val:
-            return False
-        # Continue searching for the next node in the list within the tree's children.
-        return self.checkPath(head.next, node.left) or self.checkPath(head.next, node.right)
+        def traverse(node):
+            if not node:
+                return False
+            if node.val == head.val and dfs(node, head):
+                return True
+            return traverse(node.left) or traverse(node.right)
+        
+        return traverse(root)
+# @lc code=end
