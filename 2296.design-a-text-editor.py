a252ab0e57f879c555fc03e36400c59370b07a40
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 23:14:05 +0800

    Add solution: 2296.design-a-text-editor.py

--- 2296.design-a-text-editor.py
+++ 2296.design-a-text-editor.py
@@ -10,39 +10,39 @@ class TextEditor:
     def __init__(self):
         # Characters to the left of the cursor
         self.left = []
-        # Characters to the right of the cursor (stored in reverse order)
+        # Characters to the right of the cursor (top of stack is closest to cursor)
         self.right = []
 
     def addText(self, text: str) -> None:
-        # Add characters to the left stack; cursor ends to the right of text
+        # O(L) where L is length of text
         for char in text:
             self.left.append(char)
 
     def deleteText(self, k: int) -> int:
-        # Delete up to k characters to the left of the cursor
+        # O(k)
         count = 0
         while k > 0 and self.left:
             self.left.pop()
-            count += 1
             k -= 1
+            count += 1
         return count
 
     def cursorLeft(self, k: int) -> str:
-        # Move cursor left: transfer chars from left to right stack
+        # O(k) - shift characters from left stack to right stack
         while k > 0 and self.left:
             self.right.append(self.left.pop())
             k -= 1
         # Return last min(10, len) characters to the left of cursor
-        return ''.join(self.left[-10:])
+        # Slicing the last 10 elements takes O(1) time
+        return "".join(self.left[-10:])
 
     def cursorRight(self, k: int) -> str:
-        # Move cursor right: transfer chars from right to left stack
+        # O(k) - shift characters from right stack to left stack
         while k > 0 and self.right:
             self.left.append(self.right.pop())
             k -= 1
         # Return last min(10, len) characters to the left of cursor
-        return ''.join(self.left[-10:])
-
+        return "".join(self.left[-10:])
 
 # Your TextEditor object will be instantiated and called as such:
 # obj = TextEditor()
