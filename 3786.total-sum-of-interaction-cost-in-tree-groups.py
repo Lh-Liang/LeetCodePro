fe7305771451fd35c205da772443ceccad4a6eed
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 04:08:21 +0800

    Add solution: 3786.total-sum-of-interaction-cost-in-tree-groups.py

--- 3786.total-sum-of-interaction-cost-in-tree-groups.py
+++ 3786.total-sum-of-interaction-cost-in-tree-groups.py
@@ -5,29 +5,52 @@
 #
 
 # @lc code=start
+import sys
+
+# Increase recursion depth for deep trees
+sys.setrecursionlimit(200000)
+
 class Solution:
-    def interactionCosts(self, n: int, edges: List[List[int]], group: List[int]) -> int:
-        import sys
-        sys.setrecursionlimit(10**5 + 10)
+    def interactionCosts(self, n: int, edges: list[list[int]], group: list[int]) -> int:
         adj = [[] for _ in range(n)]
-        for a, b in edges:
-            adj[a].append(b)
-            adj[b].append(a)
-        total = [0] * 21
-        for i in range(n):
-            total[group[i]] += 1
-        res = [0]
-        def dfs(node, par):
-            sub = [0] * 21
-            sub[group[node]] = 1
-            for nei in adj[node]:
-                if nei != par:
-                    subnei = dfs(nei, node)
-                    for g in range(1, 21):
-                        cnt = subnei[g]
-                        res[0] += cnt * (total[g] - cnt)
-                        sub[g] += cnt
-            return sub
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        # Identify unique groups and map them to 0-indexed values
+        unique_groups = sorted(list(set(group)))
+        group_map = {g: i for i, g in enumerate(unique_groups)}
+        num_groups = len(unique_groups)
+        
+        # Total nodes in each group
+        group_totals = [0] * num_groups
+        for g in group:
+            group_totals[group_map[g]] += 1
+            
+        total_interaction_cost = 0
+        
+        # subtree_counts[u][g] will store number of nodes of group g in subtree of u
+        # To save memory, we calculate contribution on the fly during DFS
+        def dfs(u, p):
+            nonlocal total_interaction_cost
+            # current_counts stores counts for the current subtree
+            current_counts = [0] * num_groups
+            current_counts[group_map[group[u]]] = 1
+            
+            for v in adj[u]:
+                if v == p:
+                    continue
+                child_counts = dfs(v, u)
+                for g_idx in range(num_groups):
+                    # Edge (u, v) contribution for group g_idx:
+                    # nodes_in_v_subtree * nodes_outside_v_subtree
+                    count_in = child_counts[g_idx]
+                    count_out = group_totals[g_idx] - count_in
+                    total_interaction_cost += count_in * count_out
+                    current_counts[g_idx] += count_in
+            
+            return current_counts
+
         dfs(0, -1)
-        return res[0]
+        return total_interaction_cost
 # @lc code=end
