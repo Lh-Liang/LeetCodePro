7f3abac6140d750db4137f642ff202a6a93bcc1a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 21:24:49 +0800

    Add solution: 2296.design-a-text-editor.py

--- 2296.design-a-text-editor.py
+++ 2296.design-a-text-editor.py
@@ -4,36 +4,44 @@
 # [2296] Design a Text Editor
 #
 
-from collections import deque
-
 # @lc code=start
 class TextEditor:
 
     def __init__(self):
+        # Characters to the left of the cursor
         self.left = []
-        self.right = deque()
+        # Characters to the right of the cursor (stored in reverse order)
+        self.right = []
 
     def addText(self, text: str) -> None:
-        self.left.extend(text)
+        # O(len(text)) operation
+        for char in text:
+            self.left.append(char)
 
     def deleteText(self, k: int) -> int:
-        cnt = min(k, len(self.left))
-        for _ in range(cnt):
+        # O(k) operation: remove up to k characters from the left stack
+        count = 0
+        while k > 0 and self.left:
             self.left.pop()
-        return cnt
+            k -= 1
+            count += 1
+        return count
 
     def cursorLeft(self, k: int) -> str:
-        cnt = min(k, len(self.left))
-        for _ in range(cnt):
-            self.right.appendleft(self.left.pop())
-        return ''.join(self.left[-10:])
+        # O(k) operation: move up to k characters from left to right stack
+        while k > 0 and self.left:
+            self.right.append(self.left.pop())
+            k -= 1
+        # Return last 10 characters to the left of the cursor
+        return "".join(self.left[-10:])
 
     def cursorRight(self, k: int) -> str:
-        cnt = min(k, len(self.right))
-        for _ in range(cnt):
-            self.left.append(self.right.popleft())
-        return ''.join(self.left[-10:])
-
+        # O(k) operation: move up to k characters from right to left stack
+        while k > 0 and self.right:
+            self.left.append(self.right.pop())
+            k -= 1
+        # Return last 10 characters to the left of the cursor
+        return "".join(self.left[-10:])
 
 # Your TextEditor object will be instantiated and called as such:
 # obj = TextEditor()
