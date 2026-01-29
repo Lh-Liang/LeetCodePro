bc8eb9641e3232fa369ef1d439d54b9437437e52
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 05:35:05 +0800

    Add solution: 3503.longest-palindrome-after-substring-concatenation-i.py

--- 3503.longest-palindrome-after-substring-concatenation-i.py
+++ 3503.longest-palindrome-after-substring-concatenation-i.py
@@ -7,20 +7,22 @@
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str, t: str) -> int:
-        n, m = len(s), len(t)
-        max_len = 0
+        def expand_around_center(left, right, str1, str2):
+            max_len = 0
+            while left >= 0 and right < len(str2) and str1[left] == str2[right]:
+                max_len = max(max_len, right - left + 1)
+                left -= 1
+                right += 1
+            return max_len
         
-        # Pre-calculate substrings to avoid repeated slicing in nested loops
-        s_subs = [s[i:j] for i in range(n + 1) for j in range(i, n + 1)]
-        t_subs = [t[i:j] for i in range(m + 1) for j in range(i, m + 1)]
-        
-        for sub_s in s_subs:
-            for sub_t in t_subs:
-                combined_len = len(sub_s) + len(sub_t)
-                if combined_len > max_len:
-                    combined = sub_s + sub_t
-                    if combined == combined[::-1]:
-                        max_len = combined_len
-        
-        return max_len
+        max_length = 0
+        # Check all centers in s concatenated with t reversed (to exploit symmetry)
+        for i in range(len(s)):
+            for j in range(len(t)):
+                # Expand around center between s[i] and t[j]
+                max_length = max(max_length, expand_around_center(i, len(t) - j - 1, s, t[::-1]))
+                # Expand around center between s[i:i+2] and t[j:j+2], if valid (even length palindromes)
+                if i + 1 < len(s) and j + 1 < len(t):
+                    max_length = max(max_length, expand_around_center(i + 1, len(t) - j - 2, s[:i+2], t[::-1][j:j+2]))
+        return max_length
 # @lc code=end
