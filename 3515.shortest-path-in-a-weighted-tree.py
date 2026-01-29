74d8fc0f50464aa20bdc6d1803be4756a582096a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 22:23:42 +0800

    Add solution: 3515.shortest-path-in-a-weighted-tree.py

--- 3515.shortest-path-in-a-weighted-tree.py
+++ 3515.shortest-path-in-a-weighted-tree.py
@@ -7,79 +7,72 @@
 # @lc code=start
 class Solution:
     def treeQueries(self, n: int, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
-        class FenwickTree:
-            def __init__(self, size: int):
-                self.size = size
-                self.tree = [0] * (size + 2)
-
-            def update(self, index: int, delta: int) -> None:
-                while index <= self.size:
-                    self.tree[index] += delta
-                    index += index & -index
-
-            def query(self, index: int) -> int:
-                res = 0
-                while index > 0:
-                    res += self.tree[index]
-                    index -= index & -index
-                return res
-
-        import sys
-        sys.setrecursionlimit(100010)
-
         adj = [[] for _ in range(n + 1)]
-        weights = {}
         for u, v, w in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-            key = tuple(sorted((u, v)))
-            weights[key] = w
-
-        parent = [-1] * (n + 1)
-        intime = [0] * (n + 1)
-        outtime = [0] * (n + 1)
-        timer = [1]
-
-        def dfs(node: int, par: int) -> None:
-            parent[node] = par
-            intime[node] = timer[0]
-            timer[0] += 1
-            for nei in adj[node]:
-                if nei != par:
-                    dfs(nei, node)
-            outtime[node] = timer[0]
-            timer[0] += 1
-
-        dfs(1, -1)
-
-        ft = FenwickTree(timer[0])
-
-        for node in range(2, n + 1):
-            par = parent[node]
-            key = tuple(sorted((par, node)))
-            w = weights[key]
-            ft.update(intime[node], w)
-            ft.update(outtime[node], -w)
-
-        ans = []
+            adj[u].append((v, w))
+            adj[v].append((u, w))
+        
+        parent = [0] * (n + 1)
+        tin = [0] * (n + 1)
+        tout = [0] * (n + 1)
+        edge_weight_at_node = [0] * (n + 1)
+        
+        # Iterative DFS to compute tin, tout, and parent-child mapping
+        timer = 0
+        stack = [(1, iter(adj[1]))]
+        timer += 1
+        tin[1] = timer
+        
+        while stack:
+            u, children = stack[-1]
+            try:
+                v, w = next(children)
+                if v != parent[u]:
+                    parent[v] = u
+                    edge_weight_at_node[v] = w
+                    timer += 1
+                    tin[v] = timer
+                    stack.append((v, iter(adj[v])))
+            except StopIteration:
+                tout[u] = timer
+                stack.pop()
+        
+        # Binary Indexed Tree for range updates (difference array approach)
+        bit = [0] * (n + 2)
+        def bit_update(i, delta):
+            while i < n + 2:
+                bit[i] += delta
+                i += i & (-i)
+        
+        def bit_query(i):
+            s = 0
+            while i > 0:
+                s += bit[i]
+                i -= i & (-i)
+            return s
+        
+        # Initial edge weights as range updates in the BIT
+        for v in range(2, n + 1):
+            w = edge_weight_at_node[v]
+            bit_update(tin[v], w)
+            bit_update(tout[v] + 1, -w)
+        
+        results = []
         for q in queries:
             if q[0] == 1:
-                u, v, wnew = q[1], q[2], q[3]
-                if parent[v] == u:
-                    child = v
-                    par_node = u
-                else:
-                    child = u
-                    par_node = v
-                key = tuple(sorted((par_node, child)))
-                oldw = weights[key]
-                delta = wnew - oldw
-                weights[key] = wnew
-                ft.update(intime[child], delta)
-                ft.update(outtime[child], -delta)
+                u, v, w_new = q[1], q[2], q[3]
+                # Identify which node is the child in the edge (u, v)
+                child = v if parent[v] == u else u
+                old_w = edge_weight_at_node[child]
+                delta = w_new - old_w
+                # Update the distance for all nodes in the child's subtree
+                bit_update(tin[child], delta)
+                bit_update(tout[child] + 1, -delta)
+                edge_weight_at_node[child] = w_new
             else:
                 x = q[1]
-                ans.append(ft.query(intime[x]))
-        return ans
-
+                # Distance from root to x is the prefix sum of the difference array
+                results.append(bit_query(tin[x]))
+        
+        return results
 # @lc code=end
