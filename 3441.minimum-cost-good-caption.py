eeba202ede898e65ddb3d67e56cb339fe1e36b8b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 00:30:11 +0800

    Add solution: 3441.minimum-cost-good-caption.py

--- 3441.minimum-cost-good-caption.py
+++ 3441.minimum-cost-good-caption.py
@@ -3,6 +3,7 @@
 #
 # [3441] Minimum Cost Good Caption
 #
+
 # @lc code=start
 class Solution:
     def minCostGoodCaption(self, caption: str) -> str:
@@ -10,79 +11,77 @@ class Solution:
         if n < 3:
             return ""
 
-        # Precompute prefix sums for costs to convert to each character 'a'-'z'
-        costs = [[0] * (n + 1) for _ in range(26)]
-        for c_idx in range(26):
-            target_char_code = ord('a') + c_idx
-            s = 0
+        # Precompute prefix costs for each character a-z
+        # cost_pref[c][i] is the cost to change caption[0...i-1] to char c
+        cost_pref = [[0] * (n + 1) for _ in range(26)]
+        for c in range(26):
+            target_val = ord('a') + c
             for i in range(n):
-                s += abs(ord(caption[i]) - target_char_code)
-                costs[c_idx][i + 1] = s
+                cost_pref[c][i+1] = cost_pref[c][i] + abs(ord(caption[i]) - target_val)
 
-        def get_cost(c_idx, start, length):
-            return costs[c_idx][start + length] - costs[c_idx][start]
+        def get_cost(i, L, c_idx):
+            return cost_pref[c_idx][i + L] - cost_pref[c_idx][i]
 
+        # dp[i] = (min_cost, best_char_idx, best_length)
         inf = float('inf')
-        dp = [inf] * (n + 1)
-        dp[n] = 0
-        
-        # best_trans[i] = (char_index, block_length)
-        best_trans = [None] * (n + 1)
-        # first_char[i] = char_index of the first block in optimal suffix i
-        first_char = [None] * (n + 1)
-        # diff_char[i] = char_index of the first character in optimal suffix i that != first_char[i]
-        diff_char = [None] * (n + 1)
+        dp = [(inf, 26, 0)] * (n + 1)
+        dp[n] = (0, 26, 0) # Base case: end of string
 
         for i in range(n - 3, -1, -1):
+            best_cost = inf
+            best_c = 26
+            best_L = 0
+
             for c in range(26):
-                for k in (3, 4, 5):
-                    if i + k > n:
+                # Check possible lengths that could start a new block
+                for L in (3, 4, 5):
+                    if i + L > n:
                         break
                     
-                    cost_ik = get_cost(c, i, k) + dp[i + k]
-                    if cost_ik > dp[i]:
+                    res_cost, next_c, _ = dp[i + L]
+                    if res_cost == inf:
                         continue
                     
+                    current_total = get_cost(i, L, c) + res_cost
+                    
+                    # Comparison logic for lexicographical smallest string
                     is_better = False
-                    if cost_ik < dp[i]:
+                    if current_total < best_cost:
                         is_better = True
-                    else:
-                        # Tie-break: Lexicographical comparison of (c*k + Suffix[i+k])
-                        old_c, old_k = best_trans[i]
-                        if c < old_c:
+                    elif current_total == best_cost:
+                        if c < best_c:
                             is_better = True
-                        elif c == old_c:
-                            # Compare shorter vs longer block when character is same
-                            k_min, k_max = (k, old_k) if k < old_k else (old_k, k)
-                            # We essentially compare (c * (k_max-k_min) + Suffix[i+k_max]) 
-                            # vs (Suffix[i+k_min]). Since Suffix[i+k_min] starts with c, 
-                            # we look for the first char in Suffix[i+k_min] that is not c.
-                            d_idx = diff_char[i + k_min]
-                            # If d_idx is None, suffix is all 'c's; if d_idx < c, shorter is better.
-                            if d_idx is not None:
-                                if k == k_min:
-                                    if d_idx < c: is_better = True
-                                else:
-                                    if c < d_idx: is_better = True
+                        elif c == best_c:
+                            # Tie-break lengths: Compare current char c with the 
+                            # first char of the suffix starting at the divergence point.
+                            # The divergence point between (c, L_old) and (c, L_new) 
+                            # is i + min(L_old, L_new).
+                            L_small, L_large = (L, best_L) if L < best_L else (best_L, L)
+                            # At index i + L_small, the 'large' path has char 'c'
+                            # The 'small' path has char dp[i + L_small].best_c
+                            char_in_small_path = dp[i + L_small][1]
+                            
+                            # If L is the larger one and c < char_in_small_path, L is better
+                            # If L is the smaller one and c < char_in_small_path, L_old was better
+                            if L > best_L:
+                                if c < char_in_small_path: is_better = True
+                            else:
+                                if c > char_in_small_path: is_better = True
 
                     if is_better:
-                        dp[i] = cost_ik
-                        best_trans[i] = (c, k)
-                        first_char[i] = c
-                        # The first character in suffix i+k that is not c
-                        if first_char[i + k] is not None and first_char[i + k] != c:
-                            diff_char[i] = first_char[i + k]
-                        else:
-                            diff_char[i] = diff_char[i + k]
+                        best_cost, best_c, best_L = current_total, c, L
+            
+            dp[i] = (best_cost, best_c, best_L)
 
-        if dp[0] == inf:
+        if dp[0][0] == inf:
             return ""
 
+        # Reconstruct string
         res = []
         curr = 0
         while curr < n:
-            c_idx, k = best_trans[curr]
-            res.append(chr(ord('a') + c_idx) * k)
-            curr += k
+            _, c_idx, L = dp[curr]
+            res.append(chr(ord('a') + c_idx) * L)
+            curr += L
         return "".join(res)
 # @lc code=end
