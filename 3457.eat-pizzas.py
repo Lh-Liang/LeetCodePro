8663be3af2c1d24722a5b5e3f5bc5627268f08c4
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 01:58:27 +0800

    Add solution: 3457.eat-pizzas.py

--- 3457.eat-pizzas.py
+++ 3457.eat-pizzas.py
@@ -5,32 +5,32 @@
 #
 
 # @lc code=start
+import math
+
 class Solution:
     def maxWeight(self, pizzas: List[int]) -> int:
-        # Sort pizzas in descending order to pick the heaviest ones greedily
         pizzas.sort(reverse=True)
-        
         n = len(pizzas)
         total_days = n // 4
-        # Number of odd-numbered days (1, 3, 5...)
+        
+        # Number of odd and even days
         odd_days = (total_days + 1) // 2
-        # Number of even-numbered days (2, 4, 6...)
         even_days = total_days // 2
         
-        # For odd days, we gain the largest pizza (Z).
-        # We take the top 'odd_days' pizzas.
-        total_gain = sum(pizzas[:odd_days])
+        max_total_weight = 0
         
-        # For even days, we gain the second largest pizza (Y).
-        # For each even day, we need one pizza to be Z (larger than Y).
-        # So we skip one, take one, skip one, take one...
-        # Starting from the index after the odd_day pizzas.
+        # For odd days, we take the largest available pizzas as 'Z'
+        for i in range(odd_days):
+            max_total_weight += pizzas[i]
+            
+        # For even days, we need to pick 'Y'. 
+        # Each even day requires skipping one larger pizza (to be 'Z') 
+        # and taking the next largest (to be 'Y').
         current_idx = odd_days
         for _ in range(even_days):
-            # Skip one (to be Z), take the next (to be Y)
-            current_idx += 1
-            total_gain += pizzas[current_idx]
-            current_idx += 1
+            # Skip one (the Z for this even day) and take the next (the Y)
+            max_total_weight += pizzas[current_idx + 1]
+            current_idx += 2
             
-        return total_gain
+        return max_total_weight
 # @lc code=end
