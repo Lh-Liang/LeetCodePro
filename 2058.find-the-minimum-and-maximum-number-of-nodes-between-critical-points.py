cc752eeb86345557329c901da27f69af8963b7a3
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 23:10:56 +0800

    Add solution: 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py

--- 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -12,40 +12,41 @@
 #         self.next = next
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
-        # A critical point needs a previous and next node, so list must have at least 3 nodes
         if not head or not head.next or not head.next.next:
             return [-1, -1]
         
-        first_idx = -1
-        prev_idx = -1
+        first_cp_idx = -1
+        prev_cp_idx = -1
         min_dist = float('inf')
         
-        # Tracking previous value to identify local maxima/minima
+        # Start from the second node (index 1)
         prev_val = head.val
         curr = head.next
-        idx = 1 # 0-indexed position of curr
+        idx = 1
         
         while curr.next:
             next_val = curr.next.val
             
-            # Local Maxima or Local Minima
-            is_critical = (curr.val > prev_val and curr.val > next_val) or \
-                          (curr.val < prev_val and curr.val < next_val)
+            # Identify if current node is a critical point
+            is_maxima = curr.val > prev_val and curr.val > next_val
+            is_minima = curr.val < prev_val and curr.val < next_val
             
-            if is_critical:
-                if first_idx == -1:
-                    first_idx = idx
+            if is_maxima or is_minima:
+                if first_cp_idx == -1:
+                    first_cp_idx = idx
                 else:
-                    min_dist = min(min_dist, idx - prev_idx)
-                prev_idx = idx
+                    min_dist = min(min_dist, idx - prev_cp_idx)
+                
+                prev_cp_idx = idx
             
             prev_val = curr.val
             curr = curr.next
             idx += 1
             
-        # If fewer than 2 critical points were found
-        if first_idx == -1 or prev_idx == first_idx:
+        # If we found fewer than 2 critical points
+        if min_dist == float('inf'):
             return [-1, -1]
-            
-        return [min_dist, prev_idx - first_idx]
+        
+        max_dist = prev_cp_idx - first_cp_idx
+        return [int(min_dist), int(max_dist)]
 # @lc code=end
