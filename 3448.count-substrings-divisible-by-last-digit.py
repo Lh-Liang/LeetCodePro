d6e8d30b180a7a6a406700381e98c06a22d675b8
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 02:07:09 +0800

    Add solution: 3448.count-substrings-divisible-by-last-digit.py

--- 3448.count-substrings-divisible-by-last-digit.py
+++ 3448.count-substrings-divisible-by-last-digit.py
@@ -7,30 +7,33 @@
 # @lc code=start
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        # dp[k][rem] stores the count of substrings ending at current index
-        # that have remainder 'rem' when divided by 'k'.
+        # dp[d][r] stores the number of substrings ending at the current index
+        # that have a remainder r when divided by divisor d (1-9).
         dp = [[0] * 10 for _ in range(10)]
         total_count = 0
         
         for char in s:
-            v = int(char)
-            new_dp = [[0] * 10 for _ in range(10)]
+            digit = int(char)
+            # next_dp will store the counts for the next index
+            next_dp = [[0] * 10 for _ in range(10)]
             
-            for k in range(1, 10):
-                # Case: The substring is just the current digit itself
-                new_dp[k][v % k] += 1
+            # For each potential divisor d from 1 to 9
+            for d in range(1, 10):
+                # Update remainders for all substrings ending at the previous character
+                for r in range(d):
+                    if dp[d][r] > 0:
+                        new_remainder = (r * 10 + digit) % d
+                        next_dp[d][new_remainder] += dp[d][r]
                 
-                # Case: Extend previous substrings
-                for rem in range(k):
-                    if dp[k][rem] > 0:
-                        new_rem = (rem * 10 + v) % k
-                        new_dp[k][new_rem] += dp[k][rem]
+                # Include the new substring starting and ending at the current character
+                next_dp[d][digit % d] += 1
             
-            # If the last digit is non-zero, add substrings divisible by it
-            if v > 0:
-                total_count += new_dp[v][0]
+            # If the current digit is non-zero, it is a valid divisor.
+            # Add all substrings ending here that are divisible by this digit.
+            if digit > 0:
+                total_count += next_dp[digit][0]
             
-            dp = new_dp
+            dp = next_dp
             
         return total_count
 # @lc code=end
