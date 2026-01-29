cd2fd609529e74a50f627ef35cdc1d86c2fda617
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 16:14:11 +0800

    Add solution: 3434.maximum-frequency-after-subarray-operation.py

--- 3434.maximum-frequency-after-subarray-operation.py
+++ 3434.maximum-frequency-after-subarray-operation.py
@@ -7,45 +7,18 @@
 # @lc code=start
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        # Count occurrences and positions of each number
-        n = len(nums)
-        indices = [[] for _ in range(51)]
-        count_k = [0] * (n + 1)
-        
-        total_k = 0
-        for i, x in enumerate(nums):
-            if x == k:
-                total_k += 1
-            count_k[i + 1] = total_k
-            indices[x].append(i)
-            
-        max_gain = 0
-        
-        # For each possible value v that we want to turn into k
-        for v in range(1, 51):
-            if v == k or not indices[v]:
-                continue
-            
-            current_gain = 0
-            local_max_gain = 0
-            
-            # Apply Kadane's algorithm logic on indices of v and k
-            # We only need to consider subarrays starting and ending with v
-            for i in range(len(indices[v])):
-                if i > 0:
-                    # Subtract the number of k's between the previous v and this v
-                    # Number of k's in nums[indices[v][i-1]+1 : indices[v][i]]
-                    num_k_between = count_k[indices[v][i]] - count_k[indices[v][i-1] + 1]
-                    current_gain -= num_k_between
-                    if current_gain < 0:
-                        current_gain = 0
-                
-                current_gain += 1 # for the current v
-                if current_gain > local_max_gain:
-                    local_max_gain = current_gain
-            
-            if local_max_gain > max_gain:
-                max_gain = local_max_gain
-                
-        return total_k + max_gain
+        total_k = nums.count(k)
+        ans = total_k
+        for orig in range(1, 51):
+            curr = 0
+            max_sum = 0
+            for num in nums:
+                val = (1 if num == orig else 0) - (1 if num == k else 0)
+                curr += val
+                if curr < 0:
+                    curr = 0
+                max_sum = max(max_sum, curr)
+            ans = max(ans, total_k + max_sum)
+        return ans
+
 # @lc code=end
