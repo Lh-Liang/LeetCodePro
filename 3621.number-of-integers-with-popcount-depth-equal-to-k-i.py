7d1cc1771f7bdeff329d56892387b5d4c2492e45
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-29 22:45:49 +0800

    Add solution: 3621.number-of-integers-with-popcount-depth-equal-to-k-i.py

--- 3621.number-of-integers-with-popcount-depth-equal-to-k-i.py
+++ 3621.number-of-integers-with-popcount-depth-equal-to-k-i.py
@@ -5,62 +5,51 @@
 #
 
 # @lc code=start
+import math
+
 class Solution:
     def popcountDepth(self, n: int, k: int) -> int:
-        def popcount(x: int) -> int:
-            cnt = 0
-            while x:
-                cnt += x & 1
-                x >>= 1
-            return cnt
-        
-        def pop_depth(x: int) -> int:
-            d = 0
-            while x != 1:
-                x = popcount(x)
-                d += 1
-            return d
-        
         if k == 0:
-            return 1 if n >= 1 else 0
+            return 1
         
-        good_s = [s for s in range(1, 65) if pop_depth(s) == k - 1]
-        
-        def count_ones(target: int) -> int:
-            if target == 0 or n == 0:
-                return 0
-            bits = []
-            tmp = n
-            while tmp > 0:
-                bits.append(tmp % 2)
-                tmp //= 2
-            bits = bits[::-1]  # MSB first
-            L = len(bits)
-            memo = {}
+        # Precalculate popcount depths for [1, 64]
+        # d(x) = 1 + d(popcount(x))
+        depth = [0] * 65
+        depth[1] = 0
+        for i in range(2, 65):
+            depth[i] = 1 + depth[bin(i).count('1')]
             
-            def dp(pos: int, tight: int, cnt: int) -> int:
-                if pos == L:
-                    return cnt == target
-                key = (pos, tight, cnt)
-                if key in memo:
-                    return memo[key]
-                ans = 0
-                upper = bits[pos] if tight else 1
-                for d in range(2):
-                    if d > upper:
-                        continue
-                    new_tight = 1 if tight and d == upper else 0
-                    new_cnt = cnt + d
-                    if new_cnt > target:
-                        continue
-                    ans += dp(pos + 1, new_tight, new_cnt)
-                memo[key] = ans
-                return ans
-            return dp(0, 1, 0)
+        def count_with_popcount(num, target_c):
+            if target_c < 0:
+                return 0
+            s = bin(num)[2:]
+            L = len(s)
+            res = 0
+            current_bits = 0
+            for i in range(L):
+                if s[i] == '1':
+                    # If we set this bit to 0, we can choose the remaining bits freely
+                    remaining_pos = L - 1 - i
+                    needed = target_c - current_bits
+                    if 0 <= needed <= remaining_pos:
+                        res += math.comb(remaining_pos, needed)
+                    current_bits += 1
+            # Check the number n itself
+            if current_bits == target_c:
+                res += 1
+            return res
+
+        ans = 0
+        # Iterate possible popcounts c for x in [1, n]
+        # For x <= 10^15, max popcount is < 60
+        for c in range(1, 61):
+            if depth[c] == k - 1:
+                ans += count_with_popcount(n, c)
         
-        ans = sum(count_ones(s) for s in good_s)
+        # Boundary case: k=1 requires d(popcount(x)) = 0, which means popcount(x) = 1.
+        # count_with_popcount(n, 1) includes x=1 (2^0), but depth(1) is 0, not 1.
         if k == 1:
             ans -= 1
+            
         return ans
-
 # @lc code=end
