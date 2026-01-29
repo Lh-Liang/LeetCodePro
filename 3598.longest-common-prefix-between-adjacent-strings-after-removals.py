93c5bd9b430bb1726107577b5067fb4b514df71c
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 03:22:38 +0800

    Add solution: 3598.longest-common-prefix-between-adjacent-strings-after-removals.py

--- 3598.longest-common-prefix-between-adjacent-strings-after-removals.py
+++ 3598.longest-common-prefix-between-adjacent-strings-after-removals.py
@@ -5,54 +5,54 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def longestCommonPrefix(self, words: List[str]) -> List[int]:
         n = len(words)
-        if n < 2:
+        if n <= 1:
             return [0] * n
         
-        def get_lcp(s1, s2):
+        def get_lcp_len(s1: str, s2: str) -> int:
             length = 0
-            min_len = min(len(s1), len(s2))
-            while length < min_len and s1[length] == s2[length]:
+            for c1, c2 in zip(s1, s2):
+                if c1 != c2:
+                    break
                 length += 1
             return length
-
-        # L[i] is LCP of words[i] and words[i+1]
-        L = [get_lcp(words[i], words[i+1]) for i in range(n - 1)]
         
-        # B[i] is LCP of words[i-1] and words[i+1] (the bridge pair when i is removed)
-        B = [0] * n
-        for i in range(1, n - 1):
-            B[i] = get_lcp(words[i-1], words[i+1])
+        # P[i] is the LCP of words[i] and words[i+1]
+        # There are n-1 such pairs.
+        P = [get_lcp_len(words[i], words[i+1]) for i in range(n - 1)]
+        
+        # pref_max[i] = max(P[0...i-1])
+        pref_max = [0] * n
+        curr_max = 0
+        for i in range(n - 1):
+            curr_max = max(curr_max, P[i])
+            pref_max[i + 1] = curr_max
             
-        # Prefix max of original adjacent LCPs
-        pref = [0] * (n - 1)
-        pref[0] = L[0]
-        for i in range(1, n - 1):
-            pref[i] = max(pref[i-1], L[i])
-            
-        # Suffix max of original adjacent LCPs
-        suff = [0] * (n - 1)
-        suff[n-2] = L[n-2]
-        for i in range(n - 3, -1, -1):
-            suff[i] = max(suff[i+1], L[i])
+        # suff_max[i] = max(P[i...n-2])
+        suff_max = [0] * n
+        curr_max = 0
+        for i in range(n - 2, -1, -1):
+            curr_max = max(curr_max, P[i])
+            suff_max[i] = curr_max
             
         ans = [0] * n
         for i in range(n):
-            current_max = 0
-            # Range [0, i-2] is unaffected to the left
-            if i > 1:
-                current_max = max(current_max, pref[i-2])
-            # Range [i+1, n-2] is unaffected to the right
-            if i < n - 1:
-                if i + 1 <= n - 2:
-                    current_max = max(current_max, suff[i+1])
-            # The bridge pair (words[i-1], words[i+1])
-            if 0 < i < n - 1:
-                current_max = max(current_max, B[i])
-            
-            ans[i] = current_max
-            
+            if i == 0:
+                # Pairs: (w1, w2), (w2, w3)... LCPs: P[1...n-2]
+                ans[i] = suff_max[1]
+            elif i == n - 1:
+                # Pairs: (w0, w1), (w1, w2)... LCPs: P[0...n-3]
+                ans[i] = pref_max[n - 2]
+            else:
+                # Pairs: (w0...wi-1) [P[0...i-2]], (wi-1, wi+1) [new], (wi+1...wn-1) [P[i+1...n-2]]
+                v_pref = pref_max[i - 1]
+                v_suff = suff_max[i + 1]
+                v_bridge = get_lcp_len(words[i - 1], words[i + 1])
+                ans[i] = max(v_pref, v_suff, v_bridge)
+                
         return ans
 # @lc code=end
