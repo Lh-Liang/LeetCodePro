02dde0bea2cde4b935adcdc33fc44fa6bd10dc10
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 05:30:35 +0800

    Add solution: 3493.properties-graph.py

--- 3493.properties-graph.py
+++ 3493.properties-graph.py
@@ -7,34 +7,33 @@
 # @lc code=start
 class Solution:
     def numberOfComponents(self, properties: List[List[int]], k: int) -> int:
+        def intersect(a, b):
+            return len(set(a) & set(b))
+        
         n = len(properties)
-        # Convert to sets to ensure we count 'distinct integers common to both'
-        sets = [set(p) for p in properties]
-        
-        # DSU implementation
         parent = list(range(n))
-        def find(i):
-            if parent[i] == i:
-                return i
-            parent[i] = find(parent[i])
-            return parent[i]
+        rank = [0] * n
         
-        def union(i, j):
-            root_i = find(i)
-            root_j = find(j)
-            if root_i != root_j:
-                parent[root_i] = root_j
-                return True
-            return False
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
         
-        components = n
-        # Check all pairs for edges
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                if rank[rootX] > rank[rootY]:
+                    parent[rootY] = rootX
+                elif rank[rootX] < rank[rootY]:
+                    parent[rootX] = rootY
+                else:
+                    parent[rootY] = rootX
+                    rank[rootX] += 1
+        
         for i in range(n):
             for j in range(i + 1, n):
-                # Intersection of sets gives distinct common elements
-                if len(sets[i] & sets[j]) >= k:
-                    if union(i, j):
-                        components -= 1
+                if intersect(properties[i], properties[j]) >= k:
+                    union(i, j)
         
-        return components
-# @lc code=end
+        return len(set(find(i) for i in range(n)))
