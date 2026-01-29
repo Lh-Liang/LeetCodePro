a309629f8b22660d88c9176456a531d8ffaf0452
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 05:53:12 +0800

    Add solution: 3585.find-weighted-median-node-in-tree.py

--- 3585.find-weighted-median-node-in-tree.py
+++ 3585.find-weighted-median-node-in-tree.py
@@ -5,93 +5,56 @@
 #
 
 # @lc code=start
-import sys
-
-# Increase recursion depth for deep trees
-sys.setrecursionlimit(200000)
-
 class Solution:
-    def findMedian(self, n: int, edges: list[list[int]], queries: list[list[int]]) -> list[int]:
-        adj = [[] for _ in range(n)]
+    def findMedian(self, n: int, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
+        # Step 1: Build adjacency list for the tree
+        from collections import defaultdict
+        adj_list = defaultdict(list)
         for u, v, w in edges:
-            adj[u].append((v, w))
-            adj[v].append((u, w))
-        
-        LOG = 18
-        up = [[-1] * LOG for _ in range(n)]
-        dist = [0] * n
-        depth = [0] * n
-        
-        # Standard BFS to precompute depth, dist and first parent
-        stack = [(0, -1, 0, 0)]
-        while stack:
-            u, p, d, w_sum = stack.pop()
-            up[u][0] = p
-            depth[u] = d
-            dist[u] = w_sum
-            for v, w in adj[u]:
-                if v != p:
-                    stack.append((v, u, d + 1, w_sum + w))
-        
-        # Binary lifting table
-        for j in range(1, LOG):
-            for i in range(n):
-                if up[i][j-1] != -1:
-                    up[i][j] = up[up[i][j-1]][j-1]
-        
-        def get_lca(u, v):
-            if depth[u] < depth[v]:
-                u, v = v, u
-            for j in range(LOG - 1, -1, -1):
-                if depth[u] - (1 << j) >= depth[v]:
-                    u = up[u][j]
-            if u == v:
-                return u
-            for j in range(LOG - 1, -1, -1):
-                if up[u][j] != up[v][j]:
-                    u = up[u][j]
-                    v = up[v][j]
-            return up[u][0]
+            adj_list[u].append((v, w))
+            adj_list[v].append((u, w))
 
-        ans = []
-        for u, v in queries:
-            lca = get_lca(u, v)
-            total_weight = dist[u] + dist[v] - 2 * dist[lca]
-            
-            # Weight from u to LCA
-            u_to_lca_weight = dist[u] - dist[lca]
-            
-            # Check if median is on u -> LCA segment
-            # Condition: 2 * weight(u, x) >= total_weight
-            if 2 * u_to_lca_weight >= total_weight:
-                # Find the first node x on path u -> LCA s.t. 2*(dist[u] - dist[x]) >= total_weight
-                # This is the lowest ancestor x s.t. dist[x] <= dist[u] - total_weight / 2
-                curr = u
-                # We jump up as much as possible while the condition '2*(dist[u] - dist[anc]) < total_weight' is true
-                for j in range(LOG - 1, -1, -1):
-                    anc = up[curr][j]
-                    if anc != -1 and depth[anc] >= depth[lca]:
-                        if 2 * (dist[u] - dist[anc]) < total_weight:
-                            curr = anc
-                # After moving as high as possible without crossing the threshold,
-                # the 'first' node to cross it is the parent of curr.
-                # If u itself satisfies it (which happens if total_weight=0), the loop does nothing.
-                if 2 * (dist[u] - dist[curr]) >= total_weight:
-                    ans.append(curr)
-                else:
-                    ans.append(up[curr][0])
+        # Step 2: Preprocess with DFS to calculate cumulative weights
+        cumulative_weights = [0] * n
+        parents = [-1] * n
+
+        def dfs(node, parent):
+            for neighbor, weight in adj_list[node]:
+                if neighbor == parent:
+                    continue
+                parents[neighbor] = node
+                cumulative_weights[neighbor] = cumulative_weights[node] + weight
+                dfs(neighbor, node)
+
+        dfs(0, -1)
+
+        # Helper function to find LCA of two nodes u and v
+        def find_lca(u, v):
+            ancestors_u = set()
+            while u != -1:
+                ancestors_u.add(u)
+                u = parents[u]
+            while v not in ancestors_u:
+                v = parents[v]
+            return v
+
+        results = []
+        for uj, vj in queries:
+            # Step 3 & 4: Calculate total path weight and find LCA of uj and vj
+            lca = find_lca(uj, vj)
+            total_path_weight = (cumulative_weights[uj] + cumulative_weights[vj] - 2 * cumulative_weights[lca])
+            half_weight = total_path_weight / 2.0
+            # Step 5: Traverse from uj towards vj and find weighted median node
+            current_node = uj if cumulative_weights[uj] > cumulative_weights[vj] else vj
+            while current_node != lca:
+                next_node = parents[current_node]
+                edge_weight = abs(cumulative_weights[current_node] - cumulative_weights[next_node])
+                if edge_weight >= half_weight:
+                    results.append(current_node)
+                    break
+                half_weight -= edge_weight
+                current_node = next_node
             else:
-                # Median is on LCA -> v segment
-                # Find the first node x on path LCA -> v s.t. 2*(u_to_lca_weight + dist[x] - dist[lca]) >= total_weight
-                # This is the highest ancestor x of v s.t. 2*(dist[u] + dist[x] - 2*dist[lca]) >= total_weight
-                curr = v
-                # We jump up as much as possible while the condition '2*(dist[u] + dist[anc] - 2*dist[lca]) >= total_weight' is true
-                for j in range(LOG - 1, -1, -1):
-                    anc = up[curr][j]
-                    if anc != -1 and depth[anc] >= depth[lca]:
-                        if 2 * (dist[u] + dist[anc] - 2 * dist[lca]) >= total_weight:
-                            curr = anc
-                ans.append(curr)
-                
-        return ans
+                results.append(current_node) # In case median is at or after LCA
+        return results
 # @lc code=end
