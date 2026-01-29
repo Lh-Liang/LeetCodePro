85e9dd5fb658feaac687c0059f13a4d802cb956f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 02:29:48 +0800

    Add solution: 3513.number-of-unique-xor-triplets-i.py

--- 3513.number-of-unique-xor-triplets-i.py
+++ 3513.number-of-unique-xor-triplets-i.py
@@ -13,21 +13,8 @@ class Solution:
         if n == 2:
             return 2
         
-        # For n >= 3, in a permutation of 1..n, we can always find 
-        # i, j, k such that nums[i] ^ nums[j] ^ nums[k] = 0 (e.g., 1^2^3 = 0).
-        # The set of values {nums[i] ^ nums[j] ^ nums[k]} will include {1..n} 
-        # and 0, plus any other values reachable by XORing three elements.
-        # Since we have all numbers 1..n, we can form a basis.
-        
-        max_val = 0
-        for x in nums:
-            if x > max_val:
-                max_val = x
-        
-        # Find the smallest 2^k - 1 >= n
-        limit = 1
-        while limit <= max_val:
-            limit <<= 1
-        
-        return limit
+        # For n >= 3, the set of unique XOR triplet values covers the entire range
+        # of the XOR span of integers [1, n]. This span is always [0, 2^k - 1]
+        # where k is the bit length of n.
+        return 1 << n.bit_length()
 # @lc code=end
