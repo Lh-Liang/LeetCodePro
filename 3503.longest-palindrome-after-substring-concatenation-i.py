56eaecf4f4a1f0f1b845f7cf3511a6849f882b7b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 15:42:32 +0800

    Add solution: 3503.longest-palindrome-after-substring-concatenation-i.py

--- 3503.longest-palindrome-after-substring-concatenation-i.py
+++ 3503.longest-palindrome-after-substring-concatenation-i.py
@@ -7,22 +7,24 @@
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str, t: str) -> int:
-        def expand_around_center(left, right, str1, str2):
-            max_len = 0
-            while left >= 0 and right < len(str2) and str1[left] == str2[right]:
-                max_len = max(max_len, right - left + 1)
-                left -= 1
-                right += 1
-            return max_len
+        from collections import Counter
         
-        max_length = 0
-        # Check all centers in s concatenated with t reversed (to exploit symmetry)
-        for i in range(len(s)):
-            for j in range(len(t)):
-                # Expand around center between s[i] and t[j]
-                max_length = max(max_length, expand_around_center(i, len(t) - j - 1, s, t[::-1]))
-                # Expand around center between s[i:i+2] and t[j:j+2], if valid (even length palindromes)
-                if i + 1 < len(s) and j + 1 < len(t):
-                    max_length = max(max_length, expand_around_center(i + 1, len(t) - j - 2, s[:i+2], t[::-1][j:j+2]))
-        return max_length
+        # Count frequencies of each character in s and t
+        count_s = Counter(s)
+        count_t = Counter(t)
+        
+        max_len = 0
+        center_char_used = False
+        
+        # Check all possible characters for pairing
+        for char in set(s + t):
+            pairs = min(count_s.get(char, 0), count_t.get(char, 0))
+            max_len += 2 * pairs  # Each pair adds two to the length of palindrome
+            
+            # Check if there's potential for a center character (odd count)
+            if not center_char_used and (count_s.get(char, 0) > pairs or count_t.get(char, 0) > pairs):
+                max_len += 1  # Add one for a center character if available
+                center_char_used = True
+        
+        return max_len
 # @lc code=end
