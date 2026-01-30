86764e894dc7b33cd03e16a95bc684f3e8d6db1a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 17:35:28 +0800

    Add solution: 3757.number-of-effective-subsequences.py

--- 3757.number-of-effective-subsequences.py
+++ 3757.number-of-effective-subsequences.py
@@ -3,22 +3,43 @@
 #
 # [3757] Number of Effective Subsequences
 #
-
 # @lc code=start
 class Solution:
     def countEffective(self, nums: List[int]) -> int:
-        MOD = 10**9 + 7
-        full_or = 0
-        for num in nums:
-            full_or |= num
-        
-        # Find elements essential to maintaining the full OR value
-        critical_elements = set()
-        for num in nums:
-            if (full_or & ~num) != full_or:
-                critical_elements.add(num)
-        
-        # Count effective subsequences through combinatorial logic
-        effective_count = (1 << len(critical_elements)) - 1  # All non-empty subsets of critical elements
-        return effective_count % MOD
+        MOD = 10 ** 9 + 7
+        from collections import defaultdict
+        n = len(nums)
+        or_all = 0
+        for x in nums:
+            or_all |= x
+        # Find bits in or_all
+        bits = []
+        for k in range(0, 21):
+            if (or_all >> k) & 1:
+                bits.append(k)
+        # For each bit, get indices where nums[i] has that bit
+        bit_indices = {}
+        for b in bits:
+            bit_indices[b] = []
+        for i, x in enumerate(nums):
+            for b in bits:
+                if (x >> b) & 1:
+                    bit_indices[b].append(i)
+        m = len(bits)
+        ans = 0
+        # Inclusion-Exclusion over all non-empty subsets of bits
+        for mask in range(1, 1 << m):
+            involved = set(range(n))
+            sign = -1
+            for j in range(m):
+                if (mask >> j) & 1:
+                    involved &= set(bit_indices[bits[j]])
+                    sign *= -1
+            if not involved:
+                continue
+            cnt = len(involved)
+            # Number of non-empty subsequences of these indices
+            ways = pow(2, cnt, MOD) - 1
+            ans = (ans + sign * ways) % MOD
+        return ans % MOD
 # @lc code=end
