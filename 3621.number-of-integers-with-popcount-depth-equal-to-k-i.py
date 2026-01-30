823927396f0dcf92ad6e2a08eb9e64a8a594b1e6
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 17:22:42 +0800

    Add solution: 3621.number-of-integers-with-popcount-depth-equal-to-k-i.py

--- 3621.number-of-integers-with-popcount-depth-equal-to-k-i.py
+++ 3621.number-of-integers-with-popcount-depth-equal-to-k-i.py
@@ -3,23 +3,47 @@
 #
 # [3621] Number of Integers With Popcount-Depth Equal to K I
 #
-
 # @lc code=start
 class Solution:
     def popcountDepth(self, n: int, k: int) -> int:
-        def popcount(x):
-            return bin(x).count('1')
-        
-        def depth(x):
-            d = 0
-            while x != 1:
-                x = popcount(x)
-                d += 1
-            return d
-        
-        count = 0
-        for i in range(1, n + 1):
-            if depth(i) == k:
-                count += 1
-        return count
+        from functools import lru_cache
+        import sys
+        sys.setrecursionlimit(10000)
+        # Precompute popcount-depth for all possible popcounts
+        MAX_BITS = 70  # more than needed since n <= 1e15
+        popcount_depth = [0] * (MAX_BITS+1)
+        def get_depth(x):
+            if x == 1:
+                return 0
+            if popcount_depth[x] != 0:
+                return popcount_depth[x]
+            popcount_depth[x] = 1 + get_depth(bin(x).count('1'))
+            return popcount_depth[x]
+        for i in range(1, MAX_BITS+1):
+            get_depth(i)
+        # Count numbers <= n with exactly cnt ones
+        def count(N, cnt):
+            bits = list(map(int, bin(N)[2:]))
+            L = len(bits)
+            @lru_cache(None)
+            def dp(pos, tight, ones):
+                if ones < 0:
+                    return 0
+                if pos == L:
+                    return 1 if ones == 0 else 0
+                res = 0
+                up = bits[pos] if tight else 1
+                for d in range(up+1):
+                    res += dp(pos+1, tight and d==up, ones-d)
+                return res
+            return dp(0, True, cnt)
+        if k == 0:
+            return 1 if n >= 1 else 0
+        ans = 0
+        for i in range(1, MAX_BITS+1):
+            if popcount_depth[i] == k-1:
+                ans += count(n, i)
+        if k == 1:
+            ans -= 1  # exclude number 1 (since its depth is 0)
+        return ans
 # @lc code=end
