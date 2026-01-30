0e96eee17eea0d39a66a1ffa662a8ba80065869c
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 23:16:57 +0800

    Add solution: 3695.maximize-alternating-sum-using-swaps.py

--- 3695.maximize-alternating-sum-using-swaps.py
+++ 3695.maximize-alternating-sum-using-swaps.py
@@ -7,39 +7,58 @@
 # @lc code=start
 class Solution:
     def maxAlternatingSum(self, nums: List[int], swaps: List[List[int]]) -> int:
-        # Step 1: Build DSU to group swappable indices
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                if rank[rootX] > rank[rootY]:
+                    parent[rootY] = rootX
+                elif rank[rootX] < rank[rootY]:
+                    parent[rootX] = rootY
+                else:
+                    parent[rootY] = rootX
+                    rank[rootX] += 1
+        
         n = len(nums)
         parent = list(range(n))
-        def find(x):
-            while parent[x] != x:
-                parent[x] = parent[parent[x]]
-                x = parent[x]
-            return x
-        def union(x, y):
-            px, py = find(x), find(y)
-            if px != py:
-                parent[py] = px
-        for a, b in swaps:
-            union(a, b)
-        # Step 2: Group indices by component
-        from collections import defaultdict
-        groups = defaultdict(list)
+        rank = [0] * n
+        
+        for pi, qi in swaps:
+            union(pi, qi)
+        
+        components = {}
         for i in range(n):
-            groups[find(i)].append(i)
-        # Step 3: For each group, assign highest numbers to even-indexed positions
-        res = [0]*n
-        for indices in groups.values():
-            vals = [nums[i] for i in indices]
-            indices.sort()
-            vals.sort(reverse=True)
-            for idx, i in enumerate(indices):
-                res[i] = vals[idx]
-        # Step 4: Calculate alternating sum
-        ans = 0
-        for i, v in enumerate(res):
-            if i%2==0:
-                ans += v
-            else:
-                ans -= v
-        return ans
-# @lc code=end
+            root = find(i)
+            if root not in components:
+                components[root] = []
+            components[root].append(nums[i])
+        
+n   for component in components.values():
+n   component.sort()  # sort each component
+
+    max_sum = 0
+    index = 0
+    used_components_indexes = {i: 0 for i in components.keys()}
+nfor _ in range(n):
+n    min_component_key = min(
+n          used_components_indexes.keys(), 
+n          key=lambda k: (components[k][used_components_indexes[k]], k) if used_components_indexes[k] < len(components[k]) else (float('inf'), k)
+n    )
+n   value_to_use_from_component=min_component_key
+
+    curr_value=components[min_component_key][used_components_indexes[min_component_key]]
+nused_components_indexes[min_component_key]+=1
+
+    if index % 2 == 0:
+total_sum += curr_value
+else:
+total_sum -= curr_value
+index += 1 
+n 
+n return max_sum 
+total_sum #@lc code=end
