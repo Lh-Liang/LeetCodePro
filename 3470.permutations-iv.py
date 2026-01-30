23ba9d6ca9fd0a0b0cf3fa8c9f7d0a131b6a0571
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 16:58:05 +0800

    Add solution: 3470.permutations-iv.py

--- 3470.permutations-iv.py
+++ 3470.permutations-iv.py
@@ -5,40 +5,56 @@
 #
 
 # @lc code=start
-from math import factorial
-from itertools import islice
+from typing import List
+import functools
 
 class Solution:
     def permute(self, n: int, k: int) -> List[int]:
-        # Helper function to calculate number of valid alternating permutations for size m
-        def count_alternating_permutations(m):
-            if m <= 1:
+        odds = [x for x in range(1, n+1) if x%2 == 1]
+        evens = [x for x in range(1, n+1) if x%2 == 0]
+        total_odd, total_even = len(odds), len(evens)
+        used = [False] * (n+1)
+
+        @functools.lru_cache(None)
+        def dp(remain_odd, remain_even, last_parity):
+            if remain_odd == 0 and remain_even == 0:
                 return 1
-            dp = [0] * (m + 1)
-            dp[0], dp[1] = 1, 1
-            for i in range(2, m + 1):
-                dp[i] = (i - 1) * (dp[i - 2] + dp[i - 1])
-            return dp[m]
-        
-        if k > count_alternating_permutations(n):
+            res = 0
+            if last_parity != 1 and remain_odd > 0:
+                res += remain_odd * dp(remain_odd-1, remain_even, 1)
+            if last_parity != 0 and remain_even > 0:
+                res += remain_even * dp(remain_odd, remain_even-1, 0)
+            return res
+
+        answer = []
+        curr_odd, curr_even = total_odd, total_even
+        last_parity = -1  # start with any
+        for pos in range(n):
+            for x in range(1, n+1):
+                if used[x]:
+                    continue
+                this_parity = x % 2
+                if last_parity != -1 and this_parity == last_parity:
+                    continue
+                num_ways = dp(curr_odd - (this_parity==1), curr_even - (this_parity==0), this_parity)
+                if k > num_ways:
+                    k -= num_ways
+                else:
+                    answer.append(x)
+                    used[x] = True
+                    if this_parity == 1:
+                        curr_odd -= 1
+                    else:
+                        curr_even -= 1
+                    last_parity = this_parity
+                    break
+            else:
+                return []
+        # Verification step: Check adjacent parity and length
+        if len(answer) != n:
             return []
-        
-        numbers = list(range(1, n+1))
-        permutation = []
-        remaining_k = k - 1 # Zero-indexed kth permutation
-        odd, even = numbers[::2], numbers[1::2]
-        
-        for i in range(n):
-            if i % 2 == 0: # Even index, choose from odd numbers (1-based)
-                block_size = count_alternating_permutations(n - i - 1)
-                index = remaining_k // block_size
-                remaining_k %= block_size
-                permutation.append(odd.pop(index))
-            else: # Odd index, choose from even numbers (1-based)
-                block_size = count_alternating_permutations(n - i - 1)
-                index = remaining_k // block_size
-                remaining_k %= block_size
-                permutation.append(even.pop(index))
-        
-        return permutation
+        for i in range(1, n):
+            if (answer[i] % 2) == (answer[i-1] % 2):
+                return []
+        return answer
 # @lc code=end
