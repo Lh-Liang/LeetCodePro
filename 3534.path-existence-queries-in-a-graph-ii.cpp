5cf439b2b8484e138df6b56a123260621ec411b2
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 06:53:58 +0800

    Add solution: 3534.path-existence-queries-in-a-graph-ii.cpp

--- 3534.path-existence-queries-in-a-graph-ii.cpp
+++ 3534.path-existence-queries-in-a-graph-ii.cpp
@@ -4,70 +4,87 @@
 # [3534] Path Existence Queries in a Graph II
 #
 # @lc code=start
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        // Build adjacency list
-        vector<vector<int>> adj(n);
+        if (n == 0) return {};
+
+        // 1. Get unique sorted values
+        vector<int> sorted_nums = nums;
+        sort(sorted_nums.begin(), sorted_nums.end());
+        sorted_nums.erase(unique(sorted_nums.begin(), sorted_nums.end()), sorted_nums.end());
+        int m = sorted_nums.size();
+
+        // 2. Precompute jumps (Binary Lifting)
+        // The maximum distance can be m-1. We need max_log such that 2^max_log >= m.
+        int max_log = 0;
+        while ((1 << max_log) < m) max_log++;
+        if (max_log == 0) max_log = 1;
         
-        // Sort nodes by their values to efficiently find edges
-        vector<pair<int, int>> sorted_nodes;
-        for (int i = 0; i < n; i++) {
-            sorted_nodes.push_back({nums[i], i});
+        vector<vector<int>> jump(m, vector<int>(max_log));
+
+        for (int i = 0; i < m; ++i) {
+            auto it = upper_bound(sorted_nums.begin(), sorted_nums.end(), sorted_nums[i] + maxDiff);
+            jump[i][0] = distance(sorted_nums.begin(), prev(it));
         }
-        sort(sorted_nodes.begin(), sorted_nodes.end());
-        
-        // For each node, find all nodes within maxDiff
-        for (int i = 0; i < n; i++) {
-            int idx1 = sorted_nodes[i].second;
-            int val1 = sorted_nodes[i].first;
-            
-            for (int j = i + 1; j < n; j++) {
-                int idx2 = sorted_nodes[j].second;
-                int val2 = sorted_nodes[j].first;
-                
-                if (val2 - val1 > maxDiff) break;
-                
-                adj[idx1].push_back(idx2);
-                adj[idx2].push_back(idx1);
+
+        for (int k = 1; k < max_log; ++k) {
+            for (int i = 0; i < m; ++i) {
+                jump[i][k] = jump[jump[i][k-1]][k-1];
             }
         }
-        
-        // Process queries
-        vector<int> result;
-        for (const auto& query : queries) {
-            int u = query[0];
-            int v = query[1];
-            
+
+        // 3. Process queries
+        vector<int> results;
+        results.reserve(queries.size());
+        for (const auto& q : queries) {
+            int u = q[0], v = q[1];
             if (u == v) {
-                result.push_back(0);
+                results.push_back(0);
                 continue;
             }
-            
-            // BFS to find shortest path
-            queue<int> q;
-            vector<int> dist(n, -1);
-            q.push(u);
-            dist[u] = 0;
-            
-            while (!q.empty()) {
-                int curr = q.front();
-                q.pop();
-                
-                if (curr == v) break;
-                
-                for (int neighbor : adj[curr]) {
-                    if (dist[neighbor] == -1) {
-                        dist[neighbor] = dist[curr] + 1;
-                        q.push(neighbor);
-                    }
+            int val_u = nums[u], val_v = nums[v];
+            if (val_u > val_v) swap(val_u, val_v);
+
+            int idx_u = lower_bound(sorted_nums.begin(), sorted_nums.end(), val_u) - sorted_nums.begin();
+            int idx_v = lower_bound(sorted_nums.begin(), sorted_nums.end(), val_v) - sorted_nums.begin();
+
+            // Check reachability using the deepest level of the jump table
+            // Since 2^(max_log-1) might be less than m-1, we use the property that
+            // if we can't reach idx_v in the max power of 2 steps, we check if the
+            // furthest possible node in the component reaches idx_v.
+            int curr = idx_u;
+            if (jump[curr][max_log - 1] < idx_v) {
+                // Even with the largest jump, we might need more steps.
+                // We can use the jump table to move as far as possible.
+                for (int k = max_log - 1; k >= 0; --k) {
+                    curr = jump[curr][k];
+                }
+                if (curr < idx_v) {
+                    results.push_back(-1);
+                    continue;
                 }
             }
-            
-            result.push_back(dist[v]);
+
+            // Binary lifting to find min steps
+            int steps = 0;
+            curr = idx_u;
+            for (int k = max_log - 1; k >= 0; --k) {
+                if (jump[curr][k] < idx_v) {
+                    curr = jump[curr][k];
+                    steps += (1 << k);
+                }
+            }
+            results.push_back(steps + 1);
         }
-        
-        return result;
+
+        return results;
     }
 };
 # @lc code=end
