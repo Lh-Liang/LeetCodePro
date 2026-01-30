bd15cc82fb005b1f8953192b4dfdd29da294211d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 07:11:07 +0800

    Add solution: 3594.minimum-time-to-transport-all-individuals.cpp

--- 3594.minimum-time-to-transport-all-individuals.cpp
+++ 3594.minimum-time-to-transport-all-individuals.cpp
@@ -3,78 +3,89 @@
 #
 # [3594] Minimum Time to Transport All Individuals
 #
+
 # @lc code=start
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     double minTime(int n, int k, int m, vector<int>& time, vector<double>& mul) {
-        if (n > k && k == 1) {
-            return -1.0;
-        }
-        
-        map<tuple<int, int, int>, double> memo;
-        set<tuple<int, int, int>> visiting;
-        
-        function<double(int, int, int)> dfs = [&](int at_dest, int stage, int boat_at_dest) -> double {
-            if (at_dest == (1 << n) - 1) {
-                return 0.0;
-            }
-            
-            auto key = make_tuple(at_dest, stage, boat_at_dest);
-            if (memo.count(key)) {
-                return memo[key];
-            }
-            if (visiting.count(key)) {
-                return 1e9;
-            }
-            
-            visiting.insert(key);
-            double result = 1e9;
-            
-            if (boat_at_dest == 0) {
-                vector<int> available;
-                for (int i = 0; i < n; i++) {
-                    if ((at_dest & (1 << i)) == 0) {
-                        available.push_back(i);
-                    }
+        if (k == 1 && n > 1) return -1.0;
+
+        int num_masks = 1 << n;
+        vector<int> max_time(num_masks, 0);
+        for (int i = 0; i < num_masks; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if ((i >> j) & 1) {
+                    max_time[i] = max(max_time[i], time[j]);
                 }
-                
-                int sz = available.size();
-                for (int mask = 1; mask < (1 << sz); mask++) {
-                    if (__builtin_popcount(mask) > k) continue;
-                    
-                    int new_at_dest = at_dest;
-                    int max_time = 0;
-                    for (int i = 0; i < sz; i++) {
-                        if (mask & (1 << i)) {
-                            new_at_dest |= (1 << available[i]);
-                            max_time = max(max_time, time[available[i]]);
+            }
+        }
+
+        // dist[mask][pos][stage]
+        // pos: 0 for base camp, 1 for destination
+        double dist[1 << 12][2][5];
+        for (int i = 0; i < num_masks; ++i) {
+            for (int j = 0; j < 2; ++j) {
+                for (int s = 0; s < m; ++s) {
+                    dist[i][j][s] = 1e18;
+                }
+            }
+        }
+
+        priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> pq;
+        
+        dist[0][0][0] = 0.0;
+        pq.push({0.0, 0}); // state: (mask << 4) | (pos << 3) | stage
+
+        while (!pq.empty()) {
+            auto [d, curr] = pq.top();
+            pq.pop();
+
+            int mask = curr >> 4;
+            int pos = (curr >> 3) & 1;
+            int st = curr & 7;
+
+            if (d > dist[mask][pos][st] + 1e-11) continue;
+            if (mask == num_masks - 1 && pos == 1) return d;
+
+            if (pos == 0) { // Boat at base camp
+                int base_mask = (num_masks - 1) ^ mask;
+                for (int sub = base_mask; sub > 0; sub = (sub - 1) & base_mask) {
+                    int cnt = __builtin_popcount(sub);
+                    if (cnt >= 1 && cnt <= k) {
+                        double cost = max_time[sub] * mul[st];
+                        int nst = (st + (int)floor(cost + 1e-9)) % m;
+                        int nmask = mask | sub;
+                        if (dist[nmask][1][nst] > d + cost) {
+                            dist[nmask][1][nst] = d + cost;
+                            pq.push({dist[nmask][1][nst], (nmask << 4) | (1 << 3) | nst});
                         }
                     }
-                    
-                    double crossing_time = max_time * mul[stage];
-                    int new_stage = (stage + ((int)floor(crossing_time) % m)) % m;
-                    
-                    result = min(result, crossing_time + dfs(new_at_dest, new_stage, 1));
                 }
-            } else {
-                for (int i = 0; i < n; i++) {
-                    if (at_dest & (1 << i)) {
-                        int new_at_dest = at_dest ^ (1 << i);
-                        double return_time = time[i] * mul[stage];
-                        int new_stage = (stage + ((int)floor(return_time) % m)) % m;
-                        
-                        result = min(result, return_time + dfs(new_at_dest, new_stage, 0));
+            } else { // Boat at destination
+                if (mask != num_masks - 1) {
+                    for (int i = 0; i < n; ++i) {
+                        if ((mask >> i) & 1) {
+                            double cost = time[i] * mul[st];
+                            int nst = (st + (int)floor(cost + 1e-9)) % m;
+                            int nmask = mask ^ (1 << i);
+                            if (dist[nmask][0][nst] > d + cost) {
+                                dist[nmask][0][nst] = d + cost;
+                                pq.push({dist[nmask][0][nst], (nmask << 4) | (0 << 3) | nst});
+                            }
+                        }
                     }
                 }
             }
-            
-            visiting.erase(key);
-            memo[key] = result;
-            return result;
-        };
-        
-        double ans = dfs(0, 0, 0);
-        return ans >= 1e9 ? -1.0 : ans;
+        }
+
+        return -1.0;
     }
 };
 # @lc code=end
