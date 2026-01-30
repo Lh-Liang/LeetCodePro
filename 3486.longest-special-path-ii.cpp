a4bf3a88f9b5782cb3e1acaaa059d83ead2315e8
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 04:23:37 +0800

    Add solution: 3486.longest-special-path-ii.cpp

--- 3486.longest-special-path-ii.cpp
+++ 3486.longest-special-path-ii.cpp
@@ -3,85 +3,79 @@
 #
 # [3486] Longest Special Path II
 #
+
 # @lc code=start
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
+    struct Edge {
+        int to;
+        int weight;
+    };
+
+    long long max_len = 0;
+    int min_nodes = 1;
+    int last_pos[50005];
+    long long path_dists[50005];
+
+    void dfs(int u, int p, int depth, long long curr_dist, int m1, int m2, const vector<vector<Edge>>& adj, const vector<int>& nums) {
+        int val = nums[u];
+        int prev_depth = last_pos[val];
+
+        // Update the two largest conflict depths encountered on this path
+        int nm1 = m1, nm2 = m2;
+        if (prev_depth > nm1) {
+            nm2 = nm1;
+            nm1 = prev_depth;
+        } else if (prev_depth > nm2) {
+            nm2 = prev_depth;
+        }
+
+        // The special path starts after the second most recent conflict
+        long long L = curr_dist - path_dists[nm2];
+        int C = depth - nm2;
+
+        if (L > max_len) {
+            max_len = L;
+            min_nodes = C;
+        } else if (L == max_len) {
+            min_nodes = min(min_nodes, C);
+        }
+
+        path_dists[depth] = curr_dist;
+        last_pos[val] = depth;
+
+        for (const auto& edge : adj[u]) {
+            if (edge.to != p) {
+                dfs(edge.to, u, depth + 1, curr_dist + edge.weight, nm1, nm2, adj, nums);
+            }
+        }
+
+        // Backtrack: restore the previous depth for this value
+        last_pos[val] = prev_depth;
+    }
+
 public:
     vector<int> longestSpecialPath(vector<vector<int>>& edges, vector<int>& nums) {
         int n = nums.size();
-        vector<vector<pair<int, int>>> graph(n);
-        
-        for (auto& e : edges) {
-            graph[e[0]].push_back({e[1], e[2]});
-            graph[e[1]].push_back({e[0], e[2]});
+        vector<vector<Edge>> adj(n);
+        for (const auto& e : edges) {
+            adj[e[0]].push_back({e[1], e[2]});
+            adj[e[1]].push_back({e[0], e[2]});
         }
-        
-        vector<vector<pair<int, int>>> children(n);
-        buildTree(0, -1, graph, children);
-        
-        int maxLength = 0;
-        int minNodes = INT_MAX;
-        
-        for (int i = 0; i < n; i++) {
-            unordered_map<int, int> freq;
-            explorePaths(i, children, nums, freq, 0, false, 1, maxLength, minNodes);
-        }
-        
-        return {maxLength, minNodes};
-    }
-    
-private:
-    void buildTree(int node, int parent, 
-                   vector<vector<pair<int, int>>>& graph,
-                   vector<vector<pair<int, int>>>& children) {
-        for (auto [neighbor, length] : graph[node]) {
-            if (neighbor != parent) {
-                children[node].push_back({neighbor, length});
-                buildTree(neighbor, node, graph, children);
-            }
-        }
-    }
-    
-    void explorePaths(int node, vector<vector<pair<int, int>>>& children,
-                     vector<int>& nums, unordered_map<int, int>& freq,
-                     int length, bool hasDup, int nodeCount,
-                     int& maxLen, int& minNodes) {
-        
-        int val = nums[node];
-        freq[val]++;
-        
-        bool newHasDup = hasDup;
-        bool valid = true;
-        
-        if (freq[val] == 2) {
-            if (hasDup) {
-                valid = false;
-            } else {
-                newHasDup = true;
-            }
-        } else if (freq[val] > 2) {
-            valid = false;
-        }
-        
-        if (valid) {
-            if (nodeCount >= 2) {
-                if (length > maxLen) {
-                    maxLen = length;
-                    minNodes = nodeCount;
-                } else if (length == maxLen) {
-                    minNodes = min(minNodes, nodeCount);
-                }
-            }
-            
-            for (auto [child, edgeLen] : children[node]) {
-                explorePaths(child, children, nums, freq, length + edgeLen,
-                           newHasDup, nodeCount + 1, maxLen, minNodes);
-            }
-        }
-        
-        freq[val]--;
-        if (freq[val] == 0) {
-            freq.erase(val);
-        }
+
+        // Initialize: use 1-based depth. depth 0 is a dummy for distance calculation.
+        for (int i = 0; i < 50005; ++i) last_pos[i] = 0;
+        path_dists[0] = 0;
+        max_len = 0;
+        min_nodes = 1e9; // Will be updated by the first node (root)
+
+        dfs(0, -1, 1, 0, 0, 0, adj, nums);
+
+        return {(int)max_len, min_nodes};
     }
 };
 # @lc code=end
