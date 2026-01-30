431c1c1378ceb70791dc6ab5a48d247fafab9651
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 17:53:33 +0800

    Add solution: 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py

--- 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -3,7 +3,6 @@
 #
 # [2058] Find the Minimum and Maximum Number of Nodes Between Critical Points
 #
-
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
@@ -13,9 +12,7 @@
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
         indices = []
-        prev = head
-        curr = head.next
-        idx = 1
+        prev, curr, idx = head, head.next, 1
         while curr and curr.next:
             if (curr.val > prev.val and curr.val > curr.next.val) or (curr.val < prev.val and curr.val < curr.next.val):
                 indices.append(idx)
@@ -24,7 +21,7 @@ class Solution:
             idx += 1
         if len(indices) < 2:
             return [-1, -1]
-        min_dist = min(indices[i+1] - indices[i] for i in range(len(indices)-1))
+        min_dist = min([indices[i] - indices[i-1] for i in range(1, len(indices))])
         max_dist = indices[-1] - indices[0]
         return [min_dist, max_dist]
 # @lc code=end
