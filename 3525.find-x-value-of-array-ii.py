e814f5438f6c60541eaf985246b979338d1dd9b9
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 18:41:26 +0800

    Add solution: 3525.find-x-value-of-array-ii.py

--- 3525.find-x-value-of-array-ii.py
+++ 3525.find-x-value-of-array-ii.py
@@ -6,22 +6,31 @@
 
 # @lc code=start
 from typing import List
+
 class Solution:
     def resultArray(self, nums: List[int], k: int, queries: List[List[int]]) -> List[int]:
-        # Efficiently handle multiple queries with updates and repeated computations
-        res = []
         n = len(nums)
-        nums = nums[:]
-        for index, value, start, xi in queries:
-            nums[index] = value
+        res = []
+        nums = nums[:]  # Make a copy for in-place modification
+        for idx, val, start, x in queries:
+            nums[idx] = val
+            subarr = nums[start:]
+            m = len(subarr)
+            suffix_prod = [1] * (m + 1)
+            for i in range(m - 1, -1, -1):
+                suffix_prod[i] = (suffix_prod[i + 1] * subarr[i]) % k
             count = 0
-            # Precompute prefix products modulo k for nums[start:]
-            prefix_prod = [1]
-            for i in range(start, n):
-                prefix_prod.append((prefix_prod[-1] * nums[i]) % k)
-            for end in range(1, n - start + 1):
-                if prefix_prod[end] == xi:
+            # For each possible suffix removal (including removing nothing)
+            for j in range(m):  # Remove suffix starts at j (i.e., keep subarr[:j])
+                prod = suffix_prod[0]  # full product
+                if j == 0:
+                    prod = suffix_prod[0]
+                else:
+                    prod = suffix_prod[0] * pow(suffix_prod[j], -1, k) % k
+                if prod == x:
                     count += 1
+            if suffix_prod[0] == x:  # Case when removing nothing
+                count += 1
             res.append(count)
         return res
 # @lc code=end
