797a923ec32edf8e524d615ff0bb1ab1675c3569
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 17:52:01 +0800

    Add solution: 1367.linked-list-in-binary-tree.py

--- 1367.linked-list-in-binary-tree.py
+++ 1367.linked-list-in-binary-tree.py
@@ -18,18 +18,17 @@
 #         self.right = right
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-        def dfs(tree_node, list_node):
-            if list_node is None:
+        def dfs(tree, listnode):
+            if not listnode:
                 return True
-            if tree_node is None:
+            if not tree:
                 return False
-            if tree_node.val != list_node.val:
+            if tree.val != listnode.val:
                 return False
-            # Go down to either left or right
-            return dfs(tree_node.left, list_node.next) or dfs(tree_node.right, list_node.next)
-
+            return dfs(tree.left, listnode.next) or dfs(tree.right, listnode.next)
         if not root:
             return False
-        # Try to match starting from current node, or go to left/right child
-        return dfs(root, head) or self.isSubPath(head, root.left) or self.isSubPath(head, root.right)
+        return (dfs(root, head) or
+                self.isSubPath(head, root.left) or
+                self.isSubPath(head, root.right))
 # @lc code=end
