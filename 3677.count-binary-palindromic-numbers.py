e253b92a00bcb8614ef2f79131a110ce45856c00
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 17:26:09 +0800

    Add solution: 3677.count-binary-palindromic-numbers.py

--- 3677.count-binary-palindromic-numbers.py
+++ 3677.count-binary-palindromic-numbers.py
@@ -7,22 +7,26 @@
 # @lc code=start
 class Solution:
     def countBinaryPalindromes(self, n: int) -> int:
-        def generate_palindrome(half, odd_length):
-            """ Generate a full palindrome from half depending on length type """
-            half_bin = bin(half)[2:]
-            if odd_length:
-                return int(half_bin + half_bin[-2::-1], 2)
-            else:
-                return int(half_bin + half_bin[::-1], 2)
-        
-        count = 0
-        length = len(bin(n)) - 2  # Calculate the length of the binary representation of n
-        for l in range(1, length + 1):
-            # Even and odd length palindromes
-            for half in range(1 << ((l + 1) // 2)):
-                palindrome = generate_palindrome(half, l % 2 != 0)
-                if palindrome > n:
-                    break
-                count += 1
-        return count
+        res = 0
+        L = n.bit_length()
+        seen = set()  # To ensure uniqueness
+        # Generate palindromic numbers for each possible length
+        for l in range(1, L+1):
+            half = (l + 1) // 2
+            for i in range(1 << (half - 1), 1 << half):
+                s = bin(i)[2:]
+                if l % 2 == 0:
+                    p = s + s[::-1]
+                else:
+                    p = s + s[::-1][1:]
+                val = int(p, 2)
+                if val <= n and val not in seen:
+                    seen.add(val)
+                    res += 1
+        # Ensure 0 is counted exactly once if in range
+        if 0 <= n:
+            res += 1 if 0 not in seen else 0
+        # Verification: All palindromic numbers are unique and <= n
+        assert all(x <= n for x in seen)
+        return res
 # @lc code=end
