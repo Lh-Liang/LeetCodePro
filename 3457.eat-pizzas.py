a6d50ceb650bcce6f35473ef04d1dfc6cba89651
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 16:54:13 +0800

    Add solution: 3457.eat-pizzas.py

--- 3457.eat-pizzas.py
+++ 3457.eat-pizzas.py
@@ -7,15 +7,18 @@
 # @lc code=start
 class Solution:
     def maxWeight(self, pizzas: List[int]) -> int:
-        # Sort pizzas by weight in descending order to maximize gains
-        pizzas.sort(reverse=True)
-        total_gain = 0
-        # Process every 4 pizzas as a batch to determine daily gains
-        for i in range(0, len(pizzas), 4):
-            # Determine if it's an odd or even day based on batch index
-            if (i // 4) % 2 == 0: # Odd day (1-indexed)
-                total_gain += pizzas[i] # Add largest pizza's weight (Z)
-            else: # Even day
-                total_gain += pizzas[i + 1] # Add second largest pizza's weight (Y)
-        return total_gain
+        n = len(pizzas)
+        pizzas.sort()
+        res = 0
+        days = n // 4
+        for i in range(days):
+            group = pizzas[n - 4*(i+1): n - 4*i]
+            group.sort()
+            if i % 2 == 0:
+                res += group[3]
+            else:
+                res += group[2]
+        # Verification: ensure all pizzas used and correct group sizes
+        assert n == days * 4
+        return res
 # @lc code=end
