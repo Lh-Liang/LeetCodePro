f91dd11e43f58f0913ed47ccea0965c117103135
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 03:40:16 +0800

    Add solution: 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp

--- 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
+++ 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -5,81 +5,66 @@
 #
 
 # @lc code=start
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
         int n = edges1.size() + 1;
         int m = edges2.size() + 1;
-        
-        // Build adjacency lists
-        vector<vector<int>> graph1(n), graph2(m);
-        for (auto& e : edges1) {
-            graph1[e[0]].push_back(e[1]);
-            graph1[e[1]].push_back(e[0]);
-        }
-        for (auto& e : edges2) {
-            graph2[e[0]].push_back(e[1]);
-            graph2[e[1]].push_back(e[0]);
-        }
-        
-        // Color tree1 using BFS
-        vector<int> color1(n, -1);
-        color1[0] = 0;
-        queue<int> q;
-        q.push(0);
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-            for (int v : graph1[u]) {
-                if (color1[v] == -1) {
-                    color1[v] = 1 - color1[u];
-                    q.push(v);
+
+        auto getPartitions = [](int size, const vector<vector<int>>& adj) {
+            vector<int> color(size, -1);
+            vector<int> counts(2, 0);
+            queue<int> q;
+            
+            // Tree is connected, so one BFS from node 0 suffices
+            color[0] = 0;
+            counts[0]++;
+            q.push(0);
+            
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+                for (int v : adj[u]) {
+                    if (color[v] == -1) {
+                        color[v] = 1 - color[u];
+                        counts[color[v]]++;
+                        q.push(v);
+                    }
                 }
             }
+            return make_pair(color, counts);
+        };
+
+        vector<vector<int>> adj1(n);
+        for (const auto& e : edges1) {
+            adj1[e[0]].push_back(e[1]);
+            adj1[e[1]].push_back(e[0]);
         }
-        
-        // Count colors in tree1
-        int count0_tree1 = 0, count1_tree1 = 0;
-        for (int c : color1) {
-            if (c == 0) count0_tree1++;
-            else count1_tree1++;
+
+        vector<vector<int>> adj2(m);
+        for (const auto& e : edges2) {
+            adj2[e[0]].push_back(e[1]);
+            adj2[e[1]].push_back(e[0]);
         }
+
+        auto [color1, counts1] = getPartitions(n, adj1);
+        auto [color2, counts2] = getPartitions(m, adj2);
+
+        int maxContributionFromTree2 = max(counts2[0], counts2[1]);
         
-        // Color tree2 using BFS
-        vector<int> color2(m, -1);
-        color2[0] = 0;
-        q.push(0);
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-            for (int v : graph2[u]) {
-                if (color2[v] == -1) {
-                    color2[v] = 1 - color2[u];
-                    q.push(v);
-                }
-            }
+        vector<int> answer(n);
+        for (int i = 0; i < n; ++i) {
+            // Nodes in same partition in Tree 1 + Max nodes in opposite partition in Tree 2
+            answer[i] = counts1[color1[i]] + maxContributionFromTree2;
         }
-        
-        // Count colors in tree2
-        int count0_tree2 = 0, count1_tree2 = 0;
-        for (int c : color2) {
-            if (c == 0) count0_tree2++;
-            else count1_tree2++;
-        }
-        
-        int max_from_tree2 = max(count0_tree2, count1_tree2);
-        
-        // Build result
-        vector<int> result(n);
-        for (int i = 0; i < n; i++) {
-            if (color1[i] == 0) {
-                result[i] = count0_tree1 + max_from_tree2;
-            } else {
-                result[i] = count1_tree1 + max_from_tree2;
-            }
-        }
-        
-        return result;
+
+        return answer;
     }
 };
 # @lc code=end
