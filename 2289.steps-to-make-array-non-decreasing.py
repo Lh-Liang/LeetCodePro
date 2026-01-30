8720d4dc17af3e5c8c4fd05036105fcdc18af5a4
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 16:36:28 +0800

    Add solution: 2289.steps-to-make-array-non-decreasing.py

--- 2289.steps-to-make-array-non-decreasing.py
+++ 2289.steps-to-make-array-non-decreasing.py
@@ -7,19 +7,18 @@
 # @lc code=start
 class Solution:
     def totalSteps(self, nums: List[int]) -> int:
-        n = len(nums)
         stack = []
-        steps = [0] * n  # To store steps needed at each index.
-        max_steps = 0
-        
-        for i in range(n):
-            # While current element is greater than element at stack top,
-            # pop from stack and calculate steps needed.
-            while stack and nums[stack[-1]] > nums[i]:
-                last_index = stack.pop()
-                steps[i] = max(steps[i], steps[last_index] + 1)
-            max_steps = max(max_steps, steps[i])
-            stack.append(i)
-        
-        return max_steps
+        res = 0
+        for num in nums:
+            steps = 0
+            while stack and num >= stack[-1][0]:
+                steps = max(steps, stack[-1][1])
+                stack.pop()
+            if not stack:
+                steps = 0
+            else:
+                steps += 1
+            stack.append((num, steps))
+            res = max(res, steps)
+        return res
 # @lc code=end
