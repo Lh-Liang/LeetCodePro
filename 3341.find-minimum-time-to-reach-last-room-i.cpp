3fae42beb019e147d4364d7687f76a58eea52934
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 03:39:46 +0800

    Add solution: 3341.find-minimum-time-to-reach-last-room-i.cpp

--- 3341.find-minimum-time-to-reach-last-room-i.cpp
+++ 3341.find-minimum-time-to-reach-last-room-i.cpp
@@ -3,59 +3,54 @@
 #
 # [3341] Find Minimum Time to Reach Last Room I
 #
+
 # @lc code=start
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
         int n = moveTime.size();
         int m = moveTime[0].size();
         
-        // Priority queue: {time, {row, col}}
-        priority_queue<pair<int, pair<int, int>>, 
-                       vector<pair<int, pair<int, int>>>, 
-                       greater<pair<int, pair<int, int>>>> pq;
-        
-        // Distance array
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        
-        // Start at (0, 0) at time 0
-        pq.push({0, {0, 0}});
+        // dist[i][j] stores the minimum time to reach room (i, j)
+        // Initialize with a large value (INT_MAX)
+        vector<vector<int>> dist(n, vector<int>(m, 2147483647));
         dist[0][0] = 0;
         
-        // Directions: up, down, left, right
-        int dx[] = {-1, 1, 0, 0};
-        int dy[] = {0, 0, -1, 1};
+        // Min-heap: {time, {row, col}}
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        pq.push({0, {0, 0}});
+        
+        int dr[] = {0, 0, 1, -1};
+        int dc[] = {1, -1, 0, 0};
         
         while (!pq.empty()) {
-            int time = pq.top().first;
-            int x = pq.top().second.first;
-            int y = pq.top().second.second;
+            int d = pq.top().first;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
             pq.pop();
             
-            // If we reached the destination
-            if (x == n - 1 && y == m - 1) {
-                return time;
-            }
+            // If we found a better path already, skip this one
+            if (d > dist[r][c]) continue;
             
-            // If this is not the best path to (x, y), skip
-            if (time > dist[x][y]) {
-                continue;
-            }
+            // If we reached the target, return the time
+            if (r == n - 1 && c == m - 1) return d;
             
-            // Try all 4 directions
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            for (int i = 0; i < 4; ++i) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
                 
-                // Check bounds
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                    // Calculate arrival time at (nx, ny)
-                    int next_time = max(time, moveTime[nx][ny]) + 1;
-                    
-                    // If this is a better path
-                    if (next_time < dist[nx][ny]) {
-                        dist[nx][ny] = next_time;
-                        pq.push({next_time, {nx, ny}});
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+                    // Transition: time to enter is max(current_time, room_open_time) + move_cost
+                    int next_d = max(d, moveTime[nr][nc]) + 1;
+                    if (next_d < dist[nr][nc]) {
+                        dist[nr][nc] = next_d;
+                        pq.push({next_d, {nr, nc}});
                     }
                 }
             }
