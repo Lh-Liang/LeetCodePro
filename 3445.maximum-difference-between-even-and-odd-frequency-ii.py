ed4db894d89c9724fa5d34911f1ea36acd3c785a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 22:19:12 +0800

    Add solution: 3445.maximum-difference-between-even-and-odd-frequency-ii.py

--- 3445.maximum-difference-between-even-and-odd-frequency-ii.py
+++ 3445.maximum-difference-between-even-and-odd-frequency-ii.py
@@ -3,32 +3,21 @@
 #
 # [3445] Maximum Difference Between Even and Odd Frequency II
 #
-
 # @lc code=start
 class Solution:
     def maxDifference(self, s: str, k: int) -> int:
         n = len(s)
-        digits = ['0', '1', '2', '3', '4']
-        # prefix sum for each digit
-        prefix = [[0] * (n + 1) for _ in range(5)]
+        max_diff = float('-inf')
         for i in range(n):
-            for d in range(5):
-                prefix[d][i+1] = prefix[d][i]
-            prefix[int(s[i])][i+1] += 1
-
-        max_diff = -1
-        for a in range(5):
-            for b in range(5):
-                if a == b:
-                    continue
-                # For each substring of length >= k
-                for l in range(n):
-                    for r in range(l+k-1, n):
-                        fa = prefix[a][r+1] - prefix[a][l]
-                        fb = prefix[b][r+1] - prefix[b][l]
-                        if fa % 2 == 1 and fb > 0 and fb % 2 == 0:
-                            diff = fa - fb
-                            if diff > max_diff:
-                                max_diff = diff
-        return max_diff
+            freq = [0] * 10  # Since digits are 0-9
+            for j in range(i, n):
+                freq[int(s[j])] += 1
+                if j - i + 1 >= k:
+                    odd_freq = [(f, idx) for idx, f in enumerate(freq) if f % 2 == 1]
+                    even_freq = [(f, idx) for idx, f in enumerate(freq) if f > 0 and f % 2 == 0]
+                    if odd_freq and even_freq:
+                        max_a = max(odd_freq)[0]
+                        min_b = min(even_freq)[0]
+                        max_diff = max(max_diff, max_a - min_b)
+        return max_diff if max_diff != float('-inf') else -1
 # @lc code=end
