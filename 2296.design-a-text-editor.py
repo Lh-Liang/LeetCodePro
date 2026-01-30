066ab54998b405301436ba48a3c71844f96a35f5
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 18:01:08 +0800

    Add solution: 2296.design-a-text-editor.py

--- 2296.design-a-text-editor.py
+++ 2296.design-a-text-editor.py
@@ -6,37 +6,35 @@
 
 # @lc code=start
 class TextEditor:
+
     def __init__(self):
-        self.left = []  # stack for text to the left of cursor
-        self.right = []  # stack for text to the right of cursor
+        self.left = []  # stack for text to the left of the cursor
+        self.right = []  # stack for text to the right of the cursor
 
     def addText(self, text: str) -> None:
         for c in text:
             self.left.append(c)
 
     def deleteText(self, k: int) -> int:
-        count = 0
-        while self.left and count < k:
+        deleted = 0
+        while self.left and deleted < k:
             self.left.pop()
-            count += 1
-        return count
+            deleted += 1
+        return deleted
 
     def cursorLeft(self, k: int) -> str:
-        move = min(k, len(self.left))
-        for _ in range(move):
+        moved = 0
+        while self.left and moved < k:
             self.right.append(self.left.pop())
+            moved += 1
+        # Return last min(10, len(self.left)) chars as a string
         return ''.join(self.left[-10:])
 
     def cursorRight(self, k: int) -> str:
-        move = min(k, len(self.right))
-        for _ in range(move):
+        moved = 0
+        while self.right and moved < k:
             self.left.append(self.right.pop())
+            moved += 1
         return ''.join(self.left[-10:])
 
-# Your TextEditor object will be instantiated and called as such:
-# obj = TextEditor()
-# obj.addText(text)
-# param_2 = obj.deleteText(k)
-# param_3 = obj.cursorLeft(k)
-# param_4 = obj.cursorRight(k)
 # @lc code=end
