c2518da3d8a54f577decc55be25c5aa6db7d1475
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 03:57:43 +0800

    Add solution: 3420.count-non-decreasing-subarrays-after-k-operations.cpp

--- 3420.count-non-decreasing-subarrays-after-k-operations.cpp
+++ 3420.count-non-decreasing-subarrays-after-k-operations.cpp
@@ -3,33 +3,51 @@
 #
 # [3420] Count Non-Decreasing Subarrays After K Operations
 #
+
 # @lc code=start
+#include <vector>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     long long countNonDecreasingSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
-        long long count = 0;
-        
-        for (int i = 0; i < n; i++) {
-            long long cost = 0;
-            int max_val = nums[i];
-            
-            for (int j = i; j < n; j++) {
-                if (nums[j] < max_val) {
-                    cost += max_val - nums[j];
-                } else {
-                    max_val = nums[j];
-                }
-                
-                if (cost <= k) {
-                    count++;
-                } else {
-                    break;
-                }
+        long long ans = 0;
+        long long current_cost = 0;
+        int r = n - 1;
+        deque<int> dq; // Stores indices of elements in the window in decreasing order of nums[i]
+
+        // Process from right to left to make cost calculation easier
+        // As we expand 'l' to the left, nums[l] potentially increases elements to its right.
+        for (int l = n - 1; l >= 0; --l) {
+            // When adding nums[l] at the left, it becomes the new maximum for elements to its right
+            // that are smaller than it, until it hits an element >= nums[l].
+            while (!dq.empty() && nums[l] > nums[dq.back()]) {
+                int idx = dq.back();
+                dq.pop_back();
+                // The range affected by nums[idx] being replaced by nums[l]
+                int range_end = dq.empty() ? r : dq.back() - 1;
+                current_cost += (long long)(nums[l] - nums[idx]) * (range_end - idx + 1);
             }
+            dq.push_back(l);
+
+            // If cost exceeds k, shrink window from the right
+            while (current_cost > k) {
+                // nums[dq.front()] is the current max for the rightmost part of the window.
+                // However, we need to subtract the cost contributed by the current max at index r.
+                current_cost -= (long long)(nums[dq.front()] - nums[r]);
+                if (dq.front() == r) {
+                    dq.pop_front();
+                }
+                r--;
+            }
+            ans += (r - l + 1);
         }
-        
-        return count;
+
+        return ans;
     }
 };
 # @lc code=end
