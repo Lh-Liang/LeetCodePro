501c82bdf9c18b0ccfdd717e6d22af0994a38525
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 04:18:46 +0800

    Add solution: 3455.shortest-matching-substring.cpp

--- 3455.shortest-matching-substring.cpp
+++ 3455.shortest-matching-substring.cpp
@@ -3,116 +3,111 @@
 #
 # [3455] Shortest Matching Substring
 #
+
 # @lc code=start
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
+    vector<int> find_all(const string& s, const string& p) {
+        if (p.empty()) return {};
+        int n = s.length(), m = p.length();
+        if (m > n) return {};
+        vector<int> pi(m);
+        for (int i = 1, j = 0; i < m; i++) {
+            while (j > 0 && p[i] != p[j]) j = pi[j - 1];
+            if (p[i] == p[j]) j++;
+            pi[i] = j;
+        }
+        vector<int> res;
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j > 0 && s[i] != p[j]) j = pi[j - 1];
+            if (s[i] == p[j]) j++;
+            if (j == m) {
+                res.push_back(i - m + 1);
+                j = pi[j - 1];
+            }
+        }
+        return res;
+    }
+
     int shortestMatchingSubstring(string s, string p) {
-        int first_star = p.find('*');
-        int second_star = p.find('*', first_star + 1);
-        
-        string prefix = p.substr(0, first_star);
-        string middle = p.substr(first_star + 1, second_star - first_star - 1);
-        string suffix = p.substr(second_star + 1);
-        
         int n = s.length();
-        
-        if (prefix.empty() && middle.empty() && suffix.empty()) {
-            return 0;
-        }
-        
-        if (prefix.empty()) {
-            if (middle.empty() && !suffix.empty()) {
-                return s.find(suffix) != string::npos ? suffix.length() : -1;
-            } else if (!middle.empty() && suffix.empty()) {
-                return s.find(middle) != string::npos ? middle.length() : -1;
-            } else {
-                vector<int> middle_pos, suffix_pos;
-                for (int i = 0; i <= n - (int)middle.length(); i++) {
-                    if (s.compare(i, middle.length(), middle) == 0) {
-                        middle_pos.push_back(i);
-                    }
+        int idx1 = p.find('*');
+        int idx2 = p.find('*', idx1 + 1);
+        string p0 = p.substr(0, idx1);
+        string p1 = p.substr(idx1 + 1, idx2 - idx1 - 1);
+        string p2 = p.substr(idx2 + 1);
+        int L0 = p0.length(), L1 = p1.length(), L2 = p2.length();
+
+        vector<int> pos0 = find_all(s, p0);
+        vector<int> pos1 = find_all(s, p1);
+        vector<int> pos2 = find_all(s, p2);
+
+        if (!p0.empty() && pos0.empty()) return -1;
+        if (!p1.empty() && pos1.empty()) return -1;
+        if (!p2.empty() && pos2.empty()) return -1;
+
+        vector<int> best_i(n + 1, -1);
+        if (p0.empty()) {
+            for (int j = 0; j <= n; j++) best_i[j] = j;
+        } else {
+            int cur = -1;
+            int ptr = 0;
+            for (int j = 0; j <= n; j++) {
+                while (ptr < pos0.size() && pos0[ptr] + L0 <= j) {
+                    cur = pos0[ptr];
+                    ptr++;
                 }
-                for (int i = 0; i <= n - (int)suffix.length(); i++) {
-                    if (s.compare(i, suffix.length(), suffix) == 0) {
-                        suffix_pos.push_back(i);
-                    }
-                }
-                
-                int min_length = INT_MAX;
-                for (int m : middle_pos) {
-                    for (int suf : suffix_pos) {
-                        if (suf >= m + (int)middle.length()) {
-                            min_length = min(min_length, suf + (int)suffix.length() - m);
-                        }
-                    }
-                }
-                return min_length == INT_MAX ? -1 : min_length;
+                best_i[j] = cur;
             }
         }
-        
-        vector<int> prefix_pos, middle_pos, suffix_pos;
-        
-        for (int i = 0; i <= n - (int)prefix.length(); i++) {
-            if (s.compare(i, prefix.length(), prefix) == 0) {
-                prefix_pos.push_back(i);
+
+        vector<int> best_k(n + 1, -1);
+        if (p2.empty()) {
+            for (int j = 0; j <= n; j++) {
+                if (j + L1 <= n) best_k[j] = j + L1;
+            }
+        } else {
+            int cur = -1;
+            int ptr = pos2.size() - 1;
+            for (int j = n; j >= 0; j--) {
+                while (ptr >= 0 && pos2[ptr] >= j + L1) {
+                    cur = pos2[ptr];
+                    ptr--;
+                }
+                best_k[j] = cur;
             }
         }
-        
-        if (!middle.empty()) {
-            for (int i = 0; i <= n - (int)middle.length(); i++) {
-                if (s.compare(i, middle.length(), middle) == 0) {
-                    middle_pos.push_back(i);
+
+        int ans = 2e9;
+        bool found = false;
+        if (p1.empty()) {
+            for (int j = 0; j <= n; j++) {
+                int i = best_i[j];
+                int k = best_k[j];
+                if (i != -1 && k != -1) {
+                    ans = min(ans, (k + L2) - i);
+                    found = true;
                 }
             }
-        }
-        
-        if (!suffix.empty()) {
-            for (int i = 0; i <= n - (int)suffix.length(); i++) {
-                if (s.compare(i, suffix.length(), suffix) == 0) {
-                    suffix_pos.push_back(i);
-                }
-            }
-        }
-        
-        int min_length = INT_MAX;
-        
-        for (int prefix_start : prefix_pos) {
-            int min_suffix_start = prefix_start + prefix.length() + middle.length();
-            
-            if (suffix.empty()) {
-                if (middle.empty()) {
-                    min_length = min(min_length, (int)prefix.length());
-                } else {
-                    int min_middle_start = prefix_start + prefix.length();
-                    auto it = lower_bound(middle_pos.begin(), middle_pos.end(), min_middle_start);
-                    if (it != middle_pos.end()) {
-                        min_length = min(min_length, *it + (int)middle.length() - prefix_start);
-                    }
-                }
-            } else {
-                for (int suffix_start : suffix_pos) {
-                    if (suffix_start < min_suffix_start) continue;
-                    
-                    int length = suffix_start + suffix.length() - prefix_start;
-                    
-                    if (middle.empty()) {
-                        min_length = min(min_length, length);
-                    } else {
-                        int min_middle_start = prefix_start + prefix.length();
-                        int max_middle_start = suffix_start - middle.length();
-                        
-                        if (max_middle_start >= min_middle_start) {
-                            auto it = lower_bound(middle_pos.begin(), middle_pos.end(), min_middle_start);
-                            if (it != middle_pos.end() && *it <= max_middle_start) {
-                                min_length = min(min_length, length);
-                            }
-                        }
-                    }
+        } else {
+            for (int j : pos1) {
+                int i = best_i[j];
+                int k = best_k[j];
+                if (i != -1 && k != -1) {
+                    ans = min(ans, (k + L2) - i);
+                    found = true;
                 }
             }
         }
-        
-        return min_length == INT_MAX ? -1 : min_length;
+
+        return found ? ans : -1;
     }
 };
 # @lc code=end
