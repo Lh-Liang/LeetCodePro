64517a03f3be418fed4b9344cc2f2ce9c3784435
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 22:10:40 +0800

    Add solution: 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py

--- 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
+++ 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
@@ -5,50 +5,35 @@
 #
 
 # @lc code=start
-from typing import List
-from collections import deque, defaultdict
-
 class Solution:
     def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
-        n = len(edges1) + 1
-        m = len(edges2) + 1
-        # Build graphs
-        tree1 = [[] for _ in range(n)]
-        for a, b in edges1:
-            tree1[a].append(b)
-            tree1[b].append(a)
-        tree2 = [[] for _ in range(m)]
-        for u, v in edges2:
-            tree2[u].append(v)
-            tree2[v].append(u)
-        # Get even/odd depths counts for tree1
-        depth1 = [0] * n
-        cnt1 = [0, 0] # cnt1[0]: even, cnt1[1]: odd
-        def dfs1(u, p, d):
-            depth1[u] = d
-            cnt1[d % 2] += 1
-            for v in tree1[u]:
-                if v != p:
-                    dfs1(v, u, d+1)
-        dfs1(0, -1, 0)
-        # Get even/odd depths counts for tree2
-        depth2 = [0] * m
-        cnt2 = [0, 0]
-        def dfs2(u, p, d):
-            depth2[u] = d
-            cnt2[d % 2] += 1
-            for v in tree2[u]:
-                if v != p:
-                    dfs2(v, u, d+1)
-        dfs2(0, -1, 0)
-        # For each node in tree1, find max possible target nodes
-        answer = []
-        for i in range(n):
-            parity_i = depth1[i] % 2
-            # Option 1: connect to even node in tree2
-            total_even = cnt1[parity_i] + cnt2[0]
-            # Option 2: connect to odd node in tree2
-            total_odd = cnt1[1 - parity_i] + cnt2[1]
-            answer.append(max(total_even, total_odd))
-        return answer
-# @lc code=end
+        from collections import defaultdict, deque
+        
+        # Create adjacency list for both trees
+        def build_adjacency_list(edges):
+            adj_list = defaultdict(list)
+            for u, v in edges:
+                adj_list[u].append(v)
+                adj_list[v].append(u)
+            return adj_list
+        
+        adj1 = build_adjacency_list(edges1)
+        adj2 = build_adjacency_list(edges2)
+        
+        def bfs_count_targets(start_node, adj):
+            queue = deque([(start_node, 0)])  # node and level (distance from start_node) tuple
+            seen = {start_node}
+            count = 0
+            while queue:
+                node, level = queue.popleft()
+                count += 1 if level % 2 == 0 else 0  # count as target if level is even
+                for neighbor in adj[node]:
+                    if neighbor not in seen:
+                        seen.add(neighbor)
+                        queue.append((neighbor, level + 1))
+            return count
+        
+        n = len(adj1)  # number of nodes in first tree
+m = len(adj2)  # number of nodes in second tree
+result = []
+f...
