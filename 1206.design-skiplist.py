204491c6dc9f01b0918bff66ba5e667a04104560
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 17:56:34 +0800

    Add solution: 1206.design-skiplist.py

--- 1206.design-skiplist.py
+++ 1206.design-skiplist.py
@@ -6,69 +6,66 @@
 # @lc code=start
 import random
 
-class Node:
+class SkiplistNode:
     def __init__(self, val, level):
         self.val = val
         self.forward = [None] * (level + 1)
 
 class Skiplist:
-    MAX_LEVEL = 16
-    P = 0.5
-
     def __init__(self):
-        self.head = Node(-1, Skiplist.MAX_LEVEL)
+        self.MAX_LEVEL = 16
+        self.P = 0.5
+        self.head = SkiplistNode(-1, self.MAX_LEVEL)
         self.level = 0
 
-    def random_level(self):
+    def randomLevel(self):
         lvl = 0
-        while random.random() < Skiplist.P and lvl < Skiplist.MAX_LEVEL:
+        while random.random() < self.P and lvl < self.MAX_LEVEL:
             lvl += 1
         return lvl
 
     def search(self, target: int) -> bool:
-        cur = self.head
+        curr = self.head
         for i in range(self.level, -1, -1):
-            while cur.forward[i] and cur.forward[i].val < target:
-                cur = cur.forward[i]
-        cur = cur.forward[0]
-        return cur is not None and cur.val == target
+            while curr.forward[i] and curr.forward[i].val < target:
+                curr = curr.forward[i]
+        curr = curr.forward[0]
+        return curr is not None and curr.val == target
 
     def add(self, num: int) -> None:
-        update = [None] * (Skiplist.MAX_LEVEL + 1)
-        cur = self.head
+        update = [None] * (self.MAX_LEVEL + 1)
+        curr = self.head
         for i in range(self.level, -1, -1):
-            while cur.forward[i] and cur.forward[i].val < num:
-                cur = cur.forward[i]
-            update[i] = cur
-        lvl = self.random_level()
+            while curr.forward[i] and curr.forward[i].val < num:
+                curr = curr.forward[i]
+            update[i] = curr
+        lvl = self.randomLevel()
         if lvl > self.level:
             for i in range(self.level + 1, lvl + 1):
                 update[i] = self.head
             self.level = lvl
-        node = Node(num, lvl)
+        node = SkiplistNode(num, lvl)
         for i in range(lvl + 1):
             node.forward[i] = update[i].forward[i]
             update[i].forward[i] = node
-        # Verification step: Ensure all forward pointers and levels are updated correctly
 
     def erase(self, num: int) -> bool:
-        update = [None] * (Skiplist.MAX_LEVEL + 1)
-        cur = self.head
+        update = [None] * (self.MAX_LEVEL + 1)
+        curr = self.head
         found = False
         for i in range(self.level, -1, -1):
-            while cur.forward[i] and cur.forward[i].val < num:
-                cur = cur.forward[i]
-            update[i] = cur
-        cur = cur.forward[0]
-        if cur and cur.val == num:
+            while curr.forward[i] and curr.forward[i].val < num:
+                curr = curr.forward[i]
+            update[i] = curr
+        curr = curr.forward[0]
+        if curr and curr.val == num:
             found = True
             for i in range(self.level + 1):
-                if update[i].forward[i] != cur:
+                if update[i].forward[i] != curr:
                     continue
-                update[i].forward[i] = cur.forward[i]
+                update[i].forward[i] = curr.forward[i]
             while self.level > 0 and self.head.forward[self.level] is None:
                 self.level -= 1
-            # Verification step: After erasure, check that skiplist invariants hold
         return found
 
 # Your Skiplist object will be instantiated and called as such:
