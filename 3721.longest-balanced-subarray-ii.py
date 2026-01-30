1ab59210fb8bcc0781854c4b813620f73c87a44b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 23:18:28 +0800

    Add solution: 3721.longest-balanced-subarray-ii.py

--- 3721.longest-balanced-subarray-ii.py
+++ 3721.longest-balanced-subarray-ii.py
@@ -3,29 +3,29 @@
 #
 # [3721] Longest Balanced Subarray II
 #
+
 # @lc code=start
 class Solution:
     def longestBalanced(self, nums: List[int]) -> int:
+        max_length = 0
         left = 0
-        max_len = 0
         even_set = set()
         odd_set = set()
-        count = {}
-        for right, num in enumerate(nums):
-            if num % 2 == 0:
-                even_set.add(num)
+        
+        for right in range(len(nums)):
+            if nums[right] % 2 == 0:
+                even_set.add(nums[right])
             else:
-                odd_set.add(num)
-            count[num] = count.get(num, 0) + 1
-            while left <= right and abs(len(even_set) - len(odd_set)) > 0:
-                count[nums[left]] -= 1
-                if count[nums[left]] == 0:
-                    if nums[left] % 2 == 0:
-                        even_set.remove(nums[left])
-                    else:
-                        odd_set.remove(nums[left])
+                odd_set.add(nums[right])
+                
+            while len(even_set) != len(odd_set) and left <= right:
+                if nums[left] % 2 == 0:
+                    even_set.remove(nums[left])
+                else:
+                    odd_set.remove(nums[left])
                 left += 1
+            
             if len(even_set) == len(odd_set):
-                max_len = max(max_len, right - left + 1)
-        return max_len
+                max_length = max(max_length, right - left + 1)
+        return max_length
 # @lc code=end
