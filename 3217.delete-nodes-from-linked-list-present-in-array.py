4f3dcf62314150cf3302e873c9a3fbe2fd73f6fe
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 22:04:55 +0800

    Add solution: 3217.delete-nodes-from-linked-list-present-in-array.py

--- 3217.delete-nodes-from-linked-list-present-in-array.py
+++ 3217.delete-nodes-from-linked-list-present-in-array.py
@@ -3,26 +3,26 @@
 #
 # [3217] Delete Nodes From Linked List Present in Array
 #
-
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-from typing import List, Optional
-
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        remove_set = set(nums)
-        dummy = ListNode(0)
-        dummy.next = head
-        prev, curr = dummy, head
-        while curr:
-            if curr.val in remove_set:
-                prev.next = curr.next
+        nums_set = set(nums)  # Convert array to set for O(1) lookups
+        dummy = ListNode(0)  # Dummy node to handle edge cases easily
+        dummy.next = head  # Point dummy node to head of the list
+        current = dummy  # Start with the dummy node
+        
+        while current.next is not None:
+            if current.next.val in nums_set:
+                # Skip over nodes with values in nums_set
+                current.next = current.next.next
             else:
-                prev = curr
-            curr = curr.next
-        return dummy.next
+                # Move to next node if it's not being removed
+                current = current.next
+        
+        return dummy.next  # Return head of modified list which is dummy's next pointer
 # @lc code=end
