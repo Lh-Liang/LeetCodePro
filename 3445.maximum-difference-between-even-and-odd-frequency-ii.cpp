0598d08be69461cd94651eeb905fcedc6d3141df
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 04:03:58 +0800

    Add solution: 3445.maximum-difference-between-even-and-odd-frequency-ii.cpp

--- 3445.maximum-difference-between-even-and-odd-frequency-ii.cpp
+++ 3445.maximum-difference-between-even-and-odd-frequency-ii.cpp
@@ -3,39 +3,75 @@
 #
 # [3445] Maximum Difference Between Even and Odd Frequency II
 #
+
 # @lc code=start
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
 class Solution {
 public:
     int maxDifference(string s, int k) {
         int n = s.length();
-        int maxDiff = INT_MIN;
-        
-        for (int i = 0; i < n; i++) {
-            int freq[5] = {0}; // Only '0' to '4'
-            for (int j = i; j < n; j++) {
-                freq[s[j] - '0']++;
-                
-                if (j - i + 1 >= k) {
-                    // Find max odd frequency and min even frequency
-                    int maxOdd = INT_MIN;
-                    int minEven = INT_MAX;
-                    
-                    for (int c = 0; c < 5; c++) {
-                        if (freq[c] % 2 == 1) {
-                            maxOdd = max(maxOdd, freq[c]);
-                        } else if (freq[c] > 0) {
-                            minEven = min(minEven, freq[c]);
+        vector<vector<int>> prefix(5, vector<int>(n + 1, 0));
+        for (int i = 0; i < n; ++i) {
+            for (int d = 0; d < 5; ++d) {
+                prefix[d][i + 1] = prefix[d][i] + (s[i] - '0' == d);
+            }
+        }
+
+        int max_diff = INT_MIN;
+
+        for (int a = 0; a < 5; ++a) {
+            for (int b = 0; b < 5; ++b) {
+                if (a == b) continue;
+
+                // min_satisfied[parity_a][parity_b] stores min(prefix[a][j] - prefix[b][j])
+                // for j that satisfy: j <= i-k AND s[j...i-1] contains at least one 'b'
+                vector<vector<int>> min_satisfied(2, vector<int>(2, 1e9));
+                int p = 0; 
+                int last_b_idx = -1;
+
+                for (int i = 1; i <= n; ++i) {
+                    if (s[i - 1] - '0' == b) last_b_idx = i - 1;
+
+                    // Condition: j <= i - k AND j <= last_b_idx
+                    int limit = min(i - k, last_b_idx);
+                    while (p <= limit) {
+                        int pa = prefix[a][p] % 2;
+                        int pb = prefix[b][p] % 2;
+                        int diff = prefix[a][p] - prefix[b][p];
+                        if (diff < min_satisfied[pa][pb]) {
+                            min_satisfied[pa][pb] = diff;
                         }
+                        p++;
                     }
-                    
-                    if (maxOdd != INT_MIN && minEven != INT_MAX) {
-                        maxDiff = max(maxDiff, maxOdd - minEven);
+
+                    // Current parities at i
+                    int cur_pa = prefix[a][i] % 2;
+                    int cur_pb = prefix[b][i] % 2;
+
+                    // We need freq_a = odd -> prefix[a][i] - prefix[a][j] % 2 == 1
+                    // So target_pa = 1 - cur_pa (if cur_pa is 0, we need 1; if 1, we need 0)
+                    // We need freq_b = even -> prefix[b][i] - prefix[b][j] % 2 == 0
+                    // So target_pb = cur_pb
+                    int target_pa = 1 - cur_pa;
+                    int target_pb = cur_pb;
+
+                    if (min_satisfied[target_pa][target_pb] != 1e9) {
+                        int current_total_diff = (prefix[a][i] - prefix[b][i]) - min_satisfied[target_pa][target_pb];
+                        if (current_total_diff > max_diff) {
+                            max_diff = current_total_diff;
+                        }
                     }
                 }
             }
         }
-        
-        return maxDiff;
+
+        return max_diff;
     }
 };
 # @lc code=end
