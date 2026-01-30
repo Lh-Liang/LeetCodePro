f8cb360e3a6ef37f5210ed28ca317dcf7b2832f3
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 06:48:00 +0800

    Add solution: 3519.count-numbers-with-non-decreasing-digits.cpp

--- 3519.count-numbers-with-non-decreasing-digits.cpp
+++ 3519.count-numbers-with-non-decreasing-digits.cpp
@@ -3,96 +3,86 @@
 #
 # [3519] Count Numbers with Non-Decreasing Digits
 #
+
 # @lc code=start
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
 class Solution {
+    int MOD = 1e9 + 7;
+    int memo[405][11][2][2];
+
+    // Converts a decimal string to a vector of digits in base b
+    vector<int> toBaseB(string s, int b) {
+        vector<int> decimal;
+        for (char c : s) decimal.push_back(c - '0');
+        vector<int> res;
+        while (!decimal.empty()) {
+            int rem = 0;
+            vector<int> next_decimal;
+            for (int d : decimal) {
+                int cur = d + rem * 10;
+                if (!next_decimal.empty() || cur / b > 0) {
+                    next_decimal.push_back(cur / b);
+                }
+                rem = cur % b;
+            }
+            res.push_back(rem);
+            decimal = next_decimal;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+    int dfs(const vector<int>& digits, int idx, int last, bool isLess, bool isStarted, int b) {
+        if (idx == digits.size()) return isStarted ? 1 : 0;
+        if (memo[idx][last][isLess][isStarted] != -1) return memo[idx][last][isLess][isStarted];
+
+        long long ans = 0;
+        int limit = isLess ? b - 1 : digits[idx];
+
+        for (int d = 0; d <= limit; ++d) {
+            bool nextIsLess = isLess || (d < limit);
+            if (!isStarted) {
+                // Number hasn't started yet; leading zeros don't count towards non-decreasing constraint
+                ans = (ans + dfs(digits, idx + 1, d, nextIsLess, d > 0, b)) % MOD;
+            } else if (d >= last) {
+                // Number has started; next digit must be >= last digit
+                ans = (ans + dfs(digits, idx + 1, d, nextIsLess, true, b)) % MOD;
+            }
+        }
+        return memo[idx][last][isLess][isStarted] = (int)ans;
+    }
+
+    int solve(const vector<int>& digits, int b) {
+        if (digits.empty()) return 0;
+        memset(memo, -1, sizeof(memo));
+        return dfs(digits, 0, 0, false, false, b);
+    }
+
+    bool check(const vector<int>& digits) {
+        for (size_t i = 1; i < digits.size(); ++i) {
+            if (digits[i] < digits[i - 1]) return false;
+        }
+        return !digits.empty();
+    }
+
 public:
     int countNumbers(string l, string r, int b) {
-        const int MOD = 1e9 + 7;
+        vector<int> digitsR = toBaseB(r, b);
+        int ansR = solve(digitsR, b);
         
-        int countR = countUpTo(r, b, MOD);
-        string l_minus_1 = subtract1(l);
-        int countL = (l_minus_1.empty() || l_minus_1 == "0") ? 0 : countUpTo(l_minus_1, b, MOD);
+        vector<int> digitsL = toBaseB(l, b);
+        int ansL = solve(digitsL, b);
         
-        int result = (countR - countL + MOD) % MOD;
-        return result;
-    }
-    
-private:
-    string subtract1(string s) {
-        int n = s.length();
-        for (int i = n - 1; i >= 0; i--) {
-            if (s[i] > '0') {
-                s[i]--;
-                break;
-            } else {
-                s[i] = '9';
-            }
-        }
-        int i = 0;
-        while (i < s.length() && s[i] == '0') i++;
-        return i == s.length() ? "0" : s.substr(i);
-    }
-    
-    vector<int> toBase(string s, int b) {
-        vector<int> result;
-        while (s != "0" && !s.empty()) {
-            int remainder = 0;
-            string next = "";
-            for (char c : s) {
-                int digit = c - '0';
-                int curr = remainder * 10 + digit;
-                if (!next.empty() || curr / b > 0) {
-                    next += (char)('0' + curr / b);
-                }
-                remainder = curr % b;
-            }
-            result.push_back(remainder);
-            s = next.empty() ? "0" : next;
-        }
-        reverse(result.begin(), result.end());
-        return result;
-    }
-    
-    int countUpTo(string s, int b, int MOD) {
-        vector<int> digits = toBase(s, b);
-        int n = digits.size();
+        int res = (ansR - ansL + MOD) % MOD;
+        if (check(digitsL)) res = (res + 1) % MOD;
         
-        map<tuple<int, int, int, int>, int> memo;
-        
-        function<int(int, int, bool, bool)> dp = [&](int pos, int last_digit, bool tight, bool started) -> int {
-            if (pos == n) {
-                return started ? 1 : 0;
-            }
-            
-            auto key = make_tuple(pos, last_digit, tight, started);
-            if (memo.count(key)) {
-                return memo[key];
-            }
-            
-            int limit = tight ? digits[pos] : (b - 1);
-            long long result = 0;
-            
-            for (int digit = 0; digit <= limit; digit++) {
-                bool new_tight = tight && (digit == limit);
-                
-                if (!started) {
-                    if (digit == 0) {
-                        result = (result + dp(pos + 1, 0, new_tight, false)) % MOD;
-                    } else {
-                        result = (result + dp(pos + 1, digit, new_tight, true)) % MOD;
-                    }
-                } else {
-                    if (digit >= last_digit) {
-                        result = (result + dp(pos + 1, digit, new_tight, true)) % MOD;
-                    }
-                }
-            }
-            
-            memo[key] = result;
-            return result;
-        };
-        
-        return dp(0, 0, true, false);
+        return res;
     }
 };
 # @lc code=end
