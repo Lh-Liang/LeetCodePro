70ebdaa13c5f1fc24691eea7154241eafc0c0733
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 04:07:35 +0800

    Add solution: 3448.count-substrings-divisible-by-last-digit.cpp

--- 3448.count-substrings-divisible-by-last-digit.cpp
+++ 3448.count-substrings-divisible-by-last-digit.cpp
@@ -1,3 +1,9 @@
+#include <string>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
 #
 # @lc app=leetcode id=3448 lang=cpp
 #
@@ -8,25 +14,40 @@
 class Solution {
 public:
     long long countSubstrings(string s) {
-        long long count = 0;
-        int n = s.length();
+        long long total_count = 0;
+        // dp[d][r] stores the number of substrings ending at the previous position 
+        // that have a remainder 'r' when divided by 'd'.
+        int dp[10][10];
+        memset(dp, 0, sizeof(dp));
         
-        for (int j = 0; j < n; j++) {
-            int d = s[j] - '0';
-            if (d == 0) continue;  // Skip if last digit is 0
+        for (char c : s) {
+            int v = c - '0';
+            int next_dp[10][10];
+            memset(next_dp, 0, sizeof(next_dp));
             
-            long long num = 0;
-            long long power = 1;
-            for (int i = j; i >= 0; i--) {
-                num = (num + (s[i] - '0') * power) % d;
-                if (num == 0) {
-                    count++;
+            for (int d = 1; d <= 9; ++d) {
+                // Update existing substrings ending at the previous character
+                for (int r = 0; r < d; ++r) {
+                    if (dp[d][r] > 0) {
+                        int next_r = (r * 10 + v) % d;
+                        next_dp[d][next_r] += dp[d][r];
+                    }
                 }
-                power = (power * 10) % d;
+                // Add the new substring consisting only of the current digit
+                next_dp[d][v % d]++;
             }
+            
+            // If the current digit is the divisor (non-zero last digit),
+            // add the count of substrings that have remainder 0.
+            if (v > 0) {
+                total_count += next_dp[v][0];
+            }
+            
+            // Move next_dp state to current dp for the next iteration
+            memcpy(dp, next_dp, sizeof(dp));
         }
         
-        return count;
+        return total_count;
     }
 };
 # @lc code=end
