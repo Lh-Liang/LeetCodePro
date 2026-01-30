00f25668bca718dd4a7c6ae439c92fff5112f51c
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 18:25:03 +0800

    Add solution: 3448.count-substrings-divisible-by-last-digit.py

--- 3448.count-substrings-divisible-by-last-digit.py
+++ 3448.count-substrings-divisible-by-last-digit.py
@@ -8,18 +8,18 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         n = len(s)
-        res = 0
-        for j in range(n):
-            last_digit = int(s[j])
+        ans = 0
+        for i in range(n):
+            last_digit = int(s[i])
             if last_digit == 0:
                 continue
             mod = 0
             power = 1
-            # Check all substrings ending at j
-            for i in range(j, -1, -1):
-                mod = (int(s[i]) * power + mod) % last_digit
+            # Traverse backwards for substrings ending at i
+            for j in range(i, -1, -1):
+                mod = (int(s[j]) * power + mod) % last_digit
                 if mod == 0:
-                    res += 1
+                    ans += 1
                 power = (power * 10) % last_digit
-        return res
+        return ans
 # @lc code=end
