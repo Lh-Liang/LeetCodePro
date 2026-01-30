5c57611bc5c1ff5e14a943845d80377764eead13
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 04:14:27 +0800

    Add solution: 3457.eat-pizzas.cpp

--- 3457.eat-pizzas.cpp
+++ 3457.eat-pizzas.cpp
@@ -3,32 +3,40 @@
 #
 # [3457] Eat Pizzas!
 #
+
 # @lc code=start
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
 class Solution {
 public:
     long long maxWeight(vector<int>& pizzas) {
         int n = pizzas.size();
         sort(pizzas.begin(), pizzas.end(), greater<int>());
-        
+
         int total_days = n / 4;
         int odd_days = (total_days + 1) / 2;
         int even_days = total_days / 2;
-        
-        long long result = 0;
-        
-        // Sum top odd_days pizzas (gained on odd days)
-        for (int i = 0; i < odd_days; i++) {
-            result += pizzas[i];
+
+        long long total_weight = 0;
+
+        // On odd days, we take the heaviest available pizza as Z.
+        for (int i = 0; i < odd_days; ++i) {
+            total_weight += pizzas[i];
         }
-        
-        // Sum every second pizza from the next 2*even_days pizzas
-        // (second-max of each even-day group)
-        for (int k = 1; k <= even_days; k++) {
-            int idx = odd_days + 2 * k - 1;
-            result += pizzas[idx];
+
+        // On even days, we need a pizza to be the second heaviest (Y).
+        // We skip one (to act as Z for that day) and take the next as Y.
+        int current_idx = odd_days;
+        for (int i = 0; i < even_days; ++i) {
+            total_weight += pizzas[current_idx + 1];
+            current_idx += 2;
         }
-        
-        return result;
+
+        return total_weight;
     }
 };
 # @lc code=end
