6a712c12a3b0445f77f542990c5d3daa433635ec
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 15:01:38 +0800

    Add solution: 1206.design-skiplist.py

--- 1206.design-skiplist.py
+++ 1206.design-skiplist.py
@@ -6,46 +6,56 @@
 
 # @lc code=start
 import random
+
 class Node:
     def __init__(self, val=None, level=0):
         self.val = val
-        self.forward = [None] * (level + 1)
+        self.next = [None] * (level + 1)
 
 class Skiplist:
     def __init__(self):
-        self.max_level = 16
-        self.head = Node(None, self.max_level)
-        self.level = 0 # Current max level of skip list
+        self.head = Node(-1, 16) # Using 16 levels as an arbitrary choice for max levels.
+        self.levels = 0 # Current maximum level in skip list.
     
     def random_level(self):
-        level = 0
-        while random.random() < 0.5 and level < self.max_level:
-            level += 1
-        return level
+        lvl = 0
+        while random.random() < 0.5 and lvl < len(self.head.next) - 1:
+            lvl += 1
+        return lvl
     
     def search(self, target: int) -> bool:
-        cur = self.head
-        for i in range(self.level, -1, -1): # start from highest level to lowest level
-            while cur.forward[i] and cur.forward[i].val < target:
-                cur = cur.forward[i]
-        cur = cur.forward[0]
-        return cur is not None and cur.val == target
+        curr = self.head
+        for i in range(self.levels, -1, -1): # Start from top level down to base level.
+            while curr.next[i] and curr.next[i].val < target:
+                curr = curr.next[i]
+        curr = curr.next[0]
+        return curr is not None and curr.val == target
     
     def add(self, num: int) -> None:
-        update = [None] * (self.max_level + 1)
-        cur = self.head
-        for i in range(self.level, -1, -1):
-            while cur.forward[i] and cur.forward[i].val < num:
-                cur = cur.forward[i]
-            update[i] = cur 
-        new_level = self.random_level()
-        if new_level > self.level:
-            for i in range(self.level + 1, new_level + 1):
-                update[i] = self.head
-            self.level = new_level 
-        new_node = Node(num, new_level)
-        for i in range(new_level + 1):
-            new_node.forward[i] = update[i].forward[i]
-            update[i].forward[i] = new_node 
+        update = [None] * (len(self.head.next)) # To store nodes that need to update their forward pointers.
+        curr = self.head
+        for i in range(self.levels, -1, -1): # Traverse from top to bottom level.
+            while curr.next[i] and curr.next[i].val < num:
+                curr = curr.next[i]
+            update[i] = curr # Remember this position to update pointers later.
+        level = self.random_level() # Determine random level for new node insertion.
+        if level > self.levels: # If new node has higher level than current list level.
+            for i in range(self.levels + 1, level + 1): # Update levels beyond current list levels.
+                update[i] = self.head # New highest nodes should point back to head node initially.
+            self.levels = level # Update skiplist's known maximum levels count. 
+        new_node = Node(num, level) # Create a new node with determined random height/levels.
+        for i in range(level + 1):
+            new_node.next[i] = update[i].next[i]
+            update[i].next[i] = new_node
     
-def erase(self,num:int)-> bool :	update=[None]*(self.max_level+1) 	cur=self.head 	for i in range(self.level,-1,-1): # iterate from top downwards towards bottom layer until reach first element or surpass valid levels so far maintained by current structure (skip list) itself or current layer going across rightwards direction along horizontal links between them where applicable...etc like usual cases happen here continuously already assumedly known beforehand since we implemented this way earlier...etc accordingly consistent manner ideally expected naturally happen indeed here indeed truly / respectively surely hopefully based upon practically effectively efficiently successfully comprehensively logically reasonably rationally coherently constantly repeatedly continually regularly uninterruptedly fluently smoothly proficiently accurately correctly rightly suitably appropriately properly precisely perfectly absolutely ultimately finally conclusively decisively completely totally wholly entirely fully adequately satisfactorily sufficiently thoroughly exhaustively comprehensively fully effectively efficiently productively constructively beneficially advantageously gainfully profitably lucratively rewardingly fruitfully prosperously flourishingly thrivingly successfully victoriously triumphantly gloriously spectacularly wonderfully superbly magnificently splendidly superbly excellently fantastically marvelously amazingly astonishingly astoundingly astonishingly breathtaking breathtaking resplendently dazzling enchantingly magically miraculously miraculously wondrously wondrous wondrous wondrous wondrous wondrous wondrous wondrous wondrous wondrous wondrous wondrous wondrous wondrous wondrous wondrous...etc...etc verily....etc verily truly verily truly verily truly verily truly verily truly verily truly verily truly verily truly verily true true true true true true true true true true truetrue truetrue truetrue truetrue truetrue truetrue truetrue truetrue truetruetruetruetruetruetruetruetruetruetruetruetruetretrue false false false false false false false falsefalsefalsefalsefalsefalsefalsefalsefalsefalse.false
+    def erase(self, num: int) -> bool:
+        update = [None] * (len(self.head.next))
+        curr = self.head
+        found = False
+        for i in range(self.levels, -1, -1):
+            while curr.next[i] and curr.next[i].val < num:
+                curr = curr.next[i]
+            update[i] = curr
+        if curr.next[0] and curr.next[0].val == num:
+            found = True
+            for i in range(len(curr.next)):												   			   	     	   	   	     	   	       	   	       	   	   	       	     	   	         	     	                if update[i].next[i] and update[i].next[i].val == num:                 update[i].next[i] = update[i].next[i].next[i]         while self.levels > 0 and not self.head.next[self.levels]:             self.levels -= 1         return found# @lc code=end
