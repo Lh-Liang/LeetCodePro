91c49b29f251bdc0aa402ebbcaf765cce77dc71c
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 19:06:56 +0800

    Add solution: 3768.minimum-inversion-count-in-subarrays-of-fixed-length.py

--- 3768.minimum-inversion-count-in-subarrays-of-fixed-length.py
+++ 3768.minimum-inversion-count-in-subarrays-of-fixed-length.py
@@ -6,54 +6,52 @@
 
 # @lc code=start
 from typing import List
+from bisect import bisect_left
+
+class FenwickTree:
+    def __init__(self, size):
+        self.n = size + 2
+        self.tree = [0] * (self.n)
+    def update(self, i, delta):
+        i += 1
+        while i < self.n:
+            self.tree[i] += delta
+            i += i & -i
+    def query(self, i):
+        i += 1
+        res = 0
+        while i > 0:
+            res += self.tree[i]
+            i -= i & -i
+        return res
 
 class Solution:
     def minInversionCount(self, nums: List[int], k: int) -> int:
-        # Coordinate compression
-        arr = sorted(set(nums))
-        mapping = {v: i for i, v in enumerate(arr)}
-        nums = [mapping[v] for v in nums]
+        if k == 1:
+            return 0
         n = len(nums)
-
-        class BIT:
-            def __init__(self, size):
-                self.n = size
-                self.tree = [0] * (self.n+2)
-            def update(self, i, v):
-                i += 1
-                while i <= self.n+1:
-                    self.tree[i] += v
-                    i += i & -i
-            def query(self, i):
-                i += 1
-                res = 0
-                while i > 0:
-                    res += self.tree[i]
-                    i -= i & -i
-                return res
-            def query_range(self, l, r):
-                return self.query(r) - self.query(l-1)
-
-        def count_inversions(window):
-            bit = BIT(len(arr))
-            inv = 0
-            for x in window:
-                inv += bit.query_range(x+1, len(arr)-1)
-                bit.update(x, 1)
-            return inv
-
-        bit = BIT(len(arr))
-        window = []
+        # Coordinate compression
+        sorted_nums = sorted(set(nums))
+        mapping = {v: i for i, v in enumerate(sorted_nums)}
+        arr = [mapping[x] for x in nums]
+        max_val = len(sorted_nums)
+        ft = FenwickTree(max_val)
+        # Initialize inversion count for first window
+        inv = 0
         for i in range(k):
-            window.append(nums[i])
-        inversion = count_inversions(window)
-        answer = inversion
-
+            # count number of elements greater than current to the left
+            inv += ft.query(max_val - 1) - ft.query(arr[i])
+            ft.update(arr[i], 1)
+        min_inv = inv
         for i in range(k, n):
-            # Slide window: remove nums[i-k], add nums[i]
-            window.pop(0)
-            window.append(nums[i])
-            inversion = count_inversions(window)
-            answer = min(answer, inversion)
-        return answer
+            # Remove left-most element
+            ft.update(arr[i - k], -1)
+            # Number of elements less than current in window
+            less = ft.query(arr[i] - 1)
+            # Number of elements greater than current in window
+            greater = ft.query(max_val - 1) - ft.query(arr[i])
+            inv = inv - greater + less
+            ft.update(arr[i], 1)
+            min_inv = min(min_inv, inv)
+        return min_inv
 # @lc code=end
