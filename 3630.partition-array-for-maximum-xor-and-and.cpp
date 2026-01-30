a81ee5119f8cde670763d035ac966ed4f148b18d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 07:39:19 +0800

    Add solution: 3630.partition-array-for-maximum-xor-and-and.cpp

--- 3630.partition-array-for-maximum-xor-and-and.cpp
+++ 3630.partition-array-for-maximum-xor-and-and.cpp
@@ -1,60 +1,76 @@
-#
-# @lc app=leetcode id=3630 lang=cpp
-#
-# [3630] Partition Array for Maximum XOR and AND
-#
-# @lc code=start
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     long long maximizeXorAndXor(vector<int>& nums) {
         int n = nums.size();
-        long long maxVal = 0;
+        int num_masks = 1 << n;
         
-        // Try all possible partitions
-        for (int maskA = 0; maskA < (1 << n); maskA++) {
-            int remaining = ((1 << n) - 1) ^ maskA;
-            
-            // Iterate through all subsets of remaining
-            for (int maskB = remaining; ; maskB = (maskB - 1) & remaining) {
-                int maskC = remaining ^ maskB;
-                
-                // Calculate XOR(A)
-                long long xorA = 0;
-                for (int i = 0; i < n; i++) {
-                    if (maskA & (1 << i)) {
-                        xorA ^= nums[i];
-                    }
+        // Precompute XOR and AND for all possible subsets in O(2^n)
+        vector<unsigned int> xor_vals(num_masks, 0);
+        vector<unsigned int> and_vals(num_masks, 0);
+
+        for (int i = 0; i < n; ++i) {
+            int bit = 1 << i;
+            for (int mask = 0; mask < bit; ++mask) {
+                xor_vals[mask | bit] = xor_vals[mask] ^ (unsigned int)nums[i];
+                if (mask == 0) {
+                    and_vals[mask | bit] = (unsigned int)nums[i];
+                } else {
+                    and_vals[mask | bit] = and_vals[mask] & (unsigned int)nums[i];
                 }
-                
-                // Calculate AND(B)
-                long long andB = 0;
-                bool firstB = true;
-                for (int i = 0; i < n; i++) {
-                    if (maskB & (1 << i)) {
-                        if (firstB) {
-                            andB = nums[i];
-                            firstB = false;
-                        } else {
-                            andB &= nums[i];
-                        }
-                    }
-                }
-                
-                // Calculate XOR(C)
-                long long xorC = 0;
-                for (int i = 0; i < n; i++) {
-                    if (maskC & (1 << i)) {
-                        xorC ^= nums[i];
-                    }
-                }
-                
-                maxVal = max(maxVal, xorA + andB + xorC);
-                
-                if (maskB == 0) break;
             }
         }
-        
-        return maxVal;
+
+        long long max_total = 0;
+        int full_mask = num_masks - 1;
+
+        // Iterate through all 2^n possible subsets for subsequence B
+        // The remaining elements (R) are partitioned into A and C to maximize XOR(A) + XOR(C)
+        for (int b_mask = 0; b_mask <= full_mask; ++b_mask) {
+            unsigned int b_and = and_vals[b_mask];
+            int r_mask = full_mask ^ b_mask;
+            unsigned int r_xor = xor_vals[r_mask];
+            
+            // Mathematical identity: XOR(A) + XOR(C) = XOR(R) + 2 * (XOR(A) & XOR(C))
+            // We maximize (XOR(A) & XOR(C)) by finding max XOR sum of {nums[i] & ~XOR(R) | i in R}
+            unsigned int target_mask = ~r_xor;
+            unsigned int basis[31] = {0};
+            
+            int temp_r = r_mask;
+            while (temp_r > 0) {
+                int i = __builtin_ctz(temp_r);
+                unsigned int val = (unsigned int)nums[i] & target_mask;
+                
+                // Insert into linear basis
+                for (int j = 29; j >= 0; --j) {
+                    if (!(val & (1U << j))) continue;
+                    if (!basis[j]) {
+                        basis[j] = val;
+                        break;
+                    }
+                    val ^= basis[j];
+                }
+                temp_r &= (temp_r - 1); // Remove the lowest set bit
+            }
+
+            // Find maximum XOR sum achievable from the basis
+            unsigned int max_and_bits = 0;
+            for (int j = 29; j >= 0; --j) {
+                if ((max_and_bits ^ basis[j]) > max_and_bits) {
+                    max_and_bits ^= basis[j];
+                }
+            }
+
+            long long current_val = (long long)r_xor + 2LL * max_and_bits + b_and;
+            if (current_val > max_total) {
+                max_total = current_val;
+            }
+        }
+
+        return max_total;
     }
 };
-# @lc code=end
