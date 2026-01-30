8617c7f14d91ddd4c4823d449c8fce2110ae7aeb
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 15:25:36 +0800

    Add solution: 3434.maximum-frequency-after-subarray-operation.py

--- 3434.maximum-frequency-after-subarray-operation.py
+++ 3434.maximum-frequency-after-subarray-operation.py
@@ -13,11 +13,9 @@ class Solution:
         max_freq = 0
         for right in range(len(nums)):
             total += nums[right]
-            # Check if it's feasible to turn entire window into nums[right]
-            while nums[right] * (right - left + 1) - total > k:
+            while (right - left + 1) * nums[right] - total > k:
                 total -= nums[left]
                 left += 1
-            # Update maximum frequency found so far.
             max_freq = max(max_freq, right - left + 1)
         return max_freq
 # @lc code=end
