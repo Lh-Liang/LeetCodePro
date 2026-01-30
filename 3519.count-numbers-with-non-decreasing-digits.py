9dff0f6b7f8e111add7a2bbb39b6cb012a7f6c27
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 15:47:48 +0800

    Add solution: 3519.count-numbers-with-non-decreasing-digits.py

--- 3519.count-numbers-with-non-decreasing-digits.py
+++ 3519.count-numbers-with-non-decreasing-digits.py
@@ -3,31 +3,29 @@
 #
 # [3519] Count Numbers with Non-Decreasing Digits
 #
+
 # @lc code=start
 class Solution:
     def countNumbers(self, l: str, r: str, b: int) -> int:
         MOD = 10**9 + 7
         
-        def is_non_decreasing(num_str):
-            return all(num_str[i] <= num_str[i+1] for i in range(len(num_str) - 1))
+        def is_non_decreasing(num_base_b):
+            return all(x <= y for x, y in zip(num_base_b, num_base_b[1:]))
         
-        def convert_to_base(n, b):
-            if n == 0: return '0'
-            digits = []
-            while n:
-                digits.append(int(n % b))
-                n //= b
-            return ''.join(str(x) for x in reversed(digits))
+        def convert_to_base(n, base):
+            result = []
+            while n > 0:
+                result.append(n % base)
+                n //= base
+            return result[::-1]
         
         l_int = int(l)
         r_int = int(r)
         count = 0
         
-        for num in range(l_int, r_int + 1):
-            base_b_representation = convert_to_base(num, b)
-            if is_non_decreasing(base_b_representation):
-                count += 1
-                count %= MOD
-                
-        return count
-# @lc code=end
+        for i in range(l_int, r_int + 1):
+            num_base_b = convert_to_base(i, b)
+            if is_non_decreasing(num_base_b):
+                count = (count + 1) % MOD
+        
+        return count # @lc code=end
