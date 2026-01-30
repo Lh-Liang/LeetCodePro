1aa86f797819db1c234b1c6db1e8446f8b06a67f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 06:39:12 +0800

    Add solution: 3510.minimum-pair-removal-to-sort-array-ii.cpp

--- 3510.minimum-pair-removal-to-sort-array-ii.cpp
+++ 3510.minimum-pair-removal-to-sort-array-ii.cpp
@@ -4,45 +4,94 @@
 # [3510] Minimum Pair Removal to Sort Array II
 #
 # @lc code=start
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct Node {
+    long long val;
+    int pos;
+    int prev = -1;
+    int next = -1;
+    bool deleted = false;
+};
+
+struct Pair {
+    long long sum;
+    int pos;
+    int L_idx, R_idx;
+
+    bool operator>(const Pair& other) const {
+        if (sum != other.sum) return sum > other.sum;
+        return pos > other.pos;
+    }
+};
+
 class Solution {
 public:
     int minimumPairRemoval(vector<int>& nums) {
         int n = nums.size();
         if (n <= 1) return 0;
-        
-        vector<long long> arr(nums.begin(), nums.end());
-        
-        auto isSorted = [](const vector<long long>& a) {
-            for (size_t i = 1; i < a.size(); i++) {
-                if (a[i] < a[i-1]) return false;
-            }
-            return true;
-        };
-        
-        if (isSorted(arr)) return 0;
-        
-        int operations = 0;
-        
-        while (!isSorted(arr)) {
-            // Find minimum sum pair (leftmost if tie)
-            size_t min_idx = 0;
-            long long min_sum = LLONG_MAX;
-            
-            for (size_t i = 0; i < arr.size() - 1; i++) {
-                long long sum = arr[i] + arr[i+1];
-                if (sum < min_sum) {
-                    min_sum = sum;
-                    min_idx = i;
-                }
-            }
-            
-            // Merge arr[min_idx] and arr[min_idx+1]
-            arr[min_idx] = arr[min_idx] + arr[min_idx + 1];
-            arr.erase(arr.begin() + min_idx + 1);
-            operations++;
+
+        vector<Node> nodes(n);
+        for (int i = 0; i < n; ++i) {
+            nodes[i].val = nums[i];
+            nodes[i].pos = i;
+            if (i > 0) nodes[i].prev = i - 1;
+            if (i < n - 1) nodes[i].next = i + 1;
         }
-        
-        return operations;
+
+        int bad_count = 0;
+        auto isBad = [&](int i, int j) {
+            if (i == -1 || j == -1) return false;
+            return nodes[i].val > nodes[j].val;
+        };
+
+        priority_queue<Pair, vector<Pair>, greater<Pair>> pq;
+        for (int i = 0; i < n - 1; ++i) {
+            if (isBad(i, i + 1)) bad_count++;
+            pq.push({nodes[i].val + nodes[i + 1].val, nodes[i].pos, i, i + 1});
+        }
+
+        int ops = 0;
+        while (bad_count > 0 && !pq.empty()) {
+            Pair top = pq.top();
+            pq.pop();
+
+            int L = top.L_idx;
+            int R = top.R_idx;
+
+            // Validation: ensure nodes aren't deleted and are still adjacent
+            if (nodes[L].deleted || nodes[R].deleted || nodes[L].next != R || (nodes[L].val + nodes[R].val) != top.sum) {
+                continue;
+            }
+
+            ops++;
+            int P = nodes[L].prev;
+            int N = nodes[R].next;
+
+            // Remove old 'bad' contributions
+            if (isBad(P, L)) bad_count--;
+            if (isBad(L, R)) bad_count--;
+            if (isBad(R, N)) bad_count--;
+
+            // Perform merge into L
+            nodes[L].val += nodes[R].val;
+            nodes[L].next = N;
+            if (N != -1) nodes[N].prev = L;
+            nodes[R].deleted = true;
+
+            // Add new 'bad' contributions
+            if (isBad(P, L)) bad_count++;
+            if (isBad(L, N)) bad_count++;
+
+            // Push new potential pairs
+            if (P != -1) pq.push({nodes[P].val + nodes[L].val, nodes[P].pos, P, L});
+            if (N != -1) pq.push({nodes[L].val + nodes[N].val, nodes[L].pos, L, N});
+        }
+
+        return ops;
     }
 };
 # @lc code=end
