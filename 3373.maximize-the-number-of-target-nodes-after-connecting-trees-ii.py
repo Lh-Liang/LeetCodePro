4d100693ad9b8a362d263874e6e721bdaa90f147
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 18:16:58 +0800

    Add solution: 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py

--- 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
+++ 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
@@ -3,41 +3,52 @@
 #
 # [3373] Maximize the Number of Target Nodes After Connecting Trees II
 #
+
 # @lc code=start
 from typing import List
-from collections import deque
+from collections import deque, defaultdict
 
 class Solution:
     def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
         n = len(edges1) + 1
         m = len(edges2) + 1
-
-        def get_parity_counts(edges: List[List[int]], n: int):
-            g = [[] for _ in range(n)]
-            for u, v in edges:
-                g[u].append(v)
-                g[v].append(u)
-            parity = [None] * n
-            queue = deque()
-            parity[0] = 0
-            queue.append(0)
-            cnt = [0, 0]
-            cnt[0] += 1
-            while queue:
-                u = queue.popleft()
-                for v in g[u]:
-                    if parity[v] is None:
-                        parity[v] = parity[u] ^ 1
-                        cnt[parity[v]] += 1
-                        queue.append(v)
-            return parity, cnt
-
-        parity1, cnt1 = get_parity_counts(edges1, n)
-        parity2, cnt2 = get_parity_counts(edges2, m)
-
+        # Build graphs
+        tree1 = [[] for _ in range(n)]
+        for a, b in edges1:
+            tree1[a].append(b)
+            tree1[b].append(a)
+        tree2 = [[] for _ in range(m)]
+        for u, v in edges2:
+            tree2[u].append(v)
+            tree2[v].append(u)
+        # Get even/odd depths counts for tree1
+        depth1 = [0] * n
+        cnt1 = [0, 0] # cnt1[0]: even, cnt1[1]: odd
+        def dfs1(u, p, d):
+            depth1[u] = d
+            cnt1[d % 2] += 1
+            for v in tree1[u]:
+                if v != p:
+                    dfs1(v, u, d+1)
+        dfs1(0, -1, 0)
+        # Get even/odd depths counts for tree2
+        depth2 = [0] * m
+        cnt2 = [0, 0]
+        def dfs2(u, p, d):
+            depth2[u] = d
+            cnt2[d % 2] += 1
+            for v in tree2[u]:
+                if v != p:
+                    dfs2(v, u, d+1)
+        dfs2(0, -1, 0)
+        # For each node in tree1, find max possible target nodes
         answer = []
         for i in range(n):
-            p = parity1[i]
-            answer.append(max(cnt1[p] + cnt2[0], cnt1[1-p] + cnt2[1]))
+            parity_i = depth1[i] % 2
+            # Option 1: connect to even node in tree2
+            total_even = cnt1[parity_i] + cnt2[0]
+            # Option 2: connect to odd node in tree2
+            total_odd = cnt1[1 - parity_i] + cnt2[1]
+            answer.append(max(total_even, total_odd))
         return answer
 # @lc code=end
