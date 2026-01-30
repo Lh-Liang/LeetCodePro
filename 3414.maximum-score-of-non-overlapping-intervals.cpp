61180abf8ca8eed07442fce584b1a1f8f5386e98
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 03:50:47 +0800

    Add solution: 3414.maximum-score-of-non-overlapping-intervals.cpp

--- 3414.maximum-score-of-non-overlapping-intervals.cpp
+++ 3414.maximum-score-of-non-overlapping-intervals.cpp
@@ -3,88 +3,72 @@
 #
 # [3414] Maximum Score of Non-overlapping Intervals
 #
+
 # @lc code=start
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Interval {
+    int l, r, w, id;
+};
+
 class Solution {
 public:
     vector<int> maximumWeight(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        
-        // Create indexed intervals and sort by end time
-        vector<array<int, 4>> arr(n);
-        for (int i = 0; i < n; i++) {
-            arr[i] = {intervals[i][1], intervals[i][0], intervals[i][2], i};
+        vector<Interval> ivs(n);
+        for (int i = 0; i < n; ++i) {
+            ivs[i] = {intervals[i][0], intervals[i][1], intervals[i][2], i};
         }
-        sort(arr.begin(), arr.end(), [](const auto& a, const auto& b) {
-            if (a[0] != b[0]) return a[0] < b[0];
-            return a[3] < b[3];
+
+        // Sort by start time
+        sort(ivs.begin(), ivs.end(), [](const Interval& a, const Interval& b) {
+            return a.l < b.l;
         });
-        
-        // dp[i][k] = {weight, indices}
-        vector<vector<pair<long long, vector<int>>>> dp(n + 1, vector<pair<long long, vector<int>>>(5, {-1, {}}));
-        dp[0][0] = {0, {}};
-        
-        for (int i = 1; i <= n; i++) {
-            int end = arr[i-1][0];
-            int start = arr[i-1][1];
-            int weight = arr[i-1][2];
-            int idx = arr[i-1][3];
-            
-            // Don't take interval i-1
-            for (int k = 0; k <= 4; k++) {
-                if (dp[i-1][k].first >= 0) {
-                    if (dp[i][k].first < 0 || 
-                        dp[i-1][k].first > dp[i][k].first ||
-                        (dp[i-1][k].first == dp[i][k].first && dp[i-1][k].second < dp[i][k].second)) {
-                        dp[i][k] = dp[i-1][k];
-                    }
-                }
+
+        // dp[k][i] stores {max_weight, sorted_indices}
+        // Using k from 1 to 4 and i from n down to 0
+        using State = pair<long long, vector<int>>;
+        auto compare = [](const State& a, const State& b) {
+            if (a.first != b.first) return a.first < b.first;
+            return a.second > b.second; // Lexicographically smaller vector is "greater" in our max search
+        };
+
+        vector<vector<State>> dp(5, vector<State>(n + 1, {0, {}}));
+
+        // Precompute next interval index using binary search
+        vector<int> next_idx(n);
+        for (int i = 0; i < n; ++i) {
+            int low = i + 1, high = n;
+            while (low < high) {
+                int mid = low + (high - low) / 2;
+                if (ivs[mid].l > ivs[i].r) high = mid;
+                else low = mid + 1;
             }
-            
-            // Take interval i-1: binary search
-            int j = 0;
-            if (i > 1) {
-                int left = 0, right = i - 2;
-                while (left <= right) {
-                    int mid = left + (right - left) / 2;
-                    if (arr[mid][0] < start) {
-                        j = mid + 1;
-                        left = mid + 1;
-                    } else {
-                        right = mid - 1;
-                    }
-                }
-            }
-            
-            for (int k = 0; k < 4; k++) {
-                if (dp[j][k].first >= 0) {
-                    long long new_weight = dp[j][k].first + weight;
-                    vector<int> new_indices = dp[j][k].second;
-                    new_indices.push_back(idx);
-                    sort(new_indices.begin(), new_indices.end());
-                    
-                    if (dp[i][k+1].first < 0 ||
-                        new_weight > dp[i][k+1].first ||
-                        (new_weight == dp[i][k+1].first && new_indices < dp[i][k+1].second)) {
-                        dp[i][k+1] = {new_weight, new_indices};
-                    }
+            next_idx[i] = low;
+        }
+
+        for (int k = 1; k <= 4; ++k) {
+            for (int i = n - 1; i >= 0; --i) {
+                // Option 1: Skip current interval
+                dp[k][i] = dp[k][i+1];
+
+                // Option 2: Take current interval
+                long long current_w = ivs[i].w + dp[k-1][next_idx[i]].first;
+                vector<int> current_indices = {ivs[i].id};
+                for (int idx : dp[k-1][next_idx[i]].second) current_indices.push_back(idx);
+                sort(current_indices.begin(), current_indices.end());
+
+                State take_state = {current_w, current_indices};
+                if (compare(dp[k][i], take_state)) {
+                    dp[k][i] = take_state;
                 }
             }
         }
-        
-        // Find best result
-        long long max_weight = -1;
-        vector<int> result;
-        for (int k = 0; k <= 4; k++) {
-            if (dp[n][k].first >= 0 && 
-                (max_weight < 0 || 
-                 dp[n][k].first > max_weight ||
-                 (dp[n][k].first == max_weight && dp[n][k].second < result))) {
-                max_weight = dp[n][k].first;
-                result = dp[n][k].second;
-            }
-        }
-        
-        return result;
+
+        return dp[4][0].second;
     }
 };
 # @lc code=end
