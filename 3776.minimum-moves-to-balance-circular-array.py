c34b3ff82cb207e06c74898905da1d0157671096
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 23:38:16 +0800

    Add solution: 3776.minimum-moves-to-balance-circular-array.py

--- 3776.minimum-moves-to-balance-circular-array.py
+++ 3776.minimum-moves-to-balance-circular-array.py
@@ -7,33 +7,20 @@
 # @lc code=start
 class Solution:
     def minMoves(self, balance: List[int]) -> int:
+        total_balance = sum(balance)
+        if total_balance < 0:
+            return -1  # Impossible to make all balances non-negative
         n = len(balance)
-        total = sum(balance)
-        if total < 0:
-            return -1
-        if min(balance) >= 0:
-            return 0
-        neg_idx = -1
-        for i, x in enumerate(balance):
-            if x < 0:
-                neg_idx = i
-                break
-        need = -balance[neg_idx]
+        index = balance.index(min(balance))  # Find the index with negative balance
         moves = 0
-        l, r = (neg_idx - 1) % n, (neg_idx + 1) % n
-        while need > 0:
-            # Move from the side with more units
-            if balance[l] >= balance[r]:
-                take = min(need, balance[l])
-                moves += take * min((neg_idx - l) % n, (l - neg_idx) % n)
-                balance[l] -= take
-                need -= take
-                l = (l - 1) % n
-            else:
-                take = min(need, balance[r])
-                moves += take * min((r - neg_idx) % n, (neg_idx - r) % n)
-                balance[r] -= take
-                need -= take
-                r = (r + 1) % n
-        return moves
+        for i in range(index + 1, index + n):  # Traverse circularly both sides
+            pos = i % n
+            if balance[pos] > 0:
+                move = min(balance[pos], -balance[index])
+                moves += move
+                balance[pos] -= move
+                balance[index] += move
+            if balance[index] >= 0:
+                break
+        return moves if all(b >= 0 for b in balance) else -1
 # @lc code=end
