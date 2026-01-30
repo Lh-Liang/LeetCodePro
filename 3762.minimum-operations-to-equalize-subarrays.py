ce5a2592dc91908c731f1af7c535e0cab7e97444
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 17:36:08 +0800

    Add solution: 3762.minimum-operations-to-equalize-subarrays.py

--- 3762.minimum-operations-to-equalize-subarrays.py
+++ 3762.minimum-operations-to-equalize-subarrays.py
@@ -7,17 +7,17 @@
 # @lc code=start
 class Solution:
     def minOperations(self, nums: List[int], k: int, queries: List[List[int]]) -> List[int]:
-        ans = []
+        res = []
         for li, ri in queries:
-            subarray = nums[li:ri+1]
-            remainders = [num % k for num in subarray]
-            if len(set(remainders)) != 1:
-                ans.append(-1)
+            sub = nums[li:ri+1]
+            mods = [x % k for x in sub]
+            if not all(m == mods[0] for m in mods):
+                res.append(-1)
                 continue
-            # Sort subarray to find median more easily
-            subarray.sort()
-            median = subarray[len(subarray) // 2]
-            operations = sum(abs(num - median) // k for num in subarray)
-            ans.append(operations)
-        return ans
+            arr = sorted([x // k for x in sub])
+            m = len(arr)
+            median = arr[m//2]
+            ops = sum(abs(x - median) for x in arr)
+            res.append(ops)
+        return res
 # @lc code=end
