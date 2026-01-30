3852aacd7aae5fc8b6de073e1ded3b77602438cf
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 15:31:34 +0800

    Add solution: 3457.eat-pizzas.py

--- 3457.eat-pizzas.py
+++ 3457.eat-pizzas.py
@@ -1,25 +1,21 @@
+#
 # @lc app=leetcode id=3457 lang=python3
 #
 # [3457] Eat Pizzas!
 #
 
 # @lc code=start
-from typing import List
-
 class Solution:
     def maxWeight(self, pizzas: List[int]) -> int:
-        # Sort pizzas in descending order
-        sorted_pizzas = sorted(pizzas, reverse=True)
-        total_weight_gain = 0
-        
-        # Iterate over groups of four pizzas at a time
-        for i in range(0, len(sorted_pizzas), 4):
-            # Odd days (1-indexed): Gain weight of heaviest pizza Z
-            if (i // 4) % 2 == 0:
-                total_weight_gain += sorted_pizzas[i]
-            # Even days (1-indexed): Gain weight of second heaviest pizza Y
-            else:
-                total_weight_gain += sorted_pizzas[i + 1]
-        
-        return total_weight_gain
+        # Sort pizzas by weight in descending order to maximize gains
+        pizzas.sort(reverse=True)
+        total_gain = 0
+        # Process every 4 pizzas as a batch to determine daily gains
+        for i in range(0, len(pizzas), 4):
+            # Determine if it's an odd or even day based on batch index
+            if (i // 4) % 2 == 0: # Odd day (1-indexed)
+                total_gain += pizzas[i] # Add largest pizza's weight (Z)
+            else: # Even day
+                total_gain += pizzas[i + 1] # Add second largest pizza's weight (Y)
+        return total_gain
 # @lc code=end
