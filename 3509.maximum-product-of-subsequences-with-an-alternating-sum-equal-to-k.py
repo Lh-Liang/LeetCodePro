7f479c4115a296e5c4c3f56cf0745c0c751e2eea
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 18:36:01 +0800

    Add solution: 3509.maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k.py

--- 3509.maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k.py
+++ 3509.maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k.py
@@ -9,29 +9,30 @@ class Solution:
     def maxProduct(self, nums: List[int], k: int, limit: int) -> int:
         from collections import defaultdict
         n = len(nums)
-        # dp[(alt_sum, parity)] = max_product
-        dp = defaultdict(int)
-        res = -1
-        for i, num in enumerate(nums):
-            next_dp = dp.copy()
-            # Start new subsequence at index i
-            alt_sum = num
-            parity = 0  # first element is at even index in subsequence
-            prod = num
-            if prod <= limit:
-                next_dp[(alt_sum, parity)] = max(next_dp.get((alt_sum, parity), 0), prod)
-                if alt_sum == k:
-                    res = max(res, prod)
-            # Extend all existing subsequences by including nums[i]
-            for (old_alt, old_parity), old_prod in dp.items():
-                new_parity = old_parity ^ 1  # flip parity when adding an element
-                new_alt = old_alt + num if old_parity == 0 else old_alt - num
-                new_prod = old_prod * num
-                if new_prod <= limit:
-                    if next_dp[(new_alt, new_parity)] < new_prod:
-                        next_dp[(new_alt, new_parity)] = new_prod
-                        if new_alt == k:
-                            res = max(res, new_prod)
-            dp = next_dp
-        return res
+        memo = [defaultdict(dict) for _ in range(n+1)]
+        # memo[i][sum][parity]: max product for using nums[i:] with this sum and parity
+        # parity=0: next index is even in the subsequence, parity=1: odd
+        ans = -1
+        def dfs(i, curr_sum, parity, prod):
+            nonlocal ans
+            if prod > limit:
+                return
+            if i == n:
+                if prod > 0 and curr_sum == k:
+                    ans = max(ans, prod)
+                return
+            # Prune dominated states
+            key = (curr_sum, parity)
+            if prod <= memo[i].get(curr_sum, {}).get(parity, 0):
+                return
+            memo[i].setdefault(curr_sum, {})[parity] = prod
+            # Option 1: skip nums[i]
+            dfs(i+1, curr_sum, parity, prod)
+            # Option 2: take nums[i] at this parity
+            if parity == 0:
+                dfs(i+1, curr_sum + nums[i], 1, prod * nums[i] if prod else nums[i])
+            else:
+                dfs(i+1, curr_sum - nums[i], 0, prod * nums[i] if prod else nums[i])
+        dfs(0, 0, 0, 0)
+        return ans
 # @lc code=end
