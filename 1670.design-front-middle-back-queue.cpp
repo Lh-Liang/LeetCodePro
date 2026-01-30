56f00b8cf8729686ac5a2523e9cfa5ef208fbc27
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 03:29:30 +0800

    Add solution: 1670.design-front-middle-back-queue.cpp

--- 1670.design-front-middle-back-queue.cpp
+++ 1670.design-front-middle-back-queue.cpp
@@ -3,11 +3,14 @@
 #
 # [1670] Design Front Middle Back Queue
 #
+
 # @lc code=start
+#include <deque>
+
 class FrontMiddleBackQueue {
-private:
-    deque<int> left, right;
-    
+    std::deque<int> left, right;
+
+    // Maintains the invariant: left.size() == right.size() OR right.size() == left.size() + 1
     void balance() {
         if (left.size() > right.size()) {
             right.push_front(left.back());
@@ -17,30 +20,31 @@ private:
             right.pop_front();
         }
     }
-    
+
 public:
     FrontMiddleBackQueue() {
-        
     }
-    
+
     void pushFront(int val) {
         left.push_front(val);
         balance();
     }
-    
+
     void pushMiddle(int val) {
         if (left.size() == right.size()) {
             right.push_front(val);
         } else {
             left.push_back(val);
         }
+        // balance() not strictly needed here if logic is precise, but kept for safety
+        balance();
     }
-    
+
     void pushBack(int val) {
         right.push_back(val);
         balance();
     }
-    
+
     int popFront() {
         if (left.empty() && right.empty()) return -1;
         int val;
@@ -54,7 +58,7 @@ public:
         balance();
         return val;
     }
-    
+
     int popMiddle() {
         if (left.empty() && right.empty()) return -1;
         int val;
@@ -68,17 +72,11 @@ public:
         balance();
         return val;
     }
-    
+
     int popBack() {
         if (left.empty() && right.empty()) return -1;
-        int val;
-        if (right.empty()) {
-            val = left.back();
-            left.pop_back();
-        } else {
-            val = right.back();
-            right.pop_back();
-        }
+        int val = right.back();
+        right.pop_back();
         balance();
         return val;
     }
