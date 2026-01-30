8248908f2a60c2cd23686726177b5ad92fed3f98
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 06:49:30 +0800

    Add solution: 3515.shortest-path-in-a-weighted-tree.cpp

--- 3515.shortest-path-in-a-weighted-tree.cpp
+++ 3515.shortest-path-in-a-weighted-tree.cpp
@@ -3,71 +3,79 @@
 #
 # [3515] Shortest Path in a Weighted Tree
 #
+
 # @lc code=start
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+class FenwickTree {
+    int n;
+    vector<long long> tree;
+public:
+    FenwickTree(int n) : n(n), tree(n + 2, 0) {}
+    void add(int i, long long val) {
+        for (; i <= n; i += i & -i) tree[i] += val;
+    }
+    long long query(int i) {
+        long long sum = 0;
+        for (; i > 0; i -= i & -i) sum += tree[i];
+        return sum;
+    }
+};
+
 class Solution {
 public:
     vector<int> treeQueries(int n, vector<vector<int>>& edges, vector<vector<int>>& queries) {
-        // Build adjacency list with edge indices
         vector<vector<pair<int, int>>> adj(n + 1);
-        map<pair<int, int>, int> edgeIndex;
-        
-        for (int i = 0; i < edges.size(); i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            
-            adj[u].push_back({v, i});
-            adj[v].push_back({u, i});
-            
-            edgeIndex[{min(u, v), max(u, v)}] = i;
+        for (const auto& e : edges) {
+            adj[e[0]].push_back({e[1], e[2]});
+            adj[e[1]].push_back({e[0], e[2]});
         }
+
+        vector<int> in(n + 1), out(n + 1), depth(n + 1);
+        vector<int> parent_edge_weight(n + 1, 0);
+        int timer = 0;
         
-        vector<int> edgeWeights(edges.size());
-        for (int i = 0; i < edges.size(); i++) {
-            edgeWeights[i] = edges[i][2];
-        }
-        
-        vector<int> result;
-        
-        for (const auto& query : queries) {
-            if (query[0] == 1) {
-                int u = query[1];
-                int v = query[2];
-                int w = query[3];
-                
-                int idx = edgeIndex[{min(u, v), max(u, v)}];
-                edgeWeights[idx] = w;
-            } else {
-                int target = query[1];
-                vector<bool> visited(n + 1, false);
-                int dist = dfs(1, target, adj, edgeWeights, visited);
-                result.push_back(dist);
-            }
-        }
-        
-        return result;
-    }
-    
-private:
-    int dfs(int node, int target, const vector<vector<pair<int, int>>>& adj, 
-            const vector<int>& edgeWeights, vector<bool>& visited) {
-        if (node == target) {
-            return 0;
-        }
-        
-        visited[node] = true;
-        
-        for (const auto& p : adj[node]) {
-            int neighbor = p.first;
-            int edgeIdx = p.second;
-            if (!visited[neighbor]) {
-                int dist = dfs(neighbor, target, adj, edgeWeights, visited);
-                if (dist >= 0) {
-                    return dist + edgeWeights[edgeIdx];
+        function<void(int, int, int)> dfs = [&](int u, int p, int d) {
+            in[u] = ++timer;
+            depth[u] = d;
+            for (auto& edge : adj[u]) {
+                int v = edge.first;
+                int w = edge.second;
+                if (v != p) {
+                    parent_edge_weight[v] = w;
+                    dfs(v, u, d + 1);
                 }
             }
+            out[u] = timer;
+        };
+        dfs(1, 0, 0);
+
+        FenwickTree ft(n);
+        for (int i = 1; i <= n; ++i) {
+            if (parent_edge_weight[i] != 0) {
+                ft.add(in[i], parent_edge_weight[i]);
+                ft.add(out[i] + 1, -(long long)parent_edge_weight[i]);
+            }
         }
-        
-        return -1;
+
+        vector<int> ans;
+        for (const auto& q : queries) {
+            if (q[0] == 1) {
+                int u = q[1], v = q[2], next_w = q[3];
+                int child = (depth[u] > depth[v]) ? u : v;
+                int diff = next_w - parent_edge_weight[child];
+                parent_edge_weight[child] = next_w;
+                ft.add(in[child], diff);
+                ft.add(out[child] + 1, -(long long)diff);
+            } else {
+                ans.push_back((int)ft.query(in[q[1]]));
+            }
+        }
+        return ans;
     }
 };
 # @lc code=end
