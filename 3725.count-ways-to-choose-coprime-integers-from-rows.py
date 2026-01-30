ef145fcf59bc7109abe4639c84015e486d7360be
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-30 18:59:49 +0800

    Add solution: 3725.count-ways-to-choose-coprime-integers-from-rows.py

--- 3725.count-ways-to-choose-coprime-integers-from-rows.py
+++ 3725.count-ways-to-choose-coprime-integers-from-rows.py
@@ -5,30 +5,22 @@
 #
 
 # @lc code=start
-from typing import List
-from math import gcd
-from collections import Counter
-
 class Solution:
     def countCoprime(self, mat: List[List[int]]) -> int:
+        from math import gcd
         MOD = 10**9 + 7
-        m = len(mat)
-        maxv = 150
-        # dp[g]: number of ways to pick from processed rows so that GCD is exactly g
-        count = Counter(mat[0])
-        dp = [0] * (maxv + 1)
-        for v in count:
-            dp[v] = count[v]
+        m, n = len(mat), len(mat[0])
+        max_val = 150
+        dp = [0] * (max_val + 1)
+        for num in mat[0]:
+            dp[num] += 1
         for i in range(1, m):
-            ndp = [0] * (maxv + 1)
-            cur_count = Counter(mat[i])
-            for g in range(1, maxv + 1):
-                if dp[g] == 0:
-                    continue
-                for v in cur_count:
-                    ng = gcd(g, v)
-                    ndp[ng] = (ndp[ng] + dp[g] * cur_count[v]) % MOD
+            ndp = [0] * (max_val + 1)
+            for prev_g in range(1, max_val + 1):
+                if dp[prev_g]:
+                    for num in mat[i]:
+                        new_g = gcd(prev_g, num)
+                        ndp[new_g] = (ndp[new_g] + dp[prev_g]) % MOD
             dp = ndp
-        # Ensure the result corresponds to the count of ways with GCD 1
-        return dp[1]
+        return dp[1] % MOD
 # @lc code=end
