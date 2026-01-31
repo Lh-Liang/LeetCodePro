395a3987dcec1744786426061990117bf31c44cf
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 09:30:43 +0800

    Add solution: 3785.minimum-swaps-to-avoid-forbidden-values.cpp

--- 3785.minimum-swaps-to-avoid-forbidden-values.cpp
+++ 3785.minimum-swaps-to-avoid-forbidden-values.cpp
@@ -1,42 +1,47 @@
-#
-# @lc app=leetcode id=3785 lang=cpp
-#
-# [3785] Minimum Swaps to Avoid Forbidden Values
-#
-# @lc code=start
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     int minSwaps(vector<int>& nums, vector<int>& forbidden) {
         int n = nums.size();
-        unordered_map<int, int> num_count;
-        unordered_map<int, int> t_count;
-        unordered_map<int, int> conflict_count;
-        int total_bad = 0;
-        int max_conflict = 0;
+        
+        // Global Feasibility: For any value x, count(nums, x) + count(forbidden, x) <= n
+        unordered_map<int, int> num_freq;
+        unordered_map<int, int> forbidden_freq;
         for (int i = 0; i < n; ++i) {
-            num_count[nums[i]]++;
-            t_count[forbidden[i]]++;
+            num_freq[nums[i]]++;
+            forbidden_freq[forbidden[i]]++;
+        }
+        
+        for (auto const& it : num_freq) {
+            if (it.second > n - forbidden_freq[it.first]) return -1;
+        }
+
+        // Identify conflicts (bad indices)
+        int total_bad = 0;
+        int max_bad_freq = 0;
+        unordered_map<int, int> bad_val_freq;
+        
+        for (int i = 0; i < n; ++i) {
             if (nums[i] == forbidden[i]) {
                 total_bad++;
-                conflict_count[nums[i]]++;
+                int freq = ++bad_val_freq[nums[i]];
+                if (freq > max_bad_freq) max_bad_freq = freq;
             }
         }
-        for (auto& p : t_count) {
-            int v = p.first;
-            int tv = p.second;
-            int cv = num_count[v];
-            if (tv > n - cv) {
-                return -1;
-            }
-        }
-        for (auto& p : conflict_count) {
-            max_conflict = max(max_conflict, p.second);
-        }
-        if (total_bad == 0) {
-            return 0;
-        }
-        int mm = min(total_bad / 2, total_bad - max_conflict);
-        return total_bad - mm;
+
+        if (total_bad == 0) return 0;
+
+        // The minimum swaps is constrained by two factors:
+        // 1. Each swap can fix at most 2 conflicts: ceil(total_bad / 2)
+        // 2. A dominant value appearing M times in conflicts requires at least M swaps
+        int res = (total_bad + 1) / 2;
+        if (max_bad_freq > res) res = max_bad_freq;
+        
+        return res;
     }
 };
-# @lc code=end
