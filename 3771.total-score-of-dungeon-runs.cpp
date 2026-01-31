c125c8dad926abbc0bfbb77d18b3def33e4d5b6c
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 09:07:51 +0800

    Add solution: 3771.total-score-of-dungeon-runs.cpp

--- 3771.total-score-of-dungeon-runs.cpp
+++ 3771.total-score-of-dungeon-runs.cpp
@@ -3,25 +3,40 @@
 #
 # [3771] Total Score of Dungeon Runs
 #
-
 # @lc code=start
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     long long totalScore(int hp, vector<int>& damage, vector<int>& requirement) {
         int n = damage.size();
-        vector<long long> prefix(n + 1, 0);
-        for (int i = 1; i <= n; ++i) {
-            prefix[i] = prefix[i - 1] + damage[i - 1];
+        // S[i] stores the prefix sum of damage from room 1 to i.
+        // S[0] = 0, S[1] = damage[0], ..., S[n] = sum(damage[0...n-1])
+        vector<long long> S(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            S[i + 1] = S[i] + (long long)damage[i];
         }
+        
         long long total = 0;
-        for (int k = 1; k <= n; ++k) {
-            long long T = (long long)requirement[k - 1] + prefix[k] - hp;
-            auto it = lower_bound(prefix.begin(), prefix.begin() + k, T);
-            int idx = it - prefix.begin();
-            if (idx < k) {
-                total += (k - idx);
-            }
+        for (int i = 1; i <= n; ++i) {
+            // Condition for starting room j to earn a point at room i:
+            // hp - (S[i] - S[j-1]) >= requirement[i-1]
+            // S[j-1] >= S[i] + requirement[i-1] - hp
+            long long Ri = (long long)S[i] + requirement[i - 1] - hp;
+            
+            // Since S is strictly increasing, find the number of k in [0, i-1] s.t. S[k] >= Ri
+            // lower_bound gives the first index k where S[k] >= Ri
+            auto it = lower_bound(S.begin(), S.begin() + i, Ri);
+            int k = (int)(it - S.begin());
+            
+            // The indices k, k+1, ..., i-1 all satisfy S[index] >= Ri.
+            // Total valid indices = (i-1) - k + 1 = i - k.
+            total += (long long)(i - k);
         }
+        
         return total;
     }
 };
