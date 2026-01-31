188af828ea302c48440ecd93274428aec10783a5
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-31 08:53:11 +0800

    Add solution: 3757.number-of-effective-subsequences.cpp

--- 3757.number-of-effective-subsequences.cpp
+++ 3757.number-of-effective-subsequences.cpp
@@ -5,58 +5,71 @@
 #
 
 # @lc code=start
-#include <bits/stdc++.h>
+#include <vector>
+#include <cmath>
+
 using namespace std;
 
 class Solution {
 public:
     int countEffective(vector<int>& nums) {
-        const long long MOD = 1000000007LL;
+        const int MOD = 1e9 + 7;
         int n = nums.size();
         int total_or = 0;
         for (int x : nums) total_or |= x;
+
+        if (total_or == 0) return 0;
+
+        // Map bits of total_or to a compact range [0, m-1]
         vector<int> bits;
-        for (int b = 0; b < 30; ++b) {
-            if (total_or & (1 << b)) bits.push_back(b);
+        for (int i = 0; i < 30; ++i) {
+            if ((total_or >> i) & 1) bits.push_back(i);
         }
+
         int m = bits.size();
-        int M = 1 << m;
-        vector<long long> freq(M, 0);
+        int num_masks = 1 << m;
+        vector<int> freq(num_masks, 0);
+
         for (int x : nums) {
-            int s = 0;
-            for (int j = 0; j < m; ++j) {
-                if (x & (1 << bits[j])) s |= (1 << j);
+            int mask = 0;
+            for (int i = 0; i < m; ++i) {
+                if ((x >> bits[i]) & 1) mask |= (1 << i);
             }
-            ++freq[s];
+            freq[mask]++;
         }
-        vector<long long> dp = freq;
-        for (int j = 0; j < m; ++j) {
-            for (int mask = 0; mask < M; ++mask) {
-                if (mask & (1 << j)) {
-                    dp[mask] += dp[mask ^ (1 << j)];
+
+        // SOS DP to find g(mask): number of elements x such that (x_mask & mask) == 0
+        // This is equivalent to finding elements whose mask is a subset of (ALL_BITS ^ mask)
+        vector<int> subset_sum = freq;
+        for (int i = 0; i < m; ++i) {
+            for (int mask = 0; mask < num_masks; ++mask) {
+                if (mask & (1 << i)) {
+                    subset_sum[mask] += subset_sum[mask ^ (1 << i)];
                 }
             }
         }
-        int full = M - 1;
-        vector<long long> p2(n + 1);
-        p2[0] = 1;
-        for (int i = 1; i <= n; ++i) {
-            p2[i] = p2[i - 1] * 2 % MOD;
-        }
+
+        vector<long long> pow2(n + 1);
+        pow2[0] = 1;
+        for (int i = 1; i <= n; ++i) pow2[i] = (pow2[i - 1] * 2) % MOD;
+
         long long ans = 0;
-        for (int k = 1; k < M; ++k) {
-            int comp = full ^ k;
-            long long g = dp[comp];
-            long long uk = (long long)n - g;
-            long long ways = p2[n - uk];
-            int pop = __builtin_popcount(k);
-            if (pop & 1) {
-                ans = (ans + ways) % MOD;
+        int ALL_BITS = num_masks - 1;
+
+        for (int mask = 1; mask < num_masks; ++mask) {
+            int cnt_bits = __builtin_popcount(mask);
+            // Elements x such that x & mask == 0
+            int complement_mask = ALL_BITS ^ mask;
+            int g_M = subset_sum[complement_mask];
+            
+            if (cnt_bits % 2 == 1) {
+                ans = (ans + pow2[g_M]) % MOD;
             } else {
-                ans = (ans + MOD - ways) % MOD;
+                ans = (ans - pow2[g_M] + MOD) % MOD;
             }
         }
-        return ans;
+
+        return static_cast<int>(ans);
     }
 };
 # @lc code=end
