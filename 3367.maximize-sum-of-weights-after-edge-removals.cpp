a714bda364713d11393dfefcbec324a35816eebf
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 17:41:56 +0800

    Batch add 1 solutions: 3367.maximize-sum-of...

--- 3367.maximize-sum-of-weights-after-edge-removals.cpp
+++ 3367.maximize-sum-of-weights-after-edge-removals.cpp
@@ -1,62 +1,77 @@
+#
+# @lc app=leetcode id=3367 lang=cpp
+#
+# [3367] Maximize Sum of Weights after Edge Removals
+#
+
+# @lc code=start
 #include <vector>
 #include <algorithm>
-#include <functional>
 
 using namespace std;
 
 class Solution {
     struct Result {
-        long long dp0;
-        long long dp1;
+        long long take_parent;
+        long long skip_parent;
     };
 
-    Result dfs(int u, int p, int k, const vector<vector<pair<int, int>>>& adj) {
+    Result dfs(int u, int p, const vector<vector<pair<int, int>>>& adj, int k) {
         long long base_sum = 0;
-        vector<long long> diffs;
-        
+        vector<long long> gains;
+
         for (const auto& edge : adj[u]) {
             int v = edge.first;
             int w = edge.second;
             if (v == p) continue;
+
+            Result res_v = dfs(v, u, adj, k);
             
-            Result res = dfs(v, u, k, adj);
-            base_sum += res.dp0;
-            long long diff = (res.dp1 + (long long)w) - res.dp0;
-            if (diff > 0) {
-                diffs.push_back(diff);
+            // Default: don't keep edge (u, v)
+            base_sum += res_v.skip_parent;
+            
+            // Potential gain if we keep edge (u, v)
+            long long gain = (long long)w + res_v.take_parent - res_v.skip_parent;
+            if (gain > 0) {
+                gains.push_back(gain);
             }
         }
-        
-        sort(diffs.begin(), diffs.end(), greater<long long>());
-        
-        long long sum_diffs = 0;
-        int i = 0;
-        int n_diffs = (int)diffs.size();
-        
-        // Calculate dp1: at most k-1 edges to children
-        for (; i < n_diffs && i < k - 1; ++i) {
-            sum_diffs += diffs[i];
+
+        sort(gains.begin(), gains.end(), greater<long long>());
+
+        long long total_gain = 0;
+        long long res_take = 0;
+        long long res_skip = 0;
+
+        // To calculate skip_parent (can take up to k children)
+        // To calculate take_parent (can take up to k-1 children)
+        for (int i = 0; i < (int)gains.size(); ++i) {
+            if (i < k - 1) {
+                total_gain += gains[i];
+            } else if (i == k - 1) {
+                res_take = base_sum + total_gain;
+                total_gain += gains[i];
+            }
         }
-        long long res1 = base_sum + sum_diffs;
-        
-        // Calculate dp0: at most k edges to children
-        if (i < n_diffs && i < k) {
-            sum_diffs += diffs[i];
+
+        if (gains.size() < k) {
+            res_take = base_sum + total_gain;
         }
-        long long res0 = base_sum + sum_diffs;
-        
-        return {res0, res1};
+        res_skip = base_sum + total_gain;
+
+        return {res_take, res_skip};
     }
 
 public:
     long long maximizeSumOfWeights(vector<vector<int>>& edges, int k) {
-        int n = (int)edges.size() + 1;
+        int n = edges.size() + 1;
         vector<vector<pair<int, int>>> adj(n);
-        for (const auto& e : edges) {
-            adj[e[0]].push_back({e[1], e[2]});
-            adj[e[1]].push_back({e[0], e[2]});
+        for (const auto& edge : edges) {
+            adj[edge[0]].push_back({edge[1], edge[2]});
+            adj[edge[1]].push_back({edge[0], edge[2]});
         }
-        
-        return dfs(0, -1, k, adj).dp0;
+
+        return dfs(0, -1, adj, k).skip_parent;
     }
 };
+# @lc code=end
