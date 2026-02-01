cc16c4485ff9d1c60d368d3338b4abc97d116ac5
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 19:13:17 +0800

    Batch add 1 solutions: 3414.maximum-score-o...

--- 3414.maximum-score-of-non-overlapping-intervals.cpp
+++ 3414.maximum-score-of-non-overlapping-intervals.cpp
@@ -14,6 +14,16 @@ struct Interval {
     int l, r, w, id;
 };
 
+struct State {
+    long long weight = 0;
+    vector<int> ids;
+
+    bool betterThan(const State& other) const {
+        if (weight != other.weight) return weight > other.weight;
+        return ids < other.ids;
+    }
+};
+
 class Solution {
 public:
     vector<int> maximumWeight(vector<vector<int>>& intervals) {
@@ -23,52 +33,49 @@ public:
             ivs[i] = {intervals[i][0], intervals[i][1], intervals[i][2], i};
         }
 
-        // Sort by start time
         sort(ivs.begin(), ivs.end(), [](const Interval& a, const Interval& b) {
-            return a.l < b.l;
+            return a.r < b.r;
         });
 
-        // dp[k][i] stores {max_weight, sorted_indices}
-        // Using k from 1 to 4 and i from n down to 0
-        using State = pair<long long, vector<int>>;
-        auto compare = [](const State& a, const State& b) {
-            if (a.first != b.first) return a.first < b.first;
-            return a.second > b.second; // Lexicographically smaller vector is "greater" in our max search
-        };
+        vector<int> ends;
+        for (auto& iv : ivs) ends.push_back(iv.r);
 
-        vector<vector<State>> dp(5, vector<State>(n + 1, {0, {}}));
+        // dp[count][i] stores the best State using 'count' intervals from first 'i' sorted intervals
+        vector<vector<State>> dp(5, vector<State>(n + 1));
 
-        // Precompute next interval index using binary search
-        vector<int> next_idx(n);
-        for (int i = 0; i < n; ++i) {
-            int low = i + 1, high = n;
-            while (low < high) {
-                int mid = low + (high - low) / 2;
-                if (ivs[mid].l > ivs[i].r) high = mid;
-                else low = mid + 1;
-            }
-            next_idx[i] = low;
-        }
+        for (int i = 1; i <= n; ++i) {
+            int cur_l = ivs[i - 1].l;
+            int cur_w = ivs[i - 1].w;
+            int cur_id = ivs[i - 1].id;
 
-        for (int k = 1; k <= 4; ++k) {
-            for (int i = n - 1; i >= 0; --i) {
-                // Option 1: Skip current interval
-                dp[k][i] = dp[k][i+1];
+            // Find predecessor: last interval j such that ivs[j-1].r < cur_l
+            int prev_idx = lower_bound(ends.begin(), ends.end(), cur_l) - ends.begin();
 
-                // Option 2: Take current interval
-                long long current_w = ivs[i].w + dp[k-1][next_idx[i]].first;
-                vector<int> current_indices = {ivs[i].id};
-                for (int idx : dp[k-1][next_idx[i]].second) current_indices.push_back(idx);
-                sort(current_indices.begin(), current_indices.end());
+            for (int k = 1; k <= 4; ++k) {
+                // Case 1: Skip current interval
+                dp[k][i] = dp[k][i - 1];
 
-                State take_state = {current_w, current_indices};
-                if (compare(dp[k][i], take_state)) {
-                    dp[k][i] = take_state;
+                // Case 2: Use current interval
+                State current;
+                current.weight = (long long)cur_w + dp[k - 1][prev_idx].weight;
+                current.ids = dp[k - 1][prev_idx].ids;
+                current.ids.push_back(cur_id);
+                sort(current.ids.begin(), current.ids.end());
+
+                if (current.betterThan(dp[k][i])) {
+                    dp[k][i] = current;
                 }
             }
         }
 
-        return dp[4][0].second;
+        State best;
+        for (int k = 1; k <= 4; ++k) {
+            if (dp[k][n].betterThan(best)) {
+                best = dp[k][n];
+            }
+        }
+
+        return best.ids;
     }
 };
 # @lc code=end
