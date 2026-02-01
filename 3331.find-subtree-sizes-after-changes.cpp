01e42a998f1706805895b8dd5bd238fbf1b3014e
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 16:15:44 +0800

    Batch add 1 solutions: 3331.find-subtree-si...

--- 3331.find-subtree-sizes-after-changes.cpp
+++ 3331.find-subtree-sizes-after-changes.cpp
@@ -3,7 +3,6 @@
 #
 # [3331] Find Subtree Sizes After Changes
 #
-
 # @lc code=start
 #include <vector>
 #include <string>
@@ -19,45 +18,54 @@ public:
             adj[parent[i]].push_back(i);
         }
 
-        vector<int> new_parent = parent;
+        vector<int> new_parent(n);
+        new_parent[0] = -1;
         vector<int> last_seen(26, -1);
 
-        // DFS to find new parents based on closest ancestors with same character
-        auto findNewParents = [&](auto self, int u) -> void {
+        // DFS to find new parents based on closest ancestor with same character
+        auto find_new_parents = [&](auto self, int u) -> void {
             int char_idx = s[u] - 'a';
             int prev_node = last_seen[char_idx];
             
-            if (prev_node != -1) {
-                new_parent[u] = prev_node;
+            if (u != 0) {
+                if (prev_node != -1) {
+                    new_parent[u] = prev_node;
+                } else {
+                    new_parent[u] = parent[u];
+                }
             }
-            
+
+            int old_val = last_seen[char_idx];
             last_seen[char_idx] = u;
             for (int v : adj[u]) {
                 self(self, v);
             }
-            last_seen[char_idx] = prev_node; // Backtrack
+            last_seen[char_idx] = old_val; // backtrack
         };
 
-        findNewParents(findNewParents, 0);
+        find_new_parents(find_new_parents, 0);
 
-        // Build the new tree structure
+        // Build the new tree
         vector<vector<int>> new_adj(n);
         for (int i = 1; i < n; ++i) {
             new_adj[new_parent[i]].push_back(i);
         }
 
+        vector<int> ans(n, 0);
+        
         // DFS to calculate subtree sizes in the new tree
-        vector<int> subtree_size(n, 0);
-        auto calculateSizes = [&](auto self, int u) -> void {
-            subtree_size[u] = 1;
+        auto calculate_sizes = [&](auto self, int u) -> int {
+            int size = 1;
             for (int v : new_adj[u]) {
-                self(self, v);
-                subtree_size[u] += subtree_size[v];
+                size += self(self, v);
             }
+            ans[u] = size;
+            return size;
         };
 
-        calculateSizes(calculateSizes, 0);
-        return subtree_size;
+        calculate_sizes(calculate_sizes, 0);
+
+        return ans;
     }
 };
 # @lc code=end
