8d3c7b054e6e99b9906675099a1385521c0e786d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 17:30:57 +0800

    Batch add 1 solutions: 2816.double-a-number...

--- 2816.double-a-number-represented-as-a-linked-list.cpp
+++ 2816.double-a-number-represented-as-a-linked-list.cpp
@@ -3,7 +3,6 @@
 #
 # [2816] Double a Number Represented as a Linked List
 #
-
 # @lc code=start
 /**
  * Definition for singly-linked list.
@@ -18,26 +17,24 @@
 class Solution {
 public:
     ListNode* doubleIt(ListNode* head) {
-        // If the head's value is 5 or greater, the doubled value will have an extra digit.
-        // We handle this by prepending a node with value 0.
-        if (head->val > 4) {
+        // If the head is >= 5, doubling it will create a carry to a new leading digit.
+        if (head->val >= 5) {
             head = new ListNode(0, head);
         }
-
+        
         ListNode* curr = head;
         while (curr != nullptr) {
-            // Double the current value and keep only the last digit.
+            // Double the current value and keep the unit digit
             curr->val = (curr->val * 2) % 10;
-
-            // If the next node's original value is 5 or greater, it will produce a carry.
-            // Since we haven't updated the next node yet, we can check its value directly.
-            if (curr->next != nullptr && curr->next->val > 4) {
-                curr->val++;
+            
+            // Look ahead: if the next digit is >= 5, it will produce a carry of 1
+            if (curr->next != nullptr && curr->next->val >= 5) {
+                curr->val += 1;
             }
-
+            
             curr = curr->next;
         }
-
+        
         return head;
     }
 };
