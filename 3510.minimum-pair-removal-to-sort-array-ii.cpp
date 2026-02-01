5a10ed6046a8f03950c6e63f35098e01efca453c
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 20:46:26 +0800

    Batch add 1 solutions: 3510.minimum-pair-re...

--- 3510.minimum-pair-removal-to-sort-array-ii.cpp
+++ 3510.minimum-pair-removal-to-sort-array-ii.cpp
@@ -1,9 +1,3 @@
-#
-# @lc app=leetcode id=3510 lang=cpp
-#
-# [3510] Minimum Pair Removal to Sort Array II
-#
-# @lc code=start
 #include <vector>
 #include <queue>
 
@@ -11,21 +5,8 @@ using namespace std;
 
 struct Node {
     long long val;
-    int pos;
-    int prev = -1;
-    int next = -1;
-    bool deleted = false;
-};
-
-struct Pair {
-    long long sum;
-    int pos;
-    int L_idx, R_idx;
-
-    bool operator>(const Pair& other) const {
-        if (sum != other.sum) return sum > other.sum;
-        return pos > other.pos;
-    }
+    int prev, next;
+    bool deleted;
 };
 
 class Solution {
@@ -35,63 +16,72 @@ public:
         if (n <= 1) return 0;
 
         vector<Node> nodes(n);
-        for (int i = 0; i < n; ++i) {
-            nodes[i].val = nums[i];
-            nodes[i].pos = i;
-            if (i > 0) nodes[i].prev = i - 1;
-            if (i < n - 1) nodes[i].next = i + 1;
-        }
+        int violations = 0;
 
-        int bad_count = 0;
-        auto isBad = [&](int i, int j) {
-            if (i == -1 || j == -1) return false;
-            return nodes[i].val > nodes[j].val;
+        // Helper to check if index i is the start of a non-decreasing violation
+        auto is_violation = [&](int i) {
+            if (i < 0 || nodes[i].deleted || nodes[i].next == -1) return 0;
+            return nodes[i].val > nodes[nodes[i].next].val ? 1 : 0;
         };
 
-        priority_queue<Pair, vector<Pair>, greater<Pair>> pq;
+        for (int i = 0; i < n; ++i) {
+            nodes[i].val = nums[i];
+            nodes[i].prev = i - 1;
+            nodes[i].next = (i == n - 1) ? -1 : i + 1;
+            nodes[i].deleted = false;
+        }
+
+        // Initial violation count
         for (int i = 0; i < n - 1; ++i) {
-            if (isBad(i, i + 1)) bad_count++;
-            pq.push({nodes[i].val + nodes[i + 1].val, nodes[i].pos, i, i + 1});
+            if (nodes[i].val > nodes[i + 1].val) violations++;
+        }
+
+        if (violations == 0) return 0;
+
+        // Priority queue stores {sum, leftmost_index}
+        using Element = pair<long long, int>;
+        priority_queue<Element, vector<Element>, greater<Element>> pq;
+
+        for (int i = 0; i < n - 1; ++i) {
+            pq.push({nodes[i].val + nodes[i + 1].val, i});
         }
 
         int ops = 0;
-        while (bad_count > 0 && !pq.empty()) {
-            Pair top = pq.top();
+        while (violations > 0 && !pq.empty()) {
+            auto [sum, i] = pq.top();
             pq.pop();
 
-            int L = top.L_idx;
-            int R = top.R_idx;
+            // Validation: ensure node i and its next neighbor j still exist and form the pair
+            if (nodes[i].deleted || nodes[i].next == -1) continue;
+            int j = nodes[i].next;
+            if (nodes[i].val + nodes[j].val != sum) continue;
 
-            // Validation: ensure nodes aren't deleted and are still adjacent
-            if (nodes[L].deleted || nodes[R].deleted || nodes[L].next != R || (nodes[L].val + nodes[R].val) != top.sum) {
-                continue;
-            }
+            // Identify the neighborhood of effect (p -> i -> j -> nn)
+            int p = nodes[i].prev;
+            int nn = nodes[j].next;
+
+            // Remove old violations from count
+            violations -= is_violation(p);
+            violations -= is_violation(i);
+            violations -= is_violation(j);
+
+            // Merge: sum goes into node i, node j is deleted
+            nodes[i].val = sum;
+            nodes[i].next = nn;
+            if (nn != -1) nodes[nn].prev = i;
+            nodes[j].deleted = true;
+
+            // Add new violations to count
+            violations += is_violation(p);
+            violations += is_violation(i);
 
             ops++;
-            int P = nodes[L].prev;
-            int N = nodes[R].next;
 
-            // Remove old 'bad' contributions
-            if (isBad(P, L)) bad_count--;
-            if (isBad(L, R)) bad_count--;
-            if (isBad(R, N)) bad_count--;
-
-            // Perform merge into L
-            nodes[L].val += nodes[R].val;
-            nodes[L].next = N;
-            if (N != -1) nodes[N].prev = L;
-            nodes[R].deleted = true;
-
-            // Add new 'bad' contributions
-            if (isBad(P, L)) bad_count++;
-            if (isBad(L, N)) bad_count++;
-
-            // Push new potential pairs
-            if (P != -1) pq.push({nodes[P].val + nodes[L].val, nodes[P].pos, P, L});
-            if (N != -1) pq.push({nodes[L].val + nodes[N].val, nodes[L].pos, L, N});
+            // Add new possible pairs to PQ
+            if (p != -1) pq.push({nodes[p].val + nodes[i].val, p});
+            if (nn != -1) pq.push({nodes[i].val + nodes[nn].val, i});
         }
 
         return ops;
     }
 };
-# @lc code=end
