b3be45046e69b9b4bc326bfd230df95b8e1220c0
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 15:57:52 +0800

    Batch add 1 solutions: 1367.linked-list-in-...

--- 1367.linked-list-in-binary-tree.cpp
+++ 1367.linked-list-in-binary-tree.cpp
@@ -3,41 +3,40 @@
 #
 # [1367] Linked List in Binary Tree
 #
-
 # @lc code=start
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode() : val(0), next(nullptr) {}
+*     ListNode(int x) : val(x), next(nullptr) {}
+*     ListNode(int x, ListNode *next) : val(x), next(next) {}
+* };
+*/
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+*     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+*     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+* };
+*/
 class Solution {
 public:
-    bool dfs(ListNode* head, TreeNode* root) {
-        if (head == nullptr) return true;
-        if (root == nullptr) return false;
-        if (head->val != root->val) return false;
-        return dfs(head->next, root->left) || dfs(head->next, root->right);
+    bool checkPath(ListNode* head, TreeNode* node) {
+        if (!head) return true;
+        if (!node) return false;
+        if (head->val != node->val) return false;
+        return checkPath(head->next, node->left) || checkPath(head->next, node->right);
     }
 
     bool isSubPath(ListNode* head, TreeNode* root) {
-        if (root == nullptr) return false;
-        if (dfs(head, root)) return true;
+        if (!root) return false;
+        if (checkPath(head, root)) return true;
         return isSubPath(head, root->left) || isSubPath(head, root->right);
     }
 };
