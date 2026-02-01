1d0b7aee6665d246585a94b56a391e4c4799bf70
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 16:19:30 +0800

    Batch add 1 solutions: 3341.find-minimum-ti...

--- 3341.find-minimum-time-to-reach-last-room-i.cpp
+++ 3341.find-minimum-time-to-reach-last-room-i.cpp
@@ -18,40 +18,36 @@ public:
         int m = moveTime[0].size();
         
         // dist[i][j] stores the minimum time to reach room (i, j)
-        // Using a large value for initialization (2e9 + 7 stays within int range)
+        // Initialize with a large value
         vector<vector<int>> dist(n, vector<int>(m, 2000000000));
-        // Priority queue stores {time, {row, col}}, ordered by time (min-priority queue)
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        
         dist[0][0] = 0;
+        
+        // Min-priority queue: {time, {row, col}}
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
         pq.push({0, {0, 0}});
         
         int dr[] = {0, 0, 1, -1};
         int dc[] = {1, -1, 0, 0};
         
         while (!pq.empty()) {
-            auto [t, pos] = pq.top();
+            int d = pq.top().first;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
             pq.pop();
             
-            int r = pos.first;
-            int c = pos.second;
-            
-            // If we found a better way already, skip this
-            if (t > dist[r][c]) continue;
-            
-            // If we reached the destination
-            if (r == n - 1 && c == m - 1) return t;
+            if (d > dist[r][c]) continue;
+            if (r == n - 1 && c == m - 1) return d;
             
             for (int i = 0; i < 4; ++i) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
                 
                 if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
-                    // Time to reach neighbor: max(current time, opening time) + 1 second travel
-                    int nt = max(t, moveTime[nr][nc]) + 1;
-                    if (nt < dist[nr][nc]) {
-                        dist[nr][nc] = nt;
-                        pq.push({nt, {nr, nc}});
+                    // Arrival time: max of current time and room opening time, plus 1 second move time
+                    int nextTime = max(d, moveTime[nr][nc]) + 1;
+                    if (nextTime < dist[nr][nc]) {
+                        dist[nr][nc] = nextTime;
+                        pq.push({nextTime, {nr, nc}});
                     }
                 }
             }
