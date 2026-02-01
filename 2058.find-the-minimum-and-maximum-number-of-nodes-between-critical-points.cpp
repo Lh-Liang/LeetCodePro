d22c067ec221a72bd4ca441deb7a079374605774
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 16:05:51 +0800

    Batch add 1 solutions: 2058.find-the-minimu...

--- 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -24,47 +24,51 @@ using namespace std;
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        // A critical point needs a previous and a next node.
+        // If list has less than 3 nodes, no critical points possible.
         if (!head || !head->next || !head->next->next) {
             return {-1, -1};
         }
 
-        int firstCP = -1;
-        int prevCP = -1;
+        int firstIndex = -1;
+        int prevIndex = -1;
         int minDist = INT_MAX;
         
-        ListNode* prev = head;
+        int index = 1; // Start from the second node (index 1)
+        int prevVal = head->val;
         ListNode* curr = head->next;
-        int index = 1;
-
-        while (curr->next) {
-            ListNode* next = curr->next;
-            bool isCP = false;
+        
+        while (curr->next != nullptr) {
+            int nextVal = curr->next->val;
             
-            // Check for local maxima or local minima
-            if ((curr->val > prev->val && curr->val > next->val) || 
-                (curr->val < prev->val && curr->val < next->val)) {
-                isCP = true;
-            }
-
-            if (isCP) {
-                if (firstCP == -1) {
-                    firstCP = index;
+            // Check if current node is a local maxima or minima
+            if ((curr->val > prevVal && curr->val > nextVal) || 
+                (curr->val < prevVal && curr->val < nextVal)) {
+                
+                if (firstIndex == -1) {
+                    // First critical point found
+                    firstIndex = index;
                 } else {
-                    minDist = min(minDist, index - prevCP);
+                    // Update minimum distance between adjacent critical points
+                    minDist = min(minDist, index - prevIndex);
                 }
-                prevCP = index;
+                // Update the last seen critical point index
+                prevIndex = index;
             }
-
-            prev = curr;
-            curr = next;
+            
+            // Move to the next node
+            prevVal = curr->val;
+            curr = curr->next;
             index++;
         }
-
-        if (firstCP == -1 || prevCP == firstCP) {
+        
+        // If minDist was never updated, it means we found fewer than 2 critical points
+        if (minDist == INT_MAX) {
             return {-1, -1};
         }
-
-        return {minDist, prevCP - firstCP};
+        
+        // maxDistance is the distance between the first and last critical point
+        return {minDist, prevIndex - firstIndex};
     }
 };
 # @lc code=end
