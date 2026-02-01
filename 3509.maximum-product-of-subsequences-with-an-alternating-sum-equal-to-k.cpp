e6f0780d9038b335f22b73805a3025e9708f1c94
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 20:44:53 +0800

    Batch add 1 solutions: 3509.maximum-product...

--- 3509.maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k.cpp
+++ 3509.maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k.cpp
@@ -1,13 +1,6 @@
-#
-# @lc app=leetcode id=3509 lang=cpp
-#
-# [3509] Maximum Product of Subsequences With an Alternating Sum Equal to K
-#
-
-# @lc code=start
 #include <vector>
-#include <bitset>
 #include <algorithm>
+#include <bitset>
 
 using namespace std;
 
@@ -15,73 +8,63 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums, int k, int limit) {
         const int offset = 1800;
-        const int max_sum_idx = 3600;
-        
-        if (k < -offset || k > offset) return -1;
+        const int range = 3601;
+        // dp[parity][sum] where parity 0 = even length, 1 = odd length
+        // We use bitset<5001> to store possible products.
+        vector<vector<bitset<5001>>> dp(2, vector<bitset<5001>>(range));
 
-        // dp[sum_idx][parity]: parity 0 = even length, parity 1 = odd length
-        // bitset stores possible products
-        vector<array<bitset<5001>, 2>> dp(max_sum_idx + 1);
+        int min_s = offset, max_s = offset;
 
         for (int x : nums) {
-            // Use a temporary update structure to avoid using the same element multiple times in one subsequence
-            vector<pair<int, pair<int, bitset<5001>>>> updates;
-
-            // Case 1: x starts a new subsequence (Index 0, Even index, result length 1/Odd)
+            vector<vector<bitset<5001>>> next_dp = dp;
+            
+            // Start a new subsequence (length 1, so parity is odd/1)
             if (x <= limit) {
-                bitset<5001> start_bs;
-                start_bs.set(x);
-                updates.push_back({x + offset, {1, start_bs}});
+                next_dp[1][offset + x].set(x);
             }
 
-            for (int s = 0; s <= max_sum_idx; ++s) {
-                // Case 2: x is added at an even index (current length is even, parity 0)
-                // New sum: s + x, new parity: 1 (odd length)
-                if (dp[s][0].any()) {
+            for (int s = min_s; s <= max_s; ++s) {
+                // Extend even length to odd length (Add x)
+                if (dp[0][s].any()) {
                     int ns = s + x;
-                    if (ns >= 0 && ns <= max_sum_idx) {
-                        updates.push_back({ns, {1, transform(dp[s][0], x, limit)}});
+                    if (ns >= 0 && ns < range) {
+                        if (x == 0) next_dp[1][ns].set(0);
+                        else if (x == 1) next_dp[1][ns] |= dp[0][s];
+                        else {
+                            for (int p = 0; p <= limit / x; ++p) {
+                                if (dp[0][s].test(p)) next_dp[1][ns].set(p * x);
+                            }
+                        }
                     }
                 }
-
-                // Case 3: x is added at an odd index (current length is odd, parity 1)
-                // New sum: s - x, new parity: 0 (even length)
-                if (dp[s][1].any()) {
+                // Extend odd length to even length (Subtract x)
+                if (dp[1][s].any()) {
                     int ns = s - x;
-                    if (ns >= 0 && ns <= max_sum_idx) {
-                        updates.push_back({ns, {0, transform(dp[s][1], x, limit)}});
+                    if (ns >= 0 && ns < range) {
+                        if (x == 0) next_dp[0][ns].set(0);
+                        else if (x == 1) next_dp[0][ns] |= dp[1][s];
+                        else {
+                            for (int p = 0; p <= limit / x; ++p) {
+                                if (dp[1][s].test(p)) next_dp[0][ns].set(p * x);
+                            }
+                        }
                     }
                 }
             }
-
-            for (auto& upd : updates) {
-                dp[upd.first][upd.second.first] |= upd.second.second;
-            }
+            dp = move(next_dp);
+            min_s = max(0, min_s - x);
+            max_s = min(range - 1, max_s + x);
         }
 
-        int target_idx = k + offset;
+        int target_s = k + offset;
+        if (target_s < 0 || target_s >= range) return -1;
+
+        int best = -1;
         for (int p = limit; p >= 0; --p) {
-            if (dp[target_idx][0].test(p) || dp[target_idx][1].test(p)) {
+            if (dp[0][target_s].test(p) || dp[1][target_s].test(p)) {
                 return p;
             }
         }
-
         return -1;
     }
-
-private:
-    bitset<5001> transform(const bitset<5001>& bs, int x, int limit) {
-        bitset<5001> res;
-        if (x == 0) {
-            res.set(0);
-        } else if (x == 1) {
-            res = bs;
-        } else {
-            for (int p = 0; p <= limit / x; ++p) {
-                if (bs.test(p)) res.set(p * x);
-            }
-        }
-        return res;
-    }
 };
-# @lc code=end
