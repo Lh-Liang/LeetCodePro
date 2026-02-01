51369e3a61538c0a772680b428d18a811566d72e
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 21:31:19 +0800

    Batch add 1 solutions: 2807.insert-greatest...

--- 2807.insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807.insert-greatest-common-divisors-in-linked-list.cpp
@@ -1,3 +1,5 @@
+#include <numeric>
+
 #
 # @lc app=leetcode id=2807 lang=cpp
 #
@@ -15,31 +17,20 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-#include <numeric>
-
 class Solution {
 public:
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
-        // If the list is empty or has only one node, no pairs exist.
-        if (!head || !head->next) {
-            return head;
-        }
+        if (!head || !head->next) return head;
 
         ListNode* curr = head;
-        
-        // Iterate through the list while there is a pair of nodes.
-        while (curr && curr->next) {
-            // Calculate GCD of current node and next node values.
-            int commonDivisor = std::gcd(curr->val, curr->next->val);
+        while (curr != nullptr && curr->next != nullptr) {
+            int gcdValue = std::gcd(curr->val, curr->next->val);
+            ListNode* newNode = new ListNode(gcdValue);
             
-            // Create a new node with the GCD value.
-            // Link it to the current next node.
-            ListNode* newNode = new ListNode(commonDivisor, curr->next);
-            
-            // Insert the new node after the current node.
+            newNode->next = curr->next;
             curr->next = newNode;
             
-            // Move curr to the original next node (skip the inserted node).
+            // Move curr to the node after the newly inserted GCD node
             curr = newNode->next;
         }
         
