75ee6a5808754ec93dc0af03fbc2afe8930329c1
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 17:41:19 +0800

    Batch add 1 solutions: 3217.delete-nodes-fr...

--- 3217.delete-nodes-from-linked-list-present-in-array.cpp
+++ 3217.delete-nodes-from-linked-list-present-in-array.cpp
@@ -3,42 +3,39 @@
 #
 # [3217] Delete Nodes From Linked List Present in Array
 #
+
 # @lc code=start
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode() : val(0), next(nullptr) {}
-*     ListNode(int x) : val(x), next(nullptr) {}
-*     ListNode(int x, ListNode *next) : val(x), next(next) {}
-* };
-*/
-#include <vector>
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 #include <unordered_set>
-
-using namespace std;
+#include <vector>
 
 class Solution {
 public:
-    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        unordered_set<int> numSet(nums.begin(), nums.end());
-        ListNode* dummy = new ListNode(0, head);
-        ListNode* curr = dummy;
-
-        while (curr->next != nullptr) {
-            if (numSet.count(curr->next->val)) {
+    ListNode* modifiedList(std::vector<int>& nums, ListNode* head) {
+        std::unordered_set<int> s(nums.begin(), nums.end());
+        ListNode dummy(0, head);
+        ListNode* curr = &dummy;
+        
+        while (curr->next) {
+            if (s.find(curr->next->val) != s.end()) {
                 ListNode* temp = curr->next;
                 curr->next = curr->next->next;
-                delete temp;
+                // Note: In a real environment, we might delete temp here if not using a GC-like system,
+                // but for LeetCode standard solutions, pointer redirection is the focus.
             } else {
                 curr = curr->next;
             }
         }
-
-        ListNode* newHead = dummy->next;
-        delete dummy;
-        return newHead;
+        return dummy.next;
     }
 };
 # @lc code=end
