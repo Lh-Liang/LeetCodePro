5867966c0587f79ff742c69322951ba3f3c8fa16
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 23:04:45 +0800

    Batch add 1 solutions: 3457.eat-pizzas.cpp...

--- 3457.eat-pizzas.cpp
+++ 3457.eat-pizzas.cpp
@@ -13,28 +13,30 @@ using namespace std;
 class Solution {
 public:
     long long maxWeight(vector<int>& pizzas) {
-        int n = pizzas.size();
-        sort(pizzas.begin(), pizzas.end());
+        // Sort pizzas in descending order to easily pick the largest weights.
+        sort(pizzas.begin(), pizzas.end(), greater<int>());
         
+        int n = pizzas.size();
         int totalDays = n / 4;
         int oddDays = (totalDays + 1) / 2;
         int evenDays = totalDays / 2;
         
         long long totalWeight = 0;
-        int right = n - 1;
         
-        // On odd-numbered days (1, 3, 5, ...), we gain the weight of the largest pizza (Z).
-        // To maximize this, we pick the largest available pizzas for these days.
+        // On odd days, we gain the weight of the largest pizza (Z).
+        // We take the 'oddDays' largest pizzas available.
         for (int i = 0; i < oddDays; ++i) {
-            totalWeight += pizzas[right--];
+            totalWeight += pizzas[i];
         }
         
-        // On even-numbered days (2, 4, 6, ...), we gain the weight of the second largest pizza (Y).
-        // To maximize this, we pick the two largest available pizzas; 
-        // the first is the 'largest' (Z) and the second is the 'second largest' (Y) which we gain.
-        for (int i = 0; i < evenDays; ++i) {
-            right--; // Skip the largest available pizza (this will be Z for the even day)
-            totalWeight += pizzas[right--]; // Gain the second largest (this will be Y for the even day)
+        // On even days, we gain the weight of the second largest pizza (Y).
+        // To maximize Y, we must skip one larger pizza (to act as Z) for each even day.
+        // We start from the index immediately following the pizzas used for odd days.
+        int currentIndex = oddDays;
+        for (int j = 0; j < evenDays; ++j) {
+            // Skip pizzas[currentIndex] (used as Z) and take pizzas[currentIndex + 1] (used as Y).
+            totalWeight += pizzas[currentIndex + 1];
+            currentIndex += 2;
         }
         
         return totalWeight;
