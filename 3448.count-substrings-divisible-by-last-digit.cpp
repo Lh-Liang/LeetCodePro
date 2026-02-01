8eb593f7623d8616edc32a9c4019290ff3c7a77a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 19:04:13 +0800

    Batch add 1 solutions: 3448.count-substring...

--- 3448.count-substrings-divisible-by-last-digit.cpp
+++ 3448.count-substrings-divisible-by-last-digit.cpp
@@ -3,12 +3,19 @@
 #
 # [3448] Count Substrings Divisible By Last Digit
 #
+
 # @lc code=start
+#include <string>
+#include <vector>
+#include <array>
+
+using namespace std;
+
 class Solution {
 public:
     long long countSubstrings(string s) {
-        // dp[d][r] stores the number of substrings ending at the previous position
-        // that have remainder r when divided by d.
+        // dp[divisor][remainder] stores the count of substrings ending at current index
+        // having 'remainder' when divided by 'divisor'.
         long long dp[10][10] = {0};
         long long totalCount = 0;
 
@@ -17,30 +24,27 @@ public:
             long long next_dp[10][10] = {0};
 
             for (int d = 1; d <= 9; ++d) {
-                // For each potential divisor d from 1 to 9, update the remainders 
-                // of all substrings ending at the current position.
-                // If a substring ending at the previous position had remainder 'r',
-                // extending it with 'digit' gives a new remainder: (r * 10 + digit) % d.
+                // Transition existing substrings to the new index
                 for (int r = 0; r < d; ++r) {
                     if (dp[d][r] > 0) {
-                        next_dp[d][(r * 10 + digit) % d] += dp[d][r];
+                        int next_r = (r * 10 + digit) % d;
+                        next_dp[d][next_r] += dp[d][r];
                     }
                 }
-                // Also account for the new single-digit substring ending at the current position.
+                // Add the new single-digit substring [i...i]
                 next_dp[d][digit % d]++;
             }
 
-            // Update dp table for the next iteration.
-            for (int i = 1; i <= 9; ++i) {
-                for (int j = 0; j < i; ++j) {
-                    dp[i][j] = next_dp[i][j];
-                }
-            }
-
-            // If the current digit is non-zero, it acts as the divisor for substrings 
-            // ending at this position. Count those that are divisible (remainder 0).
+            // If the current digit is a valid divisor, add count of substrings with remainder 0
             if (digit > 0) {
-                totalCount += dp[digit][0];
+                totalCount += next_dp[digit][0];
+            }
+            
+            // Update DP table for the next iteration
+            for (int d = 1; d <= 9; ++d) {
+                for (int r = 0; r < d; ++r) {
+                    dp[d][r] = next_dp[d][r];
+                }
             }
         }
 
