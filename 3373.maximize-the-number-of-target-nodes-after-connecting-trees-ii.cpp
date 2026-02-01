240e1cf6008beebadf4bc54fd5808f0b7476dbac
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-01 16:27:08 +0800

    Batch add 1 solutions: 3373.maximize-the-nu...

--- 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
+++ 3373.maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -1,10 +1,3 @@
-#
-# @lc app=leetcode id=3373 lang=cpp
-#
-# [3373] Maximize the Number of Target Nodes After Connecting Trees II
-#
-
-# @lc code=start
 #include <vector>
 #include <queue>
 #include <algorithm>
@@ -17,16 +10,23 @@ public:
         int n = edges1.size() + 1;
         int m = edges2.size() + 1;
 
-        auto getPartitions = [](int size, const vector<vector<int>>& adj) {
+        auto get_tree_info = [](int size, const vector<vector<int>>& edges) {
+            vector<vector<int>> adj(size);
+            for (const auto& e : edges) {
+                adj[e[0]].push_back(e[1]);
+                adj[e[1]].push_back(e[0]);
+            }
+            
             vector<int> color(size, -1);
             vector<int> counts(2, 0);
             queue<int> q;
-            
-            // Tree is connected, so one BFS from node 0 suffices
-            color[0] = 0;
-            counts[0]++;
-            q.push(0);
-            
+
+            if (size > 0) {
+                q.push(0);
+                color[0] = 0;
+                counts[0]++;
+            }
+
             while (!q.empty()) {
                 int u = q.front();
                 q.pop();
@@ -41,30 +41,16 @@ public:
             return make_pair(color, counts);
         };
 
-        vector<vector<int>> adj1(n);
-        for (const auto& e : edges1) {
-            adj1[e[0]].push_back(e[1]);
-            adj1[e[1]].push_back(e[0]);
-        }
+        auto [color1, counts1] = get_tree_info(n, edges1);
+        auto [color2, counts2] = get_tree_info(m, edges2);
 
-        vector<vector<int>> adj2(m);
-        for (const auto& e : edges2) {
-            adj2[e[0]].push_back(e[1]);
-            adj2[e[1]].push_back(e[0]);
-        }
+        int max_tree2 = max(counts2[0], counts2[1]);
 
-        auto [color1, counts1] = getPartitions(n, adj1);
-        auto [color2, counts2] = getPartitions(m, adj2);
-
-        int maxContributionFromTree2 = max(counts2[0], counts2[1]);
-        
         vector<int> answer(n);
         for (int i = 0; i < n; ++i) {
-            // Nodes in same partition in Tree 1 + Max nodes in opposite partition in Tree 2
-            answer[i] = counts1[color1[i]] + maxContributionFromTree2;
+            answer[i] = counts1[color1[i]] + max_tree2;
         }
 
         return answer;
     }
 };
-# @lc code=end
