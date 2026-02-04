4b3fec7ab638db2427433ad2caad11f1c66f26c7
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 15:55:26 +0800

    Batch add 1 solutions: 3640.trionic-array-i...

--- 3640.trionic-array-ii.cpp
+++ 3640.trionic-array-ii.cpp
@@ -1,64 +1,58 @@
-#include <vector>
-#include <algorithm>
-#include <climits>
+#
+# @lc app=leetcode id=3640 lang=cpp
+#
+# [3640] Trionic Array II
+#
 
-using namespace std;
-
+# @lc code=start
 class Solution {
 public:
     long long maxSumTrionic(vector<int>& nums) {
         int n = nums.size();
-        const long long INF = 1e18;
-
-        // max_inc_prefix[p]: Max sum of strictly increasing nums[l...p] with l < p
-        vector<long long> max_inc_prefix(n, -INF);
+        if (n < 4) return LLONG_MIN;
+        vector<long long> leftInc(n, 0), rightDec(n, 0);
+        vector<long long> rightInc(n, 0), leftDec(n, 0);
+        
+        // Calculate strictly increasing sequences sums from left to right
         for (int i = 1; i < n; ++i) {
             if (nums[i] > nums[i - 1]) {
-                max_inc_prefix[i] = (long long)nums[i] + max((long long)nums[i - 1], max_inc_prefix[i - 1]);
-            }
-        }
-
-        // max_inc_suffix[q]: Max sum of strictly increasing nums[q...r] with q < r
-        vector<long long> max_inc_suffix(n, -INF);
-        for (int i = n - 2; i >= 0; --i) {
-            if (nums[i] < nums[i + 1]) {
-                max_inc_suffix[i] = (long long)nums[i] + max((long long)nums[i + 1], max_inc_suffix[i + 1]);
-            }
-        }
-
-        // Prefix sums for range sum calculation
-        vector<long long> pref(n + 1, 0);
-        for (int i = 0; i < n; ++i) {
-            pref[i + 1] = pref[i] + nums[i];
-        }
-
-        long long max_trionic_sum = -INF;
-        // best_peak_contribution stores the max value of [Sum(l, p) - pref[p+1]]
-        // for a peak p that is part of a strictly decreasing sequence ending at current q
-        long long best_peak_contribution = -INF;
-
-        // We need l < p < q < r. 
-        // Smallest possible p is 1 (l=0). Smallest possible q is 2 (p=1).
-        // Largest possible q is n-2 (r=n-1).
-        for (int q = 2; q <= n - 2; ++q) {
-            // Can we extend the decreasing sequence from q-1 to q?
-            if (nums[q] < nums[q - 1]) {
-                // The previous index q-1 is a potential peak p
-                if (max_inc_prefix[q - 1] != -INF) {
-                    best_peak_contribution = max(best_peak_contribution, max_inc_prefix[q - 1] - pref[q]);
-                }
+                leftInc[i] = leftInc[i - 1] + nums[i];
             } else {
-                // Decreasing sequence broken
-                best_peak_contribution = -INF;
+                leftInc[i] = nums[i];
             }
-
-            // If we have a valid peak-to-valley path and a valid suffix increasing path
-            if (best_peak_contribution != -INF && max_inc_suffix[q] != -INF) {
-                // Formula: (Sum(l, p) - pref[p+1]) + pref[q] + Sum(q, r)
-                max_trionic_sum = max(max_trionic_sum, best_peak_contribution + pref[q] + max_inc_suffix[q]);
+            
+            if (nums[n - i - 1] < nums[n - i]) {
+                rightDec[n - i - 1] = rightDec[n - i] + nums[n - i - 1];
+            } else {
+                rightDec[n - i - 1] = nums[n - i - 1];
             }
         }
 
-        return max_trionic_sum;
+        // Calculate strictly decreasing sequence sums from right to left
+        for (int i = n - 2; i >= 0; --i) {
+            if (nums[i] > nums[i + 1]) {
+                rightInc[i] = rightInc[i + 1] + nums[i];
+            } else {
+                rightInc[i] = nums[i];
+            }
+
+            if (nums[n - i - 2] < nums[n - i - 3]) {
+                leftDec[n - i - 2] = leftDec[n - i - 3] + nums[n - i - 2];
+            } else {
+                leftDec[n - i - 2] = nums[n - i - 2];
+            }
+        }
+
+        long long max_sum = LLONG_MIN;
+        // Find maximum sum of trionic subarray using precomputed arrays
+        for (int p = 2; p < n-2; ++p) { // Middle point p must allow space for l and r
+            if (leftInc[p-1] > nums[p-1] && rightDec[p+1] > nums[p+1]) { // Ensure valid configuration
+                long long current_sum = leftInc[p-1] + nums[p-1]+rightDec[p+1];
+                max_sum = max(max_sum, current_sum);
+            }
+        }
+
+        return max_sum;
     }
 };
+# @lc code=end
