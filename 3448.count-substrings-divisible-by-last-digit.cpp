106634bbc1bc6233c75d950b9f9a859b3bde8fa7
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 18:50:20 +0800

    Batch add 1 solutions: 3448.count-substring...

--- 3448.count-substrings-divisible-by-last-digit.cpp
+++ 3448.count-substrings-divisible-by-last-digit.cpp
@@ -3,23 +3,19 @@
 #
 # [3448] Count Substrings Divisible By Last Digit
 #
-
 # @lc code=start
 class Solution {
 public:
     long long countSubstrings(string s) {
-        int n = s.size();
         long long count = 0;
-        // Iterate over each position in the string
+        int n = s.size();
         for (int i = 0; i < n; ++i) {
-            int last_digit = s[i] - '0';
-            if (last_digit == 0) continue; // Skip if last digit is zero
-            int current_number = 0;
-            // Consider substrings ending at i
-            for (int j = i; j >= 0; --j) {
-                current_number = (current_number * 10 + (s[j] - '0')) % last_digit;
-                if (current_number == 0) {
-                    ++count;
+            int num = 0;
+            for (int j = i; j < n; ++j) {
+                num = num * 10 + (s[j] - '0');
+                int last_digit = s[j] - '0';
+                if (last_digit != 0 && num % last_digit == 0) {
+                    count++;
                 }
             }
         }
