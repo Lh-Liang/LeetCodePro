0ad6e3243d246aebbb7bebf0644141c6483a54c3
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 05:44:43 +0800

    Batch add 1 solutions: 3739.count-subarrays...

--- 3739.count-subarrays-with-majority-element-ii.cpp
+++ 3739.count-subarrays-with-majority-element-ii.cpp
@@ -8,22 +8,39 @@ class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
         int n = nums.size();
-        long long result = 0;
-        unordered_map<int, int> prefixCount;
-        prefixCount[0] = 1;
-        int currentCount = 0;
+        vector<int> arr(n);
         for (int i = 0; i < n; ++i) {
-            if (nums[i] == target) {
-                currentCount++;
-            } else {
-                currentCount--;
-            }
-            if (prefixCount.find(currentCount - 1) != prefixCount.end()) {
-                result += prefixCount[currentCount - 1];
-            }
-            prefixCount[currentCount]++;
+            arr[i] = (nums[i] == target) ? 1 : -1;
         }
-        return result;
+        vector<long long> prefix(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            prefix[i+1] = prefix[i] + arr[i];
+        }
+        // For prefix sums, we want for subarray [l, r]:
+        // prefix[r+1] - prefix[l] > 0 => prefix[l] < prefix[r+1]
+        // Count for each r+1, the number of prefix[l] < prefix[r+1] (l in [0, r+1))
+        vector<long long> all_vals = prefix;
+        sort(all_vals.begin(), all_vals.end());
+        all_vals.erase(unique(all_vals.begin(), all_vals.end()), all_vals.end());
+        int m = all_vals.size();
+        vector<int> bit(m + 2, 0);
+        auto add = [&](int idx) {
+            for (++idx; idx < bit.size(); idx += idx & -idx) ++bit[idx];
+        };
+        auto sum = [&](int idx) {
+            int res = 0;
+            for (++idx; idx > 0; idx -= idx & -idx) res += bit[idx];
+            return res;
+        };
+        long long ans = 0;
+        for (int i = 0; i < prefix.size(); ++i) {
+            int idx = lower_bound(all_vals.begin(), all_vals.end(), prefix[i]) - all_vals.begin();
+            if (i > 0) {
+                ans += sum(idx - 1);
+            }
+            add(idx);
+        }
+        return ans;
     }
 };
 # @lc code=end
