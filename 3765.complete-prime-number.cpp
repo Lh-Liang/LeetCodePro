4345052e7b4690a38555be97b4a054ce57e32a62
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 05:58:41 +0800

    Batch add 1 solutions: 3765.complete-prime-...

--- 3765.complete-prime-number.cpp
+++ 3765.complete-prime-number.cpp
@@ -7,26 +7,30 @@
 # @lc code=start
 class Solution {
 public:
-    bool isPrime(int n) {
+    bool is_prime(int n) {
         if (n <= 1) return false;
-        if (n <= 3) return true;
-        if (n % 2 == 0 || n % 3 == 0) return false;
-        for (int i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        for (int i = 3; i * 1LL * i <= n; i += 2) {
+            if (n % i == 0) return false;
         }
         return true;
     }
+
     bool completePrime(int num) {
         std::string s = std::to_string(num);
-        int len = s.length();
-        for (int i = 1; i <= len; ++i) {
+        int n = s.size();
+        // Check all prefixes
+        for (int i = 1; i <= n; ++i) {
             int prefix = std::stoi(s.substr(0, i));
-            int suffix = std::stoi(s.substr(len - i));
-            if (!isPrime(prefix) || !isPrime(suffix)) {
-                return false;
-            }
+            if (!is_prime(prefix)) return false;
+        }
+        // Check all suffixes
+        for (int i = 0; i < n; ++i) {
+            int suffix = std::stoi(s.substr(i));
+            if (!is_prime(suffix)) return false;
         }
         return true;
     }
-}; 
+};
 # @lc code=end
