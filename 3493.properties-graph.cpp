8c10f399076504c56e43222a5e6c3dcfa0a5f79f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 19:08:14 +0800

    Batch add 1 solutions: 3493.properties-grap...

--- 3493.properties-graph.cpp
+++ 3493.properties-graph.cpp
@@ -3,48 +3,37 @@
 #
 # [3493] Properties Graph
 #
+
 # @lc code=start
 class Solution {
 public:
     int numberOfComponents(vector<vector<int>>& properties, int k) {
         int n = properties.size();
-        vector<vector<int>> graph(n);
-        // Build graph based on intersection condition
+        vector<vector<int>> adj(n);
+        
+        // Calculate intersections and build graph
+        auto intersect = [](const vector<int>& a, const vector<int>& b) {
+            unordered_set<int> setA(a.begin(), a.end());
+            unordered_set<int> setB(b.begin(), b.end());
+            int common = 0;
+            for (int num : setA) {
+                if (setB.count(num)) common++;
+            }
+            return common;
+        };
+        
         for (int i = 0; i < n; ++i) {
             for (int j = i + 1; j < n; ++j) {
                 if (intersect(properties[i], properties[j]) >= k) {
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
-        // Find connected components using DFS
+        
+        // Count connected components using DFS or union-find (DFS here)
         vector<bool> visited(n, false);
-        int components = 0;
-        for (int i = 0; i < n; ++i) {
-            if (!visited[i]) {
-                ++components;
-                dfs(i, graph, visited);
-            }
-        }
-        return components;
-    }
-private:
-    // Function to find the number of distinct integers common to both arrays a and b
-    int intersect(const vector<int>& a, const vector<int>& b) {
-        unordered_set<int> setA(a.begin(), a.end());
-        int count = 0;
-        for (int num : b) {
-            if (setA.count(num)) { ++count; } 
-        }
-        return count;
-    }
-    // Depth First Search function to mark all reachable nodes from current node
-    void dfs(int node, const vector<vector<int>>& graph, vector<bool>& visited) { 
-        visited[node] = true;
-        for (int neighbor : graph[node]) { 
-            if (!visited[neighbor]) { dfs(neighbor, graph, visited); } 
-        } 
-    }
-};
-# @lc code=end
+        function<void(int)> dfs = [&](int node) {
+            visited[node] = true;
+            for (int neighbor : adj[node]) {
+                if (!visited[neighbor]) dfs(neighbor);
