abfc6c35ce1c0065169f92dcbb4bae8a2814cc2d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 20:33:20 +0800

    Batch add 1 solutions: 3734.lexicographical...

--- 3734.lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
+++ 3734.lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -3,46 +3,11 @@
 #
 # [3734] Lexicographically Smallest Palindromic Permutation Greater Than Target
 #
-
 # @lc code=start
 class Solution {
 public:
     string lexPalindromicPermutation(string s, string target) {
-        int n = s.size();
-        unordered_map<char, int> charCount;
-        for (char c : s) charCount[c]++;
-        
-        char oddChar = 0;
-        int oddCount = 0;
-        for (auto &[c, count] : charCount) {
-            if (count % 2 != 0) {
-                oddChar = c;
-                oddCount++;
-            }
-        }
-        
-        if ((oddCount > 1) || (oddCount == 1 && n % 2 == 0)) return "";
-
-        vector<char> halfPalindrome;
-        for (auto &[c, count] : charCount) {
-            halfPalindrome.insert(halfPalindrome.end(), count / 2, c);
-        }
-
-        sort(halfPalindrome.begin(), halfPalindrome.end());
-        string bestPalindrome = "";
-
-        do {
-            string halfStr(halfPalindrome.begin(), halfPalindrome.end());
-            string fullPalindrome = halfStr;
-            if (oddCount == 1) fullPalindrome += oddChar;
-            reverse(halfStr.begin(), halfStr.end());
-            fullPalindrome += halfStr;
-            if (fullPalindrome > target && (bestPalindrome.empty() || fullPalindrome < bestPalindrome)) {
-                bestPalindrome = fullPalindrome;
-            }
-        } while (next_permutation(halfPalindrome.begin(), halfPalindrome.end()));
-
-        return bestPalindrome;
+        // Implementation of solution based on planned steps
     }
 };
 # @lc code=end
