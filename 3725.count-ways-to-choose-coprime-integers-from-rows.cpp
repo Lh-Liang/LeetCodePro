d4a0d41367566d3e8b43d65faa3170f9ffb32b35
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 16:11:21 +0800

    Batch add 1 solutions: 3725.count-ways-to-c...

--- 3725.count-ways-to-choose-coprime-integers-from-rows.cpp
+++ 3725.count-ways-to-choose-coprime-integers-from-rows.cpp
@@ -1,13 +1,8 @@
-#
-# @lc app=leetcode id=3725 lang=cpp
-#
-# [3725] Count Ways to Choose Coprime Integers from Rows
-#
-
 # @lc code=start
 #include <vector>
-#include <numeric>
-
+#include <numeric> // for gcd
+typedef long long ll;
+const int MOD = 1000000007;
 using namespace std;
 
 class Solution {
@@ -15,70 +10,25 @@ public:
     int countCoprime(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        int max_val = 0;
-        for (const auto& row : mat) {
-            for (int val : row) {
-                if (val > max_val) max_val = val;
-            }
+        vector<vector<ll>> dp(m, vector<ll>(151));
+        // Initialize for the first row
+        for (int num : mat[0]) {
+            dp[0][num]++;
         }
-
-        if (max_val < 1) return 0;
-
-        // Mobius Sieve
-        vector<int> mu(max_val + 1, 0);
-        vector<int> primes;
-        vector<bool> is_prime(max_val + 1, true);
-        mu[1] = 1;
-        for (int i = 2; i <= max_val; ++i) {
-            if (is_prime[i]) {
-                primes.push_back(i);
-                mu[i] = -1;
-            }
-            for (int p : primes) {
-                if (1LL * i * p > max_val) break;
-                is_prime[i * p] = false;
-                if (i % p == 0) {
-                    mu[i * p] = 0;
-                    break;
-                } else {
-                    mu[i * p] = -mu[i];
+        
+        // Fill dp table for subsequent rows
+        for (int i = 1; i < m; ++i) {
+            vector<ll> new_dp(151);
+            for (int num : mat[i]) {
+                for (int g = 1; g <= 150; ++g) {
+                    int new_gcd = gcd(g, num);
+                    new_dp[new_gcd] = (new_dp[new_gcd] + dp[i-1][g]) % MOD;
                 }
             }
+            dp[i] = new_dp;
         }
-
-        // Row-wise counts for each possible value
-        vector<vector<int>> row_val_counts(m, vector<int>(max_val + 1, 0));
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                row_val_counts[i][mat[i][j]]++;
-            }
-        }
-
-        long long total_ans = 0;
-        const int MOD = 1e9 + 7;
-
-        // Inclusion-Exclusion using Mobius
-        for (int g = 1; g <= max_val; ++g) {
-            if (mu[g] == 0) continue;
-
-            long long ways_for_g = 1;
-            for (int i = 0; i < m; ++i) {
-                long long count_mult = 0;
-                for (int multiple = g; multiple <= max_val; multiple += g) {
-                    count_mult += row_val_counts[i][multiple];
-                }
-                ways_for_g = (ways_for_g * count_mult) % MOD;
-                if (ways_for_g == 0) break;
-            }
-
-            if (mu[g] == 1) {
-                total_ans = (total_ans + ways_for_g) % MOD;
-            } else {
-                total_ans = (total_ans - ways_for_g + MOD) % MOD;
-            }
-        }
-
-        return static_cast<int>(total_ans);
+        
+        return dp[m-1][1]; // Return the count of combinations with GCD=1
     }
 };
 # @lc code=end
