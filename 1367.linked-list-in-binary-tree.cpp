891363bb972b097676f518e3d1b3486544f119ac
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 18:11:46 +0800

    Batch add 1 solutions: 1367.linked-list-in-...

--- 1367.linked-list-in-binary-tree.cpp
+++ 1367.linked-list-in-binary-tree.cpp
@@ -1,41 +1,10 @@
-#
-# @lc app=leetcode id=1367 lang=cpp
-#
-# [1367] Linked List in Binary Tree
-#
-# @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- */
-class Solution {
-public:
-    bool isSubPath(ListNode* head, TreeNode* root) {
-        if (!root) return false;
-        return dfs(root, head) || isSubPath(head, root->left) || isSubPath(head, root->right);
-    }
-    
-private:
-    bool dfs(TreeNode* root, ListNode* head) {
-        if (!head) return true; // All nodes in linked list matched
-        if (!root || root->val != head->val) return false; // Mismatch or end of path
-        return dfs(root->left, head->next) || dfs(root->right, head->next);
-    }
-};
-# @lc code=end
+bool isSubPath(ListNode* head, TreeNode* root) {
+    if (!head) return true;
+    if (!root) return false;
+    return dfs(head, root) || isSubPath(head, root->left) || isSubPath(head, root->right);
+}
+bool dfs(ListNode* head, TreeNode* node) {
+    if (!head) return true;
+    if (!node) return false;
+    return (head->val == node->val) && (dfs(head->next, node->left) || dfs(head->next, node->right));
+}
