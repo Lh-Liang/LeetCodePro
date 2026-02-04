1c216cadb65f39a48945b4b2b1fb0e6a2379d58d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 14:24:24 +0800

    Batch add 1 solutions: 3501.maximize-active...

--- 3501.maximize-active-section-with-trade-ii.cpp
+++ 3501.maximize-active-section-with-trade-ii.cpp
@@ -1,93 +1,45 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <cmath>
+#
+# @lc app=leetcode id=3501 lang=cpp
+#
+# [3501] Maximize Active Section with Trade II
+#
 
-using namespace std;
-
-class SegmentTree {
-    int n;
-    vector<int> tree;
-public:
-    SegmentTree(const vector<int>& data) {
-        n = data.size();
-        if (n == 0) return;
-        tree.assign(4 * n, 0);
-        build(data, 1, 0, n - 1);
-    }
-    void build(const vector<int>& data, int node, int start, int end) {
-        if (start == end) {
-            tree[node] = data[start];
-            return;
-        }
-        int mid = (start + end) / 2;
-        build(data, 2 * node, start, mid);
-        build(data, 2 * node + 1, mid + 1, end);
-        tree[node] = max(tree[2 * node], tree[2 * node + 1]);
-    }
-    int query(int node, int start, int end, int l, int r) {
-        if (r < start || end < l) return 0;
-        if (l <= start && end <= r) return tree[node];
-        int mid = (start + end) / 2;
-        return max(query(2 * node, start, mid, l, r), query(2 * node + 1, mid + 1, end, l, r));
-    }
-};
-
+# @lc code=start
 class Solution {
 public:
     vector<int> maxActiveSectionsAfterTrade(string s, vector<vector<int>>& queries) {
-        int n = s.length();
-        int total_ones = 0;
-        for (char c : s) if (c == '1') total_ones++;
-
-        struct Block { int start, end, len; };
-        vector<Block> blocks;
-        for (int i = 0; i < n; ) {
-            if (s[i] == '0') {
-                int start = i;
-                while (i < n && s[i] == '0') i++;
-                blocks.push_back({start, i - 1, i - start});
-            } else i++;
-        }
-
-        if (blocks.empty()) return vector<int>(queries.size(), total_ones);
-
-        int m = blocks.size();
-        vector<int> pair_sums(m > 1 ? m - 1 : 0);
-        for (int i = 0; i < m - 1; ++i) {
-            pair_sums[i] = blocks[i].len + blocks[i + 1].len;
-        }
-
-        SegmentTree st(pair_sums);
-        vector<int> starts;
-        for (const auto& b : blocks) starts.push_back(b.start);
-
-        vector<int> ans;
-        for (const auto& q : queries) {
-            int L = q[0], R = q[1];
-            int i = lower_bound(starts.begin(), starts.end(), L) - starts.begin();
-            if (i > 0 && blocks[i - 1].end >= L) i--;
-            int j = upper_bound(starts.begin(), starts.end(), R) - starts.begin() - 1;
-
-            if (i >= j) {
-                ans.push_back(total_ones);
-                continue;
+        vector<int> results;
+        for (const auto& query : queries) {
+            int li = query[0], ri = query[1];
+            string t = "1" + s.substr(li, ri - li + 1) + "1"; // Augment the substring with 1s
+            int max_active = 0;
+            vector<int> zero_blocks, one_blocks;
+            
+            // Detect blocks and calculate initial active count
+            int i = 0;
+            while (i < t.size()) {
+                int j = i;
+                while (j < t.size() && t[j] == t[i]) j++;
+                if (t[i] == '0') zero_blocks.push_back(j - i);
+                else one_blocks.push_back(j - i);
+                i = j;
             }
 
-            int max_gain = 0;
-            int len_i = min(blocks[i].len, blocks[i].end - L + 1);
-            int len_j = min(blocks[j].len, R - blocks[j].start + 1);
+            // Calculate initial active sections count without any trade
+            int current_active = 0;
+            for (int len : one_blocks) current_active += len;
+            max_active = current_active;
 
-            if (j == i + 1) {
-                max_gain = len_i + len_j;
-            } else {
-                max_gain = max(len_i + blocks[i + 1].len, blocks[j - 1].len + len_j);
-                if (j - 1 > i + 1) {
-                    max_gain = max(max_gain, st.query(1, 0, pair_sums.size() - 1, i + 1, j - 2));
+            // Attempt trades and calculate maximum active sections
+            for (int z : zero_blocks) { // Consider each block of zeros that can be flipped to ones
+                // Ensure there is at least one block of ones that can be flipped to zeros first
+                if (one_blocks.size() > 1) { 
+                    max_active = max(max_active, current_active + z);
                 }
             }
-            ans.push_back(total_ones + max_gain);
+            results.push_back(max_active);
         }
-        return ans;
+        return results;
     }
 };
+# @lc code=end
