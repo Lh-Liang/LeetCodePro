c2a74597e9c695a7e39f490acc2091be139cd716
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 13:41:47 +0800

    Batch add 1 solutions: 3420.count-non-decre...

--- 3420.count-non-decreasing-subarrays-after-k-operations.cpp
+++ 3420.count-non-decreasing-subarrays-after-k-operations.cpp
@@ -3,53 +3,24 @@
 #
 # [3420] Count Non-Decreasing Subarrays After K Operations
 #
-
 # @lc code=start
-#include <vector>
-#include <deque>
-
-using namespace std;
-
 class Solution {
 public:
     long long countNonDecreasingSubarrays(vector<int>& nums, int k) {
-        int n = nums.size();
-        long long total_count = 0;
-        long long current_cost = 0;
-        int r = n - 1;
-        // Deque stores indices of elements that are maximums for some suffix
-        // Elements in deque are in increasing order of index and decreasing order of value
-        deque<int> dq;
-        
-        for (int l = n - 1; l >= 0; --l) {
-            // When adding nums[l] to the left, it may become the new prefix maximum
-            // for several elements to its right.
-            while (!dq.empty() && nums[l] >= nums[dq.front()]) {
-                int idx = dq.front();
-                dq.pop_front();
-                // This element was the maximum until the next element in the deque or the end of the window
-                int next_max_idx = dq.empty() ? r + 1 : dq.front();
-                current_cost += (long long)(next_max_idx - idx) * (nums[l] - nums[idx]);
+        long long count = 0;
+        int n = nums.size(), left = 0;
+        vector<int> operations(n, 0);
+        for (int right = 1; right < n; ++right) {
+            if (nums[right] < nums[right - 1]) {
+                operations[right] = nums[right - 1] - nums[right];
             }
-            dq.push_front(l);
-            
-            // If cost exceeds k, shrink window from the right
-            while (current_cost > (long long)k) {
-                // The contribution of nums[r] to the cost is (prefix_max_at_r - nums[r])
-                // The prefix_max_at_r is nums[dq.back()]
-                current_cost -= (long long)(nums[dq.back()] - nums[r]);
-                
-                // If r was the index providing the maximum value, remove it
-                if (r == dq.back()) {
-                    dq.pop_back();
-                }
-                r--;
+            operations[right] += operations[right - 1];
+            while (operations[right] - (left > 0 ? operations[left - 1] : 0) > k) {
+                ++left;
             }
-            
-            total_count += (long long)(r - l + 1);
+            count += right - left + 1;
         }
-        
-        return total_count;
+        return count + n; // Include single element subarrays which are always non-decreasing.
     }
 };
 # @lc code=end
