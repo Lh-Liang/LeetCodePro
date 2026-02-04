d1fde9d1a2528e0efe68ba6104194f0337bdb611
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 13:06:10 +0800

    Batch add 1 solutions: 2058.find-the-minimu...

--- 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,69 +1,35 @@
-#
+# 
 # @lc app=leetcode id=2058 lang=cpp
-#
+# 
 # [2058] Find the Minimum and Maximum Number of Nodes Between Critical Points
-#
+# 
 
 # @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-#include <vector>
-#include <algorithm>
-#include <climits>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        // A critical point needs a previous and next node, so we need at least 3 nodes.
-        if (!head || !head->next || !head->next->next) {
-            return {-1, -1};
-        }
-
-        int firstCriticalIdx = -1;
-        int lastCriticalIdx = -1;
-        int minDistance = INT_MAX;
-        
-        ListNode* prev = head;
-        ListNode* curr = head->next;
-        int currentIndex = 1; // 0-indexed or 1-indexed doesn't matter for distance
-
-        while (curr->next != nullptr) {
-            ListNode* next = curr->next;
-            bool isMaxima = (curr->val > prev->val && curr->val > next->val);
-            bool isMinima = (curr->val < prev->val && curr->val < next->val);
-
-            if (isMaxima || isMinima) {
-                if (firstCriticalIdx == -1) {
-                    firstCriticalIdx = currentIndex;
-                } else {
-                    // Update minimum distance using the gap between consecutive critical points
-                    minDistance = min(minDistance, currentIndex - lastCriticalIdx);
-                }
-                lastCriticalIdx = currentIndex;
+        if (!head || !head->next || !head->next->next) return {-1, -1};
+        vector<int> criticalPoints;
+        ListNode *previousNode = head;
+        ListNode *currentNode = head->next;
+        int index = 1; // Starting from second node as head is index 0.
+        while (currentNode && currentNode->next) {
+            ListNode *nextNode = currentNode->next;
+            if ((currentNode->val > previousNode->val && currentNode->val > nextNode->val) || // Local maxima
+                (currentNode->val < previousNode->val && currentNode->val < nextNode->val)) { // Local minima
+                criticalPoints.push_back(index);
             }
-
-            prev = curr;
-            curr = next;
-            currentIndex++;
+            previousNode = currentNode;
+            currentNode = nextNode;
+            ++index;
         }
-
-        // If we found fewer than two critical points
-        if (firstCriticalIdx == -1 || lastCriticalIdx == firstCriticalIdx) {
-            return {-1, -1};
+        if (criticalPoints.size() < 2) return {-1, -1};
+        int minDistance = INT_MAX;
+        for (size_t i = 1; i < criticalPoints.size(); ++i) {
+            minDistance = min(minDistance, criticalPoints[i] - criticalPoints[i - 1]);
         }
-
-        // Max distance is always the distance between the first and last critical points found
-        return {minDistance, lastCriticalIdx - firstCriticalIdx};
+        int maxDistance = criticalPoints.back() - criticalPoints.front();
+        return {minDistance, maxDistance};
     }
 };
 # @lc code=end
