017c3e2adcd6c21c0582e43888ab5c3e7780d794
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 18:23:41 +0800

    Batch add 1 solutions: 3217.delete-nodes-fr...

--- 3217.delete-nodes-from-linked-list-present-in-array.cpp
+++ 3217.delete-nodes-from-linked-list-present-in-array.cpp
@@ -3,34 +3,33 @@
 #
 # [3217] Delete Nodes From Linked List Present in Array
 #
-
 # @lc code=start
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode() : val(0), next(nullptr) {}
+*     ListNode(int x) : val(x), next(nullptr) {}
+*     ListNode(int x, ListNode *next) : val(x), next(next) {}
+* };
+*/
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        std::unordered_set<int> numSet(nums.begin(), nums.end()); // Step 1: Initialize hash set.
-        ListNode dummy(0); // Step 2: Create dummy node.
-        dummy.next = head;
-        ListNode* prev = &dummy;
-        while (head != nullptr) { // Step 3: Traverse using two pointers.
-            if (numSet.find(head->val) != numSet.end()) { // Step 4: Check if current node's value is in hash set.
-                prev->next = head->next; // Remove current node.
-            } else {
-                prev = head; // Move prev only if current node is not removed.
-            }
-            head = head->next; // Move current pointer forward (Step 5).
-        }
-        return dummy.next; // Step 6: Return new head of modified list.
-    }
-};
+        std::unordered_set<int> numSet(nums.begin(), nums.end()); // Step 1: Convert nums to set for quick lookup.
+        ListNode dummy(0); // Step 2: Create a dummy node.
+        dummy.next = head; // Point dummy node to head. 
+        ListNode *prev = &dummy; // Step 3: Initialize prev pointer at dummy. 
+        while (head != nullptr) { 
+            if (numSet.count(head->val)) { 
+                prev->next = head->next; // Skip node if value is in numSet. 
+            } else { 
+                prev = head; // Move prev pointer if no removal needed. 
+            } 
+            head = head->next; // Move forward in list. 
+        } 
+        return dummy.next; // Step 4: Return modified list starting from dummy.next. 
+    } 
+}; 
 # @lc code=end
