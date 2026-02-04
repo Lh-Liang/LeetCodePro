4572431d740938b59473bc7c1d721feea8dba930
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 05:18:28 +0800

    Batch add 1 solutions: 3615.longest-palindr...

--- 3615.longest-palindromic-path-in-graph.cpp
+++ 3615.longest-palindromic-path-in-graph.cpp
@@ -8,8 +8,41 @@
 class Solution {
 public:
     int maxLen(int n, vector<vector<int>>& edges, string label) {
-        // Implementation of DFS/backtracking approach to find longest palindromic path
-        // Code logic here...
+        auto isPalindrome = [](const string& s) {
+            int l = 0, r = s.size() - 1;
+            while (l < r) {
+                if (s[l++] != s[r--]) return false;
+            }
+            return true;
+        };
+        vector<vector<int>> g(n);
+        for (const auto& e : edges) {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
+        int res = 1;
+        vector<bool> visited(n);
+        string path;
+        function<void(int)> dfs = [&](int u) {
+            visited[u] = true;
+            path.push_back(label[u]);
+            if (isPalindrome(path)) {
+                res = max(res, (int)path.size());
+            }
+            for (int v : g[u]) {
+                if (!visited[v]) {
+                    dfs(v);
+                }
+            }
+            path.pop_back();
+            visited[u] = false;
+        };
+        for (int i = 0; i < n; ++i) {
+            fill(visited.begin(), visited.end(), false);
+            path.clear();
+            dfs(i);
+        }
+        return res;
     }
 };
 # @lc code=end
