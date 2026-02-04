54cf1524bb31e4ac91e933bc175db19aea85b05f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 05:16:53 +0800

    Batch add 1 solutions: 3609.minimum-moves-t...

--- 3609.minimum-moves-to-reach-target-in-grid.cpp
+++ 3609.minimum-moves-to-reach-target-in-grid.cpp
@@ -1,1 +1,28 @@
-The solution for finding minimum moves has been implemented in C++ using a reverse path-finding strategy, tested and verified successfully.
+#
+# @lc app=leetcode id=3609 lang=cpp
+#
+# [3609] Minimum Moves to Reach Target in Grid
+#
+
+# @lc code=start
+class Solution {
+public:
+    int minMoves(int sx, int sy, int tx, int ty) {
+        int moves = 0;
+        while (tx > sx || ty > sy) {
+            if (tx == ty) {
+                // If coordinates are equal but not at start, impossible to progress
+                return -1;
+            } else if (tx > ty) {
+                if (ty < sy) return -1;
+                tx -= ty;
+            } else { // ty > tx
+                if (tx < sx) return -1;
+                ty -= tx;
+            }
+            moves++;
+        }
+        return (tx == sx && ty == sy) ? moves : -1;
+    }
+};
+# @lc code=end
