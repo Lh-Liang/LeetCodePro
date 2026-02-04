35c20a8c2fc14daf8ecf10f5c0ec0403a61d7cba
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 13:13:44 +0800

    Batch add 1 solutions: 3217.delete-nodes-fr...

--- 3217.delete-nodes-from-linked-list-present-in-array.cpp
+++ 3217.delete-nodes-from-linked-list-present-in-array.cpp
@@ -15,29 +15,22 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-#include <unordered_set>
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        unordered_set<int> numSet(nums.begin(), nums.end());
-        ListNode dummy(0, head);
-        ListNode* curr = &dummy;
-
-        while (curr->next != nullptr) {
-            if (numSet.find(curr->next->val) != numSet.end()) {
-                ListNode* temp = curr->next;
-                curr->next = curr->next->next;
-                delete temp;
+        std::unordered_set<int> numSet(nums.begin(), nums.end()); // Step 1: Initialize hash set.
+        ListNode dummy(0); // Step 2: Create dummy node.
+        dummy.next = head;
+        ListNode* prev = &dummy;
+        while (head != nullptr) { // Step 3: Traverse using two pointers.
+            if (numSet.find(head->val) != numSet.end()) { // Step 4: Check if current node's value is in hash set.
+                prev->next = head->next; // Remove current node.
             } else {
-                curr = curr->next;
+                prev = head; // Move prev only if current node is not removed.
             }
+            head = head->next; // Move current pointer forward (Step 5).
         }
-
-        return dummy.next;
+        return dummy.next; // Step 6: Return new head of modified list.
     }
 };
 # @lc code=end
