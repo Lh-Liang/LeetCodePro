4b235e0bc03a0778eac15bba75eea37d6953bcda
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 13:12:06 +0800

    Batch add 1 solutions: 2807.insert-greatest...

--- 2807.insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807.insert-greatest-common-divisors-in-linked-list.cpp
@@ -1,40 +1,33 @@
-#include <numeric>
-
 #
 # @lc app=leetcode id=2807 lang=cpp
 #
 # [2807] Insert Greatest Common Divisors in Linked List
 #
-
 # @lc code=start
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode() : val(0), next(nullptr) {}
+*     ListNode(int x) : val(x), next(nullptr) {}
+*     ListNode(int x, ListNode *next) : val(x), next(next) {}
+* };
+*/
+#include <algorithm> // For std::gcd function
 class Solution {
 public:
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
-        if (!head || !head->next) return head;
-
-        ListNode* curr = head;
-        while (curr != nullptr && curr->next != nullptr) {
-            int gcdValue = std::gcd(curr->val, curr->next->val);
-            ListNode* newNode = new ListNode(gcdValue);
-            
-            newNode->next = curr->next;
-            curr->next = newNode;
-            
-            // Move curr to the node after the newly inserted GCD node
-            curr = newNode->next;
+        if (!head || !head->next) return head; // Check if list has fewer than two nodes; return early as no GCD insertion is possible.
+        ListNode* current = head;
+        while (current && current->next) {
+            int gcdValue = std::gcd(current->val, current->next->val); // Compute GCD of current pair of nodes' values.
+            ListNode* gcdNode = new ListNode(gcdValue); // Create a new node with this GCD value.
+            gcdNode->next = current->next; // Link new node to next node.
+            current->next = gcdNode; // Insert new node after current node.
+            current = gcdNode->next; // Move to the next pair of nodes, skipping over newly inserted node.
         }
-        
-        return head;
+        return head; // Return modified list starting from head after all insertions.
     }
 };
 # @lc code=end
