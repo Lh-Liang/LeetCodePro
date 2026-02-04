50e65e8237e017aa5e875042cbd9b6dcac13adf1
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 19:47:15 +0800

    Batch add 1 solutions: 3563.lexicographical...

--- 3563.lexicographically-smallest-string-after-adjacent-removals.cpp
+++ 3563.lexicographically-smallest-string-after-adjacent-removals.cpp
@@ -1,24 +1,26 @@
+#
 # @lc app=leetcode id=3563 lang=cpp
 #
 # [3563] Lexicographically Smallest String After Adjacent Removals
 #
+
 # @lc code=start
 class Solution {
 public:
     string lexicographicallySmallestString(string s) {
-        std::stack<char> stk;
+        stack<char> stk;
         for (char c : s) {
-            while (!stk.empty() && ((stk.top() == c + 1) || (stk.top() == c - 1) || (stk.top() == 'a' && c == 'z') || (stk.top() == 'z' && c == 'a'))) {
+            while (!stk.empty() && ((stk.top() == c + 1) || (stk.top() == c - 1))) {
                 stk.pop();
             }
             stk.push(c);
         }
-        std::string result;
+        string result;
         while (!stk.empty()) {
             result += stk.top();
             stk.pop();
         }
-        std::reverse(result.begin(), result.end());
+        reverse(result.begin(), result.end());
         return result;
     }
 };
