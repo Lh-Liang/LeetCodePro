beeebcc5339aafdbab881a4b5220d4b4fd10200f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 18:14:50 +0800

    Batch add 1 solutions: 1670.design-front-mi...

--- 1670.design-front-middle-back-queue.cpp
+++ 1670.design-front-middle-back-queue.cpp
@@ -1,33 +1,41 @@
+#
 # @lc app=leetcode id=1670 lang=cpp
+#
 # [1670] Design Front Middle Back Queue
 #
+
 # @lc code=start
 #include <deque>
 using namespace std;
-
 class FrontMiddleBackQueue {
-private:
-    deque<int> front;
-    deque<int> back;
-    
-    void balance() {
-        // Ensure that front does not have more than one extra element than back
-        if (front.size() > back.size() + 1) {
-            back.push_front(front.back());
-            front.pop_back();
-        } else if (back.size() > front.size()) {
-            front.push_back(back.front());
-            back.pop_front();
-        }
-    }
-    
 public:
-    FrontMiddleBackQueue() {
-        
+    deque<int> dq;
+    FrontMiddleBackQueue() { }
+    void pushFront(int val) { dq.push_front(val); }
+    void pushMiddle(int val) { 
+        int mid = dq.size() / 2; 
+        dq.insert(dq.begin() + mid, val); 
     }
-    
-    void pushFront(int val) {
-        front.push_front(val);
-        balance();																	
-    }	 	 	 	 	 	 	 	 	 	 	  void pushMiddle(int val) {           // Insert into middle by pushing into front when equal or more elements in front       if (front.size() >= back.size()) {           front.push_back(val);       } else {           back.push_front(val);       }       balance();     }      void pushBack(int val) {         back.push_back(val);         balance();     }      int popFront() {         if (front.empty() && back.empty()) return -1;         int val = front.empty() ? back.front() : front.front();         if (!front.empty()) {             front.pop_front();         } else {             back.pop_front();         }         balance();         return val;     }      int popMiddle() {         if (front.empty() && back.empty()) return -1;         int val = (front.size() > back.size()) ? front.back() : back.front();         if (front.size() > back.size()) {             front.pop_back();         } else {             back.pop_front();         }         balance();         return val;     }      int popBack() {         if (back.empty() && front.empty()) return -1;         int val = !back.empty() ? back.back() : front.back();         if (!back.empty()) {             back.pop_back();         } else {             front.pop_back();         }         balance();         return val;     }};
-t
+    void pushBack(int val) { dq.push_back(val); }
+    int popFront() { 
+        if(dq.empty()) return -1; 
+        int val = dq.front(); 
+        dq.pop_front(); 
+        return val; 
+    }
+    int popMiddle() { 
+        if(dq.empty()) return -1; 
+        int mid = (dq.size() - 1) / 2; 
+        auto it = dq.begin() + mid; 
+        int val = *it; 
+        dq.erase(it); 
+        return val; 
+    }
+    int popBack() { 
+        if(dq.empty()) return -1; 
+        int val = dq.back(); 
+        dq.pop_back(); 
+        return val; 
+    }
+};
+done
