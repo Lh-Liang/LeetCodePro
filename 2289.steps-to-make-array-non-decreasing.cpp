f037fe1f7f362571983254e95f132871fc8e020a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 03:15:20 +0800

    Batch add 1 solutions: 2289.steps-to-make-a...

--- 2289.steps-to-make-array-non-decreasing.cpp
+++ 2289.steps-to-make-array-non-decreasing.cpp
@@ -1,26 +1,26 @@
-#@lc app=leetcode id=2289 lang=cpp
+#
+# @lc app=leetcode id=2289 lang=cpp
 #
 # [2289] Steps to Make Array Non-decreasing
 #
+
 # @lc code=start
 class Solution {
 public:
     int totalSteps(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, 0); // dp array to store steps for each element
-        stack<int> stk;
-        int maxSteps = 0;
-        
-        // Iterate over each number in nums
+        int n = nums.size(), ans = 0;
+        stack<pair<int, int>> st; // pair: value, steps needed
         for (int i = 0; i < n; ++i) {
-            while (!stk.empty() && nums[stk.top()] <= nums[i]) {
-                dp[i] = max(dp[i], dp[stk.top()] + 1); // Calculate steps required for current element
-                stk.pop(); // Pop elements that are less than or equal to current element from stack
+            int maxT = 0;
+            while (!st.empty() && nums[i] >= st.top().first) {
+                maxT = max(maxT, st.top().second);
+                st.pop();
             }
-            stk.push(i); // Push current index onto stack
-            maxSteps = max(maxSteps, dp[i]); // Update maxSteps if current dp[i] is greater than maxSteps so far
+            int step = st.empty() ? 0 : maxT + 1;
+            ans = max(ans, step);
+            st.push({nums[i], step});
         }
-        return maxSteps; // Return maximum steps required for entire array to be non-decreasing 
-    } 
-}; 
+        return ans;
+    }
+};
 # @lc code=end
