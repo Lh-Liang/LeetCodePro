5d65937bbfd754fc6b450dd0931b332690dca624
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 06:07:12 +0800

    Batch add 1 solutions: 3782.last-remaining-...

--- 3782.last-remaining-integer-after-alternating-deletion-operations.cpp
+++ 3782.last-remaining-integer-after-alternating-deletion-operations.cpp
@@ -3,23 +3,23 @@
 #
 # [3782] Last Remaining Integer After Alternating Deletion Operations
 #
-
 # @lc code=start
 class Solution {
 public:
     long long lastInteger(long long n) {
-        long long start = 1;
+        long long head = 1;
         long long step = 1;
         bool left = true;
         while (n > 1) {
-            if (left || n % 2 == 1) { // if going left or odd count, update start
-                start += step;
+            // Move head if deleting from left, or from right and n is odd
+            if (left || n % 2 == 1) {
+                head += step;
             }
-            n /= 2; // halve the count of numbers remaining
-            step *= 2; // double the step size for next round
-            left = !left; // alternate direction
+            n /= 2;
+            step *= 2;
+            left = !left;
         }
-        return start;
+        return head;
     }
 };
 # @lc code=end
