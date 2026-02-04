19ff183a8727a07ead8a54632e9b1d8ce0fa09e0
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 19:13:37 +0800

    Batch add 1 solutions: 3503.longest-palindr...

--- 3503.longest-palindrome-after-substring-concatenation-i.cpp
+++ 3503.longest-palindrome-after-substring-concatenation-i.cpp
@@ -9,32 +9,19 @@ class Solution {
 public:
     int longestPalindrome(string s, string t) {
         int maxLength = 0;
-        
-        // Helper lambda to check if a string is palindrome
         auto isPalindrome = [](const string& str) {
-            int left = 0, right = str.size() - 1;
-            while (left < right) {
-                if (str[left] != str[right]) return false;
-                ++left;
-                --right;
-            }
-            return true;
+            return str == string(str.rbegin(), str.rend());
         };
         
-        // Check all combinations of prefixes and suffixes for both strings
         for (int i = 0; i <= s.size(); ++i) {
             for (int j = 0; j <= t.size(); ++j) {
-                string concat1 = s.substr(0, i) + t.substr(j);
-                string concat2 = t.substr(0, j) + s.substr(i);
-                if (isPalindrome(concat1)) {
-                    maxLength = max(maxLength, static_cast<int>(concat1.size()));
-                }
-                if (isPalindrome(concat2)) {
-                    maxLength = max(maxLength, static_cast<int>(concat2.size()));
+                string combined = s.substr(0, i) + t.substr(0, j);
+                if (isPalindrome(combined)) {
+                    maxLength = max(maxLength, static_cast<int>(combined.size()));
                 }
             }
         }
         return maxLength;
-    }									
-};								
+    }
+};
 # @lc code=end
