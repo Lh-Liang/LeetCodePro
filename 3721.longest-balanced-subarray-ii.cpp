2437707cd998ce4b0a6bcc7f6922209e59de7ccf
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 05:38:07 +0800

    Batch add 1 solutions: 3721.longest-balance...

--- 3721.longest-balanced-subarray-ii.cpp
+++ 3721.longest-balanced-subarray-ii.cpp
@@ -3,30 +3,42 @@
 #
 # [3721] Longest Balanced Subarray II
 #
-
 # @lc code=start
+#include <vector>
+#include <unordered_map>
+using namespace std;
 class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
-        unordered_map<int, int> difference_index;
-        difference_index[0] = -1; // initial condition for zero difference
-        int maxLength = 0, evenCount = 0, oddCount = 0;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] % 2 == 0) {
-                evenCount++; // increment if even number
-            } else {
-                oddCount++; // increment if odd number
+        int n = nums.size();
+        unordered_map<int, int> even_count, odd_count;
+        int left = 0, res = 0;
+        for (int right = 0; right < n; ++right) {
+            int val = nums[right];
+            if (val % 2 == 0) ++even_count[val];
+            else ++odd_count[val];
+            while (left <= right && even_count.size() > odd_count.size()) {
+                int lval = nums[left];
+                if (lval % 2 == 0) {
+                    if (--even_count[lval] == 0) even_count.erase(lval);
+                } else {
+                    if (--odd_count[lval] == 0) odd_count.erase(lval);
+                }
+                ++left;
             }
-            int diff = evenCount - oddCount; // calculate current difference
-            
-            if (difference_index.find(diff) != difference_index.end()) {
-                maxLength = max(maxLength, i - difference_index[diff]); // calculate max length from previous occurrence of same diff
-            } else {
-                difference_index[diff] = i; // store first occurrence of this diff in map
+            while (left <= right && odd_count.size() > even_count.size()) {
+                int lval = nums[left];
+                if (lval % 2 == 0) {
+                    if (--even_count[lval] == 0) even_count.erase(lval);
+                } else {
+                    if (--odd_count[lval] == 0) odd_count.erase(lval);
+                }
+                ++left;
             }
+            if (even_count.size() == odd_count.size())
+                res = max(res, right - left + 1);
         }
-        return maxLength; // return the maximum length found
+        return res;
     }
 };
 # @lc code=end
