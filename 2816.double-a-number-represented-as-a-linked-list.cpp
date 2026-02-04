0bc6ed320570434806d8591570870eeb2c83f347
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 13:14:21 +0800

    Batch add 1 solutions: 2816.double-a-number...

--- 2816.double-a-number-represented-as-a-linked-list.cpp
+++ 2816.double-a-number-represented-as-a-linked-list.cpp
@@ -3,38 +3,33 @@
 #
 # [2816] Double a Number Represented as a Linked List
 #
-
 # @lc code=start
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode() : val(0), next(nullptr) {}
+*     ListNode(int x) : val(x), next(nullptr) {}
+*     ListNode(int x, ListNode *next) : val(x), next(next) {}
+* };
+*/
 class Solution {
 public:
     ListNode* doubleIt(ListNode* head) {
-        // If the head will result in a carry, prepend a dummy node
-        if (head->val >= 5) {
-            head = new ListNode(0, head);
-        }
-        
-        ListNode* curr = head;
-        while (curr != nullptr) {
-            // Double the current digit and handle carry-out from the next node
-            int newVal = (curr->val * 2) % 10;
-            if (curr->next != nullptr && curr->next->val >= 5) {
-                newVal += 1;
+        int carry = 0; // Initialize carry to zero for any overflow during doubling
+        ListNode* current = head; // Start at head of linked list
+        while (current != nullptr) { // Traverse each node in list
+            int newVal = current->val * 2 + carry; // Double current value and add carry
+            current->val = newVal % 10; // Update current node with the single digit result
+            carry = newVal / 10; // Calculate new carry for next iteration if any overflow occurred
+            if (!current->next && carry > 0) { // If at end, check if an extra node is needed for remaining carry
+                current->next = new ListNode(carry); // Append new node with remaining carry value
+                break; 
             }
-            curr->val = newVal;
-            curr = curr->next;
+            current = current->next; // Move to next node in list for further processing
         }
-        
-        return head;
+        return head; // Return modified list representing doubled number
     }
 };
 # @lc code=end
