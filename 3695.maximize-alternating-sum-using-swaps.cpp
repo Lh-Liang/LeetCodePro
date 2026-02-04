b8933e3bfd9f0fa506ef2402f352eb23533694d0
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 05:33:47 +0800

    Batch add 1 solutions: 3695.maximize-altern...

--- 3695.maximize-alternating-sum-using-swaps.cpp
+++ 3695.maximize-alternating-sum-using-swaps.cpp
@@ -3,7 +3,6 @@
 #
 # [3695] Maximize Alternating Sum Using Swaps
 #
-
 # @lc code=start
 class Solution {
 public:
@@ -11,23 +10,52 @@ public:
         int n = nums.size();
         vector<int> parent(n);
         iota(parent.begin(), parent.end(), 0);
-        function<int(int)> find = [&](int x) { return parent[x] == x ? x : parent[x] = find(parent[x]); };
-        for (const auto& swap : swaps) {
-            int u = find(swap[0]), v = find(swap[1]);
-            if (u != v) parent[u] = v;
+        function<int(int)> find = [&](int x) {
+            if (parent[x] != x) parent[x] = find(parent[x]);
+            return parent[x];
+        };
+        for (auto& s : swaps) {
+            int a = find(s[0]), b = find(s[1]);
+            if (a != b) parent[a] = b;
         }
         unordered_map<int, vector<int>> groups;
         for (int i = 0; i < n; ++i) {
-            groups[find(i)].push_back(nums[i]);
+            groups[find(i)].push_back(i);
         }
-        for (auto& [_, group] : groups) {
-            sort(group.rbegin(), group.rend()); // Sort in descending order for optimal placement.
+        vector<int> res(n);
+        for (auto& [_, idxs] : groups) {
+            vector<int> vals;
+            for (int i : idxs) vals.push_back(nums[i]);
+            sort(idxs.begin(), idxs.end());
+            sort(vals.rbegin(), vals.rend()); // descending
+            // Assign largest values to smallest indices (even positions)
+            for (int j = 0; j < idxs.size(); ++j) {
+                res[idxs[j]] = vals[j];
+            }
+            // Verification step: For small components, check both possible assignments (start even or odd)
+            if (idxs.size() <= 10) {
+                vector<int> perm = vals;
+                long long best = LLONG_MIN;
+                sort(perm.begin(), perm.end());
+                do {
+                    long long cur = 0;
+                    for (int j = 0; j < idxs.size(); ++j) {
+                        if (idxs[j] % 2 == 0) cur += perm[j];
+                        else cur -= perm[j];
+                    }
+                    if (cur > best) {
+                        for (int j = 0; j < idxs.size(); ++j) res[idxs[j]] = perm[j];
+                        best = cur;
+                    }
+                } while (next_permutation(perm.begin(), perm.end()));
+            }
         }
-        vector<int> result(n);
-        unordered_map<int, int> indices;
+        long long ans = 0;
         for (int i = 0; i < n; ++i) {
-            int root = find(i);
-            result[i] = groups[root][indices[root]++];
+            if (i % 2 == 0) ans += res[i];
+            else ans -= res[i];
         }
-        long long maxSum = 0;
-        for (int i = 0; i < n; ++i) { "maxSum += (i % 2 == 0 ? result[i] : -result[i]); "}" return maxSum; "}"}; "# @lc code=end "}
+        return ans;
+    }
+};
+# @lc code=end
