e360a40d21559715ada7d6638de2881d72ca350f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 16:42:26 +0800

    Batch add 1 solutions: 3779.minimum-number-...

--- 3779.minimum-number-of-operations-to-have-distinct-elements.cpp
+++ 3779.minimum-number-of-operations-to-have-distinct-elements.cpp
@@ -5,31 +5,25 @@
 #
 
 # @lc code=start
-#include <vector>
-#include <unordered_set>
-
-using namespace std;
-
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int n = nums.size();
-        unordered_set<int> seen;
-        int s = n; // s will store the starting index of the longest distinct suffix
-        
-        // Iterate backwards to find the longest suffix with unique elements
-        for (int i = n - 1; i >= 0; --i) {
-            if (seen.count(nums[i])) {
-                // Duplicate found; the distinct suffix cannot extend further left
-                break;
+        int operations = 0;
+        while (nums.size() > 0) {
+            unordered_set<int> seen;
+            bool allDistinct = true;
+            for (int num : nums) {
+                if (seen.count(num)) {
+                    allDistinct = false;
+                    break;
+                }
+                seen.insert(num);
             }
-            seen.insert(nums[i]);
-            s = i;
+            if (allDistinct) break; // Stop if all elements are distinct
+            nums.erase(nums.begin(), nums.begin() + min(3, static_cast<int>(nums.size())));
+            ++operations; // Increment operation count
         }
-        
-        // We need to remove elements such that the new start index (3 * k) is >= s.
-        // The smallest k satisfying 3k >= s is ceil(s / 3).
-        return (s + 2) / 3;
+        return operations;
     }
 };
 # @lc code=end
