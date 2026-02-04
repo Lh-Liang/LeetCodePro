026ce7cfe20e8c09c0cc7c0d23dc441b33ee56bf
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 04:42:58 +0800

    Batch add 1 solutions: 3525.find-x-value-of...

--- 3525.find-x-value-of-array-ii.cpp
+++ 3525.find-x-value-of-array-ii.cpp
@@ -1,1 +1,29 @@
-Solution code for LeetCode problem 3525 has been implemented, optimized, and tested.
+#
+# @lc app=leetcode id=3525 lang=cpp
+#
+# [3525] Find X Value of Array II
+#
+
+# @lc code=start
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums, int k, vector<vector<int>>& queries) {
+        int n = nums.size();
+        vector<int> res;
+        for (auto& q : queries) {
+            int idx = q[0], val = q[1], s = q[2], x = q[3];
+            nums[idx] = val;
+            int m = n - s;
+            int answer = 0;
+            // Compute the product of nums[s..j] for all j in [s, n-1]
+            int prod = 1;
+            for (int j = s; j < n; ++j) {
+                prod = (1LL * prod * nums[j]) % k;
+                if (prod == x) answer++;
+            }
+            res.push_back(answer);
+        }
+        return res;
+    }
+};
+# @lc code=end
