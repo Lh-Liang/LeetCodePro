ca207a5855deff38c23ad535b71cf4a67ed37ba5
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 15:59:02 +0800

    Batch add 1 solutions: 3686.number-of-stabl...

--- 3686.number-of-stable-subsequences.cpp
+++ 3686.number-of-stable-subsequences.cpp
@@ -1,6 +1,3 @@
-#include <vector>
-using namespace std;
-
 #
 # @lc app=leetcode id=3686 lang=cpp
 #
@@ -11,36 +8,20 @@ using namespace std;
 class Solution {
 public:
     int countStableSubsequences(vector<int>& nums) {
-        long long mod = 1e9 + 7;
-        // dp[parity][count-1]
-        // dp[0][0]: ends in one even, dp[0][1]: ends in two evens
-        // dp[1][0]: ends in one odd,  dp[1][1]: ends in two odds
-        long long dp00 = 0, dp01 = 0, dp10 = 0, dp11 = 0;
-
-        for (int num : nums) {
-            int p = num % 2;
-            if (p == 0) {
-                // New count for subsequences ending in two evens
-                long long next_dp01 = (dp01 + dp00) % mod;
-                // New count for subsequences ending in one even
-                // (starts new, or follows any odd-ending sequence)
-                long long next_dp00 = (dp00 + 1 + dp10 + dp11) % mod;
-                
-                dp01 = next_dp01;
-                dp00 = next_dp00;
-            } else {
-                // New count for subsequences ending in two odds
-                long long next_dp11 = (dp11 + dp10) % mod;
-                // New count for subsequences ending in one odd
-                // (starts new, or follows any even-ending sequence)
-                long long next_dp10 = (dp10 + 1 + dp00 + dp01) % mod;
-
-                dp11 = next_dp11;
-                dp10 = next_dp10;
+        const int MOD = 1e9 + 7;
+        int n = nums.size();
+        vector<int> even(n + 1), odd(n + 1);
+        even[0] = odd[0] = 1; // Base case: empty subsequence
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] % 2 == 0) { // Current number is even
+                even[i + 1] = (even[i] + odd[i]) % MOD;
+                odd[i + 1] = odd[i]; // No change in odd count as no new odd is added
+            } else { // Current number is odd
+                odd[i + 1] = (even[i] + odd[i]) % MOD;
+                even[i + 1] = even[i]; // No change in even count as no new even is added
             }
         }
-
-        return (dp00 + dp01 + dp10 + dp11) % mod;
+        return (even[n] + odd[n] - 1) % MOD; // Subtracting 1 to exclude empty subsequence
     }
 };
 # @lc code=end
