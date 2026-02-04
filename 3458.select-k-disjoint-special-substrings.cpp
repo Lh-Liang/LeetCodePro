734a18e2c806abc3e929f952dc5a937a033d34dd
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 18:55:41 +0800

    Batch add 1 solutions: 3458.select-k-disjoi...

--- 3458.select-k-disjoint-special-substrings.cpp
+++ 3458.select-k-disjoint-special-substrings.cpp
@@ -3,48 +3,22 @@
 #
 # [3458] Select K Disjoint Special Substrings
 #
+
 # @lc code=start
 class Solution {
 public:
     bool maxSubstringLength(string s, int k) {
-        unordered_map<char, int> frequency;
+        unordered_map<char, int> freq;
         for (char c : s) {
-            frequency[c]++;
+            freq[c]++;
         }
-        vector<pair<int, int>> potentialSubstrings;
-        int i = 0;
-        while (i < s.size()) {
-            // Find start of potential substring
-            while (i < s.size() && frequency[s[i]] != 1) {
-                i++;
-            }
-            int start = i;
-            // Find end of potential substring
-            while (i < s.size() && frequency[s[i]] == 1) {
-                i++;
-            }
-            int end = i;
-            if (end > start) {
-                potentialSubstrings.push_back({start, end});
+        int unique_count = 0;
+        for (const auto &entry : freq) {
+            if (entry.second == 1) {
+                unique_count++;
             }
         }
-        // Validate isolation and count valid substrings
-        int specialCount = 0;
-        for (auto &range : potentialSubstrings) {
-            unordered_set<char> seen(s.begin() + range.first, s.begin() + range.second);
-            bool valid = true;
-            for (char c : seen) {
-                if (frequency[c] != 1) { // Shouldn't appear more than once overall
-                    valid = false;
-                    break;
-                }
-            }
-            if (valid) {
-                specialCount++;
-                if (specialCount >= k) return true;
-            }
-        }
-        return false;
+        return unique_count >= k;
     }
 };
 # @lc code=end
