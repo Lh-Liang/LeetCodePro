23bc6f09ac6f4dcef1808ac38ed9ccca4d4c2a7e
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 16:18:32 +0800

    Batch add 1 solutions: 3734.lexicographical...

--- 3734.lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
+++ 3734.lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -5,108 +5,44 @@
 #
 
 # @lc code=start
-#include <string>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     string lexPalindromicPermutation(string s, string target) {
-        int n = s.length();
-        vector<int> counts(26, 0);
-        for (char c : s) counts[c - 'a']++;
-
-        int odd_count = 0;
-        int mid_idx = -1;
-        for (int i = 0; i < 26; ++i) {
-            if (counts[i] % 2 == 1) {
-                odd_count++;
-                mid_idx = i;
+        int n = s.size();
+        unordered_map<char, int> charCount;
+        for (char c : s) charCount[c]++;
+        
+        char oddChar = 0;
+        int oddCount = 0;
+        for (auto &[c, count] : charCount) {
+            if (count % 2 != 0) {
+                oddChar = c;
+                oddCount++;
             }
         }
+        
+        if ((oddCount > 1) || (oddCount == 1 && n % 2 == 0)) return "";
 
-        if (odd_count > (n % 2)) return "";
-
-        int m = n / 2;
-        vector<int> half_counts(26, 0);
-        for (int i = 0; i < 26; ++i) half_counts[i] = counts[i] / 2;
-
-        string mid = "";
-        if (n % 2 == 1) mid += (char)('a' + mid_idx);
-
-        string best_p = "";
-
-        // Case A: Match prefix exactly
-        bool possible = true;
-        vector<int> temp_counts = half_counts;
-        for (int i = 0; i < m; ++i) {
-            if (--temp_counts[target[i] - 'a'] < 0) {
-                possible = false;
-                break;
-            }
-        }
-        if (possible) {
-            string Q = target.substr(0, m);
-            string P = Q + mid;
-            string revQ = Q;
-            reverse(revQ.begin(), revQ.end());
-            P += revQ;
-            if (P > target) {
-                best_p = P;
-            }
+        vector<char> halfPalindrome;
+        for (auto &[c, count] : charCount) {
+            halfPalindrome.insert(halfPalindrome.end(), count / 2, c);
         }
 
-        // Case B: Smallest prefix strictly greater than target's first half
-        vector<bool> prefix_possible(m + 1, false);
-        prefix_possible[0] = true;
-        vector<int> p_counts = half_counts;
-        for (int i = 0; i < m; ++i) {
-            if (--p_counts[target[i] - 'a'] >= 0) {
-                prefix_possible[i + 1] = true;
-            } else {
-                break;
+        sort(halfPalindrome.begin(), halfPalindrome.end());
+        string bestPalindrome = "";
+
+        do {
+            string halfStr(halfPalindrome.begin(), halfPalindrome.end());
+            string fullPalindrome = halfStr;
+            if (oddCount == 1) fullPalindrome += oddChar;
+            reverse(halfStr.begin(), halfStr.end());
+            fullPalindrome += halfStr;
+            if (fullPalindrome > target && (bestPalindrome.empty() || fullPalindrome < bestPalindrome)) {
+                bestPalindrome = fullPalindrome;
             }
-        }
+        } while (next_permutation(halfPalindrome.begin(), halfPalindrome.end()));
 
-        string candidate2 = "";
-        for (int i = m - 1; i >= 0; --i) {
-            if (prefix_possible[i]) {
-                vector<int> rem_counts = half_counts;
-                for (int k = 0; k < i; ++k) rem_counts[target[k] - 'a']--;
-                
-                int best_char = -1;
-                for (int c = (target[i] - 'a' + 1); c < 26; ++c) {
-                    if (rem_counts[c] > 0) {
-                        best_char = c;
-                        break;
-                    }
-                }
-
-                if (best_char != -1) {
-                    string Q = target.substr(0, i);
-                    Q += (char)('a' + best_char);
-                    rem_counts[best_char]--;
-                    for (int c = 0; c < 26; ++c) {
-                        while (rem_counts[c] > 0) {
-                            Q += (char)('a' + c);
-                            rem_counts[c]--;
-                        }
-                    }
-                    string P = Q + mid;
-                    string revQ = Q;
-                    reverse(revQ.begin(), revQ.end());
-                    P += revQ;
-                    candidate2 = P;
-                    break; 
-                }
-            }
-        }
-
-        if (best_p == "") return candidate2;
-        if (candidate2 == "") return best_p;
-        return best_p < candidate2 ? best_p : candidate2;
+        return bestPalindrome;
     }
 };
 # @lc code=end
