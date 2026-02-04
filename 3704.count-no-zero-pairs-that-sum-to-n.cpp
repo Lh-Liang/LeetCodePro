ae8a23b2b44f6274192c01dd697c11f0810157ce
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 20:24:25 +0800

    Batch add 1 solutions: 3704.count-no-zero-p...

--- 3704.count-no-zero-pairs-that-sum-to-n.cpp
+++ 3704.count-no-zero-pairs-that-sum-to-n.cpp
@@ -3,24 +3,22 @@
 #
 # [3704] Count No-Zero Pairs That Sum to N
 #
+
 # @lc code=start
 class Solution {
 public:
-    bool isNoZeroInteger(long long x) {
-        while (x > 0) {
-            if (x % 10 == 0) return false;
-            x /= 10;
-        }
-        return true;
-    }
-    
     long long countNoZeroPairs(long long n) {
-        int count = 0;
-        for (long long a = 1; a <= n / 2; ++a) {
-            long long b = n - a;
-            if (isNoZeroInteger(a) && isNoZeroInteger(b)) {
-                ++count;
+        auto isNoZero = [](long long num) {
+            while (num > 0) {
+                if (num % 10 == 0) return false;
+                num /= 10;
             }
+            return true;
+        };
+        int count = 0;
+        for (long long a = 1; a < n; ++a) {
+            long long b = n - a;
+            if (isNoZero(a) && isNoZero(b)) ++count;
         }
         return count;
     }
