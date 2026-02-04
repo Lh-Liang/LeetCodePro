624fde674125ecb21bfa90c648dc3f6aec286dd8
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 03:09:50 +0800

    Batch add 1 solutions: 1367.linked-list-in-...

--- 1367.linked-list-in-binary-tree.cpp
+++ 1367.linked-list-in-binary-tree.cpp
@@ -1,10 +1,42 @@
-bool isSubPath(ListNode* head, TreeNode* root) {
-    if (!head) return true;
-    if (!root) return false;
-    return dfs(head, root) || isSubPath(head, root->left) || isSubPath(head, root->right);
-}
-bool dfs(ListNode* head, TreeNode* node) {
-    if (!head) return true;
-    if (!node) return false;
-    return (head->val == node->val) && (dfs(head->next, node->left) || dfs(head->next, node->right));
-}
+#
+# @lc app=leetcode id=1367 lang=cpp
+#
+# [1367] Linked List in Binary Tree
+#
+# @lc code=start
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode() : val(0), next(nullptr) {}
+*     ListNode(int x) : val(x), next(nullptr) {}
+*     ListNode(int x, ListNode *next) : val(x), next(next) {}
+* };
+*/
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+*     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+*     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+* };
+*/
+class Solution {
+public:
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if (!root) return false;
+        // Try to match from this node, or from left or right subtree
+        return dfs(head, root) || isSubPath(head, root->left) || isSubPath(head, root->right);
+    }
+    bool dfs(ListNode* head, TreeNode* node) {
+        if (!head) return true;
+        if (!node) return false;
+        if (node->val != head->val) return false;
+        return dfs(head->next, node->left) || dfs(head->next, node->right);
+    }
+};
+# @lc code=end
