22a0cfd4f1dfd035539ddda1a01e728fb463f9f3
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 14:25:07 +0800

    Batch add 1 solutions: 3503.longest-palindr...

--- 3503.longest-palindrome-after-substring-concatenation-i.cpp
+++ 3503.longest-palindrome-after-substring-concatenation-i.cpp
@@ -5,56 +5,36 @@
 #
 
 # @lc code=start
-#include <string>
-#include <algorithm>
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     int longestPalindrome(string s, string t) {
-        int n = s.length();
-        int m = t.length();
-        int max_len = 0;
-
+        int maxLength = 0;
+        
+        // Helper lambda to check if a string is palindrome
         auto isPalindrome = [](const string& str) {
-            if (str.empty()) return false;
-            int left = 0, right = str.length() - 1;
+            int left = 0, right = str.size() - 1;
             while (left < right) {
-                if (str[left++] != str[right--]) return false;
+                if (str[left] != str[right]) return false;
+                ++left;
+                --right;
             }
             return true;
         };
-
-        // Pre-extract all substrings of s including empty
-        vector<string> subs_s;
-        for (int i = 0; i <= n; ++i) {
-            for (int len = 0; i + len <= n; ++len) {
-                subs_s.push_back(s.substr(i, len));
-            }
-        }
-
-        // Pre-extract all substrings of t including empty
-        vector<string> subs_t;
-        for (int i = 0; i <= m; ++i) {
-            for (int len = 0; i + len <= m; ++len) {
-                subs_t.push_back(t.substr(i, len));
-            }
-        }
-
-        for (const string& sub1 : subs_s) {
-            for (const string& sub2 : subs_t) {
-                int current_len = sub1.length() + sub2.length();
-                if (current_len > max_len) {
-                    if (isPalindrome(sub1 + sub2)) {
-                        max_len = current_len;
-                    }
+        
+        // Check all combinations of prefixes and suffixes for both strings
+        for (int i = 0; i <= s.size(); ++i) {
+            for (int j = 0; j <= t.size(); ++j) {
+                string concat1 = s.substr(0, i) + t.substr(j);
+                string concat2 = t.substr(0, j) + s.substr(i);
+                if (isPalindrome(concat1)) {
+                    maxLength = max(maxLength, static_cast<int>(concat1.size()));
+                }
+                if (isPalindrome(concat2)) {
+                    maxLength = max(maxLength, static_cast<int>(concat2.size()));
                 }
             }
         }
-
-        return max_len;
-    }
-};
+        return maxLength;
+    }									
+};								
 # @lc code=end
