d3687f56af35293c706923837e90780f98f67eec
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 15:31:43 +0800

    Batch add 1 solutions: 3598.longest-common-...

--- 3598.longest-common-prefix-between-adjacent-strings-after-removals.cpp
+++ 3598.longest-common-prefix-between-adjacent-strings-after-removals.cpp
@@ -1,57 +1,34 @@
-#include <vector>
-#include <string>
-#include <algorithm>
+#
+# @lc app=leetcode id=3598 lang=cpp
+#
+# [3598] Longest Common Prefix Between Adjacent Strings After Removals
+#
 
-using namespace std;
-
+# @lc code=start
 class Solution {
 public:
-    int getLCP(const string& s1, const string& s2) {
-        int len = min((int)s1.length(), (int)s2.length());
-        for (int i = 0; i < len; ++i) {
-            if (s1[i] != s2[i]) return i;
-        }
-        return len;
-    }
-
     vector<int> longestCommonPrefix(vector<string>& words) {
         int n = words.size();
-        vector<int> ans(n, 0);
-        if (n <= 1) return ans;
-
-        vector<int> adjLCP(n - 1);
-        for (int i = 0; i < n - 1; ++i) {
-            adjLCP[i] = getLCP(words[i], words[i + 1]);
-        }
-
-        vector<int> preMax(n, 0);
-        preMax[0] = adjLCP[0];
-        for (int i = 1; i < n - 1; ++i) {
-            preMax[i] = max(preMax[i - 1], adjLCP[i]);
-        }
-
-        vector<int> sufMax(n, 0);
-        sufMax[n - 2] = adjLCP[n - 2];
-        for (int i = n - 3; i >= 0; --i) {
-            sufMax[i] = max(sufMax[i + 1], adjLCP[i]);
-        }
-
+        vector<int> result(n);
+        auto lcp = [](const string& a, const string& b) {
+            int len = min(a.size(), b.size());
+            for (int i = 0; i < len; ++i) {
+                if (a[i] != b[i]) return i;
+            }
+            return len;
+        };
         for (int i = 0; i < n; ++i) {
-            int currentMax = 0;
-            // Max from pairs to the left of i: adjLCP[0...i-2]
-            if (i >= 2) {
-                currentMax = max(currentMax, preMax[i - 2]);
+            vector<string> modifiedWords;
+            for (int j = 0; j < n; ++j) {
+                if (j != i) modifiedWords.push_back(words[j]);
             }
-            // Max from pairs to the right of i: adjLCP[i+1...n-2]
-            if (i + 1 <= n - 2) {
-                currentMax = max(currentMax, sufMax[i + 1]);
+            int maxLCP = 0;
+            for (int j = 0; j < modifiedWords.size() - 1; ++j) {
+                maxLCP = max(maxLCP, lcp(modifiedWords[j], modifiedWords[j + 1]));
             }
-            // Max from the bridge pair (i-1, i+1)
-            if (i > 0 && i < n - 1) {
-                currentMax = max(currentMax, getLCP(words[i - 1], words[i + 1]));
-            }
-            ans[i] = currentMax;
+            result[i] = maxLCP;
         }
-        return ans;
+        return result;
     }
 };
+# @lc code=end
