b945792df3a238b264db7ea9ede896fcb7f1344a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 04:27:33 +0800

    Batch add 1 solutions: 3503.longest-palindr...

--- 3503.longest-palindrome-after-substring-concatenation-i.cpp
+++ 3503.longest-palindrome-after-substring-concatenation-i.cpp
@@ -3,25 +3,41 @@
 #
 # [3503] Longest Palindrome After Substring Concatenation I
 #
-
 # @lc code=start
 class Solution {
 public:
     int longestPalindrome(string s, string t) {
-        int maxLength = 0;
-        auto isPalindrome = [](const string& str) {
-            return str == string(str.rbegin(), str.rend());
+        int n = s.size(), m = t.size();
+        int max_len = 0;
+        // Helper: check if a string is palindrome
+        auto is_palindrome = [](const string &str) {
+            int l = 0, r = str.size() - 1;
+            while (l < r) {
+                if (str[l] != str[r]) return false;
+                ++l; --r;
+            }
+            return true;
         };
-        
-        for (int i = 0; i <= s.size(); ++i) {
-            for (int j = 0; j <= t.size(); ++j) {
-                string combined = s.substr(0, i) + t.substr(0, j);
-                if (isPalindrome(combined)) {
-                    maxLength = max(maxLength, static_cast<int>(combined.size()));
+        // Consider all possible substrings of s
+        for (int i = 0; i <= n; ++i) {
+            for (int j = i; j <= n; ++j) {
+                string sub_s = s.substr(i, j - i);
+                // Consider all possible substrings of t
+                for (int k = 0; k <= m; ++k) {
+                    for (int l = k; l <= m; ++l) {
+                        string sub_t = t.substr(k, l - k);
+                        // Exclude the case where both substrings are empty
+                        if (sub_s.empty() && sub_t.empty()) continue;
+                        string candidate = sub_s + sub_t;
+                        if (is_palindrome(candidate)) {
+                            int len = candidate.size();
+                            if (len > max_len) max_len = len;
+                        }
+                    }
                 }
             }
         }
-        return maxLength;
+        return max_len;
     }
 };
 # @lc code=end
