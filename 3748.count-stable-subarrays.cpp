dfb3986cf49d37cd357aa33064a62060fc8ee66a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 05:52:28 +0800

    Batch add 1 solutions: 3748.count-stable-su...

--- 3748.count-stable-subarrays.cpp
+++ 3748.count-stable-subarrays.cpp
@@ -3,25 +3,37 @@
 #
 # [3748] Count Stable Subarrays
 #
-
 # @lc code=start
 class Solution {
 public:
     vector<long long> countStableSubarrays(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<long long> ans(queries.size(), 0);
-        for (int q = 0; q < queries.size(); ++q) {
-            int l = queries[q][0], r = queries[q][1];
-            long long count = 0;
-            int start = l;
-            for (int end = l; end <= r; ++end) {
-                while (start < end && nums[start] > nums[end]) {
-                    ++start;
-                }
-                count += end - start + 1;
+        int n = nums.size();
+        vector<int> seg_start(n), seg_end(n);
+        int l = 0;
+        while (l < n) {
+            int r = l;
+            while (r + 1 < n && nums[r] <= nums[r + 1]) ++r;
+            for (int k = l; k <= r; ++k) {
+                seg_start[k] = l;
+                seg_end[k] = r;
             }
-            ans[q] = count;
+            l = r + 1;
         }
-        return ans;
+        vector<long long> result;
+        for (auto& q : queries) {
+            int left = q[0], right = q[1];
+            long long ans = 0;
+            int i = left;
+            while (i <= right) {
+                int s = max(i, seg_start[i]);
+                int e = min(seg_end[i], right);
+                long long len = e - s + 1;
+                ans += len * (len + 1) / 2;
+                i = e + 1;
+            }
+            result.push_back(ans);
+        }
+        return result;
     }
 };
 # @lc code=end
