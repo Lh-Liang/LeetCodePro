47de802715c4af76d5badf9556f1b4e3b07c63e4
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 04:06:34 +0800

    Batch add 1 solutions: 3435.frequencies-of-...

--- 3435.frequencies-of-shortest-supersequences.cpp
+++ 3435.frequencies-of-shortest-supersequences.cpp
@@ -1,1 +1,24 @@
-Completed solution for Frequencies of Shortest Supersequences with validated code.
+#
+# @lc app=leetcode id=3435 lang=cpp
+#
+# [3435] Frequencies of Shortest Supersequences
+#
+
+# @lc code=start
+class Solution {
+public:
+    vector<vector<int>> supersequences(vector<string>& words) {
+        // There are at most 16 unique letters and all words are length 2
+        // For each SCS, any permutation with same letter counts is considered equivalent
+        // So, output only one frequency array: sum all letters' frequencies
+        vector<int> freq(26, 0);
+        for (const auto& word : words) {
+            for (char ch : word) {
+                freq[ch - 'a']++;
+            }
+        }
+        // Only one equivalence class
+        return {freq};
+    }
+};
+# @lc code=end
