c8bda8de6bb88ba6b8cf36a293382bc5a757bfef
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 20:48:01 +0800

    Batch add 1 solutions: 3768.minimum-inversi...

--- 3768.minimum-inversion-count-in-subarrays-of-fixed-length.cpp
+++ 3768.minimum-inversion-count-in-subarrays-of-fixed-length.cpp
@@ -1,71 +1,35 @@
 # @lc app=leetcode id=3768 lang=cpp
+#
 # [3768] Minimum Inversion Count in Subarrays of Fixed Length
-
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-class FenwickTree {
-public:
-    vector<int> tree;
-    int size;
-
-    FenwickTree(int n) : size(n) {
-        tree.resize(n + 1, 0);
-    }
-
-    void update(int index, int delta) {
-        for (; index <= size; index += index & -index) {
-            tree[index] += delta;
-        }
-    }
-
-    int query(int index) const {
-        int sum = 0;
-        for (; index > 0; index -= index & -index) {
-            sum += tree[index];
-        }
-        return sum;
-    }
-};
-
+#
+# @lc code=start
 class Solution {
 public:
     long long minInversionCount(vector<int>& nums, int k) {
         int n = nums.size();
-        long long min_inversions = LLONG_MAX;
-
-        // Coordinate compression to fit within Fenwick Tree range
-        vector<int> sorted_nums(nums.begin(), nums.end());
-        sort(sorted_nums.begin(), sorted_nums.end());
-        for (int& num : nums) {
-            num = lower_bound(sorted_nums.begin(), sorted_nums.end(), num) - sorted_nums.begin() + 1;
-        }
-
-        // Use Fenwick Tree for inversion counting within window size k
-        FenwickTree fenwick_tree(n);
-
-        // Initial calculation for first window
-        long long current_inversions = 0;
+        long long min_inv_count = LLONG_MAX;
+        vector<int> bit(n + 1, 0);
+        auto update = [&](int idx, int val) {
+            while (idx <= n) {
+                bit[idx] += val;
+                idx += idx & -idx;
+            }
+        };
+        auto query = [&](int idx) {
+            int sum = 0;
+            while (idx > 0) {
+                sum += bit[idx];
+                idx -= idx & -idx;
+            }
+            return sum;
+        };
         for (int i = 0; i < k; ++i) {
-            current_inversions += i - fenwick_tree.query(nums[i]);
-            fenwick_tree.update(nums[i], 1);
+            update(nums[i], 1);
         }
-        min_inversions = min(min_inversions, current_inversions);
-
-        // Slide window across entire array and update inversions accordingly
+        long long current_inv_count = 0;
+        for (int i = 0; i < k; ++i) {
+            current_inv_count += query(n) - query(nums[i]);
+        }
+        min_inv_count = min(min_inv_count, current_inv_count);
         for (int i = k; i < n; ++i) {
-            // Remove influence of element going out of window from fenwick tree & inversions count
-            fenwick_tree.update(nums[i - k], -1);
-            current_inversions -= fenwick_tree.query(nums[i - k] - 1);
-
-            // Add influence of new element into window
-            current_inversions += (k - 1) - fenwick_tree.query(nums[i]);
-            fenwick_tree.update(nums[i], 1);
-
-            min_inversions = min(min_inversions, current_inversions);
-        }
-
-         return min_inversions;
-     }
-};
+            update(nums[i - k], -1); // Remove element going out of window. " update(nums[i], 1); // Add new element into window. " // Calculate inversions with new element added into the window. " current_inv_count -= query(nums[i - k] - 1); " current_inv_count += query(n) - query(nums[i]); " min_inv_count = min(min_inv_count, current_inv_count); " } " return min_inv_count; " } " }; " # @lc code=end
