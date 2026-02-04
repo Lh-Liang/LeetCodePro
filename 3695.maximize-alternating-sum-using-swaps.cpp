662fb6678f562d59c944b6fac3e80cb6d9f39ba4
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 20:22:38 +0800

    Batch add 1 solutions: 3695.maximize-altern...

--- 3695.maximize-alternating-sum-using-swaps.cpp
+++ 3695.maximize-alternating-sum-using-swaps.cpp
@@ -3,60 +3,31 @@
 #
 # [3695] Maximize Alternating Sum Using Swaps
 #
-# @lc code=start
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
-using namespace std;
 
+# @lc code=start
 class Solution {
 public:
-    // Union-Find structure with path compression and union by rank
-    class UnionFind {
-    private:
-        vector<int> parent, rank;
-    public:
-        UnionFind(int size) : parent(size), rank(size, 0) {
-            for (int i = 0; i < size; ++i) {
-                parent[i] = i;
-            }
-        }
-        int find(int x) {
-            if (parent[x] != x) {
-                parent[x] = find(parent[x]); // Path compression
-            }
-            return parent[x];
-        }
-        void unionSets(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX != rootY) {
-                if (rank[rootX] > rank[rootY]) {
-                    parent[rootY] = rootX;
-                } else if (rank[rootX] < rank[rootY]) {
-                    parent[rootX] = rootY;
-                } else {
-                    parent[rootY] = rootX;
-                    rank[rootX]++;
-                }
-            }
-        }
-    };
-
     long long maxAlternatingSum(vector<int>& nums, vector<vector<int>>& swaps) {
         int n = nums.size();
-        UnionFind uf(n);
+        vector<int> parent(n);
+        iota(parent.begin(), parent.end(), 0);
+        function<int(int)> find = [&](int x) { return parent[x] == x ? x : parent[x] = find(parent[x]); };
         for (const auto& swap : swaps) {
-            uf.unionSets(swap[0], swap[1]);
+            int u = find(swap[0]), v = find(swap[1]);
+            if (u != v) parent[u] = v;
         }
-
-        unordered_map<int, vector<int>> components;
+        unordered_map<int, vector<int>> groups;
         for (int i = 0; i < n; ++i) {
-            components[uf.find(i)].push_back(nums[i]);
+            groups[find(i)].push_back(nums[i]);
         }
-
+        for (auto& [_, group] : groups) {
+            sort(group.rbegin(), group.rend()); // Sort in descending order for optimal placement.
+        }
+        vector<int> result(n);
+        unordered_map<int, int> indices;
+        for (int i = 0; i < n; ++i) {
+            int root = find(i);
+            result[i] = groups[root][indices[root]++];
+        }
         long long maxSum = 0;
-nint index = 0;
-nfor (auto& [root, componentNums] : components) {\nnsort(componentNums.begin(), componentNums.end(), greater<int>());\nnfor (int num : componentNums) {\nnmaxSum += (index % 2 == 0 ? num : -num);\nnindex++;\nn}\nn}\nnreturn maxSum;n}
-n};
-n# @lc code=end
+        for (int i = 0; i < n; ++i) { "maxSum += (i % 2 == 0 ? result[i] : -result[i]); "}" return maxSum; "}"}; "# @lc code=end "}
