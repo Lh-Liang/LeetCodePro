b37379d1c52f107aa74c255126204ba996b25fcb
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-04 13:07:43 +0800

    Batch add 1 solutions: 2289.steps-to-make-a...

--- 2289.steps-to-make-array-non-decreasing.cpp
+++ 2289.steps-to-make-array-non-decreasing.cpp
@@ -3,45 +3,21 @@
 #
 # [2289] Steps to Make Array Non-decreasing
 #
-
 # @lc code=start
-#include <vector>
-#include <stack>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     int totalSteps(vector<int>& nums) {
         int n = nums.size();
-        int max_total_steps = 0;
-        // Stack stores pairs of {value, steps_to_be_removed}
-        stack<pair<int, int>> s;
-        
-        for (int x : nums) {
-            int cur_steps = 0;
-            // While current element is larger or equal, it 'outlasts' the elements on the stack.
-            // We need to find the element to the left that is strictly greater than x.
-            while (!s.empty() && s.top().first <= x) {
-                cur_steps = max(cur_steps, s.top().second);
+        vector<int> steps(n, 0);
+        stack<int> s;
+        for (int i = n - 1; i >= 0; --i) {
+            while (!s.empty() && nums[i] > nums[s.top()]) {
+                steps[i] = max(steps[i] + 1, steps[s.top()]);
                 s.pop();
             }
-            
-            int steps_for_x = 0;
-            if (!s.empty()) {
-                // x will be removed 1 step after the elements between it and its remover are gone.
-                steps_for_x = cur_steps + 1;
-            } else {
-                // No larger element to the left, x stays forever.
-                steps_for_x = 0;
-            }
-            
-            max_total_steps = max(max_total_steps, steps_for_x);
-            s.push({x, steps_for_x});
+            s.push(i);
         }
-        
-        return max_total_steps;
+        return *max_element(steps.begin(), steps.end());
     }
 };
 # @lc code=end
