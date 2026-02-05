742529b75a707dca2dbe4abae5aa584c6d395d2d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 14:14:34 +0800

    Batch add 1 solutions: 3530.maximum-profit-...

--- 3530.maximum-profit-from-valid-topological-order-in-dag.cpp
+++ 3530.maximum-profit-from-valid-topological-order-in-dag.cpp
@@ -3,41 +3,31 @@
 #
 # [3530] Maximum Profit from Valid Topological Order in DAG
 #
-
 # @lc code=start
 class Solution {
 public:
     int maxProfit(int n, vector<vector<int>>& edges, vector<int>& score) {
-        vector<int> indeg(n, 0);
-        vector<vector<int>> adj(n);
+        vector<int> dep(n, 0);
+        // Precompute dependencies as bitmask for each node
         for (auto& e : edges) {
-            adj[e[0]].push_back(e[1]);
-            indeg[e[1]]++;
-        }
-        // Precompute prerequisites mask for each node
-        vector<int> pre(n, 0);
-        for (int u = 0; u < n; ++u) {
-            for (int v : adj[u]) {
-                pre[v] |= (1 << u);
-            }
+            dep[e[1]] |= (1 << e[0]);
         }
         int N = 1 << n;
-        vector<long long> dp(N, -1);
+        vector<int> dp(N, -1);
         dp[0] = 0;
         for (int mask = 0; mask < N; ++mask) {
-            int pos = __builtin_popcount(mask) + 1;
-            for (int node = 0; node < n; ++node) {
-                // Only add node if all its dependencies are already placed
-                if ((mask & (1 << node)) == 0 && (pre[node] & mask) == pre[node]) {
-                    int new_mask = mask | (1 << node);
-                    if (dp[new_mask] < dp[mask] + 1LL * score[node] * pos) {
-                        dp[new_mask] = dp[mask] + 1LL * score[node] * pos;
+            int cnt = __builtin_popcount(mask); // number of nodes placed so far
+            for (int i = 0; i < n; ++i) {
+                if (!(mask & (1 << i)) && (dep[i] & mask) == dep[i]) {
+                    int next_mask = mask | (1 << i);
+                    int profit = dp[mask] + score[i] * (cnt + 1);
+                    if (dp[next_mask] < profit) {
+                        dp[next_mask] = profit;
                     }
                 }
             }
         }
-        // Final verification: ensure all nodes are placed and dependencies are respected
-        return (int)dp[N - 1];
+        return dp[N - 1];
     }
 };
 # @lc code=end
