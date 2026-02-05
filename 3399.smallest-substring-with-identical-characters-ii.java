32661a4798392f88a6945a924226ee9ab81d5cd4
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 03:28:42 +0800

    Batch add 1 solutions: 3399.smallest-substr...

--- 3399.smallest-substring-with-identical-characters-ii.java
+++ 3399.smallest-substring-with-identical-characters-ii.java
@@ -1,31 +1,38 @@
-#
-# @lc app=leetcode id=3399 lang=java
-#
-# [3399] Smallest Substring With Identical Characters II
-#
-
-# @lc code=start
+//
+// @lc app=leetcode id=3399 lang=java
+//
+// [3399] Smallest Substring With Identical Characters II
+//
+// @lc code=start
 class Solution {
     public int minLength(String s, int numOps) {
         int n = s.length();
-        int maxLen = n;
-        for (char target : new char[]{'0', '1'}) {
-            int left = 0, right = 0, flipCount = 0;
-            while (right < n) {
-                if (s.charAt(right) != target) {
-                    flipCount++;
-                }
-                while (flipCount > numOps) {
-                    if (s.charAt(left) != target) {
-                        flipCount--;
-                    }
-                    left++;
-                }
-                maxLen = Math.min(maxLen, right - left + 1);
-                right++;
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isPossible(s, numOps, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return maxLen;
+        return left;
+    }
+    // Check if all runs can be broken into pieces of length <= len with numOps flips
+    private boolean isPossible(String s, int numOps, int len) {
+        int flips = 0;
+        int i = 0, n = s.length();
+        while (i < n) {
+            char ch = s.charAt(i);
+            int j = i;
+            while (j < n && s.charAt(j) == ch) j++;
+            int runLen = j - i;
+            if (runLen > len) {
+                flips += (runLen - 1) / len;
+            }
+            i = j;
+        }
+        return flips <= numOps;
     }
 }
-# @lc code=end
+// @lc code=end
