ba70f980ce45a7c2438d9469c81ba1ddb363b6e6
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 14:03:41 +0800

    Batch add 1 solutions: 3501.maximize-active...

--- 3501.maximize-active-section-with-trade-ii.cpp
+++ 3501.maximize-active-section-with-trade-ii.cpp
@@ -1,3 +1,4 @@
+#
 # @lc app=leetcode id=3501 lang=cpp
 #
 # [3501] Maximize Active Section with Trade II
@@ -6,58 +7,52 @@
 class Solution {
 public:
     vector<int> maxActiveSectionsAfterTrade(string s, vector<vector<int>>& queries) {
-        int n = s.size();
-        vector<int> answer;
+        vector<int> res;
         for (const auto& q : queries) {
             int l = q[0], r = q[1];
-            string t = "1" + s.substr(l, r - l + 1) + "1";
-            int m = t.size();
-            vector<pair<char, int>> blocks;
-            // Count blocks
-            int cnt = 1;
-            for (int i = 1; i < m; ++i) {
-                if (t[i] != t[i-1]) {
-                    blocks.push_back({t[i-1], cnt});
-                    cnt = 1;
-                } else {
-                    cnt++;
-                }
-            }
-            blocks.push_back({t[m-1], cnt});
-            // Initial number of '1' blocks (excluding first and last augmentation)
-            int init = 0;
-            for (int i = 0; i < (int)blocks.size(); ++i) {
-                if (blocks[i].first == '1') {
-                    // Exclude the first and last blocks (the augmentations)
-                    if (i != 0 && i != (int)blocks.size()-1)
-                        init++;
-                }
-            }
-            int maxActive = init;
-            // Try all possible trades
-            for (int i = 1; i+1 < (int)blocks.size(); ++i) {
-                // Must be a '1' block surrounded by '0's
-                if (blocks[i].first == '1' && blocks[i-1].first == '0' && blocks[i+1].first == '0') {
-                    // Simulate trade: remove this '1' block, add a '0' block
-                    // For the new string, after turning block i to '0', we can turn any '0' block (surrounded by '1's) to '1'
-                    // So, try all such '0' blocks
-                    for (int j = 1; j+1 < (int)blocks.size(); ++j) {
-                        if (blocks[j].first == '0' && blocks[j-1].first == '1' && blocks[j+1].first == '1') {
-                            if (j == i || (j == i-1 && blocks[j].first == '0' && blocks[i].first == '1')) continue; // Can't swap the same block just changed
-                            // Calculate new number of '1' blocks
-                            int cur = init;
-                            // Trading away block i removes 1 '1' block
-                            cur--;
-                            // Trading in block j adds 1 '1' block
-                            cur++;
-                            maxActive = max(maxActive, cur);
+            string t = '1' + s.substr(l, r - l + 1) + '1';
+            int n = t.size();
+            // Count original number of '1's
+            int orig = 0;
+            for (int i = 1; i < n - 1; ++i) orig += t[i] == '1';
+            int max_ones = orig;
+            // Find all blocks of '1's surrounded by '0's
+            for (int i = 1; i < n - 1;) {
+                // Eligible '1' block: t[i-1]=='0', t[i]=='1', ... t[j]=='1', t[j+1]=='0'
+                if (t[i-1] == '0' && t[i] == '1') {
+                    int j = i;
+                    while (j < n - 1 && t[j] == '1') ++j;
+                    if (t[j] == '0') {
+                        // Simulate converting [i,j-1] to '0's
+                        string t2 = t;
+                        for (int k = i; k < j; ++k) t2[k] = '0';
+                        // Now find all eligible '0' blocks surrounded by '1's
+                        for (int x = 1; x < n - 1;) {
+                            if (t2[x-1] == '1' && t2[x] == '0') {
+                                int y = x;
+                                while (y < n - 1 && t2[y] == '0') ++y;
+                                if (t2[y] == '1') {
+                                    // Simulate converting [x, y-1] to '1's
+                                    string t3 = t2;
+                                    for (int k = x; k < y; ++k) t3[k] = '1';
+                                    int count = 0;
+                                    for (int z = 1; z < n - 1; ++z) count += t3[z] == '1';
+                                    if (count > max_ones) max_ones = count;
+                                }
+                                x = y;
+                            } else {
+                                ++x;
+                            }
                         }
                     }
+                    i = j;
+                } else {
+                    ++i;
                 }
             }
-            answer.push_back(maxActive);
+            res.push_back(max_ones);
         }
-        return answer;
+        return res;
     }
 };
 # @lc code=end
