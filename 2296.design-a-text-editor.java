3eaf6ef62be61a0a5342240025b7774599e3bf53
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 21:27:13 +0800

    Batch add 1 solutions: 2296.design-a-text-e...

--- 2296.design-a-text-editor.java
+++ 2296.design-a-text-editor.java
@@ -1,1 +1,53 @@
-#\n# @lc app=leetcode id=2296 lang=java\n#\n# [2296] Design a Text Editor\n#\n\n# @lc code=start\nimport java.util.Stack;\nimport java.util.ListIterator;\n\nclass TextEditor {\n    private Stack<Character> leftStack;\n    private Stack<Character> rightStack;\n    \n    public TextEditor() {\n        this.leftStack = new Stack<>();\n        this.rightStack = new Stack<>();\n    }\n    \n    public void addText(String text) {\n        for (char c : text.toCharArray()) {\n            leftStack.push(c);\n        }\n    }\n    \n    public int deleteText(int k) {\n        int count = 0;\n        while (!leftStack.isEmpty() && count < k) {\n            leftStack.pop();\n            count++;\n        }\n        return count;\n    }\nbpublic String cursorLeft(int k) { \nbwhile (!leftStack.isEmpty() && k > 0) { \nbrightStac.kpush(leftStac.kpop()); \nbk--; \b} \breturn getLast10Chars(); \b} \nbpublic String cursorRight(int k) { \nbwhile (!rightStac.kisEmpty() && k > 0) { \nbleftStac.kpush(rightStac.kpop()); \nbk--; \b} \breturn getLast10Chars(); \b} \bprivate String getLast10Chars() { \nbint count = Math.min(10, leftStac.ksize()); \nbStringBuilder sb = new StringBuilder(); \nbListIterator<Character> iterator = leftStac.klistIterator(leftStac.ksize()); \nbwhile (count > 0 && iterator.hasPrevious()) { \nsb.insert(0, iterator.previous()); \nbcount--; \b} \breturn sb.toString(); } # @lc code=end
+#
+# @lc app=leetcode id=2296 lang=java
+#
+# [2296] Design a Text Editor
+#
+# @lc code=start
+import java.util.*;
+class TextEditor {
+    Deque<Character> left; // Text to the left of cursor
+    Deque<Character> right; // Text to the right of cursor
+    public TextEditor() {
+        left = new ArrayDeque<>();
+        right = new ArrayDeque<>();
+    }
+    public void addText(String text) {
+        for (char c : text.toCharArray()) {
+            left.offerLast(c);
+        }
+    }
+    public int deleteText(int k) {
+        int count = 0;
+        while (count < k && !left.isEmpty()) {
+            left.pollLast();
+            count++;
+        }
+        return count;
+    }
+    public String cursorLeft(int k) {
+        while (k > 0 && !left.isEmpty()) {
+            right.offerFirst(left.pollLast());
+            k--;
+        }
+        return getLeftText();
+    }
+    public String cursorRight(int k) {
+        while (k > 0 && !right.isEmpty()) {
+            left.offerLast(right.pollFirst());
+            k--;
+        }
+        return getLeftText();
+    }
+    private String getLeftText() {
+        StringBuilder sb = new StringBuilder();
+        Iterator<Character> iter = left.descendingIterator();
+        int count = 10; // We only need last 10 characters
+        while (count > 0 && iter.hasNext()) {
+            sb.append(iter.next());
+            count--;
+        }
+        return sb.reverse().toString(); // Reverse because we added in reverse order
+    }
+}
+deque "/return # @lc code=end
