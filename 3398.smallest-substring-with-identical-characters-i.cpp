e05635782a0eede101215fdd664ec9ce6d04c74b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 13:33:41 +0800

    Batch add 1 solutions: 3398.smallest-substr...

--- 3398.smallest-substring-with-identical-characters-i.cpp
+++ 3398.smallest-substring-with-identical-characters-i.cpp
@@ -10,33 +10,30 @@ public:
     int minLength(string s, int numOps) {
         int n = s.size();
         int left = 1, right = n;
-        auto check = [&](int l) {
-            int flips = 0;
-            int cnt = 1;
+        auto canAchieve = [&](int maxLen) {
+            int flips = 0, cnt = 1;
             for (int i = 1; i < n; ++i) {
                 if (s[i] == s[i-1]) {
-                    cnt++;
-                    if (cnt > l) {
-                        flips++;
-                        cnt = 1;
-                    }
+                    ++cnt;
                 } else {
+                    if (cnt > maxLen) flips += (cnt - 1) / maxLen;
                     cnt = 1;
                 }
             }
+            if (cnt > maxLen) flips += (cnt - 1) / maxLen;
             return flips <= numOps;
         };
-        int ans = n;
+        int res = n;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (check(mid)) {
-                ans = mid;
+            if (canAchieve(mid)) {
+                res = mid;
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return ans;
+        return res;
     }
 };
 # @lc code=end
