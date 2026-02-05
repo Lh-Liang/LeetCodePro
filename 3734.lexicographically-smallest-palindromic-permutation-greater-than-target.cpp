514f34b676fa3ffdbec6f4752d1e7fad7ddfae45
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 14:57:16 +0800

    Batch add 1 solutions: 3734.lexicographical...

--- 3734.lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
+++ 3734.lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -3,31 +3,32 @@
 #
 # [3734] Lexicographically Smallest Palindromic Permutation Greater Than Target
 #
+
 # @lc code=start
-#include <string>
-#include <vector>
-#include <algorithm>
-using namespace std;
 class Solution {
 public:
     string lexPalindromicPermutation(string s, string target) {
         int n = s.size();
-        vector<int> freq(26, 0);
-        for(char c : s) freq[c-'a']++;
+        vector<int> cnt(26, 0);
+        for (char c : s) cnt[c - 'a']++;
         int odd = 0, odd_char = -1;
-        for(int i=0; i<26; ++i) if(freq[i]%2) { odd++; odd_char = i; }
-        if(odd > 1) return "";
-        vector<char> half;
-        for(int i=0; i<26; ++i) for(int j=0; j<freq[i]/2; ++j) half.push_back('a'+i);
-        string mid = (odd == 1 ? string(1, 'a'+odd_char) : "");
-        string pal;
+        for (int i = 0; i < 26; ++i) {
+            if (cnt[i] % 2) {
+                odd++;
+                odd_char = i;
+            }
+        }
+        if (odd > 1) return "";
+        string half = "";
+        for (int i = 0; i < 26; ++i) half += string(cnt[i]/2, 'a' + i);
+        string middle = odd_char != -1 ? string(1, 'a' + odd_char) : "";
+        string smallest = half + middle + string(half.rbegin(), half.rend());
+        if (smallest > target) return smallest;
+        sort(half.begin(), half.end());
         do {
-            string first(half.begin(), half.end());
-            string second = first;
-            reverse(second.begin(), second.end());
-            pal = first + mid + second;
-            if(pal > target) return pal;
-        } while(next_permutation(half.begin(), half.end()));
+            string candidate = half + middle + string(half.rbegin(), half.rend());
+            if (candidate > target) return candidate;
+        } while (next_permutation(half.begin(), half.end()));
         return "";
     }
 };
