14889a7734d35f1978c6ac44e0f9a85f03cdb9e2
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 15:03:23 +0800

    Batch add 1 solutions: 3721.longest-balance...

--- 3721.longest-balanced-subarray-ii.cpp
+++ 3721.longest-balanced-subarray-ii.cpp
@@ -3,40 +3,46 @@
 #
 # [3721] Longest Balanced Subarray II
 #
+
 # @lc code=start
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
         int n = nums.size();
         unordered_map<int, int> even_count, odd_count;
-        int left = 0, res = 0;
-        for (int right = 0; right < n; ++right) {
-            int val = nums[right];
-            if (val % 2 == 0) ++even_count[val];
-            else ++odd_count[val];
-            while (left <= right && even_count.size() > odd_count.size()) {
-                int lval = nums[left];
-                if (lval % 2 == 0) {
-                    if (--even_count[lval] == 0) even_count.erase(lval);
-                } else {
-                    if (--odd_count[lval] == 0) odd_count.erase(lval);
-                }
-                ++left;
+        int left = 0, right = 0, res = 0;
+        int distinct_even = 0, distinct_odd = 0;
+        while (right < n) {
+            int num = nums[right];
+            if (num % 2 == 0) {
+                if (++even_count[num] == 1) distinct_even++;
+            } else {
+                if (++odd_count[num] == 1) distinct_odd++;
             }
-            while (left <= right && odd_count.size() > even_count.size()) {
-                int lval = nums[left];
-                if (lval % 2 == 0) {
-                    if (--even_count[lval] == 0) even_count.erase(lval);
+            while (left <= right && distinct_even > distinct_odd) {
+                int lnum = nums[left++];
+                if (lnum % 2 == 0) {
+                    if (--even_count[lnum] == 0) distinct_even--;
                 } else {
-                    if (--odd_count[lval] == 0) odd_count.erase(lval);
+                    if (--odd_count[lnum] == 0) distinct_odd--;
                 }
-                ++left;
             }
-            if (even_count.size() == odd_count.size())
+            while (left <= right && distinct_odd > distinct_even) {
+                int lnum = nums[left++];
+                if (lnum % 2 == 0) {
+                    if (--even_count[lnum] == 0) distinct_even--;
+                } else {
+                    if (--odd_count[lnum] == 0) distinct_odd--;
+                }
+            }
+            if (distinct_even == distinct_odd) {
                 res = max(res, right - left + 1);
+            }
+            right++;
         }
         return res;
     }
