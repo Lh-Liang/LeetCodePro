e641cb3e18aeba44b9ae334710207fc26b2ddaf7
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 06:08:33 +0800

    Batch add 1 solutions: 3757.number-of-effec...

--- 3757.number-of-effective-subsequences.java
+++ 3757.number-of-effective-subsequences.java
@@ -5,33 +5,48 @@
 #
 
 # @lc code=start
-import java.util.*;
 class Solution {
     public int countEffective(int[] nums) {
         int n = nums.length;
-        final int MOD = (int)(1e9 + 7);
-        int totalOR = 0;
-        for (int num : nums) {
-            totalOR |= num;
+        final int MOD = 1_000_000_007;
+        int totalOr = 0;
+        for (int x : nums) totalOr |= x;
+
+        // Find all set bits in totalOr
+        List<Integer> bits = new ArrayList<>();
+        for (int b = 0; b < 22; ++b)
+            if (((totalOr >> b) & 1) == 1)
+                bits.add(b);
+        int k = bits.size();
+        int[][] bitContrib = new int[k][];
+        // For each bit, collect indices of contributors
+        for (int i = 0; i < k; ++i) {
+            int b = bits.get(i);
+            List<Integer> indices = new ArrayList<>();
+            for (int j = 0; j < n; ++j) if (((nums[j] >> b) & 1) == 1) indices.add(j);
+            bitContrib[i] = indices.stream().mapToInt(e -> e).toArray();
         }
-        
-        long effectiveCount = 0;
-        for (int i = 0; i < n; i++) {
-            if ((totalOR & ~nums[i]) != totalOR) { // Check if removing nums[i] affects the OR result
-                effectiveCount += Math.pow(2, countBits(nums[i]));
-                effectiveCount %= MOD;
+        int[] pow2 = new int[n+1];
+        pow2[0] = 1;
+        for (int i = 1; i <= n; ++i) pow2[i] = (int)((pow2[i-1]*2L)%MOD);
+        int res = 0;
+        // Inclusion-exclusion over non-empty bit subsets
+        for (int mask = 1; mask < (1<<k); ++mask) {
+            Set<Integer> toRemove = new HashSet<>();
+            for (int b = 0; b < k; ++b) {
+                if (((mask>>b)&1)==1) {
+                    for (int idx : bitContrib[b]) toRemove.add(idx);
+                }
             }
+            int m = toRemove.size();
+            int ways = (int)((pow2[m]-1L+MOD)%MOD * (long)pow2[n-m]%MOD);
+            // Inclusion-Exclusion: add if odd number of bits, subtract if even
+            if (Integer.bitCount(mask)%2==1)
+                res = (res + ways) % MOD;
+            else
+                res = (res - ways + MOD) % MOD;
         }
-        return (int)effectiveCount;
-    }
-    
-    private int countBits(int num) {
-        int count = 0;
-        while (num > 0) {
-            count += num & 1;
-            num >>= 1;
-        }
-        return count;
+        return res;
     }
 }
 # @lc code=end
