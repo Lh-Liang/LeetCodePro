6240d12d1bc1994d86b541d92bc62029b7b61227
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 05:00:08 +0800

    Batch add 1 solutions: 3594.minimum-time-to...

--- 3594.minimum-time-to-transport-all-individuals.java
+++ 3594.minimum-time-to-transport-all-individuals.java
@@ -1,56 +1,73 @@
-# \@lc code=start
 class Solution {
-    private double[][][] memo;
-    private int n, k, m;
-    private int[] time;
-    private double[] mul;
-
     public double minTime(int n, int k, int m, int[] time, double[] mul) {
-        this.n = n;
-        this.k = k;
-        this.m = m;
-        this.time = time;
-        this.mul = mul;
-        this.memo = new double[1 << n][1 << n][m];
-        for (double[][] side : memo) {
-            for (double[] stage : side) {
-                Arrays.fill(stage, -1);
-            }
+        Map<Long, Double> memo = new HashMap<>();
+        int FULL = (1 << n) - 1;
+        double res = dfs(FULL, 1, 0, n, k, m, time, mul, memo);
+        if (res == Double.MAX_VALUE || Double.isInfinite(res) || Double.isNaN(res)) {
+            return -1.0;
         }
-        // Start DFS from initial state with all individuals on base side
-        return dfs((1 << n) - 1, 0, 0);
+        return res;
     }
-
-    private double dfs(int baseSideMask, int destSideMask, int currentStage) {
-        if (baseSideMask == 0) return 0; // All transported
-        if (memo[baseSideMask][destSideMask][currentStage] != -1)
-            return memo[baseSideMask][destSideMask][currentStage];
-
-        double minTime = Double.MAX_VALUE;
-        // Try sending up to 'k' individuals from base side to destination
-        for (int subset = baseSideMask; subset > 0; subset = (subset - 1) & baseSideMask) {
-            if (Integer.bitCount(subset) <= k) { // Check boat capacity
-                double maxTime = getMaxTime(subset);
-                double crossingTime = maxTime * mul[currentStage];
-                int nextStage = (currentStage + ((int)Math.floor(crossingTime)) % m) % m;
-                minTime = Math.min(minTime,
-dfs(baseSideMask ^ subset, destSideMask | subset, nextStage) + crossingTime);
+    private double dfs(int baseMask, int boatAtBase, int stage, int n, int k, int m, int[] time, double[] mul, Map<Long, Double> memo) {
+        if (baseMask == 0 && boatAtBase == 0) {
+            return 0.0;
+        }
+        long key = (((long) baseMask) << 6) | (boatAtBase << 3) | stage;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        double ans = Double.MAX_VALUE;
+        if (boatAtBase == 1) {
+            List<Integer> basePeople = new ArrayList<>();
+            for (int i = 0; i < n; ++i) {
+                if (((baseMask >> i) & 1) == 1) {
+                    basePeople.add(i);
+                }
+            }
+            int sz = basePeople.size();
+            for (int mask = 1; mask < (1 << sz); ++mask) {
+                if (Integer.bitCount(mask) > k) continue;
+                int groupMax = 0;
+                int newBaseMask = baseMask;
+                for (int j = 0; j < sz; ++j) {
+                    if (((mask >> j) & 1) == 1) {
+                        groupMax = Math.max(groupMax, time[basePeople.get(j)]);
+                        newBaseMask ^= (1 << basePeople.get(j));
+                    }
+                }
+                double crossTime = groupMax * mul[stage];
+                int advance = (int) Math.floor(crossTime) % m;
+                int nextStage = (stage + advance) % m;
+                double recur = dfs(newBaseMask, 0, nextStage, n, k, m, time, mul, memo);
+                if (recur != Double.MAX_VALUE) {
+                    ans = Math.min(ans, crossTime + recur);
+                }
+            }
+        } else {
+            int destMask = ((1 << n) - 1) ^ baseMask;
+            List<Integer> destPeople = new ArrayList<>();
+            for (int i = 0; i < n; ++i) {
+                if (((destMask >> i) & 1) == 1) {
+                    destPeople.add(i);
+                }
+            }
+            // If there are still people at base but nobody at destination to return the boat, infeasible
+            if (baseMask != 0 && destPeople.isEmpty()) {
+                memo.put(key, Double.MAX_VALUE);
+                return Double.MAX_VALUE;
+            }
+            for (int idx : destPeople) {
+                double retTime = time[idx] * mul[stage];
+                int advance = (int) Math.floor(retTime) % m;
+                int nextStage = (stage + advance) % m;
+                int newBaseMask = baseMask | (1 << idx);
+                double recur = dfs(newBaseMask, 1, nextStage, n, k, m, time, mul, memo);
+                if (recur != Double.MAX_VALUE) {
+                    ans = Math.min(ans, retTime + recur);
+                }
             }
         }
-
-        // Consider returning an individual if needed
-        if ((baseSideMask | destSideMask) != ((1 << n) - 1)) { // Not all on base or dest
-            for (int r = 0; r < n; r++) {
-                if ((destSideMask & (1 << r)) != 0) { // If person 'r' is at destination
-                    double returnTime = time[r] * mul[currentStage];
-inextStage = (currentStage + ((int)Math.floor(returnTime)) % m) % m;
-timeOption = dfs(baseSideMask | (1 << r), destSideMask ^ (1 << r), nextStage)+ returnTime;
-inminTime = Math.min(minTime, timeOption);
-in}
-in}
-in}
-memo[baseSideMask][destSideMask][currentStage] = minTime != Double.MAX_VALUE ? minTime : -1;
-inreturn minTime;}private double getMaxTime(int mask){
-double maxT=0;or(inti=0;i<n;i++){if((mask&(1<<i))!=0){maxT=Math.max(maxT,time[i]);}
-in}
-inreturn maxT;}\@lc code=end
+        memo.put(key, ans);
+        return ans;
+    }
+}
