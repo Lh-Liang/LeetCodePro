d7ba229775272f2049ad80f3b00d6ff7165b125b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 13:42:47 +0800

    Batch add 1 solutions: 3435.frequencies-of-...

--- 3435.frequencies-of-shortest-supersequences.cpp
+++ 3435.frequencies-of-shortest-supersequences.cpp
@@ -8,17 +8,42 @@
 class Solution {
 public:
     vector<vector<int>> supersequences(vector<string>& words) {
-        // There are at most 16 unique letters and all words are length 2
-        // For each SCS, any permutation with same letter counts is considered equivalent
-        // So, output only one frequency array: sum all letters' frequencies
-        vector<int> freq(26, 0);
-        for (const auto& word : words) {
-            for (char ch : word) {
-                freq[ch - 'a']++;
+        // Step 1: Build all shortest common supersequences (SCS)
+        // For small input size and all words length 2, enumerate all orders
+        int n = words.size();
+        vector<int> idx(n, 0);
+        iota(idx.begin(), idx.end(), 0);
+        set<vector<int>> freq_set;
+        int min_len = INT_MAX;
+        do {
+            string scs = words[idx[0]];
+            for (int i = 1; i < n; ++i) {
+                // Merge scs and words[idx[i]]
+                string& w = words[idx[i]];
+                int p1 = 0, p2 = 0;
+                string merged;
+                int m = scs.size(), l = w.size();
+                int common = 0;
+                // Find maximal suffix of scs matching prefix of w
+                for (int k = min(m, l); k >= 0; --k) {
+                    if (scs.substr(m - k, k) == w.substr(0, k)) {
+                        common = k; break;
+                    }
+                }
+                scs += w.substr(common);
             }
-        }
-        // Only one equivalence class
-        return {freq};
+            if ((int)scs.size() < min_len) {
+                freq_set.clear();
+                min_len = scs.size();
+            }
+            if ((int)scs.size() == min_len) {
+                vector<int> freq(26, 0);
+                for (char c : scs) freq[c - 'a']++;
+                freq_set.insert(freq);
+            }
+        } while (next_permutation(idx.begin(), idx.end()));
+        vector<vector<int>> result(freq_set.begin(), freq_set.end());
+        return result;
     }
 };
 # @lc code=end
