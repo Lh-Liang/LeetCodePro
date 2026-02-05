addf2519476a8f31fce718a8db1d90b8eab218ff
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 14:44:44 +0800

    Batch add 1 solutions: 3670.maximum-product...

--- 3670.maximum-product-of-two-integers-with-no-common-bits.cpp
+++ 3670.maximum-product-of-two-integers-with-no-common-bits.cpp
@@ -7,32 +7,27 @@
 class Solution {
 public:
     long long maxProduct(vector<int>& nums) {
-        // Step 1: Map bitmask to the largest number with that mask
-        unordered_map<int, int> mask_max;
+        unordered_map<int, int> bitmask_max;
         for (int num : nums) {
-            int mask = num;
-            if (mask_max.count(mask)) {
-                mask_max[mask] = max(mask_max[mask], num);
-            } else {
-                mask_max[mask] = num;
-            }
+            int bm = num;
+            if (bitmask_max.count(bm))
+                bitmask_max[bm] = max(bitmask_max[bm], num);
+            else
+                bitmask_max[bm] = num;
         }
-        // Step 2: Collect unique (mask, max_value) pairs
-        vector<pair<int, int>> v;
-        for (auto& [mask, val] : mask_max) {
-            v.emplace_back(mask, val);
+        vector<pair<int, int>> masks;
+        for (auto& p : bitmask_max) {
+            masks.push_back({p.first, p.second});
         }
-        // Step 3: Compare all pairs for disjoint masks
         long long res = 0;
-        int n = v.size();
+        int n = masks.size();
         for (int i = 0; i < n; ++i) {
             for (int j = i + 1; j < n; ++j) {
-                if ((v[i].first & v[j].first) == 0) {
-                    res = max(res, 1LL * v[i].second * v[j].second);
+                if ((masks[i].first & masks[j].first) == 0) {
+                    res = max(res, 1LL * masks[i].second * masks[j].second);
                 }
             }
         }
-        // Step 4: Return the result
         return res;
     }
 };
