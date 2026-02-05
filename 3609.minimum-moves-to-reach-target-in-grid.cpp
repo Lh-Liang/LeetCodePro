59d515feed031cbd1f50397f1ac8425753ba84b6
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 14:37:15 +0800

    Batch add 1 solutions: 3609.minimum-moves-t...

--- 3609.minimum-moves-to-reach-target-in-grid.cpp
+++ 3609.minimum-moves-to-reach-target-in-grid.cpp
@@ -3,24 +3,26 @@
 #
 # [3609] Minimum Moves to Reach Target in Grid
 #
-
 # @lc code=start
 class Solution {
 public:
     int minMoves(int sx, int sy, int tx, int ty) {
         int moves = 0;
         while (tx > sx || ty > sy) {
-            if (tx == ty) {
-                // If coordinates are equal but not at start, impossible to progress
+            if (tx < sx || ty < sy) return -1;
+            if (tx >= ty && tx > sx) {
+                int m = std::max(tx, ty);
+                int jump = std::max(1, (tx - sx) / m);
+                tx -= jump * m;
+                moves += jump;
+            } else if (ty > tx && ty > sy) {
+                int m = std::max(tx, ty);
+                int jump = std::max(1, (ty - sy) / m);
+                ty -= jump * m;
+                moves += jump;
+            } else {
                 return -1;
-            } else if (tx > ty) {
-                if (ty < sy) return -1;
-                tx -= ty;
-            } else { // ty > tx
-                if (tx < sx) return -1;
-                ty -= tx;
             }
-            moves++;
         }
         return (tx == sx && ty == sy) ? moves : -1;
     }
