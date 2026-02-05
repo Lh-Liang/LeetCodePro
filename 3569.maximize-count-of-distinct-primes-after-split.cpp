40d65737e61da645732c5c50d6ef74da7e6b19ad
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 14:28:23 +0800

    Batch add 1 solutions: 3569.maximize-count-...

--- 3569.maximize-count-of-distinct-primes-after-split.cpp
+++ 3569.maximize-count-of-distinct-primes-after-split.cpp
@@ -1,49 +1,52 @@
-//
-// @lc app=leetcode id=3569 lang=cpp
-//
-// [3569] Maximize Count of Distinct Primes After Split
-//
-// @lc code=start
+#
+# @lc app=leetcode id=3569 lang=cpp
+#
+# [3569] Maximize Count of Distinct Primes After Split
+#
+
+# @lc code=start
 #include <vector>
 #include <unordered_set>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
     vector<int> maximumCount(vector<int>& nums, vector<vector<int>>& queries) {
-        const int MAXV = 100005;
-        vector<bool> is_prime(MAXV, true);
+        const int MAX_N = 100005;
+        vector<bool> is_prime(MAX_N, true);
         is_prime[0] = is_prime[1] = false;
-        for (int i = 2; i * i < MAXV; ++i) {
-            if (is_prime[i]) {
-                for (int j = i * i; j < MAXV; j += i)
+        for(int i = 2; i * i < MAX_N; ++i) {
+            if(is_prime[i])
+                for(int j = i * i; j < MAX_N; j += i)
                     is_prime[j] = false;
-            }
         }
         int n = nums.size();
         vector<int> res;
-        for (auto& q : queries) {
-            int idx = q[0], val = q[1];
-            nums[idx] = val;
-            unordered_set<int> prefix_set, suffix_set;
-            vector<int> prefix_count(n, 0), suffix_count(n, 0);
-            for (int i = 0; i < n; ++i) {
-                if (is_prime[nums[i]]) prefix_set.insert(nums[i]);
-                prefix_count[i] = prefix_set.size();
+        vector<int> arr = nums;
+        for(auto& q : queries) {
+            arr[q[0]] = q[1];
+            // Count distinct primes in prefix and suffix for each k
+            unordered_set<int> left, right;
+            vector<int> prefix(n), suffix(n);
+            for(int i=0; i<n; ++i) {
+                if(is_prime[arr[i]]) left.insert(arr[i]);
+                prefix[i] = left.size();
             }
-            for (int i = n - 1; i >= 0; --i) {
-                if (is_prime[nums[i]]) suffix_set.insert(nums[i]);
-                suffix_count[i] = suffix_set.size();
+            right.clear();
+            for(int i=n-1; i>=0; --i) {
+                if(is_prime[arr[i]]) right.insert(arr[i]);
+                suffix[i] = right.size();
             }
-            int max_sum = 0;
-            for (int k = 1; k < n; ++k) {
-                int left = prefix_count[k - 1];
-                int right = suffix_count[k];
-                max_sum = max(max_sum, left + right);
+            int mx = 0;
+            for(int k=1; k<n; ++k) {
+                int left_cnt = prefix[k-1];
+                int right_cnt = suffix[k];
+                mx = max(mx, left_cnt + right_cnt);
             }
-            res.push_back(max_sum);
+            res.push_back(mx);
         }
         return res;
     }
 };
-// @lc code=end
+# @lc code=end
