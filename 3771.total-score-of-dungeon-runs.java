a7f9c8945d853b0bbee58c9a12593bea4b56207d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 06:14:03 +0800

    Batch add 1 solutions: 3771.total-score-of-...

--- 3771.total-score-of-dungeon-runs.java
+++ 3771.total-score-of-dungeon-runs.java
@@ -5,27 +5,37 @@
 #
 
 # @lc code=start
+import java.util.*;
 class Solution {
     public long totalScore(int hp, int[] damage, int[] requirement) {
         int n = damage.length;
-        long totalScore = 0;
-        
-        // Iterate over each starting room
-        for (int start = 0; start < n; start++) {
-            int currentHp = hp;
-            int currentScore = 0;
-            
-            // Calculate score starting from room 'start'
-            for (int i = start; i < n; i++) {
-                currentHp -= damage[i];
-                if (currentHp >= requirement[i]) {
-                    currentScore++;
+        long[] prefix = new long[n + 1];
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + damage[i];
+        }
+        // prefix[0..n] is sorted, so we can binary search
+        long total = 0;
+        for (int i = 0; i < n; ++i) {
+            long threshold = hp - requirement[i] + prefix[i + 1];
+            // Find the rightmost j-1 <= i such that prefix[j-1] <= threshold
+            // i.e., count of j in [1, i+1] with prefix[j-1] <= threshold
+            int left = 0, right = i + 1;
+            int pos = -1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (prefix[mid] <= threshold) {
+                    pos = mid;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
                 }
             }
-            totalScore += currentScore;
+            // The number of valid j is pos in [0, i], i.e., pos - 0 + 1 = pos + 1
+            if (pos >= 0) {
+                total += Math.min(pos + 1, i + 1);
+            }
         }
-        
-        return totalScore;
+        return total;
     }
 }
 # @lc code=end
