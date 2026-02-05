0db27b12684a9a828eefed8ff6c5a3fbfb5a52b9
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 03:53:12 +0800

    Batch add 1 solutions: 3448.count-substring...

--- 3448.count-substrings-divisible-by-last-digit.java
+++ 3448.count-substrings-divisible-by-last-digit.java
@@ -1,17 +1,30 @@
+#
+# @lc app=leetcode id=3448 lang=java
+#
+# [3448] Count Substrings Divisible By Last Digit
+#
+# @lc code=start
 class Solution {
     public long countSubstrings(String s) {
-        long count = 0;
         int n = s.length();
-        for (int i = 0; i < n; i++) {
-            int num = 0;
-            for (int j = i; j < n; j++) {
-                num = num * 10 + (s.charAt(j) - '0');
-                int lastDigit = s.charAt(j) - '0';
-                if (lastDigit != 0 && num % lastDigit == 0) {
-                    count++;
-                }
+        long res = 0;
+        for (int i = 0; i < n; ++i) {
+            char lastChar = s.charAt(i);
+            if (lastChar == '0') continue; // skip if last digit is zero
+            int d = lastChar - '0';
+            int mod = 0;
+            int pow = 1;
+            // Check all substrings ending at i
+            for (int j = i; j >= 0; --j) {
+                int digit = s.charAt(j) - '0';
+                mod = (digit * pow + mod) % d;
+                if (mod == 0) res++;
+                pow = (pow * 10) % d;
+                // For efficiency: If pow becomes 0, further digits won't affect mod
+                if (pow == 0) break;
             }
         }
-        return count;
+        return res;
     }
 }
+# @lc code=end
