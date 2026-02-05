5a93840d2b2441048383e80c105281deeac62219
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 14:25:45 +0800

    Batch add 1 solutions: 3562.maximum-profit-...

--- 3562.maximum-profit-from-trading-stocks-with-discounts.cpp
+++ 3562.maximum-profit-from-trading-stocks-with-discounts.cpp
@@ -9,41 +9,64 @@ class Solution {
 public:
     int maxProfit(int n, vector<int>& present, vector<int>& future, vector<vector<int>>& hierarchy, int budget) {
         vector<vector<int>> tree(n);
-        for (auto& h : hierarchy) {
-            int u = h[0] - 1, v = h[1] - 1;
+        for (const auto& edge : hierarchy) {
+            int u = edge[0] - 1, v = edge[1] - 1;
             tree[u].push_back(v);
         }
-        // dp[u][0][c]: max profit for u and subtree with cost c if boss doesn't buy (no discount)
-        // dp[u][1][c]: max profit for u and subtree with cost c if boss buys (discounted price for u)
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(budget + 1, -1e9)) );
-
+        // dp[node][discount][cost] = max profit; discount: 0=no discount, 1=discount
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(budget+1, -1)));
         function<void(int)> dfs = [&](int u) {
-            for (int b = 0; b <= budget; ++b) {
-                dp[u][0][b] = dp[u][1][b] = 0;
+            // Base initialization
+            for (int disc = 0; disc <= 1; ++disc) {
+                fill(dp[u][disc].begin(), dp[u][disc].end(), 0);
             }
-            int cost = present[u];
-            int profit = future[u] - present[u];
-            if (cost <= budget) dp[u][0][cost] = profit;
-            int dcost = present[u] / 2;
-            int dprofit = future[u] - dcost;
-            if (dcost <= budget) dp[u][1][dcost] = dprofit;
-            dp[u][0][0] = dp[u][1][0] = 0;
-            for (int v : tree[u]) {
-                dfs(v);
-                vector<vector<int>> ndp(2, vector<int>(budget + 1, -1e9));
-                for (int x = 0; x <= budget; ++x) {
-                    for (int y = 0; y <= budget - x; ++y) {
-                        ndp[0][x + y] = max(ndp[0][x + y], dp[u][0][x] + max(dp[v][0][y], dp[v][1][y]));
-                        ndp[1][x + y] = max(ndp[1][x + y], dp[u][1][x] + dp[v][1][y]);
+            // First, not buy at u: just combine children
+            for (int disc = 0; disc <= 1; ++disc) {
+                vector<int> cur(budget+1, 0);
+                for (int v : tree[u]) {
+                    dfs(v);
+                    vector<int> next(budget+1, 0);
+                    for (int c1 = 0; c1 <= budget; ++c1) {
+                        if (cur[c1] < 0) continue;
+                        for (int c2 = 0; c2 + c1 <= budget; ++c2) {
+                            if (dp[v][0][c2] < 0) continue;
+                            next[c1+c2] = max(next[c1+c2], cur[c1] + dp[v][0][c2]);
+                        }
                     }
+                    swap(cur, next);
                 }
-                dp[u] = ndp;
+                for (int c = 0; c <= budget; ++c) {
+                    dp[u][disc][c] = max(dp[u][disc][c], cur[c]);
+                }
+            }
+            // Then, buy at u (with or without discount)
+            for (int disc = 0; disc <= 1; ++disc) {
+                int buy_price = disc ? present[u] / 2 : present[u];
+                int profit = future[u] - buy_price;
+                if (buy_price > budget) continue;
+                vector<int> cur(budget+1, -1);
+                cur[buy_price] = profit;
+                for (int v : tree[u]) {
+                    vector<int> next(budget+1, -1);
+                    for (int c1 = 0; c1 <= budget; ++c1) {
+                        if (cur[c1] < 0) continue;
+                        for (int c2 = 0; c2 + c1 <= budget; ++c2) {
+                            if (dp[v][1][c2] < 0) continue;
+                            next[c1+c2] = max(next[c1+c2], cur[c1] + dp[v][1][c2]);
+                        }
+                    }
+                    swap(cur, next);
+                }
+                for (int c = 0; c <= budget; ++c) {
+                    if (cur[c] >= 0)
+                        dp[u][disc][c] = max(dp[u][disc][c], cur[c]);
+                }
             }
         };
         dfs(0);
         int ans = 0;
-        for (int b = 0; b <= budget; ++b) {
-            ans = max({ans, dp[0][0][b], dp[0][1][b]});
+        for (int c = 0; c <= budget; ++c) {
+            ans = max(ans, dp[0][0][c]);
         }
         return ans;
     }
