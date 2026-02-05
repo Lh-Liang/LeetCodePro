f982018bb61b3fe2832e7b68c583a91c7d7508ee
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 04:44:24 +0800

    Batch add 1 solutions: 3559.number-of-ways-...

--- 3559.number-of-ways-to-assign-edge-weights-ii.java
+++ 3559.number-of-ways-to-assign-edge-weights-ii.java
@@ -4,50 +4,79 @@
 # [3559] Number of Ways to Assign Edge Weights II
 #
 # @lc code=start
-import java.util.*;
-
 class Solution {
-    private static final int MOD = 1000000007;
-    private List<Integer>[] tree;
-    private int[] parent;
-    private int[] depth;
-    
+    static final int MOD = 1000000007;
+    int[][] up;
+    int[] depth;
+    int LOG;
+    List<Integer>[] tree;
     public int[] assignEdgeWeights(int[][] edges, int[][] queries) {
         int n = edges.length + 1;
-        tree = new ArrayList[n + 1];
-        parent = new int[n + 1];
+        LOG = 1;
+        while ((1 << LOG) <= n) LOG++;
+        tree = new List[n + 1];
+        for (int i = 1; i <= n; i++) tree[i] = new ArrayList<>();
+        for (int[] e : edges) {
+            tree[e[0]].add(e[1]);
+            tree[e[1]].add(e[0]);
+        }
+        up = new int[n + 1][LOG];
         depth = new int[n + 1];
-        Arrays.fill(parent, -1);
-        
-        for (int i = 0; i <= n; i++) {
-            tree[i] = new ArrayList<>();
-        }
-        
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1];
-            tree[u].add(v);
-            tree[v].add(u);
-        }
-        
-        dfs(1, -1, 0);
-        
-        int[] answer = new int[queries.length];
+        dfs(1, 1);
+        int[] res = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
             int u = queries[i][0], v = queries[i][1];
-            answer[i] = countOddPaths(u, v);
+            int k = getDist(u, v);
+            if (k == 0) {
+                res[i] = 0;
+            } else {
+                res[i] = pow2(k - 1, MOD);
+            }
         }
-        return answer;
+        return res;
     }
-    
-    private void dfs(int node, int par, int d) {
-        parent[node] = par;
-        depth[node] = d;
-        for (int neighbor : tree[node]) {
-            if (neighbor != par) {
-                dfs(neighbor, node, d + 1);
+    void dfs(int u, int p) {
+        up[u][0] = p;
+        for (int i = 1; i < LOG; i++) {
+            up[u][i] = up[up[u][i - 1]][i - 1];
+        }
+        for (int v : tree[u]) {
+            if (v != p) {
+                depth[v] = depth[u] + 1;
+                dfs(v, u);
             }
         }
     }
-
-    private int lca(int u, int v) {	
-	while(depth[u] > depth[v]) u=parent[u];	while(depth[v] > depth[u]) v=parent[v];	while(u != v){	u=parent[u];v=parent[v];}	return u;}	private long combination(int n,int r){ 	long result=1L;for(int i=0;i<r;i++){result=result*(long)(nt-i)/(long)(i+1)%MOD;}return result;}private int countOddPaths(int u,int v){int lcaNode=lca(u,v);int pathLength=depth[u]+depth[v]-2*depth[lcaNode];int ways=0;for(int i=1;i<=pathLength;i+=2){ways=(ways+combination(pathLength,i))%MOD;}return ways;}
+    int lca(int u, int v) {
+        if (depth[u] < depth[v]) {
+            int t = u; u = v; v = t;
+        }
+        for (int i = LOG - 1; i >= 0; i--) {
+            if (depth[u] - (1 << i) >= depth[v]) {
+                u = up[u][i];
+            }
+        }
+        if (u == v) return u;
+        for (int i = LOG - 1; i >= 0; i--) {
+            if (up[u][i] != up[v][i]) {
+                u = up[u][i];
+                v = up[v][i];
+            }
+        }
+        return up[u][0];
+    }
+    int getDist(int u, int v) {
+        int a = lca(u, v);
+        return depth[u] + depth[v] - 2 * depth[a];
+    }
+    int pow2(int k, int mod) {
+        long res = 1, base = 2;
+        while (k > 0) {
+            if ((k & 1) != 0) res = (res * base) % mod;
+            base = (base * base) % mod;
+            k >>= 1;
+        }
+        return (int) res;
+    }
+}
+# @lc code=end
