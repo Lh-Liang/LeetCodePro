a81bf164571eba5bb22ca475be733f6a06733eeb
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 05:57:59 +0800

    Batch add 1 solutions: 3734.lexicographical...

--- 3734.lexicographically-smallest-palindromic-permutation-greater-than-target.java
+++ 3734.lexicographically-smallest-palindromic-permutation-greater-than-target.java
@@ -6,41 +6,51 @@
 # @lc code=start
 class Solution {
     public String lexPalindromicPermutation(String s, String target) {
+        int n = s.length();
         int[] count = new int[26];
-        for (char c : s.toCharArray()) {
-            count[c - 'a']++;
-        }
-        StringBuilder half = new StringBuilder();
+        for (char c : s.toCharArray()) count[c-'a']++;
+        int odd = 0;
         char oddChar = 0;
         for (int i = 0; i < 26; i++) {
-            if (count[i] % 2 == 1) {
-                if (oddChar != 0) return ""; // More than one odd character, can't be palindromic
-                oddChar = (char)(i + 'a');
-            }
-            for (int j = 0; j < count[i] / 2; j++) {
-                half.append((char)(i + 'a'));
+            if ((count[i] & 1) == 1) {
+                odd++; oddChar = (char)('a'+i);
             }
         }
-        char[] halfArray = half.toString().toCharArray();
-        Arrays.sort(halfArray);
-        do {
-            StringBuilder palindrome = new StringBuilder(new String(halfArray));
-            if (oddChar != 0) palindrome.append(oddChar);
-            palindrome.append(new StringBuilder(new String(halfArray)).reverse());
-            if (palindrome.toString().compareTo(target) > 0) return palindrome.toString();
-        } while (nextPermutation(halfArray)); // Find next permutation that is greater than current one. 
-        return ""; // No valid permutation found. 
-    } 
-    private boolean nextPermutation(char[] array) { 
-        int i = array.length - 2; 
-        while (i >= 0 && array[i] >= array[i + 1]) i--; 
-        if (i < 0) return false; // No next permutation possible. 
-        int j = array.length - 1; 
-        while (array[j] <= array[i]) j--; 
-        swap(array, i, j); 
-        reverse(array, i + 1); 
-        return true; 
-    } 
-    private void swap(char[] array, int i, int j) { 
-       char temp = array[i]; array[i] = array[j]; array[j] = temp; 
-    } 	private void reverse(char[] array, int start) { 		int end = array.length - 1;		while(start < end){ swap(array,start++,end--);} } } # @lc code=end
+        if ((n & 1) == 0 && odd != 0) return "";
+        if ((n & 1) == 1 && odd != 1) return "";
+        // build the half and center of the palindrome
+        StringBuilder half = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < count[i]/2; j++) half.append((char)('a'+i));
+        }
+        String center = (n%2==1) ? String.valueOf(oddChar) : "";
+        char[] halfArr = half.toString().toCharArray();
+        boolean first = true;
+        while (true) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(halfArr);
+            sb.append(center);
+            for (int i = halfArr.length-1; i >= 0; i--) sb.append(halfArr[i]);
+            String palindrome = sb.toString();
+            if (palindrome.compareTo(target) > 0) return palindrome;
+            // get next permutation for halfArr
+            if (!nextPermutation(halfArr)) break;
+            first = false;
+        }
+        return "";
+    }
+    // Standard next permutation, returns false if already last permutation
+    private boolean nextPermutation(char[] arr) {
+        int i = arr.length - 2;
+        while (i >= 0 && arr[i] >= arr[i+1]) i--;
+        if (i < 0) return false;
+        int j = arr.length - 1;
+        while (arr[j] <= arr[i]) j--;
+        char tmp = arr[i]; arr[i] = arr[j]; arr[j] = tmp;
+        for (int l = i+1, r = arr.length-1; l < r; l++, r--) {
+            tmp = arr[l]; arr[l] = arr[r]; arr[r] = tmp;
+        }
+        return true;
+    }
+}
+# @lc code=end
