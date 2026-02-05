3b35cc3bdad02fa545b658856bf700f076bc0cd5
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 14:13:54 +0800

    Batch add 1 solutions: 3519.count-numbers-w...

--- 3519.count-numbers-with-non-decreasing-digits.cpp
+++ 3519.count-numbers-with-non-decreasing-digits.cpp
@@ -5,84 +5,70 @@
 #
 
 # @lc code=start
-#include <vector>
-#include <string>
-#include <cstring>
-using namespace std;
-
 class Solution {
-    static constexpr int MOD = 1e9 + 7;
 public:
-    int countNumbers(string l, string r, int b) {
-        vector<int> r_digits = to_base_b(r, b);
-        vector<int> l_digits = to_base_b(subtract_one(l), b);
-        memset(dp, -1, sizeof(dp));
-        int cnt_r = dfs(0, 0, 1, 1, r_digits, b);
-        memset(dp, -1, sizeof(dp));
-        int cnt_l = dfs(0, 0, 1, 1, l_digits, b);
-        int ans = (cnt_r - cnt_l + MOD) % MOD;
-        return ans;
-    }
-
-private:
-    int dp[105][11][2][2];
-    // pos, prev_digit, tight, leading_zero
-    int dfs(int pos, int prev, int tight, int leading_zero, const vector<int>& digits, int b) {
-        if (pos == digits.size()) return leading_zero ? 0 : 1;
-        int &res = dp[pos][prev][tight][leading_zero];
-        if (res != -1) return res;
-        res = 0;
-        int limit = tight ? digits[pos] : b - 1;
-        for (int d = 0; d <= limit; ++d) {
-            if (!leading_zero && d < prev) continue;
-            int next_leading_zero = leading_zero && d == 0;
-            int next_tight = tight && (d == limit);
-            int next_prev = next_leading_zero ? 0 : d;
-            res = (res + dfs(pos + 1, next_prev, next_tight, next_leading_zero, digits, b)) % MOD;
-        }
-        return res;
-    }
-    // Converts decimal string to base-b vector (MSD first)
-    vector<int> to_base_b(string s, int b) {
-        vector<int> v;
-        vector<int> num;
-        for(char ch : s) num.push_back(ch - '0');
-        while(!num.empty()) {
+    static constexpr int MOD = 1000000007;
+    // Convert decimal string to vector of base-b digits (most to least significant)
+    vector<int> to_base_b(const string& s, int b) {
+        vector<int> res;
+        string n = s;
+        while (n != "0") {
             int rem = 0;
-            vector<int> next;
-            for(int d : num) {
-                int cur = rem * 10 + d;
-                next.push_back(cur / b);
+            string next;
+            for (char c : n) {
+                int cur = rem * 10 + (c - '0');
+                next += (cur / b) + '0';
                 rem = cur % b;
             }
-            v.push_back(rem);
-            // Remove leading zeros
-            int idx = 0;
-            while(idx < next.size() && next[idx] == 0) ++idx;
-            num.assign(next.begin() + idx, next.end());
+            res.push_back(rem);
+            // remove leading zeros
+            int i = 0;
+            while (i < next.size() && next[i] == '0') ++i;
+            n = (i == next.size() ? "0" : next.substr(i));
         }
-        while(v.size() < s.size()) v.push_back(0);
-        reverse(v.begin(), v.end());
-        return v;
+        if (res.empty()) res.push_back(0);
+        reverse(res.begin(), res.end());
+        return res;
     }
-    // Subtract one from decimal string, handles big numbers
-    string subtract_one(const string& s) {
-        string res = s;
-        int n = res.size();
-        int i = n - 1;
-        while(i >= 0) {
-            if(res[i] > '0') {
-                res[i]--;
-                break;
-            } else {
-                res[i] = '9';
-                i--;
+
+    // Decrement decimal string by 1
+    string dec_string_minus_one(string s) {
+        int n = s.size(), i = n - 1;
+        while (i >= 0 && s[i] == '0') s[i--] = '9';
+        if (i >= 0) s[i]--;
+        if (s[0] == '0' && s.size() > 1) s = s.substr(1);
+        return s == "" ? "0" : s;
+    }
+
+    int count_dp(const vector<int>& digits, int b) {
+        int n = digits.size();
+        vector<vector<vector<int>>> memo(n+1, vector<vector<int>>(b+1, vector<int>(2, -1)));
+        function<int(int, int, bool, bool)> dfs = [&](int pos, int prev, bool tight, bool lead_zero) -> int {
+            if (pos == n) return lead_zero ? 0 : 1;
+            int &res = memo[pos][prev][tight ? 1 : 0];
+            if (!lead_zero && res != -1) return res;
+            int limit = tight ? digits[pos] : b-1;
+            int ans = 0;
+            for (int d = 0; d <= limit; ++d) {
+                if (!lead_zero && d < prev) continue;
+                bool next_lead_zero = lead_zero && d == 0;
+                int next_prev = next_lead_zero ? 0 : d;
+                bool next_tight = tight && (d == limit);
+                ans = (ans + dfs(pos+1, next_prev, next_tight, next_lead_zero)) % MOD;
             }
-        }
-        // Remove leading zeros
-        i = 0;
-        while(i < res.size()-1 && res[i] == '0') i++;
-        return res.substr(i);
+            if (!lead_zero) res = ans;
+            return ans;
+        };
+        return dfs(0, 0, true, true);
+    }
+
+    int countNumbers(string l, string r, int b) {
+        vector<int> dr = to_base_b(r, b);
+        int cnt_r = count_dp(dr, b);
+        string l1 = dec_string_minus_one(l);
+        int cnt_l = (l1 == "0") ? 0 : count_dp(to_base_b(l1, b), b);
+        int ans = (cnt_r - cnt_l + MOD) % MOD;
+        return ans;
     }
 };
 # @lc code=end
