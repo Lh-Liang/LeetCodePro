a56c8f205433db849669365453d493251df642fc
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 16:03:33 +0800

    Batch add 1 solutions: 2807.insert-greatest...

--- 2807.insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807.insert-greatest-common-divisors-in-linked-list.cpp
@@ -3,36 +3,42 @@
 #
 # [2807] Insert Greatest Common Divisors in Linked List
 #
+
 # @lc code=start
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode() : val(0), next(nullptr) {}
-*     ListNode(int x) : val(x), next(nullptr) {}
-*     ListNode(int x, ListNode *next) : val(x), next(next) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <numeric>
+
 class Solution {
 public:
-    int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
-        if (!head || !head->next) return head;
-        ListNode* curr = head;
-        while (curr && curr->next) {
-            int g = gcd(curr->val, curr->next->val);
-            ListNode* node = new ListNode(g, curr->next);
-            curr->next = node;
-            curr = node->next;
+        // If the list is empty or has only one node, no adjacent pairs exist.
+        if (!head || !head->next) {
+            return head;
         }
+
+        ListNode* curr = head;
+        // Traverse the list as long as there is an adjacent pair.
+        while (curr && curr->next) {
+            // Calculate GCD of current node and the next node values.
+            int gcdVal = std::gcd(curr->val, curr->next->val);
+            
+            // Create and insert the new GCD node.
+            ListNode* newNode = new ListNode(gcdVal, curr->next);
+            curr->next = newNode;
+            
+            // Jump to the original 'next' node, which is now two steps ahead.
+            curr = newNode->next;
+        }
+        
         return head;
     }
 };
