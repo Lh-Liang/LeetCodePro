0e06fead441f641e9a57eebb0a563b043791e9fd
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 16:10:32 +0800

    Batch add 1 solutions: 2816.double-a-number...

--- 2816.double-a-number-represented-as-a-linked-list.cpp
+++ 2816.double-a-number-represented-as-a-linked-list.cpp
@@ -3,45 +3,41 @@
 #
 # [2816] Double a Number Represented as a Linked List
 #
+
 # @lc code=start
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode() : val(0), next(nullptr) {}
-*     ListNode(int x) : val(x), next(nullptr) {}
-*     ListNode(int x, ListNode *next) : val(x), next(next) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
-        while (head) {
-            ListNode* next = head->next;
-            head->next = prev;
-            prev = head;
-            head = next;
-        }
-        return prev;
-    }
     ListNode* doubleIt(ListNode* head) {
-        head = reverseList(head);
+        // If the leading digit is >= 5, doubling it will create a new digit '1' at the front.
+        if (head->val > 4) {
+            head = new ListNode(0, head);
+        }
+        
         ListNode* curr = head;
-        int carry = 0;
-        ListNode* prev = nullptr;
-        while (curr) {
-            int val = curr->val * 2 + carry;
-            curr->val = val % 10;
-            carry = val / 10;
-            prev = curr;
+        while (curr != nullptr) {
+            // The current node's value becomes (val * 2) % 10.
+            // We look ahead to the next node to see if it will produce a carry.
+            int newValue = (curr->val * 2) % 10;
+            
+            if (curr->next != nullptr && curr->next->val > 4) {
+                newValue += 1;
+            }
+            
+            curr->val = newValue;
             curr = curr->next;
         }
-        if (carry) {
-            prev->next = new ListNode(carry);
-        }
-        return reverseList(head);
+        
+        return head;
     }
 };
 # @lc code=end
