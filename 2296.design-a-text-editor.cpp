cede9309656274e4636d2e33355e9aa711896192
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 13:22:23 +0800

    Batch add 1 solutions: 2296.design-a-text-e...

--- 2296.design-a-text-editor.cpp
+++ 2296.design-a-text-editor.cpp
@@ -7,52 +7,36 @@
 # @lc code=start
 class TextEditor {
 public:
-    std::stack<char> left, right;
     TextEditor() {
+        // left and right hold the text to the left and right of the cursor
     }
-    void addText(std::string text) {
-        for(char c : text) left.push(c);
+
+    void addText(string text) {
+        left += text;
     }
+
     int deleteText(int k) {
-        int cnt = 0;
-        while(!left.empty() && k--) {
-            left.pop();
-            ++cnt;
-        }
+        int cnt = min(k, (int)left.size());
+        left.erase(left.size() - cnt, cnt);
         return cnt;
     }
-    std::string cursorLeft(int k) {
-        while(k-- && !left.empty()) {
-            right.push(left.top());
-            left.pop();
-        }
-        return getLast10();
+
+    string cursorLeft(int k) {
+        int move = min(k, (int)left.size());
+        right = left.substr(left.size() - move, move) + right;
+        left.erase(left.size() - move, move);
+        return left.substr(left.size() - min(10, (int)left.size()));
     }
-    std::string cursorRight(int k) {
-        while(k-- && !right.empty()) {
-            left.push(right.top());
-            right.pop();
-        }
-        return getLast10();
+
+    string cursorRight(int k) {
+        int move = min(k, (int)right.size());
+        left += right.substr(0, move);
+        right.erase(0, move);
+        return left.substr(left.size() - min(10, (int)left.size()));
     }
+
 private:
-    std::string getLast10() {
-        std::string res;
-        std::stack<char> tmp;
-        int cnt = 0;
-        // move up to 10 chars to tmp to reverse, and restore afterward
-        while(cnt < 10 && !left.empty()) {
-            tmp.push(left.top());
-            left.pop();
-            cnt++;
-        }
-        while(!tmp.empty()) {
-            res.push_back(tmp.top());
-            left.push(tmp.top()); // restore
-            tmp.pop();
-        }
-        return res;
-    }
+    string left, right;
 };
 
 /**
