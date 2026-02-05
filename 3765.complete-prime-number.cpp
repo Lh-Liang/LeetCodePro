f049289f028bf5fd278efdf52348f9ba6fa4e621
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 15:08:42 +0800

    Batch add 1 solutions: 3765.complete-prime-...

--- 3765.complete-prime-number.cpp
+++ 3765.complete-prime-number.cpp
@@ -3,12 +3,11 @@
 #
 # [3765] Complete Prime Number
 #
-
 # @lc code=start
 class Solution {
 public:
-    bool is_prime(int n) {
-        if (n <= 1) return false;
+    bool isPrime(int n) {
+        if (n < 2) return false;
         if (n == 2) return true;
         if (n % 2 == 0) return false;
         for (int i = 3; i * 1LL * i <= n; i += 2) {
@@ -16,19 +15,20 @@ public:
         }
         return true;
     }
-
     bool completePrime(int num) {
         std::string s = std::to_string(num);
         int n = s.size();
         // Check all prefixes
         for (int i = 1; i <= n; ++i) {
             int prefix = std::stoi(s.substr(0, i));
-            if (!is_prime(prefix)) return false;
+            if (!isPrime(prefix))
+                return false;
         }
         // Check all suffixes
         for (int i = 0; i < n; ++i) {
             int suffix = std::stoi(s.substr(i));
-            if (!is_prime(suffix)) return false;
+            if (!isPrime(suffix))
+                return false;
         }
         return true;
     }
