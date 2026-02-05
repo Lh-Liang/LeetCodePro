a67d8b7298bafbb4dea54670b2dc0280df4c707a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 13:53:13 +0800

    Batch add 1 solutions: 3462.maximum-sum-wit...

--- 3462.maximum-sum-with-at-most-k-elements.cpp
+++ 3462.maximum-sum-with-at-most-k-elements.cpp
@@ -3,29 +3,27 @@
 #
 # [3462] Maximum Sum With at Most K Elements
 #
+
 # @lc code=start
 class Solution {
 public:
     long long maxSum(vector<vector<int>>& grid, vector<int>& limits, int k) {
+        vector<int> candidates;
         int n = grid.size();
-        vector<long long> dp(k + 1, 0);
         for (int i = 0; i < n; ++i) {
-            vector<int> row = grid[i];
+            auto row = grid[i];
             sort(row.rbegin(), row.rend());
-            vector<long long> prefix(1, 0);
-            int lim = min((int)row.size(), limits[i]);
-            for (int j = 0; j < lim; ++j) {
-                prefix.push_back(prefix.back() + row[j]);
+            int cnt = min((int)row.size(), limits[i]);
+            for (int j = 0; j < cnt; ++j) {
+                candidates.push_back(row[j]);
             }
-            vector<long long> ndp = dp;
-            for (int t = 1; t <= lim; ++t) {
-                for (int x = k; x >= t; --x) {
-                    ndp[x] = max(ndp[x], dp[x-t] + prefix[t]);
-                }
-            }
-            dp = move(ndp);
         }
-        return *max_element(dp.begin(), dp.end());
+        sort(candidates.rbegin(), candidates.rend());
+        long long ans = 0;
+        for (int i = 0; i < min(k, (int)candidates.size()); ++i) {
+            ans += candidates[i];
+        }
+        return ans;
     }
 };
 # @lc code=end
