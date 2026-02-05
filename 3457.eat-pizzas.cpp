f8b77b6693d4d4730d23fc1fd5911e0a12bb47df
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 13:48:43 +0800

    Batch add 1 solutions: 3457.eat-pizzas.cpp...

--- 3457.eat-pizzas.cpp
+++ 3457.eat-pizzas.cpp
@@ -1,27 +1,26 @@
-//
-// @lc app=leetcode id=3457 lang=cpp
-//
-// [3457] Eat Pizzas!
-//
-
-// @lc code=start
+#
+# @lc app=leetcode id=3457 lang=cpp
+#
+# [3457] Eat Pizzas!
+#
+# @lc code=start
 class Solution {
 public:
     long long maxWeight(vector<int>& pizzas) {
-        int n = pizzas.size();
         sort(pizzas.begin(), pizzas.end());
-        long long total = 0;
-        int day = 1;
-        for (int i = n - 1; i >= 3; i -= 4) {
-            // pizzas[i-3], pizzas[i-2], pizzas[i-1], pizzas[i]
-            if (day % 2 == 1) {
-                total += pizzas[i];      // Odd day: add largest (Z)
+        int n = pizzas.size();
+        int days = n / 4;
+        long long ans = 0;
+        for (int i = 0; i < days; ++i) {
+            // Odd day: take Z (largest in the group)
+            if (i % 2 == 0) {
+                ans += pizzas[n - 1 - i * 4];
             } else {
-                total += pizzas[i - 1];  // Even day: add second largest (Y)
+                // Even day: take Y (second largest in the group)
+                ans += pizzas[n - 2 - i * 4];
             }
-            day++;
         }
-        return total;
+        return ans;
     }
 };
-// @lc code=end
+# @lc code=end
