12dd94fe5891f968a05c520b237dc4b7bb013ee9
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 14:32:46 +0800

    Batch add 1 solutions: 3594.minimum-time-to...

--- 3594.minimum-time-to-transport-all-individuals.cpp
+++ 3594.minimum-time-to-transport-all-individuals.cpp
@@ -3,72 +3,58 @@
 #
 # [3594] Minimum Time to Transport All Individuals
 #
-# @lc code=start
-#include <vector>
-#include <cmath>
-#include <cstring>
-#include <algorithm>
-using namespace std;
 
+# @lc code=start
 class Solution {
 public:
     double minTime(int n, int k, int m, vector<int>& time, vector<double>& mul) {
-        // DP state: (people_at_base_mask, stage, boat_at_base)
-        // boat_at_base: 1 = at base, 0 = at destination
-        // Use memoization
-        constexpr double INF = 1e18;
-        double dp[1<<12][5][2];
-        bool vis[1<<12][5][2];
-        memset(vis, 0, sizeof(vis));
-        
-        function<double(int,int,int)> dfs = [&](int mask, int stage, int boat) -> double {
-            // Sanity check: if all people have crossed and boat is at destination, done
-            if(mask == 0 && boat == 0) return 0.0;
-            // If boat is at base but no one left to cross, invalid
-            if(mask == 0) return INF;
-            if(vis[mask][stage][boat]) return dp[mask][stage][boat];
-            vis[mask][stage][boat] = 1;
-            double& res = dp[mask][stage][boat];
-            res = INF;
-            if(boat == 1) { // boat at base
-                // Generate all valid non-empty groups of up to k people at base
-                vector<int> idxs;
-                for(int i=0;i<n;++i) if((mask>>i)&1) idxs.push_back(i);
-                int bsize = idxs.size();
-                for(int sel = 1; sel < (1<<bsize); ++sel) {
-                    if(__builtin_popcount(sel) > k) continue;
-                    int newmask = mask;
-                    double mx = 0.0;
-                    for(int j=0;j<bsize;++j) {
-                        if((sel>>j)&1) {
-                            mx = max(mx, (double)time[idxs[j]]);
-                            newmask ^= (1<<idxs[j]);
+        using State = tuple<int,int,bool>;
+        unordered_map<long long, double> memo;
+        function<double(int,int,bool)> dp = [&](int mask, int stage, bool at_base) -> double {
+            if(mask == 0) return 0.0; // all at destination
+            long long key = ((long long)mask << 3) | (stage << 1) | at_base;
+            if(memo.count(key)) return memo[key];
+            double res = 1e18;
+            if(at_base) {
+                // choose group: all non-empty subsets of size <= k
+                vector<int> people;
+                for(int i=0;i<n;++i) if(mask & (1<<i)) people.push_back(i);
+                int sz = people.size();
+                for(int group=1; group<(1<<sz); ++group) {
+                    int cnt = __builtin_popcount(group);
+                    if(cnt > k) continue;
+                    int next_mask = mask;
+                    double mx_time = 0;
+                    vector<int> crossers;
+                    for(int j=0;j<sz;++j) {
+                        if(group & (1<<j)) {
+                            crossers.push_back(people[j]);
+                            mx_time = max(mx_time, (double)time[people[j]]);
+                            next_mask ^= (1<<people[j]);
                         }
                     }
-                    double trip = mx * mul[stage];
-                    int adv = (int)floor(trip)%m;
-                    int newstage = (stage + adv)%m;
-                    double subres = dfs(newmask,newstage,0);
-                    if(subres < INF) res = min(res, trip+subres);
-                }
-            } else { // boat at destination, need to return someone if people still at base
-                // If all at base, can't return
-                if(mask == (1<<n)-1) return INF;
-                vector<int> idxs;
-                for(int i=0;i<n;++i) if(!((mask>>i)&1)) idxs.push_back(i); // at dest
-                for(int i: idxs) {
-                    double rtrip = time[i] * mul[stage];
-                    int adv = (int)floor(rtrip)%m;
-                    int newstage = (stage + adv)%m;
-                    double subres = dfs(mask|(1<<i),newstage,1);
-                    if(subres < INF) res = min(res, rtrip+subres);
+                    double cross_time = mx_time * mul[stage];
+                    int new_stage = (stage + ((int)floor(cross_time)) % m) % m;
+                    if(next_mask == 0) {
+                        res = min(res, cross_time);
+                        continue;
+                    }
+                    // If k==n and not all cross, must return
+                    for(int ret: crossers) {
+                        double ret_time = time[ret] * mul[new_stage];
+                        int ret_stage = (new_stage + ((int)floor(ret_time)) % m) % m;
+                        int ret_mask = next_mask | (1<<ret);
+                        double sub = dp(ret_mask, ret_stage, true);
+                        if(sub < 1e18)
+                            res = min(res, cross_time + ret_time + sub);
+                    }
                 }
             }
-            // Complete: return computed value for this state
+            memo[key] = res;
             return res;
         };
-        double ans = dfs((1<<n)-1, 0, 1);
-        if(ans >= INF) return -1.0;
+        double ans = dp((1<<n)-1, 0, true);
+        if(ans > 1e17) return -1.0;
         return ans;
     }
 };
