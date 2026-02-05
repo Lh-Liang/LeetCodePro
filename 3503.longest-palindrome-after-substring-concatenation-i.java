41f0aa39d0445116400ac1d8eca4903d0011b1ff
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 23:22:14 +0800

    Batch add 1 solutions: 3503.longest-palindr...

---
+++ 3503.longest-palindrome-after-substring-concatenation-i.java
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=3503 lang=java
+#
+# [3503] Longest Palindrome After Substring Concatenation I
+#
+
+# @lc code=start
+class Solution {
+    public int longestPalindrome(String s, String t) {
+        int maxLength = 0;
+        // Iterate over all substrings of s
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = i; j <= s.length(); j++) {
+                String subS = s.substring(i, j);
+                // Iterate over all substrings of t
+                for (int k = 0; k <= t.length(); k++) {
+                    for (int l = k; l <= t.length(); l++) {
+                        String subT = t.substring(k, l);
+                        // Concatenate and check if palindrome
+                        String candidate = subS + subT;
+                        if (isPalindrome(candidate)) {
+                            maxLength = Math.max(maxLength, candidate.length());
+                        }
+                    }
+                }
+            }
+        }
+        return maxLength;
+    }
+    
+    // Helper method to check if a string is a palindrome
+    private boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+# @lc code=end
