3c7696b844e19ecf580874fc78bc973dfec422d5
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 05:52:51 +0800

    Batch add 1 solutions: 3725.count-ways-to-c...

--- 3725.count-ways-to-choose-coprime-integers-from-rows.java
+++ 3725.count-ways-to-choose-coprime-integers-from-rows.java
@@ -1,52 +1,28 @@
-#
-# @lc app=leetcode id=3725 lang=java
-#
-# [3725] Count Ways to Choose Coprime Integers from Rows
-#
-
-# @lc code=start
 class Solution {
     public int countCoprime(int[][] mat) {
-        // Constants
-        final int MOD = 1000000007;
+        int MOD = 1_000_000_007;
         int m = mat.length;
-        int n = mat[0].length;
-        
-        // Dynamic programming table for counting ways
-        long[] dp = new long[151]; // Possible GCD values from 1 to 150
-        
-        // Initialize dp array with first row elements
-        for (int num : mat[0]) {
-            dp[num] = 1;
-        }
-        
-        // Iterate over each subsequent row in the matrix
-        for (int i = 1; i < m; i++) {
-            long[] nextDp = new long[151]; 
-            for (int j = 0; j < n; j++) {
-                int num = mat[i][j];
-                for (int k = 1; k <= 150; k++) {
-                    if (dp[k] > 0) { // Only consider non-zero counts
-                        int newGCD = gcd(num, k);
-                        nextDp[newGCD] = (nextDp[newGCD] + dp[k]) % MOD;
-                    }
+        java.util.Map<Integer, Integer> dp = new java.util.HashMap<>();
+        for (int x : mat[0]) dp.put(x, dp.getOrDefault(x, 0) + 1);
+        for (int i = 1; i < m; ++i) {
+            java.util.Map<Integer, Integer> ndp = new java.util.HashMap<>();
+            for (java.util.Map.Entry<Integer, Integer> e : dp.entrySet()) {
+                int prev = e.getKey(), cnt = e.getValue();
+                for (int x : mat[i]) {
+                    int g = gcd(prev, x);
+                    ndp.put(g, (int)(((long)ndp.getOrDefault(g, 0) + cnt) % MOD));
                 }
             }
-            dp = nextDp; // Move to next row's state as current state. 
-        } 
-        
-        // Sum up all ways leading to a GCD of 1 combination. 
-        long result = dp[1]; 
-        return (int) result;
+            dp = ndp;
+        }
+        return dp.getOrDefault(1, 0);
     }
-    
     private int gcd(int a, int b) {
-        while(b != 0){
-            int temp= b;
-            b=a % b;
-            a=temp;
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
         }
         return a;
     }
 }
-# @lc code=end
