bf4b45e5b9e84db641e67ab28111ece9ea8f778c
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 13:40:53 +0800

    Batch add 1 solutions: 3425.longest-special...

--- 3425.longest-special-path.cpp
+++ 3425.longest-special-path.cpp
@@ -3,39 +3,45 @@
 #
 # [3425] Longest Special Path
 #
-
 # @lc code=start
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
 class Solution {
 public:
     vector<int> longestSpecialPath(vector<vector<int>>& edges, vector<int>& nums) {
         int n = nums.size();
-        vector<vector<pair<int, int>>> tree(n); // node, edge length
-        for (auto& e : edges) {
+        vector<vector<pair<int, int>>> tree(n);
+        for (const auto& e : edges) {
             int u = e[0], v = e[1], l = e[2];
             tree[u].emplace_back(v, l);
             tree[v].emplace_back(u, l);
         }
-        int maxLength = 0;
-        int minNodes = 1;
-        unordered_map<int, int> valueCount;
-        function<void(int, int, int, int)> dfs = [&](int node, int parent, int currLen, int nodeCount) {
-            valueCount[nums[node]]++;
-            if (currLen > maxLength) {
-                maxLength = currLen;
-                minNodes = nodeCount;
-            } else if (currLen == maxLength) {
-                minNodes = min(minNodes, nodeCount);
+
+        int maxLen = 0, minNodes = 1;
+        unordered_set<int> seen;
+        function<void(int, int, int, int)> dfs = [&](int node, int parent, int curLen, int curNodes) {
+            if (seen.count(nums[node])) return;
+            seen.insert(nums[node]);
+            if (curLen > maxLen) {
+                maxLen = curLen;
+                minNodes = curNodes;
+            } else if (curLen == maxLen) {
+                minNodes = min(minNodes, curNodes);
             }
-            for (auto& [nei, w] : tree[node]) {
-                if (nei == parent) continue;
-                if (valueCount[nums[nei]] == 0) {
-                    dfs(nei, node, currLen + w, nodeCount + 1);
+            for (auto& [nei, l] : tree[node]) {
+                if (nei != parent) {
+                    dfs(nei, node, curLen + l, curNodes + 1);
                 }
             }
-            valueCount[nums[node]]--;
+            seen.erase(nums[node]);
         };
-        dfs(0, -1, 0, 1);
-        return {maxLength, minNodes};
+        for (int i = 0; i < n; ++i) {
+            seen.clear();
+            dfs(i, -1, 0, 1);
+        }
+        return {maxLen, minNodes};
     }
 };
 # @lc code=end
