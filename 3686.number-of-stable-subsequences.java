c3c42c8b16d66e611bc2f9b9a9a3ca9764058830
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 05:42:35 +0800

    Batch add 1 solutions: 3686.number-of-stabl...

--- 3686.number-of-stable-subsequences.java
+++ 3686.number-of-stable-subsequences.java
@@ -3,25 +3,51 @@
 #
 # [3686] Number of Stable Subsequences
 #
-
 # @lc code=start
 class Solution {
     public int countStableSubsequences(int[] nums) {
-        final int MOD = 1000000007;
         int n = nums.length;
-        long evenEnd = 0, oddEnd = 0;
-        
-        for (int num : nums) {
-            if (num % 2 == 0) { // num is even
-                long newEvenCount = (evenEnd + 1) % MOD; // Count new sequences ending with this even
-                evenEnd = (evenEnd + newEvenCount) % MOD; // Update sequences ending in even numbers
-            } else { // num is odd
-                long newOddCount = (oddEnd + 1) % MOD; // Count new sequences ending with this odd
-                oddEnd = (oddEnd + newOddCount) % MOD; // Update sequences ending in odd numbers
+        int MOD = 1000000007;
+        // dp[i][c][p]: number of stable subsequences using first i elements,
+        // ending with c consecutive p parity elements (c=0: no last elem, 1 or 2), p=0: even, 1: odd
+        // To optimize, we only need 2 dp arrays (prev, curr), since state at i only depends on i-1
+        int[][][] dp = new int[2][3][2];
+        dp[0][0][0] = 1; // empty subsequence, parity doesn't matter
+        dp[0][0][1] = 1;
+        for(int i = 0; i < n; ++i) {
+            int curr = (i+1)%2, prev = i%2;
+            // reset curr layer
+            for(int c=0; c<=2; ++c) for(int p=0; p<=1; ++p) dp[curr][c][p]=0;
+            int parity = nums[i]%2;
+            // For all previous states
+            for(int c=0; c<=2; ++c) {
+                for(int p=0; p<=1; ++p) {
+                    int val = dp[prev][c][p];
+                    if(val==0) continue;
+                    // Option 1: skip nums[i] (don't take)
+                    dp[curr][c][p] = (dp[curr][c][p] + val)%MOD;
+                    // Option 2: take nums[i]
+                    if(c==0) {
+                        // Start a new subsequence
+                        dp[curr][1][parity] = (dp[curr][1][parity] + val)%MOD;
+                    } else {
+                        if(parity==p) {
+                            if(c<2) {
+                                dp[curr][c+1][p] = (dp[curr][c+1][p] + val)%MOD;
+                            } // else: would make three consecutive, skip
+                        } else {
+                            // reset count
+                            dp[curr][1][parity] = (dp[curr][1][parity] + val)%MOD;
+                        }
+                    }
+                }
             }
         }
-        
-        return (int)((evenEnd + oddEnd) % MOD); // Total stable subsequences modulo MOD
+        int ans = 0;
+        int last = n%2;
+        // Sum all non-empty subsequences
+        for(int c=1; c<=2; ++c) for(int p=0; p<=1; ++p) ans = (ans + dp[last][c][p])%MOD;
+        return ans;
     }
 }
 # @lc code=end
