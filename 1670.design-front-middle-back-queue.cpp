63ca4c72440a6fdf68eaeac837a74658032bc5ef
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 15:41:23 +0800

    Batch add 1 solutions: 1670.design-front-mi...

--- 1670.design-front-middle-back-queue.cpp
+++ 1670.design-front-middle-back-queue.cpp
@@ -5,78 +5,87 @@
 #
 # @lc code=start
 #include <deque>
+
 class FrontMiddleBackQueue {
-public:
     std::deque<int> left, right;
-    FrontMiddleBackQueue() { }
-    void rebalance() {
-        while (left.size() > right.size() + 1) {
+
+    // Invariant: right.size() == left.size() OR right.size() == left.size() + 1
+    void balance() {
+        if (left.size() > right.size()) {
             right.push_front(left.back());
             left.pop_back();
-        }
-        while (left.size() < right.size()) {
+        } else if (right.size() > left.size() + 1) {
             left.push_back(right.front());
             right.pop_front();
         }
     }
+
+public:
+    FrontMiddleBackQueue() {}
+
     void pushFront(int val) {
         left.push_front(val);
-        rebalance();
+        balance();
     }
+
     void pushMiddle(int val) {
-        if (left.size() > right.size()) {
-            right.push_front(left.back());
-            left.pop_back();
+        if (left.size() == right.size()) {
+            right.push_front(val);
+        } else {
+            left.push_back(val);
         }
-        left.push_back(val);
     }
+
     void pushBack(int val) {
         right.push_back(val);
-        rebalance();
+        balance();
     }
+
     int popFront() {
-        if (left.empty() && right.empty()) return -1;
-        int res;
-        if (!left.empty()) {
-            res = left.front();
-            left.pop_front();
+        if (right.empty()) return -1;
+        int val;
+        if (left.empty()) {
+            val = right.front();
+            right.pop_front();
         } else {
-            res = right.front();
+            val = left.front();
+            left.pop_front();
+        }
+        balance();
+        return val;
+    }
+
+    int popMiddle() {
+        if (right.empty()) return -1;
+        int val;
+        if (left.size() == right.size()) {
+            val = left.back();
+            left.pop_back();
+        } else {
+            val = right.front();
             right.pop_front();
         }
-        rebalance();
-        return res;
+        // No balance() needed here as the logic maintains the size constraint directly
+        return val;
     }
-    int popMiddle() {
-        if (left.empty() && right.empty()) return -1;
-        int res = left.back();
-        left.pop_back();
-        rebalance();
-        return res;
-    }
+
     int popBack() {
-        if (left.empty() && right.empty()) return -1;
-        int res;
-        if (!right.empty()) {
-            res = right.back();
-            right.pop_back();
-        } else {
-            res = left.back();
-            left.pop_back();
-        }
-        rebalance();
-        return res;
+        if (right.empty()) return -1;
+        int val = right.back();
+        right.pop_back();
+        balance();
+        return val;
     }
 };
 
 /**
-* Your FrontMiddleBackQueue object will be instantiated and called as such:
-* FrontMiddleBackQueue* obj = new FrontMiddleBackQueue();
-* obj->pushFront(val);
-* obj->pushMiddle(val);
-* obj->pushBack(val);
-* int param_4 = obj->popFront();
-* int param_5 = obj->popMiddle();
-* int param_6 = obj->popBack();
-*/
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * FrontMiddleBackQueue* obj = new FrontMiddleBackQueue();
+ * obj->pushFront(val);
+ * obj->pushMiddle(val);
+ * obj->pushBack(val);
+ * int param_4 = obj->popFront();
+ * int param_5 = obj->popMiddle();
+ * int param_6 = obj->popBack();
+ */
 # @lc code=end
