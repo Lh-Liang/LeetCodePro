d705d831f5703679ba12bff25ce53aa3f9e147ac
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-05 15:02:03 +0800

    Batch add 1 solutions: 3743.maximize-cyclic...

--- 3743.maximize-cyclic-partition-score.cpp
+++ 3743.maximize-cyclic-partition-score.cpp
@@ -1,74 +1,46 @@
-//
-// @lc app=leetcode id=3743 lang=cpp
-//
-// [3743] Maximize Cyclic Partition Score
-//
+#
+# @lc app=leetcode id=3743 lang=cpp
+#
+# [3743] Maximize Cyclic Partition Score
+#
 
-// @lc code=start
+# @lc code=start
 class Solution {
 public:
-    using ll = long long;
-    int n;
-    vector<vector<int>> min_st, max_st;
-    vector<int> log2s;
-
-    void buildSparseTable(const vector<int>& arr) {
-        int N = arr.size();
-        int max_log = 32 - __builtin_clz(N);
-        min_st.assign(N, vector<int>(max_log));
-        max_st.assign(N, vector<int>(max_log));
-        log2s.assign(N + 1, 0);
-        for (int i = 2; i <= N; ++i) log2s[i] = log2s[i / 2] + 1;
-        for (int i = 0; i < N; ++i) {
-            min_st[i][0] = arr[i];
-            max_st[i][0] = arr[i];
-        }
-        for (int j = 1; (1 << j) <= N; ++j) {
-            for (int i = 0; i + (1 << j) <= N; ++i) {
-                min_st[i][j] = min(min_st[i][j-1], min_st[i+(1<<(j-1))][j-1]);
-                max_st[i][j] = max(max_st[i][j-1], max_st[i+(1<<(j-1))][j-1]);
-            }
-        }
-    }
-    int query_min(int l, int r) { // [l, r]
-        int len = r - l + 1;
-        int j = log2s[len];
-        return min(min_st[l][j], min_st[r-(1<<j)+1][j]);
-    }
-    int query_max(int l, int r) {
-        int len = r - l + 1;
-        int j = log2s[len];
-        return max(max_st[l][j], max_st[r-(1<<j)+1][j]);
-    }
-    ll dp_solve(int start, int k, int rem_len, const vector<int>& arr, vector<vector<vector<ll>>>& dp) {
-        if (k == 1) {
-            int mi = query_min(start, start+rem_len-1);
-            int ma = query_max(start, start+rem_len-1);
-            return ma - mi;
-        }
-        if (dp[start][k][rem_len] != -1) return dp[start][k][rem_len];
-        ll ans = 0;
-        for (int len = 1; len <= rem_len - (k - 1); ++len) {
-            int l = start, r = start + len - 1;
-            int mi = query_min(l, r);
-            int ma = query_max(l, r);
-            ll score = ma - mi + dp_solve((r+1), k-1, rem_len-len, arr, dp);
-            ans = max(ans, score);
-        }
-        return dp[start][k][rem_len] = ans;
-    }
     long long maximumScore(vector<int>& nums, int k) {
-        n = nums.size();
+        int n = nums.size();
         vector<int> arr(nums);
         arr.insert(arr.end(), nums.begin(), nums.end()); // duplicate for cyclic
-        buildSparseTable(arr);
-        ll res = 0;
-        for (int start = 0; start < n; ++start) {
-            // Use a fresh DP table for each rotation to ensure state isolation
-            vector<vector<vector<ll>>> dp(2*n+1, vector<vector<ll>>(k+2, vector<ll>(n+1, -1)));
-            res = max(res, dp_solve(start, k, n, arr, dp));
+        // Precompute range for all intervals of length up to n
+        vector<vector<int>> maxv(2 * n, vector<int>(n + 1, INT_MIN));
+        vector<vector<int>> minv(2 * n, vector<int>(n + 1, INT_MAX));
+        for (int i = 0; i < 2 * n; ++i) {
+            int mx = INT_MIN, mn = INT_MAX;
+            for (int l = 1; l <= n && i + l - 1 < 2 * n; ++l) {
+                mx = max(mx, arr[i + l - 1]);
+                mn = min(mn, arr[i + l - 1]);
+                maxv[i][l] = mx;
+                minv[i][l] = mn;
+            }
         }
-        return res;
+        long long ans = 0;
+        for (int st = 0; st < n; ++st) {
+            vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, LLONG_MIN));
+            dp[0][0] = 0;
+            for (int i = 1; i <= n; ++i) {
+                for (int p = 1; p <= k; ++p) {
+                    for (int t = 1; t <= i; ++t) {
+                        int l = i - t + 1;
+                        long long range = maxv[st + l - 1][t] - minv[st + l - 1][t];
+                        if (dp[i - t][p - 1] != LLONG_MIN)
+                            dp[i][p] = max(dp[i][p], dp[i - t][p - 1] + range);
+                    }
+                }
+            }
+            for (int p = 1; p <= k; ++p)
+                ans = max(ans, dp[n][p]);
+        }
+        return ans;
     }
 };
-// @lc code=end
+# @lc code=end
