e7a4347e439a6b998106265644c1c40b52f6407c
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 14:30:08 +0800

    Batch add 1 solutions: 3725.count-ways-to-c...

--- 3725.count-ways-to-choose-coprime-integers-from-rows.java
+++ 3725.count-ways-to-choose-coprime-integers-from-rows.java
@@ -1,28 +1,38 @@
+#
+# @lc app=leetcode id=3725 lang=java
+#
+# [3725] Count Ways to Choose Coprime Integers from Rows
+#
+
+# @lc code=start
+import java.util.*;
 class Solution {
     public int countCoprime(int[][] mat) {
         int MOD = 1_000_000_007;
-        int m = mat.length;
-        java.util.Map<Integer, Integer> dp = new java.util.HashMap<>();
-        for (int x : mat[0]) dp.put(x, dp.getOrDefault(x, 0) + 1);
+        int m = mat.length, n = mat[0].length;
+        Map<Integer, Integer> dp = new HashMap<>();
+        // Initialize DP with first row
+        for (int val : mat[0]) {
+            dp.put(val, dp.getOrDefault(val, 0) + 1);
+        }
+        // Process each row
         for (int i = 1; i < m; ++i) {
-            java.util.Map<Integer, Integer> ndp = new java.util.HashMap<>();
-            for (java.util.Map.Entry<Integer, Integer> e : dp.entrySet()) {
-                int prev = e.getKey(), cnt = e.getValue();
-                for (int x : mat[i]) {
-                    int g = gcd(prev, x);
-                    ndp.put(g, (int)(((long)ndp.getOrDefault(g, 0) + cnt) % MOD));
+            Map<Integer, Integer> next = new HashMap<>();
+            for (int val : mat[i]) {
+                for (Map.Entry<Integer, Integer> entry : dp.entrySet()) {
+                    int newGcd = gcd(val, entry.getKey());
+                    next.put(newGcd, (int)(((long)next.getOrDefault(newGcd, 0) + entry.getValue()) % MOD));
                 }
+                // Also start new sequences from this row
+                next.put(val, (int)(((long)next.getOrDefault(val, 0) + 1) % MOD));
             }
-            dp = ndp;
+            dp = next;
         }
         return dp.getOrDefault(1, 0);
     }
     private int gcd(int a, int b) {
-        while (b != 0) {
-            int t = b;
-            b = a % b;
-            a = t;
-        }
+        while (b != 0) { int t = b; b = a % b; a = t; }
         return a;
     }
 }
+# @lc code=end
