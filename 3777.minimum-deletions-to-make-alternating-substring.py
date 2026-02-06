99d2506eaeab50d0ddbba5a304fd4251aeee5df4
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 06:21:28 +0800

    Batch add 1 solutions: 3777.minimum-deletio...

--- 3777.minimum-deletions-to-make-alternating-substring.py
+++ 3777.minimum-deletions-to-make-alternating-substring.py
@@ -7,30 +7,29 @@
 # @lc code=start
 class Solution:
     def minDeletions(self, s: str, queries: List[List[int]]) -> List[int]:
-        n = len(s)
         result = []
-        s = list(s)  # To allow mutation of the string
+        s = list(s)  # Convert string to list for mutability
         
-        def min_deletions_to_alternate(l, r):
-            # Calculate minimum deletions to make s[l:r+1] alternating
-            del_count_1 = del_count_2 = 0
+        def flip(index):
+            s[index] = 'B' if s[index] == 'A' else 'A'
+            
+        def calculate_min_deletions(l, r):
+            deletions1 = deletions2 = 0
+            expected1 = 'A'  # Start pattern 'ABAB...'
+            expected2 = 'B'  # Start pattern 'BABA...'
             for i in range(l, r + 1):
-                expected_char_1 = 'A' if (i - l) % 2 == 0 else 'B'
-                expected_char_2 = 'B' if (i - l) % 2 == 0 else 'A'
-                if s[i] != expected_char_1:
-                    del_count_1 += 1
-                if s[i] != expected_char_2:
-                    del_count_2 += 1
-            return min(del_count_1, del_count_2)
+                if s[i] != expected1:
+                    deletions1 += 1
+                if s[i] != expected2:
+                    deletions2 += 1
+                expected1 = 'B' if expected1 == 'A' else 'A'
+                expected2 = 'B' if expected2 == 'A' else 'A'
+            return min(deletions1, deletions2)
         
         for query in queries:
             if query[0] == 1:
-                _, j = query
-                # Flip character at index j
-                s[j] = 'B' if s[j] == 'A' else 'A'
+                flip(query[1])
             elif query[0] == 2:
-                _, l, r = query
-                # Calculate minimum deletions for substring s[l:r+1]
-                result.append(min_deletions_to_alternate(l, r))
+                result.append(calculate_min_deletions(query[1], query[2]))
         return result
 # @lc code=end
