9c3d4da741e37a3363f9130a69acf9d4719c1b78
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 22:29:26 +0800

    Batch add 1 solutions: 3785.minimum-swaps-t...

--- 3785.minimum-swaps-to-avoid-forbidden-values.py
+++ 3785.minimum-swaps-to-avoid-forbidden-values.py
@@ -1,14 +1,22 @@
+#
+# @lc app=leetcode id=3785 lang=python3
+#
+# [3785] Minimum Swaps to Avoid Forbidden Values
+#
+
+# @lc code=start
 class Solution:
     def minSwaps(self, nums: List[int], forbidden: List[int]) -> int:
         n = len(nums)
-        swap_count = 0
+        swaps = 0
         for i in range(n):
             if nums[i] == forbidden[i]:
-                for j in range(n):
-                    if nums[j] != forbidden[i]:
+                for j in range(i + 1, n):
+                    if nums[j] != forbidden[j] and nums[j] != forbidden[i]:
                         nums[i], nums[j] = nums[j], nums[i]
-                        swap_count += 1
+                        swaps += 1
                         break
                 else:
-                    return -1  # No valid swap found
-        return swap_count
+                    return -1 # No valid swap found for position i
+        return swaps # Return total number of swaps made.
+# @lc code=end
