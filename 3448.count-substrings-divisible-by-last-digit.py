e26529e91b333ed3b75b8a71fc33bb70132bcb0f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 01:45:54 +0800

    Batch add 1 solutions: 3448.count-substring...

--- 3448.count-substrings-divisible-by-last-digit.py
+++ 3448.count-substrings-divisible-by-last-digit.py
@@ -1,25 +1,23 @@
-#
+# 
 # @lc app=leetcode id=3448 lang=python3
-#
+# 
 # [3448] Count Substrings Divisible By Last Digit
-#
+# 
 
 # @lc code=start
 class Solution:
     def countSubstrings(self, s: str) -> int:
         count = 0
         n = len(s)
-        # Iterate over all possible end indices of substrings
-        for end in range(n):
-            last_digit = int(s[end])
+        for i in range(n):
+            last_digit = int(s[i])
             if last_digit == 0:
-                continue  # Skip if last digit is zero, as no divisibility possible
-            # Initialize current number starting from s[end] as a single digit number
+                continue
             num = 0
-            # Extend the substring backwards checking divisibility by last_digit
-            for start in range(end, -1, -1):
-                num = num + int(s[start]) * (10 ** (end - start))  # Update number ending at 'end' starting from 'start'
+            count += 1
+            for j in range(i - 1, -1, -1):
+                num = num * 10 + int(s[j])
                 if num % last_digit == 0:
-                    count += 1  # If divisible, increment count
-        return count  # Return total count of such substrings
+                    count += 1
+        return count
 # @lc code=end
