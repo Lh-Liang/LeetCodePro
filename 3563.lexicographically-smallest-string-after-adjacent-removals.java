d6ac8098564162d81291382e163ce65639dc7b34
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 17:48:09 +0800

    Batch add 1 solutions: 3563.lexicographical...

--- 3563.lexicographically-smallest-string-after-adjacent-removals.java
+++ 3563.lexicographically-smallest-string-after-adjacent-removals.java
@@ -4,34 +4,20 @@
 # [3563] Lexicographically Smallest String After Adjacent Removals
 #
 # @lc code=start
-import java.util.*;
-
 class Solution {
     public String lexicographicallySmallestString(String s) {
-        Map<String, String> memo = new HashMap<>();
-        return dfs(s, memo);
-    }
-    
-    private String dfs(String s, Map<String, String> memo) {
-        if (s.length() <= 1) return s;
-        if (memo.containsKey(s)) return memo.get(s);
-        String minStr = s;
-        for (int i = 0; i < s.length() - 1; ++i) {
-            char a = s.charAt(i), b = s.charAt(i + 1);
-            if (isConsecutive(a, b)) {
-                String next = s.substring(0, i) + s.substring(i + 2);
-                String candidate = dfs(next, memo);
-                if (candidate.compareTo(minStr) < 0) {
-                    minStr = candidate;
-                }
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            while (!stack.isEmpty() && (stack.peek() == c + 1 || stack.peek() == c - 1 || (stack.peek() == 'a' && c == 'z') || (stack.peek() == 'z' && c == 'a'))) {
+                stack.pop();
             }
+            stack.push(c);
         }
-        memo.put(s, minStr);
-        return minStr;
-    }
-    
-    private boolean isConsecutive(char a, char b) {
-        return Math.abs(a - b) == 1 || (a == 'a' && b == 'z') || (a == 'z' && b == 'a');
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+        return result.toString();
     }
 }
 # @lc code=end
