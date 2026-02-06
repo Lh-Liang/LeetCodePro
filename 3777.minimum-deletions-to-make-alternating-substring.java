c7fc180e8f1cb4f2bae622eb2e8d2c7084cd5537
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 14:47:12 +0800

    Batch add 1 solutions: 3777.minimum-deletio...

--- 3777.minimum-deletions-to-make-alternating-substring.java
+++ 3777.minimum-deletions-to-make-alternating-substring.java
@@ -6,24 +6,37 @@
 # @lc code=start
 class Solution {
     public int[] minDeletions(String s, int[][] queries) {
+        int n = s.length();
         char[] arr = s.toCharArray();
-        List<Integer> answers = new ArrayList<>();
-        for (int[] query : queries) {
-            if (query[0] == 1) {
-                int idx = query[1];
-                arr[idx] = (arr[idx] == 'A') ? 'B' : 'A';
-            } else {
-                int l = query[1], r = query[2];
-                int minDel = 0;
-                for (int i = l + 1; i <= r; ++i) {
-                    if (arr[i] == arr[i - 1]) minDel++;
+        int[] answer = new int[queries.length];
+        // Build prefix sum of adjacent equals
+        int[] prefix = new int[n];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i-1] + (arr[i]==arr[i-1]?1:0);
+        }
+        for (int qi = 0, ai = 0; qi < queries.length; qi++) {
+            int[] q = queries[qi];
+            if (q[0] == 1) {
+                int j = q[1];
+                arr[j] = arr[j]=='A'?'B':'A';
+                // Only update prefix at j and j+1
+                if (j > 0) {
+                    prefix[j] = prefix[j-1] + (arr[j]==arr[j-1]?1:0);
                 }
-                answers.add(minDel);
+                for (int k = j+1; k < n && k <= j+1; k++) {
+                    prefix[k] = prefix[k-1] + (arr[k]==arr[k-1]?1:0);
+                }
+            } else {
+                int l = q[1], r = q[2];
+                answer[ai++] = prefix[r] - (l>0?prefix[l]:0);
             }
         }
-        int[] res = new int[answers.size()];
-        for (int i = 0; i < answers.size(); ++i) res[i] = answers.get(i);
-        return res;
+        // filter only type 2 answers
+        int[] result = new int[(int) java.util.Arrays.stream(queries).filter(q->q[0]==2).count()];
+        for (int i = 0, j = 0; i < queries.length; i++) {
+            if (queries[i][0]==2) result[j++] = answer[i];
+        }
+        return result;
     }
 }
 # @lc code=end
