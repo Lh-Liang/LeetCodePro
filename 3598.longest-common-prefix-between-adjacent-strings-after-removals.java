553e66e91f694b03f32e1003899d9fe59e603e74
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 14:08:51 +0800

    Batch add 1 solutions: 3598.longest-common-...

--- 3598.longest-common-prefix-between-adjacent-strings-after-removals.java
+++ 3598.longest-common-prefix-between-adjacent-strings-after-removals.java
@@ -1,9 +1,10 @@
-//
-// @lc app=leetcode id=3598 lang=java
-//
-// [3598] Longest Common Prefix Between Adjacent Strings After Removals
-//
-// @lc code=start
+#
+# @lc app=leetcode id=3598 lang=java
+#
+# [3598] Longest Common Prefix Between Adjacent Strings After Removals
+#
+
+# @lc code=start
 class Solution {
     private int commonPrefix(String a, String b) {
         int minLen = Math.min(a.length(), b.length());
@@ -13,53 +14,51 @@ class Solution {
         }
         return i;
     }
+
     public int[] longestCommonPrefix(String[] words) {
         int n = words.length;
-        int[] res = new int[n];
-        if (n < 2) {
-            for (int i = 0; i < n; i++) res[i] = 0;
-            return res;
-        }
+        if (n == 1) return new int[]{0};
         int[] prefix = new int[n - 1];
-        for (int i = 0; i < n - 1; ++i) {
+        for (int i = 0; i < n - 1; i++) {
             prefix[i] = commonPrefix(words[i], words[i+1]);
         }
-        for (int i = 0; i < n; ++i) {
-            int maxPref = 0;
+        int max = 0;
+        for (int p : prefix) max = Math.max(max, p);
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            int curMax;
             if (n == 2) {
-                // Only one pair exists, removing either leaves no pairs
+                // Only one pair, removing either leaves no pairs
                 res[i] = 0;
                 continue;
             }
             if (i == 0) {
-                // Remove first word, pairs are (words[1], words[2]), ..., (words[n-2], words[n-1])
-                for (int j = 1; j < n - 1; ++j) {
-                    maxPref = Math.max(maxPref, prefix[j]);
-                }
+                // Remove first; pairs from 1 to n-2
+                curMax = 0;
+                for (int j = 1; j < n - 1; j++) curMax = Math.max(curMax, prefix[j]);
             } else if (i == n - 1) {
-                // Remove last word, pairs are (words[0], words[1]), ..., (words[n-3], words[n-2])
-                for (int j = 0; j < n - 2; ++j) {
-                    maxPref = Math.max(maxPref, prefix[j]);
-                }
+                // Remove last; pairs from 0 to n-3
+                curMax = 0;
+                for (int j = 0; j < n - 2; j++) curMax = Math.max(curMax, prefix[j]);
             } else {
-                // Remove word at i, new adjacent pair is (words[i-1], words[i+1])
-                int newPrefix = commonPrefix(words[i-1], words[i+1]);
-                for (int j = 0; j < i - 1; ++j) {
-                    maxPref = Math.max(maxPref, prefix[j]);
-                }
-                maxPref = Math.max(maxPref, newPrefix);
-                for (int j = i + 1; j < n - 1; ++j) {
-                    maxPref = Math.max(maxPref, prefix[j]);
+                // Remove in the middle
+                int combined = commonPrefix(words[i-1], words[i+1]);
+                curMax = 0;
+                for (int j = 0; j < n-1; j++) {
+                    if (j == i-1) {
+                        // This pair is replaced
+                        curMax = Math.max(curMax, combined);
+                    } else if (j == i) {
+                        // This pair is removed
+                        continue;
+                    } else {
+                        curMax = Math.max(curMax, prefix[j]);
+                    }
                 }
             }
-            res[i] = maxPref;
-        }
-        // Final check to guarantee output is correct length and values
-        if (res.length != n) throw new AssertionError("Result length mismatch.");
-        for (int val : res) {
-            if (val < 0) throw new AssertionError("Negative prefix length detected.");
+            res[i] = curMax;
         }
         return res;
     }
 }
-// @lc code=end
+# @lc code=end
