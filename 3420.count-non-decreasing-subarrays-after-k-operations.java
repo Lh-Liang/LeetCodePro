6590930b9c6d5140fbd4ac9e39047f255cdf0057
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 16:54:32 +0800

    Batch add 1 solutions: 3420.count-non-decre...

--- 3420.count-non-decreasing-subarrays-after-k-operations.java
+++ 3420.count-non-decreasing-subarrays-after-k-operations.java
@@ -1,27 +1,20 @@
-#
-# @lc app=leetcode id=3420 lang=java
-#
-# [3420] Count Non-Decreasing Subarrays After K Operations
-#
-# @lc code=start
 class Solution {
     public long countNonDecreasingSubarrays(int[] nums, int k) {
         int n = nums.length;
-        long result = 0;
-        for (int i = 0; i < n; ++i) {
-            long ops = 0;
-            int prev = nums[i];
-            result++; // single element subarray is always valid
-            for (int j = i + 1; j < n; ++j) {
-                if (nums[j] < prev) {
-                    ops += (prev - nums[j]);
+        long count = 0;
+        for (int start = 0; start < n; start++) {
+            int operations = 0;
+            for (int end = start; end < n; end++) {
+                if (end > start && nums[end] < nums[end - 1]) {
+                    operations += nums[end - 1] - nums[end];
                 }
-                if (ops > k) break;
-                prev = Math.max(prev, nums[j]);
-                result++;
+                if (operations <= k) {
+                    count++;
+                } else {
+                    break; // No further subarray starting from 'start' can be valid
+                }
             }
         }
-        return result;
+        return count;
     }
 }
-# @lc code=end
