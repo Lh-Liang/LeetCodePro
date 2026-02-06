a01991c7b27fc8abb913a7b5f22c35e96b670d24
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 18:32:20 +0800

    Batch add 1 solutions: 3771.total-score-of-...

--- 3771.total-score-of-dungeon-runs.java
+++ 3771.total-score-of-dungeon-runs.java
@@ -3,30 +3,23 @@
 #
 # [3771] Total Score of Dungeon Runs
 #
-
 # @lc code=start
 class Solution {
     public long totalScore(int hp, int[] damage, int[] requirement) {
+        long total_score = 0;
         int n = damage.length;
-        // prefix sum of damage
-        long[] prefix = new long[n + 1];
         for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + damage[i];
-        }
-        long total = 0;
-        // For each start index
-        for (int start = 0; start < n; start++) {
-            long curHP = hp;
+            int current_hp = hp;
             int score = 0;
-            for (int i = start; i < n; i++) {
-                curHP -= damage[i];
-                if (curHP >= requirement[i]) {
+            for (int j = i; j < n; j++) {
+                current_hp -= damage[j];
+                if (current_hp >= requirement[j]) {
                     score++;
                 }
             }
-            total += score;
+            total_score += score;
         }
-        return total;
+        return total_score;
     }
 }
 # @lc code=end
