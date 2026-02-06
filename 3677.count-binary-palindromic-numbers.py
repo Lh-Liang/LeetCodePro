0ef2f13035024c0ac2411ff2e02f77818a78f10b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 03:34:55 +0800

    Batch add 1 solutions: 3677.count-binary-pa...

--- 3677.count-binary-palindromic-numbers.py
+++ 3677.count-binary-palindromic-numbers.py
@@ -7,11 +7,25 @@
 # @lc code=start
 class Solution:
     def countBinaryPalindromes(self, n: int) -> int:
-        def is_palindrome(s: str) -> bool:
-            return s == s[::-1]
+        def generate_palindromes(length):
+            """Generate all binary palindromes of a given length."""
+            if length == 1: # single digit palindrome is trivial
+                return ['0', '1']
+            half_len = (length + 1) // 2 
+            palindromes = []
+            for i in range(1 << (half_len - 1), 1 << half_len): # avoid leading zeroes
+                half = bin(i)[2:] # convert to binary and remove '0b'
+                if length % 2 == 0:
+                    palindromes.append(half + half[::-1])
+                else:
+                    palindromes.append(half + half[-2::-1])
+            return palindromes
+        
         count = 0
-        for k in range(n + 1):
-            if is_palindrome(bin(k)[2:]):
-                count += 1
+        max_length = len(bin(n)) - 2 # Get bit length of n without '0b'
+        for bit_length in range(1, max_length + 1):
+            for palindrome in generate_palindromes(bit_length):
+                if int(palindrome, 2) <= n:
+                    count += 1
         return count
 # @lc code=end
