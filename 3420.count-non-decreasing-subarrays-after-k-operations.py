8927976528667a8a5fe5d4e842f5ac764423b160
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 01:29:25 +0800

    Batch add 1 solutions: 3420.count-non-decre...

--- 3420.count-non-decreasing-subarrays-after-k-operations.py
+++ 3420.count-non-decreasing-subarrays-after-k-operations.py
@@ -5,19 +5,22 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def countNonDecreasingSubarrays(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        result = 0
-        left = 0
-        needed_operations = 0
-        for right in range(1, n):
-            if nums[right] < nums[right - 1]:
-                needed_operations += nums[right - 1] - nums[right]
-            while needed_operations > k:
-                if nums[left + 1] < nums[left]:
-                    needed_operations -= nums[left] - nums[left + 1]
-                left += 1
-            result += right - left + 1
-        return result
-# @lc code=end
+        total_subarrays = n * (n + 1) // 2
+        start = 0
+        used_operations = 0
+        valid_count = 0
+
+        for end in range(n):
+            if end > 0 and nums[end] < nums[end - 1]:
+                used_operations += nums[end - 1] - nums[end]
+            while used_operations > k:
+                if start < end and nums[start] > nums[start + 1]:
+                    used_operations -= nums[start] - nums[start + 1]
+                start += 1
+            valid_count += (end - start + 1)
+        return valid_count # @lc code=end
