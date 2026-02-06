b9229ac77faa4b3373bb2961485c50f4716be667
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 16:50:16 +0800

    Batch add 1 solutions: 3399.smallest-substr...

--- 3399.smallest-substring-with-identical-characters-ii.java
+++ 3399.smallest-substring-with-identical-characters-ii.java
@@ -3,49 +3,25 @@
 #
 # [3399] Smallest Substring With Identical Characters II
 #
-
 # @lc code=start
 class Solution {
     public int minLength(String s, int numOps) {
         int n = s.length();
-        int left = 1, right = n;
-        int answer = n;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canAchieve(s, numOps, mid)) {
-                answer = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = (s.charAt(0) == '1') ? 1 : 0;
+        right[n - 1] = (s.charAt(n - 1) == '1') ? 1 : 0;
+        for (int i = 1; i < n; i++) {
+            left[i] = (s.charAt(i) == '1') ? left[i - 1] + 1 : 0;
         }
-        return answer;
-    }
-    // Check if we can make all runs of identical chars of length <= len with numOps flips
-    private boolean canAchieve(String s, int numOps, int len) {
-        // Try both: making all substrings at most len by flipping to '0' or '1'
-        return minFlips(s, '0', len) <= numOps || minFlips(s, '1', len) <= numOps;
-    }
-    // For given char c, compute minimal flips to make all substrings of c of length <= len
-    private int minFlips(String s, char c, int len) {
-        int n = s.length();
-        int flips = 0;
-        int i = 0;
-        while (i < n) {
-            if (s.charAt(i) != c) {
-                i++;
-                continue;
-            }
-            int j = i;
-            while (j < n && s.charAt(j) == c) j++;
-            int runLen = j - i;
-            if (runLen > len) {
-                // Need to flip at least (runLen-1)/len substrings inside this run
-                flips += (runLen - 1) / len;
-            }
-            i = j;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = (s.charAt(i) == '1') ? right[i + 1] + 1 : 0;
         }
-        return flips;
-    }
+        int maxOnesBlock = 0;
+        for (int i = 0; i < n; i++) {
+            maxOnesBlock = Math.max(maxOnesBlock, Math.max(left[i], right[i]));
+        }
+        return Math.max(maxOnesBlock - numOps, 1); 
+    } 
 }
 # @lc code=end
