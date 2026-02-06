7ee2ddfc459b89d9d5dbb3c31a5463ea66358fb4
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 21:12:33 +0800

    Batch add 1 solutions: 3470.permutations-iv...

--- 3470.permutations-iv.py
+++ 3470.permutations-iv.py
@@ -3,9 +3,22 @@
 #
 # [3470] Permutations IV
 #
+
 # @lc code=start
+from typing import List
+import itertools
+
 class Solution:
     def permute(self, n: int, k: int) -> List[int]:
-        # Implement an efficient backtracking method here
-        return []  # Placeholder
+        def is_alternating(perm):
+            return all((perm[i] % 2) != (perm[i+1] % 2) for i in range(len(perm)-1))
+        
+        nums = list(range(1, n + 1))
+        alternating_perms = [list(perm) for perm in itertools.permutations(nums) if is_alternating(perm)]
+        alternating_perms.sort() # Sort lexicographically
+        
+        if k <= len(alternating_perms):
+            return alternating_perms[k-1]
+        else:
+            return []
 # @lc code=end
