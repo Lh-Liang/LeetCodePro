3770c05f0bc4524597826e6e2ce9ab1a96a1c17a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 13:09:02 +0800

    Batch add 1 solutions: 3327.check-if-dfs-st...

--- 3327.check-if-dfs-strings-are-palindromes.java
+++ 3327.check-if-dfs-strings-are-palindromes.java
@@ -1,30 +1,51 @@
+#
+# @lc app=leetcode id=3327 lang=java
+#
+# [3327] Check if DFS Strings Are Palindromes
+#
+# @lc code=start
 class Solution {
     public boolean[] findAnswer(int[] parent, String s) {
         int n = parent.length;
         List<Integer>[] tree = new List[n];
         for (int i = 0; i < n; ++i) tree[i] = new ArrayList<>();
-        for (int i = 1; i < n; ++i) tree[parent[i]].add(i);
+        for (int i = 1; i < n; ++i) {
+            tree[parent[i]].add(i);
+        }
+        // Sort children for each node to enforce increasing order
+        for (int i = 0; i < n; ++i) Collections.sort(tree[i]);
+        
+        // Cache postorder traversals
+        List<Integer>[] postorder = new List[n];
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; ++i) {
+            postorder[i] = new ArrayList<>();
+            dfs(i, tree, postorder[i], visited);
+            Arrays.fill(visited, false); // Reset for each node
+        }
         boolean[] answer = new boolean[n];
         for (int i = 0; i < n; ++i) {
-            StringBuilder sb = new StringBuilder();
-            dfs(i, s, tree, sb);
-            answer[i] = isPalindrome(sb);
+            List<Integer> post = postorder[i];
+            int l = 0, r = post.size() - 1;
+            boolean isPalin = true;
+            while (l < r) {
+                if (s.charAt(post.get(l)) != s.charAt(post.get(r))) {
+                    isPalin = false;
+                    break;
+                }
+                l++; r--;
+            }
+            answer[i] = isPalin;
         }
         return answer;
     }
-    private void dfs(int node, String s, List<Integer>[] tree, StringBuilder sb) {
-        List<Integer> children = tree[node];
-        Collections.sort(children);
-        for (int child : children) {
-            dfs(child, s, tree, sb);
+    private void dfs(int x, List<Integer>[] tree, List<Integer> res, boolean[] visited) {
+        if (visited[x]) return;
+        visited[x] = true;
+        for (int y : tree[x]) {
+            dfs(y, tree, res, visited);
         }
-        sb.append(s.charAt(node));
-    }
-    private boolean isPalindrome(StringBuilder sb) {
-        int l = 0, r = sb.length() - 1;
-        while (l < r) {
-            if (sb.charAt(l++) != sb.charAt(r--)) return false;
-        }
-        return true;
+        res.add(x);
     }
 }
+# @lc code=end
