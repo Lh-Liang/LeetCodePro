a85994812c7246e813f304620b6257c4bcb1324b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 20:41:37 +0800

    Batch add 1 solutions: 3382.maximum-area-re...

--- 3382.maximum-area-rectangle-with-point-constraints-ii.py
+++ 3382.maximum-area-rectangle-with-point-constraints-ii.py
@@ -7,13 +7,12 @@
 # @lc code=start
 class Solution:
     def maxRectangleArea(self, xCoord: List[int], yCoord: List[int]) -> int:
-        points = set(zip(xCoord, yCoord))
+        point_set = set(zip(xCoord, yCoord))
         max_area = -1
-        n = len(xCoord)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if xCoord[i] != xCoord[j] and yCoord[i] != yCoord[j]:  # Ensure different x and y to form diagonal
-                    if (xCoord[i], yCoord[j]) in points and (xCoord[j], yCoord[i]) in points:
+        for i in range(len(xCoord)):
+            for j in range(i + 1, len(xCoord)):
+                if xCoord[i] != xCoord[j] and yCoord[i] != yCoord[j]:
+                    if ((xCoord[i], yCoord[j]) in point_set) and ((xCoord[j], yCoord[i]) in point_set):
                         area = abs(xCoord[i] - xCoord[j]) * abs(yCoord[i] - yCoord[j])
                         max_area = max(max_area, area)
         return max_area if max_area != -1 else -1
