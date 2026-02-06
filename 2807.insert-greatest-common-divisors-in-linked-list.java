10018610b5019670105118f034cdabf17d71d592
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 16:31:31 +0800

    Batch add 1 solutions: 2807.insert-greatest...

--- 2807.insert-greatest-common-divisors-in-linked-list.java
+++ 2807.insert-greatest-common-divisors-in-linked-list.java
@@ -3,7 +3,6 @@
 #
 # [2807] Insert Greatest Common Divisors in Linked List
 #
-
 # @lc code=start
 /**
 * Definition for singly-linked list.
@@ -15,26 +14,23 @@
 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 * }
 */
+import java.util.*; 
 class Solution {
-    public ListNode insertGreatestCommonDivisors(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode curr = head;
-        while (curr != null && curr.next != null) {
-            int gcdVal = gcd(curr.val, curr.next.val);
-            ListNode gcdNode = new ListNode(gcdVal);
-            gcdNode.next = curr.next;
-            curr.next = gcdNode;
-            curr = gcdNode.next;
-        }
-        return head;
-    }
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
+    public ListNode insertGreatestCommonDivisors(ListNode head) { 
+        if (head == null || head.next == null) return head; 
+        ListNode current = head; 
+        while (current != null && current.next != null) { 
+            int gcdValue = gcd(current.val, current.next.val); 
+            ListNode gcdNode = new ListNode(gcdValue); 
+            gcdNode.next = current.next; 
+            current.next = gcdNode; 
+            current = gcdNode.next; 
+        } 
+        return head; 
+    } 
+    private int gcd(int a, int b) { 
+        if (b == 0) return a; 
+        return gcd(b, a % b); 
+    } 
 }
 # @lc code=end
