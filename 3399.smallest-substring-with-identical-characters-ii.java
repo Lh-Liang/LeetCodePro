fb8da78a2bb5177d89bb9ad03d34effab46f50f4
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 13:16:45 +0800

    Batch add 1 solutions: 3399.smallest-substr...

--- 3399.smallest-substring-with-identical-characters-ii.java
+++ 3399.smallest-substring-with-identical-characters-ii.java
@@ -1,38 +1,51 @@
-//
-// @lc app=leetcode id=3399 lang=java
-//
-// [3399] Smallest Substring With Identical Characters II
-//
-// @lc code=start
+#
+# @lc app=leetcode id=3399 lang=java
+#
+# [3399] Smallest Substring With Identical Characters II
+#
+
+# @lc code=start
 class Solution {
     public int minLength(String s, int numOps) {
         int n = s.length();
         int left = 1, right = n;
-        while (left < right) {
+        int answer = n;
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (isPossible(s, numOps, mid)) {
-                right = mid;
+            if (canAchieve(s, numOps, mid)) {
+                answer = mid;
+                right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return left;
+        return answer;
     }
-    // Check if all runs can be broken into pieces of length <= len with numOps flips
-    private boolean isPossible(String s, int numOps, int len) {
+    // Check if we can make all runs of identical chars of length <= len with numOps flips
+    private boolean canAchieve(String s, int numOps, int len) {
+        // Try both: making all substrings at most len by flipping to '0' or '1'
+        return minFlips(s, '0', len) <= numOps || minFlips(s, '1', len) <= numOps;
+    }
+    // For given char c, compute minimal flips to make all substrings of c of length <= len
+    private int minFlips(String s, char c, int len) {
+        int n = s.length();
         int flips = 0;
-        int i = 0, n = s.length();
+        int i = 0;
         while (i < n) {
-            char ch = s.charAt(i);
+            if (s.charAt(i) != c) {
+                i++;
+                continue;
+            }
             int j = i;
-            while (j < n && s.charAt(j) == ch) j++;
+            while (j < n && s.charAt(j) == c) j++;
             int runLen = j - i;
             if (runLen > len) {
+                // Need to flip at least (runLen-1)/len substrings inside this run
                 flips += (runLen - 1) / len;
             }
             i = j;
         }
-        return flips <= numOps;
+        return flips;
     }
 }
-// @lc code=end
+# @lc code=end
