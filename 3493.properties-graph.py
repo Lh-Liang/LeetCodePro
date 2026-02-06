60a5b8a1e2a1660b827c5e767a410140750a2269
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 21:17:32 +0800

    Batch add 1 solutions: 3493.properties-grap...

--- 3493.properties-graph.py
+++ 3493.properties-graph.py
@@ -7,37 +7,22 @@
 # @lc code=start
 class Solution:
     def numberOfComponents(self, properties: List[List[int]], k: int) -> int:
-        n = len(properties)
-        adjacency_list = [[] for _ in range(n)]
-        
-        # Define intersect function
-        def intersect(a, b):
+        def intersect(a: List[int], b: List[int]) -> int:
             return len(set(a) & set(b))
-        
-        # Construct adjacency list
+        n = len(properties)
+        uf = list(range(n))
+        def find(x):
+            if uf[x] != x:
+                uf[x] = find(uf[x])
+            return uf[x]
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                uf[rootX] = rootY
         for i in range(n):
             for j in range(i + 1, n):
                 if intersect(properties[i], properties[j]) >= k:
-                    adjacency_list[i].append(j)
-                    adjacency_list[j].append(i)
-        
-        # Find connected components using DFS
-        visited = [False] * n
-        
-        def dfs(node):
-            stack = [node]
-            while stack:
-                current = stack.pop()
-                for neighbor in adjacency_list[current]:
-                    if not visited[neighbor]:
-                        visited[neighbor] = True
-                        stack.append(neighbor)
-        
-        # Count connected components
-        count = 0
-        for i in range(n):
-            if not visited[i]:
-                dfs(i)
-                count += 1
-                visited[i] = True # Ensure marking as visited after DFS call to avoid revisiting. 
-        return count  "}
+                    union(i, j)
+        return len(set(find(x) for x in range(n))) 
+# @lc code=end
