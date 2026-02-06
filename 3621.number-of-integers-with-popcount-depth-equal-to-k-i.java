cd0170090fad86935db771da832fb04a36d89b43
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 14:13:37 +0800

    Batch add 1 solutions: 3621.number-of-integ...

--- 3621.number-of-integers-with-popcount-depth-equal-to-k-i.java
+++ 3621.number-of-integers-with-popcount-depth-equal-to-k-i.java
@@ -5,56 +5,61 @@
 #
 # @lc code=start
 class Solution {
-    private int[] depthMemo = new int[100];
-    
-    private int popcountDepth(int x) {
-        if (x == 1) return 0;
-        if (depthMemo[x] != 0) return depthMemo[x];
-        int next = Integer.bitCount(x);
-        depthMemo[x] = 1 + popcountDepth(next);
-        return depthMemo[x];
+    // Precompute popcount-depth for numbers up to 60 (since 10^15 < 2^50)
+    private int[] depth = new int[65];
+    private void initPopcountDepth() {
+        depth[0] = -1; // 0 is not valid for our problem
+        depth[1] = 0;
+        for (int i = 2; i < depth.length; i++) {
+            int cnt = 0, x = i;
+            while (x != 1) {
+                x = Integer.bitCount(x);
+                cnt++;
+            }
+            depth[i] = cnt;
+        }
     }
-    
+
+    // Count how many numbers <= n have exactly s set bits
+    private long count(long n, int s) {
+        if (s == 0) return 1;
+        int bits = 0; long temp = n;
+        int[] pos = new int[65];
+        while (temp > 0) {
+            pos[bits++] = (int)(temp & 1);
+            temp >>= 1;
+        }
+        long res = 0; int ones = 0;
+        for (int i = bits - 1; i >= 0; i--) {
+            if (pos[i] == 1) {
+                res += comb(i, s - ones);
+            }
+            if (pos[i] == 1) ones++;
+            if (ones > s) break;
+        }
+        if (ones == s) res += 1;
+        return res;
+    }
+
+    // Compute n choose k
     private long comb(int n, int k) {
-        if (k < 0 || n < k) return 0;
+        if (k < 0 || k > n) return 0;
         long res = 1;
         for (int i = 1; i <= k; i++) {
             res = res * (n - i + 1) / i;
         }
         return res;
     }
-    
-    private long countWithSetBits(long n, int setBits) {
-        String bin = Long.toBinaryString(n);
-        int len = bin.length();
-        long res = 0;
-        int cnt = 0;
-        for (int i = 0; i < len; i++) {
-            if (bin.charAt(i) == '1') {
-                int rem = len - i - 1;
-                res += comb(rem, setBits - cnt - 1);
-                cnt++;
-            }
-        }
-        if (cnt == setBits) res++;
-        return res;
-    }
-    
+
     public long popcountDepth(long n, int k) {
-        // Step 4: Explicitly handle special edge cases
-        if (k == 0) return (n >= 1) ? 1 : 0; // Only x=1 qualifies if n>=1
-        int maxS = 60;
-        for (int i = 0; i <= maxS; i++) depthMemo[i] = 0;
+        if (k == 0) return n == 1 ? 1 : 0;
+        initPopcountDepth();
         long ans = 0;
-        for (int s = 1; s <= maxS; s++) {
-            if (popcountDepth(s) == k - 1) {
-                long cnt = countWithSetBits(n, s);
-                ans += cnt;
+        for (int i = 1; i < depth.length; i++) {
+            if (depth[i] == k - 1) { // popcount-depth is k (k-1 steps to get to 1)
+                ans += count(n, i);
             }
         }
-        if (k == 1) ans--; // Subtract x=1 (since popcount-depth of 1 is 0)
-        // Step 5: Systematically validate all special-case adjustments
-        // Step 6: Ensure strict output format adherence (handled by return type here)
         return ans;
     }
 }
