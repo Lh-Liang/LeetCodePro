ae2c3d32c1225f7f4610f36292f2dd2630ad7ea1
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 13:58:59 +0800

    Batch add 1 solutions: 3553.minimum-weighte...

--- 3553.minimum-weighted-subgraph-with-the-required-paths-ii.java
+++ 3553.minimum-weighted-subgraph-with-the-required-paths-ii.java
@@ -1,94 +1,76 @@
+#
+# @lc app=leetcode id=3553 lang=java
+#
+# [3553] Minimum Weighted Subgraph With the Required Paths II
+#
+# @lc code=start
+import java.util.*;
 class Solution {
-    int N, LOG;
+    int LOG = 17; // since n <= 1e5, log2(1e5) ~ 17
+    int n;
     List<int[]>[] tree;
-    int[][] parent;
-    int[][] upSum;
-    int[] depth;
-
-    public int[] minimumWeight(int[][] edges, int[][] queries) {
-        N = edges.length + 1;
-        LOG = 1;
-        while ((1 << LOG) <= N) LOG++;
-        tree = new ArrayList[N];
-        for (int i = 0; i < N; i++) tree[i] = new ArrayList<>();
-        for (int[] e : edges) {
-            tree[e[0]].add(new int[]{e[1], e[2]});
-            tree[e[1]].add(new int[]{e[0], e[2]});
-        }
-        parent = new int[N][LOG];
-        upSum = new int[N][LOG];
-        depth = new int[N];
-        dfs(0, -1, 0, 0);
-        for (int k = 1; k < LOG; k++) {
-            for (int i = 0; i < N; i++) {
-                if (parent[i][k-1] != -1) {
-                    parent[i][k] = parent[parent[i][k-1]][k-1];
-                    upSum[i][k] = upSum[i][k-1] + upSum[parent[i][k-1]][k-1];
-                } else {
-                    parent[i][k] = -1;
-                }
-            }
-        }
-        int[] ans = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            int src1 = queries[i][0], src2 = queries[i][1], dest = queries[i][2];
-            int l1 = lca(src1, dest);
-            int l2 = lca(src2, dest);
-            int l3 = lca(src1, src2);
-            // Find the LCA of all three nodes to account for overlap.
-            int l = lca(l1, src2);
-            // Inclusion-exclusion: total weight is sum of paths from each to l, with overlaps counted once.
-            int total = dist(src1, l) + dist(src2, l) + dist(dest, l);
-            ans[i] = total;
-        }
-        return ans;
-    }
-
-    void dfs(int u, int p, int d, int w) {
-        parent[u][0] = p;
-        upSum[u][0] = w;
+    int[] depth, parent, tin, tout;
+    int[][] up;
+    long[] weightSum;
+    int timer = 0;
+    void dfs(int u, int p, int d, long w) {
         depth[u] = d;
+        tin[u] = timer++;
+        parent[u] = p;
+        weightSum[u] = w;
+        up[u][0] = p == -1 ? u : p;
+        for (int i = 1; i < LOG; ++i) {
+            up[u][i] = up[up[u][i-1]][i-1];
+        }
         for (int[] nei : tree[u]) {
-            if (nei[0] != p) {
-                dfs(nei[0], u, d + 1, nei[1]);
+            int v = nei[0], wt = nei[1];
+            if (v != p) {
+                dfs(v, u, d+1, w + wt);
             }
         }
+        tout[u] = timer++;
     }
-
+    boolean isAncestor(int u, int v) {
+        return tin[u] <= tin[v] && tout[u] >= tout[v];
+    }
     int lca(int u, int v) {
-        if (depth[u] < depth[v]) { int tmp = u; u = v; v = tmp; }
-        for (int k = LOG - 1; k >= 0; k--) {
-            if (parent[u][k] != -1 && depth[parent[u][k]] >= depth[v]) {
-                u = parent[u][k];
+        if (isAncestor(u, v)) return u;
+        if (isAncestor(v, u)) return v;
+        for (int i = LOG - 1; i >= 0; --i) {
+            if (!isAncestor(up[u][i], v)) {
+                u = up[u][i];
             }
         }
-        if (u == v) return u;
-        for (int k = LOG - 1; k >= 0; k--) {
-            if (parent[u][k] != -1 && parent[u][k] != parent[v][k]) {
-                u = parent[u][k];
-                v = parent[v][k];
-            }
-        }
-        return parent[u][0];
+        return up[u][0];
     }
-
-    int dist(int u, int v) {
-        int ret = 0;
-        if (depth[u] < depth[v]) { int tmp = u; u = v; v = tmp; }
-        for (int k = LOG - 1; k >= 0; k--) {
-            if (parent[u][k] != -1 && depth[parent[u][k]] >= depth[v]) {
-                ret += upSum[u][k];
-                u = parent[u][k];
-            }
+    public int[] minimumWeight(int[][] edges, int[][] queries) {
+        n = edges.length + 1;
+        tree = new ArrayList[n];
+        for (int i = 0; i < n; ++i) tree[i] = new ArrayList<>();
+        for (int[] e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            tree[u].add(new int[]{v, w});
+            tree[v].add(new int[]{u, w});
         }
-        if (u == v) return ret;
-        for (int k = LOG - 1; k >= 0; k--) {
-            if (parent[u][k] != -1 && parent[u][k] != parent[v][k]) {
-                ret += upSum[u][k] + upSum[v][k];
-                u = parent[u][k];
-                v = parent[v][k];
-            }
+        depth = new int[n];
+        parent = new int[n];
+        tin = new int[n];
+        tout = new int[n];
+        up = new int[n][LOG];
+        weightSum = new long[n];
+        dfs(0, -1, 0, 0L);
+        int q = queries.length;
+        int[] res = new int[q];
+        for (int i = 0; i < q; ++i) {
+            int a = queries[i][0], b = queries[i][1], c = queries[i][2];
+            int l1 = lca(a, c);
+            int l2 = lca(b, c);
+            int l3 = lca(a, b);
+            // The union of paths from a to c and b to c is: sum(a to c) + sum(b to c) - sum(lca(a, b) to c)
+            long ans = weightSum[a] + weightSum[b] - weightSum[l3] + weightSum[c] - weightSum[l1] - weightSum[l2] + weightSum[lca(l1, l2)];
+            res[i] = (int) ans;
         }
-        return ret + upSum[u][0] + upSum[v][0];
+        return res;
     }
 }
+# @lc code=end
