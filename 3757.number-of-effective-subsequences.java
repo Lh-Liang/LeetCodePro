8914fb357a57cbd21212518905239ff1d99abafc
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 18:29:05 +0800

    Batch add 1 solutions: 3757.number-of-effec...

--- 3757.number-of-effective-subsequences.java
+++ 3757.number-of-effective-subsequences.java
@@ -5,50 +5,24 @@
 #
 
 # @lc code=start
-import java.util.*;
 class Solution {
     public int countEffective(int[] nums) {
-        final int MOD = 1000000007;
-        int n = nums.length;
+        int MOD = 1000000007;
         int totalOR = 0;
-        for (int num : nums) totalOR |= num;
-        List<Integer> bits = new ArrayList<>();
-        for (int b = 0; b < 21; ++b) if (((totalOR >> b) & 1) != 0) bits.add(b);
-        int m = bits.size();
-        List<List<Integer>> bitIndices = new ArrayList<>();
-        for (int i = 0; i < m; ++i) bitIndices.add(new ArrayList<>());
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (((nums[i] >> bits.get(j)) & 1) != 0) bitIndices.get(j).add(i);
+        for (int num : nums) {
+            totalOR |= num;
+        }
+        
+        // Calculate effective subsequences count based on OR properties
+        long result = (1L << nums.length) - 1; // Total non-empty subsequences count
+        
+        // Exclude non-effective subsequences by evaluating their ORs
+        for (int num : nums) {
+            if ((totalOR ^ num) < totalOR) { // Check if removing 'num' decreases OR strength
+                result--; // Subtract ineffective ones that don't change strength
             }
         }
-        int res = 0;
-        for (int mask = 1; mask < (1 << m); ++mask) {
-            Set<Integer> indices = null;
-            for (int j = 0; j < m; ++j) {
-                if (((mask >> j) & 1) != 0) {
-                    if (indices == null) indices = new HashSet<>(bitIndices.get(j));
-                    else indices.retainAll(bitIndices.get(j));
-                }
-            }
-            if (indices == null || indices.isEmpty()) continue;
-            int cnt = indices.size();
-            long ways = pow(2, cnt, MOD) - 1;
-            if (ways < 0) ways += MOD;
-            int bitsOn = Integer.bitCount(mask);
-            if ((bitsOn & 1) == 1) res = (int)((res + ways) % MOD);
-            else res = (int)((res - ways + MOD) % MOD);
-        }
-        return res;
-    }
-    private long pow(long base, int exp, int mod) {
-        long ans = 1;
-        while (exp > 0) {
-            if ((exp & 1) == 1) ans = ans * base % mod;
-            base = base * base % mod;
-            exp >>= 1;
-        }
-        return ans;
+        return (int)(result % MOD);
     }
 }
 # @lc code=end
