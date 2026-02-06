b030e18671e8daccd02df29eb03ece4ec9bf585f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 22:25:37 +0800

    Batch add 1 solutions: 3777.minimum-deletio...

--- 3777.minimum-deletions-to-make-alternating-substring.py
+++ 3777.minimum-deletions-to-make-alternating-substring.py
@@ -7,28 +7,30 @@
 # @lc code=start
 class Solution:
     def minDeletions(self, s: str, queries: List[List[int]]) -> List[int]:
-        answer = []
-        s = list(s)  # Convert to list for mutability
+        n = len(s)
+        result = []
+        s = list(s)  # To allow mutation of the string
+        
+        def min_deletions_to_alternate(l, r):
+            # Calculate minimum deletions to make s[l:r+1] alternating
+            del_count_1 = del_count_2 = 0
+            for i in range(l, r + 1):
+                expected_char_1 = 'A' if (i - l) % 2 == 0 else 'B'
+                expected_char_2 = 'B' if (i - l) % 2 == 0 else 'A'
+                if s[i] != expected_char_1:
+                    del_count_1 += 1
+                if s[i] != expected_char_2:
+                    del_count_2 += 1
+            return min(del_count_1, del_count_2)
+        
         for query in queries:
             if query[0] == 1:
-                j = query[1]
+                _, j = query
                 # Flip character at index j
-                s[j] = 'A' if s[j] == 'B' else 'B'
+                s[j] = 'B' if s[j] == 'A' else 'A'
             elif query[0] == 2:
-                l, r = query[1], query[2]
-                # Compute minimum deletions for s[l:r+1] to be alternating
-                count1 = 0  # Count changes assuming start with 'A'
-                count2 = 0  # Count changes assuming start with 'B'
-                current_expected_char_1 = 'A'
-                current_expected_char_2 = 'B'
-                for i in range(l, r + 1):
-                    if s[i] != current_expected_char_1:
-                        count1 += 1
-                    if s[i] != current_expected_char_2:
-                        count2 += 1
-                    # Alternate expected characters
-                    current_expected_char_1 = 'B' if current_expected_char_1 == 'A' else 'A'
-                    current_expected_char_2 = 'A' if current_expected_char_2 == 'B' else 'B'
-                answer.append(min(count1, count2))
-        return answer
+                _, l, r = query
+                # Calculate minimum deletions for substring s[l:r+1]
+                result.append(min_deletions_to_alternate(l, r))
+        return result
 # @lc code=end
