bc4e6e92db199f05f7655e9696c69b5b6ddd0797
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 22:27:19 +0800

    Batch add 1 solutions: 3779.minimum-number-...

--- 3779.minimum-number-of-operations-to-have-distinct-elements.py
+++ 3779.minimum-number-of-operations-to-have-distinct-elements.py
@@ -1,4 +1,3 @@
-#
 # @lc app=leetcode id=3779 lang=python3
 #
 # [3779] Minimum Number of Operations to Have Distinct Elements
@@ -7,9 +6,10 @@
 # @lc code=start
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
+        from collections import Counter
+        freq = Counter(nums)
         operations = 0
         while len(nums) > len(set(nums)):
-            nums = nums[3:] if len(nums) >= 3 else []
+            nums = nums[3:]  # Remove first three elements
             operations += 1
-        return operations
-# @lc code=end
+        return operations # @lc code=end
