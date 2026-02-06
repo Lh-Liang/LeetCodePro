46303a3dc836457bdf9b7e1f6d4874748cb3a3a6
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 03:42:44 +0800

    Batch add 1 solutions: 3704.count-no-zero-p...

--- 3704.count-no-zero-pairs-that-sum-to-n.py
+++ 3704.count-no-zero-pairs-that-sum-to-n.py
@@ -7,12 +7,12 @@
 # @lc code=start
 class Solution:
     def countNoZeroPairs(self, n: int) -> int:
-        def contains_no_zero(x):
+        def is_no_zero(x):
             return '0' not in str(x)
         count = 0
-        for a in range(1, (n // 2) + 1):
+        for a in range(1, n):
             b = n - a
-            if contains_no_zero(a) and contains_no_zero(b):
+            if is_no_zero(a) and is_no_zero(b):
                 count += 1
         return count
 # @lc code=end
