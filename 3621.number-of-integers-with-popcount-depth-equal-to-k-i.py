4fee92fb52b856d44f1d1adc4c798fb0dd1ecb80
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 21:55:39 +0800

    Batch add 1 solutions: 3621.number-of-integ...

--- 3621.number-of-integers-with-popcount-depth-equal-to-k-i.py
+++ 3621.number-of-integers-with-popcount-depth-equal-to-k-i.py
@@ -1,1 +1,25 @@
-The problem has been solved successfully.
+#
+# @lc app=leetcode id=3621 lang=python3
+#
+# [3621] Number of Integers With Popcount-Depth Equal to K I
+#
+
+# @lc code=start
+class Solution:
+    def popcountDepth(self, n: int, k: int) -> int:
+        def popcount(x):
+            return bin(x).count('1')
+        
+        def calculate_depth(x):
+            depth = 0
+            while x != 1:
+                x = popcount(x)
+                depth += 1
+            return depth
+        
+        count = 0
+        for i in range(1, n + 1):
+            if calculate_depth(i) == k:
+                count += 1
+        return count
+# @lc code=end
