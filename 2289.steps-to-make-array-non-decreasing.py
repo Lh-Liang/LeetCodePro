55c84c0db4ee4621e1d2f07cea797e7bcddc4b6a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 20:29:12 +0800

    Batch add 1 solutions: 2289.steps-to-make-a...

--- 2289.steps-to-make-array-non-decreasing.py
+++ 2289.steps-to-make-array-non-decreasing.py
@@ -7,22 +7,20 @@
 # @lc code=start
 class Solution:
     def totalSteps(self, nums: List[int]) -> int:
-        # stack will store (value, steps_to_be_removed)
-        stack = []
-        ans = 0
-        for x in nums:
-            t = 0
-            while stack and stack[-1][0] <= x:
-                t = max(t, stack.pop()[1])
+        n = len(nums)
+        stack = []  # will store pairs of (number, steps)
+        max_steps = 0
+        
+        # Traverse from right to left
+        for i in range(n - 1, -1, -1):
+            current_steps = 0
+            # Pop all elements from stack which are smaller than current element
+            while stack and nums[i] > stack[-1][0]:
+                current_steps = max(current_steps + 1, stack[-1][1])
+                stack.pop()
             
-            if not stack:
-                # x is the largest so far, won't be removed
-                t = 0
-            else:
-                # x will be removed by stack[-1][0] at time t + 1
-                t = t + 1
-            
-            ans = max(ans, t)
-            stack.append((x, t))
-        return ans
+            max_steps = max(max_steps, current_steps)
+            # Append current number with its count of steps required to remove it completely (if ever needed)
+            stack.append((nums[i], current_steps))
+        return max_steps
 # @lc code=end
