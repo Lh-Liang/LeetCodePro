205b4d879027fe94aea4f995e7bfbae424e66381
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 17:31:13 +0800

    Batch add 1 solutions: 3519.count-numbers-w...

--- 3519.count-numbers-with-non-decreasing-digits.java
+++ 3519.count-numbers-with-non-decreasing-digits.java
@@ -4,63 +4,36 @@
 # [3519] Count Numbers with Non-Decreasing Digits
 #
 # @lc code=start
-import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.HashMap;
-
 class Solution {
-    static final int MOD = 1000000007;
-
     public int countNumbers(String l, String r, int b) {
-        int[] digitsR = toBaseB(r, b);
-        int[] digitsL = toBaseB(new BigInteger(l).subtract(BigInteger.ONE).toString(), b);
-        return (int) ((count(digitsR, b) - count(digitsL, b) + MOD) % MOD);
-    }
-
-    private int[] toBaseB(String num, int b) {
-        BigInteger n = new BigInteger(num);
-        if (n.equals(BigInteger.ZERO)) return new int[]{0};
-        int[] tmp = new int[105];
-        int idx = tmp.length - 1;
-        while (n.compareTo(BigInteger.ZERO) > 0) {
-            BigInteger[] dr = n.divideAndRemainder(BigInteger.valueOf(b));
-            tmp[idx--] = dr[1].intValue();
-            n = dr[0];
+        int mod = 1000000007;
+        long left = Long.parseLong(l);
+        long right = Long.parseLong(r);
+        int count = 0;
+        for (long num = left; num <= right; num++) {
+            if (isNonDecreasing(toBase(num, b))) {
+                count = (count + 1) % mod;
+            }
         }
-        int[] res = Arrays.copyOfRange(tmp, idx+1, tmp.length);
-        return res;
+        return count;
     }
-
-    private long count(int[] digits, int b) {
-        int n = digits.length;
-        Long[][][][] memo = new Long[n+1][b+1][2][2];
-        return dp(0, 0, true, false, digits, b, memo);
+    
+    private List<Integer> toBase(long num, int base) {
+        List<Integer> digits = new ArrayList<>();
+        while (num > 0) {
+            digits.add(0, (int)(num % base));
+            num /= base;
+        }
+        return digits;
     }
-
-    // pos: current digit
-    // prev: previous digit (0~b-1), or 0 when not started
-    // tight: whether we are still tight to the bound
-    // started: whether we've placed any nonzero digit yet
-    private long dp(int pos, int prev, boolean tight, boolean started, int[] digits, int b, Long[][][][] memo) {
-        if (pos == digits.length) return started ? 1 : 0;
-        int tightLim = tight ? digits[pos] : b-1;
-        int minDigit = started ? prev : 0;
-        if (memo[pos][prev][tight?1:0][started?1:0] != null)
-            return memo[pos][prev][tight?1:0][started?1:0];
-        long res = 0;
-        for (int d = minDigit; d <= tightLim; d++) {
-            boolean nextTight = tight && (d == tightLim);
-            boolean nextStarted = started || (d != 0);
-            res = (res + dp(pos+1, nextStarted ? d : 0, nextTight, nextStarted, digits, b, memo)) % MOD;
+    
+    private boolean isNonDecreasing(List<Integer> digits) {
+        for (int i = 1; i < digits.size(); i++) {
+            if (digits.get(i) < digits.get(i - 1)) {
+                return false;
+            }
         }
-        // If not started, we can skip leading zeros
-        if (!started) {
-            // Only skip if not at last digit
-            if (pos < digits.length - 1)
-                res = (res + dp(pos+1, 0, tight && (0 == tightLim), false, digits, b, memo)) % MOD;
-        }
-        memo[pos][prev][tight?1:0][started?1:0] = res;
-        return res;
+        return true;
     }
 }
 # @lc code=end
