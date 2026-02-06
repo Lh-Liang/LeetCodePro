199e5f3eb41fedb7123df0f11db27ae481424a81
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 22:21:15 +0800

    Batch add 1 solutions: 3765.complete-prime-...

--- 3765.complete-prime-number.py
+++ 3765.complete-prime-number.py
@@ -3,34 +3,34 @@
 #
 # [3765] Complete Prime Number
 #
-
 # @lc code=start
 class Solution:
-    def completePrime(self, num: int) -> bool:
-        # Helper function to check if a number is prime
-        def is_prime(n):
-            if n <= 1:
-                return False
-            if n <= 3:
-                return True
-            if n % 2 == 0 or n % 3 == 0:
-                return False
-            i = 5
-            while i * i <= n:
-                if n % i == 0 or n % (i + 2) == 0:
-                    return False
-                i += 6
+    def is_prime(self, num: int) -> bool:
+        if num <= 1:
+            return False
+        if num <= 3:
             return True
-        
-        # Convert num to string for prefix/suffix operations
-        num_str = str(num)
-        length = len(num_str)
-        
-        # Check all prefixes and suffixes
+        if num % 2 == 0 or num % 3 == 0:
+            return False
+        i = 5
+        while i * i <= num:
+            if num % i == 0 or num % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+    
+    def completePrime(self, num: int) -> bool:
+        str_num = str(num)
+        length = len(str_num)
+        # Check all prefixes
         for i in range(1, length + 1):
-            prefix = int(num_str[:i])
-            suffix = int(num_str[-i:])
-            if not (is_prime(prefix) and is_prime(suffix)):
+            prefix = int(str_num[:i])
+            if not self.is_prime(prefix):
+                return False
+        # Check all suffixes
+        for i in range(length):
+            suffix = int(str_num[i:])
+            if not self.is_prime(suffix):
                 return False
         return True
 # @lc code=end
