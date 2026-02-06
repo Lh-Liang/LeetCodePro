ba7eabf933c8ceff3cdf6ccdba21f36bc395028a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 22:28:09 +0800

    Batch add 1 solutions: 3782.last-remaining-...

--- 3782.last-remaining-integer-after-alternating-deletion-operations.py
+++ 3782.last-remaining-integer-after-alternating-deletion-operations.py
@@ -1,1 +1,16 @@
-The derived formula for the last remaining integer after alternating deletion operations simplifies the process without simulating each step.
+#
+# @lc app=leetcode id=3782 lang=python3
+#
+# [3782] Last Remaining Integer After Alternating Deletion Operations
+#
+# @lc code=start
+class Solution:
+    def lastInteger(self, n: int) -> int:
+        return self.josephus(n)
+    
+    def josephus(self, n: int) -> int:
+        if n == 1:
+            return 1
+        else:
+            return (self.josephus(n - 1) + 2) % n or n
+# @lc code=end
