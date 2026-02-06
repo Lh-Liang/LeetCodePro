8d346c64d224192d093cefdefa2d6fd77ba3694c
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 18:34:43 +0800

    Batch add 1 solutions: 3777.minimum-deletio...

--- 3777.minimum-deletions-to-make-alternating-substring.java
+++ 3777.minimum-deletions-to-make-alternating-substring.java
@@ -1,42 +1,31 @@
-#
-# @lc app=leetcode id=3777 lang=java
-#
-# [3777] Minimum Deletions to Make Alternating Substring
-#
-# @lc code=start
 class Solution {
     public int[] minDeletions(String s, int[][] queries) {
         int n = s.length();
-        char[] arr = s.toCharArray();
         int[] answer = new int[queries.length];
-        // Build prefix sum of adjacent equals
-        int[] prefix = new int[n];
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i-1] + (arr[i]==arr[i-1]?1:0);
-        }
-        for (int qi = 0, ai = 0; qi < queries.length; qi++) {
-            int[] q = queries[qi];
-            if (q[0] == 1) {
-                int j = q[1];
-                arr[j] = arr[j]=='A'?'B':'A';
-                // Only update prefix at j and j+1
-                if (j > 0) {
-                    prefix[j] = prefix[j-1] + (arr[j]==arr[j-1]?1:0);
-                }
-                for (int k = j+1; k < n && k <= j+1; k++) {
-                    prefix[k] = prefix[k-1] + (arr[k]==arr[k-1]?1:0);
-                }
-            } else {
-                int l = q[1], r = q[2];
-                answer[ai++] = prefix[r] - (l>0?prefix[l]:0);
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < queries.length; i++) {
+            int[] query = queries[i];
+            if (query[0] == 1) { // Flip operation
+                int j = query[1];
+                arr[j] = arr[j] == 'A' ? 'B' : 'A';
+            } else if (query[0] == 2) { // Compute operation
+                int l = query[1], r = query[2];
+                answer[i] = minDeletionsForAlternating(arr, l, r);
             }
         }
-        // filter only type 2 answers
-        int[] result = new int[(int) java.util.Arrays.stream(queries).filter(q->q[0]==2).count()];
-        for (int i = 0, j = 0; i < queries.length; i++) {
-            if (queries[i][0]==2) result[j++] = answer[i];
+        return answer;
+    }
+    
+    private int minDeletionsForAlternating(char[] arr, int l, int r) {
+        int deletions1 = 0; // For pattern ABAB...
+        int deletions2 = 0; // For pattern BABA...
+        for (int i = l; i <= r; i++) {
+            if ((i % 2 == 0 && arr[i] != 'A') || (i % 2 == 1 && arr[i] != 'B')) {
+                deletions1++;
+            } else if ((i % 2 == 0 && arr[i] != 'B') || (i % 2 == 1 && arr[i] != 'A')) {
+                deletions2++;
+            }
         }
-        return result;
+        return Math.min(deletions1, deletions2);
     }
 }
-# @lc code=end
