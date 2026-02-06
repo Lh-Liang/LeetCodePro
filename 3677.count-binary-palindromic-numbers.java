7233d818b579b583ea5aa447f819fa273fbfb063
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 18:12:33 +0800

    Batch add 1 solutions: 3677.count-binary-pa...

--- 3677.count-binary-palindromic-numbers.java
+++ 3677.count-binary-palindromic-numbers.java
@@ -7,30 +7,23 @@
 class Solution {
     public int countBinaryPalindromes(long n) {
         int count = 0;
-        // Special case: 0 is a palindrome
-        if (n == 0) return 1;
-        int maxBits = Long.toBinaryString(n).length();
-        // For each bit length
-        for (int l = 1; l <= maxBits; l++) {
-            int half = (l + 1) / 2;
-            long start = 1L << (half - 1);
-            long end = 1L << half;
-            for (long firstHalf = start; firstHalf < end; firstHalf++) {
-                long palindrome = buildPalindrome(firstHalf, l % 2 == 1);
-                if (palindrome <= n) count++;
+        for (long i = 0; i <= n; i++) {
+            String binaryStr = Long.toBinaryString(i);
+            if (isPalindrome(binaryStr)) {
+                count++;
             }
         }
         return count;
     }
-    // Helper to build palindrome from firstHalf. If odd, exclude the middle bit when mirroring
-    private long buildPalindrome(long firstHalf, boolean oddLength) {
-        long res = firstHalf;
-        if (oddLength) firstHalf >>= 1;
-        while (firstHalf > 0) {
-            res = (res << 1) | (firstHalf & 1);
-            firstHalf >>= 1;
+    
+    private boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
         }
-        return res;
+        return true;
     }
 }
 # @lc code=end
