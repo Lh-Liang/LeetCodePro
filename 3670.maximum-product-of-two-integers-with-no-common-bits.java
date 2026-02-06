c4178049da601119f872810b2378182c6e59f51f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 18:11:51 +0800

    Batch add 1 solutions: 3670.maximum-product...

--- 3670.maximum-product-of-two-integers-with-no-common-bits.java
+++ 3670.maximum-product-of-two-integers-with-no-common-bits.java
@@ -3,23 +3,15 @@
 #
 # [3670] Maximum Product of Two Integers With No Common Bits
 #
+
 # @lc code=start
 class Solution {
     public long maxProduct(int[] nums) {
-        // Map from bitmask to maximum number with that bitmask
-        java.util.Map<Integer, Integer> maskToMax = new java.util.HashMap<>();
-        for (int num : nums) {
-            int mask = num;
-            maskToMax.put(mask, Math.max(maskToMax.getOrDefault(mask, 0), num));
-        }
         long maxProduct = 0;
-        Integer[] masks = maskToMax.keySet().toArray(new Integer[0]);
-        int n = masks.length;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if ((masks[i] & masks[j]) == 0) {
-                    long prod = (long)maskToMax.get(masks[i]) * maskToMax.get(masks[j]);
-                    if (prod > maxProduct) maxProduct = prod;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] & nums[j]) == 0) { // No common set bits
+                    maxProduct = Math.max(maxProduct, (long) nums[i] * nums[j]);
                 }
             }
         }
