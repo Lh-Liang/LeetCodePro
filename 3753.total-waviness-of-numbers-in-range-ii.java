6d0ff98d88e5919f2c38cea5b01de15871255c05
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 14:39:41 +0800

    Batch add 1 solutions: 3753.total-waviness-...

--- 3753.total-waviness-of-numbers-in-range-ii.java
+++ 3753.total-waviness-of-numbers-in-range-ii.java
@@ -4,36 +4,38 @@
 # [3753] Total Waviness of Numbers in Range II
 #
 # @lc code=start
+import java.util.*;
 class Solution {
     public long totalWaviness(long num1, long num2) {
         return countWaviness(num2) - countWaviness(num1 - 1);
     }
-
     private long countWaviness(long n) {
+        if (n < 100) return 0;
         char[] digits = Long.toString(n).toCharArray();
-        int len = digits.length;
-        // dp[pos][prev][pprev][tight][started] = waviness sum
-        Long[][][][][] memo = new Long[len + 1][11][11][2][2];
-        return dfs(0, 10, 10, 1, 0, digits, memo);
+        // dp[pos][tight][prev][prev2][leadZero] = long
+        Long[][][][][] memo = new Long[digits.length + 1][2][11][11][2];
+        return dfs(0, 1, 10, 10, 1, digits, memo);
     }
-
-    // pos: current index, prev: previous digit, pprev: pre-previous digit
-    private long dfs(int pos, int prev, int pprev, int tight, int started, char[] digits, Long[][][][][] memo) {
+    // pos: current digit position, tight: bound flag, prev1: previous digit, prev2: digit before prev1, leadZero: leading zero flag
+    private long dfs(int pos, int tight, int prev1, int prev2, int leadZero, char[] digits, Long[][][][][] memo) {
         if (pos == digits.length) return 0;
-        if (memo[pos][prev][pprev][tight][started] != null) return memo[pos][prev][pprev][tight][started];
-        long res = 0;
+        if (memo[pos][tight][prev1][prev2][leadZero] != null) return memo[pos][tight][prev1][prev2][leadZero];
         int limit = tight == 1 ? digits[pos] - '0' : 9;
+        long res = 0;
         for (int d = 0; d <= limit; ++d) {
-            int nTight = (tight == 1 && d == limit) ? 1 : 0;
-            int nStarted = (started == 1 || d != 0) ? 1 : 0;
-            int wave = 0;
-            if (nStarted == 1 && pos >= 2) {
-                if (prev > pprev && prev > d) wave = 1; // peak
-                if (prev < pprev && prev < d) wave = 1; // valley
+            int newTight = (tight == 1 && d == limit) ? 1 : 0;
+            int newLeadZero = (leadZero == 1 && d == 0) ? 1 : 0;
+            int newPrev1 = (newLeadZero == 1) ? 10 : d;
+            long add = 0;
+            if (pos >=2 && leadZero == 0) {
+                // prev2 and prev1 are valid
+                if (prev1 > prev2 && prev1 > d) add = 1;
+                if (prev1 < prev2 && prev1 < d) add = 1;
             }
-            res += wave + dfs(pos + 1, d, prev, nTight, nStarted, digits, memo);
+            res += dfs(pos+1, newTight, newPrev1, prev1, newLeadZero, digits, memo) + add;
         }
-        return memo[pos][prev][pprev][tight][started] = res;
+        memo[pos][tight][prev1][prev2][leadZero] = res;
+        return res;
     }
 }
 # @lc code=end
