d8ed00f26ece1903f59870907fcadac39e52cb88
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 21:59:10 +0800

    Batch add 1 solutions: 3630.partition-array...

--- 3630.partition-array-for-maximum-xor-and-and.py
+++ 3630.partition-array-for-maximum-xor-and-and.py
@@ -8,20 +8,18 @@
 class Solution:
     def maximizeXorAndXor(self, nums: List[int]) -> int:
         n = len(nums)
-        max_val = 0
-        
-        def dfs(index, a_xor, b_and, c_xor):
-            nonlocal max_val
-            if index == n:
-                max_val = max(max_val, a_xor + b_and + c_xor)
-                return
-            # Option 1: Add nums[index] to A
-            dfs(index + 1, a_xor ^ nums[index], b_and, c_xor)
-            # Option 2: Add nums[index] to B
-            dfs(index + 1, a_xor, b_and & nums[index] if b_and != 0 else nums[index], c_xor)
-            # Option 3: Add nums[index] to C
-            dfs(index + 1, a_xor, b_and, c_xor ^ nums[index])
-        
-        dfs(0, 0, 0xFFFFFFFFFF, 0) # Start with full range for AND operation in B
-        return max_val
-# @lc code=end
+        max_result = 0
+        for i in range(1 << n):
+            A, B, C = [], [], []
+            for j in range(n):
+                if i & (1 << j):
+                    A.append(nums[j])
+                elif (i >> j) & 1 == 0:
+                    B.append(nums[j])
+                else:
+                    C.append(nums[j])
+            xor_A = 0 if not A else reduce(lambda x, y: x ^ y, A)
+            and_B = (1 << 32) - 1 if not B else reduce(lambda x, y: x & y, B)
+            xor_C = 0 if not C else reduce(lambda x, y: x ^ y, C)
+            max_result = max(max_result, xor_A + and_B + xor_C)
+        return max_result # @lc code=end
