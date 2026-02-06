aa7fa6f090cda0c3f1b93ab3297a4cde29f9cff0
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 00:52:46 +0800

    Batch add 1 solutions: 1670.design-front-mi...

--- 1670.design-front-middle-back-queue.py
+++ 1670.design-front-middle-back-queue.py
@@ -1,1 +1,32 @@
-FrontMiddleBackQueue implementation completed.
+#
+# @lc app=leetcode id=1670 lang=python3
+#
+# [1670] Design Front Middle Back Queue
+#
+
+# @lc code=start
+class FrontMiddleBackQueue:
+    def __init__(self):
+        self.queue = []
+    
+    def pushFront(self, val: int) -> None:
+        self.queue.insert(0, val)
+    
+    def pushMiddle(self, val: int) -> None:
+        mid = len(self.queue) // 2
+        self.queue.insert(mid, val)
+    
+    def pushBack(self, val: int) -> None:
+        self.queue.append(val)
+    
+    def popFront(self) -> int:
+        return self.queue.pop(0) if self.queue else -1
+    
+    def popMiddle(self) -> int:
+        if not self.queue:
+            return -1
+        mid = (len(self.queue) - 1) // 2
+        return self.queue.pop(mid)
+    
+    def popBack(self) -> int:
+        return self.queue.pop() if self.queue else -1
