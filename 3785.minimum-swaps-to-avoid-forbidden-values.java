7fda4ee0fc4646bbb1d3d4287f3fded55ceef3a5
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 18:39:27 +0800

    Batch add 1 solutions: 3785.minimum-swaps-t...

--- 3785.minimum-swaps-to-avoid-forbidden-values.java
+++ 3785.minimum-swaps-to-avoid-forbidden-values.java
@@ -3,72 +3,29 @@
 #
 # [3785] Minimum Swaps to Avoid Forbidden Values
 #
-
 # @lc code=start
 class Solution {
-    public int minSwaps(int[] nums, int[] forbidden) {
-        int n = nums.length;
-        int[] pos = new int[n];
-        for (int i = 0; i < n; ++i) pos[i] = i;
-        boolean[] visited = new boolean[n];
-        int swaps = 0;
-
-        // Build mapping from value to indices (positions)
-        Map<Integer, List<Integer>> valToIdx = new HashMap<>();
-        for (int i = 0; i < n; ++i) {
-            valToIdx.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
-        }
-        // To find a derangement avoiding forbidden[i], we need to try to permute nums such that nums[i] != forbidden[i] for all i
-        // This can be solved by finding cycles in the initial permutation and checking for forbidden constraints
-        int[] target = new int[n];
-        Arrays.fill(target, -1);
-        boolean possible = true;
-        // For each index, try to assign a value that is not forbidden[i], and try to keep original positions if possible
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int num : nums) count.put(num, count.getOrDefault(num,0)+1);
-        List<Integer> available = new ArrayList<>();
-        for (int num : nums) available.add(num);
-        // Try to create a derangement avoiding forbidden[i] for each i
-        for (int i = 0; i < n; ++i) {
-            boolean found = false;
-            for (int j = 0; j < available.size(); ++j) {
-                int cand = available.get(j);
-                if (cand != forbidden[i] && !(cand == nums[i] && forbidden[i] == nums[i] && count.get(nums[i]) == 1)) {
-                    target[i] = cand;
-                    available.remove(j);
-                    found = true;
+public int minSwaps(int[] nums, int[] forbidden) {
+    int n = nums.length;
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != forbidden[i]) {
+            map.put(nums[i], i);
+        } else {
+            boolean swapped = false;
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] != forbidden[i] && nums[j] != forbidden[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    swapped = true;
                     break;
                 }
             }
-            if (!found) {
-                possible = false;
-                break;
-            }
+            if (!swapped) return -1; // No valid swap found to separate from forbidden value. 
         }
-        if (!possible) return -1;
-        // Now, count minimum swaps to convert nums to target
-        Map<Integer, Queue<Integer>> valToQueue = new HashMap<>();
-        for (int i = 0; i < n; ++i) {
-            valToQueue.computeIfAbsent(target[i], k -> new LinkedList<>()).add(i);
-        }
-        int[] perm = new int[n];
-        for (int i = 0; i < n; ++i) {
-            perm[i] = valToQueue.get(nums[i]).poll();
-        }
-        boolean[] done = new boolean[n];
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            if (done[i] || perm[i] == i) continue;
-            int cycle = 0;
-            int j = i;
-            while (!done[j]) {
-                done[j] = true;
-                j = perm[j];
-                cycle++;
-            }
-            ans += cycle - 1;
-        }
-        return ans;
     }
+    return map.size(); // Count of successful swaps made. 
+}
 }
 # @lc code=end
