087aefaa64e7d437ebc18851262e207901e0f74b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 06:24:43 +0800

    Batch add 1 solutions: 3782.last-remaining-...

--- 3782.last-remaining-integer-after-alternating-deletion-operations.py
+++ 3782.last-remaining-integer-after-alternating-deletion-operations.py
@@ -3,14 +3,21 @@
 #
 # [3782] Last Remaining Integer After Alternating Deletion Operations
 #
+
 # @lc code=start
 class Solution:
     def lastInteger(self, n: int) -> int:
-        return self.josephus(n)
-    
-    def josephus(self, n: int) -> int:
-        if n == 1:
-            return 1
-        else:
-            return (self.josephus(n - 1) + 2) % n or n
+        step = 1
+        start = 1
+        remaining = n
+        left_to_right = True
+        
+        while remaining > 1:
+            if left_to_right or remaining % 2 == 1:
+                start += step
+            remaining //= 2
+            step *= 2
+            left_to_right = not left_to_right
+        
+        return start
 # @lc code=end
