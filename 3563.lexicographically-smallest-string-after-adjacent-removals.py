df4e7b0fa9e6d9907df75be8d48e1cbfe75cbd1d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 21:42:08 +0800

    Batch add 1 solutions: 3563.lexicographical...

--- 3563.lexicographically-smallest-string-after-adjacent-removals.py
+++ 3563.lexicographically-smallest-string-after-adjacent-removals.py
@@ -9,8 +9,9 @@ class Solution:
     def lexicographicallySmallestString(self, s: str) -> str:
         stack = []
         for char in s:
-            while (stack and (ord(stack[-1]) + 1 == ord(char) or ord(stack[-1]) - 1 == ord(char))):
+            if stack and (ord(stack[-1]) == ord(char) - 1 or ord(stack[-1]) == ord(char) + 1 or (stack[-1] == 'z' and char == 'a') or (stack[-1] == 'a' and char == 'z')):
                 stack.pop()
-            stack.append(char)
+            else:
+                stack.append(char)
         return ''.join(stack)
 # @lc code=end
