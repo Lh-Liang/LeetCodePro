6871374b4b71d1481bfd7f48e3ff757418446b01
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 18:03:37 +0800

    Batch add 1 solutions: 3621.number-of-integ...

--- 3621.number-of-integers-with-popcount-depth-equal-to-k-i.java
+++ 3621.number-of-integers-with-popcount-depth-equal-to-k-i.java
@@ -3,64 +3,23 @@
 #
 # [3621] Number of Integers With Popcount-Depth Equal to K I
 #
+
 # @lc code=start
 class Solution {
-    // Precompute popcount-depth for numbers up to 60 (since 10^15 < 2^50)
-    private int[] depth = new int[65];
-    private void initPopcountDepth() {
-        depth[0] = -1; // 0 is not valid for our problem
-        depth[1] = 0;
-        for (int i = 2; i < depth.length; i++) {
-            int cnt = 0, x = i;
-            while (x != 1) {
-                x = Integer.bitCount(x);
-                cnt++;
-            }
-            depth[i] = cnt;
-        }
-    }
-
-    // Count how many numbers <= n have exactly s set bits
-    private long count(long n, int s) {
-        if (s == 0) return 1;
-        int bits = 0; long temp = n;
-        int[] pos = new int[65];
-        while (temp > 0) {
-            pos[bits++] = (int)(temp & 1);
-            temp >>= 1;
-        }
-        long res = 0; int ones = 0;
-        for (int i = bits - 1; i >= 0; i--) {
-            if (pos[i] == 1) {
-                res += comb(i, s - ones);
-            }
-            if (pos[i] == 1) ones++;
-            if (ones > s) break;
-        }
-        if (ones == s) res += 1;
-        return res;
-    }
-
-    // Compute n choose k
-    private long comb(int n, int k) {
-        if (k < 0 || k > n) return 0;
-        long res = 1;
-        for (int i = 1; i <= k; i++) {
-            res = res * (n - i + 1) / i;
-        }
-        return res;
-    }
-
     public long popcountDepth(long n, int k) {
-        if (k == 0) return n == 1 ? 1 : 0;
-        initPopcountDepth();
-        long ans = 0;
-        for (int i = 1; i < depth.length; i++) {
-            if (depth[i] == k - 1) { // popcount-depth is k (k-1 steps to get to 1)
-                ans += count(n, i);
+        long count = 0;
+        for (long i = 1; i <= n; i++) {
+            int current = (int) i;
+            int depth = 0;
+            while (current != 1 && depth <= k) {
+                current = Integer.bitCount(current);
+                depth++;
+            }
+            if (depth == k && current == 1) {
+                count++;
             }
         }
-        return ans;
+        return count;
     }
 }
 # @lc code=end
