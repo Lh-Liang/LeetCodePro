f87e4e97332a39bca6a0ba5d51a49d6e2644cea8
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-06 22:05:28 +0800

    Batch add 1 solutions: 3677.count-binary-pa...

--- 3677.count-binary-palindromic-numbers.py
+++ 3677.count-binary-palindromic-numbers.py
@@ -7,10 +7,11 @@
 # @lc code=start
 class Solution:
     def countBinaryPalindromes(self, n: int) -> int:
+        def is_palindrome(s: str) -> bool:
+            return s == s[::-1]
         count = 0
-        for i in range(n + 1):
-            bin_rep = bin(i)[2:]
-            if bin_rep == bin_rep[::-1]:
+        for k in range(n + 1):
+            if is_palindrome(bin(k)[2:]):
                 count += 1
         return count
 # @lc code=end
