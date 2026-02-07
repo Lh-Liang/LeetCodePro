2f6ff949f6615c1fb756d4437f2c546710353267
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 00:21:46 +0800

    Batch add 1 solutions: 2807.insert-greatest...

--- 2807.insert-greatest-common-divisors-in-linked-list.go
+++ 2807.insert-greatest-common-divisors-in-linked-list.go
@@ -3,7 +3,6 @@
 #
 # [2807] Insert Greatest Common Divisors in Linked List
 #
-
 # @lc code=start
 /**
 * Definition for singly-linked list.
@@ -13,18 +12,22 @@
 * }
 */
 func gcd(a, b int) int {
-    if b == 0 { return a } 
-    return gcd(b, a % b) 
+    for b != 0 {
+        a, b = b, a % b
+    }
+    return a
 }
+
 func insertGreatestCommonDivisors(head *ListNode) *ListNode {
-    current := head 
-    for current != nil && current.Next != nil { 
-        gcdValue := gcd(current.Val, current.Next.Val) 
-        newNode := &ListNode{Val: gcdValue} 
-        newNode.Next = current.Next 
-        current.Next = newNode 
-        current = newNode.Next 
-    } 
-    return head 
+    current := head
+    for current != nil && current.Next != nil {
+        next := current.Next
+        gcdValue := gcd(current.Val, next.Val) // Calculate GCD using custom function.
+        gcdNode := &ListNode{Val: gcdValue} // Create new node with GCD value.
+        gcdNode.Next = next // Link new node between current and next.
+        current.Next = gcdNode // Update current's next to point to this new node.
+        current = next // Move to next original node in sequence.
+    }
+    return head // Return modified head of linked list after insertion of all GCDs.
 }
 # @lc code=end
