a80625646644e2b3c1e82cdae9c8ec7a505416ad
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 18:11:11 +0800

    Batch add 1 solutions: 3503.longest-palindr...

--- 3503.longest-palindrome-after-substring-concatenation-i.cpp
+++ 3503.longest-palindrome-after-substring-concatenation-i.cpp
@@ -3,24 +3,27 @@
 #
 # [3503] Longest Palindrome After Substring Concatenation I
 #
+
 # @lc code=start
 class Solution {
 public:
     int longestPalindrome(string s, string t) {
-        vector<int> freqS(26, 0), freqT(26, 0);
-        // Count frequencies of characters in both strings
-        for (char c : s) ++freqS[c - 'a'];
-        for (char c : t) ++freqT[c - 'a'];
         int maxLength = 0;
-        bool oddFrequencyFound = false;
-        // Sum pairs and detect if there's any odd frequency to add one more to result
-        for (int i = 0; i < 26; ++i) {
-            int totalFreq = freqS[i] + freqT[i];
-            maxLength += (totalFreq / 2) * 2; // Add maximum even part
-            if (totalFreq % 2 == 1) oddFrequencyFound = true;
+        auto isPalindrome = [](const string &str) {
+            int left = 0, right = str.size() - 1;
+            while (left < right) {
+                if (str[left++] != str[right--]) return false;
+            }
+            return true;
+        };
+        for (int i = 0; i <= s.size(); ++i) {
+            for (int j = 0; j <= t.size(); ++j) {
+                string combined = s.substr(0, i) + t.substr(j);
+                if (isPalindrome(combined)) {
+                    maxLength = max(maxLength, int(combined.size()));
+                }
+            }
         }
-        // If there's an odd frequency, we can have a center of a palindrome
-        if (oddFrequencyFound) ++maxLength;
         return maxLength;
     }
 };
