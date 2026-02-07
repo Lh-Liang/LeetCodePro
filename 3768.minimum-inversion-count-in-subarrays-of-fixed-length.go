b5bf61c9a407daa30ede7ec378ff1c5bd0451bc4
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 03:33:36 +0800

    Batch add 1 solutions: 3768.minimum-inversi...

--- 3768.minimum-inversion-count-in-subarrays-of-fixed-length.go
+++ 3768.minimum-inversion-count-in-subarrays-of-fixed-length.go
@@ -1,41 +1,57 @@
-#
-# @lc app=leetcode id=3768 lang=golang
-#
-# [3768] Minimum Inversion Count in Subarrays of Fixed Length
-#
+//
+// @lc app=leetcode id=3768 lang=golang
+//
+// [3768] Minimum Inversion Count in Subarrays of Fixed Length
+//
 
-# @lc code=start
+// @lc code=start
 func minInversionCount(nums []int, k int) int64 {
     n := len(nums)
-    minInversions := int64(n * n) // Initialize with a large number
-    for i := 0; i <= n-k; i++ {
-        _, count := mergeSortAndCount(nums[i:i+k])
-        if count < minInversions {
-            minInversions = count
+    if k == 1 {
+        return 0 // No inversions possible in single-element subarrays
+    }
+    
+    // Define a Fenwick Tree structure
+    type FenwickTree struct {
+        tree []int64
+        size int
+    }
+    
+    // Initialize Fenwick Tree
+    func (ft *FenwickTree) init(size int) {
+        ft.tree = make([]int64, size+1)
+        ft.size = size
+    }
+    
+    // Update function for Fenwick Tree
+    func (ft *FenwickTree) update(index int, delta int64) {
+        for i := index; i <= ft.size; i += i & (-i) {
+            ft.tree[i] += delta
         }
     }
-    return minInversions
-}
-
-func mergeSortAndCount(arr []int) ([]int, int64) {
-    if len(arr) < 2 {
-        return arr, 0
+    
+    // Prefix sum query for Fenwick Tree
+    func (ft *FenwickTree) query(index int) int64 {
+        var sum int64 = 0
+        for i := index; i > 0; i -= i & (-i) {
+            sum += ft.tree[i]
+        }
+        return sum
     }
-    mid := len(arr) / 2
-    left, leftInv := mergeSortAndCount(arr[:mid])
-    right, rightInv := mergeSortAndCount(arr[mid:])
-    mergedArr, splitInv := mergeAndCount(left, right)
-    return mergedArr, leftInv + rightInv + splitInv
-}
-
-func mergeAndCount(left []int, right []int) ([]int, int64) {
-    i, j := 0, 0
-    var invCount int64 = 0
-    mergedArr := make([]int, 0, len(left)+len(right))
-    for i < len(left) && j < len(right) {
-        if left[i] <= right[j] {
-            mergedArr = append(mergedArr, left[i])
-            i++
-        } else { // there are mid - i inversions because left[i] > right[j]
-            mergedArr = append(mergedArr, right[j])
-            invCount += int64(len(left) - i) & // Count inversions here & j++ & } & } & & // Append remaining elements & mergedArr = append(mergedArr,left[i:]...) & mergedArr = append(mergedArr,right[j:]...) & return mergedArr , invCount & } # @lc code=end
+    
+    // Coordinate compression map to ensure indices fit within tree size limits
+    coordinateMap := make(map[int]int)
+    sortedNums := make([]int, n)
+    copy(sortedNums, nums)
+    sort.Ints(sortedNums)
+    idx := 1 // Start indices from 1 for Fenwick Tree compatibility
+    for _, num := range sortedNums {
+        if _, exists := coordinateMap[num]; !exists {
+            coordinateMap[num] = idx
+            idx++
+        }
+    }
+    
+    fenwickTree := FenwickTree{}
+    fenwickTree.init(len(coordinateMap))
+    															minInversions := int64(^uint64(0) >> 1) // Max integer value as initial min inversion count// Sliding Window Algorithm with Inversion Counting using a Fenwick Tree.for startIdx := 0; startIdx+k <= n; startIdx++ {if startIdx > 0 { prevIndexMapped := coordinateMap[nums[startIdx-1]] fenwickTree.update(prevIndexMapped, -1)} inversionsInWindow := int64(0)	for j := startIdx; j < startIdx+k; j++ {indexMapped := coordinateMap[nums[j]]inversionsInWindow += fenwickTree.query(fenwickTree.size) - fenwickTree.query(indexMapped)	fenwickTree.update(indexMapped, 1)}if inversionsInWindow < minInversions {minInversions = inversionsInWindow}}return minInversions}// @lc code=end
