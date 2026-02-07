78e2fc023d79399b239ed7e622a6555bc94252a9
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 00:41:41 +0800

    Batch add 1 solutions: 3398.smallest-substr...

--- 3398.smallest-substring-with-identical-characters-i.go
+++ 3398.smallest-substring-with-identical-characters-i.go
@@ -3,25 +3,38 @@
 #
 # [3398] Smallest Substring With Identical Characters I
 #
+
 # @lc code=start
 func minLength(s string, numOps int) int {
     n := len(s)
-    if numOps == 0 {
-        return findLongestIdenticalSubstringLength(s)
-    }
-    minLen := n // Start with maximum possible length
-    left := 0
-    maxCount := 0 // Maximum count of identical chars in current window
-    countMap := make(map[byte]int)
+    maxLen := 0 // This will track the maximum length of a substring with identical characters
+    left := 0   // Left pointer for our sliding window
+    count0, count1 := 0, 0 // Count of '0's and '1's in current window
+    
     for right := 0; right < n; right++ {
-        countMap[s[right]]++
-        maxCount = max(maxCount, countMap[s[right]])
-        // Check if we need to shrink the window
-        if (right - left + 1 - maxCount) > numOps {
-            countMap[s[left]]--
-            left++
+        if s[right] == '0' {
+            count0++
         } else {
-            minLen = min(minLen, right - left + 1) // Update minimum length found so far within allowed flips
+            count1++
         }
+        
+        // If we need more than numOps flips to make all chars identical in this window
+        while min(count0, count1) > numOps {
+            if s[left] == '0' {
+                count0--
+            } else {
+                count1--
+            }
+            left++
+        }
+        
+        // Calculate current maxLen as longest substring of identical chars within current valid window
+        maxLen = max(maxLen, right - left + 1)
     }
-    return minLen// Return final minimum length found after operations "}
+    
+    return n - maxLen // Minimum length after minimizing longest identical character substring
+}
+
+func min(a, b int) int { if a < b { return a }; return b }
+func max(a, b int) int { if a > b { return a }; return b }
+# @lc code=end
