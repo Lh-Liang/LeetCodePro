de765e6c3bad562faca4776eece93215ca3d3f3e
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 17:33:26 +0800

    Batch add 1 solutions: 3445.maximum-differe...

--- 3445.maximum-difference-between-even-and-odd-frequency-ii.cpp
+++ 3445.maximum-difference-between-even-and-odd-frequency-ii.cpp
@@ -3,45 +3,34 @@
 #
 # [3445] Maximum Difference Between Even and Odd Frequency II
 #
+
 # @lc code=start
 class Solution {
 public:
     int maxDifference(string s, int k) {
-        int n = s.size();
-        unordered_map<char, int> freq;
         int maxDiff = INT_MIN;
-        int start = 0;
-        
-        for (int end = 0; end < n; ++end) {
-            freq[s[end]]++;
-            
-            // When we have a valid window of at least size k
-            if (end - start + 1 >= k) {
-                // Check for odd/even frequency characters within this window
+        unordered_map<char, int> freq;
+        // Initialize sliding window
+        for (int i = 0; i < s.size(); ++i) {
+            ++freq[s[i]];
+            if (i >= k - 1) { // Only consider substrings of at least size k
                 int oddMax = INT_MIN, evenMax = INT_MIN;
-                for (auto &p : freq) {
-                    if (p.second % 2 == 1) { // odd frequency
-                        oddMax = max(oddMax, p.second);
-                    } else if (p.second > 0) { // non-zero even frequency
-                        evenMax = max(evenMax, p.second);
+                // Calculate current max difference between odd and even frequencies
+                for (auto &[char, count] : freq) {
+                    if (count % 2 == 1) { // Odd frequency
+                        oddMax = max(oddMax, count);
+                    } else if (count > 0 && count % 2 == 0) { // Even non-zero frequency
+                        evenMax = max(evenMax, count);
                     }
                 }
-                
-                // Compute potential max difference
-                if (oddMax != INT_MIN && evenMax != INT_MIN) {
+                if (oddMax != INT_MIN && evenMax != INT_MIN) { // Valid pair found
                     maxDiff = max(maxDiff, oddMax - evenMax);
                 }
-                
-                // Slide the window by reducing start point
-                freq[s[start]]--;
-                if (freq[s[start]] == 0) {
-                    freq.erase(s[start]);
-                }
-                start++;
+                // Slide window: remove leftmost character from consideration
+                --freq[s[i - k + 1]];
             }
         }
-        
-        return maxDiff == INT_MIN ? -1 : maxDiff;
-    }
-};
+        return maxDiff == INT_MIN ? -1 : maxDiff; // Return -1 if no valid difference found
+    } 
+}; 
 # @lc code=end
