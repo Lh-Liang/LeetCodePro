381040263822f6bf761fc87d061d01a750e15c95
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 05:56:21 +0800

    Batch add 1 solutions: 3621.number-of-integ...

--- 3621.number-of-integers-with-popcount-depth-equal-to-k-i.go
+++ 3621.number-of-integers-with-popcount-depth-equal-to-k-i.go
@@ -3,33 +3,78 @@
 #
 # [3621] Number of Integers With Popcount-Depth Equal to K I
 #
-
 # @lc code=start
+import (
+    "math/bits"
+)
+
 func popcountDepth(n int64, k int) int64 {
-    // Dynamic programming map to store calculated depths
-    dp := make(map[int64]int)
-    var count int64 = 0
-    
-    // Helper function to calculate popcount-depth recursively with memoization
-    var calcDepth func(int64) int
-    calcDepth = func(x int64) int {
+    if k == 0 {
+        if n >= 1 {
+            return 1
+        }
+        return 0
+    }
+    maxBits := 60 // since n <= 1e15
+    // Helper: computes popcount-depth for a given integer
+    var depth func(int) int
+    depth = func(x int) int {
         if x == 1 {
             return 0
         }
-        if d, found := dp[x]; found {
-            return d
-        }
-        nextX := int64(bits.OnesCount(uint(x)))
-        dp[x] = 1 + calcDepth(nextX)
-        return dp[x]
+        return 1 + depth(bits.OnesCount(uint(x)))
     }
-    
-    // Check all numbers from 1 to n
-    for x := int64(1); x <= n; x++ {
-        if calcDepth(x) == k {
-            count++
+    // Precompute valid set bit counts
+    var validS []int
+    for s := 1; s <= maxBits; s++ {
+        if depth(s)+1 == k {
+            validS = append(validS, s)
         }
     }
-    return count
+    // Precompute combinations
+    var comb [61][61]int64
+    for i := 0; i <= maxBits; i++ {
+        comb[i][0] = 1
+        for j := 1; j <= i; j++ {
+            comb[i][j] = comb[i-1][j-1] + comb[i-1][j]
+        }
+    }
+    nCr := func(n, r int) int64 {
+        if n < 0 || r < 0 || n < r {
+            return 0
+        }
+        return comb[n][r]
+    }
+    // Counts numbers <= n with exactly setBits set bits
+    var count func(int64, int) int64
+    count = func(n int64, setBits int) int64 {
+        if setBits == 0 {
+            return 1
+        }
+        res := int64(0)
+        bitsLeft := setBits
+        for i := maxBits - 1; i >= 0; i-- {
+            if (n>>i)&1 != 0 {
+                if bitsLeft > 0 {
+                    res += nCr(i, bitsLeft-1)
+                }
+            }
+            if (n>>i)&1 != 0 {
+                bitsLeft--
+                if bitsLeft < 0 {
+                    break
+                }
+            }
+        }
+        if bits.OnesCount64(uint64(n)) == setBits {
+            res++
+        }
+        return res
+    }
+    ans := int64(0)
+    for _, s := range validS {
+        ans += count(n, s)
+    }
+    return ans
 }
-// @lc code=end
+# @lc code=end
