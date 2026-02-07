24042066da0c8da8cce3142f613505edf75f154d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 06:14:18 +0800

    Batch add 1 solutions: 3695.maximize-altern...

--- 3695.maximize-alternating-sum-using-swaps.go
+++ 3695.maximize-alternating-sum-using-swaps.go
@@ -5,13 +5,17 @@
 #
 
 # @lc code=start
+import (
+    "sort"
+)
+
 func maxAlternatingSum(nums []int, swaps [][]int) int64 {
     n := len(nums)
     parent := make([]int, n)
     for i := range parent {
         parent[i] = i
     }
-    var find func(x int) int
+    var find func(int) int
     find = func(x int) int {
         if parent[x] != x {
             parent[x] = find(parent[x])
@@ -19,30 +23,47 @@ func maxAlternatingSum(nums []int, swaps [][]int) int64 {
         return parent[x]
     }
     union := func(x, y int) {
-        rootX := find(x)
-        rootY := find(y)
-        if rootX != rootY {
-            parent[rootX] = rootY
+        px, py := find(x), find(y)
+        if px != py {
+            parent[py] = px
         }
     }
     for _, swap := range swaps {
         union(swap[0], swap[1])
     }
-    components := make(map[int][]int)
+    groups := make(map[int][]int)
     for i := 0; i < n; i++ {
         root := find(i)
-        components[root] = append(components[root], nums[i])
+        groups[root] = append(groups[root], i)
     }
-    result := int64(0)
-    for _, comp := range components {
-        sort.Slice(comp, func(i, j int) bool { return comp[i] > comp[j] }) // Sort in descending order to maximize alternating sum.
-       for i, num := range comp { 
-           if i%2 == 0 { 
-               result += int64(num) 
-           } else { 
-               result -= int64(num) 
-           } 
-       } 
-   } 
-   return result 
-} # @lc code=end
+    res := int64(0)
+    for _, idxs := range groups {
+        values := make([]int, len(idxs))
+        for i, idx := range idxs {
+            values[i] = nums[idx]
+        }
+        sort.Ints(idxs)
+        sort.Sort(sort.Reverse(sort.IntSlice(values)))
+        evenIdxs, oddIdxs := []int{}, []int{}
+        for _, idx := range idxs {
+            if idx%2 == 0 {
+                evenIdxs = append(evenIdxs, idx)
+            } else {
+                oddIdxs = append(oddIdxs, idx)
+            }
+        }
+        // Assign largest values to even indices, smallest to odd
+        evenCount := len(evenIdxs)
+        oddCount := len(oddIdxs)
+        sum := int64(0)
+        for i := 0; i < evenCount; i++ {
+            sum += int64(values[i])
+        }
+        for i := 0; i < oddCount; i++ {
+            sum -= int64(values[evenCount+i])
+        }
+        res += sum
+    }
+    return res
+}
+# @lc code=end
