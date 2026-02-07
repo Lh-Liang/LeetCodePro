97a60909dcb8fdd62061a77cb38bab3488b885e0
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 14:45:05 +0800

    Batch add 1 solutions: 3748.count-stable-su...

--- 3748.count-stable-subarrays.cpp
+++ 3748.count-stable-subarrays.cpp
@@ -3,49 +3,29 @@
 #
 # [3748] Count Stable Subarrays
 #
-
 # @lc code=start
 class Solution {
 public:
     vector<long long> countStableSubarrays(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        vector<int> run_start(n), run_end(n);
-        // Precompute the runs
-        int l = 0;
-        while (l < n) {
-            int r = l;
-            while (r + 1 < n && nums[r] <= nums[r+1]) ++r;
-            for (int i = l; i <= r; ++i) {
-                run_start[i] = l;
-                run_end[i] = r;
+        vector<long long> results;
+        for (const auto& query : queries) {
+            int li = query[0], ri = query[1];
+            long long count = 0;
+            int length = 1; // Track current length of stable sequence
+            for (int i = li + 1; i <= ri; ++i) {
+                if (nums[i] >= nums[i - 1]) {
+                    ++length;
+                } else {
+                    // Calculate number of stable subarrays from current sequence length
+                    count += (long long)(length * (length + 1)) / 2;
+                    length = 1; // Reset length for new sequence
+                }
             }
-            l = r + 1;
+            // Handle last segment if it ends with a non-decreasing sequence
+            count += (long long)(length * (length + 1)) / 2;
+            results.push_back(count);
         }
-        // Precompute prefix sums of stable subarrays count up to each position
-        vector<long long> prefix(n+1);
-        int i = 0;
-        while (i < n) {
-            int j = run_end[i];
-            long long len = j - i + 1;
-            long long cnt = len * (len + 1) / 2;
-            prefix[j+1] = prefix[i] + cnt;
-            i = j + 1;
-        }
-        // Compute answer for each query
-        vector<long long> ans;
-        for (auto& q : queries) {
-            int l = q[0], r = q[1];
-            long long res = 0;
-            int i = l;
-            while (i <= r) {
-                int seg_r = min(run_end[i], r);
-                long long len = seg_r - i + 1;
-                res += len * (len + 1) / 2;
-                i = seg_r + 1;
-            }
-            ans.push_back(res);
-        }
-        return ans;
+        return results;
     }
 };
 # @lc code=end
