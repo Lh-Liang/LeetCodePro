ad3045af970f5e7fb73ebb41eff776ef12eaf54d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 06:28:45 +0800

    Batch add 1 solutions: 3739.count-subarrays...

--- 3739.count-subarrays-with-majority-element-ii.go
+++ 3739.count-subarrays-with-majority-element-ii.go
@@ -6,25 +6,60 @@
 # @lc code=start
 func countMajoritySubarrays(nums []int, target int) int64 {
     n := len(nums)
-    count := int64(0)
-    prefixSum := 0
-    countMap := make(map[int]int)
-    countMap[0] = 1 // Initialize with 0 to handle full array as possibility
-    
-    for _, num := range nums {
-        if num == target {
-            prefixSum += 1
+    prefix := make([]int, n+1)
+    for i, v := range nums {
+        if v == target {
+            prefix[i+1] = prefix[i] + 1
         } else {
-            prefixSum -= 1
+            prefix[i+1] = prefix[i] - 1
         }
-        
-        // Check if there's any previous prefixSum such that current prefixSum - previous > 0 (majority condition)
-        count += int64(countMap[prefixSum - 1])
-        
-        // Update map with current prefixSum occurrence
-        countMap[prefixSum]++
     }
-    
-    return count
+    // To count for each i, number of j < i such that prefix[i] > prefix[j]
+    // Use a map to count frequency of prefix sums
+    // For efficient counting, use a sorted list of prefix sums
+    // But since values can be negative, compress values
+    allPrefix := make([]int, n+1)
+    copy(allPrefix, prefix)
+    // Coordinate compression
+    valMap := make(map[int]int)
+    uniq := make([]int, 0)
+    for _, v := range allPrefix {
+        valMap[v] = 1
+    }
+    for k := range valMap {
+        uniq = append(uniq, k)
+    }
+    sort.Ints(uniq)
+    for i, v := range uniq {
+        valMap[v] = i
+    }
+    // Fenwick Tree
+    type BIT struct {
+        tree []int
+    }
+    func (b *BIT) add(i, x int) {
+        for i < len(b.tree) {
+            b.tree[i] += x
+            i += i & -i
+        }
+    }
+    func (b *BIT) sum(i int) int {
+        res := 0
+        for i > 0 {
+            res += b.tree[i]
+            i -= i & -i
+        }
+        return res
+    }
+    size := len(uniq) + 2
+    bit := &BIT{tree: make([]int, size)}
+    var ans int64 = 0
+    for i := 0; i <= n; i++ {
+        idx := valMap[prefix[i]] + 1
+        // For i, number of prefix[j] < prefix[i] is sum(idx-1)
+        ans += int64(bit.sum(idx - 1))
+        bit.add(idx, 1)
+    }
+    return ans
 }
 # @lc code=end
