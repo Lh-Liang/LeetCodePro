fe85d9736d9ecdb4331247e30b677d1ac1c23876
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 03:15:54 +0800

    Batch add 1 solutions: 3739.count-subarrays...

--- 3739.count-subarrays-with-majority-element-ii.go
+++ 3739.count-subarrays-with-majority-element-ii.go
@@ -3,9 +3,28 @@
 #
 # [3739] Count Subarrays With Majority Element II
 #
-
 # @lc code=start
 func countMajoritySubarrays(nums []int, target int) int64 {
-    // Implementation code here with optimization techniques
+    n := len(nums)
+    count := int64(0)
+    prefixSum := 0
+    countMap := make(map[int]int)
+    countMap[0] = 1 // Initialize with 0 to handle full array as possibility
+    
+    for _, num := range nums {
+        if num == target {
+            prefixSum += 1
+        } else {
+            prefixSum -= 1
+        }
+        
+        // Check if there's any previous prefixSum such that current prefixSum - previous > 0 (majority condition)
+        count += int64(countMap[prefixSum - 1])
+        
+        // Update map with current prefixSum occurrence
+        countMap[prefixSum]++
+    }
+    
+    return count
 }
 # @lc code=end
