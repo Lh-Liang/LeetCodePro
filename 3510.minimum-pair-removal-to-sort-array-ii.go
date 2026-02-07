c071f4b41531fb79aa6bee03f71511b61358895a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 01:43:16 +0800

    Batch add 1 solutions: 3510.minimum-pair-re...

--- 3510.minimum-pair-removal-to-sort-array-ii.go
+++ 3510.minimum-pair-removal-to-sort-array-ii.go
@@ -7,19 +7,35 @@
 # @lc code=start
 func minimumPairRemoval(nums []int) int {
     ops := 0
-    for i := 0; i < len(nums)-1; {
-        if nums[i] > nums[i+1] {
-            nums[i+1] = nums[i] + nums[i+1]
-            ops++
-            if i > 0 {
-                i-- // Move back one step to re-evaluate after replacement.
-            } else {
-                i++ // Move forward if at start of array.
+    for len(nums) > 1 {
+        n := len(nums)
+        minSum := int(^uint(0) >> 1) 
+        minIndex := -1
+        for i := 0; i < n-1; i++ {
+            sum := nums[i] + nums[i+1]
+            if sum < minSum {
+                minSum = sum
+                minIndex = i
             }
-        } else {
-            i++ // Move forward if current pair is non-decreasing.
+        }
+        if minIndex != -1 {
+            nums[minIndex] = minSum
+            nums = append(nums[:minIndex+1], nums[minIndex+2:]...)
+            ops++
+        }
+        if isNonDecreasing(nums) { 
+            break
         }
     }
     return ops
+}
+
+func isNonDecreasing(arr []int) bool {
+    for i := 0; i < len(arr)-1; i++ {
+        if arr[i] > arr[i+1] {
+            return false
+        }
+    }
+    return true
 }
 # @lc code=end
