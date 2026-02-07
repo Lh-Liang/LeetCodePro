fdd211afc17ec7b65bd6cb34a1f10fb95c57002b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 03:38:55 +0800

    Batch add 1 solutions: 3779.minimum-number-...

--- 3779.minimum-number-of-operations-to-have-distinct-elements.go
+++ 3779.minimum-number-of-operations-to-have-distinct-elements.go
@@ -1,36 +1,33 @@
-#
 # @lc app=leetcode id=3779 lang=golang
 #
 # [3779] Minimum Number of Operations to Have Distinct Elements
 #
-
 # @lc code=start
 func minOperations(nums []int) int {
-    operationCount := 0
+    operations := 0
     
-    // Create a function to check if there are duplicates
-    hasDuplicates := func(arr []int) bool {
-        seen := make(map[int]bool)
-        for _, num := range arr {
-            if seen[num] {
-                return true
-            }
-            seen[num] = true
-        }
-        return false
-    }
-    
-    // Continue performing operations until there are no duplicates or list is empty
-    for len(nums) > 0 && hasDuplicates(nums) {
-        // Remove up to three elements from the start of nums
-        if len(nums) >= 3 {
-            nums = nums[3:]   // remove first three elements if possible
+    for containsDuplicates(nums) {
+        if len(nums) <= 3 {
+            // Remove all remaining elements if fewer than three
+            nums = []int{}
         } else {
-            nums = []int{}   // clear all remaining if less than three left
+            // Remove the first three elements
+            nums = nums[3:]
         }
-        operationCount++   // increment operation count after each removal operation
+        operations++
     }
     
-    return operationCount  // return total number of operations performed 
+    return operations
+}
+
+func containsDuplicates(nums []int) bool {
+    elementCount := make(map[int]bool)
+    for _, num := range nums {
+        if elementCount[num] {
+            return true
+        }
+        elementCount[num] = true
+    }
+    return false
 }
 # @lc code=end
