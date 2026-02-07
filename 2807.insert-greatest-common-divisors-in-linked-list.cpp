ad165f3c463ee6d6d45acd30466d18855cc25728
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 11:48:58 +0800

    Batch add 1 solutions: 2807.insert-greatest...

--- 2807.insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807.insert-greatest-common-divisors-in-linked-list.cpp
@@ -3,43 +3,31 @@
 #
 # [2807] Insert Greatest Common Divisors in Linked List
 #
-
 # @lc code=start
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-#include <numeric>
-
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode() : val(0), next(nullptr) {}
+*     ListNode(int x) : val(x), next(nullptr) {}
+*     ListNode(int x, ListNode *next) : val(x), next(next) {}
+* };
+*/
+#include <algorithm> // for std::gcd
 class Solution {
 public:
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
-        // If the list is empty or has only one node, no adjacent pairs exist.
-        if (!head || !head->next) {
-            return head;
+        if (!head || !head->next) return head; // Edge case: 0 or 1 node, return as is.
+        ListNode* current = head;
+        while (current && current->next) {
+            int gcdValue = std::gcd(current->val, current->next->val); // Compute GCD of current and next node's values.
+            ListNode* newNode = new ListNode(gcdValue); // Create new node with GCD value.
+            newNode->next = current->next; // Link new node to current's next node.
+            current->next = newNode; // Link current's next to new node.
+            current = newNode->next; // Move to next pair of nodes by skipping over inserted node.
         }
-
-        ListNode* curr = head;
-        // Traverse the list as long as there is an adjacent pair.
-        while (curr && curr->next) {
-            // Calculate GCD of current node and the next node values.
-            int gcdVal = std::gcd(curr->val, curr->next->val);
-            
-            // Create and insert the new GCD node.
-            ListNode* newNode = new ListNode(gcdVal, curr->next);
-            curr->next = newNode;
-            
-            // Jump to the original 'next' node, which is now two steps ahead.
-            curr = newNode->next;
-        }
-        
-        return head;
+        return head; // Return modified list.
     }
 };
 # @lc code=end
