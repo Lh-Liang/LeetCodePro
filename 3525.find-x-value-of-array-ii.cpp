14df41a6975046d92766052ed0ced719de5bc809
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 13:20:27 +0800

    Batch add 1 solutions: 3525.find-x-value-of...

--- 3525.find-x-value-of-array-ii.cpp
+++ 3525.find-x-value-of-array-ii.cpp
@@ -3,38 +3,46 @@
 #
 # [3525] Find X Value of Array II
 #
-
 # @lc code=start
 class Solution {
 public:
     vector<int> resultArray(vector<int>& nums, int k, vector<vector<int>>& queries) {
+        vector<int> results;
         int n = nums.size();
-        vector<int> res;
-        // We will keep nums updated throughout queries
-        for (auto& q : queries) {
-            int idx = q[0], val = q[1], start = q[2], xi = q[3];
-            nums[idx] = val; // apply persistent update
-            vector<int> arr(nums.begin() + start, nums.end());
-            int m = arr.size();
-            // Suffix product modulo k
-            vector<int> suffix_prod(m + 1, 1);
-            for (int i = m - 1; i >= 0; --i) {
-                suffix_prod[i] = (1LL * arr[i] * suffix_prod[i + 1]) % k;
-            }
-            // For each possible suffix removal, i.e., arr[0..j], where j = 0..m-1
-            // The product is prod(arr[0] * ... * arr[j]) = suffix_prod[0] / suffix_prod[j+1] (modulo k)
-            // But division is not always possible in modular arithmetic, but since k is small, we can brute-force
-            // Instead, accumulate prefix product modulo k from left to right, and for each possible suffix, check if the prefix product equals xi
-            int prefix = 1, count = 0;
-            for (int i = 0; i < m; ++i) {
-                if (prefix == xi) count++;
-                prefix = (1LL * prefix * arr[i]) % k;
-            }
-            // Also consider keeping the whole array (no suffix removed)
-            if (prefix == xi) count++;
-            res.push_back(count);
+        
+        // Precompute prefix products modulo k
+        vector<int> prefixProducts(n + 1, 1);
+        for (int i = 1; i <= n; ++i) {
+            prefixProducts[i] = (prefixProducts[i - 1] * nums[i - 1]) % k;
         }
-        return res;
+        
+        for (auto& query : queries) {
+            int index = query[0];
+            int value = query[1];
+            int start = query[2];
+            int x_value = query[3];
+            
+            // Update nums with new value at specified index and recompute prefix product if needed
+            if (nums[index] != value) {
+                nums[index] = value;
+                for (int i = index + 1; i <= n; ++i) {
+                    prefixProducts[i] = (prefixProducts[i - 1] * nums[i - 1]) % k;
+                }
+            }
+            
+            // Calculate x-value after removing prefix up to 'start'.
+            long long product_mod_k = prefixProducts[start];
+            int count_x_value = 0;
+            for (int i = start; i < n; ++i) {
+                long long suffix_product_mod_k = (prefixProducts[n] / prefixProducts[i]) % k;
+                if (suffix_product_mod_k == x_value) {
+                    count_x_value++;
+                }
+                product_mod_k = (product_mod_k * nums[i]) % k;
+            }
+            results.push_back(count_x_value);
+        }
+        return results;
     }
 };
 # @lc code=end
