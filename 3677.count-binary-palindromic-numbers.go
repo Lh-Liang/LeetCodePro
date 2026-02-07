8ac9e44a516d5b71823be2063d3585a4ebdd2974
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 02:59:24 +0800

    Batch add 1 solutions: 3677.count-binary-pa...

--- 3677.count-binary-palindromic-numbers.go
+++ 3677.count-binary-palindromic-numbers.go
@@ -3,26 +3,59 @@
 #
 # [3677] Count Binary Palindromic Numbers
 #
-
 # @lc code=start
 func countBinaryPalindromes(n int64) int {
+    if n == 0 {
+        return 1 // "0" is a palindrome
+    }
+    
     count := 0
-    for i := int64(0); i <= n; i++ {
-        binStr := strconv.FormatInt(i, 2)
-        if isPalindrome(binStr) {
+    
+    // Generate palindromes
+    for length := 1; ; length++ {
+        found := false
+        // Generate odd-length palindromes
+        for half := int64(1 << (length - 1)); half < (1 << length); half++ {
+            palindrome := buildPalindrome(half, true)
+            if palindrome > n {
+                found = true
+                break
+            }
             count++
+        }
+        // Generate even-length palindromes
+        for half := int64(1 << (length - 1)); half < (1 << length); half++ {
+            palindrome := buildPalindrome(half, false)
+            if palindrome > n {
+                found = true
+                break
+            }
+            count++
+        }
+        if found {
+            break
         }
     }
     return count
 }
 
-func isPalindrome(s string) bool {
-    l := len(s)
-    for i := 0; i < l/2; i++ {
-        if s[i] != s[l-i-1] {
-            return false
-        }
+// Function to build a binary palindrome given half of it.
+func buildPalindrome(half int64, isOddLength bool) int64 {
+    binaryStr := strconv.FormatInt(half, 2)
+    reversedHalf := reverseString(binaryStr)
+    if isOddLength {
+        reversedHalf = reversedHalf[1:] // Omit the middle character for odd lengths
     }
-    return true
+    fullBinaryStr := binaryStr + reversedHalf
+    palindrome, _ := strconv.ParseInt(fullBinaryStr, 2, 64)
+    return palindrome
+}
+
+func reverseString(s string) string {
+    runes := []rune(s)
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
 }
 # @lc code=end
