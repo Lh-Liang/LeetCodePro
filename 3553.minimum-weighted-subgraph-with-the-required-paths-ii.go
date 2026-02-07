8fd441a3699ac4447ae5306cc74e89ff07ada9b0
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 05:28:11 +0800

    Batch add 1 solutions: 3553.minimum-weighte...

--- 3553.minimum-weighted-subgraph-with-the-required-paths-ii.go
+++ 3553.minimum-weighted-subgraph-with-the-required-paths-ii.go
@@ -1,67 +1,92 @@
+#
 # @lc app=leetcode id=3553 lang=golang
+#
 # [3553] Minimum Weighted Subgraph With the Required Paths II
 #
 # @lc code=start
-import "container/heap"
-
-type Item struct {
-    node int
-    distance int
-}
-type PriorityQueue []*Item
-func (pq PriorityQueue) Len() int { return len(pq) }
-func (pq PriorityQueue) Less(i, j int) bool { return pq[i].distance < pq[j].distance }
-func (pq PriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }
-func (pq *PriorityQueue) Push(x interface{}) { *pq = append(*pq, x.(*Item)) }
-func (pq *PriorityQueue) Pop() interface{} {
-    old := *pq
-    n := len(old)
-    item := old[n-1]
-    *pq = old[0 : n-1]
-    return item
-}
-
-func dijkstra(n int, adjList [][]struct{node, weight int}, start int) []int {
-    dist := make([]int, n)
-    for i := range dist {
-        dist[i] = 1<<63 - 1 // MaxInt equivalent in Go for infinity representation
+func minimumWeight(edges [][]int, queries [][]int) []int {
+    n := len(edges) + 1
+    tree := make([][][2]int, n)
+    for _, e := range edges {
+        u, v, w := e[0], e[1], e[2]
+        tree[u] = append(tree[u], [2]int{v, w})
+        tree[v] = append(tree[v], [2]int{u, w})
     }
-    dist[start] = 0
-    pq := &PriorityQueue{{start, 0}}
-    heap.Init(pq)
-    
-    for pq.Len() > 0 {
-        current := heap.Pop(pq).(*Item)
-        u := current.node
-        if current.distance > dist[u] {
-            continue // Skip if a shorter path has been found already
+    const LOG = 17 // since n <= 1e5, log2(1e5) < 17
+    parent := make([][]int, n)
+    up := make([][]int, n)
+    depth := make([]int, n)
+    sum := make([]int, n)
+    for i := range parent {
+        parent[i] = make([]int, LOG)
+        up[i] = make([]int, LOG)
+        for j := 0; j < LOG; j++ {
+            parent[i][j] = -1
         }
-        for _, neighbor := range adjList[u] {
-            v, w := neighbor.node, neighbor.weight
-            newDist := dist[u] + w
-            if newDist < dist[v] {
-                dist[v] = newDist
-                heap.Push(pq, &Item{v, newDist})
+    }
+    var dfs func(u, p, d, s int)
+    dfs = func(u, p, d, s int) {
+        parent[u][0] = p
+        depth[u] = d
+        sum[u] = s
+        for _, e := range tree[u] {
+            v, w := e[0], e[1]
+            if v != p {
+                dfs(v, u, d+1, s+w)
             }
         }
     }
-    return dist
-}
-
-func minimumWeight(edges [][]int, queries [][]int) []int {
-    n := len(edges) + 1
-    adjList := make([][]struct{node, weight int}, n)
-   
-    // Build adjacency list from edges input
-    for _, edge := range edges {
-        u, v, w := edge[0], edge[1], edge[2]
-        adjList[u] = append(adjList[u], struct{node, weight int}{v, w})
-        adjList[v] = append(adjList[v], struct{node, weight int}{u, w})
+    dfs(0, -1, 0, 0)
+    for j := 1; j < LOG; j++ {
+        for i := 0; i < n; i++ {
+            if parent[i][j-1] != -1 {
+                parent[i][j] = parent[parent[i][j-1]][j-1]
+            }
+        }
     }
-   
-   result := make([]int, len(queries))
-   	for i, q := range queries {
-src1, src2 , dest:= q[0], q[1], q[2]
-distFromSrc1:= dijkstra(n ,adjList ,src1 )
-distFromSrc2:= dijkstra(n ,adjList ,src2 )
-distToDest:= dijkstra(n ,adjList ,dest )		minWeightSubtree:= 1<<63 -1// MaxInt equivalent initially		for v:=0; v<n ; v++{		if distFromSrc1[v]!=1<<63-1 &&distFromSrc2[v]!=1<<63-1 &&distToDest[v]!=1<<63-1{	totalWeight:=distFromSrc1[v]+distFromSrc2[v]+distToDest[v]	if totalWeight<minWeightSubtree{	minWeightSubtree=totalWeight	}	}	}	result[i]=minWeightSubtree	}	return result	}
+    lca := func(u, v int) int {
+        if depth[u] < depth[v] {
+            u, v = v, u
+        }
+        for k := LOG-1; k >= 0; k-- {
+            if parent[u][k] != -1 && depth[parent[u][k]] >= depth[v] {
+                u = parent[u][k]
+            }
+        }
+        if u == v {
+            return u
+        }
+        for k := LOG-1; k >= 0; k-- {
+            if parent[u][k] != -1 && parent[u][k] != parent[v][k] {
+                u = parent[u][k]
+                v = parent[v][k]
+            }
+        }
+        return parent[u][0]
+    }
+    minSubtreeLCA := func(a, b, c int) int {
+        x := lca(a, b)
+        y := lca(b, c)
+        z := lca(a, c)
+        // The LCA of all three nodes is the 'lowest' one among x, y, z
+        // Since tree, they are on the same path
+        if depth[x] < depth[y] {
+            x = y
+        }
+        if depth[x] < depth[z] {
+            x = z
+        }
+        return x
+    }
+    ans := make([]int, len(queries))
+    for i, q := range queries {
+        a, b, c := q[0], q[1], q[2]
+        u := minSubtreeLCA(a, b, c)
+        total := sum[a] + sum[b] + sum[c] - 2*sum[u]
+        // Additional check (domain-agnostic): ensure that all nodes are distinct and subtree covers all required nodes
+        // This is always true by construction in a tree, but would be checked if generalizing
+        ans[i] = total
+    }
+    return ans
+}
+# @lc code=end
