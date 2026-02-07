6c8a31ace6458d6136dc07a6b8109f17765ecabf
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 12:14:33 +0800

    Batch add 1 solutions: 3414.maximum-score-o...

--- 3414.maximum-score-of-non-overlapping-intervals.cpp
+++ 3414.maximum-score-of-non-overlapping-intervals.cpp
@@ -1,68 +1,49 @@
-#
 # @lc app=leetcode id=3414 lang=cpp
-#
 # [3414] Maximum Score of Non-overlapping Intervals
-#
 
 # @lc code=start
 #include <vector>
 #include <algorithm>
-using namespace std;
+#include <set>
+#include <tuple>
+
 class Solution {
 public:
-    vector<int> maximumWeight(vector<vector<int>>& intervals) {
+    std::vector<int> maximumWeight(std::vector<std::vector<int>>& intervals) {
+        // Sort intervals based on their end time (ri)
+        std::sort(intervals.begin(), intervals.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
+            return a[1] < b[1];
+        });
+        
         int n = intervals.size();
-        vector<tuple<int,int,int,int>> tmp;
-        for (int i = 0; i < n; ++i) {
-            tmp.emplace_back(intervals[i][1], intervals[i][0], intervals[i][2], i);
-        }
-        sort(tmp.begin(), tmp.end());
-        // dp[i][k]: pair of (score, indices) for first i intervals, using k intervals
-        vector<vector<pair<long long, vector<int>>>> dp(n+1, vector<pair<long long, vector<int>>>(5, {0, {}}));
-        vector<int> ends(n);
-        for (int i = 0; i < n; ++i) ends[i] = get<0>(tmp[i]);
-        for (int i = 1; i <= n; ++i) {
-            auto [end, start, weight, idx] = tmp[i-1];
-            // find prev that does not overlap
-            int l = 0, r = i-1, pre = 0;
-            while (l < r) {
-                int m = (l + r) / 2;
-                if (ends[m] < start) l = m+1;
-                else r = m;
-            }
-            pre = (ends[0] < start) ? l+1 : 0;
-            for (int k = 1; k <= 4; ++k) {
-                // not take
-                if (dp[i-1][k].first > dp[i][k].first || 
-                    (dp[i-1][k].first == dp[i][k].first && dp[i-1][k].second < dp[i][k].second)) {
-                    dp[i][k] = dp[i-1][k];
-                }
-                // take
-                if (dp[pre][k-1].first + weight > dp[i][k].first) {
-                    dp[i][k].first = dp[pre][k-1].first + weight;
-                    dp[i][k].second = dp[pre][k-1].second;
-                    dp[i][k].second.push_back(idx);
-                } else if (dp[pre][k-1].first + weight == dp[i][k].first) {
-                    auto cand = dp[pre][k-1].second;
-                    cand.push_back(idx);
-                    if (cand < dp[i][k].second) {
-                        dp[i][k].second = cand;
+        std::vector<int> dp(n, 0);
+        std::vector<std::set<int>> indices(n);
+        dp[0] = intervals[0][2];
+        indices[0].insert(0);
+
+        for (int i = 1; i < n; ++i) {
+            int l = intervals[i][0], w = intervals[i][2];
+            dp[i] = w;
+            indices[i].insert(i);
+            
+            // Binary search for non-overlapping interval using lambda function for custom comparator
+            int low = 0, high = i - 1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (intervals[mid][1] < l) {
+                    if (dp[mid] + w > dp[i]) {
+                        dp[i] = dp[mid] + w;
+                        indices[i] = indices[mid];
+                        indices[i].insert(i);
                     }
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
                 }
             }
-        }
-        long long best = 0;
-        vector<int> ans;
-        for (int k = 1; k <= 4; ++k) {
-            if (dp[n][k].first > best) {
-                best = dp[n][k].first;
-                ans = dp[n][k].second;
-            } else if (dp[n][k].first == best && (ans.empty() || dp[n][k].second < ans)) {
-                ans = dp[n][k].second;
-            }
-        }
-        sort(ans.begin(), ans.end());
-        return ans;
-    }
-};
-# @lc code=end
+            
+            // If not taking current interval gives a better result
+            if (dp[i - 1] > dp[i]) {
+                dp[i] = dp[i - 1];
+                indices[i] = indices[i - 1];
+            }		}		// Find lexicographically smallest set of at most four indices with max score		int maxScoreIndex = std::max_element(dp.begin(), dp.end()) - dp.begin(); auto resultIndices = indices[maxScoreIndex]; std::vector<int> result(resultIndices.begin(), resultIndices.end()); while (result.size() > 4) { result.pop_back(); } std::sort(result.begin(), result.end()); return result; } }; # @lc code=end
