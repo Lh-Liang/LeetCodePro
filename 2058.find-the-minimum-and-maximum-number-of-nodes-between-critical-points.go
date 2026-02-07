b09e01e37a5dd4aadce1dafdb0200b326c73e7e4
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 21:37:50 +0800

    Batch add 1 solutions: 2058.find-the-minimu...

--- 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.go
+++ 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.go
@@ -13,32 +13,34 @@
 * }
 */
 func nodesBetweenCriticalPoints(head *ListNode) []int {
-    if head == nil || head.Next == nil || head.Next.Next == nil {
+    var indices []int
+    var prev *ListNode = nil
+    var curr = head
+    position := 0
+    
+    for curr != nil && curr.Next != nil {
+        if prev != nil {
+            if (curr.Val > prev.Val && curr.Val > curr.Next.Val) || (curr.Val < prev.Val && curr.Val < curr.Next.Val) {
+                indices = append(indices, position)
+            }
+        }
+        prev = curr
+        curr = curr.Next
+        position++
+    }
+    
+    if len(indices) < 2 {
         return []int{-1, -1}
     }
-    firstCriticalIndex := -1
-    lastCriticalIndex := -1
-    minDistance := int(^uint(0) >> 1) // Max Int Value
-    prev, curr := head, head.Next
-    index := 1 // Start from second node because we need prev, curr, next for comparison. 
-    criticalIndices := []int{} 
     
-    for curr.Next != nil {
-        next := curr.Next 
-        if (curr.Val > prev.Val && curr.Val > next.Val) || (curr.Val < prev.Val && curr.Val < next.Val) { 
-            if firstCriticalIndex == -1 { 
-                firstCriticalIndex = index 
-            } else { 
-                minDistance = min(minDistance, index-lastCriticalIndex) 
-            } 
-            lastCriticalIndex = index 
-            criticalIndices = append(criticalIndices, index) 
-        } 
-        prev = curr 
-        curr = next 
-        index++ 
-    } 
-    if len(criticalIndices) < 2 { 
-        return []int{-1, -1} 
-    } 
-to give }; minDistance = int(^uint(0) >>  1); return []int{minDistance, criticalIndices[len(criticalIndices)-1]-criticalIndices[0]} 	   }		// @lc code=end
+    minDistance := indices[1] - indices[0]
+    maxDistance := indices[len(indices)-1] - indices[0]
+   
+   for i := 1; i < len(indices); i++ { 
+       if indices[i] - indices[i-1] < minDistance { 
+           minDistance = indices[i] - indices[i-1] 
+       } 
+   } 
+   return []int{minDistance, maxDistance} 
+}
+# @lc code=end
