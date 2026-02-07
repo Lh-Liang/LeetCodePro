1a165e1d8e4574d6be6a70c4e69bd2dee01cf658
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 01:34:23 +0800

    Batch add 1 solutions: 3503.longest-palindr...

--- 3503.longest-palindrome-after-substring-concatenation-i.go
+++ 3503.longest-palindrome-after-substring-concatenation-i.go
@@ -3,33 +3,36 @@
 #
 # [3503] Longest Palindrome After Substring Concatenation I
 #
+
 # @lc code=start
 func longestPalindrome(s string, t string) int {
+    // Helper function to check if a string is a palindrome
+    isPalindrome := func(str string) bool {
+        l, r := 0, len(str)-1
+        for l < r {
+            if str[l] != str[r] {
+                return false
+            }
+            l++
+            r--
+        }
+        return true
+    }
+    
     maxLen := 0
-    for i := range s {
-        for j := i; j <= len(s); j++ {
-            subS := s[i:j]
-            for k := range t {
-                for l := k; l <= len(t); l++ {
-                    subT := t[k:l]
-                    concat := subS + subT
-                    if isPalindrome(concat) && len(concat) > maxLen {
-                        maxLen = len(concat)
-                    }
-                }
+    // Check all combinations of substrings from s and t that could form a palindrome
+    for i := 0; i <= len(s); i++ {
+        for j := 0; j <= len(t); j++ {
+            candidate1 := s[:i] + t[j:]
+            candidate2 := t[:j] + s[i:]
+            if isPalindrome(candidate1) && len(candidate1) > maxLen {
+                maxLen = len(candidate1)
+            }
+            if isPalindrome(candidate2) && len(candidate2) > maxLen {
+                maxLen = len(candidate2)
             }
         }
     }
-    return maxLen 
-}
- 
-func isPalindrome(s string) bool { 
-    n := len(s) 
-    for i := 0; i < n/2; i++ { 
-        if s[i] != s[n-i-1] { 
-            return false 
-        } 
-    } 
-    return true 
+    return maxLen
 }
 # @lc code=end
