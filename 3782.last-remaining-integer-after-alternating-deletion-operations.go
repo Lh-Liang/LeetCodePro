75c2b000b95991752bb67674d00592a9df0ff5ae
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 06:56:48 +0800

    Batch add 1 solutions: 3782.last-remaining-...

--- 3782.last-remaining-integer-after-alternating-deletion-operations.go
+++ 3782.last-remaining-integer-after-alternating-deletion-operations.go
@@ -1,6 +1,22 @@
+#
+# @lc app=leetcode id=3782 lang=golang
+#
+# [3782] Last Remaining Integer After Alternating Deletion Operations
+#
+# @lc code=start
 func lastInteger(n int64) int64 {
-    if n == 1 {
-        return 1
+    head := int64(1)
+    step := int64(1)
+    left := true
+    remaining := n
+    for remaining > 1 {
+        if left || remaining%2 == 1 {
+            head += step
+        }
+        remaining /= 2
+        step *= 2
+        left = !left
     }
-    return 2 * (n/2 + 1 - lastInteger(n/2))
+    return head
 }
+# @lc code=end
