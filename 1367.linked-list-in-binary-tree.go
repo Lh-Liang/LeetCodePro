670ce3cd8ca53b73ff98454071097ac7170eda4a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 21:22:51 +0800

    Batch add 1 solutions: 1367.linked-list-in-...

--- 1367.linked-list-in-binary-tree.go
+++ 1367.linked-list-in-binary-tree.go
@@ -1,1 +1,33 @@
-The problem has been solved by implementing DFS in Go.
+#
+# @lc app=leetcode id=1367 lang=golang
+#
+# [1367] Linked List in Binary Tree
+#
+# @lc code=start
+/**
+* Definition for singly-linked list.
+* type ListNode struct {
+*     Val int
+*     Next *ListNode
+* }
+*/
+/**
+* Definition for a binary tree node.
+* type TreeNode struct {
+*     Val int
+*     Left *TreeNode
+*     Right *TreeNode
+* }
+*/
+func isSubPath(head *ListNode, root *TreeNode) bool { 
+    if head == nil { return true } 
+    if root == nil { return false } 
+    return dfs(head, root) || isSubPath(head, root.Left) || isSubPath(head, root.Right) 
+}
+func dfs(head *ListNode, node *TreeNode) bool { 
+    if head == nil { return true } 
+    if node == nil { return false } 
+    if head.Val != node.Val { return false } 
+    return dfs(head.Next, node.Left) || dfs(head.Next, node.Right) 
+}
+# @lc code=end
