acac52f714d8de2be5fb9d83e6accf99e0661663
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 11:50:41 +0800

    Batch add 1 solutions: 2816.double-a-number...

--- 2816.double-a-number-represented-as-a-linked-list.cpp
+++ 2816.double-a-number-represented-as-a-linked-list.cpp
@@ -3,41 +3,34 @@
 #
 # [2816] Double a Number Represented as a Linked List
 #
-
 # @lc code=start
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode() : val(0), next(nullptr) {}
+*     ListNode(int x) : val(x), next(nullptr) {}
+*     ListNode(int x, ListNode *next) : val(x), next(next) {}
+* };
+*/
 class Solution {
 public:
     ListNode* doubleIt(ListNode* head) {
-        // If the leading digit is >= 5, doubling it will create a new digit '1' at the front.
-        if (head->val > 4) {
-            head = new ListNode(0, head);
-        }
-        
-        ListNode* curr = head;
-        while (curr != nullptr) {
-            // The current node's value becomes (val * 2) % 10.
-            // We look ahead to the next node to see if it will produce a carry.
-            int newValue = (curr->val * 2) % 10;
-            
-            if (curr->next != nullptr && curr->next->val > 4) {
-                newValue += 1;
+        if (!head) return nullptr; // Edge case handled but not needed per constraints
+        int carry = 0; // Initialize carry to handle overflow
+        ListNode *current = head; // Pointer to traverse the list
+        while (current != nullptr) { // Traverse each node in the list
+            int newVal = current->val * 2 + carry; // Double current value and add carry
+            current->val = newVal % 10; // Update node value with last digit of newVal
+            carry = newVal / 10; // Update carry for overflow
+            if (current->next == nullptr && carry > 0) { // If at last node and have a carry
+                current->next = new ListNode(carry); // Add new node for remaining carry
+                break;
             }
-            
-            curr->val = newValue;
-            curr = curr->next;
+            current = current->next; // Move to next node
         }
-        
-        return head;
+        return head; // Return modified list head
     }
-};
+}; 
 # @lc code=end
