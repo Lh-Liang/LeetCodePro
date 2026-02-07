04015663a923cb746fdccd757264deef0de15fc8
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 13:05:20 +0800

    Batch add 1 solutions: 3503.longest-palindr...

--- 3503.longest-palindrome-after-substring-concatenation-i.cpp
+++ 3503.longest-palindrome-after-substring-concatenation-i.cpp
@@ -3,58 +3,25 @@
 #
 # [3503] Longest Palindrome After Substring Concatenation I
 #
-
 # @lc code=start
 class Solution {
 public:
     int longestPalindrome(string s, string t) {
-        int ans = 0;
-        auto is_palindrome = [](const string& x) {
-            int l = 0, r = x.size() - 1;
-            while (l < r) {
-                if (x[l] != x[r]) return false;
-                ++l; --r;
-            }
-            return true;
-        };
-        int n = s.size(), m = t.size();
-        // Check all substrings of s and t concatenated
-        for (int i = 0; i <= n; ++i) {
-            for (int j = i; j <= n; ++j) {
-                string subs = s.substr(i, j - i);
-                for (int p = 0; p <= m; ++p) {
-                    for (int q = p; q <= m; ++q) {
-                        string subt = t.substr(p, q - p);
-                        string candidate = subs + subt;
-                        if (!candidate.empty() && is_palindrome(candidate)) {
-                            int len = candidate.size();
-                            if (len > ans) ans = len;
-                        }
-                    }
-                }
-            }
+        vector<int> freqS(26, 0), freqT(26, 0);
+        // Count frequencies of characters in both strings
+        for (char c : s) ++freqS[c - 'a'];
+        for (char c : t) ++freqT[c - 'a'];
+        int maxLength = 0;
+        bool oddFrequencyFound = false;
+        // Sum pairs and detect if there's any odd frequency to add one more to result
+        for (int i = 0; i < 26; ++i) {
+            int totalFreq = freqS[i] + freqT[i];
+            maxLength += (totalFreq / 2) * 2; // Add maximum even part
+            if (totalFreq % 2 == 1) oddFrequencyFound = true;
         }
-        // Check all substrings of s
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j <= n; ++j) {
-                string subs = s.substr(i, j - i);
-                if (is_palindrome(subs)) {
-                    int len = subs.size();
-                    if (len > ans) ans = len;
-                }
-            }
-        }
-        // Check all substrings of t
-        for (int i = 0; i < m; ++i) {
-            for (int j = i + 1; j <= m; ++j) {
-                string subt = t.substr(i, j - i);
-                if (is_palindrome(subt)) {
-                    int len = subt.size();
-                    if (len > ans) ans = len;
-                }
-            }
-        }
-        return ans;
+        // If there's an odd frequency, we can have a center of a palindrome
+        if (oddFrequencyFound) ++maxLength;
+        return maxLength;
     }
 };
 # @lc code=end
