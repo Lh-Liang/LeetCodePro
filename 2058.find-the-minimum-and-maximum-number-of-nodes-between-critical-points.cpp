57d0d895261fa69f164c2fb515ce554d2edea383
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 16:34:14 +0800

    Batch add 1 solutions: 2058.find-the-minimu...

--- 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058.find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -3,6 +3,7 @@
 #
 # [2058] Find the Minimum and Maximum Number of Nodes Between Critical Points
 #
+
 # @lc code=start
 /**
 * Definition for singly-linked list.
@@ -17,29 +18,25 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        int index = 0;
-        int firstCritical = -1; 
-        int lastCritical = -1; 
-        vector<int> criticalIndices; 
-        ListNode* prev = nullptr; 
-        ListNode* curr = head; 
-        while (curr && curr->next) { 
-            if (prev && ((curr->val > prev->val && curr->val > curr->next->val) || (curr->val < prev->val && curr->val < curr->next->val))) { 
-                if (firstCritical == -1) firstCritical = index; 
-                lastCritical = index; 
-                criticalIndices.push_back(index); 
-            } 
-            prev = curr; 
-            curr = curr->next; 
-            index++; 
+        if (!head || !head->next || !head->next->next) return {-1, -1};
+        vector<int> critical_points;
+        ListNode *prev = head, *curr = head->next;
+        int index = 1;
+        while (curr->next) {
+            if ((curr->val > prev->val && curr->val > curr->next->val) || 
+                (curr->val < prev->val && curr->val < curr->next->val)) {
+                critical_points.push_back(index);
+            }
+            prev = curr;
+            curr = curr->next;
+            index++;
+        }
+        if (critical_points.size() < 2) return {-1, -1}; 
+        int min_dist = INT_MAX, max_dist = critical_points.back() - critical_points.front(); 
+        for (size_t i = 1; i < critical_points.size(); ++i) { 
+            min_dist = min(min_dist, critical_points[i] - critical_points[i-1]); 
         } 
-        if (criticalIndices.size() < 2) return {-1, -1}; 
-        int minDistance = INT_MAX; 
-        for (int i = 1; i < criticalIndices.size(); ++i) { 
-            minDistance = min(minDistance, criticalIndices[i] - criticalIndices[i-1]); 
-        } 
-        int maxDistance = lastCritical - firstCritical; 
-        return {minDistance, maxDistance}; 
-    }
-};
-# @lc code=end
+        return {min_dist, max_dist}; 
+    } 
+}; 
+lc code=end
