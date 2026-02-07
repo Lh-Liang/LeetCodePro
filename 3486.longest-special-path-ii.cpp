4c5a7a57a5574f957088df03d7d388f6bf6940d4
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 12:58:48 +0800

    Batch add 1 solutions: 3486.longest-special...

--- 3486.longest-special-path-ii.cpp
+++ 3486.longest-special-path-ii.cpp
@@ -10,35 +10,40 @@ public:
     vector<int> longestSpecialPath(vector<vector<int>>& edges, vector<int>& nums) {
         int n = nums.size();
         vector<vector<pair<int, int>>> adj(n);
-        for (auto& e : edges) {
-            int u = e[0], v = e[1], w = e[2];
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
+        for (auto& edge : edges) {
+            adj[edge[0]].emplace_back(edge[1], edge[2]);
+            adj[edge[1]].emplace_back(edge[0], edge[2]);
         }
-        unordered_map<int, int> count;
-        int maxLen = 0, minNodes = INT_MAX;
-        function<void(int,int,int,int,bool)> dfs = [&](int u, int parent, int currLen, int currNodes, bool dupUsed) {
-            count[nums[u]]++;
-            if (count[nums[u]] == 2 && dupUsed) {
-                count[nums[u]]--;
-                return;
+        
+        int maxLength = 0;
+        int minNodes = INT_MAX;
+        unordered_map<int, int> nodeValueCounts;
+        vector<bool> visited(n, false);
+
+        function<void(int, int, int)> dfs = [&](int node, int length, int nodesCount) {
+            nodeValueCounts[nums[node]]++;
+            visited[node] = true;
+            
+            int duplicateCount = 0;
+            for (const auto& p : nodeValueCounts) {
+                if (p.second > 1) duplicateCount++;
             }
-            bool newDupUsed = dupUsed || (count[nums[u]] == 2);
-            // Update result if at a leaf or at every node
-            if (currLen > maxLen) {
-                maxLen = currLen;
-                minNodes = currNodes;
-            } else if (currLen == maxLen) {
-                minNodes = min(minNodes, currNodes);
+            if (duplicateCount <= 1) {
+                if (length > maxLength || (length == maxLength && nodesCount < minNodes)) {
+                    maxLength = length;
+                    minNodes = nodesCount;
+                }
+                for (auto& [neighbor, weight] : adj[node]) {
+                    if (!visited[neighbor]) {
+                        dfs(neighbor, length + weight, nodesCount + 1);
+                    }
+                }
             }
-            for (auto& [v, w] : adj[u]) {
-                if (v == parent) continue;
-                dfs(v, u, currLen + w, currNodes + 1, newDupUsed);
-            }
-            count[nums[u]]--;
-        };
-        dfs(0, -1, 0, 1, false);
-        return {maxLen, minNodes};
-    }
-};
-# @lc code=end
+            
+nodenodeValueCounts[nums[node]]--;
+nodenodeif (nodeValueCounts[nums[node]] == 0) {
+nodenodenodeValueCounts.erase(nums[node]);
+nodenodene}
+nodenodevisited[node] = false;
+odendfs(0 /*root*/, 0 /*path length*/, 1 /*starting with root*/);
+odreturn {maxLength,minNodes};od}}; # @lc code=end
