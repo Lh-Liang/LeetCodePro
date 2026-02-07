c0ab9aa24caed277d386c5a03f56105374a509a3
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 03:02:27 +0800

    Batch add 1 solutions: 3695.maximize-altern...

--- 3695.maximize-alternating-sum-using-swaps.go
+++ 3695.maximize-alternating-sum-using-swaps.go
@@ -6,7 +6,43 @@
 
 # @lc code=start
 func maxAlternatingSum(nums []int, swaps [][]int) int64 {
-    // Implement the logic to calculate maximum alternating sum here
-    return 0 // Placeholder return statement
-}
-# @lc code=end
+    n := len(nums)
+    parent := make([]int, n)
+    for i := range parent {
+        parent[i] = i
+    }
+    var find func(x int) int
+    find = func(x int) int {
+        if parent[x] != x {
+            parent[x] = find(parent[x])
+        }
+        return parent[x]
+    }
+    union := func(x, y int) {
+        rootX := find(x)
+        rootY := find(y)
+        if rootX != rootY {
+            parent[rootX] = rootY
+        }
+    }
+    for _, swap := range swaps {
+        union(swap[0], swap[1])
+    }
+    components := make(map[int][]int)
+    for i := 0; i < n; i++ {
+        root := find(i)
+        components[root] = append(components[root], nums[i])
+    }
+    result := int64(0)
+    for _, comp := range components {
+        sort.Slice(comp, func(i, j int) bool { return comp[i] > comp[j] }) // Sort in descending order to maximize alternating sum.
+       for i, num := range comp { 
+           if i%2 == 0 { 
+               result += int64(num) 
+           } else { 
+               result -= int64(num) 
+           } 
+       } 
+   } 
+   return result 
+} # @lc code=end
