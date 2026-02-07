cc1477c59900b4e8c358cc9ea4be78a8a6a7411b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 14:27:24 +0800

    Batch add 1 solutions: 3695.maximize-altern...

--- 3695.maximize-alternating-sum-using-swaps.cpp
+++ 3695.maximize-alternating-sum-using-swaps.cpp
@@ -1,51 +1,36 @@
-#
 # @lc app=leetcode id=3695 lang=cpp
-#
 # [3695] Maximize Alternating Sum Using Swaps
-#
+# 
 # @lc code=start
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    vector<int> parent;
-    int find(int x) {
-        if (parent[x] != x) parent[x] = find(parent[x]);
-        return parent[x];
-    }
-    void unite(int x, int y) {
-        parent[find(x)] = find(y);
-    }
     long long maxAlternatingSum(vector<int>& nums, vector<vector<int>>& swaps) {
         int n = nums.size();
-        parent.resize(n);
-        for (int i = 0; i < n; ++i) parent[i] = i;
-        for (const auto& s : swaps) {
-            unite(s[0], s[1]);
+        vector<int> parent(n);
+        iota(parent.begin(), parent.end(), 0); // Initialize union-find structure
+        function<int(int)> find = [&](int x) { return x == parent[x] ? x : parent[x] = find(parent[x]); }; // Find with path compression
+        auto unite = [&](int x, int y) { parent[find(x)] = find(y); }; // Union operation
+        // Unite indices based on swaps
+        for (const auto& s : swaps)
+            unite(s[0], s[1]); 
+        unordered_map<int, vector<int>> groups; 
+        for (int i = 0; i < n; ++i) 
+            groups[find(i)].push_back(i); 
+        vector<int> sorted_nums(n); 
+        for (auto& [root, group] : groups) {
+            vector<int> values;
+            for (int idx : group)
+                values.push_back(nums[idx]);
+            sort(values.begin(), values.end(), greater<>()); // Sort in descending order to maximize sum at even positions
+            sort(group.begin(), group.end());
+            for (int i = 0; i < group.size(); ++i)
+                sorted_nums[group[i]] = values[i];
         }
-        unordered_map<int, vector<int>> groups;
+        long long max_sum = 0;
         for (int i = 0; i < n; ++i) {
-            groups[find(i)].push_back(i);
+            if (i % 2 == 0) max_sum += sorted_nums[i]; else max_sum -= sorted_nums[i];
         }
-        vector<int> res(nums);
-        for (const auto& [p, idxs] : groups) {
-            vector<int> vals;
-            for (int idx : idxs) vals.push_back(nums[idx]);
-            sort(idxs.begin(), idxs.end());
-            sort(vals.begin(), vals.end(), greater<int>());
-            for (int i = 0; i < idxs.size(); ++i) {
-                res[idxs[i]] = vals[i];
-            }
-        }
-        long long ans = 0;
-        for (int i = 0; i < n; ++i) {
-            if (i % 2 == 0) ans += res[i];
-            else ans -= res[i];
-        }
-        return ans;
+        return max_sum;
     }
 };
 # @lc code=end
