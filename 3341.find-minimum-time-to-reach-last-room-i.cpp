701d98664d608211c416353887a0e43ded9a0a9b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 11:55:38 +0800

    Batch add 1 solutions: 3341.find-minimum-ti...

--- 3341.find-minimum-time-to-reach-last-room-i.cpp
+++ 3341.find-minimum-time-to-reach-last-room-i.cpp
@@ -1,9 +1,8 @@
-#
+# 
 # @lc app=leetcode id=3341 lang=cpp
-#
+# 
 # [3341] Find Minimum Time to Reach Last Room I
-#
-
+# 
 # @lc code=start
 #include <vector>
 #include <queue>
@@ -12,28 +11,28 @@ using namespace std;
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        int n = moveTime.size(), m = moveTime[0].size();
+        int n = moveTime.size();
+        int m = moveTime[0].size();
         vector<vector<int>> minTime(n, vector<int>(m, INT_MAX));
         priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
-        pq.push({0, 0, 0}); // time, x, y
+        pq.push({0, 0, 0}); // {time, row, col}
         minTime[0][0] = 0;
-        vector<pair<int, int>> dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+        vector<int> dir = {-1, 0, 1, 0, -1}; // Directions for moving in grid
         while (!pq.empty()) {
-            auto [t, x, y] = pq.top(); pq.pop();
-            if (x == n-1 && y == m-1) return t;
-            if (t > minTime[x][y]) continue;
-            for (auto& d : dirs) {
-                int nx = x + d.first, ny = y + d.second;
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                    int arrive = max(t + 1, moveTime[nx][ny]);
-                    if (arrive < minTime[nx][ny]) {
-                        minTime[nx][ny] = arrive;
-                        pq.push({arrive, nx, ny});
+            auto [t, r, c] = pq.top(); pq.pop();
+            if (r == n - 1 && c == m - 1) return t; // Reached last room
+            for (int i = 0; i < 4; ++i) { // Explore all directions
+                int nr = r + dir[i], nc = c + dir[i + 1];
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+                    int waitTime = max(t + 1, moveTime[nr][nc]); // Wait if needed before moving
+                    if (waitTime < minTime[nr][nc]) {
+                        minTime[nr][nc] = waitTime;
+                        pq.push({waitTime, nr, nc});
                     }
                 }
             }
         }
-        return -1;
+        return -1; // Should never reach here if input is valid as per constraint. 
     }
 };
 # @lc code=end
