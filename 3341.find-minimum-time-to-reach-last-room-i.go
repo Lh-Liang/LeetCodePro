bd546133566ee44597e5687393c397595c2abf7f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 04:22:53 +0800

    Batch add 1 solutions: 3341.find-minimum-ti...

--- 3341.find-minimum-time-to-reach-last-room-i.go
+++ 3341.find-minimum-time-to-reach-last-room-i.go
@@ -3,40 +3,67 @@
 #
 # [3341] Find Minimum Time to Reach Last Room I
 #
-
 # @lc code=start
+import (
+    "container/heap"
+    "math"
+)
+
+type Item struct {
+    time, x, y int
+}
+
+type PriorityQueue []Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+func (pq PriorityQueue) Less(i, j int) bool { return pq[i].time < pq[j].time }
+func (pq PriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }
+func (pq *PriorityQueue) Push(x interface{}) {
+    *pq = append(*pq, x.(Item))
+}
+func (pq *PriorityQueue) Pop() interface{} {
+    old := *pq
+    n := len(old)
+    item := old[n-1]
+    *pq = old[:n-1]
+    return item
+}
+
 func minTimeToReach(moveTime [][]int) int {
-    n := len(moveTime)
-    m := len(moveTime[0])
-    directions := [][2]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}} // Right, Down, Left, Up
-    visited := make([][]int, n)
-    for i := range visited {
-        visited[i] = make([]int, m)
-        for j := range visited[i] {
-            visited[i][j] = int(1e9) // Initialize with a large number
+    n, m := len(moveTime), len(moveTime[0])
+    dirs := [][]int{{0,1},{1,0},{0,-1},{-1,0}}
+    dist := make([][]int, n)
+    for i := 0; i < n; i++ {
+        dist[i] = make([]int, m)
+        for j := 0; j < m; j++ {
+            dist[i][j] = math.MaxInt32
         }
     }
-    
-    queue := []struct{row, col, time int}{{0, 0, 0}}
-    visited[0][0] = 0
-    
-    for len(queue) > 0 {
-        current := queue[0]
-        queue = queue[1:]
-        
-        for _, dir := range directions {
-            newRow := current.row + dir[0]
-            newCol := current.col + dir[1]
-            if newRow >= 0 && newRow < n && newCol >= 0 && newCol < m {
-                enterTime := max(current.time + 1, moveTime[newRow][newCol])
-                if enterTime < visited[newRow][newCol] {
-                    visited[newRow][newCol] = enterTime
-                    queue = append(queue, struct{row, col, time int}{newRow, newCol, enterTime})
+    dist[0][0] = 0
+    pq := &PriorityQueue{}
+    heap.Init(pq)
+    heap.Push(pq, Item{0, 0, 0})
+    for pq.Len() > 0 {
+        it := heap.Pop(pq).(Item)
+        t, x, y := it.time, it.x, it.y
+        if x == n-1 && y == m-1 {
+            return t
+        }
+        if t > dist[x][y] {
+            continue
+        }
+        for _, d := range dirs {
+            nx, ny := x + d[0], y + d[1]
+            if nx >= 0 && nx < n && ny >= 0 && ny < m {
+                arrive := max(t+1, moveTime[nx][ny])
+                if arrive < dist[nx][ny] {
+                    dist[nx][ny] = arrive
+                    heap.Push(pq, Item{arrive, nx, ny})
                 }
             }
         }
     }
-    return visited[n-1][m-1]
+    return -1
 }
 
 func max(a, b int) int {
