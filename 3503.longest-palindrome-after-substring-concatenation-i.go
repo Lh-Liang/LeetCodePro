8f2975f47cf79e34ab11fba17594e97708bbea6d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 05:05:56 +0800

    Batch add 1 solutions: 3503.longest-palindr...

--- 3503.longest-palindrome-after-substring-concatenation-i.go
+++ 3503.longest-palindrome-after-substring-concatenation-i.go
@@ -6,33 +6,42 @@
 
 # @lc code=start
 func longestPalindrome(s string, t string) int {
-    // Helper function to check if a string is a palindrome
-    isPalindrome := func(str string) bool {
-        l, r := 0, len(str)-1
-        for l < r {
-            if str[l] != str[r] {
-                return false
-            }
-            l++
-            r--
-        }
-        return true
-    }
-    
     maxLen := 0
-    // Check all combinations of substrings from s and t that could form a palindrome
+    // Precompute all substrings of s
+    subsS := make([]string, 0)
     for i := 0; i <= len(s); i++ {
-        for j := 0; j <= len(t); j++ {
-            candidate1 := s[:i] + t[j:]
-            candidate2 := t[:j] + s[i:]
-            if isPalindrome(candidate1) && len(candidate1) > maxLen {
-                maxLen = len(candidate1)
-            }
-            if isPalindrome(candidate2) && len(candidate2) > maxLen {
-                maxLen = len(candidate2)
+        for j := i; j <= len(s); j++ {
+            subsS = append(subsS, s[i:j])
+        }
+    }
+    // Precompute all substrings of t
+    subsT := make([]string, 0)
+    for i := 0; i <= len(t); i++ {
+        for j := i; j <= len(t); j++ {
+            subsT = append(subsT, t[i:j])
+        }
+    }
+    // For each pair of substrings, check if concatenation is a palindrome
+    for _, ss := range subsS {
+        for _, st := range subsT {
+            candidate := ss + st
+            if isPalindrome(candidate) {
+                if len(candidate) > maxLen {
+                    maxLen = len(candidate)
+                }
             }
         }
     }
     return maxLen
+}
+
+func isPalindrome(s string) bool {
+    n := len(s)
+    for i := 0; i < n/2; i++ {
+        if s[i] != s[n-1-i] {
+            return false
+        }
+    }
+    return true
 }
 # @lc code=end
