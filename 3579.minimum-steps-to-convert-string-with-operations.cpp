5f5e92b2d8673e002ae9974a3626524b4a678661
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 19:28:33 +0800

    Batch add 1 solutions: 3579.minimum-steps-t...

--- 3579.minimum-steps-to-convert-string-with-operations.cpp
+++ 3579.minimum-steps-to-convert-string-with-operations.cpp
@@ -1,34 +1,34 @@
-# \@lc app=leetcode id=3579 lang=cpp
+#
+# @lc app=leetcode id=3579 lang=cpp
+#
 # [3579] Minimum Steps to Convert String with Operations
+#
 
-# \@lc code=start
+# @lc code=start
 class Solution {
 public:
     int minOperations(string word1, string word2) {
         int n = word1.size();
-        vector<int> dp(n + 1, INT_MAX);
-        dp[0] = 0; // Base case for empty substring transformation
-        
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                // Check if substring word1[j:i] can be directly transformed to word2[j:i]
-                if (canTransform(word1.substr(j, i - j), word2.substr(j, i - j))) {
-                    dp[i] = min(dp[i], dp[j] + costToTransform(word1.substr(j, i - j), word2.substr(j, i - j)));
-                }
-            }
-        }
-        return dp[n];
-    }
-    
-private:
-    bool canTransform(const string& s1, const string& s2) {
-        // Implement logic to determine if s1 can be transformed into s2 with available ops
-        return true; // Placeholder logic
-    }
-    
-    int costToTransform(const string& s1, const string& s2) {
-        // Implement logic to compute cost of transforming s1 into s2 using minimal ops
-        return 0; // Placeholder logic
-    }
-};
-# \@lc code=end
+        int operations = 0;
+        int i = 0;
+        while (i < n) {
+            if (word1[i] != word2[i]) {
+                int j = i;
+                while (j < n && word1[j] != word2[j]) j++;
+                // Operations needed for substring word1[i:j]
+                int len = j - i;
+                if (len == 2) { // Special case optimization for length 2
+                    operations += 1; // Either swap or reverse is enough
+                } else { 
+                    // General case: 3 operations (reverse, replace x2) are enough for any len > 2 
+                    operations += 3; 
+                } 
+                i = j; // Move past this segment 
+            } else { 
+                i++; 
+            } 
+        } 
+        return operations; 
+    } 
+}; 
+# @lc code=end
