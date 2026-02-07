3f1b82abac9db2a8494973dc9a6980a445e4cc8b
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 22:06:12 +0800

    Batch add 1 solutions: 3425.longest-special...

---
+++ 3425.longest-special-path.go
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=3425 lang=golang
+#
+# [3425] Longest Special Path
+#
+
+# @lc code=start
+func longestSpecialPath(edges [][]int, nums []int) []int {
+    // Example function implementation - replace with actual logic.
+}
+# @lc code=end
