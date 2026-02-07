246673b07be8c95fee62e2b21d8179cd44c426bf
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 16:38:34 +0800

    Batch add 1 solutions: 2296.design-a-text-e...

--- 2296.design-a-text-editor.cpp
+++ 2296.design-a-text-editor.cpp
@@ -1,11 +1,44 @@
-#\n# @lc app=leetcode id=2296 lang=cpp\n#\n# [2296] Design a Text Editor\n#\n# @lc code=start\nclass TextEditor {\npublic:\n    std::string leftText;\n    std::string rightText;\n    \n    TextEditor() {}\n    \n    void addText(std::string text) {\n        leftText += text;\n    }\n    \n    int deleteText(int k) {\n        int count = std::min(k, static_cast<int>(leftText.size()));\n        leftText.erase(leftText.end() - count, leftText.end());\n        return count;\n    }\n    \n    std::string cursorLeft(int k) {\n        int moveCount = std::min(k, static_cast<int>(leftText.size()));\n        rightText.insert(rightText.begin(), leftText.end() - moveCount, leftText.end());\n        leftText.erase(leftText.end() - moveCount, leftText.end());\n        return getLeftContext();   \nn}\nnstd::string cursorRight(int k) {nnint moveCount = std::min(k, static_cast<int>(rightText.size()));nnleftText.append(rightText.begin(), rightText.begin() + moveCount);nnrightText.erase(rightText.begin(), rightText.begin() + moveCount);nnreturn getLeftContext();   \nn}nnprivate:nnstd::string getLeftContext() const {nnint len = std::min(10, static_cast<int>(leftText.size()));nnreturn leftText.substr(leftText.size() - len); nn}\nn};
+#
+# @lc app=leetcode id=2296 lang=cpp
+#
+# [2296] Design a Text Editor
+#
 
-/**
- * Your TextEditor object will be instantiated and called as such:
- * TextEditor* obj = new TextEditor();
- * obj->addText(text);
- * int param_2 = obj->deleteText(k);
- * std::string param_3 = obj->cursorLeft(k);
- * std::string param_4 = obj->cursorRight(k);
- */
-# @lc code=end
+# @lc code=start
+class TextEditor {
+public:
+    std::deque<char> left; // Characters to the left of the cursor
+    std::deque<char> right; // Characters to the right of the cursor
+    
+    TextEditor() {
+        // Initially, both sides are empty
+    }
+    
+    void addText(std::string text) {
+        for (char c : text) {
+            left.push_back(c); // Add each character to the left deque where the cursor is
+        }
+    }
+    
+    int deleteText(int k) {
+        int count = 0;
+        while (k > 0 && !left.empty()) {
+            left.pop_back(); // Remove characters from left of cursor up to k times or until empty
+            k--; count++;
+        }
+        return count; // Return number of characters actually deleted
+    }
+    
+    std::string cursorLeft(int k) {
+        while (k > 0 && !left.empty()) {
+            right.push_front(left.back()); // Move characters from left to right up to k times or until left is empty
+            left.pop_back(); k--; 
+        }
+        return getLeftView(); // Return viewable string from left deque after moving cursor left
+    }
+    
+    std::string cursorRight(int k) {
+        while (k > 0 && !right.empty()) {
+            left.push_back(right.front()); // Move characters from right to left up to k times or until right is empty
+            right.pop_front(); k--; 
+        }  	     	     	     	     	       	     	       return getLeftView(); // Return viewable string from left deque after moving cursor right   	     	}         	     	               std::string getLeftView() {       int len = std::min(10, (int)left.size()); return std::string(left.end() - len, left.end());   }   }; /* Your TextEditor object will be instantiated and called as such: TextEditor* obj = new TextEditor(); obj->addText(text); int param_2 = obj->deleteText(k); std::string param_3 = obj->cursorLeft(k); std::string param_4 = obj->cursorRight(k); */ # @lc code=end
