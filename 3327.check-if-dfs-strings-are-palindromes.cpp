8b3489f5c17b62e959bad1831df5905c2d374a76
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 16:43:20 +0800

    Batch add 1 solutions: 3327.check-if-dfs-st...

--- 3327.check-if-dfs-strings-are-palindromes.cpp
+++ 3327.check-if-dfs-strings-are-palindromes.cpp
@@ -3,21 +3,26 @@
 #
 # [3327] Check if DFS Strings Are Palindromes
 #
-# @lc code=start
-#include <vector>
-#include <string>
-#include <functional>
-using namespace std;
 
+# @lc code=start
 class Solution {
 public:
     vector<bool> findAnswer(vector<int>& parent, string s) {
-        int n = s.size();
+        int n = parent.size();
         vector<vector<int>> children(n);
         for (int i = 1; i < n; ++i) {
             children[parent[i]].push_back(i);
         }
-
+        
+        vector<bool> answer(n);
+        
+        function<void(int, string&)> dfs = [&](int x, string& dfsStr) {
+            for (int y : children[x]) {
+                dfs(y, dfsStr);
+            }
+            dfsStr.push_back(s[x]);
+        };
+        
         auto isPalindrome = [](const string& str) {
             int l = 0, r = str.size() - 1;
             while (l < r) {
@@ -25,22 +30,12 @@ public:
             }
             return true;
         };
-
-        vector<bool> answer(n);
-        function<void(int)> dfs = [&](int node) {
-            string dfsStr;
-            function<void(int)> dfsHelper = [&](int x) {
-                for (int y : children[x]) dfsHelper(y);
-                dfsStr.push_back(s[x]);
-            };
-            dfsHelper(node);
-            answer[node] = isPalindrome(dfsStr);
-        };
-
+        
         for (int i = 0; i < n; ++i) {
-            dfs(i);
+            string dfsStr;
+            dfs(i, dfsStr);
+            answer[i] = isPalindrome(dfsStr);
         }
-
         return answer;
     }
 };
