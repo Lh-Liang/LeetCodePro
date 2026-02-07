d779a2ca63316c51cf2ad39ff29626652b12b3d8
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 11:53:45 +0800

    Batch add 1 solutions: 3327.check-if-dfs-st...

--- 3327.check-if-dfs-strings-are-palindromes.cpp
+++ 3327.check-if-dfs-strings-are-palindromes.cpp
@@ -3,32 +3,44 @@
 #
 # [3327] Check if DFS Strings Are Palindromes
 #
-
 # @lc code=start
+#include <vector>
+#include <string>
+#include <functional>
+using namespace std;
+
 class Solution {
 public:
     vector<bool> findAnswer(vector<int>& parent, string s) {
-        int n = parent.size();
-        vector<vector<int>> tree(n);
+        int n = s.size();
+        vector<vector<int>> children(n);
         for (int i = 1; i < n; ++i) {
-            tree[parent[i]].push_back(i);
+            children[parent[i]].push_back(i);
         }
-        for (int i = 0; i < n; ++i) {
-            sort(tree[i].begin(), tree[i].end());
-        }
-        vector<bool> answer(n);
-        string dfsStr;
-        function<void(int)> dfs = [&](int x) {
-            for (int y : tree[x]) dfs(y);
-            dfsStr += s[x];
+
+        auto isPalindrome = [](const string& str) {
+            int l = 0, r = str.size() - 1;
+            while (l < r) {
+                if (str[l++] != str[r--]) return false;
+            }
+            return true;
         };
+
+        vector<bool> answer(n);
+        function<void(int)> dfs = [&](int node) {
+            string dfsStr;
+            function<void(int)> dfsHelper = [&](int x) {
+                for (int y : children[x]) dfsHelper(y);
+                dfsStr.push_back(s[x]);
+            };
+            dfsHelper(node);
+            answer[node] = isPalindrome(dfsStr);
+        };
+
         for (int i = 0; i < n; ++i) {
-            dfsStr.clear();
             dfs(i);
-            string t = dfsStr;
-            reverse(t.begin(), t.end());
-            answer[i] = (dfsStr == t);
         }
+
         return answer;
     }
 };
