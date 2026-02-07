08b4006a7a8b810376f9df1b8d173c256d6c0a58
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 04:12:54 +0800

    Batch add 1 solutions: 1670.design-front-mi...

--- 1670.design-front-middle-back-queue.go
+++ 1670.design-front-middle-back-queue.go
@@ -6,55 +6,66 @@
 
 # @lc code=start
 type FrontMiddleBackQueue struct {
-    data []int
+    q []int
 }
 
 func Constructor() FrontMiddleBackQueue {
-    return FrontMiddleBackQueue{data: []int{}}
+    return FrontMiddleBackQueue{q: []int{}}
 }
 
 func (this *FrontMiddleBackQueue) PushFront(val int)  {
-    this.data = append([]int{val}, this.data...)
+    this.q = append([]int{val}, this.q...)
 }
 
 func (this *FrontMiddleBackQueue) PushMiddle(val int)  {
-    middle := len(this.data) / 2
-    this.data = append(this.data[:middle], append([]int{val}, this.data[middle:]...)...)
+    n := len(this.q)
+    mid := n / 2
+    this.q = append(this.q[:mid], append([]int{val}, this.q[mid:]...)...)
 }
 
 func (this *FrontMiddleBackQueue) PushBack(val int)  {
-    this.data = append(this.data, val)
+    this.q = append(this.q, val)
 }
 
 func (this *FrontMiddleBackQueue) PopFront() int {
-    if len(this.data) == 0 { return -1 }
-    val := this.data[0]
-    this.data = this.data[1:]
+    if len(this.q) == 0 {
+        return -1
+    }
+    val := this.q[0]
+    this.q = this.q[1:]
     return val
 }
 
 func (this *FrontMiddleBackQueue) PopMiddle() int {
-    if len(this.data) == 0 { return -1 }
-    middle := (len(this.data)-1) / 2
-    val := this.data[middle]
-    this.data = append(this.data[:middle], this.data[middle+1:]...)
+    n := len(this.q)
+    if n == 0 {
+        return -1
+    }
+    mid := (n - 1) / 2
+    val := this.q[mid]
+    this.q = append(this.q[:mid], this.q[mid+1:]...)
     return val
 }
 
 func (this *FrontMiddleBackQueue) PopBack() int {
-    if len(this.data) == 0 { return -1 }
-    val := this.data[len(this.data)-1]
-    this.data = this.data[:len(this.data)-1]
+    n := len(this.q)
+    if n == 0 {
+        return -1
+    }
+    val := this.q[n-1]
+    this.q = this.q[:n-1]
     return val
 }
+
+
 /**
- * Your FrontMiddleBackQueue object will be instantiated and called as such:
- * obj := Constructor();
- * obj.PushFront(val);
- * obj.PushMiddle(val);
- * obj.PushBack(val);
- * param_4 := obj.PopFront();
- * param_5 := obj.PopMiddle();
- * param_6 := obj.PopBack();
- */
+* Your FrontMiddleBackQueue object will be instantiated and called as such:
+* obj := Constructor();
+* obj.PushFront(val);
+* obj.PushMiddle(val);
+* obj.PushBack(val);
+* param_4 := obj.PopFront();
+* param_5 := obj.PopMiddle();
+* param_6 := obj.PopBack();
+*/
 # @lc code=end
