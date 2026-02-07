05140acf090f182d6117b7e9d9a83b232441f9d2
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 16:44:59 +0800

    Batch add 1 solutions: 3341.find-minimum-ti...

--- 3341.find-minimum-time-to-reach-last-room-i.cpp
+++ 3341.find-minimum-time-to-reach-last-room-i.cpp
@@ -1,38 +1,34 @@
-# 
+#
 # @lc app=leetcode id=3341 lang=cpp
-# 
+#
 # [3341] Find Minimum Time to Reach Last Room I
-# 
+#
+
 # @lc code=start
 #include <vector>
 #include <queue>
 #include <tuple>
 using namespace std;
+
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
         int n = moveTime.size();
         int m = moveTime[0].size();
         vector<vector<int>> minTime(n, vector<int>(m, INT_MAX));
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
-        pq.push({0, 0, 0}); // {time, row, col}
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+        pq.emplace(0, 0, 0); // (current_time, row_index, col_index)
         minTime[0][0] = 0;
-        vector<int> dir = {-1, 0, 1, 0, -1}; // Directions for moving in grid
+        constexpr static int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         while (!pq.empty()) {
-            auto [t, r, c] = pq.top(); pq.pop();
-            if (r == n - 1 && c == m - 1) return t; // Reached last room
-            for (int i = 0; i < 4; ++i) { // Explore all directions
-                int nr = r + dir[i], nc = c + dir[i + 1];
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
-                    int waitTime = max(t + 1, moveTime[nr][nc]); // Wait if needed before moving
-                    if (waitTime < minTime[nr][nc]) {
-                        minTime[nr][nc] = waitTime;
-                        pq.push({waitTime, nr, nc});
-                    }
-                }
-            }
-        }
-        return -1; // Should never reach here if input is valid as per constraint. 
-    }
-};
-# @lc code=end
+            auto [current_time, x, y] = pq.top(); pq.pop();
+            if (x == n - 1 && y == m - 1) return current_time; // reached destination
+            for (auto& dir : directions) {
+                int nx = x + dir[0], ny = y + dir[1];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    int wait_time = max(0, moveTime[nx][ny] - (current_time + 1)); // wait if needed until room is open
+                    int new_time = current_time + 1 + wait_time; // add moving time + waiting time if any
+                    if (new_time < minTime[nx][ny]) { // update only if found a better way/time to reach there
+                        minTime[nx][ny] = new_time;
+                        pq.emplace(new_time, nx, ny); // push new state into priority queue for exploration
+                    } /1}/1}/1}/1}/1 /1 } /1 }; # @lc code=end
