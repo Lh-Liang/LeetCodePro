1c6a0b9662953ba285223d4e464ca0365fe129af
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 06:09:25 +0800

    Batch add 1 solutions: 3670.maximum-product...

--- 3670.maximum-product-of-two-integers-with-no-common-bits.go
+++ 3670.maximum-product-of-two-integers-with-no-common-bits.go
@@ -3,20 +3,48 @@
 #
 # [3670] Maximum Product of Two Integers With No Common Bits
 #
-
 # @lc code=start
 func maxProduct(nums []int) int64 {
-    maxProduct := int64(0)
-    for i := 0; i < len(nums); i++ {
-        for j := i + 1; j < len(nums); j++ {
-            if nums[i]&nums[j] == 0 { // no common set bits
-                product := int64(nums[i]) * int64(nums[j])
-                if product > maxProduct {
-                    maxProduct = product
+    type pair struct{first, second int}
+    maskMax := make(map[int]pair) // mask -> top 2 values
+    for _, num := range nums {
+        mask := num
+        if curr, ok := maskMax[mask]; ok {
+            if num > curr.first {
+                maskMax[mask] = pair{num, curr.first}
+            } else if num > curr.second {
+                maskMax[mask] = pair{curr.first, num}
+            }
+        } else {
+            maskMax[mask] = pair{num, 0}
+        }
+    }
+    masks := make([]int, 0, len(maskMax))
+    for m := range maskMax {
+        masks = append(masks, m)
+    }
+    maxProd := int64(0)
+    // Compare different masks
+    for i := 0; i < len(masks); i++ {
+        for j := i + 1; j < len(masks); j++ {
+            if masks[i]&masks[j] == 0 {
+                a, b := maskMax[masks[i]].first, maskMax[masks[j]].first
+                prod := int64(a) * int64(b)
+                if prod > maxProd {
+                    maxProd = prod
                 }
             }
         }
     }
-    return maxProduct
+    // For same mask, check if there are at least two numbers
+    for _, p := range maskMax {
+        if p.second > 0 {
+            prod := int64(p.first) * int64(p.second)
+            if prod > maxProd {
+                maxProd = prod
+            }
+        }
+    }
+    return maxProd
 }
 # @lc code=end
