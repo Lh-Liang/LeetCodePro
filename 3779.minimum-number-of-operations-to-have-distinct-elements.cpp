b3598da3dbda2f4368b46054d0ee09b03b3c3aec
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 20:48:48 +0800

    Batch add 1 solutions: 3779.minimum-number-...

--- 3779.minimum-number-of-operations-to-have-distinct-elements.cpp
+++ 3779.minimum-number-of-operations-to-have-distinct-elements.cpp
@@ -1,25 +1,30 @@
+#
 # @lc app=leetcode id=3779 lang=cpp
 #
 # [3779] Minimum Number of Operations to Have Distinct Elements
 #
 # @lc code=start
-#include <vector>
-#include <unordered_set>
-using namespace std;
-
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int operations = 0;
-        while (!nums.empty()) {
-            // Check if all elements are distinct
-            unordered_set<int> numSet(nums.begin(), nums.end());
-            if (numSet.size() == nums.size()) break; // All elements are distinct
-            // Remove first three elements or fewer if remaining size is less than three
-            nums.erase(nums.begin(), nums.begin() + min(3, (int)nums.size()));
-            operations++;
-        }
-        return operations;
-    }
-};
+        while(true) {
+            unordered_set<int> seen;
+            bool hasDuplicates = false;
+            for(int num : nums) {
+                if(seen.count(num)) {
+                    hasDuplicates = true;
+                    break;
+                } else {
+                    seen.insert(num);
+                }
+            }
+            if(!hasDuplicates) break; // Stop if no duplicates exist. 
+            // Remove first three elements or all if less than three remain. 
+            nums.erase(nums.begin(), nums.size() < 3 ? nums.end() : nums.begin() + 3); 
+            operations++; 
+        } 
+        return operations; 
+    } 
+}; 
 # @lc code=end
