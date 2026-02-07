a759110a3e3af802e0d709f70dc27b31592afe8a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 14:57:04 +0800

    Batch add 1 solutions: 3768.minimum-inversi...

--- 3768.minimum-inversion-count-in-subarrays-of-fixed-length.cpp
+++ 3768.minimum-inversion-count-in-subarrays-of-fixed-length.cpp
@@ -3,61 +3,60 @@
 #
 # [3768] Minimum Inversion Count in Subarrays of Fixed Length
 #
-
 # @lc code=start
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
-using namespace std;
-
-class BIT {
-public:
-    vector<int> tree;
-    int n;
-    BIT(int n) : n(n), tree(n + 2, 0) {}
-    void add(int x, int delta) {
-        for (++x; x <= n + 1; x += x & -x) tree[x] += delta;
-    }
-    int sum(int x) {
-        int res = 0;
-        for (++x; x > 0; x -= x & -x) res += tree[x];
-        return res;
-    }
-    int range_sum(int l, int r) {
-        return sum(r) - sum(l - 1);
-    }
-};
-
 class Solution {
 public:
     long long minInversionCount(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> sorted(nums);
-        sort(sorted.begin(), sorted.end());
-        sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
-        unordered_map<int, int> mp;
-        for (int i = 0; i < sorted.size(); ++i) mp[sorted[i]] = i;
-        vector<int> arr(n);
-        for (int i = 0; i < n; ++i) arr[i] = mp[nums[i]];
-        int m = sorted.size();
-        BIT bit(m);
-        long long inv = 0;
-        // Initial window
+        vector<int> sorted_nums = nums;
+        sort(sorted_nums.begin(), sorted_nums.end());
+        unordered_map<int, int> rank_map;
+        for (int i = 0; i < n; ++i) {
+            rank_map[sorted_nums[i]] = i + 1; // Rank starts from 1
+        }
+        
+        vector<int> bit(n + 1, 0);
+        auto update = [&](int index, int value) {
+            while (index <= n) {
+                bit[index] += value;
+                index += index & -index;
+            }
+        };
+        
+        auto query = [&](int index) {
+            int sum = 0;
+            while (index > 0) {
+                sum += bit[index];
+                index -= index & -index;
+            }
+            return sum;
+        };
+
+        long long current_inversions = 0;
+
+        // Initial window setup
         for (int i = 0; i < k; ++i) {
-            inv += bit.range_sum(arr[i] + 1, m - 1);
-            bit.add(arr[i], 1);
+            int rank = rank_map[nums[i]];
+            current_inversions += query(n) - query(rank);
+            update(rank, 1);
         }
-        long long res = inv;
+
+        long long min_inversions = current_inversions;
+
+        // Sliding window
         for (int i = k; i < n; ++i) {
-            // Remove arr[i - k]
-            bit.add(arr[i - k], -1);
-            inv -= bit.range_sum(0, arr[i - k] - 1);
-            // Add arr[i]
-            inv += bit.range_sum(arr[i] + 1, m - 1);
-            bit.add(arr[i], 1);
-            res = min(res, inv);
+            int rank_outgoing = rank_map[nums[i-k]];
+            update(rank_outgoing, -1);
+            current_inversions -= query(n) - query(rank_outgoing);
+
+            int rank_incoming = rank_map[nums[i]];
+            current_inversions += query(n) - query(rank_incoming);
+            update(rank_incoming, 1);
+
+            min_inversions = min(min_inversions, current_inversions);
         }
-        return res;
+
+        return min_inversions;
     }
 };
 # @lc code=end
