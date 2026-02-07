466f6292dd114a73feb12cc1c66d6c6b4943dae9
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 14:58:42 +0800

    Batch add 1 solutions: 3777.minimum-deletio...

--- 3777.minimum-deletions-to-make-alternating-substring.cpp
+++ 3777.minimum-deletions-to-make-alternating-substring.cpp
@@ -9,17 +9,22 @@ class Solution {
 public:
     vector<int> minDeletions(string s, vector<vector<int>>& queries) {
         vector<int> answer;
-        for (const auto& q : queries) {
-            if (q[0] == 1) {
-                int j = q[1];
-                s[j] = (s[j] == 'A' ? 'B' : 'A');
-            } else {
-                int l = q[1], r = q[2];
-                int deletions = 0;
+        for (const auto& query : queries) {
+            if (query[0] == 1) { // Flip operation
+                int j = query[1];
+                s[j] = (s[j] == 'A') ? 'B' : 'A';
+            } else if (query[0] == 2) { // Compute deletions for alternating substring
+                int l = query[1], r = query[2];
+                int deletionsAB = 0, deletionsBA = 0;
                 for (int i = l; i < r; ++i) {
-                    if (s[i] == s[i+1]) ++deletions;
+                    if ((i % 2 == l % 2 && s[i] != 'A') || (i % 2 != l % 2 && s[i] != 'B')) {
+                        ++deletionsAB;
+                    }
+                    if ((i % 2 == l % 2 && s[i] != 'B') || (i % 2 != l % 2 && s[i] != 'A')) {
+                        ++deletionsBA;
+                    }
                 }
-                answer.push_back(deletions);
+                answer.push_back(min(deletionsAB, deletionsBA));
             }
         }
         return answer;
