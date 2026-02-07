1550f3ef9d3dbf62f91d33c7f832d52679208e1e
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 22:53:27 +0800

    Batch add 1 solutions: 3569.maximize-count-...

---
+++ 3569.maximize-count-of-distinct-primes-after-split.go
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=3569 lang=golang
+#
+# [3569] Maximize Count of Distinct Primes After Split
+#
+
+# @lc code=start
+func maximumCount(nums []int, queries [][]int) []int {
+    // Implemented logic here...
+}
+# @lc code=end
