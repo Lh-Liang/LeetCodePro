d755eea62569710ded6e205d285ac44e19d1f6c1
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 00:20:04 +0800

    Batch add 1 solutions: 2289.steps-to-make-a...

--- 2289.steps-to-make-array-non-decreasing.go
+++ 2289.steps-to-make-array-non-decreasing.go
@@ -6,18 +6,29 @@
 
 # @lc code=start
 func totalSteps(nums []int) int {
-    n := len(nums)
-    steps := make([]int, n)
-    stack := []int{}
-    maxSteps := 0
-    for i := n - 1; i >= 0; i-- {
-        for len(stack) > 0 && nums[i] > nums[stack[len(stack)-1]] {
-            steps[i] = max(steps[i]+1, steps[stack[len(stack)-1]])
-            stack = stack[:len(stack)-1]
+    stack := [][]int{} // Stack stores pairs: value and step count
+    maxSteps := 0 // Track maximum steps required overall
+    for _, num := range nums {
+        count := 0 // Steps for current number
+        // Compare with elements in stack and pop those less or equal to current
+        while len(stack) > 0 && stack[len(stack)-1][0] <= num {
+            count = max(count, stack[len(stack)-1][1]) // Get max step count from popped elements
+            stack = stack[:len(stack)-1] // Remove top element as it's not blocking anymore
         }
-        maxSteps = max(maxSteps, steps[i])
-        stack = append(stack, i)
+        if len(stack) > 0 {
+            count++ // Increment step as there are larger numbers before it that will cause removal in future steps
+        }
+        // Push current number with calculated step count onto stack
+        stack = append(stack, []int{num, count})
+        maxSteps = max(maxSteps, count) // Update overall max steps if needed
     }
-    return maxSteps
+    return maxSteps 
 }
-func max(a, b int) int { if a > b { return a } else { return b } }
+// Helper function to get maximum value between two integers 
+func max(a, b int) int { 
+    if a > b { 
+        return a 
+    } 
+    return b 
+}
+# @lc code=end
