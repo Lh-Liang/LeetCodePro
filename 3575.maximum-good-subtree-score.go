9c0d445fe79d24f1a9716f08bfb8ad0be6b61a63
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 05:36:35 +0800

    Batch add 1 solutions: 3575.maximum-good-su...

--- 3575.maximum-good-subtree-score.go
+++ 3575.maximum-good-subtree-score.go
@@ -6,42 +6,79 @@
 
 # @lc code=start
 func goodSubtreeSum(vals []int, par []int) int {
-    const MOD = 1000000007
+    const MOD = 1_000_000_007
     n := len(vals)
-    adj := make([][]int, n)
+    tree := make([][]int, n)
     for i := 1; i < n; i++ {
-        adj[par[i]] = append(adj[par[i]], i)
-    }
-    
-    var max func(a, b int) int
-    max = func(a, b int) int {
-        if a > b {
-            return a
-        }
-        return b
+        tree[par[i]] = append(tree[par[i]], i)
     }
 
-    var dfs func(node int) (int, int)
-    dfs = func(node int) (int, int) {
-        score := vals[node]
-        bitmask := 0       
-        for x := vals[node]; x > 0; x /= 10 {
-            digit := x % 10           
-            if (bitmask & (1 << digit)) != 0 { 
-                return 0, 0 
-            }           
-            bitmask |= (1 << digit)
+    // Helper to get the bitmask for a value
+    digitMask := func(val int) int {
+        mask := 0
+        for val > 0 {
+            d := val % 10
+            if (mask&(1<<d)) != 0 {
+                return -1 // duplicate
+            }
+            mask |= 1 << d
+            val /= 10
         }
-        
-        maxScore := score          
-        for _, child := range adj[node] {
-            childScore, childMask := dfs(child)             
-            if childMask & bitmask == 0 {                
-s                maxScore = max(maxScore, score + childScore)
-s                bitmask |= childMask              
-s           }           
-s       }     
-s       return maxScore % MOD, bitmask   
-s   }     
-totalMaxScore, _ := dfs(0)   // Correctly use tuple unpacking to get result   
-s   return totalMaxScore % MOD}          # @lc code=end
+        return mask
+    }
+
+    // For each node: map[used_digit_mask]max_sum
+    var dfs func(int) map[int]int
+    maxScore := make([]int, n)
+
+    dfs = func(u int) map[int]int {
+        curMask := digitMask(vals[u])
+        res := make(map[int]int)
+        if curMask != -1 {
+            res[curMask] = vals[u]
+        }
+        for _, v := range tree[u] {
+            child := dfs(v)
+            newRes := make(map[int]int)
+            // Don't combine: just keep original
+            for mask, sum := range res {
+                if newRes[mask] < sum {
+                    newRes[mask] = sum
+                }
+            }
+            // Combine child with current
+            for cm, cs := range child {
+                // Option 1: take only child
+                if newRes[cm] < cs {
+                    newRes[cm] = cs
+                }
+                // Option 2: combine with current
+                for mask, sum := range res {
+                    if mask&cm == 0 {
+                        if newRes[mask|cm] < sum+cs {
+                            newRes[mask|cm] = sum + cs
+                        }
+                    }
+                }
+            }
+            res = newRes
+        }
+        // Find max for this node
+        best := 0
+        for _, v := range res {
+            if v > best {
+                best = v
+            }
+        }
+        maxScore[u] = best
+        return res
+    }
+
+    dfs(0)
+    ans := 0
+    for _, v := range maxScore {
+        ans = (ans + v) % MOD
+    }
+    return ans
+}
+# @lc code=end
