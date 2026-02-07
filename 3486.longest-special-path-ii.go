d2695042d8272d648ce1560073cf7b6e3821c9c4
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 05:02:11 +0800

    Batch add 1 solutions: 3486.longest-special...

--- 3486.longest-special-path-ii.go
+++ 3486.longest-special-path-ii.go
@@ -6,64 +6,48 @@
 
 # @lc code=start
 func longestSpecialPath(edges [][]int, nums []int) []int {
-    type Node struct {
-        id int
-        length int
-    }
     n := len(nums)
-    graph := make([][]Node, n)
-    for _, edge := range edges {
-        u, v, length := edge[0], edge[1], edge[2]
-        graph[u] = append(graph[u], Node{id: v, length: length})
-        graph[v] = append(graph[v], Node{id: u, length: length})
+    adj := make([][][2]int, n)
+    for _, e := range edges {
+        u, v, w := e[0], e[1], e[2]
+        adj[u] = append(adj[u], [2]int{v, w})
+        adj[v] = append(adj[v], [2]int{u, w})
     }
-    
-    var dfs func(node int, parent int, path map[int]int) (int, int)
-    dfs = func(node int, parent int, path map[int]int) (int, int) {
-        maxPathLength := 0
-        minNodes := n // Start with max nodes possible for comparison
-        
-        newPath := make(map[int]int)
-        for k, v := range path {
-            newPath[k] = v
+    maxLen := 0
+    minNodes := n + 1
+    freq := map[int]int{}
+    var dfs func(node, parent int, dupCount int, length int, nodes int)
+    dfs = func(node, parent int, dupCount int, length int, nodes int) {
+        val := nums[node]
+        freq[val]++
+        if freq[val] == 2 {
+            dupCount++
         }
-        newPath[nums[node]]++
-      
-        distinctCount := len(newPath)
-        duplicates := 0 // Track duplicates separately with a counter
-        for _, count := range newPath {
-            if count > 1 {
-                duplicates++
+        if dupCount > 1 {
+            if freq[val] == 2 {
+                dupCount--
             }
+            freq[val]--
+            return
         }
-      
-        if duplicates > 1 { // More than one duplicate invalidates this path branch
-            return 0, n // Invalid path returns zero length and max nodes (to be ignored)
+        if length > maxLen {
+            maxLen = length
+            minNodes = nodes
+        } else if length == maxLen && nodes < minNodes {
+            minNodes = nodes
         }
-      
-        for _, neighbor := range graph[node] {
-            if neighbor.id == parent {
-                continue // Avoid revisiting parent node in DFS
+        for _, nb := range adj[node] {
+            if nb[0] == parent {
+                continue
             }
-            
-            length, nodes := dfs(neighbor.id, node, newPath)
-          totalLength := length + neighbor.length // Accumulate edge lengths in path computation
-          if totalLength > maxPathLength || (totalLength == maxPathLength && nodes < minNodes) {
-              maxPathLength = totalLength
-              minNodes = nodes + 1 // Include current node in count
-          }
-      }
-      return maxPathLength, minNodes
-   }
-   
-   resultLength := 0
-   resultNodes := n // Start with max nodes possible for initialization comparison purposes
-   
-   length, nodes := dfs(0, -1, map[int]int{}) // Start DFS from root node (node 0)
-   if length > resultLength || (length == resultLength && nodes < resultNodes) {
-       resultLength = length
-       resultNodes = nodes
-   }
-   return []int{resultLength, resultNodes}
-in}
-in# @lc code=end
+            dfs(nb[0], node, dupCount, length+nb[1], nodes+1)
+        }
+        if freq[val] == 2 {
+            dupCount--
+        }
+        freq[val]--
+    }
+    dfs(0, -1, 0, 0, 1)
+    return []int{maxLen, minNodes}
+}
+# @lc code=end
