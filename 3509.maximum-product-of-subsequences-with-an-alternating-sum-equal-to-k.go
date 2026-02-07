dc944b6f2cbc093322961c0032f9a551e35c8bbc
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 05:10:47 +0800

    Batch add 1 solutions: 3509.maximum-product...

--- 3509.maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k.go
+++ 3509.maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k.go
@@ -3,37 +3,62 @@
 #
 # [3509] Maximum Product of Subsequences With an Alternating Sum Equal to K
 #
-
 # @lc code=start
 func maxProduct(nums []int, k int, limit int) int {
-    maxProduct := -1
-    var dfs func(index int, alternatingSum int, currentProduct int)
-    
-    dfs = func(index int, alternatingSum int, currentProduct int) {
-        if index == len(nums) {  // Base case: reached end of nums
-            if alternatingSum == k && currentProduct <= limit {
-                if currentProduct > maxProduct {
-                    maxProduct = currentProduct
+    type state struct {
+        sum   int
+        parity int
+    }
+    dp := make(map[state]int)
+    n := len(nums)
+    for i := 0; i < n; i++ {
+        ndp := make(map[state]int)
+        // Start a new subsequence with nums[i]
+        s := nums[i]
+        p := 0
+        prod := nums[i]
+        if prod <= limit {
+            st := state{s, p}
+            if ndp[st] < prod {
+                ndp[st] = prod
+            }
+        }
+        // Try to extend all existing subsequences
+        for key, val := range dp {
+            // Not picking nums[i]
+            if ndp[key] < val {
+                ndp[key] = val
+            }
+            // Picking nums[i]
+            nparity := (key.parity + 1) % 2
+            if key.parity == 0 {
+                nsum := key.sum - nums[i]
+                nprod := val * nums[i]
+                if nprod <= limit {
+                    st := state{nsum, nparity}
+                    if ndp[st] < nprod {
+                        ndp[st] = nprod
+                    }
+                }
+            } else {
+                nsum := key.sum + nums[i]
+                nprod := val * nums[i]
+                if nprod <= limit {
+                    st := state{nsum, nparity}
+                    if ndp[st] < nprod {
+                        ndp[st] = nprod
+                    }
                 }
             }
-            return
         }
-        
-        // Option 1: Include nums[index] in subsequence
-        newAlternatingSum := alternatingSum + nums[index]
-        if index % 2 == 1 {  // Odd index, subtract instead of add
-            newAlternatingSum = alternatingSum - nums[index]
-        }
-        newProduct := currentProduct * nums[index]
-        if newProduct <= limit {  // Only consider valid products within limit
-            dfs(index + 1, newAlternatingSum, newProduct)
-        }
-        
-        // Option 2: Exclude nums[index] from subsequence
-        dfs(index + 1, alternatingSum, currentProduct)
+        dp = ndp
     }
-    
-    dfs(0, 0, 1)  // Start DFS with initial values
-    return maxProduct
+    res := -1
+    for key, val := range dp {
+        if key.sum == k && val > res {
+            res = val
+        }
+    }
+    return res
 }
 # @lc code=end
