86691594fea2e53dac8aba0e96bd8015ab7ddd05
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 20:19:21 +0800

    Batch add 1 solutions: 3695.maximize-altern...

--- 3695.maximize-alternating-sum-using-swaps.cpp
+++ 3695.maximize-alternating-sum-using-swaps.cpp
@@ -1,36 +1,24 @@
+#
 # @lc app=leetcode id=3695 lang=cpp
+#
 # [3695] Maximize Alternating Sum Using Swaps
-# 
+#
 # @lc code=start
 class Solution {
 public:
     long long maxAlternatingSum(vector<int>& nums, vector<vector<int>>& swaps) {
-        int n = nums.size();
-        vector<int> parent(n);
-        iota(parent.begin(), parent.end(), 0); // Initialize union-find structure
-        function<int(int)> find = [&](int x) { return x == parent[x] ? x : parent[x] = find(parent[x]); }; // Find with path compression
-        auto unite = [&](int x, int y) { parent[find(x)] = find(y); }; // Union operation
-        // Unite indices based on swaps
-        for (const auto& s : swaps)
-            unite(s[0], s[1]); 
-        unordered_map<int, vector<int>> groups; 
-        for (int i = 0; i < n; ++i) 
-            groups[find(i)].push_back(i); 
-        vector<int> sorted_nums(n); 
-        for (auto& [root, group] : groups) {
-            vector<int> values;
-            for (int idx : group)
-                values.push_back(nums[idx]);
-            sort(values.begin(), values.end(), greater<>()); // Sort in descending order to maximize sum at even positions
-            sort(group.begin(), group.end());
-            for (int i = 0; i < group.size(); ++i)
-                sorted_nums[group[i]] = values[i];
+        // Sort nums in descending order
+        sort(nums.begin(), nums.end(), greater<int>());
+        // Rearrange into alternating pattern starting from largest at even indices
+        long long maxSum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i % 2 == 0) {
+                maxSum += nums[i]; // Add for even indices
+            } else {
+                maxSum -= nums[i]; // Subtract for odd indices
+            }
         }
-        long long max_sum = 0;
-        for (int i = 0; i < n; ++i) {
-            if (i % 2 == 0) max_sum += sorted_nums[i]; else max_sum -= sorted_nums[i];
-        }
-        return max_sum;
+        return maxSum;
     }
 };
 # @lc code=end
