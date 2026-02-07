343858a0b7b7516ebbc77e6f0c80fcd21c0199f6
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 02:57:20 +0800

    Batch add 1 solutions: 3670.maximum-product...

--- 3670.maximum-product-of-two-integers-with-no-common-bits.go
+++ 3670.maximum-product-of-two-integers-with-no-common-bits.go
@@ -1,1 +1,22 @@
-The task has been completed successfully.
+#
+# @lc app=leetcode id=3670 lang=golang
+#
+# [3670] Maximum Product of Two Integers With No Common Bits
+#
+
+# @lc code=start
+func maxProduct(nums []int) int64 {
+    maxProduct := int64(0)
+    for i := 0; i < len(nums); i++ {
+        for j := i + 1; j < len(nums); j++ {
+            if nums[i]&nums[j] == 0 { // no common set bits
+                product := int64(nums[i]) * int64(nums[j])
+                if product > maxProduct {
+                    maxProduct = product
+                }
+            }
+        }
+    }
+    return maxProduct
+}
+# @lc code=end
