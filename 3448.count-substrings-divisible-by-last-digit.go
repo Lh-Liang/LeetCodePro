d2b541e8f9867f4a114285a993d15ee474a37794
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 01:07:50 +0800

    Batch add 1 solutions: 3448.count-substring...

--- 3448.count-substrings-divisible-by-last-digit.go
+++ 3448.count-substrings-divisible-by-last-digit.go
@@ -6,20 +6,21 @@
 
 # @lc code=start
 func countSubstrings(s string) int64 {
-    count := int64(0)
+    var count int64 = 0
     n := len(s)
-    
     for i := 0; i < n; i++ {
+        num := 0
         lastDigit := int(s[i] - '0')
         if lastDigit == 0 {
-            continue // Skip if last digit is zero since division is not possible
+            continue // Skip substrings ending with zero as divisor cannot be zero
         }
-        num := 0 // This will hold our current number representation
+        // Incremental update for substring values
         for j := i; j >= 0; j-- {
-            num = num + int(s[j]-'0')*int(math.Pow10(i-j)) // Build number backwards
+            num = num * 10 + int(s[j]-'0') // Build number from right to left correctly considering positional significance
             if num % lastDigit == 0 {
                 count++ // Valid substring found
             }
+            num %= lastDigit // Ensure number doesn't grow too large (optimization)
         }
     }
     return count
