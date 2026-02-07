419f5d2ac866c86792f3b7eca7d9d4c74664bbdd
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 02:05:58 +0800

    Batch add 1 solutions: 3553.minimum-weighte...

--- 3553.minimum-weighted-subgraph-with-the-required-paths-ii.go
+++ 3553.minimum-weighted-subgraph-with-the-required-paths-ii.go
@@ -1,42 +1,67 @@
-#
 # @lc app=leetcode id=3553 lang=golang
-#
 # [3553] Minimum Weighted Subgraph With the Required Paths II
 #
-
 # @lc code=start
-func minimumWeight(edges [][]int, queries [][]int) []int {
-    // Convert edge list to adjacency list
-    adjList := make(map[int][][2]int)
-    for _, edge := range edges {
-        u, v, w := edge[0], edge[1], edge[2]
-        adjList[u] = append(adjList[u], [2]int{v, w})
-        adjList[v] = append(adjList[v], [2]int{u, w})
+import "container/heap"
+
+type Item struct {
+    node int
+    distance int
+}
+type PriorityQueue []*Item
+func (pq PriorityQueue) Len() int { return len(pq) }
+func (pq PriorityQueue) Less(i, j int) bool { return pq[i].distance < pq[j].distance }
+func (pq PriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }
+func (pq *PriorityQueue) Push(x interface{}) { *pq = append(*pq, x.(*Item)) }
+func (pq *PriorityQueue) Pop() interface{} {
+    old := *pq
+    n := len(old)
+    item := old[n-1]
+    *pq = old[0 : n-1]
+    return item
+}
+
+func dijkstra(n int, adjList [][]struct{node, weight int}, start int) []int {
+    dist := make([]int, n)
+    for i := range dist {
+        dist[i] = 1<<63 - 1 // MaxInt equivalent in Go for infinity representation
     }
+    dist[start] = 0
+    pq := &PriorityQueue{{start, 0}}
+    heap.Init(pq)
     
-    // Function to perform BFS and return distances from a source node
-    bfs := func(source int) map[int]int {
-        dist := make(map[int]int)
-        for node := range adjList { dist[node] = int(1e9) } // Use a large number as infinity
-        queue := []int{source}
-        dist[source] = 0
-        
-        for len(queue) > 0 {
-            node := queue[0]
-            queue = queue[1:]
-            for _, neighbor := range adjList[node] {
-                nextNode, weight := neighbor[0], neighbor[1]
-                if dist[node] + weight < dist[nextNode] {
-                    dist[nextNode] = dist[node] + weight
-                    queue = append(queue, nextNode)
-                }
+    for pq.Len() > 0 {
+        current := heap.Pop(pq).(*Item)
+        u := current.node
+        if current.distance > dist[u] {
+            continue // Skip if a shorter path has been found already
+        }
+        for _, neighbor := range adjList[u] {
+            v, w := neighbor.node, neighbor.weight
+            newDist := dist[u] + w
+            if newDist < dist[v] {
+                dist[v] = newDist
+                heap.Push(pq, &Item{v, newDist})
             }
         }
-        return dist
     }
-    
-    // Answer queries by finding minimum subtrees connecting sources to destination
-    results := make([]int, len(queries))
-    for i, q := range queries {
-        src1, src2, dest := q[0], q[1], q[2]
-        distFromSrc1 := bfs(src1) "// Get distances from src1 node
+    return dist
+}
+
+func minimumWeight(edges [][]int, queries [][]int) []int {
+    n := len(edges) + 1
+    adjList := make([][]struct{node, weight int}, n)
+   
+    // Build adjacency list from edges input
+    for _, edge := range edges {
+        u, v, w := edge[0], edge[1], edge[2]
+        adjList[u] = append(adjList[u], struct{node, weight int}{v, w})
+        adjList[v] = append(adjList[v], struct{node, weight int}{u, w})
+    }
+   
+   result := make([]int, len(queries))
+   	for i, q := range queries {
+src1, src2 , dest:= q[0], q[1], q[2]
+distFromSrc1:= dijkstra(n ,adjList ,src1 )
+distFromSrc2:= dijkstra(n ,adjList ,src2 )
+distToDest:= dijkstra(n ,adjList ,dest )		minWeightSubtree:= 1<<63 -1// MaxInt equivalent initially		for v:=0; v<n ; v++{		if distFromSrc1[v]!=1<<63-1 &&distFromSrc2[v]!=1<<63-1 &&distToDest[v]!=1<<63-1{	totalWeight:=distFromSrc1[v]+distFromSrc2[v]+distToDest[v]	if totalWeight<minWeightSubtree{	minWeightSubtree=totalWeight	}	}	}	result[i]=minWeightSubtree	}	return result	}
