dbbf014bad0c3b7615f291f9d906dbcaf1110acb
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 12:23:53 +0800

    Batch add 1 solutions: 3434.maximum-frequen...

--- 3434.maximum-frequency-after-subarray-operation.cpp
+++ 3434.maximum-frequency-after-subarray-operation.cpp
@@ -5,24 +5,26 @@
 #
 
 # @lc code=start
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> diff(n);
-        for (int i = 0; i < n; ++i) {
-            diff[i] = k - nums[i];
-        }
-        int max_freq = 1, curr_freq = 1;
-        for (int i = 1; i < n; ++i) {
-            if (diff[i] == diff[i-1]) {
-                curr_freq++;
-                if (curr_freq > max_freq) max_freq = curr_freq;
-            } else {
-                curr_freq = 1;
+        sort(nums.begin(), nums.end());
+        long long totalOperations = 0;
+        int left = 0;
+        int maxFreq = 0;
+        for (int right = 0; right < nums.size(); ++right) {
+            totalOperations += nums[right];
+            while ((right - left + 1) * nums[right] - totalOperations > k) {
+                totalOperations -= nums[left];
+                ++left;
             }
+            maxFreq = max(maxFreq, right - left + 1);
         }
-        return max_freq;
+        return maxFreq;
     }
 };
 # @lc code=end
