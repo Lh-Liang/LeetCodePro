b634362ceb1c9a042313b6ca024e96dedc70f5af
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 21:38:58 +0800

    Batch add 1 solutions: 2289.steps-to-make-a...

--- 2289.steps-to-make-array-non-decreasing.go
+++ 2289.steps-to-make-array-non-decreasing.go
@@ -1,20 +1,23 @@
+#
+# @lc app=leetcode id=2289 lang=golang
+#
+# [2289] Steps to Make Array Non-decreasing
+#
+
+# @lc code=start
 func totalSteps(nums []int) int {
-    type pair struct {
-        value int
-        steps int
-    }
-    var stack []pair
+    n := len(nums)
+    steps := make([]int, n)
+    stack := []int{}
     maxSteps := 0
-    for i := len(nums) - 1; i >= 0; i-- {
-        currentSteps := 0
-        for len(stack) > 0 && nums[i] > stack[len(stack)-1].value {
-            currentSteps = max(currentSteps+1, stack[len(stack)-1].steps)
+    for i := n - 1; i >= 0; i-- {
+        for len(stack) > 0 && nums[i] > nums[stack[len(stack)-1]] {
+            steps[i] = max(steps[i]+1, steps[stack[len(stack)-1]])
             stack = stack[:len(stack)-1]
         }
-        maxSteps = max(maxSteps, currentSteps)
-        stack = append(stack, pair{nums[i], currentSteps})
+        maxSteps = max(maxSteps, steps[i])
+        stack = append(stack, i)
     }
     return maxSteps
 }
-
-func max(a, b int) int { if a > b { return a } else { return b }}
+func max(a, b int) int { if a > b { return a } else { return b } }
