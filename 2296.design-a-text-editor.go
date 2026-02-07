6fb4b7f535b20120dc0dc3292857ca1b94f58f5a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 04:16:15 +0800

    Batch add 1 solutions: 2296.design-a-text-e...

--- 2296.design-a-text-editor.go
+++ 2296.design-a-text-editor.go
@@ -1,49 +1,63 @@
+#
+# @lc app=leetcode id=2296 lang=golang
+#
+# [2296] Design a Text Editor
+#
+
 # @lc code=start
 type TextEditor struct {
-    left, right []rune
+    left  []byte // characters to the left of cursor
+    right []byte // characters to the right of cursor
 }
 
 func Constructor() TextEditor {
-    return TextEditor{left: []rune{}, right: []rune{}}
+    return TextEditor{
+        left:  make([]byte, 0),
+        right: make([]byte, 0),
+    }
 }
 
-func (this *TextEditor) AddText(text string) {
-    for _, c := range text {
-        this.left = append(this.left, c)
-    }
+func (this *TextEditor) AddText(text string)  {
+    this.left = append(this.left, text...)
 }
 
 func (this *TextEditor) DeleteText(k int) int {
-    deleteCount := min(k, len(this.left))
-    this.left = this.left[:len(this.left)-deleteCount]
-    return deleteCount
+    actual := k
+    if len(this.left) < k {
+        actual = len(this.left)
+    }
+    this.left = this.left[:len(this.left)-actual]
+    return actual
 }
 
 func (this *TextEditor) CursorLeft(k int) string {
-    moveCount := min(k, len(this.left))
-    this.right = append(this.right, this.left[len(this.left)-moveCount:]...)
-    this.left = this.left[:len(this.left)-moveCount]
-    return getLastTenChars(this.left)
+    move := k
+    if len(this.left) < k {
+        move = len(this.left)
+    }
+    this.right = append(this.right, this.left[len(this.left)-move:]...)
+    this.left = this.left[:len(this.left)-move]
+    l := len(this.left)
+    start := l - 10
+    if start < 0 {
+        start = 0
+    }
+    return string(this.left[start:])
 }
 
 func (this *TextEditor) CursorRight(k int) string {
-    moveCount := min(k, len(this.right))
-    this.left = append(this.left, this.right[len(this.right)-moveCount:]...)
-    this.right = this.right[:len(this.right)-moveCount]
-    return getLastTenChars(this.left)
+    move := k
+    if len(this.right) < k {
+        move = len(this.right)
+    }
+    this.left = append(this.left, this.right[len(this.right)-move:]...)
+    this.right = this.right[:len(this.right)-move]
+    l := len(this.left)
+    start := l - 10
+    if start < 0 {
+        start = 0
+    }
+    return string(this.left[start:])
 }
 
-func min(a, b int) int {
-    if a < b {
-        return a
-    }
-    return b
-}
-
-func getLastTenChars(s []rune) string {
-    if len(s) <= 10 {
-        return string(s)
-    }
-    return string(s[len(s)-10:])
-}
 # @lc code=end
