9cb63de88a1b92ca202f4fe5db2297ae95ee12f6
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 21:34:22 +0800

    Batch add 1 solutions: 1367.linked-list-in-...

--- 1367.linked-list-in-binary-tree.go
+++ 1367.linked-list-in-binary-tree.go
@@ -1,33 +1,33 @@
-#
 # @lc app=leetcode id=1367 lang=golang
-#
+
 # [1367] Linked List in Binary Tree
-#
+
 # @lc code=start
 /**
-* Definition for singly-linked list.
-* type ListNode struct {
-*     Val int
-*     Next *ListNode
-* }
-*/
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 /**
-* Definition for a binary tree node.
-* type TreeNode struct {
-*     Val int
-*     Left *TreeNode
-*     Right *TreeNode
-* }
-*/
-func isSubPath(head *ListNode, root *TreeNode) bool { 
-    if head == nil { return true } 
-    if root == nil { return false } 
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSubPath(head *ListNode, root *TreeNode) bool {
+    if head == nil { return true } // If head is nil, we've matched all nodes.
+    if root == nil { return false } // If root is nil before matching all, fail.
     return dfs(head, root) || isSubPath(head, root.Left) || isSubPath(head, root.Right) 
 }
+// Helper function to check current path matches or not. 
 func dfs(head *ListNode, node *TreeNode) bool { 
-    if head == nil { return true } 
-    if node == nil { return false } 
-    if head.Val != node.Val { return false } 
+    if head == nil { return true } // All matched 
+    if node == nil { return false } // Path ends without full match 
+    if head.Val != node.Val { return false }  // Values do not match 
     return dfs(head.Next, node.Left) || dfs(head.Next, node.Right) 
 }
 # @lc code=end
