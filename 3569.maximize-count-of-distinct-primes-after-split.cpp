318019270f7777ca4c808c624649673b93f6461f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 13:47:12 +0800

    Batch add 1 solutions: 3569.maximize-count-...

--- 3569.maximize-count-of-distinct-primes-after-split.cpp
+++ 3569.maximize-count-of-distinct-primes-after-split.cpp
@@ -5,48 +5,68 @@
 #
 
 # @lc code=start
-#include <vector>
-#include <unordered_set>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
-    vector<int> maximumCount(vector<int>& nums, vector<vector<int>>& queries) {
-        const int MAX_N = 100005;
-        vector<bool> is_prime(MAX_N, true);
+    vector<int> sieve(int max_num) {
+        vector<bool> is_prime(max_num + 1, true);
         is_prime[0] = is_prime[1] = false;
-        for(int i = 2; i * i < MAX_N; ++i) {
-            if(is_prime[i])
-                for(int j = i * i; j < MAX_N; j += i)
+        for (int i = 2; i * i <= max_num; ++i) {
+            if (is_prime[i]) {
+                for (int j = i * i; j <= max_num; j += i) {
                     is_prime[j] = false;
+                }
+            }
         }
+        vector<int> primes;
+        for (int i = 2; i <= max_num; ++i) {
+            if (is_prime[i]) primes.push_back(i);
+        }
+        return primes;
+    }
+
+    bool isPrime(int num, const unordered_set<int>& prime_set) {
+        return prime_set.count(num) > 0;
+    }
+    
+    vector<int> maximumCount(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        vector<int> res;
-        vector<int> arr = nums;
-        for(auto& q : queries) {
-            arr[q[0]] = q[1];
-            // Count distinct primes in prefix and suffix for each k
-            unordered_set<int> left, right;
-            vector<int> prefix(n), suffix(n);
-            for(int i=0; i<n; ++i) {
-                if(is_prime[arr[i]]) left.insert(arr[i]);
-                prefix[i] = left.size();
+        vector<int> results;
+        vector<int> all_primes = sieve(100000);
+        unordered_set<int> prime_set(all_primes.begin(), all_primes.end());
+
+        for (const auto& query : queries) {
+            int idx = query[0];
+            int val = query[1];
+            nums[idx] = val;
+
+            vector<unordered_set<int>> prefix_primes(n), suffix_primes(n);
+            unordered_set<int> seen_primes;
+
+            // Build prefix primes count
+            for (int i = 0; i < n; ++i) {
+                if (isPrime(nums[i], prime_set)) seen_primes.insert(nums[i]);
+                prefix_primes[i] = seen_primes;
             }
-            right.clear();
-            for(int i=n-1; i>=0; --i) {
-                if(is_prime[arr[i]]) right.insert(arr[i]);
-                suffix[i] = right.size();
+
+            seen_primes.clear();
+
+            // Build suffix primes count
+            for (int i = n - 1; i >= 0; --i) {
+                if (isPrime(nums[i], prime_set)) seen_primes.insert(nums[i]);
+                suffix_primes[i] = seen_primes;
             }
-            int mx = 0;
-            for(int k=1; k<n; ++k) {
-                int left_cnt = prefix[k-1];
-                int right_cnt = suffix[k];
-                mx = max(mx, left_cnt + right_cnt);
+
+            int max_distinct_primes = 0;
+
+            // Calculate max distinct primes between splits
+            for (int k = 1; k < n; ++k) {
+                int total_distinct_primes = prefix_primes[k-1].size() + suffix_primes[k].size();
+                max_distinct_primes = max(max_distinct_primes, total_distinct_primes);
             }
-            res.push_back(mx);
+
+            results.push_back(max_distinct_primes);
         }
-        return res;
+        return results;
     }
 };
 # @lc code=end
