bb91c609f923ab27867e902c6c735a509bdd192f
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 14:15:35 +0800

    Batch add 1 solutions: 3630.partition-array...

--- 3630.partition-array-for-maximum-xor-and-and.cpp
+++ 3630.partition-array-for-maximum-xor-and-and.cpp
@@ -1,32 +1,39 @@
-#
-# @lc app=leetcode id=3630 lang=cpp
-#
-# [3630] Partition Array for Maximum XOR and AND
-#
-# @lc code=start
 class Solution {
 public:
     long long maximizeXorAndXor(vector<int>& nums) {
         int n = nums.size();
-        long long ans = 0;
-        function<void(int, long long, long long, bool, long long)> dfs = [&](int idx, long long xora, long long andb, bool hasB, long long xorc) {
-            if (idx == n) {
-                long long sum = xora + (hasB ? andb : 0) + xorc;
-                ans = max(ans, sum);
-                return;
-            }
-            // Assign nums[idx] to A
-            dfs(idx+1, xora ^ nums[idx], andb, hasB, xorc);
-            // Assign nums[idx] to B
-            if (hasB)
-                dfs(idx+1, xora, andb & nums[idx], true, xorc);
-            else
-                dfs(idx+1, xora, nums[idx], true, xorc);
-            // Assign nums[idx] to C
-            dfs(idx+1, xora, andb, hasB, xorc ^ nums[idx]);
+        long long max_value = 0;
+
+        // Helper function to calculate XOR of a set
+        auto calculateXOR = [](const vector<int>& subset) {
+            int xor_value = 0;
+            for (int num : subset) xor_value ^= num;
+            return xor_value;
         };
-        dfs(0, 0, 0, false, 0);
-        return ans;
+
+        // Helper function to calculate AND of a set
+        auto calculateAND = [](const vector<int>& subset) {
+            if (subset.empty()) return 0;
+            int and_value = subset[0];
+            for (int i = 1; i < subset.size(); ++i) and_value &= subset[i];
+            return and_value;
+        };
+
+        // Try all possible partitions using bitmasks
+        for (int maskA = 0; maskA < (1 << n); ++maskA) {
+            for (int maskB = 0; maskB < (1 << n); ++maskB) {
+                if ((maskA & maskB) != 0) continue; // A and B must be disjoint
+                vector<int> A, B, C;
+                for (int i = 0; i < n; ++i) {
+                    if ((maskA & (1 << i)) != 0) A.push_back(nums[i]);
+                    else if ((maskB & (1 << i)) != 0) B.push_back(nums[i]);
+                    else C.push_back(nums[i]);
+                }
+                long long current_value = calculateXOR(A) + calculateAND(B) + calculateXOR(C);
+                max_value = max(max_value, current_value);
+            }
+        }
+
+        return max_value;
     }
 };
-# @lc code=end
