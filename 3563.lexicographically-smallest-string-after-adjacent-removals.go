15502b4ef4c8bd48832fa82f5f3dac711dbc6bdc
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 02:15:14 +0800

    Batch add 1 solutions: 3563.lexicographical...

--- 3563.lexicographically-smallest-string-after-adjacent-removals.go
+++ 3563.lexicographically-smallest-string-after-adjacent-removals.go
@@ -3,15 +3,16 @@
 #
 # [3563] Lexicographically Smallest String After Adjacent Removals
 #
+
 # @lc code=start
 func lexicographicallySmallestString(s string) string {
-    var stack []rune
+    stack := []rune{}
     for _, c := range s {
-        if len(stack) > 0 && (c == stack[len(stack)-1]+1 || c == stack[len(stack)-1]-1 || (c == 'a' && stack[len(stack)-1] == 'z') || (c == 'z' && stack[len(stack)-1] == 'a')) {
+        for len(stack) > 0 && (stack[len(stack)-1] == c+1 || stack[len(stack)-1] == c-1 || (stack[len(stack)-1] == 'a' && c == 'z') || (stack[len(stack)-1] == 'z' && c == 'a')) {
+            // Remove adjacent consecutive pairs considering circular adjacency
             stack = stack[:len(stack)-1]
-        } else {
-            stack = append(stack, c)
         }
+        stack = append(stack, c)
     }
     return string(stack)
 }
