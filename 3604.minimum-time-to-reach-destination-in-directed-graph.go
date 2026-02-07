3918c5f70a87c8f9abe8760ff60d51f95df33223
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 02:39:06 +0800

    Batch add 1 solutions: 3604.minimum-time-to...

--- 3604.minimum-time-to-reach-destination-in-directed-graph.go
+++ 3604.minimum-time-to-reach-destination-in-directed-graph.go
@@ -1,39 +1,61 @@
-#
-# @lc app=leetcode id=3604 lang=golang
-#
-# [3604] Minimum Time to Reach Destination in Directed Graph
-#
-
-# @lc code=start
-package main
-
-import (
-    "container/heap"
-    "math"
-)
+import "container/heap"
+import "math"
 
 type Edge struct {
-    target, start, end int
+    to    int
+    start int
+    end   int
 }
-
-type State struct {
-    node, time int
+type Item struct {
+    node int
+    time int64
 }
-type PriorityQueue []State
+type PriorityQueue []*Item
 func (pq PriorityQueue) Len() int { return len(pq) }
 func (pq PriorityQueue) Less(i, j int) bool { return pq[i].time < pq[j].time }
 func (pq PriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }
-func (pq *PriorityQueue) Push(x interface{}) { *pq = append(*pq, x.(State)) }
-func (pq *PriorityQueue) Pop() interface{} { old := *pq; n := len(old); item := old[n-1]; *pq = old[0 : n-1]; return item } 
-type Graph map[int][]Edge 
-func minTime(n int, edges [][]int) int { 
-graph := make(Graph) 
-for _, edge := range edges { 
-graph[edge[0]] = append(graph[edge[0]], Edge{edge[1], edge[2], edge[3]}) 
-} 
-dist := make([]int, n) 
-for i := range dist { dist[i] = math.MaxInt64 } 
-dist[0] = 0 
-pq := &PriorityQueue{{0, 0}} 
-heap.Init(pq) 
-for pq.Len() > 0 { state := heap.Pop(pq).(State) if state.node == n-1 { return state.time } if state.time > dist[state.node] { continue } for _, edge := range graph[state.node] { nextTime := max(state.time+1, edge.start) if nextTime <= edge.end && nextTime < dist[edge.target] { dist[edge.target] = nextTime heap.Push(pq, State{edge.target, nextTime}) }} } return -1 } func max(a, b int) int { if a > b { return a } else { return b }} # @lc code=end
+func (pq *PriorityQueue) Push(x interface{}) { *pq = append(*pq, x.(*Item)) }
+func (pq *PriorityQueue) Pop() interface{} {
+    old := *pq
+    n := len(old)
+    item := old[n-1]
+    *pq = old[0 : n-1]
+    return item
+}
+func minTime(n int, edges [][]int) int {
+    graph := make([][]Edge, n)
+    for _, edge := range edges {
+        u, v, start, end := edge[0], edge[1], edge[2], edge[3]
+        graph[u] = append(graph[u], Edge{v, start, end})
+    }
+
+    pq := &PriorityQueue{}
+    heap.Init(pq)
+    heap.Push(pq, &Item{node: 0, time: 0})
+
+    dist := make([]int64, n)
+    for i := range dist {
+        dist[i] = math.MaxInt64 // infinity
+    }
+    dist[0] = 0
+
+    for pq.Len() > 0 {
+        item := heap.Pop(pq).(*Item)
+        if item.time > dist[item.node] {
+            continue
+        }
+        for _, e := range graph[item.node] {
+            waitTime := max(item.time, e.start)
+            if waitTime <= e.end && waitTime+1 < dist[e.to] {
+                dist[e.to] = waitTime + 1
+                heap.Push(pq, &Item{node: e.to, time: dist[e.to]})
+            }
+        }
+    }
+
+    if dist[n-1] == math.MaxInt64 {
+        return -1
+    }
+    return int(dist[n-1])
+}
+type PriorityQueue []*Item
