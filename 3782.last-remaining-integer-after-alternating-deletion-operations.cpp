8e79e9945700d3bfd830268d0be0a2ef283b1f11
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 20:50:22 +0800

    Batch add 1 solutions: 3782.last-remaining-...

--- 3782.last-remaining-integer-after-alternating-deletion-operations.cpp
+++ 3782.last-remaining-integer-after-alternating-deletion-operations.cpp
@@ -1,3 +1,4 @@
+#
 # @lc app=leetcode id=3782 lang=cpp
 #
 # [3782] Last Remaining Integer After Alternating Deletion Operations
@@ -6,8 +7,18 @@
 class Solution {
 public:
     long long lastInteger(long long n) {
-        if (n == 1) return 1;
-        return 2 * lastInteger(n / 2) - (n % 2 == 0 ? 1 : -1);
+        long long head = 1;
+        long long step = 1;
+        bool leftToRight = true;
+        while (n > 1) {
+            if (leftToRight || n % 2 == 1) {
+                head += step;
+            }
+            n /= 2;
+            step *= 2;
+            leftToRight = !leftToRight;
+        }
+        return head;
     }
 };
 # @lc code=end
