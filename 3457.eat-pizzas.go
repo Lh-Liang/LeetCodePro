e9dbf4583290250a78826f9a6a33867e763ebbce
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 01:10:44 +0800

    Batch add 1 solutions: 3457.eat-pizzas.go...

--- 3457.eat-pizzas.go
+++ 3457.eat-pizzas.go
@@ -3,18 +3,20 @@
 #
 # [3457] Eat Pizzas!
 #
-
 # @lc code=start
 func maxWeight(pizzas []int) int64 {
-    n := len(pizzas)
+    // Sort pizzas to facilitate optimal selection
     sort.Ints(pizzas)
-    var totalWeight int64 = 0
-    
+    n := len(pizzas)
+    totalWeight := int64(0)
+    // Iterate through batches of 4 pizzas each day
     for i := 0; i < n; i += 4 {
-        if (i/4)%2 == 0 { // Odd day (1-indexed) since i starts from 0
-            totalWeight += int64(pizzas[i+3]) // Gain weight of Z
-        } else { // Even day
-            totalWeight += int64(pizzas[i+2]) // Gain weight of Y
+        if (i/4)%2 == 0 {
+            // Odd-numbered days: Gain weight Z (maximum of 4)
+            totalWeight += int64(pizzas[i+3])
+        } else {
+            // Even-numbered days: Gain weight Y (second maximum of 4)
+            totalWeight += int64(pizzas[i+2])
         }
     }
     return totalWeight
