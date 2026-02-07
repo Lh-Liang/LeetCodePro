83ebefc93935f25e46c20982b3ab8e556e0ec54a
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 20:27:27 +0800

    Batch add 1 solutions: 3729.count-distinct-...

--- 3729.count-distinct-subarrays-divisible-by-k-in-sorted-array.cpp
+++ 3729.count-distinct-subarrays-divisible-by-k-in-sorted-array.cpp
@@ -1,1 +1,27 @@
-#\n# @lc app=leetcode id=3729 lang=cpp\n#\n# [3729] Count Distinct Subarrays Divisible by K in Sorted Array\n#\n# @lc code=start\nclass Solution {\npublic:\n    long long numGoodSubarrays(vector<int>& nums, int k) {\n        unordered_map<int, int> prefixCount; // To keep track of mod values\n        unordered_set<int> seenPrefixMods; // To track unique prefix sums mod k\n        int n = nums.size();\n        long long count = 0;\n        int prefixSum = 0;\n        prefixCount[0] = 1; // Initial case for whole array divisible by k\n        for (int i = 0; i < n; ++i) {\n            prefixSum += nums[i];\n            int modValue = ((prefixSum % k) + k) % k; // Non-negative modulo adjustment\n            if (prefixCount[modValue] > 0 && seenPrefixMods.find(modValue) == seenPrefixMods.end()) { \n                ++count; \n                seenPrefixMods.insert(modValue); \n            } \n            ++prefixCount[modValue]; // Update count for this modValue in map\n        }\n        return count; // Return total number of distinct good subarrays found\n    } \nt};\nt# @lc code=end
+#
+# @lc app=leetcode id=3729 lang=cpp
+#
+# [3729] Count Distinct Subarrays Divisible by K in Sorted Array
+#
+
+# @lc code=start
+class Solution {
+public:
+    long long numGoodSubarrays(vector<int>& nums, int k) {
+        unordered_map<int, int> mod_count;
+        mod_count[0] = 1; // base case for a complete subarray itself being good
+        int cum_sum = 0;
+        int count = 0;
+        for (int num : nums) {
+            cum_sum += num;
+            int mod_value = cum_sum % k;
+            if (mod_value < 0) mod_value += k; // handle negative mods due to negative numbers or subtraction
+            if (mod_count.find(mod_value) != mod_count.end()) {
+                count += mod_count[mod_value]; // add the frequency of this mod value seen before
+            }
+            mod_count[mod_value]++; // increment the frequency of this mod value in the map
+        }
+        return count;
+    }
+};
+# @lc code=end
