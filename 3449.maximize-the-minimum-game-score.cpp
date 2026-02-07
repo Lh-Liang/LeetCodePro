0a74743f7837eaf473e534451fdec6703ef3dd4d
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 12:33:46 +0800

    Batch add 1 solutions: 3449.maximize-the-mi...

--- 3449.maximize-the-minimum-game-score.cpp
+++ 3449.maximize-the-minimum-game-score.cpp
@@ -9,25 +9,29 @@ class Solution {
 public:
     long long maxScore(vector<int>& points, int m) {
         int n = points.size();
-        long long left = 0, right = 1e15, answer = 0;
-        while (left <= right) {
-            long long mid = left + (right - left) / 2;
-            // Calculate how many moves are needed to make all gameScore[i] >= mid
-            long long moves = 0;
-            for (int i = 0; i < n; ++i) {
-                // To get gameScore[i] >= mid, need at least ceil(mid / points[i]) visits
-                long long need = (mid + points[i] - 1) / points[i];
-                moves += need;
+        long long left = 0, right = accumulate(points.begin(), points.end(), 0LL);
+        
+        auto canAchieve = [&](long long minScore) {
+            vector<long long> tempScore(n, 0);
+            long long totalMoves = 0;
+            for (int i = 0; i < n && totalMoves <= m; ++i) {
+                if (tempScore[i] < minScore) {
+                    long long needed = minScore - tempScore[i];
+                    totalMoves += needed / points[i] + ((needed % points[i]) ? 1 : 0);
+                }
             }
-            // One move transitions from -1 to 0, so moves - 1 transitions
-            if (moves - 1 <= m) {
-                answer = mid;
-                left = mid + 1;
+            return totalMoves <= m;
+        };
+        
+        while (left < right) {
+            long long mid = left + (right - left + 1) / 2;
+            if (canAchieve(mid)) {
+                left = mid;
             } else {
                 right = mid - 1;
             }
         }
-        return answer;
+        return left;
     }
 };
 # @lc code=end
