f25fefaf2558739e3a3a5b6ed958cc79be37baf9
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 11:47:23 +0800

    Batch add 1 solutions: 2296.design-a-text-e...

--- 2296.design-a-text-editor.cpp
+++ 2296.design-a-text-editor.cpp
@@ -1,72 +1,11 @@
-#
-# @lc app=leetcode id=2296 lang=cpp
-#
-# [2296] Design a Text Editor
-#
-
-# @lc code=start
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
-class TextEditor {
-    // left stores characters to the left of the cursor
-    // right stores characters to the right of the cursor in reverse order
-    string left, right;
-
-public:
-    TextEditor() {
-    }
-    
-    void addText(string text) {
-        // O(text.length) - Appending to the end of left string
-        left += text;
-    }
-    
-    int deleteText(int k) {
-        // O(k) - Removing from the end of left string
-        int actual_deleted = min(k, (int)left.size());
-        for (int i = 0; i < actual_deleted; ++i) {
-            left.pop_back();
-        }
-        return actual_deleted;
-    }
-    
-    string cursorLeft(int k) {
-        // O(k) - Moving characters from left to right stack
-        int move_count = min(k, (int)left.size());
-        for (int i = 0; i < move_count; ++i) {
-            right.push_back(left.back());
-            left.pop_back();
-        }
-        return getLeft10();
-    }
-    
-    string cursorRight(int k) {
-        // O(k) - Moving characters from right to left stack
-        int move_count = min(k, (int)right.size());
-        for (int i = 0; i < move_count; ++i) {
-            left.push_back(right.back());
-            right.pop_back();
-        }
-        return getLeft10();
-    }
-
-private:
-    // Helper to get up to 10 characters to the left of the cursor
-    string getLeft10() {
-        int len = min(10, (int)left.size());
-        return left.substr(left.size() - len);
-    }
-};
+#\n# @lc app=leetcode id=2296 lang=cpp\n#\n# [2296] Design a Text Editor\n#\n# @lc code=start\nclass TextEditor {\npublic:\n    std::string leftText;\n    std::string rightText;\n    \n    TextEditor() {}\n    \n    void addText(std::string text) {\n        leftText += text;\n    }\n    \n    int deleteText(int k) {\n        int count = std::min(k, static_cast<int>(leftText.size()));\n        leftText.erase(leftText.end() - count, leftText.end());\n        return count;\n    }\n    \n    std::string cursorLeft(int k) {\n        int moveCount = std::min(k, static_cast<int>(leftText.size()));\n        rightText.insert(rightText.begin(), leftText.end() - moveCount, leftText.end());\n        leftText.erase(leftText.end() - moveCount, leftText.end());\n        return getLeftContext();   \nn}\nnstd::string cursorRight(int k) {nnint moveCount = std::min(k, static_cast<int>(rightText.size()));nnleftText.append(rightText.begin(), rightText.begin() + moveCount);nnrightText.erase(rightText.begin(), rightText.begin() + moveCount);nnreturn getLeftContext();   \nn}nnprivate:nnstd::string getLeftContext() const {nnint len = std::min(10, static_cast<int>(leftText.size()));nnreturn leftText.substr(leftText.size() - len); nn}\nn};
 
 /**
  * Your TextEditor object will be instantiated and called as such:
  * TextEditor* obj = new TextEditor();
  * obj->addText(text);
  * int param_2 = obj->deleteText(k);
- * string param_3 = obj->cursorLeft(k);
- * string param_4 = obj->cursorRight(k);
+ * std::string param_3 = obj->cursorLeft(k);
+ * std::string param_4 = obj->cursorRight(k);
  */
 # @lc code=end
