c9aee758f6ba88bfe7a3b2051981255bc7f669f6
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-08 06:06:56 +0800

    Batch add 1 solutions: 3646.next-special-pa...

--- 3646.next-special-palindrome-number.go
+++ 3646.next-special-palindrome-number.go
@@ -5,47 +5,75 @@
 #
 # @lc code=start
 import (
+    "fmt"
     "strconv"
 )
 
 func specialPalindrome(n int64) int64 {
-    // Helper function to check if a number is special
-    hasValidDigitCounts := func(num int64) bool {
-        counts := make(map[rune]int)
-        s := strconv.FormatInt(num, 10)
-        for _, digit := range s {
-            counts[digit]++
-        }
-        for digit, count := range counts {
-            if int(digit-'0') != count { 
+    // Helper: Check if a string is a palindrome
+    isPalindrome := func(s string) bool {
+        for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+            if s[i] != s[j] {
                 return false
             }
         }
         return true
     }
-
-    // Generate next palindrome candidate greater than n
-    generateNextPalindrome := func(num int64) int64 {
-        s := strconv.FormatInt(num, 10)
-        length := len(s)
-        halfLength := length / 2
-        incremented := false
-        var candidate string
-        if length%2 == 0 {
-            firstHalf, _ := strconv.ParseInt(s[:halfLength], 10, 64)
-            firstHalf++
-            incremented = true
-            firstHalfStr := strconv.FormatInt(firstHalf, 10)
-            candidate = firstHalfStr + reverseString(firstHalfStr)
-        } else {
-            middleIndex := halfLength
-            firstHalf, _ := strconv.ParseInt(s[:middleIndex+1], 10, 64)
-            firstHalf++
-            incremented = true
-            firstHalfStr := strconv.FormatInt(firstHalf, 10)
-            candidate = firstHalfStr + reverseString(firstHalfStr[:middleIndex])
+    // Helper: Check if a string is special (digits 1..9 appear exactly k times, 0 must not appear)
+    isSpecial := func(s string) bool {
+        freq := make([]int, 10)
+        for i := 0; i < len(s); i++ {
+            d := s[i] - '0'
+            freq[d]++
         }
-        result, _ := strconv.ParseInt(candidate, 10, 64)
-        return result
+        if freq[0] > 0 {
+            return false
+        }
+        for k := 1; k <= 9; k++ {
+            if freq[k] > 0 && freq[k] != k {
+                return false
+            }
+        }
+        return true
     }
-    	// Reverse string utility function	reverseString := func(s string) string {		runes := []rune(s)		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {			runes[i], runes[j] = runes[j], runes[i]		}		return string(runes)	}		candidate := n + 1	for {	candidate = generateNextPalindrome(candidate) // generate next palindrome	if hasValidDigitCounts(candidate){	return candidate	}	candidate++ // ensure we are always moving forward	}	}
+    
+    sn := strconv.FormatInt(n+1, 10)
+    maxLen := 18
+    // Try increasing lengths, starting from len(sn)
+    for length := len(sn); length <= maxLen; length++ {
+        // Generate all palindromes of this length
+        halfLen := (length + 1) / 2
+        start, end := 1, 9
+        for i := 1; i < halfLen; i++ {
+            start *= 10
+            end = end*10 + 9
+        }
+        for half := start; half <= end; half++ {
+            halfStr := fmt.Sprintf("%0*d", halfLen, half)
+            // Build palindrome
+            var p string
+            if length%2 == 0 {
+                rev := reverse(halfStr)
+                p = halfStr + rev
+            } else {
+                rev := reverse(halfStr[:len(halfStr)-1])
+                p = halfStr + rev
+            }
+            // Convert to int and check constraints
+            num, _ := strconv.ParseInt(p, 10, 64)
+            if num > n && isSpecial(p) {
+                return num
+            }
+        }
+    }
+    return -1
+}
+// Helper: reverse a string
+func reverse(s string) string {
+    r := []byte(s)
+    for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+        r[i], r[j] = r[j], r[i]
+    }
+    return string(r)
+}
+# @lc code=end
