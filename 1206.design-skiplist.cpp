8f2a3b17d64ea9510fcfaf9da60b1c58d4020281
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 11:41:57 +0800

    Batch add 1 solutions: 1206.design-skiplist...

--- 1206.design-skiplist.cpp
+++ 1206.design-skiplist.cpp
@@ -1,109 +1,54 @@
-#
+# 
 # @lc app=leetcode id=1206 lang=cpp
-#
+# 
 # [1206] Design Skiplist
-#
-
+# 
 # @lc code=start
 #include <vector>
 #include <cstdlib>
-
+#include <ctime>
 using namespace std;
 
-struct Node {
-    int val;
-    vector<Node*> next;
-    Node(int v, int level) : val(v), next(level, nullptr) {}
+class Node {
+public:
+    int value;
+    vector<Node*> forward;
+    Node(int val, int level) : value(val), forward(level + 1, nullptr) {}
 };
 
 class Skiplist {
 private:
     Node* head;
-    const int MAX_LEVEL = 16;
-    const float P = 0.5;
-
+    int maxLevel;
+    float probability;
     int randomLevel() {
-        int lv = 1;
-        while (lv < MAX_LEVEL && (float)rand() / RAND_MAX < P) {
-            lv++;
+        int lvl = 0;
+        while ((rand() / double(RAND_MAX)) < probability && lvl < maxLevel) {
+            lvl++;
         }
-        return lv;
+        return lvl;
     }
-
 public:
-    Skiplist() {
-        head = new Node(-1, MAX_LEVEL);
+    Skiplist() : maxLevel(16), probability(0.5) {
+        srand(time(0));
+        head = new Node(-1, maxLevel);
     }
     
-    ~Skiplist() {
-        Node* curr = head;
-        while (curr) {
-            Node* temp = curr->next[0];
-            delete curr;
-            curr = temp;
-        }
-    }
-
-    bool search(int target) {
-        Node* curr = head;
-        for (int i = MAX_LEVEL - 1; i >= 0; i--) {
-            while (curr->next[i] && curr->next[i]->val < target) {
-                curr = curr->next[i];
+bool search(int target) {
+        Node* current = head;
+        for (int i = maxLevel; i >= 0; i--) {
+            while (current->forward[i] && current->forward[i]->value < target) {
+                current = current->forward[i];
             }
         }
-        curr = curr->next[0];
-        return curr != nullptr && curr->val == target;
+        current = current->forward[0];
+        return current != nullptr && current->value == target;
     }
-
-    void add(int num) {
-        vector<Node*> update(MAX_LEVEL);
-        Node* curr = head;
-        for (int i = MAX_LEVEL - 1; i >= 0; i--) {
-            while (curr->next[i] && curr->next[i]->val < num) {
-                curr = curr->next[i];
-            }
-            update[i] = curr;
-        }
-
-        int lv = randomLevel();
-        Node* newNode = new Node(num, lv);
-        for (int i = 0; i < lv; i++) {
-            newNode->next[i] = update[i]->next[i];
-            update[i]->next[i] = newNode;
-        }
-    }
-
-    bool erase(int num) {
-        vector<Node*> update(MAX_LEVEL);
-        Node* curr = head;
-        for (int i = MAX_LEVEL - 1; i >= 0; i--) {
-            while (curr->next[i] && curr->next[i]->val < num) {
-                curr = curr->next[i];
-            }
-            update[i] = curr;
-        }
-
-        curr = curr->next[0];
-        if (curr == nullptr || curr->val != num) {
-            return false;
-        }
-
-        for (int i = 0; i < MAX_LEVEL; i++) {
-            if (update[i]->next[i] != curr) {
-                break;
-            }
-            update[i]->next[i] = curr->next[i];
-        }
-        delete curr;
-        return true;
-    }
-};
-
-/**
- * Your Skiplist object will be instantiated and called as such:
- * Skiplist* obj = new Skiplist();
- * bool param_1 = obj->search(target);
- * obj->add(num);
- * bool param_3 = obj->erase(num);
- */
-# @lc code=end
+    
+bool add(int num) {
+g vector<Node*> update(maxLevel + 1);
+g vector<Node*> update(maxLevel + 1);
+d add(int num);current = head;current = head;for (int i = maxLevel; i >= 0; i--) {while (current->forward[i] && current->forward[i]->value < num) {current = current->forward[i];}update[i] = current;}int lvl = randomLevel();Node* newNode = new Node(num, lvl);for (int i = 0; i <= lvl; i++) {newNode->forward[i] = update[i]->forward[i];update[i]->forward[i] = newNode;}return true;
+bool erase(int num) {
+erase(num);bool erase(num);erase(num);bool erase(num);erase(num);erase(num);
+erase(num);}
