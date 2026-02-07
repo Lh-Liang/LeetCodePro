773d6bb4391f26e7d1bde06071c6c371547307d0
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 13:09:41 +0800

    Batch add 1 solutions: 3513.number-of-uniqu...

--- 3513.number-of-unique-xor-triplets-i.cpp
+++ 3513.number-of-unique-xor-triplets-i.cpp
@@ -1,25 +1,13 @@
-#
 # @lc app=leetcode id=3513 lang=cpp
 #
 # [3513] Number of Unique XOR Triplets I
 #
-
 # @lc code=start
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        int n = nums.size();
-        set<int> unique;
-        for (int i = 0; i < n; ++i) {
-            int x = 0;
-            for (int j = i; j < n; ++j) {
-                for (int k = j; k < n; ++k) {
-                    int val = nums[i] ^ nums[j] ^ nums[k];
-                    unique.insert(val);
-                }
-            }
-        }
-        return unique.size();
+        // Since nums is a permutation of [1, n], all numbers from 0 to n-1 can be formed by XOR.
+        return nums.size();
     }
 };
 # @lc code=end
