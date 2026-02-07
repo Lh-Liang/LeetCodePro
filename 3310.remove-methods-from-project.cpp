54d241dbfd27bf9dea848508bf8523ddcd03c702
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 11:52:16 +0800

    Batch add 1 solutions: 3310.remove-methods-...

--- 3310.remove-methods-from-project.cpp
+++ 3310.remove-methods-from-project.cpp
@@ -3,60 +3,53 @@
 #
 # [3310] Remove Methods From Project
 #
-
 # @lc code=start
-#include <vector>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
-        // Build adjacency list for reachability
-        vector<vector<int>> adj(n);
-        for (const auto& inv : invocations) {
-            adj[inv[0]].push_back(inv[1]);
+        // Initialize adjacency lists for direct and reverse graphs
+        vector<vector<int>> adjList(n), reverseAdjList(n);
+        for (auto& invocation : invocations) {
+            adjList[invocation[0]].push_back(invocation[1]);
+            reverseAdjList[invocation[1]].push_back(invocation[0]);
         }
-
-        // Find all suspicious methods using BFS
-        vector<bool> is_suspicious(n, false);
+        
+        // Perform BFS/DFS from method k to find all reachable/suspicious nodes
+        vector<bool> isSuspicious(n, false);
         queue<int> q;
         q.push(k);
-        is_suspicious[k] = true;
-
+        isSuspicious[k] = true;
         while (!q.empty()) {
-            int curr = q.front();
-            q.pop();
-            for (int neighbor : adj[curr]) {
-                if (!is_suspicious[neighbor]) {
-                    is_suspicious[neighbor] = true;
+            int current = q.front(); q.pop();
+            for (int neighbor : adjList[current]) {
+                if (!isSuspicious[neighbor]) {
+                    isSuspicious[neighbor] = true;
                     q.push(neighbor);
                 }
             }
         }
 
-        // Check if any non-suspicious method invokes a suspicious method
-        bool can_remove = true;
-        for (const auto& inv : invocations) {
-            if (!is_suspicious[inv[0]] && is_suspicious[inv[1]]) {
-                can_remove = false;
-                break;
+        // Check if any non-suspicious method invokes a suspicious one
+        for (int i = 0; i < n; ++i) {
+            if (!isSuspicious[i]) {
+                for (int neighbor : reverseAdjList[i]) {
+                    if (isSuspicious[neighbor]) {
+                        // If any such invocation exists, we cannot safely remove the suspicious group.
+                        return vector<int>(begin({0}), end({0}) + n); // return all methods
+                    }
+                }
             }
         }
 
-        // Prepare the result list
-        vector<int> res;
-        if (can_remove) {
-            for (int i = 0; i < n; ++i) {
-                if (!is_suspicious[i]) res.push_back(i);
-            }
-        } else {
-            for (int i = 0; i < n; ++i) {
-                res.push_back(i);
+        // Collect non-suspicious methods
+        vector<int> remainingMethods;
+        for (int i = 0; i < n; ++i) {
+            if (!isSuspicious[i]) {
+                remainingMethods.push_back(i);
             }
         }
-        return res;
+
+        return remainingMethods;
     }
 };
 # @lc code=end
