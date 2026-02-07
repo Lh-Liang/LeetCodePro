10e74c830b4081debe48f3369d07fc1a5553a497
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-02-07 11:57:26 +0800

    Batch add 1 solutions: 3367.maximize-sum-of...

--- 3367.maximize-sum-of-weights-after-edge-removals.cpp
+++ 3367.maximize-sum-of-weights-after-edge-removals.cpp
@@ -7,43 +7,42 @@
 class Solution {
 public:
     long long maximizeSumOfWeights(vector<vector<int>>& edges, int k) {
-        int n = edges.size() + 1;
-        vector<vector<pair<int,int>>> adj(n); // node, edgeId
-        vector<int> degree(n, 0);
-        vector<int> toU(edges.size()), toV(edges.size()), weight(edges.size());
-        for (int i = 0; i < edges.size(); ++i) {
-            int u = edges[i][0], v = edges[i][1], w = edges[i][2];
-            adj[u].push_back({v, i});
-            adj[v].push_back({u, i});
-            degree[u]++; degree[v]++;
-            toU[i] = u; toV[i] = v; weight[i] = w;
-        }
-        vector<bool> removed(edges.size(), false);
-        for (int node = 0; node < n; ++node) {
-            if (degree[node] > k) {
-                vector<pair<int,int>> incEdges; // (weight, edgeId)
-                for (auto& [nei, eid] : adj[node]) {
-                    if (!removed[eid])
-                        incEdges.push_back({weight[eid], eid});
-                }
-                sort(incEdges.begin(), incEdges.end());
-                int toRemove = degree[node] - k;
-                for (int j = 0, cnt = 0; cnt < toRemove && j < incEdges.size(); ++j) {
-                    int eid = incEdges[j].second;
-                    if (!removed[eid]) {
-                        removed[eid] = true;
-                        degree[toU[eid]]--;
-                        degree[toV[eid]]--;
-                        cnt++;
-                    }
-                }
+        // Sort edges based on weights in descending order
+        sort(edges.begin(), edges.end(), [](const vector<int>& a, const vector<int>& b) { return a[2] > b[2]; });
+        
+        int n = edges.size() + 1;  // since there are n-1 edges for n nodes
+        vector<int> parent(n), rank(n, 0), degree(n, 0);
+        iota(parent.begin(), parent.end(), 0);  // Initialize parent for each node
+
+        function<int(int)> find = [&](int u) {
+            if (parent[u] != u) {
+                parent[u] = find(parent[u]);  // Path compression
+            }
+            return parent[u];
+        };
+
+        auto unionSets = [&](int u, int v) {
+            int rootU = find(u);
+            int rootV = find(v);
+            if (rootU != rootV) {
+                if (rank[rootU] < rank[rootV]) swap(rootU, rootV);
+                parent[rootV] = rootU;
+                if (rank[rootU] == rank[rootV]) rank[rootU]++;
+                return true;
+            }
+            return false;
+        };
+
+        long long maxWeight = 0;
+        for (const auto& edge : edges) {
+            int u = edge[0], v = edge[1], w = edge[2];
+            if (degree[u] < k && degree[v] < k && unionSets(u, v)) {
+                maxWeight += w;
+                degree[u]++;
+                degree[v]++;
             }
         }
-        long long res = 0;
-        for (int i = 0; i < edges.size(); ++i) {
-            if (!removed[i]) res += weight[i];
-        }
-        return res;
+        return maxWeight;
     }
 };
 # @lc code=end
