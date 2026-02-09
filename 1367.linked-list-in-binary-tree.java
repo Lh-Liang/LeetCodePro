6434f7ba21dc7a452a8fa3411e536e5e0554d62a
liang-lh <liang-lh@users.noreply.github.com>
2026-02-10 04:15:39 +0800

    Batch add 1 solutions: 1367.linked-list-in-...

--- 1367.linked-list-in-binary-tree.java
+++ 1367.linked-list-in-binary-tree.java
@@ -31,13 +31,15 @@
 */
 class Solution {
     public boolean isSubPath(ListNode head, TreeNode root) {
+        if (head == null) return true;
         if (root == null) return false;
-        return dfs(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+        if (dfs(head, root)) return true;
+        return isSubPath(head, root.left) || isSubPath(head, root.right);
     }
     private boolean dfs(ListNode head, TreeNode node) {
         if (head == null) return true;
         if (node == null) return false;
-        if (node.val != head.val) return false;
+        if (head.val != node.val) return false;
         return dfs(head.next, node.left) || dfs(head.next, node.right);
     }
 }
